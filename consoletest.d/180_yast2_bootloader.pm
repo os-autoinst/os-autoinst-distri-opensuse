7bc8d12c54e3b54f4fc2436d6386a91b613454b1
Max Lin <mlin@suse.com>
2014-02-20 12:20:17 +0800

    use waitforneedle to wait yast2_bootloader initialed, put some free time on after take the screenshot with following clear screen

--- consoletest.d/180_yast2_bootloader.pm
+++ consoletest.d/180_yast2_bootloader.pm
@@ -12,14 +12,21 @@ sub run()
 {
 	my $self=shift;
 	script_sudo("/sbin/yast2 bootloader");
+	while (1) {
+	  my $ret = waitforneedle("yast2_bootloader-initialed", 300);
+	  last if $ret->{needle}->has_tag("yast2_bootloader-initialed");
+	}
+	waitstillimage(12,60);
 	sendkey "ctrl-l"; # redraw in case kernel painted on us
-	sleep 3;
+	sleep 2;
 	$self->check_screen;
+	waitidle 5;
 	sendkey "alt-o"; # OK => Close # might just close warning on livecd
 	sleep 2;
 	sendkey "alt-o"; # OK => Close
-	waitidle;
+	waitstillimage(16,60);
 	$self->take_screenshot;
+	waitidle 5;
 	sendkey "ctrl-l";
 	script_run('echo $?');
 	waitforneedle("exited-bootloader", 2);
