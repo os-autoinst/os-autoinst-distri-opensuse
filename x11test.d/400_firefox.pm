27921f73b94d4194b68bd839b9bb6dfaaf58883f
Max Lin <mlin@suse.com>
2014-03-13 20:09:37 +0800

    default browser confiramation pop-up is gone, don't need confirm it

--- x11test.d/400_firefox.pm
+++ x11test.d/400_firefox.pm
@@ -8,10 +8,6 @@ sub run()
 	x11_start_program("firefox");
 	$self->check_screen;
 	if($ENV{UPGRADE}) { sendkey("alt-d");waitidle; } # dont check for updated plugins
-	if($ENV{DESKTOP}=~/xfce|lxde/i) {
-		sendkey "ret"; # confirm default browser setting popup
-		waitidle;
-	}
 	if(0) { # 4.0b10 changed default value - b12 has showQuitWarning
 		sendkey "ctrl-t"; sleep 1;
 		sendautotype "about:config\n"; sleep 1;
@@ -20,6 +16,8 @@ sub run()
 		sendkey "ret"; waitidle;
 		sendkey "ctrl-w"; sleep 1;
 	}
+	# just leave it here, then don't need modify test-firefox-2 and test-firefox-3
+	# tag in all related needles
 	$self->check_screen;
 	sendkey "alt-h"; sleep 2;	# Help
 	sendkey "a"; sleep 2;		# About
