9ed57a99a0c6b638cfce2252b3d5d0f5d2126473
Stephan Kulow <coolo@suse.de>
2014-05-13 00:30:04 +0200

    simplify bootloader test to make it less fragile

--- consoletest.d/180_yast2_bootloader.pm
+++ consoletest.d/180_yast2_bootloader.pm
@@ -11,24 +11,12 @@ sub is_applicable() {
 sub run() {
     my $self = shift;
     script_sudo("/sbin/yast2 bootloader");
-    while (1) {
-        my $ret = assert_screen "yast2_bootloader-initialed", 300;
-        last if $ret->{needle}->has_tag("yast2_bootloader-initialed");
-    }
-    waitstillimage( 12, 60 );
-    send_key "ctrl-l";    # redraw in case kernel painted on us
-    sleep 2;
-    assert_screen 'test-yast2_bootloader-1', 3;
-    waitidle 5;
-    send_key "alt-o";     # OK => Close # might just close warning on livecd
-    sleep 2;
+    my $ret = assert_screen "test-yast2_bootloader-1", 300;
     send_key "alt-o";     # OK => Close
-    waitstillimage( 16, 60 );
-    $self->take_screenshot;
-    waitidle 5;
+    assert_screen 'exited-bootloader', 15;
     send_key "ctrl-l";
-    script_run('echo $?');
-    assert_screen "exited-bootloader", 2;
+    script_run('echo "EXIT-$?" > /dev/serial');
+    die unless waitserial( "EXIT-0", 2 );
     script_run('rpm -q hwinfo');
     $self->take_screenshot;
 }
