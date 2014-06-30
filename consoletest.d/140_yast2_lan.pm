f13cab698655fbb6b68c684a3c1aa849a3d4e5e0
Stephan Kulow <coolo@suse.de>
2014-06-30 15:06:39 +0200

    Do not change NetworkManager

--- consoletest.d/140_yast2_lan.pm
+++ consoletest.d/140_yast2_lan.pm
@@ -7,38 +7,31 @@ use bmwqemu;
 sub run() {
     my $self = shift;
     script_sudo("/sbin/yast2 lan");
-    waitstillimage();
 
-    # FIXME: add assert_screen here
-    save_screenshot;
-    if ( $vars{LIVETEST} || $vars{DISTRI} eq "sled-11" || $vars{LAPTOP} ) {
-        send_key "ret";      # confirm networkmanager popup
-        sleep 1;
-        send_key "alt-t";    # traditional ifup
-        sleep 1;
+    my $ret = assert_screen [qw/Networkmanager_controlled yast2_lan/], 12;
+    if($ret->{needle}->has_tag('Networkmanager_controlled')) {
+      send_key "ret";      # confirm networkmanager popup
+      assert_screen "Networkmanager_controlled-approved";
+      send_key "alt-c";
+      assert_screen 'yast2-lan-exited', 30;
+      return; # don't change any settings
     }
 
     my $hostname = "susetest";
     my $domain   = "zq1.de";
 
     send_key "alt-s";       # open hostname tab
-    sleep 2;
+    assert_screen "yast2_lan-hostname-tab";
     send_key "tab";
     for ( 1 .. 15 ) { send_key "backspace" }
     type_string $hostname;
     send_key "tab";
     for ( 1 .. 15 ) { send_key "backspace" }
     type_string $domain;
-    sleep 5;
-    assert_screen 'test-yast2_lan-1', 3;
-    send_key "alt-o";       # confirm possible network manager warning
-    send_key "alt-o";       # OK=>Save&Exit
-    sleep 20;
-    wait_idle;
-    wait_idle 180;
+    assert_screen 'test-yast2_lan-1', 8;
 
-    send_key "ret";
-    save_screenshot;
+    send_key "alt-o";       # OK=>Save&Exit
+    assert_screen 'yast2-lan-exited', 30;
 
     send_key "ctrl-l";      # clear screen
     script_run('echo $?');
