6e5af4176a8ec2f8780bbc483708ff567df314f0
Max Lin <mlin@suse.com>
2014-07-08 14:00:52 +0800

    disable Factory repos for staging tests

--- consoletest.d/120_zypper_ref.pm
+++ consoletest.d/120_zypper_ref.pm
@@ -8,7 +8,27 @@ sub run() {
     type_string "PS1=\"# \"\n";
     script_run("killall gpk-update-icon kpackagekitsmarticon packagekitd");
     sleep 2;
-    script_run("zypper lr");
+    script_run("zypper lr -d");
+    save_screenshot; # take a screenshot before any changes
+    send_key "ctrl-l";
+    if ( $vars{FLAVOR} =~ m/^Staging2?[\-]DVD$/ && $vars{SUSEMIRROR} ) {
+        # remove Factory repos
+        my $repos_folder = '/etc/zypp/repos.d';
+        script_run("find $repos_folder/*.repo -type f -exec grep -q 'baseurl=http://download.opensuse.org/' {} \\; -delete", 5);
+        script_run("zypper lr -d");
+        save_screenshot; # take a screenshot after Factory repos removed
+    }
+    if ( !$vars{NET} && !$vars{TUMBLEWEED} && !$vars{EVERGREEN} && $vars{SUSEMIRROR} ) {
+        # non-NET installs have only milestone repo, which might be incompatible.
+        my $repourl = 'http://' . $vars{SUSEMIRROR};
+        unless ( $vars{FULLURL} ) {
+            $repourl = $repourl . "/repo/oss";
+        }
+        script_run("zypper ar -c $repourl Factory && echo 'worked' > /dev/$serialdev");
+        wait_serial "worked", 10  || die "zypper failed";
+    }
+    script_run("zypper lr -d");
+    save_screenshot; # take a screenshot after the staging repo added
     script_run("zypper ref");
     script_run('echo $?');
     assert_screen("zypper_ref");
