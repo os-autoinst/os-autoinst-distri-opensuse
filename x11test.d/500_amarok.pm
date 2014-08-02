939d7e56a3ac7007c8bb1ea0c6fdafc06613198d
Stephan Kulow <coolo@suse.de>
2014-08-02 17:18:35 +0200

    rework amarok test

--- x11test.d/500_amarok.pm
+++ x11test.d/500_amarok.pm
@@ -8,18 +8,15 @@ sub is_applicable() {
 sub run() {
     my $self = shift;
     ensure_installed("amarok");
-    $self->start_audiocapture;
-    x11_start_program("amarok http://openqa.opensuse.org/opensuse/audio/1d5d9dD.oga");
-    sleep 3;
-    $self->assert_DTMF('159D');
-    sleep 2;
+    x11_start_program("amarok");
     assert_screen 'test-amarok-1', 3;
     send_key "alt-y";    # use music path as collection folder
     assert_screen 'test-amarok-2', 3;
+    $self->start_audiocapture;
+    x11_start_program("amarok -l http://$vars{OPENQA_HOSTNAME}/test-data/$vars{DISTRI}/data/1d5d9dD.oga");
+    assert_screen 'test-amarok-3', 10;
+    $self->assert_DTMF('159D');
     send_key "ctrl-q";    # really quit (alt-f4 just backgrounds)
-    sleep 2;
-    wait_idle;
-    x11_start_program("killall amarok") unless $vars{NICEVIDEO};    # to be sure that it does not interfere with later tests
 }
 
 1;
