9aba5ec6420471e29c42f00e6103f92d254b6a8c
Stephan Kulow <coolo@suse.de>
2014-08-05 12:53:24 +0200

    assert_screen on memtest working
    also remove tons of old workarounds no longer valid

--- inst.d/020_bootloader.pm
+++ inst.d/020_bootloader.pm
@@ -29,15 +29,17 @@ sub run() {
         return;
     }
 
-    if ( $vars{MEMTEST} ) {    # special
-        # only run this one
+    # special
+    if ( $vars{MEMTEST} ) {
         for ( 1 .. 6 ) {
             send_key "down";
         }
         assert_screen "inst-onmemtest", 3;
         send_key "ret";
-        sleep 6000;
-        exit 0;               # done
+        assert_screen "pass-complete", 700;
+        send_key "esc";
+
+        # at this point the test is done - but Alberto will refactor this
     }
 
     # assume bios+grub+anim already waited in start.sh
@@ -90,17 +92,10 @@ sub run() {
         send_key "ret";
     }
 
-    #type_string "nohz=off "; # NOHZ caused errors with 2.6.26
-    #type_string "nomodeset "; # coolo said, 12.3-MS0 kernel/kms broken with cirrus/vesa #fixed 2012-11-06
-
-    # https://wiki.archlinux.org/index.php/Kernel_Mode_Setting#Forcing_modes_and_EDID
+# https://wiki.archlinux.org/index.php/Kernel_Mode_Setting#Forcing_modes_and_EDID
     type_string "vga=791 ";
     type_string "Y2DEBUG=1 ";
-    type_string "video=1024x768-16 ",                              13;
-
-    # not needed anymore atm as cirrus has 1024 as default now:
-    # https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=121a6a17439b000b9699c3fa876636db20fa4107
-    #type_string "drm_kms_helper.edid_firmware=edid/1024x768.bin ", 7;
+    type_string "video=1024x768-16 ", 13;
 
     assert_screen "inst-video-typed", 13;
     if ( !$vars{NICEVIDEO} ) {
@@ -123,7 +118,9 @@ sub run() {
         assert_screen "inst-instsourcedialog", 4;
         my $mirroraddr = "";
         my $mirrorpath = "/factory";
-        if ( $vars{SUSEMIRROR} && $vars{SUSEMIRROR} =~ m{^([a-zA-Z0-9.-]*)(/.*)$} ) {
+        if (   $vars{SUSEMIRROR}
+            && $vars{SUSEMIRROR} =~ m{^([a-zA-Z0-9.-]*)(/.*)$} )
+        {
             ( $mirroraddr, $mirrorpath ) = ( $1, $2 );
         }
 
@@ -166,8 +163,6 @@ sub run() {
         #type_string "ZYPP_ARIA2C=0 "; sleep 9;
         #type_string "ZYPP_MULTICURL=0 "; sleep 2;
     }
-
-    #if($vars{BTRFS}) {sleep 9; type_string "squash=0 loadimage=0 ";sleep 21} # workaround 697671
 
     # set language last so that above typing will not depend on keyboard layout
     if ( $vars{INSTLANG} ) {
@@ -248,7 +243,7 @@ sub run() {
             send_key "f2";
             assert_screen "inst-languagemenu", 6;
             for ( 1 .. abs($n) ) {
-                send_key($n < 0 ? "up" : "down");
+                send_key( $n < 0 ? "up" : "down" );
             }
 
             # TODO: add needles for some often tested
@@ -257,30 +252,16 @@ sub run() {
         }
     }
 
-    if ( $vars{ISO} =~ m/i586/ ) {
-
-        #	type_string "info=";sleep 4; type_string "http://zq1.de/i "; sleep 15; type_string "insecure=1 "; sleep 15;
-    }
     my $args = "";
     if ( $vars{AUTOYAST} ) {
-        $args .= " netsetup=dhcp,all autoyast=http://$vars{OPENQA_HOSTNAME}/test-data/$vars{DISTRI}/data/$vars{AUTOYAST} ";
+        $args .= " netsetup=dhcp,all";
+        $args .= " autoyast=http://$vars{OPENQA_HOSTNAME}/test-data/$vars{DISTRI}/data/$vars{AUTOYAST} ";
     }
     type_string $args, 13;
     save_screenshot;
-    if ( 0 && $vars{RAIDLEVEL} ) {
-
-        # workaround bnc#711724
-        $vars{ADDONURL} = "http://download.opensuse.org/repositories/home:/snwint/openSUSE_Factory/";    #TODO: drop
-        $vars{DUD}      = "dud=http://zq1.de/bl10";
-        type_string "$vars{DUD} ";
-        sleep 20;
-        type_string "insecure=1 ";
-        sleep 20;
-        save_vars();
-    }
 
     if ( $vars{LIVETEST} && $vars{LIVEOBSWORKAROUND} ) {
-        send_key "1";       # runlevel 1
+        send_key "1";      # runlevel 1
         send_key "ret";    # boot
         sleep(40);
         type_string( "
@@ -301,4 +282,5 @@ sub test_flags() {
 }
 
 1;
+
 # vim: set sw=4 et:
