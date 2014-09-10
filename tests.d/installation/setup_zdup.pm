3e5d704dea9c0cf3d80fbda42e3f5b9edf5e7c21
Alberto Planas <aplanas@suse.com>
2014-09-10 16:31:12 +0200

    A different way to init 3. Check for errors during zdup.

--- tests.d/installation/setup_zdup.pm
+++ tests.d/installation/setup_zdup.pm
@@ -9,7 +9,7 @@ sub run() {
 
     # log into text console
     send_key "ctrl-alt-f4";
-    sleep 2;
+    assert_screen "linux-login", 2;
     type_string "$username\n";
     sleep 2;
     sendpassword;
@@ -17,15 +17,28 @@ sub run() {
     sleep 3;
 
     # Remove the graphical stuff
-    script_sudo("/sbin/init 3");
+    # This do not work in 13.2
+    # script_sudo "/sbin/init 3";
 
-    # Reloging
-    assert_screen "linux-login", 3;
+    script_sudo "systemctl set-default multi-user.target";
+    # The CD was ejected in the bootloader test
+    script_sudo "/sbin/reboot";
+
+    # login, again : )
+    assert_screen "linux-login", 30;
     type_string "$username\n";
     sleep 2;
     sendpassword;
     type_string "\n";
     sleep 3;
+
+    # Reloging
+    # assert_screen "linux-login", 3;
+    # type_string "$username\n";
+    # sleep 2;
+    # sendpassword;
+    # type_string "\n";
+    # sleep 3;
 
     type_string "PS1=\$\n";    # set constant shell promt
     sleep 1;
