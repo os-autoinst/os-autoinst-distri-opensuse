6a199c209765f952c2062ef78f88bbcd3d79eb6b
Dominique Leuenberger <dimstar@opensuse.org>
2014-10-01 01:01:08 +0200

    test: add test for gnome-control-center
    Identify bugs like https://bugzilla.suse.com/show_bug.cgi?id=897687
    earlier.

---
+++ tests.d/x11/gnome_control_center.pm
@@ -0,0 +1,22 @@
+use base "gnomestep";
+use strict;
+use bmwqemu;
+
+# test gnome-control-center, with panel (boo#897687)
+
+# this part contains the steps to run this test
+sub run() {
+    my $self = shift;
+    mouse_hide(1);
+    x11_start_program("gnome-control-center");
+    sleep 2;
+    send_key "detail\n";
+    sleep 2;
+    assert_screen 'test-gnome_control_center-1', 3;
+    sleep 2;
+    send_key "alt-f4";
+    sleep 2;
+}
+
+1;
+# vim: set sw=4 et:
