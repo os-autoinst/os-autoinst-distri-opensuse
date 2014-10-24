625573903ba8219b51ed69752ad6a4a150ce9208
Stephan Kulow <coolo@suse.de>
2014-10-24 08:45:52 +0200

    fix rescue cd tests:
    - don't run chromium test on rescue cd - no PackageKit
    - don't logout of xfce - too much hassle

--- tests.d/x11/xfce_lightdm_logout_login.pm
+++ tests.d/x11/xfce_lightdm_logout_login.pm
@@ -2,6 +2,11 @@ use base "xfcestep";
 use strict;
 use bmwqemu;
 
+sub is_applicable {
+    my $self = shift;
+    return $self->SUPER::is_applicable && !( $vars{FLAVOR} eq 'Rescue-CD' );
+}
+
 # log out, check lightdm-gtk-greeter and log in again
 
 # this part contains the steps to run this test
