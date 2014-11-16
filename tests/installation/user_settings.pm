85edce7732daabe843461d61f8bd2d5719e044c8
Stephan Kulow <coolo@suse.de>
2014-11-16 07:59:53 +0100

    fixes for the user settings sync

--- tests/installation/user_settings.pm
+++ tests/installation/user_settings.pm
@@ -7,30 +7,30 @@ sub run() {
     my $self = shift;
 
     # user setup
-    assert_screen( "inst-usersetup", 10 );
-    type_string($realname);
+    assert_screen "inst-usersetup", 10;
+    type_string $realname;
     send_key "tab";
 
     #sleep 1;
     send_key "tab";
     for ( 1 .. 2 ) {
-        type_string("$password\t");
+        type_string "$password\t";
     }
     assert_screen( "inst-userinfostyped", 5 );
     if ( $vars{NOAUTOLOGIN} && !check_screen('autologindisabled') ) {
         send_key $cmd{"noautologin"};
-        assert_screen( "autologindisabled", 5 );
+        assert_screen "autologindisabled", 5;
     }
     if ( $vars{DOCRUN} ) {
         send_key $cmd{"otherrootpw"};
-        assert_screen( "rootpwdisabled", 5 );
+        assert_screen "rootpwdisabled", 5;
     }
 
     # done user setup
     send_key $cmd{"next"};
 
     # loading cracklib
-    assert_screen( "inst-userpasswdtoosimple", 6 );
+    assert_screen "inst-userpasswdtoosimple", 6;
     send_key "ret";
 }
 
