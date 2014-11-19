14fc7029c17d8fafc456445cc905302b77894cd5
Stephan Kulow <coolo@suse.de>
2014-11-19 15:04:18 -0500

    sync zypper_up - it doesn't make sense to make that sle specific

--- tests/console/zypper_up.pm
+++ tests/console/zypper_up.pm
@@ -5,29 +5,32 @@ sub run() {
     my $self = shift;
 
     become_root();
-    type_string "PS1=\"# \"\n";
 
     # Killall is used here, make sure that is installed
     script_run("zypper -n -q in psmisc");
 
-    script_run("killall gpk-update-icon kpackagekitsmarticon packagekitd");
-    $self->take_screenshot;
-
-    script_run("zypper patch -l; echo zypper-patch-1-status-\$? > /dev/$serialdev");
+    script_run("zypper patch -l && echo 'worked' > /dev/$serialdev");
     my $ret = assert_screen( [qw/test-zypper_up-confirm test-zypper_up-nothingtodo/] );
     if ( $ret->{needle}->has_tag("test-zypper_up-confirm") ) {
-        send_key "y\n";
-    }
-    wait_serial( "zypper-patch-1-status-0", 700 ) || die "zypper failed";
-    script_run("zypper patch -l; echo zypper-patch-2-status-\$? > /dev/$serialdev");    # first one might only have installed "update-test-affects-package-manager"
-    if ( check_screen("test-zypper_up-confirm") ) {
         type_string "y\n";
     }
-    wait_serial( "zypper-patch-2-status-0", 700 ) || die "zypper failed";
-
-    script_run('exit');
-    script_run( "rpm -q libzypp zypper", 0 );
+    die "zypper failed" unless wait_serial "worked", 700;
+    script_run("zypper patch -l && echo 'worked' > /dev/$serialdev");    # first one might only have installed "update-test-affects-package-manager"
+    if ( check_screen "test-zypper_up-confirm" ) {
+        type_string "y\n";
+    }
+    die "zypper failed" unless wait_serial "worked", 700;
+    script_run("rpm -q libzypp zypper");
+    check_screen "rpm-q-libzypp", 5;
     save_screenshot;
+
+    # XXX: does this below make any sense? what if updates got
+    # published meanwhile?
+    send_key "ctrl-l";    # clear screen to see that second update does not do any more
+    script_run("zypper -n -q patch");
+    script_run('echo $?');
+    script_run('exit');
+    assert_screen 'test-zypper_up-1', 3;
 }
 
 sub test_flags() {
@@ -35,3 +38,4 @@ sub test_flags() {
 }
 
 1;
+# vim: set sw=4 et:
