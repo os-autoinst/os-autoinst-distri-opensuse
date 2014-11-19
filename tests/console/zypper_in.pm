230f923effe6e81085296d3dfdda028c3cf4948f
Stephan Kulow <coolo@suse.de>
2014-11-18 21:23:21 -0500

    sync the zypper_in logic with sle's

--- tests/console/zypper_in.pm
+++ tests/console/zypper_in.pm
@@ -7,19 +7,19 @@ sub run() {
 
     script_run("zypper lr -d > /dev/$serialdev");
 
-    script_run("zypper --gpg-auto-import-keys -n in screen xdelta && echo 'installed' > /dev/$serialdev");
+    my $pkgname = $vars{PACKAGETOINSTALL};
+    script_run("zypper -n in screen $pkgname && echo 'installed' > /dev/$serialdev");
     wait_serial("installed", 200) || die "zypper install failed";
     wait_idle 5;
     script_run('echo $?');
     assert_screen 'test-zypper_in-1', 3;
     sleep 5;
     send_key "ctrl-l";    # clear screen to see that second update does not do any more
-    my $pkgname = "xdelta";
     script_run("rpm -e $pkgname && echo 'package_removed' > /dev/$serialdev");
     wait_serial("package_removed") || die "package remove failed";
     script_run("rpm -q $pkgname");
     script_run('exit');
-    assert_screen 'test-zypper_in-2', 3;
+    assert_screen "package-$pkgname-not-installed", 5;
 }
 
 1;
