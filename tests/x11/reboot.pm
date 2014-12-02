a65bc5662c298b022bf74844179908a793cac90c
Max Lin <mlin@suse.com>
2014-12-02 19:02:49 +0800

    split reboot per DESKTOP

--- tests/x11/reboot.pm
+++ tests/x11/reboot.pm
@@ -4,55 +4,6 @@ use testapi;
 sub run() {
     my $self = shift;
 
-    # 550_reboot_kde
-    if ( check_var("DESKTOP", "kde") || get_var("DESKTOP") eq "gnome" ) {
-        wait_idle;
-        send_key "ctrl-alt-delete";    # reboot
-        assert_screen 'logoutdialog', 15;
-        send_key "tab";
-        send_key "tab";
-        my $ret;
-        for (my $counter = 10; $counter > 0; $counter--) {
-            $ret = check_screen "logoutdialog-reboot-highlighted", 3;
-            if ( defined($ret) ) {
-                last;
-            }
-            else {
-                send_key "tab";
-            }
-        }
-        # report the failure or green
-        unless ( defined($ret) ) {
-            assert_screen "logoutdialog-reboot-highlighted", 1;
-        }
-        send_key "ret";                # confirm
-
-        if (get_var("SHUTDOWN_NEEDS_AUTH")) {
-            assert_screen 'reboot-auth', 15;
-            type_password;
-            send_key "ret";
-        }
-    }
-
-    # 550_reboot_xfce
-    if ( check_var("DESKTOP", "xfce") ) {
-        wait_idle;
-        send_key "alt-f4"; # open logout dialog
-        assert_screen 'logoutdialog', 15;
-        send_key "tab";    # reboot
-        save_screenshot;
-        send_key "ret";    # confirm
-    }
-
-    # 550_reboot_lxde
-    if ( check_var("DESKTOP", "lxde") ) {
-        wait_idle;
-
-        #send_key "ctrl-alt-delete"; # does open task manager instead of reboot
-        x11_start_program("xterm");
-        script_sudo "/sbin/reboot";
-    }
-
     assert_screen "grub2", 100;    # wait until reboot
     if ( get_var("ENCRYPT") ) {
         $self->pass_disk_encrypt_check;
---
+++ tests/x11/reboot_xfce_pre.pm
@@ -0,0 +1,21 @@
+use base "x11test";
+use testapi;
+
+sub run() {
+    my $self = shift;
+
+    # 550_reboot_xfce
+    wait_idle;
+    send_key "alt-f4"; # open logout dialog
+    assert_screen 'logoutdialog', 15;
+    send_key "tab";    # reboot
+    save_screenshot;
+    send_key "ret";    # confirm
+}
+
+sub test_flags() {
+    return { 'important' => 1 };
+}
+1;
+
+# vim: set sw=4 et:
