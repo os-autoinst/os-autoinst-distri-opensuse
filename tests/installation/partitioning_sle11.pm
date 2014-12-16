9b150c57cc014c699c2375070281f533faa85d5d
Richard Brown <rbrownccb@opensuse.org>
2014-12-16 13:57:14 +0100

    sle11 partitioning/filesystem tests

--- tests/installation/partitioning_sle11.pm
+++ tests/installation/partitioning_sle11.pm
@@ -2,63 +2,62 @@ use base "y2logsstep";
 use strict;
 use testapi;
 
-sub key_round($$) {
-    my ($tag, $key) = @_;
-
-    my $counter = 10;
-    while ( !check_screen( $tag, 1 ) ) {
-        send_key $key;
-        if (!$counter--) {
-            # DIE!
-            assert_screen $tag, 1;
-        }
-    }
-}
-
 sub run() {
 	my $self = shift;
 
     assert_screen 'inst-overview', 5;
     send_key $cmd{change};
-    send_key 'p'; # paritioning
+    send_key 'p'; # partitioning
     
     # Basic little hop through to give it a default scenario to edit
     assert_screen 'preparing-disk', 5;
     send_key 'alt-1';
     send_key $cmd{"next"};
-    send_key 'alt-h';
-    send_key $cmd{"next"};
-
-    assert_screen 'inst-overview', 5;
-    send_key $cmd{change};
-    send_key 'p'; # paritioning
-    
     assert_screen 'preparing-disk', 5;
-    send_key 'alt-c';
+    send_key 'alt-u'; #to use btrfs
     send_key $cmd{"next"};
 
-    assert_screen 'expert-partitioning', 5;
-    
-	send_key 'down';
-		# TODO throw some asserts in :)
-    send_key 'down';
-    send_key 'right';
-    send_key 'down'; #should select first disk'
-    send_key 'right';
-    send_key 'down'; #should be swap
-    send_key 'down'; #should be root partition
-    
-    send_key 'alt-s'; #goto filesystem list
-    send_key ' '; #open filesystem list
-    send_key 'home'; #go to top of the list
-    
-    my $wfs = get_var('FILESYSTEM');
-    
-    key_round 'filesystem-$wfs', 'down';
-	send_key 'ret';
-	send_key 'alt+f';
-	send_key 'alt+a';
-    
+	if ( !check_var( "FILESYSTEM", "btrfs" ) ) {
+		
+		assert_screen 'inst-overview', 5;
+		send_key $cmd{change};
+		send_key 'p'; # partitioning
+		assert_screen 'preparing-disk', 5;
+		send_key 'alt-c';
+		send_key $cmd{"next"};
+		assert_screen 'expert-partitioning', 5;
+		send_key 'down';
+		send_key 'down';
+		send_key 'right';
+		send_key 'down'; #should select first disk'
+		send_key 'right';
+		send_key 'down'; #should be boot
+		send_key 'down'; #should be swap
+		send_key 'down'; #should be root partition
+		send_key 'alt-e'; #got to actually edit..
+		send_key 'alt-s'; #goto filesystem list
+		send_key ' '; #open filesystem list
+		send_key 'home'; #go to top of the list
+		
+		my $counter = 20;
+		while (1) {
+			my $ret = wait_screen_change {
+				send_key 'down';
+			};
+			# down didn't change the screen, so exit here
+			die "looping for too long/filesystem not found" if (!$ret || $counter-- == 0);
+			
+			my $fs = get_var('FILESYSTEM');
+		
+			if (check_screen("filesystem-$fs", 1)) {
+				send_key 'ret';
+				send_key 'alt-f';
+				send_key 'alt-a';
+				assert_screen('inst-overview', 5);
+				last;
+			}
+		}
+	}
 }
 
 1;
