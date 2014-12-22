28b32a5f327b9ee460883cfc330f22d8820f66cf
Stephan Kulow <coolo@suse.de>
2014-12-22 19:36:09 +0100

    give textinfo more time

--- tests/console/textinfo.pm
+++ tests/console/textinfo.pm
@@ -6,7 +6,7 @@ use testapi;
 sub run() {
     my $self = shift;
     script_sudo("/home/$username/data/textinfo 2>&1 | tee /home/$username/info.txt");
-    wait_serial('textinfo_done', 35) || die "textinfo test failed";
+    wait_serial('textinfo_done', 180) || die "textinfo test failed";
     upload_logs("info.txt");
     upload_logs("/tmp/logs.tar.bz2");
     assert_screen "texinfo-logs-uploaded";
