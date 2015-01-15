fbfda9307318573f5766b9bbdc97f80a4afb8211
Dinar Valeev <dvaleev@suse.com>
2015-01-15 11:43:59 +0100

    Add yaboot test
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

---
+++ tests/installation/bootloader_ofw_yaboot.pm
@@ -0,0 +1,13 @@
+use base "installbasetest";
+use strict;
+use testapi;
+use Time::HiRes qw(sleep);
+
+# hint: press shift-f10 trice for highest debug level
+sub run() {
+    assert_screen "bootloader-ofw-yaboot", 15;
+    send_key "ret";
+}
+
+1;
+# vim: set sw=4 et:
