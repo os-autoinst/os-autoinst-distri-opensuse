6fa8c5f120b49a80194d640cf4217870e0f78077
Richard Brown <rbrownccb@opensuse.org>
2015-01-16 15:34:13 +0100

    Make installation_overview spot YaST's warning of dependency issues and drill down/provide extra info if they appear (good for dependency bug reports..and we've had a lot of these with SLE11 already)

--- tests/installation/installation_overview.pm
+++ tests/installation/installation_overview.pm
@@ -3,6 +3,19 @@ use strict;
 use base "y2logsstep";
 use testapi;
 
+sub key_round($$) {
+    my ($tag, $key) = @_;
+
+    my $counter = 15;
+    while ( !check_screen( $tag, 1 ) ) {
+        send_key $key;
+        if (!$counter--) {
+            # DIE!
+            assert_screen $tag, 1;
+        }
+    }
+}
+
 sub run() {
 
     # overview-generation
@@ -11,6 +24,25 @@ sub run() {
 
     # preserve it for the video
     wait_idle 10;
+    
+    # check for dependancy issues, if found, drill down to software selection, take a screenshot, then die
+    if (check_screen("inst-overview-dep-warning",1)){
+       
+        if (check_var('VIDEOMODE', 'text')) {
+            send_key 'alt-c';
+            assert_screen 'inst-overview-options', 3;
+            send_key 'alt-s';
+        }
+        else {
+            key_round 'packages-section-selected', 'tab';
+            send_key 'ret';
+        }
+        
+        assert_screen 'pattern_selector';
+        send_key 'alt-o';
+        assert_screen 'dependancy-issue', 5;
+        die "Dependency Warning Detected"
+    }
 }
 
 1;
