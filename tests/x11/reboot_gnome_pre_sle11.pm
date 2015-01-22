0f955e70a7398aa4880c4f943bd6516c7ddfaaaf
Frederic Crozat <fcrozat@suse.com>
2015-01-22 16:10:06 +0100

    add acpi based test for GNOME reboot for SLE11

---
+++ tests/x11/reboot_gnome_pre_sle11.pm
@@ -0,0 +1,44 @@
+use base "opensusebasetest";
+use testapi;
+
+sub run() {
+    my $self = shift;
+
+    wait_idle;
+    power('acpi'); # request reboot through ACPI, requires to reset VM
+    assert_screen 'logoutdialog', 15;
+    send_key "tab";
+    send_key "tab";
+    my $ret;
+    for (my $counter = 10; $counter > 0; $counter--) {
+        $ret = check_screen "logoutdialog-reboot-highlighted", 3;
+        if ( defined($ret) ) {
+            last;
+        }
+        else {
+            send_key "tab";
+        }
+    }
+    # report the failure or green
+    unless ( defined($ret) ) {
+        assert_screen "logoutdialog-reboot-highlighted", 1;
+    }
+    send_key "ret";                # confirm
+
+    if (get_var("SHUTDOWN_NEEDS_AUTH")) {
+        assert_screen 'reboot-auth', 15;
+        type_password;
+        send_key "ret";
+    }
+
+    # qemu is not reliable in sending last screenshot, so don't assert here
+    check_screen "machine-is-shutdown", 30
+    power('reset');
+}
+
+sub test_flags() {
+    return { 'important' => 1 };
+}
+1;
+
+# vim: set sw=4 et:
