d9c4b46ab46a0fd6d0a25b1f1cff8011a60c2183
Dinar Valeev <dvaleev@suse.com>
2015-02-09 15:45:46 +0100

    Honor textmode setting for PowerPC bootloader
    In case VIDEOMODE is text we need to pass appropriate options to linux
    
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

--- tests/installation/bootloader_ofw_yaboot.pm
+++ tests/installation/bootloader_ofw_yaboot.pm
@@ -6,6 +6,9 @@ use Time::HiRes qw(sleep);
 # hint: press shift-f10 trice for highest debug level
 sub run() {
     assert_screen "bootloader-ofw-yaboot", 15;
+    if (check_var('VIDEOMODE', 'text')) {
+       type_string "install textmode=1", 15;
+    }
     send_key "ret";
 }
 
