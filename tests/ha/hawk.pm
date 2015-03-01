f3db0ce8bcabefd7de990a41f31c4777a38240f3
Richard Brown <rbrownccb@opensuse.org>
2015-03-01 22:47:38 +0100

    HA Hawk Fix

--- tests/ha/hawk.pm
+++ tests/ha/hawk.pm
@@ -7,7 +7,9 @@ sub run() {
     type_string "firefox https://10.0.2.16:7630/\n";
     assert_and_click 'firefox-understand-risks';
     send_key 'f11';	
-    assert_and_click 'firefox-add-exception';
+    #assert_and_click 'firefox-add-exception';
+    send_key 'tab';
+    send_key 'ret';
     assert_and_click 'firefox-confirm-exception';
     assert_screen 'hawk-login';
     assert_and_click 'hawk-username';
