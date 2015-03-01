51adfe44ac5fa31b88d126e6a90d63b7d3894b9b
Richard Brown <rbrownccb@opensuse.org>
2015-03-01 02:27:14 +0100

    Make firefox fullscreen

--- tests/ha/hawk.pm
+++ tests/ha/hawk.pm
@@ -6,6 +6,7 @@ sub run() {
     type_string "exit\n";
     type_string "firefox https://10.0.2.16:7630/\n";
     assert_and_click 'firefox-understand-risks';
+    send_key 'f11';	
     assert_and_click 'firefox-add-exception';
     assert_and_click 'firefox-confirm-exception';
     assert_screen 'hawk-login';
