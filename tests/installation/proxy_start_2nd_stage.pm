141835c85697736407b9330dea041bebc24e7e4c
Richard Brown <rbrownccb@opensuse.org>
2015-03-03 16:55:20 +0100

    Have second stage check rather than wait blindly

--- tests/installation/proxy_start_2nd_stage.pm
+++ tests/installation/proxy_start_2nd_stage.pm
@@ -13,8 +13,19 @@ sub reconnectsshinstall($) {
     assert_screen 'second-stage', 40;
 }
 
+sub waitfor2ndstage($) {
+    my ($nodenum) = @_;
+    type_string "vncviewer localhost:9$nodenum -shared -fullscreen\n";
+    assert_screen "inst-ssh-ready", 500;
+    send_key 'f8';
+    send_key 'down';
+    send_key 'ret';
+    sleep 5;
+    send_key 'ctrl-l';
+}
+
 sub run() {
-    sleep 240; #500 is too long, seems to shut down the VMs
+    waitfor2ndstage "1";
     reconnectsshinstall "1";
 }
 
