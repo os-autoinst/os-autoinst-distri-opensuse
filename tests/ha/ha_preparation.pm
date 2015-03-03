dd0636b0731e5f428192a5f2a867dd4ae517853b
Richard Brown <rbrownccb@opensuse.org>
2015-03-03 03:41:43 +0100

    Remove duplicate iSCSI configuration

--- tests/ha/ha_preparation.pm
+++ tests/ha/ha_preparation.pm
@@ -103,30 +103,6 @@ sub run() {
     type_string "hostname node3\n";
     send_key 'ctrl-pgup';
     send_key 'ctrl-pgup';
-    send_key 'ctrl-alt-g';
-    type_string "yast iscsi-client\n";
-    assert_screen 'yast-iscsi-client-loaded';
-    send_key 'alt-b';
-    send_key 'alt-v';
-    assert_screen 'yast-iscsi-discovered-targets';
-    send_key 'alt-d';
-    assert_screen 'yast-iscsi-initiator-discovery';
-    send_key 'alt-i';
-    type_string '10.0.2.15';
-    send_key 'alt-n';
-    assert_screen 'yast-iscsi-discovered-targets';
-    send_key 'alt-l';
-    assert_screen 'yast-iscsi-initiator-login';
-    send_key 'alt-s';
-    send_key 'down';
-    send_key 'down';
-    send_key 'ret';
-    send_key 'alt-n';
-    assert_screen 'yast-iscsi-discovered-targets';
-    send_key 'alt-o';
-    sleep 5;
-    send_key 'ctrl-l';
-    assert_screen 'proxy-terminator-clean';
 }
 
 1;
