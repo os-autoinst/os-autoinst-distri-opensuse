c05c72099feac20df17576455d0a69e44a4895b4
Jozef Pupava <jpupava@suse.com>
2015-03-09 09:03:15 +0100

    SLE11 firefox with KDE has to accept "default browser check"

---
+++ tests/x11/sle11_firefox.pm
@@ -0,0 +1,30 @@
+use base "x11test";
+use testapi;
+
+sub run() {
+    my $self = shift;
+    wait_idle;
+    mouse_hide(1);
+    x11_start_program("firefox", 6, { valid => 1 } );
+    assert_screen 'test-firefox-1', 60;
+    if (get_var(DESKTOP) eq "kde") {
+        # uncheck Always perform default browser check, firefox audio without default browser check
+        send_key "tab";
+        send_key "tab";
+        send_key " ";
+        send_key "alt-y";   # accept firefox as default browser
+    }
+    send_key "alt-h";
+    sleep 2;    # Help
+    send_key "a";
+    sleep 2;    # About
+    assert_screen 'test-firefox-3', 3;
+    send_key "alt-f4";
+    sleep 2;    # close About
+    send_key "alt-f4";
+    sleep 2;
+    send_key "ret";    # confirm "save&quit"
+}
+
+1;
+# vim: set sw=4 et:
