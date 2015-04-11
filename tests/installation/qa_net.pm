cffd433388ae1ff835bc3ffa81dbded882b16f73
Stephan Kulow <coolo@suse.de>
2015-04-11 17:39:24 +0200

    more fixes for qa_net

--- tests/installation/qa_net.pm
+++ tests/installation/qa_net.pm
@@ -16,15 +16,18 @@ sub run() {
     assert_screen 'qa-net-boot', 8;
 
     my $arch = get_var('ARCH');
+    my $type_speed = 20;
     my $path = "/mnt/openqa/repo/" . get_var('REPO_0') . "/boot/$arch/loader/";
-    type_string "$path/linux initrd=$path/initrd install=http://" . get_var('SUSEMIRROR') . " ", 13;
+    type_string "$path/linux initrd=$path/initrd install=http://" . get_var('SUSEMIRROR') . " ", $type_speed;
 
-    type_string "vga=791 ", 4;
-    type_string "Y2DEBUG=1 ", 4;
-    type_string "video=1024x768-16 ", 4;
-    type_string "console=$serialdev,115200 ", 4;    # to get crash dumps as text
-    type_string "console=tty ",   4;    # to get crash dumps as text
-    #    assert_screen 'qa-net-typed', 5;
+    type_string "vga=791 ", $type_speed;
+    type_string "Y2DEBUG=1 ", $type_speed;
+    type_string "video=1024x768-16 ", $type_speed; 
+    type_string "console=$serialdev,115200 ", $type_speed;    # to get crash dumps as text
+    type_string "console=tty ", $type_speed;    # to get crash dumps as text
+    # for some reason we need to reset the "VNC" connection again to see the bottom - most likely because it has the odd resolution of 752x413
+    $bmwqemu::backend->relogin_vnc();
+    assert_screen 'qa-net-typed', 5;
 
     my $e = get_var("EXTRABOOTPARAMS");
     if ($e) {
