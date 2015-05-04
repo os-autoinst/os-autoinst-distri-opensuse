4e85ada3b2b378751d713f5d3cb3ca372271a5bb
Dinar Valeev <dvaleev@suse.com>
2015-05-04 14:55:22 +0200

    Workaround slow grub2
    we have slow grub2 on Power. Let's do stuff manually as a workaround
    
    bsc#894483
    
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

--- tests/installation/bootloader_ofw.pm
+++ tests/installation/bootloader_ofw.pm
@@ -5,24 +5,32 @@ use Time::HiRes qw(sleep);
 
 # hint: press shift-f10 trice for highest debug level
 sub run() {
+    my $self = shift;
+
     assert_screen "bootloader-ofw", 15;
-    send_key "up";
-    send_key "up";
-    send_key "up";
+    $self->key_round('inst-oninstallation', 'up');
     if (check_var('VIDEOMODE', 'text') || get_var('NETBOOT')) {
-        send_key "e";
-        send_key "down";
-        send_key "down";
-        send_key "down";
-        send_key "end";
+        # go to cmdline
+        send_key "c";
+        # load kernel manually with append
+        type_string "linux /boot/" . get_var("ARCH") . "/linux";
         if (check_var('VIDEOMODE', 'text')) {
             type_string " textmode=1", 15;
         }
-        send_key "spc";
         if ( get_var("NETBOOT") && get_var("SUSEMIRROR") ) {
             type_string " install=http://" . get_var("SUSEMIRROR"), 15;
         }
-        send_key "ctrl-x";
+        if ( get_var("AUTOYAST") ) {
+            type_string " netsetup=dhcp,all";
+            type_string " autoyast=" . autoinst_url . "/data/" . get_var("AUTOYAST") . " ";
+        }
+        send_key "ret";
+        #load initrd
+        type_string "initrd /boot/". get_var("ARCH") . "/initrd";
+        send_key "ret";
+        #finally boot
+        type_string "boot";
+        send_key "ret";
     }
     else {
         send_key "ret";
