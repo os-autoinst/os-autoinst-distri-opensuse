2cacd2472e386569a74c978b467093e5574d8e9f
Jozef Pupava <jpupava@suse.com>
2015-05-19 15:19:52 +0200

    fix for SMT update bnc928895

--- tests/installation/upgrade_select_sle11.pm
+++ tests/installation/upgrade_select_sle11.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run() {
     my $self = shift;
 
-    if ( get_var("UPGRADE") && get_var("ADDONS") ) { # Netwrok setup
+    if (get_var("UPGRADE") && get_var("ADDONS")) { # Netwrok setup
         if ( check_screen('network-setup', 10)) { # won't appear for NET installs
             send_key $cmd{"next"};    # use network
             assert_screen 'dhcp-network';
@@ -17,39 +17,58 @@ sub run() {
     # hardware detection can take a while
     assert_screen "select-for-update", 100;
     send_key $cmd{"next"}, 1;
-
     assert_screen 'previously-used-repositories', 5;
-    send_key $cmd{"next"}, 1;
 
-    if ( get_var("UPGRADE") && get_var("ADDONS") ) {
+    if (get_var("UPGRADE") && get_var("ADDONS")) {
         foreach $a (split(/,/, get_var('ADDONS'))) {
-            send_key 'alt-d';	# DVD
-            send_key $cmd{"xnext"}, 1;
-            assert_screen 'dvd-selector', 3;
-            $self->key_round('addon-dvd-list', 'tab', 10);
-            $self->key_round("addon-dvd-$a", 'down', 10);
-            send_key 'alt-o';
-            if (get_var("BETA")) {
-                assert_screen "addon-betawarning-$a", 10;
-                send_key "ret";
-                assert_screen "addon-license-beta", 10;
+            if ($a eq 'smt') {
+                $self->key_round('used-repo-list', 'tab', 5);       # enable SMT repository
+                $self->key_round('smt-repo-selected', 'down', 5);
+                $self->key_round('used-smt-enabled', 'alt-t', 5);
+                send_key $cmd{"next"}, 1;
+                if (check_screen('network-not-configured', 5)) {
+                    send_key 'ret', 1;          # Yes
+                    send_key $cmd{"next"};      # use network
+                    assert_screen 'dhcp-network';
+                    send_key 'alt-d';           # DHCP
+                    send_key "alt-o", 2;        # OK
+                }
+                record_soft_failure # https://bugzilla.suse.com/show_bug.cgi?id=928895
+                assert_screen 'correct-media';  # Correct media request
+                send_key "alt-o", 2;            # OK
             }
             else {
-                assert_screen "addon-license-$a", 10;
-            }
-            sleep 2;
-            send_key 'alt-y'; # yes, agree
-            sleep 2;
-            send_key 'alt-n';
-            assert_screen 'addon-list';
-            if ((split(/,/, get_var('ADDONS')))[-1] ne $a) {
-                send_key 'alt-a';
-                assert_screen 'addon-selection', 15;
+                send_key $cmd{"next"}, 1;
+                send_key 'alt-d';	# DVD
+                send_key $cmd{"xnext"}, 1;
+                assert_screen 'dvd-selector', 3;
+                $self->key_round('addon-dvd-list', 'tab', 10);
+                $self->key_round("addon-dvd-$a", 'down', 10);
+                send_key 'alt-o';
+                if (get_var("BETA")) {
+                    assert_screen "addon-betawarning-$a", 10;
+                    send_key "ret";
+                    assert_screen "addon-license-beta", 10;
+                }
+                else {
+                    assert_screen "addon-license-$a", 10;
+                }
+                sleep 2;
+                send_key 'alt-y'; # yes, agree
+                sleep 2;
+                send_key 'alt-n';
+                assert_screen 'addon-list';
+                if ((split(/,/, get_var('ADDONS')))[-1] ne $a) {
+                    send_key 'alt-a';
+                    assert_screen 'addon-selection', 15;
+                }
             }
         }
 
-        assert_screen 'addon-list', 5;
-        send_key $cmd{"next"}, 1;
+        if (!check_var('ADDONS', 'smt')) {  # no add-on list for SMT
+            assert_screen 'addon-list', 5;
+            send_key $cmd{"next"}, 1;
+        }
     }
 
     assert_screen "update-installation-overview", 15;
