bc5cd1f1159c9539eda391c4b47875e05581dc7e
Richard Brown <rbrownccb@opensuse.org>
2015-05-20 18:10:45 +0200

    Make test fatal, and fix if so it's not always true

--- tests/installation/mouse_hide.pm
+++ tests/installation/mouse_hide.pm
@@ -2,10 +2,14 @@ use base "opensusebasetest";
 use testapi;
 
 sub run() {
-    if (check_screen('mouse-not-hidden'), 120) {
+    if (check_screen('mouse-not-hidden', 120)) {
         die 'Mouse Stuck Detected';
     }
     $self->result('ok');
+}
+
+sub test_flags {
+    return { fatal => 1 };
 }
 
 1;
