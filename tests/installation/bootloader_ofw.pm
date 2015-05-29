e92db707782cb39fb032e33bbe7701983c0f63ef
Dinar Valeev <dvaleev@suse.com>
2015-05-29 15:54:05 +0200

    Get back to grub menu edit on OFW
    We've finally got SLOF fixed. So the menu redraw speed is acceptable
    now.
    
    Signed-off-by: Dinar Valeev <dvaleev@suse.com>

--- tests/installation/bootloader_ofw.pm
+++ tests/installation/bootloader_ofw.pm
@@ -10,31 +10,22 @@ sub run() {
     assert_screen "bootloader-ofw", 15;
     $self->key_round('inst-oninstallation', 'up');
     if (check_var('VIDEOMODE', 'text') || get_var('NETBOOT')) {
-        # go to cmdline
-        send_key "c";
-        #wait until we get to cli
-        wait_idle(10);
+        # edit menu
+        send_key "e";
+        #wait until we get to grub edit
+        wait_idle(5);
         # load kernel manually with append
-        type_string "linux /boot/" . get_var("ARCH") . "/linux", 30;
         if (check_var('VIDEOMODE', 'text')) {
-            wait_idle(10);
             type_string " textmode=1", 15;
         }
         if ( get_var("NETBOOT") && get_var("SUSEMIRROR") ) {
-            wait_idle(10);
             type_string " install=http://" . get_var("SUSEMIRROR"), 15;
         }
         if ( get_var("AUTOYAST") ) {
-            wait_idle(10);
             type_string " netsetup=dhcp,all", 15;
             type_string " autoyast=" . autoinst_url . "/data/" . get_var("AUTOYAST") . " ", 15;
         }
-        send_key "ret";
-        #load initrd
-        type_string "initrd /boot/". get_var("ARCH") . "/initrd", 30;
-        send_key "ret";
-        #finally boot
-        type_string "boot", 15;
+        send_key "ctrl-x";
     }
     send_key "ret";
 }
