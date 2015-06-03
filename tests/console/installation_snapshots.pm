5b0efc502f0a8bb752ae8cbad30aada69d56f59d
Imobach González Sosa <igonzalezsosa@suse.com>
2015-06-03 14:44:28 +0100

    Update from code review

--- tests/console/installation_snapshots.pm
+++ tests/console/installation_snapshots.pm
@@ -2,14 +2,10 @@ use base "consoletest";
 use testapi;
 
 sub run() {
-    my $self = shift;
-
     become_root();
-
     script_run("snapper list | tee /dev/$serialdev");
     # Check if the snapshot called 'after installation' is there
-    $self->result('fail') unless wait_serial("after installation", 5);
-    save_screenshot();
+    wait_serial("after installation", 5);
 }
 
 sub test_flags() {
