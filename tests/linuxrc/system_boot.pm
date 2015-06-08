ad402fc7c49bf3042c0c428a934e0cba814f459d
Lukas Ocilka <lukas.ocilka@gmail.com>
2015-06-08 16:15:18 +0200

    Added Linuxrc test for booting the installed system
    - bsc#906990
    - Requires linuxrc-5.0.44 and higher to be used for install media

---
+++ tests/linuxrc/system_boot.pm
@@ -0,0 +1,36 @@
+use strict;
+use base "y2logsstep";
+use testapi;
+use linuxrc;
+
+#
+# bsc#906990 [linuxrc] add "Boot Linux" to installation media
+# Added in linuxrc-5.0.44
+#
+
+sub run() {
+    my $self = shift;
+
+    $self->linuxrc::wait_for_bootmenu();
+
+    $self->linuxrc::boot_with_parameters("SystemBoot=1");
+    assert_screen("linuxrc-system_boot-select-a-system-to-boot", 60);
+
+    # Press [Enter] till the last dialog for booting appears
+    $self->key_round("linuxrc-system-boot-kernel-options", "ret", 8);
+
+    # Confirm booting
+    bmwqemu::diag "Booting the installed system now...";
+    send_key "ret", 0;
+
+    # Wait for the system to boot
+    assert_screen("displaymanager", 60);
+}
+
+sub test_flags() {
+    return { 'important' => 1, 'fatal' => 1, 'milestone' => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
