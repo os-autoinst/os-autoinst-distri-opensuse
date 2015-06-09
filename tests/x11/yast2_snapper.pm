4369e2fc32397095a705f5d1c2bbdc7cd61eaf3f
Ancor Gonzalez Sosa <ancor@suse.de>
2015-06-09 08:13:02 +0200

    Fixes from review
     - Use the default snapper configuration instead of creating a new subvolume
     - Use waitserial to check the tar

--- tests/x11/yast2_snapper.pm
+++ tests/x11/yast2_snapper.pm
@@ -21,6 +21,10 @@ sub y2snapper_create_snapshot() {
     sleep 2;
     send_key "tab";
     sleep 2;
+    send_key "tab";
+    sleep 2;
+    send_key "tab";
+    sleep 2;
     type_string "a=1,b=2";
     sleep 2;
     send_key "alt-o";
@@ -33,31 +37,35 @@ sub run() {
     x11_start_program("xterm");
     wait_idle;
     become_root;
-    # Create a snapper config
-    script_run "btrfs subvolume create /tmp/testdata";
-    script_run "snapper create-config /tmp/testdata";
+    script_run "cd";
     # Start the yast2 snapper module and wait until it is started
     script_run "yast2 snapper";
-    assert_screen 'yast2_snapper-nosnapshots', 100;
+    assert_screen 'yast2_snapper-no_new_snapshot', 100;
     # Create a new snapshot
     $self->y2snapper_create_snapshot();
     # Make sure the snapshot is listed in the main window
-    assert_screen 'yast2_snapper-snapshotlisted', 100;
+    assert_screen 'yast2_snapper-new_snapshot', 100;
     # C'l'ose  the snapper module
     send_key "alt-l";
     # Download & untar test files
     wait_idle;
-    script_run "cd /tmp";
-    script_run "tar -xzf /home/$username/data/yast2_snapper.tgz";
-    wait_idle;
+    script_run "tar -xzf /home/$username/data/yast2_snapper.tgz && echo tar_complete > /dev/$serialdev";
+    wait_serial('tar_complete') || die 'tar -xzf failed';
     # Start the yast2 snapper module and wait until it is started
     script_run "yast2 snapper";
     # Make sure the snapper module is started
-    assert_screen 'yast2_snapper-snapshotlisted', 100;
+    assert_screen 'yast2_snapper-new_snapshot', 100;
+    # Select the new snapshot
+    send_key "down";
+    sleep 2;
+    send_key "down";
+    sleep 2;
+    send_key "down";
+    sleep 2;
     # Press 'S'how changes button and select both directories that have been
     # extracted from the tarball
     send_key "alt-s";
-    sleep 2;
+    assert_screen 'yast2_snapper-collapsed_testdata', 100;
     send_key "tab";
     sleep 2;
     send_key "spc";
@@ -69,20 +77,25 @@ sub run() {
     assert_screen 'yast2_snapper-show_testdata', 100;
     # Close the dialog and make sure it is closed
     send_key "alt-c";
-    assert_screen 'yast2_snapper-snapshotlisted', 100;
+    assert_screen 'yast2_snapper-new_snapshot', 100;
+    # Select the new snapshot
+    send_key "down";
+    sleep 2;
+    send_key "down";
+    sleep 2;
+    send_key "down";
+    sleep 2;
     # Dele't'e the snapshot
     send_key "alt-t";
     assert_screen 'yast2_snapper-confirm_delete', 100;
     send_key "alt-y";
-    assert_screen 'yast2_snapper-nosnapshots', 100;
+    assert_screen 'yast2_snapper-no_new_snapshot', 100;
     # C'l'ose  the snapper module
     send_key "alt-l";
-    # Wait until xterm is focussed, delete the subvolume and close xterm
+    # Wait until xterm is focussed, delete the directory and close xterm
     wait_idle;
-    script_run "btrfs subvolume delete /tmp/testdata/.snapshots";
-    script_run "rm -rf /tmp/testdata/*";
-    script_run "btrfs subvolume delete /tmp/testdata";
-    script_run "ls /tmp";
+    script_run "rm -rf testdata";
+    script_run "ls";
     script_run "exit";
     save_screenshot;
     script_run "exit";
