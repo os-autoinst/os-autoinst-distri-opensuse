bf41de511a95c64e19ba71078d31e3b37e76ee47
Imobach González Sosa <igonzalezsosa@suse.com>
2015-06-17 09:56:07 +0100

    Add a basic yast2-network test

---
+++ tests/x11/yast2_network.pm
@@ -0,0 +1,67 @@
+use base "x11test";
+use testapi;
+
+# Helper method to enable routing through `lan` client.
+sub enable_routing() {
+  send_key "alt-u"; # Routing tab
+  assert_screen "yast2_network-routing_disabled";
+  send_key "alt-i"; # Activate IPv4 routing
+  send_key "alt-p"; # Activate IPv6 routing
+  assert_screen "yast2_network-routing_enabled";
+}
+
+# Helper method to check if routing is enabled.
+sub check_routing_enabled_ui() {
+    # Check at UI level
+    send_key "alt-u"; # Routing
+    assert_screen "yast2_network-rounting_enabled";
+}
+
+sub check_routing_enabled_console() {
+    # Check at system level
+    script_run "sysctl net.ipv4.conf.all.forwarding > /dev/$serialdev";
+    wait_serial("net.ipv4.conf.all.forwarding = 1", 5) || die("IPv4 not enabled");
+}
+
+sub start_yast2_lan() {
+    script_run "yast2 lan";
+    assert_screen "yast2_network-lan", 50;
+}
+
+# Test for basic yast2-network functionality.
+sub run() {
+  my $self = shift;
+
+  # Make sure yast2-network ins installed (if not, install it)
+  ensure_installed "yast2-network";
+
+  # Start xterm as root
+  x11_start_program("xterm");
+  wait_idle;
+  become_root;
+
+  # Make sure that firewall is running
+  script_run "yast2 firewall enable";
+
+  # Enable routing
+  $self->start_yast2_lan();
+  $self->enable_routing();
+
+  # Save settings
+  send_key "alt-o";
+  wait_idle;
+
+  # Stop firewall
+  script_run "yast2 firewall disable";
+
+  # Check that routing is still enabled
+  $self->check_routing_enabled_console();
+  $self->start_yast2_lan();
+  $self->check_routing_enabled_ui();
+
+  # Success
+  $self->clean_and_quit;
+}
+
+1;
+# vim: set sw=4 et;
