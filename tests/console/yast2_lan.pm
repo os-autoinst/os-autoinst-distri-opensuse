1f644d766c9ee44c4e889c00bd388c417c89dfaf
Richard Brown <rbrownccb@opensuse.org>
2015-06-22 10:48:08 +0200

    Increase yast2_lan timeout

--- tests/console/yast2_lan.pm
+++ tests/console/yast2_lan.pm
@@ -8,7 +8,7 @@ sub run() {
     my $self = shift;
     script_sudo("/sbin/yast2 lan");
 
-    my $ret = assert_screen [qw/Networkmanager_controlled yast2_lan install-susefirewall2/], 20;
+    my $ret = assert_screen [qw/Networkmanager_controlled yast2_lan install-susefirewall2/], 30;
     if ( $ret->{needle}->has_tag('Networkmanager_controlled') ) {
         send_key "ret";      # confirm networkmanager popup
         assert_screen "Networkmanager_controlled-approved";
