3d43e0c374928a4dac5fc56f8ea3b0f96e470314
Ludwig Nussel <ludwig.nussel@suse.de>
2015-06-24 16:07:21 +0200

    new test that installs configured packages

---
+++ tests/console/install_packages.pm
@@ -0,0 +1,20 @@
+use base "consoletest";
+use testapi;
+
+sub run() {
+    become_root();
+
+    my $packages = get_var("INSTALL_PACKAGES");
+
+    assert_script_run("zypper -n in -l $packages");
+    assert_script_run("rpm -q $packages");
+
+    script_run('exit');
+}
+
+sub test_flags() {
+    return { 'fatal' => 1, };
+}
+
+1;
+# vim: set sw=4 et:
