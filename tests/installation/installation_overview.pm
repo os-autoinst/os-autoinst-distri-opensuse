788ea99471fe8b7f9b48e8725d8438931654e620
Richard Brown <rbrownccb@opensuse.org>
2015-07-09 18:15:33 +0200

    Get past auto change

--- tests/installation/installation_overview.pm
+++ tests/installation/installation_overview.pm
@@ -40,6 +40,7 @@ sub run() {
                 send_key 'alt-o', 3;
             }
             send_key 'alt-a', 3;
+            send_key 'alt-o', 3;
             assert_screen "inst-overview", 15; # Make sure you're back on the inst-overview before doing anything else
         }
         else {
