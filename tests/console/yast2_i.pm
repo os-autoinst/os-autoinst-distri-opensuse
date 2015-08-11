c8b41331ab474e0bbb6cdb3de66ed5823e3dde49
openQA web UI <openqa@subbotin>
2015-08-11 14:07:09 +0200

    Fix for opensuse: Respect already installed libslang2

--- tests/console/yast2_i.pm
+++ tests/console/yast2_i.pm
@@ -68,8 +68,10 @@ sub run() {
 
     send_key "alt-a", 1; # accept
 
-    assert_screen 'yast2-sw_autochange_no_mc-lang', 60;
-    send_key "alt-o"; # Ok
+    if ( !check_var( 'DISTRI', 'opensuse' ) ) {
+        assert_screen 'yast2-sw_autochange_no_mc-lang', 60;
+        send_key "alt-o"; # Ok
+    }
     end_yast();
 
 #    This testcase is disabled because we consider it not just package manager
