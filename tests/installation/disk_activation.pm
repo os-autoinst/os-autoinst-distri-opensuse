f7b84a45559f367cdd92117a0d1bac70402f245e
Jozef Pupava <jpupava@suse.com>
2015-08-27 12:59:42 +0200

    working disk activation

--- tests/installation/disk_activation.pm
+++ tests/installation/disk_activation.pm
@@ -6,38 +6,49 @@ sub run {
     my $self = shift;
 
     assert_screen 'disk-activation', 15;
-    send_key 'alt-d';
+    send_key 'alt-d';   # configure DASD disk
     assert_screen 'dasd-disk-management';
-    send_key 'alt-m';
+    send_key 'alt-m';   # minimum channel ID
     type_string '0.0.0150';
-    send_key 'alt-x';
+    send_key 'alt-x';   # maximum channel ID
     type_string '0.0.0150';
-    send_key 'alt-f';
+    send_key 'alt-f';   # filter button
     assert_screen 'dasd-unselected';
-    send_key 'alt-s';
+    send_key 'alt-s';   # select all
     assert_screen 'dasd-selected';
-    send_key 'alt-a';
+    send_key 'alt-a';   # perform action button
     assert_screen 'action-list';
-    send_key 'a';
-    assert_screen 'dasd-active';
-    if ( !get_var('UPGRADE') && !get_var('ZDUP') ) {
-        send_key 'alt-s';
-        assert_screen 'dasd-selected';
-        send_key 'alt-a';
-        assert_screen 'action-list';
-        send_key 'f';
-        send_key 'f'; # Pressing f twice because of bsc#940817
-        send_key 'ret';
-        assert_screen 'confirm-format';
-        send_key 'alt-y';
-        assert_screen 'process-format'; # Make sure formatting starts
-        until (check_screen 'process-format', 600 || die 'formatting took too long') {
-            printf 'formating ...';
+    send_key 'a';       # activate
+
+    if (check_screen 'dasd-format-device') {    # format device pop-up
+        send_key 'alt-o';   # continue
+        while (check_screen 'process-format') { # format progress
+            printf "formating ...\n";
+            sleep 2;
         }
     }
-    send_key 'alt-n';
+    if (!get_var('UPGRADE') && !get_var('ZDUP')) {
+        send_key 'alt-s';   # select all
+        assert_screen 'dasd-selected';
+        send_key 'alt-a';   # perform action button
+        if (check_screen 'dasd-device-formatted'){
+            assert_screen 'action-list';
+            send_key 'f';
+            send_key 'f';   # Pressing f twice because of bsc#940817
+            send_key 'ret';
+            assert_screen 'confirm-format';
+            send_key 'alt-y';
+            while (check_screen 'process-format') {
+                printf "formating ...\n";
+                sleep 2;
+            }
+        }
+    }
+    sleep 5;
+    assert_screen 'dasd-active';
+    send_key 'alt-n';   # next
     assert_screen 'disk-activation', 15;
-    send_key 'alt-n';
+    send_key 'alt-n';   # next
 }
 
 1;
