00900ef8a6ad9325525925023bc920ec78f0bc4e
Jozef Pupava <jpupava@suse.com>
2015-08-27 08:56:44 +0200

    more time for C++ compilation

--- tests/toolchain/gcc5_Cpp_compilation.pm
+++ tests/toolchain/gcc5_Cpp_compilation.pm
@@ -15,7 +15,7 @@ sub run() {
     script_run 'mv cfe-3.6.2.src/* llvm-3.6.2.src/tools/clang/';
     script_run 'cd llvm-3.6.2.src';
     assert_script_run './configure --disable-bindings', 100;
-    assert_script_run 'make -j$(getconf _NPROCESSORS_ONLN)', 3000;
+    assert_script_run 'make -j$(getconf _NPROCESSORS_ONLN)', 4000;
     script_run 'cd tools/clang';
     assert_script_run 'make test', 500;
 }
