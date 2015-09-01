804240b1c4cd906533ddc5002931467ac6c14236
Jozef Pupava <jpupava@suse.com>
2015-09-01 14:05:18 +0200

    updated JeOS scripts

--- tests/jeos/sccreg.pm
+++ tests/jeos/sccreg.pm
@@ -7,16 +7,8 @@ sub run() {
     my $sccmail = get_var("SCC_EMAIL");
     my $scccode = get_var("SCC_REGCODE");
 
-    assert_screen 'linux-login';
-    ttylogin;
-    
-    become_root;
-
     assert_script_run "SUSEConnect --url=https://scc.suse.com -e $sccmail -r $scccode";
-    
     script_run 'exit'; # leave root
-    script_run 'exit'; # leave user
-    
 }
 
 sub test_flags() {
