c4fd07f6ad491b6bf3c541ee88414c2312b4d853
sysrich <sysrich@linux.com>
2015-09-15 15:20:51 +0200

    Slow down before reconnect even more, only reconnect VNC if not textmode

--- tests/installation/reconnect_s390.pm
+++ tests/installation/reconnect_s390.pm
@@ -15,7 +15,7 @@ use feature qw/say/;
 sub run() {
 
     wait_serial("Welcome to SUSE Linux Enterprise Server", 300);
-    sleep 10; #Slight delay to make sure the machine has really started and is ready for connection via SSH
+    sleep 30; #FIXME Slight delay to make sure the machine has really started and is ready for connection via SSH
 
     activate_console("ctrl-alt-f2", "ssh-xterm_vt");
     activate_console("ctrl-alt-f3", "ssh-xterm_vt");
@@ -23,11 +23,12 @@ sub run() {
     activate_console("ctrl-alt-f5", "ssh-xterm_vt");
     activate_console("ctrl-alt-f6", "ssh-xterm_vt");
 
-#TODO - this test probably needs to be smarter and use one of the newly created xterms to configure/start VNC, and then be dumber, because we're not going to test X11, SSH-X and SSH textmode after this point
-    if (get_var("DISPLAY")->{TYPE} eq "VNC") {
-    # The vnc parameters are taken from vars.json; connect to the
-    # Xvnc running on the system under test...
-    activate_console("installation", "remote-vnc" );
+    if (!check_var('DESKTOP', 'textmode') {
+        if (get_var("DISPLAY")->{TYPE} eq "VNC") {
+        # The vnc parameters are taken from vars.json; connect to the
+        # Xvnc running on the system under test...
+        activate_console("installation", "remote-vnc" );
+        }
     }
 }
 
