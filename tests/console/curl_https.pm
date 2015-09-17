b3e056c9463fed318bb5abad12704a7f506640ef
Ludwig Nussel <ludwig.nussel@suse.de>
2015-09-17 10:16:08 +0200

    fix curl_https test by using a different site

--- tests/console/curl_https.pm
+++ tests/console/curl_https.pm
@@ -4,8 +4,8 @@ use testapi;
 # test for bug https://bugzilla.novell.com/show_bug.cgi?id=598574
 sub run() {
     my $self = shift;
-    validate_script_output('curl -v https://www.opensuse.org 2>&1',
-                           sub { m,Location: http://www.opensuse.org/en/, });
+    validate_script_output('curl -v https://eu.httpbin.org/get 2>&1',
+                           sub { m,subjectAltName: eu.httpbin.org matched, });
 }
 
 1;
