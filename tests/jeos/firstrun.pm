7e0ae03ac05ab337fd93973ca205e48857f5181c
Ludwig Nussel <ludwig.nussel@suse.de>
2015-09-25 14:08:46 +0200

    update test cases for JeOS

--- tests/jeos/firstrun.pm
+++ tests/jeos/firstrun.pm
@@ -6,49 +6,46 @@ use ttylogin;
 sub run() {
     my $self = shift;
 
-    assert_screen "jeos-grub2", 15;
-    send_key 'ret'; # Press enter
+    assert_screen 'jeos-license'; # License time
+    send_key 'ret';
+    assert_screen 'jeos-doyouaccept';
+    send_key 'ret';
+
     assert_screen 'jeos-keylayout', 200; # Language picker
 
-    if (get_var("INSTLANG")) {
-        my $lang = get_var("INSTLANG");
-        send_key_until_needlematch "jeos-lang-$lang", 'u'; # Press u until it gets to the $lang menu option
-        send_key 'ret';
-        send_key_until_needlematch "jeos-system-locale-$lang", 'e', 50;
-        send_key 'ret';
-    } else {
-        send_key_until_needlematch 'jeos-lang-us', 'u'; # Press u until it gets to the US menu option
-        send_key 'ret';
-        send_key_until_needlematch 'jeos-system-locale-us', 'e', 50;
-        send_key 'ret';
+    my $lang = get_var("INSTLANG", 'us');
+    send_key_until_needlematch "jeos-lang-$lang", 'u'; # Press u until it gets to the $lang menu option
+    send_key 'ret';
+    send_key_until_needlematch "jeos-system-locale-$lang", 'e', 50;
+    send_key 'ret';
 
-    }
     assert_screen 'jeos-timezone';  # timzezone window, continue with selected timezone
     send_key "ret";
+
     assert_screen 'jeos-root-password'; # set root password
     type_password;
-    send_key 'ret'; # Press enter, go to License
+    send_key 'ret';
+
+    assert_screen 'jeos-confirm-root-password'; # confirm root password
+    type_password;
+    send_key 'ret';
+
+    assert_screen 'jeos-please-register';
+    send_key 'ret';
+
     assert_screen 'linux-login';
-    send_key 'ctrl-alt-f4';
-    assert_screen 'tty4-selected';
-    assert_screen 'text-login';
-    type_string "root\n";
-    assert_screen 'password-prompt', 10;
-    type_password;
-    send_key 'ret';
-    assert_screen 'jeos-license'; # License time
-    send_key_until_needlematch 'jeos-license-end', 'pgdn'; # Might as well scroll to the bottom, somewhat redundant
-    send_key 'q';   # quit license
-    assert_screen 'jeos-doyouaccept';
-    send_key 'y';   # acept license
-    send_key 'ret';
-    assert_screen 'jeos-firstrun-finished'; # Check the config made
-    script_run "useradd -m $username"; # Make bernhard his account
-    script_run "passwd $username"; # set bernhards password
+
+    ttylogin 4, 'root';
+
+    assert_script_run "useradd -m $username"; # create bernhard account
+    my $str = time;
+    script_run "passwd $username; echo $str-\$?- > /dev/$serialdev"; # set bernhards password
     type_password;
     send_key 'ret';
     type_password;
     send_key 'ret';
+    my $ret = wait_serial "$str-\\d+-", 10;
+    die "passwd failed" unless (defined $ret && $ret =~ /$str-0-/);
 }
 
 sub test_flags() {
