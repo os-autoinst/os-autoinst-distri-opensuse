fa3a306604569bf0df61e3d373885bdb7d48c195
Stephan Kulow <coolo@suse.de>
2015-09-26 20:27:55 +0200

    fixing the chrome test to install properly
    The 2nd page is still not loaded correctly, but I that's something that
    might be random

--- tests/x11/chrome.pm
+++ tests/x11/chrome.pm
@@ -16,16 +16,24 @@ sub run() {
     x11_start_program("xterm");
     assert_screen('xterm-started');
 
-    script_sudo "zypper -n install $chrome_url; echo \"zypper-chrome-\$?- > /dev/$serialdev\"";
-    wait_serial "zypper-chrome-0-";
+    # fetch the google key first
+    assert_script_run "wget https://dl.google.com/linux/linux_signing_key.pub";
+    script_sudo "sudo rpm --import linux_signing_key.pub";
+
+    # validate it's properly installed
+    script_run "rpm -qi gpg-pubkey-7fac5991-*";
+    assert_screen 'google-key-installed';
+
+    script_sudo "zypper -n install $chrome_url; echo zypper-chrome-\$?- > /dev/$serialdev";
+    wait_serial "zypper-chrome-0-" || die "didn't install chrome";
     save_screenshot;
     send_key "alt-f4";
 
     x11_start_program("google-chrome");
 
-    assert_screen 'google-chrome-main-window', 10;
-    send_key "esc"; # get rid of popup
-    sleep 1;
+    assert_and_click 'chrome-default-browser-query';
+
+    assert_screen 'google-chrome-main-window', 50;
     send_key "ctrl-l";
     sleep 1;
     type_string "about:\n";
