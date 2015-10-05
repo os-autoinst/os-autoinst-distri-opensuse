c0c432ff93cd22068a04b3ee8350393ee25ef7d5
Jozef Pupava <jpupava@suse.com>
2015-10-05 08:56:55 +0200

    wait longer till previously used repo appears

--- tests/installation/upgrade_select.pm
+++ tests/installation/upgrade_select.pm
@@ -16,7 +16,7 @@ sub run() {
     # hardware detection can take a while
     assert_screen "select-for-update", 100;
     send_key $cmd{"next"}, 1;
-    assert_screen "remove-repository", 30;
+    assert_screen "remove-repository", 100;
     send_key $cmd{"next"}, 1;
     if (check_var('DISTRI', 'opensuse')) {
         if (check_screen('network-not-configured', 5)) {
