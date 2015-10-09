6a0c300a14c311876981a02fdbe2713183e64d16
soulofdestiny <mgriessmeier@suse.com>
2015-10-09 10:43:59 +0200

    fix for different console-behaviour on s390x

--- lib/ttylogin.pm
+++ lib/ttylogin.pm
@@ -20,16 +20,21 @@ sub ttylogin {
     # we need to wait more than five seconds here to pass the idle timeout in
     # case the system is still booting (https://bugzilla.novell.com/show_bug.cgi?id=895602)
     assert_screen "tty$ttynr-selected", 10;
-    assert_screen "text-login", 10;
-    type_string "$user\n";
-    if (!get_var("LIVETEST")) {
-        assert_screen "password-prompt", 10;
-        type_password;
-        type_string "\n";
+
+    # different console-behaviour for s390x
+    if (check_var('BACKEND', 's390x')) {
+      script_run ("su -m $user");
+    } 
+    else {
+      assert_screen "text-login", 10;
+      type_string "$user\n";
+      if (!get_var("LIVETEST")) {
+          assert_screen "password-prompt", 10;
+          type_password;
+          type_string "\n";
+      } 
     }
     assert_screen "text-logged-in", 10;
 }
 
 1;
-
-# vim: sw=4 et
