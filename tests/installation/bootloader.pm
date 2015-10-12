58de56e606db6e1d97b974fbeef4f628d1b5701e
Ludwig Nussel <ludwig.nussel@suse.de>
2015-10-12 11:07:09 +0200

    no need to eject cd for zdup
    legacy has boot from harddisk anyways, so no need to eject the cd

--- tests/installation/bootloader.pm
+++ tests/installation/bootloader.pm
@@ -23,11 +23,6 @@ sub run() {
 
     assert_screen "inst-bootmenu", 15;
     if (get_var('ZDUP')) {
-        if (get_var('SUSEMIRROR') || get_var('ZDUPREPOS')) {
-            eject_cd;
-            power('reset');
-            sleep 10;
-        }
         send_key 'ret';    # boot from hard disk
         return;
     }
