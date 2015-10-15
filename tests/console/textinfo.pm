9a8a06607eaf83c483d622e893ead4c1cc8b38b0
Ludwig Nussel <ludwig.nussel@suse.de>
2015-10-15 18:04:17 +0200

    avoid duplicating serial device info in textinfo

--- tests/console/textinfo.pm
+++ tests/console/textinfo.pm
@@ -5,8 +5,7 @@ use testapi;
 # have various useful general info included in videos
 sub run() {
     my $self = shift;
-    script_sudo("/home/$username/data/textinfo 2>&1 | tee /home/$username/info.txt");
-    wait_serial('textinfo_done', 180) || die "textinfo test failed";
+    assert_script_sudo("/home/$username/data/textinfo 2>&1 | tee /home/$username/info.txt");
     upload_logs("info.txt");
     upload_logs("/tmp/logs.tar.bz2");
     assert_screen "texinfo-logs-uploaded";
