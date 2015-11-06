2bfcc67dd481e4e58e4254dd8b2fbf059b08126f
Jozef Pupava <jpupava@suse.com>
2015-11-06 23:44:25 +0100

    [revert] updated installation

--- tests/installation/livecdreboot.pm
+++ tests/installation/livecdreboot.pm
@@ -5,31 +5,40 @@ use bmwqemu ();
 
 sub run() {
     my $self = shift;
+    # NET isos are slow to install
+    my $timeout = 2000;
 
+    # workaround for yast popups
+    my @tags = qw/rebootnow/;
+    if (get_var("UPGRADE")) {
+        push(@tags, "ERROR-removing-package");
+        $timeout = 5500;    # upgrades are slower
+    }
     while (1) {
-        if (check_screen 'inst-finish', 180) {
-            last;
+        my $ret = assert_screen \@tags, $timeout;
+
+        if ($ret->{needle}->has_tag("popup-warning")) {
+            record_soft_failure;
+            bmwqemu::diag "warning popup caused dent";
+            send_key "ret";
+            pop @tags;
+            next;
         }
-        mouse_set(50, 50);  # avoid black screen
-        mouse_hide;
-        if (wait_still_screen(10, 11)) {    # don't look for pop-ups when screen is changing
-            if (check_screen 'inst-error-details', 1) {
-                send_key "alt-d", 2;   # details
-            }
-            if (check_screen 'inst-error-retry', 1) {
-                record_soft_failure;
-                send_key "alt-y", 2;   # retry
-            }
-            if (check_screen 'inst-error-info', 1) {
-                record_soft_failure;
-                send_key "alt-o", 2;   # OK
-            }
+        # can happen multiple times
+        if ($ret->{needle}->has_tag("ERROR-removing-package")) {
+            record_soft_failure;
+            send_key 'alt-d';
+            assert_screen 'ERROR-removing-package-details';
+            send_key 'alt-i';
+            assert_screen 'ERROR-removing-package-warning';
+            send_key 'alt-o';
+            next;
         }
+        last;
     }
 
-    if (assert_screen 'rebootnow', 420) {
-        send_key 'alt-s', 4;   # Stop the reboot countdown
-    }
+    send_key 'alt-s';    # Stop the reboot countdown
+
     send_key "ctrl-alt-f2";
     if (get_var("LIVECD")) {
         # LIVE CDa do not run inst-consoles as started by inst-linux (it's regular live run, auto-starting yast live installer)
@@ -71,33 +80,33 @@ sub run() {
     }
 
     # XXX old stuff
-    #		if(get_var("XDEBUG") && assert_screen "the-system-will-reboot-now", 3000) {
-    #			send_key "alt-s";
-    #			send_key "ctrl-alt-f2";
-    #			if(!get_var("NET")) {
-    #				script_run "dhcpcd eth0";
-    #				#ifconfig eth0 10.0.2.15
-    #				#route add default gw 10.0.2.2
-    #				sleep 20;
-    #			}
-    #			script_run "mount /dev/vda2 /mnt";
-    #			script_run "chroot /mnt";
-    #			script_run "echo nameserver 213.133.99.99 > /etc/resolv.conf";
-    #			script_run "wget www3.zq1.de/bernhard/linux/xdebug";
-    #			script_run "sh -x xdebug";
-    #			sleep 99;
-    #			send_key "ctrl-d";
-    #			script_run "umount /mnt";
-    #			wait_idle;
-    #			sleep 20;
-    #			send_key "ctrl-alt-f7";
-    #			sleep 5;
-    #			send_key "alt-o";
-    #		}
-    #		if(get_var("UPGRADE")) {
-    #			send_key "alt-n"; # ignore repos dialog
-    #			wait_still_screen(6,60);
-    #		}
+    #          if(get_var("XDEBUG") && assert_screen "the-system-will-reboot-now", 3000) {
+    #                  send_key "alt-s";
+    #                  send_key "ctrl-alt-f2";
+    #                  if(!get_var("NET")) {
+    #                          script_run "dhcpcd eth0";
+    #                          #ifconfig eth0 10.0.2.15
+    #                          #route add default gw 10.0.2.2
+    #                          sleep 20;
+    #                  }
+    #                  script_run "mount /dev/vda2 /mnt";
+    #                  script_run "chroot /mnt";
+    #                  script_run "echo nameserver 213.133.99.99 > /etc/resolv.conf";
+    #                  script_run "wget www3.zq1.de/bernhard/linux/xdebug";
+    #                  script_run "sh -x xdebug";
+    #                  sleep 99;
+    #                  send_key "ctrl-d";
+    #                  script_run "umount /mnt";
+    #                  wait_idle;
+    #                  sleep 20;
+    #                  send_key "ctrl-alt-f7";
+    #                  sleep 5;
+    #                  send_key "alt-o";
+    #          }
+    #          if(get_var("UPGRADE")) {
+    #                  send_key "alt-n"; # ignore repos dialog
+    #                  wait_still_screen(6,60);
+    #          }
 
     # Await a grub screen for 30s, if seen hit ENTER (in case we did not wait long enough, the 'grub timeout' would
     # pass and still perform the boot; so we want a value short enough to not wait forever if grub does not appear,
