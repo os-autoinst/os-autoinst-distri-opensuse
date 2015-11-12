b80aac177207b0f1f74a11da6ef0274013c2b305
Yong Sun <yosun@suse.com>
2015-11-12 16:51:31 +0800

    splite acceptance test into three to avoid timeout issue

---
+++ tests/qa_automation/acceptance_fs_stress.pm
@@ -0,0 +1,17 @@
+use base "qa_run";
+use testapi;
+
+sub test_run_list() {
+    return qw(_reboot_off fs_stress);
+}
+
+sub junit_type() {
+    return 'stress_validation';
+}
+
+sub test_suite() {
+    return 'acceptance';
+}
+
+1;
+
