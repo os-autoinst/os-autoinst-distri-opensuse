d6f2045c6b9e7d37dfeddc62d9fd30cd26f4bec6
Dumitru Gutu <dgutu@suse.com>
2015-11-12 16:48:12 +0100

    Addapted a2ps as requested

--- tests/console/a2ps.pm
+++ tests/console/a2ps.pm
@@ -3,12 +3,14 @@ use base "consoletest";
 use testapi;
 use ttylogin;
 
+# a2ps is available by default only in openSUSE*
+
 sub run() {
-    become_root;
-    assert_script_run("zypper -n in a2ps | tee /dev/$serialdev");
-    assert_script_run("curl https://www.suse.com > /tmp/suse.html");
-    assert_script_run("a2ps -o /tmp/suse.ps /tmp/suse.html | tee /dev/$serialdev");
-    assert_screen "a2ps_saved", 5;
+become_root;
+    assert_script_run("zypper -n in a2ps");
+    assert_script_run("curl https://www.suse.com -o /tmp/suse.html");
+    assert_script_run("a2ps -o /tmp/suse.ps /tmp/suse.html");
+    assert_screen "a2ps_saved";
 }
 
 1;
