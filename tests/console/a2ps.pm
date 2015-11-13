4d2459f41c215206d21b768727b477ee549bf08a
Dumitru Gutu <dgutu@suse.com>
2015-11-13 13:10:30 +0100

    Tidy tools executed against a2ps.pm

--- tests/console/a2ps.pm
+++ tests/console/a2ps.pm
@@ -6,7 +6,7 @@ use ttylogin;
 # a2ps is available by default only in openSUSE*
 
 sub run() {
-become_root;
+    become_root;
     assert_script_run("zypper -n in a2ps");
     assert_script_run("curl https://www.suse.com -o /tmp/suse.html");
     assert_script_run("a2ps -o /tmp/suse.ps /tmp/suse.html");
