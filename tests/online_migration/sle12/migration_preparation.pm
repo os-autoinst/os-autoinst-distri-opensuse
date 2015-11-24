600eabb11483dd77a26e3bd947aaa42e2a8a3d87
mitiao <mitiao@gmail.com>
2015-11-24 16:26:07 +0800

    tidy up some codes

--- tests/online_migration/sle12/migration_preparation.pm
+++ tests/online_migration/sle12/migration_preparation.pm
@@ -6,8 +6,8 @@ sub run() {
     my $self = shift;
 
     # add migration repo
-    my $script = "zypper ar -f ".get_var('MIGRATION_REPO')." migration"."\n";
-    validate_script_output $script, sub {m/successfully added/};
+    my $script = "zypper ar -f " . get_var('MIGRATION_REPO') . " migration\n";
+    validate_script_output $script, sub { m/successfully added/ };
 
     # dup from migration repo
     assert_script_run "zypper --gpg-auto-import-keys refresh";
@@ -15,12 +15,12 @@ sub run() {
     wait_serial qr/^There are some running programs that might use files/m, 200;
 
     # install yast2 migration or zypper migration plugin
-    assert_script_run "zypper -n in yast2-migration" if (get_var('MIGRATION_METHOD') eq 'yast');
+    assert_script_run "zypper -n in yast2-migration"         if (get_var('MIGRATION_METHOD') eq 'yast');
     assert_script_run "zypper -n in zypper-migration-plugin" if (get_var('MIGRATION_METHOD') eq 'zypper');
 }
 
 sub test_flags {
-    return { 'fatal' => 1, 'important' => 1};
+    return {'fatal' => 1, 'important' => 1};
 }
 
 1;
