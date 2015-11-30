53b6dcf4248dd7613c99ed090243c9ca1dec64dd
Jozef Pupava <jpupava@suse.com>
2015-11-30 13:31:49 +0100

    addon_products_yast2 with SCC registration

--- lib/registration.pm
+++ lib/registration.pm
@@ -25,91 +25,93 @@ use testapi;
 our @EXPORT = qw/fill_in_registration_data registration_bootloader_params yast_scc_registration/;
 
 sub fill_in_registration_data {
-
-    send_key "alt-e";    # select email field
+    my ($addon, $uc_addon);
+    send_key "alt-m";        # select email field if yast2 add-on
+    send_key "alt-e";        # select email field if installation
+    send_key "backspace";    # delete m or e
     type_string get_var("SCC_EMAIL");
-    send_key "alt-c";    # select registration code field
+    send_key "alt-c";        # select registration code field
     type_string get_var("SCC_REGCODE");
     save_screenshot;
     send_key "alt-n", 1;
-    my @tags = qw/local-registration-servers registration-online-repos import-untrusted-gpg-key module-selection/;
-    push @tags, 'untrusted-ca-cert' if get_var('SCC_URL');
-    while (check_screen(\@tags, 60)) {
-        if (match_has_tag("local-registration-servers")) {
-            send_key "alt-o";
-            @tags = grep { $_ ne 'local-registration-servers' } @tags;
-            next;
-        }
-        elsif (match_has_tag("import-untrusted-gpg-key")) {
-            if (check_var("IMPORT_UNTRUSTED_KEY", 1)) {
-                send_key "alt-t", 1;    # import
+    unless (get_var('SCC_REGISTER', '') =~ /addon|network/) {
+        my @tags = qw/local-registration-servers registration-online-repos import-untrusted-gpg-key module-selection/;
+        push @tags, 'untrusted-ca-cert' if get_var('SCC_URL');
+        while (check_screen(\@tags, 60)) {
+            if (match_has_tag("local-registration-servers")) {
+                send_key "alt-o";
+                @tags = grep { $_ ne 'local-registration-servers' } @tags;
+                next;
             }
-            else {
-                send_key "alt-c", 1;    # cancel
-            }
-            next;
-        }
-        elsif (match_has_tag("registration-online-repos")) {
-            send_key "alt-y", 1;        # want updates
-            @tags = grep { $_ ne 'registration-online-repos' } @tags;
-            next;
-        }
-        elsif (get_var('SCC_URL') && match_has_tag("untrusted-ca-cert")) {
-            # bsc#943966
-            record_soft_failure if get_var('SCC_CERT');
-            send_key "alt-t", 1;        # trust
-            @tags = grep { $_ ne 'untrusted-ca-cert' } @tags;
-            next;
-        }
-        last;
-    }
-
-    assert_screen("module-selection");
-    if (get_var('SCC_ADDONS')) {
-        send_key 'tab';                 # jump to beginning of addon selection
-        for my $a (split(/,/, get_var('SCC_ADDONS'))) {
-            my $counter = 30;
-            while ($counter > 0) {
-                if (check_screen("scc-help-selected", 5)) {
-                    send_key 'tab';     # end of addon fields, jump over control buttons
-                    send_key 'tab';
-                    send_key 'tab';
-                    send_key 'tab';
-                    send_key 'tab';
+            elsif (match_has_tag("import-untrusted-gpg-key")) {
+                if (check_var("IMPORT_UNTRUSTED_KEY", 1)) {
+                    send_key "alt-t", 1;    # import
                 }
                 else {
-                    send_key ' ';       # select checkbox for needle match
-                    if (check_screen("scc-marked-$a", 5)) {
-                        last;           # match, go to next addon
-                    }
-                    else {
-                        send_key ' ';      # unselect addon if it's not expected one
-                        send_key 'tab';    # go to next field
-                    }
+                    send_key "alt-c", 1;    # cancel
                 }
-                $counter--;
+                next;
+            }
+            elsif (match_has_tag("registration-online-repos")) {
+                send_key "alt-y", 1;        # want updates
+                @tags = grep { $_ ne 'registration-online-repos' } @tags;
+                next;
+            }
+            elsif (get_var('SCC_URL') && match_has_tag("untrusted-ca-cert")) {
+                # bsc#943966
+                record_soft_failure if get_var('SCC_CERT');
+                send_key "alt-t", 1;        # trust
+                @tags = grep { $_ ne 'untrusted-ca-cert' } @tags;
+                next;
+            }
+            last;
+        }
+    }
+
+    if (check_var('SCC_REGISTER', 'installation')) {
+        for $addon (split(/,/, get_var('ADDONS', ''))) {
+            if (check_var('DESKTOP', 'textmode')) {
+                send_key_until_needlematch "scc-module-$addon", 'tab';
+                send_key "spc";
+            }
+            else {
+                assert_and_click "scc-module-$addon";
             }
         }
-        send_key 'alt-n';                  # next, all addons selected
-        for $a (split(/,/, get_var('SCC_ADDONS'))) {
-            assert_screen("scc-addon-license-$a");
-            send_key "alt-a";              # accept license
-            send_key "alt-n";              # next
+        send_key 'alt-n';    # next, all addons selected
+        for $addon (split(/,/, get_var('ADDONS', ''))) {
+            assert_screen("scc-addon-license-$addon");
+            send_key "alt-a", 1;    # accept license
+            send_key "alt-n", 1;    # next
         }
-        for $a (split(/,/, get_var('SCC_ADDONS'))) {
-            $a = uc $a;                    # change to uppercase to match variable
-            if (my $regcode = get_var("SCC_REGCODE_$a")) {
-                assert_screen("scc-addon-regcode-$a");
-                send_key 'tab';            # jump to code field
+        for $addon (split(/,/, get_var('ADDONS', ''))) {
+            $uc_addon = uc $addon;    # change to uppercase to match variable
+            if (my $regcode = get_var("SCC_REGCODE_$uc_addon")) {
+                next if ($addon =~ /sdk|rt/);    # bsc#956726
+                if (check_var('DESKTOP', 'textmode')) {
+                    send_key_until_needlematch "scc-code-field-$addon", 'tab';
+                }
+                else {
+                    assert_and_click "scc-code-field-$addon";
+                }
                 type_string $regcode;
-                send_key "alt-n";          # next
+                sleep 1;
+                save_screenshot;
             }
         }
+        send_key 'alt-n', 2;
+        if (check_screen('import-untrusted-gpg-key')) {
+            record_soft_failure;
+            send_key 'alt-t', 2;
+        }
+        sleep 20;    # scc registration need some time
     }
     else {
-        send_key "alt-n";                  # next
+        if (!get_var('SCC_REGISTER', '') =~ /addon|network/) {
+            assert_screen("module-selection");
+            send_key "alt-n";    # next
+        }
     }
-    sleep 10;                              # scc registration need some time
 }
 
 sub registration_bootloader_params {
--- tests/installation/addon_products_yast2.pm
+++ tests/installation/addon_products_yast2.pm
@@ -3,64 +3,8 @@ use strict;
 use testapi;
 use registration;
 
-sub addon_yast2 {
-    if (get_var("BETA_$b")) {
-        assert_screen "addon-betawarning-$a";
-        send_key "ret";
-        assert_screen "addon-license-beta";
-    }
-    else {
-        assert_screen "addon-license-$a";
-    }
-    send_key 'alt-a', 2;    # yes, agree
-    send_key 'alt-n', 2;    # next
-    assert_screen 'addon-yast2-patterns';
-    sleep 2;
-    send_key 'alt-v', 2;    # view tab
-    send_key 'spc';         # open view menu
-    send_key 'alt-r';
-    send_key 'alt-r';       # go to repositories
-    send_key 'ret';         # open repositories tab
-    assert_screen "addon-yast2-repo-$a";
-    send_key 'alt-a';       # accept
-    assert_screen 'automatic-changes';
-    send_key 'alt-o';       # OK
-    if (check_screen 'unsupported-packages', 5) {
-        record_soft_failure;
-        send_key 'alt-o';
-    }
-    if (check_screen 'addon-installation-pop-up', 100) {    # e.g. RT reboot to activate new kernel
-        send_key 'alt-o';                                   # OK
-    }
-    assert_screen "addon-installation-report";
-    send_key 'alt-f';                                       # finish
-    assert_screen 'scc-registration';
-
-    if (get_var('SCC_REGISTER') eq 'addon') {
-        fill_in_registration_data;
-    }
-    else {
-        send_key "alt-s", 1;                                # skip SCC registration
-        if (check_screen("scc-skip-reg-warning")) {
-            send_key "alt-y", 1;                            # confirmed skip SCC registration
-        }
-        if (check_screen("scc-skip-base-system-reg-warning")) {
-            send_key "alt-y", 1;                            # confirmed skip SCC registration
-        }
-        send_key "tab",                                 1;
-        send_key "pgup",                                1;
-        send_key_until_needlematch "addon-products-$a", 'down';
-        if ((split(/,/, get_var('ADDONS')))[-1] ne $a) {
-            send_key 'alt-a', 2;
-        }
-        else {
-            send_key 'alt-o', 2;
-        }
-    }
-}
-
 sub run() {
-    my $self = shift;
+    my ($addon, $uc_addon);
     x11_start_program("xdg-su -c '/sbin/yast2 add-on'");
     if ($password) { type_password; send_key "ret", 1; }
     if (check_screen 'packagekit-warning') {
@@ -69,31 +13,91 @@ sub run() {
     assert_screen 'addon-products';
     send_key 'alt-a', 2;     # add add-on
     if (get_var("ADDONS")) {
-        foreach $a (split(/,/, get_var('ADDONS'))) {
-            our $b = uc $a;    # varibale name is upper case
-            if (get_var("ADDONURL_$b")) {
-                send_key 'alt-u';    # specify url
+        foreach $addon (split(/,/, get_var('ADDONS'))) {
+            $uc_addon = uc $addon;    # varibale name is upper case
+            assert_screen 'inst-addon';
+            if (check_var('SCC_REGISTER', 'network')) {
+                send_key 'alt-u';     # specify url
                 send_key 'alt-n';
                 assert_screen 'addonurl-entry', 3;
-                type_string get_var("ADDONURL_$b");
-                send_key 'alt-p';    # name
-                type_string "SLE$b" . "12-SP1_repo";
+                type_string get_var("ADDONURL_$uc_addon");
+                send_key 'alt-p';     # name
+                type_string "SLE$uc_addon" . "12-SP1_repo";
                 send_key 'alt-n';
-                $self->addon_yast2;
             }
             else {
-                send_key 'alt-v',                            3;             # DVD
-                send_key 'alt-n',                            3;
-                assert_screen 'dvd-selector',                3;
-                send_key_until_needlematch 'addon-dvd-list', 'tab', 10;     # jump into addon list
-                send_key_until_needlematch "addon-dvd-$a",   'down', 10;    # select addon in list
-                send_key 'alt-o';                                           # continue
-                $self->addon_yast2;
+                send_key 'alt-v',                              3;             # DVD
+                send_key 'alt-n',                              3;
+                assert_screen 'dvd-selector',                  3;
+                send_key_until_needlematch 'addon-dvd-list',   'tab', 10;     # jump into addon list
+                send_key_until_needlematch "addon-dvd-$addon", 'down', 10;    # select addon in list
+                send_key 'alt-o';                                             # continue
+            }
+            if (get_var("BETA_$uc_addon")) {
+                assert_screen "addon-betawarning-$addon";
+                send_key "ret";
+                assert_screen "addon-license-beta";
+            }
+            else {
+                assert_screen "addon-license-$addon";
+            }
+            send_key 'alt-a',                                       2;             # yes, agree
+            send_key 'alt-n',                                       2;             # next
+            send_key_until_needlematch 'addon-yast2-view-selected', 'alt-v', 10;
+            send_key 'spc';                                                        # open view menu
+            send_key 'alt-r', 1;
+            send_key 'alt-r', 1;                                                   # go to repositories
+            send_key 'ret';                                                        # open repositories tab
+            assert_screen "addon-yast2-repo-$addon";
+            send_key 'alt-a';                                                      # accept
+            assert_screen 'automatic-changes';
+            send_key 'alt-o';                                                      # OK
+            if (check_screen 'unsupported-packages', 5) {
+                record_soft_failure;
+                send_key 'alt-o';
+            }
+            if (check_screen 'addon-installation-pop-up', 100) {                   # e.g. RT reboot to activate new kernel
+                send_key 'alt-o';                                                  # OK
+            }
+            assert_screen "addon-installation-report";
+            send_key 'alt-f', 2;                                                   # finish
+            if (get_var('SCC_REGISTER')) {
+                if (check_screen('scc-registration', 5)) {
+                    fill_in_registration_data;
+                }
+                if ($addon ne 'sdk') {                                             # sdk doesn't ask for code
+                    my $regcode = get_var("SCC_REGCODE_$uc_addon");
+                    assert_screen "addon-reg-code";
+                    send_key 'tab';                                                # jump to code field
+                    type_string $regcode;
+                    sleep 1;
+                    save_screenshot;
+                    send_key "alt-n";                                              # next
+                }
+                assert_screen 'addon-products',                     60;
+                send_key "tab",                                     1;             # select addon-products-$addon
+                send_key "pgup",                                    1;
+                send_key_until_needlematch "addon-products-$addon", 'down';
+            }
+            else {
+                send_key "alt-s", 1;                                               # skip SCC registration
+                if (check_screen("scc-skip-reg-warning")) {
+                    send_key "alt-y", 1;                                           # confirmed skip SCC registration
+                }
+                if (check_screen("scc-skip-base-system-reg-warning")) {
+                    send_key "alt-y", 1;                                           # confirmed skip SCC registration
+                }
+            }
+            if ((split(/,/, get_var('ADDONS')))[-1] ne $addon) {                   # if $addon is not first from all ADDONS
+                send_key 'alt-a', 2;                                               # add another add-on
+            }
+            else {
+                send_key 'alt-o', 2;                                               # ok continue
             }
         }
     }
     else {
-        send_key 'alt-n', 2;                                                # done
+        send_key 'alt-n', 2;                                                       # done
     }
 }
 
