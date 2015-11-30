33f8bae36235fd5e815ea894daa462f786a272ef
Stephan Kulow <coolo@suse.de>
2015-11-30 07:48:17 +0100

    log packages also on ipmi machines

--- main.pm
+++ main.pm
@@ -391,7 +391,7 @@ sub load_inst_tests() {
     if (noupdatestep_is_applicable && !get_var("LIVECD")) {
         loadtest "installation/installer_timezone.pm";
     }
-    if (noupdatestep_is_applicable && !get_var("LIVECD") && !get_var("NICEVIDEO") && !get_var("IPMI_HOSTNAME")) {
+    if (noupdatestep_is_applicable && !get_var("LIVECD")) {
         loadtest "installation/logpackages.pm";
     }
     if (noupdatestep_is_applicable) {
