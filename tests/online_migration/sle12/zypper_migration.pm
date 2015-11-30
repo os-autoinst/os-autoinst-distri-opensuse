ad37cda363ff939767df4fc0b18231c2d02a0917
mitiao <mitiao@gmail.com>
2015-11-30 16:09:28 +0800

    tidy up some codes

--- tests/online_migration/sle12/zypper_migration.pm
+++ tests/online_migration/sle12/zypper_migration.pm
@@ -6,17 +6,17 @@ sub run() {
     my $self = shift;
 
     # precompile regexes
-    my $zypper_migration_target = qr/\[num\/q\]/m;
-    my $zypper_disable_repos = qr/^Disable obsolete repository/m;
-    my $zypper_migraiton_conflict = qr/^Choose from above solutions by number[\s\S,]* \[1/m;
-    my $zypper_migration_error = qr/^Abort, retry, ignore\? \[a/m;
+    my $zypper_migration_target       = qr/\[num\/q\]/m;
+    my $zypper_disable_repos          = qr/^Disable obsolete repository/m;
+    my $zypper_migraiton_conflict     = qr/^Choose from above solutions by number[\s\S,]* \[1/m;
+    my $zypper_migration_error        = qr/^Abort, retry, ignore\? \[a/m;
     my $zypper_migration_fileconflict = qr/^File conflicts .*^Continue\? \[y/ms;
-    my $zypper_migration_done = qr/^Executing.*after online migration|^ZYPPER-DONE/m;
-    my $zypper_continue = qr/^Continue\? \[y/m;
-    my $zypper_finish = qr/^There are some running programs that might use files|^ZYPPER-DONE/m;
+    my $zypper_migration_done         = qr/^Executing.*after online migration|^ZYPPER-DONE/m;
+    my $zypper_continue               = qr/^Continue\? \[y/m;
+    my $zypper_finish                 = qr/^There are some running programs that might use files|^ZYPPER-DONE/m;
 
     # start migration
-    script_run ("(zypper migration;echo ZYPPER-DONE) | tee /dev/$serialdev");
+    script_run("(zypper migration;echo ZYPPER-DONE) | tee /dev/$serialdev");
     my $out = wait_serial([$zypper_migration_target, $zypper_disable_repos, $zypper_continue, $zypper_migration_done, $zypper_migration_error, $zypper_migraiton_conflict, $zypper_migration_fileconflict], 5000);
     while ($out) {
         if ($out =~ $zypper_migration_target) {
--- tests/online_migration/sle12/zypper_patch_up.pm
+++ tests/online_migration/sle12/zypper_patch_up.pm
@@ -6,17 +6,17 @@ sub run() {
     my $self = shift;
 
     # precompile regexes
-    my $zypper_continue = qr/^Continue\? \[y/m;
-    my $zypper_conflict = qr/^Choose from above solutions by number[\s\S,]* \[1/m;
-    my $zypper_error = qr/^Abort, retry, ignore\? \[a/m;
+    my $zypper_continue     = qr/^Continue\? \[y/m;
+    my $zypper_conflict     = qr/^Choose from above solutions by number[\s\S,]* \[1/m;
+    my $zypper_error        = qr/^Abort, retry, ignore\? \[a/m;
     my $zypper_fileconflict = qr/^File conflicts .*^Continue\? \[y/ms;
-    my $zypper_finish = qr/^There are some running programs that might use files|^ZYPPER-DONE/m;
+    my $zypper_finish       = qr/^There are some running programs that might use files|^ZYPPER-DONE/m;
 
     # zypper patch and fully update the system
-    script_run ("(zypper -n patch;echo ZYPPER-DONE) | tee /dev/$serialdev");
+    script_run("(zypper -n patch;echo ZYPPER-DONE) | tee /dev/$serialdev");
     wait_serial $zypper_finish, 100;
 
-    script_run ("(zypper up --auto-agree-with-licenses;echo ZYPPER-DONE) | tee /dev/$serialdev");
+    script_run("(zypper up --auto-agree-with-licenses;echo ZYPPER-DONE) | tee /dev/$serialdev");
     my $out = wait_serial([$zypper_continue, $zypper_conflict, $zypper_error, $zypper_fileconflict, $zypper_finish], 5000);
     while ($out) {
        if ($out =~ $zypper_conflict || $out =~ $zypper_error || $out =~ $zypper_fileconflict) {
