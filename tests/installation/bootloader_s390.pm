fe90629c19c36709efc797e36dada7939015c04b
Stephan Kulow <coolo@suse.de>
2015-12-05 11:38:20 +0100

    the extracted ISO is REPO_0 now

--- tests/installation/bootloader_s390.pm
+++ tests/installation/bootloader_s390.pm
@@ -61,7 +61,7 @@ sub get_to_yast() {
 
     # we have to hardcode the hostname here - the true hostname would
     # create a too long parameter ;(
-    $params .= " install=ftp://openqa/" . get_var('REPO_8') . " ";
+    $params .= " install=ftp://openqa/" . get_var('REPO_0') . " ";
 
     my $parmfile_with_Newline_s = split_lines($params);
     print "P $parmfile_with_Newline_s \n";
@@ -72,7 +72,7 @@ sub get_to_yast() {
     # qboot
     my $ftp_server = get_var('OPENQA_HOSTNAME') or die;
     # TODO: find the proper repo for 'ISO'
-    my $dir_with_suse_ins = get_var('REPO_8');
+    my $dir_with_suse_ins = get_var('REPO_0');
     $s3270->sequence_3270("String(\"qaboot $ftp_server $dir_with_suse_ins\")", "ENTER", "Wait(InputField)",);
 
     ##############################
