9e9eacb189dd3befc4d45776bf0826b9e74d22a8
Stephan Kulow <coolo@suse.de>
2015-12-14 12:41:55 +0100

    fix perl failure

--- tests/installation/change_desktop.pm
+++ tests/installation/change_desktop.pm
@@ -6,7 +6,7 @@ use testapi;
 sub accept3rdparty {
     my ($self) = @_;
     #Third party licenses sometimes appear
-    while (my $ret = check_screen([qw/3rdpartylicense automatic-changes inst-overview/]), 15) {
+    while (my $ret = check_screen([qw/3rdpartylicense automatic-changes inst-overview/], 15)) {
         last if $ret->{needle}->has_tag("automatic-changes");
         last if $ret->{needle}->has_tag("inst-overview");
         send_key $cmd{acceptlicense}, 1;
