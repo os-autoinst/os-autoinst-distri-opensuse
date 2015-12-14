9835ceecbde6a4110115a08ffb45c74a59d984e1
Stephan Kulow <coolo@suse.de>
2015-12-14 17:19:07 +0100

    record at least a second of silence

--- tests/x11/firefox_audio.pm
+++ tests/x11/firefox_audio.pm
@@ -6,6 +6,7 @@ sub run() {
     start_audiocapture;
     x11_start_program("firefox " . autoinst_url . "/data/1d5d9dD.oga");
     assert_screen 'test-firefox_audio-1', 35;
+    sleep 1; # at least a second of silence
     assert_recorded_sound('DTMF-159D');
     send_key "alt-f4";
     if (check_screen('firefox-save-and-quit', 4)) {
