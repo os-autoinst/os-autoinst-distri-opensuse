14203beedb59f30a6c545de3026128cde99e9423
mkravec <mkravec@suse.com>
2016-01-04 15:04:52 +0100

    JeOS-SLE-SP1 fixes

--- tests/jeos/firewall_enabled.pm
+++ tests/jeos/firewall_enabled.pm
@@ -5,11 +5,11 @@ use testapi;
 sub run() {
     my $check_state    = "systemctl show --no-pager SuSEfirewall2 | grep ActiveState | cut -d'=' -f2";
     my $check_substate = "systemctl show --no-pager SuSEfirewall2 | grep SubState | cut -d'=' -f2";
-    my $check_ssh_port = "grep 'FW_SERVICES_EXT_TCP=' /etc/sysconfig/SuSEfirewall2 | cut -d'\"' -f2";
+    my $check_ssh_port = "grep -E 'FW_SERVICES_EXT_TCP=|FW_CONFIGURATIONS_EXT=' /etc/sysconfig/SuSEfirewall2 | cut -d'\"' -f2 | grep -v '^\$'";
 
     validate_script_output $check_state,    sub { /^active$/ };
     validate_script_output $check_substate, sub { /^exited$/ };
-    validate_script_output $check_ssh_port, sub { /^22$/ };
+    validate_script_output $check_ssh_port, sub { /^sshd|22$/ };
 }
 
 sub test_flags() {
