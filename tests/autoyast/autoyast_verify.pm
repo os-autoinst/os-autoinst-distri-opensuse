132aa705d7be0a1be4ed0e57689162c54579ec28
Vladimir Nadvornik <nadvornik@suse.cz>
2016-01-06 17:54:45 +0100

    the test can continue after autoyast_verify

--- tests/autoyast/autoyast_verify.pm
+++ tests/autoyast/autoyast_verify.pm
@@ -89,7 +89,7 @@ sub test_flags {
     # 'fatal' - whole test suite is in danger if this fails
     # 'milestone' - after this test succeeds, update 'lastgood'
     # 'important' - if this fails, set the overall state to 'fail'
-    return {fatal => 1};
+    return {important => 1};
 }
 
 1;
