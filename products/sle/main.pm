4003408d9628a1c588fdb7ed833bf3fb3014ba6d
Fabian Vogt <fvogt@suse.com>
2016-01-29 13:06:43 +0100

    Add new yast2_nfs_server test
    This tests "yast2 nfs-server" by creating an NFS share,
    writing a file to it and validating that the file is accessible
    after mounting.
    It can also be used as a server in an "/ on NFS" test scenario.
    In this case, NFSSERVER has to be 1, the server is accessible as
    10.0.2.1 and it provides a mutex "nfs_ready".

--- products/sle/main.pm
+++ products/sle/main.pm
@@ -567,6 +567,10 @@ sub load_consoletests() {
         }
         loadtest "console/mtab.pm";
         if (!get_var("NOINSTALL") && !get_var("LIVETEST") && !is_desktop && (check_var("DESKTOP", "textmode"))) {
+            if (check_var('BACKEND', 'qemu')) {
+                # The NFS test expects the IP to be 10.0.2.15
+                loadtest "console/yast2_nfs_server.pm";
+            }
             loadtest "console/http_srv.pm";
             loadtest "console/mysql_srv.pm";
         }
