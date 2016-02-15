893cadc8c60b8f45a07cf010ccdb6a55faf644e8
Yong Sun <yosun@suse.com>
2016-02-15 17:08:56 +0800

    add kernel regression test memeat and memtester

---
+++ tests/qa_automation/kernel_memtester.pm
@@ -0,0 +1,17 @@
+use base "qa_run";
+use testapi;
+
+sub test_run_list() {
+        return qw(_reboot_off memtester);
+}
+
+sub test_suite() {
+        return 'kernel';
+}
+
+sub junit_type() {
+        return 'kernel_regression';
+}
+
+1;
+
