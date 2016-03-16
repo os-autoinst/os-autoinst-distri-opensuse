b6371ad653c2e3ad3ca3d7b51d527bbc6c8e6b65
xiaojun <xjin@suse.com>
2016-03-16 15:55:40 +0800

    openqa script and entry for tc#1503803 tc#1503905; entry for tc#1503973

---
+++ tests/x11regressions/gnomecase/login_test.pm
@@ -0,0 +1,51 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+use base "x11test";
+use testapi;
+use utils;
+
+#testcase 5255-1503803: Gnome:Change Password
+#other login scenario has been coverred by the change_password script, here
+#only cover the auto_login
+
+sub auto_login_alter {
+    send_key "super";
+    type_string "users", 1;    #use 1 to give gnome-shell enough time searching the user-settings module.
+    send_key "ret";
+    assert_screen "users-settings";
+    assert_and_click "Unlock";
+    type_string "$password";
+    assert_and_click "authenticate";
+    send_key "alt-u";
+    send_key "alt-f4";
+}
+
+sub reboot_system {
+    wait_idle;
+    send_key "ctrl-alt-delete";    #reboot
+    assert_screen 'logoutdialog', 15;
+    assert_and_click 'logoutdialog-reboot-highlighted';
+    if (check_screen("reboot-auth", 5)) {
+        type_string "$rootpwd", 1;
+        assert_and_click "authenticate";
+    }
+    assert_screen "generic-desktop", 200;
+}
+
+sub run () {
+    my $self = shift;
+
+    auto_login_alter;
+    reboot_system;
+    auto_login_alter;
+}
+
+1;
+# vim: set sw=4 et:
