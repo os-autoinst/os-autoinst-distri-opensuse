ffa245b023193a4a831047f95558ba9ab1c2dba9
Ondřej Súkup <osukup@suse.cz>
2016-05-09 09:38:24 +0200

    capture dmesg and journal in qam.pm

--- lib/qam.pm
+++ lib/qam.pm
@@ -40,12 +40,19 @@ sub capture_state {
         upload_logs "/tmp/y2logs_$state.tar.bz2";
         save_screenshot();
     }
+    #upload ip status
     script_run("ip a | tee /tmp/ip_a_$state.log");
     upload_logs("/tmp/ip_a_$state.log");
     save_screenshot();
     script_run("ip r | tee /tmp/ip_r_$state.log");
     upload_logs("/tmp/ip_r_$state.log");
     save_screenshot();
+    #upload dmesg
+    script_run("dmesg > /tmp/dmesg_$state.log");
+    upload_logs("/tmp/dmesg_$state.log");
+    #upload journal
+    script_run("journalctl -b > /tmp/journal_$state.log");
+    upload_logs("/tmp/journal_$state.log");
 }
 
 1;
