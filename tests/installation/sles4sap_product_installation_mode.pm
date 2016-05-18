227190261eb07e1b3bc05d595e07efb0b76c2994
Denis Zyuzin <dzyuzin@suse.com>
2016-05-18 11:40:28 +0200

    Add checks to SLES4SAP product selection

--- tests/installation/sles4sap_product_installation_mode.pm
+++ tests/installation/sles4sap_product_installation_mode.pm
@@ -6,10 +6,13 @@ use testapi;
 sub run() {
     assert_screen "sles4sap-product-installation-mode";
     send_key "alt-p";    # Proceed with standard SLES installation
+    assert_screen "sles4sap-standard-sles-selected";
     if (get_var("SLES4SAP_MODE") =~ /sles4sap/) {
         send_key "alt-o";    # prOceed with standard SLES for SAP Application installation
+        assert_screen "sles4sap-product-selected";
         if (check_var('SLES4SAP_MODE', 'sles4sap_wizard')) {
             send_key "alt-s";    # Start the SAP Installation Wizard right after the OS installation
+            assert_screen "sles4sap-wizard-selected";
         }
     }
     save_screenshot;
