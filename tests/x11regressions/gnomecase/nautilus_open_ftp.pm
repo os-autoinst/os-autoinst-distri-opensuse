2a7cc23bbbc090d474dca800a2a3311185d6a45e
xiaojun <xjin@suse.com>
2016-05-27 17:26:32 +0800

    Reneedle and Fix an o.s.d issue
      Reneedling for gnome:nautilus - Apply for sp2 or later
      Fix issue - installation_snapshots failed due to timeout

--- tests/x11regressions/gnomecase/nautilus_open_ftp.pm
+++ tests/x11regressions/gnomecase/nautilus_open_ftp.pm
@@ -24,6 +24,11 @@ sub run() {
     send_key "ret";
     assert_screen 'nautilus-ftp-suse-com', 20;
 
+    if (sle_version_at_least('12-SP2')) {
+        assert_and_click "unselected-pub";
+        assert_and_click "ftp-path-selected";
+        wait_still_screen;
+    }
     send_key "shift-f10";
     assert_screen 'nautilus-ftp-rightkey-menu', 3;
     send_key "u";    #umount the ftp
