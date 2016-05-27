946e811ac2800004164db81dd01f5bf606e4c160
Dumitru Gutu <dgutu@suse.com>
2016-05-27 12:05:48 +0200

    Updated version of - Snapper cleanup test based on fate##312751

--- tests/console/snapper_cleanup.pm
+++ tests/console/snapper_cleanup.pm
@@ -13,20 +13,7 @@ use strict;
 use utils;
 use testapi;
 
-my $snapper_create;
-
-sub run() {
-    select_console 'root-console';
-
-    if (get_var("UPGRADE") || get_var("AUTOUPGRADE")) {
-        script_run("snapper setup-quota");
-        script_run("snapper set-config NUMBER_LIMIT=2-10 NUMBER_LIMIT_IMPORTANT=4-10 | tee /dev/$serialdev");
-    }
-    script_run("snapper set-config NUMBER_MIN_AGE=0 | tee /dev/$serialdev");
-    assert_script_run("btrfs qgroup show -p /");
-
-
-    $snapper_create .= "
+my $snapper_create .= "
     snapper create --cleanup number --command 'dd if=/dev/urandom of=data bs=1M count=1024'
     snapper create --cleanup number --command 'dd if=/dev/urandom of=data bs=1M count=1024'
     snapper create --cleanup number --command 'dd if=/dev/urandom of=data bs=1M count=1024'
@@ -35,48 +22,33 @@ sub run() {
     snapper create --cleanup number -u important=yes --command 'dd if=/dev/urandom of=data bs=1M count=1024'
     snapper create --cleanup number -u important=yes --command 'dd if=/dev/urandom of=data bs=1M count=1024'
     snapper create --cleanup number -u important=yes --command 'dd if=/dev/urandom of=data bs=1M count=1024'";
-    #1
+
+sub snapper() {
     assert_script_run("$snapper_create", 800);
     clear_console;
-
-    script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper cleanup number");    # cleanup created snapshots
+    assert_script_run("btrfs qgroup show -p / | tail -20", 3);
+    script_run("snapper cleanup number"); # cleanup created snapshots
     assert_script_run("btrfs qgroup show -p / ", 3);
     script_run("snapper ls");
+    script_run("btrfs filesystem df -h /"); 
     clear_console;
+    }
 
-    script_run("df -h /");
-    script_run("btrfs filesystem df -h /");
-    #2
-    assert_script_run("$snapper_create", 800);
-    script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper cleanup number");    # cleanup created snapshots
-    assert_script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper ls");
-    clear_console;
+sub run() {
+    select_console 'root-console';
 
-    script_run("df -h /");
-    script_run("btrfs filesystem df -h /");
-    #3
-    assert_script_run("$snapper_create", 800);
-    script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper cleanup number");    # cleanup created snapshots
-    assert_script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper ls");
-    clear_console;
+    if (get_var("UPGRADE") || get_var("AUTOUPGRADE")) {
+    script_run("snapper setup-quota");
+    script_run("snapper set-config NUMBER_LIMIT=2-10 NUMBER_LIMIT_IMPORTANT=4-10 | tee /dev/$serialdev");
+    };
+    
+    script_run("snapper set-config NUMBER_MIN_AGE=0 | tee /dev/$serialdev");
+    assert_script_run("btrfs qgroup show -p /");
 
-    script_run("df -h /");
-    script_run("btrfs filesystem df -h /");
-    #4
-    assert_script_run("$snapper_create", 800);
-    script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper cleanup number");    # cleanup created snapshots
-    assert_script_run("btrfs qgroup show -p / ", 3);
-    script_run("snapper ls");
-    clear_console;
-
-    script_run("df -h /");
-    script_run("btrfs filesystem df -h /");
+    snapper();
+    snapper();
+    snapper();
+    snapper();
 
     script_run("snapper set-config NUMBER_MIN_AGE=1800 | tee /dev/$serialdev");
 }
