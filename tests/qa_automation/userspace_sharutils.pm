4251d793b237ac42ffa24bf2d9767ec5597eeaa0
Yong Sun <yosun@suse.com>
2016-06-20 15:37:56 +0800

    split userspace into pieces for easy review.

---
+++ tests/qa_automation/userspace_sharutils.pm
@@ -0,0 +1,28 @@
+# SUSE's openQA tests
+#
+# Copyright © 2009-2013 Bernhard M. Wiedemann
+# Copyright © 2012-2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+use base "qa_run";
+use strict;
+use testapi;
+
+sub test_run_list() {
+    return qw(_reboot_off sharutils);
+}
+
+sub test_suite() {
+    return 'regression';
+}
+
+sub junit_type() {
+    return 'user_regression';
+}
+
+1;
+
