973436093caa458ec9e17c1ff3ea2f329cd10018
Qingming Su <qingming.su@suse.com>
2016-07-07 12:41:18 +0800

    Setup system work into fips mode
    Install fips pattern and update grub.cfg with fips=1
    
    Also include workaround of bsc#982268:
    Due to bsc#982268, openssl couldn't enter fips mode even the
    system is booted with fips=1. Workaround is create the file
    /etc/system-fips manually.

---
+++ tests/fips/fips_setup.pm
@@ -0,0 +1,32 @@
+# SUSE's FIPS tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+use base "consoletest";
+use testapi;
+use strict;
+
+# Install fips pattern and update grub.cfg to boot with fips=1
+sub run() {
+    select_console 'root-console';
+
+    if (!script_output "grep 'fips=1' /proc/cmdline | tee") {
+        script_run 'zypper -n install --type pattern fips', 300;
+        script_run 'sed -i \'/^GRUB_CMDLINE_LINUX_DEFAULT/s/\("\)$/ fips=1\1/\' /etc/default/grub';
+        script_run 'grub2-mkconfig -o /boot/grub2/grub.cfg';
+    }
+
+    # Workaround bsc#982268
+    if (!script_output "find /etc -type f -name system-fips") {
+        script_run 'touch /etc/system-fips';
+        record_soft_failure 'bsc#982268';
+    }
+}
+
+1;
+# vim: set sw=4 et:
