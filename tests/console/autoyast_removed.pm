68ef53d3e8b14dc5e2cef7ebf86dd286f7035e83
mkravec <mkravec@suse.com>
2016-07-13 14:23:38 +0200

    Confirm autoyast creation is removed from installation overview Resolves poo#11442

---
+++ tests/console/autoyast_removed.pm
@@ -0,0 +1,21 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+use base "consoletest";
+use strict;
+use testapi;
+
+# Check autoyast has been removed in SP2 (fate#317970)
+sub run() {
+    select_console("root-console");
+    assert_script_run("[ ! -f /root/autoinst.xml ]");
+}
+
+1;
+# vim: set sw=4 et:
