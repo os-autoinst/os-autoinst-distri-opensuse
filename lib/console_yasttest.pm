e3743c1ea839595906d4ec2a373a3f505ae4b741
Oliver Kurz <okurz@suse.de>
2016-07-29 22:21:28 +0200

    Must use physical device link for btrfs-debug-tree

--- lib/console_yasttest.pm
+++ lib/console_yasttest.pm
@@ -22,10 +22,11 @@ sub post_fail_hook() {
         # there is a severe problem, e.g. could be bsc#985850 or bsc#990384 so
         # save more, let's hope there is enough memory for intermediate
         # storage
-        # TODO just assuming that '/dev/root' is the root device here, does
+        # TODO just assuming that '/dev/vda2' is the root device here, does
         # not work for LVM setup and others but we want to debug non-LVM first
-        $fn = '/dev/shm/root_brfs_debug_tree';
-        assert_script_run "btrfs-debug-tree /dev/root &> $fn";
+        # /dev/root is not recognized as btrfs device
+        $fn = '/dev/shm/vda2_brfs_debug_tree';
+        assert_script_run "btrfs-debug-tree /dev/vda2 &> $fn";
         upload_logs $fn;
     }
     save_screenshot;
