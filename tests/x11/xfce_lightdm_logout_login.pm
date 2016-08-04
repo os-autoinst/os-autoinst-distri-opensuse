f88e701f3e95cecd4404aa53a1fddf904a8cbf3d
Stephan Kulow <coolo@kde.org>
2016-08-04 08:37:14 +0200

    Fix lightdm check: hide the mouse after coming back to desktop (#1635)
    Fixes https://openqa.opensuse.org/tests/236012#step/inkscape/1

--- tests/x11/xfce_lightdm_logout_login.pm
+++ tests/x11/xfce_lightdm_logout_login.pm
@@ -14,19 +14,19 @@ use testapi;
 
 # log out, check lightdm-gtk-greeter and log in again
 
-# this part contains the steps to run this test
 sub run() {
     my $self = shift;
     x11_start_program("xfce4-session-logout");
     send_key "alt-l";
     assert_screen 'test-xfce_lightdm_logout_login-1', 13;
-    mouse_hide();
     type_password;
     send_key "ret";
+    assert_screen 'generic-desktop';
+    mouse_hide;
 }
 
 sub test_flags() {
-    return {important => 1};
+    return {important => 1, milestone => 1};
 }
 
 1;
