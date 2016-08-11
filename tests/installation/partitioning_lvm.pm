07f8184adf8a365fca67f94fce5ab33f4e46d62a
Richard Palethorpe <rpalethorpe@suse.com>
2016-08-11 16:05:59 +0200

    cryptlvm: Introduce ENCRYPT_FORCE_RECOMPUTE
    Introduces a variable to force an encrypted partition proposal to be
    generated regardless of whether the default partition proposal is an
    encrypted one. If the default partition proposal is not encrypted it
    will now signal a soft failure whether the variable is set or not.
    
    Also ensures a screen shot of the radio buttons in the partitioning
    proposal is taken.

--- tests/installation/partitioning_lvm.pm
+++ tests/installation/partitioning_lvm.pm
@@ -14,16 +14,24 @@ use base "y2logsstep";
 use testapi;
 
 sub run() {
-    my $self = shift;
+    my $self             = shift;
+    my $file_system_tags = [
+        qw/
+          partitioning-no-root-filesystem partitioning-encrypt-activated-existing
+          partitioning-encrypt-ignored-existing
+          /
+    ];
 
     if (get_var('ENCRYPT_ACTIVATE_EXISTING')) {
-        assert_screen [qw/partitioning-no-root-filesystem partitioning-encrypt-activated-existing/];
+        assert_screen $file_system_tags;
         if (match_has_tag('partitioning-no-root-filesystem')) {
             record_soft_failure 'bsc#989750';
         }
-        else {
-            return;
+        elsif (match_has_tag('partitioning-encrypt-ignored-existing')) {
+            record_soft_failure 'bsc#993247';
         }
+
+        return unless get_var 'ENCRYPT_FORCE_RECOMPUTE';
     }
 
     send_key "alt-d";
@@ -37,13 +45,18 @@ sub run() {
                 send_key 'tab';
                 type_password;
                 send_key 'ret';
+            } else {
+                save_screenshot;
             }
         }
         else {
             send_key 'alt-l';
         }
         send_key 'alt-o';
-        assert_screen [qw/partition-lvm-new-summary partitioning-encrypt-activated-existing/];
+        assert_screen [qw/partition-lvm-new-summary partitioning-encrypt-activated-existing partitioning-encrypt-broke-existing/];
+        if (match_has_tag('partitioning-encrypt-broke-existing')) {
+            record_soft_failure 'bsc#993249';
+        }
     }
     elsif (!get_var('ENCRYPT_ACTIVATE_EXISTING')) {    # old behaviour still needed
         send_key "alt-l", 1;                           # enable LVM-based proposal
