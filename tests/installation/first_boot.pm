f59f3dc646d0237f29b82a52099cf0e8b8ca2288
Richard Palethorpe <rpalethorpe@suse.com>
2016-08-12 16:26:51 +0200

    first_reboot: use wait_screen_change for screenshot

--- tests/installation/first_boot.pm
+++ tests/installation/first_boot.pm
@@ -102,9 +102,9 @@ sub post_fail_hook() {
     my $self = shift;
 
     # Reveal what is behind Plymouth splash screen
-    send_key 'esc';
-    save_screenshot;
-
+    wait_screen_change {
+        send_key 'esc';
+    };
     $self->export_logs();
 }
 
