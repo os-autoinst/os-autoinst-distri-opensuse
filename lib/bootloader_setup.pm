os-autoinst/os-autoinst-distri-opensuse

commit 0809e3943c5a005a987e7e96bf5d774d1ededa08

Fix return cases in bootloader

lib/bootloader_setup.pm
@@ -33,7 +33,7 @@ our @EXPORT = qw/
 sub pre_bootmenu_setup {
     if (get_var("IPXE")) {
         sleep 60;
-        return;
+        return 3;
     }
     if (get_var("USBBOOT")) {
         assert_screen "boot-menu", 5;
@@ -47,7 +47,7 @@ sub pre_bootmenu_setup {
     if (get_var("BOOT_HDD_IMAGE")) {
         assert_screen "grub2", 15;    # Use the same bootloader needle as in grub-test
         send_key "ret";               # boot from hd
-        return;
+        return 3;
     }
 }
 
@@ -55,7 +55,7 @@ sub select_bootmenu_option {
     assert_screen 'inst-bootmenu';
     if (get_var('ZDUP') || get_var("ONLINE_MIGRATION")) {
         send_key 'ret';               # boot from hard disk
-        return;
+        return 3;
     }
 
     if (get_var("UPGRADE")) {

tests/installation/bootloader.pm
@@ -18,8 +18,8 @@ use registration;
 # hint: press shift-f10 trice for highest debug level
 sub run() {
     pre_bootmenu_setup;
-    select_bootmenu_option;
-    bootmenu_default_params;
+    return if select_bootmenu_option == 3;
+    return if bootmenu_default_params == 3;
     bootmenu_network_source;
     specific_bootmenu_params;
     registration_bootloader_params(utils::VERY_SLOW_TYPING_SPEED);

commit e8eea02e628520ab56ed0908b6c996c4241407f1

Fix uninitialized value caused by 0809e3943c5a005a987e7e96bf5d774d1ededa08

lib/bootloader_setup.pm
@@ -49,6 +49,7 @@ sub pre_bootmenu_setup {
         send_key "ret";               # boot from hd
         return 3;
     }
+    return 0;
 }
 
 sub select_bootmenu_option {
@@ -70,6 +71,7 @@ sub select_bootmenu_option {
             send_key_until_needlematch('inst-oninstallation', 'down', 10, 5);
         }
     }
+    return 0;
 }
 
 sub bootmenu_default_params {

tests/installation/bootloader.pm
@@ -17,9 +17,9 @@ use registration;
 
 # hint: press shift-f10 trice for highest debug level
 sub run() {
-    pre_bootmenu_setup;
+    return if pre_bootmenu_setup == 3;
     return if select_bootmenu_option == 3;
-    return if bootmenu_default_params == 3;
+    bootmenu_default_params;
     bootmenu_network_source;
     specific_bootmenu_params;
     registration_bootloader_params(utils::VERY_SLOW_TYPING_SPEED);