ba3b9d162f55832fb9a1cd80f83dd832eebc5063
Jozef Pupava <jpupava@suse.com>
2016-09-08 09:38:27 +0200

    Retry 7 times on most failing parts
    https://progress.opensuse.org/issues/12758

--- tests/x11/reboot_gnome.pm
+++ tests/x11/reboot_gnome.pm
@@ -14,20 +14,21 @@ use testapi;
 use utils;
 
 sub run() {
-    wait_idle;
-    send_key "ctrl-alt-delete";    # reboot
-    assert_screen 'logoutdialog', 15;
+    wait_still_screen;
+    send_key_until_needlematch 'logoutdialog', 'ctrl-alt-delete', 7, 10;    # reboot
     assert_and_click 'logoutdialog-reboot-highlighted';
 
     if (get_var("SHUTDOWN_NEEDS_AUTH")) {
-        assert_screen 'reboot-auth', 15;
-        sleep 3;
+        assert_screen 'reboot-auth';
+        wait_still_screen;
         type_password;
-        sleep 3;
-        assert_and_click 'reboot-auth-typed', 'right';    # Extra assert_and_click (with right click) to check the correct number of characters is typed and open up the 'show text' option
-        assert_and_click 'reboot-auth-showtext';          # Click the 'Show Text' Option to enable the display of the typed text
-        assert_screen 'reboot-auth-correct-password';     # Check the password is correct
-
+        wait_still_screen;
+        wait_screen_change {
+            assert_and_click 'reboot-auth-typed', 'right';                  # Extra assert_and_click (with right click) to check the correct number of characters is typed and open up the 'show text' option
+        };
+        wait_screen_change {
+            assert_and_click 'reboot-auth-showtext';                        # Click the 'Show Text' Option to enable the display of the typed text
+        };
         # we need to kill ssh for iucvconn here,
         # because after pressing return, the system is down
         prepare_system_reboot;
