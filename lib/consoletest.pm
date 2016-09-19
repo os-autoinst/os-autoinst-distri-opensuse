67d5b8c82cf9d0ee8476d663b0b41abe23c8148b
mkravec <mkravec@suse.com>
2016-09-19 16:31:53 +0200

    Go to home directory after console test finishes

--- lib/consoletest.pm
+++ lib/consoletest.pm
@@ -1,13 +1,16 @@
 package consoletest;
 use base "opensusebasetest";
+
 use strict;
+use testapi;
 
-# Base class for all openSUSE tests
-
-use testapi ();
+# Base class for all openSUSE console tests
 
 sub post_run_hook {
     my ($self) = @_;
+
+    # start next test in home directory
+    type_string "cd\n";
 
     # clear screen to make screen content ready for next test
     $self->clear_and_verify_console;
