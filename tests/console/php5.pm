31d49f8ed05f6ea8801182a656d672713888b5b3
Romanos Dodopoulos <romanos.dodopoulos@suse.cz>
2016-10-12 13:31:05 +0200

    LAMP(php) test
    Install LAMP and test the basic functionality of PHP
    (apache2-mod_php5), the connector between PHP and MySQL (php5-mysql),
    the connector between PHP and PostgreSQL (php5-pgsql), and
    shibboleth-sp.
    
    This commit enables also the conosole/postgresql94server test in
    main.pm
    
    Those test are all triggered when DESKTOP is set as 'textmode'. The
    php5* tests are triggered when the wsm module is also defined either
    in ADDONS or in SCC_ADDONS variables.

---
+++ tests/console/php5.pm
@@ -0,0 +1,37 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Simple PHP5 code hosted locally
+# Maintainer: Romanos Dodopoulos <romanos.dodopoulos@suse.cz>
+
+# This test requires the Web and Scripting module on SLE. Also, it
+# should preferably be executed after the 'console/http_srv' test.
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run() {
+    my $self = shift;
+    select_console 'root-console';
+
+    # install requirements
+    zypper_call "in php5 apache2-mod_php5";
+    assert_script_run "a2enmod php5";
+
+    # configure and restart the web server
+    type_string qq{echo -e "<?php\nphpinfo()\n?>" > /srv/www/htdocs/index.php\n};
+    assert_script_run "systemctl restart apache2.service";
+
+    # test that PHP works
+    assert_script_run "curl --no-buffer http://localhost/index.php | grep \"\$(uname -s -n -r -v -m)\"";
+}
+1;
