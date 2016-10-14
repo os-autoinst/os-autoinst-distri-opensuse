0bb8cbaeabc0cff83cc2a2df14caec37f59f10d4
xiao li ai <xlai@suse.com>
2016-10-14 21:42:52 +0800

    Virtualization: Add new feature test, pvusb test (#1857)
    Summary:
        This feature allows USB devices to be passthrough directly to the guest.
        The added tests are unit test for this feature.
        Fate link: https://fate.suse.com/316612
    
    Maintainer:
        Alice Ai, email: xlai@suse.com

---
+++ tests/virt_autotest/pvusb_run.pm
@@ -0,0 +1,38 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: This feature allows USB devices to be passthrough directly to the guest.
+#          The added tests are unit test for this feature.
+#          Fate link: https://fate.suse.com/316612
+#
+# Maintainer: xlai@suse.com
+use strict;
+use warnings;
+use base "virt_autotest_base";
+use testapi;
+
+sub get_script_run() {
+    my $pre_test_cmd = "/usr/share/qa/tools/test_virtualization-pvusb-run";
+    my $which_usb = get_var("USB_PATTERN", "");
+    $pre_test_cmd = $pre_test_cmd . " -w \"" . $which_usb . "\"";
+
+    return $pre_test_cmd;
+}
+
+sub run() {
+    my $self = shift;
+    $self->run_test(5000, "Congratulations! All test is successful!", "no", "yes", "/var/log/qa/ctcs2/", "pvusb-test-logs");
+}
+
+sub test_flags {
+    return {important => 1};
+}
+
+1;
+
