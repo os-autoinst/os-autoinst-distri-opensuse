a318e8d082731fbd45e50ab2fe27b6c3dbd94d40
Ondřej Súkup <mimi1vx@users.noreply.github.com>
2016-10-17 14:20:43 +0200

    Cleanup DEFAULT_VM to start GDM with default DE (#1975)

--- tests/qam-minimal/install_patterns.pm
+++ tests/qam-minimal/install_patterns.pm
@@ -8,14 +8,14 @@
 # without any warranty.
 
 
-# G-Summary: QAM Minimal test in openQA
+# Summary: QAM Minimal test in openQA
 #    it prepares minimal instalation, boot it, install tested incident , try
 #    reboot and update system with all released updates.
 #
 #    with QAM_MINIMAL=full it also installs gnome-basic, base, apparmor and
 #    x11 patterns and reboot system to graphical login + start console and
 #    x11 tests
-# G-Maintainer: Ondřej Súkup <osukup@suse.cz>
+# Maintainer: Ondřej Súkup <osukup@suse.cz>
 
 use base "basetest";
 
@@ -38,9 +38,9 @@ sub run {
     zypper_call("in -t pattern base x11 gnome-basic apparmor", exitcode => [0, 102], timeout => 2000);
 
     assert_script_run("systemctl set-default graphical.target");
-    assert_script_run('sed -i -r "s/^DISPLAYMANAGER=\"\"/DISPLAYMANAGER=\"gdm\"/" /etc/sysconfig/displaymanager');
-    assert_script_run('sed -i -r "s/^DISPLAYMANAGER_AUTOLOGIN/#DISPLAYMANAGER_AUTOLOGIN/" /etc/sysconfig/displaymanager');
-
+    script_run('sed -i -r "s/^DISPLAYMANAGER=\"\"/DISPLAYMANAGER=\"gdm\"/" /etc/sysconfig/displaymanager');
+    script_run('sed -i -r "s/^DISPLAYMANAGER_AUTOLOGIN/#DISPLAYMANAGER_AUTOLOGIN/" /etc/sysconfig/displaymanager');
+    script_run('sed -i -r "s/^DEFAULT_WM=\"icewm\"/DEFAULT_VM=\"\"/" /etc/sysconfig/windowmanager');
     # now we have gnome installed - restore DESKTOP variable
     set_var('DESKTOP', get_var('FULL_DESKTOP'));
 
