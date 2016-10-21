83e2a5e8e7f2573fd15fb4b56dc80f6c594c963b
Michal Nowak <mnowak@suse.com>
2016-10-21 09:24:11 +0200

    Better handle screen distortion from previous test
    On PPC64LE, at least, the test which runs before `crash`, creates
    post-mortem screen distortion. Previously we tried to clear the screen
    before needle matching, but a more robust solution is to restart the
    system and then do the matching. To achieve that code was reshuffled
    here and there.

--- tests/toolchain/crash.pm
+++ tests/toolchain/crash.pm
@@ -16,46 +16,20 @@ use testapi;
 use utils;
 
 sub install_kernel_debuginfo {
+    # list installed kernels
+    assert_script_run 'rpmquery -a kernel-default*';
     # kernel debug symbols are huge, this can take a while
-    assert_script_run 'zypper ref; zypper -n -v in kernel-default-base-debuginfo kernel-default-debuginfo', 1200;
+    assert_script_run 'zypper ref; zypper -n -v in kernel-default-debuginfo', 1200;
 }
 
 sub run() {
     select_console('root-console');
 
-    script_run 'zypper -n in yast2-kdump kdump crash';
-
-    script_run 'yast2 kdump', 0;
-
-    # It seems that `toolchain/gcc5_Cpp_compilation.pm` spits some visual artifacts
-    # (https://openqa.suse.de/tests/522356/file/video.ogv) which prevent needle match.
-    # It's not enough to wait, we have to refresh the screen before check_screen or
-    # assert_screen.
-    wait_still_screen(20, 60);
-    send_key 'ctrl-l';    # refresh the screen
-
-    if (check_screen 'yast2-kdump-disabled') {
-        send_key 'alt-u';    # enable kdump
-    }
-
-    send_key 'ctrl-l';       # refresh the screen
-    assert_screen 'yast2-kdump-enabled';
-    send_key 'alt-o';        # OK
-
-    send_key 'ctrl-l';       # refresh the screen
-    if (check_screen 'yast2-kdump-restart-info') {
-        send_key 'alt-o';    # OK
-    }
-
-    # activate kdump
-    wait_still_screen(10, 30);
-    script_run 'reboot', 0;
-    wait_boot;
-    select_console 'root-console';
-
     # disable packagekitd
     script_run 'systemctl mask packagekit.service';
     script_run 'systemctl stop packagekit.service';
+
+    script_run 'zypper -n in yast2-kdump kdump crash';
 
     # add debuginfo channels
     if (check_var('DISTRI', 'sle')) {
@@ -74,6 +48,25 @@ sub run() {
         assert_script_run "zypper -n mr -d $opensuse_debug_repos";
     }
 
+    # activate kdump
+    script_run 'yast2 kdump', 0;
+    if (check_screen 'yast2-kdump-disabled') {
+        send_key 'alt-u';    # enable kdump
+    }
+
+    assert_screen 'yast2-kdump-enabled';
+    send_key 'alt-o';        # OK
+
+    if (check_screen 'yast2-kdump-restart-info') {
+        send_key 'alt-o';    # OK
+    }
+
+    wait_still_screen(10, 30);
+    script_run 'reboot', 0;
+    wait_boot;
+    select_console 'root-console';
+
+    # make sure kdump is enabled after reboot
     validate_script_output "yast2 kdump show 2>&1", sub { m/Kdump is enabled/ }, 90;
 
     # get dump
@@ -83,8 +76,9 @@ sub run() {
     wait_boot;
     select_console 'root-console';
 
+    # all but PPC64LE arch's vmlinux images are gzipped
     my $suffix = check_var('ARCH', 'ppc64le') ? '' : '.gz';
-    my $crash_cmd = 'echo exit | crash `ls -1t /var/crash/*/vmcore | head -n1` /boot/vmlinux-`uname -r`' . $suffix;
+    my $crash_cmd = "echo exit | crash `ls -1t /var/crash/*/vmcore | head -n1` /boot/vmlinux-`uname -r`$suffix";
     assert_script_run "$crash_cmd";
     validate_script_output "$crash_cmd", sub { m/PANIC/ };
 }
