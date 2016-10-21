18df23c4f6343ccc75bb8d8ffefdc4ed28939e6b
Dehai Kong <dhkong@suse.com>
2016-10-21 16:53:11 +0800

    Git:add git case based on test case #1525281 (#2003)
    This script based on test case #1525281-FIPS: git, could be running at
    opensuse and sle , to check whether can be work with ssh or https protocol.

---
+++ tests/console/git.pm
@@ -0,0 +1,66 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Case 1525281  - FIPS: git
+# Summary: To check  whether can be successful via ssh or https protocol by using git
+# Maintainer: Dehai Kong <dhkong@suse.com>
+
+use base "consoletest";
+use strict;
+use testapi;
+use utils;
+
+sub run() {
+
+    my $password = $testapi::password;
+    my $username = $testapi::username;
+    my $email    = "you\@example.com";
+    select_console "root-console";
+
+    # Create a test repo
+    zypper_call("in git-core");
+    assert_script_run("mkdir -p repos/qa1;cd repos/qa1");
+    assert_script_run("git init");
+    assert_script_run("echo \"SUSE Test\" > README");
+    assert_script_run("git config --global user.email \"$email\"");
+    assert_script_run("git config --global user.name \"$username\"");
+    assert_script_run("git add README;git commit -m \"Initial commit\"");
+
+    # Clone to a bare git repo
+    assert_script_run("cd ~/repos;git clone --bare qa1 qa0");
+
+    # Clone repo via ssh
+    type_string("clear\n");
+    script_run("git clone ssh://localhost:/root/repos/qa0 qa2 | tee /dev/$serialdev", 0);
+    assert_screen("input-yes");
+    type_string("yes\n");
+    assert_screen("password-prompt");
+    type_string("$password\n");
+
+    # Push update via ssh
+    assert_script_run("cd ~/repos/qa2;echo \"Update\" >> README");
+    assert_script_run("git add README;git commit  -m \"Update README\"");
+    type_string("clear\n");
+    script_run("git push ssh://localhost:/root/repos/qa0 | tee /dev/$serialdev", 0);
+    assert_screen("password-prompt");
+    type_string("$password\n");
+
+    # git clone via https protocol
+    assert_script_run("cd ~;git clone -q https://github.com/os-autoinst/os-autoinst-distri-example");
+
+    # clean up
+    assert_script_run("rm -rf ~/repos ~/os-autoinst*");
+}
+
+sub test_flags() {
+    return {important => 1};
+}
+
+1;
+# vim: set sw=4 et:
