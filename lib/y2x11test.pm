f9dd09f9bd4c3481b040f789febecff3d121d6d7
Stephan Kulow <coolo@suse.de>
2016-10-29 17:11:07 +0200

    Remove duplicated code between 2 post_fail_hooks

--- lib/y2x11test.pm
+++ lib/y2x11test.pm
@@ -25,26 +25,8 @@ sub save_upload_y2logs() {
 }
 
 sub post_fail_hook() {
-    my $self = shift;
-
-    select_console 'root-console';
-    save_screenshot;
-
-    if (check_var("DESKTOP", "kde")) {
-        if (get_var('PLASMA5')) {
-            my $fn = '/tmp/plasma5_configs.tar.bz2';
-            my $cmd = sprintf 'tar cjf %s /home/%s/.config/*rc', $fn, $username;
-            type_string "$cmd\n";
-            upload_logs $fn;
-        }
-        else {
-            my $fn = '/tmp/kde4_configs.tar.bz2';
-            my $cmd = sprintf 'tar cjf %s /home/%s/.kde4/share/config/*rc', $fn, $username;
-            type_string "$cmd\n";
-            upload_logs $fn;
-        }
-        save_screenshot;
-    }
+    my ($self) = shift;
+    $self->export_kde_logs;
 
     type_string "cat /home/*/.xsession-errors* > /tmp/XSE\n";
     upload_logs "/tmp/XSE";
