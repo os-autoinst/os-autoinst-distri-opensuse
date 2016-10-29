dd24f97a39c18e49672b621c026fec1244ad4e3f
Stephan Kulow <coolo@suse.de>
2016-10-29 10:50:48 +0200

    Move setup of EWS in evolution to common function

--- lib/x11regressiontest.pm
+++ lib/x11regressiontest.pm
@@ -508,5 +508,89 @@ sub unlock_user_settings {
     assert_and_click "authenticate";
 }
 
+sub setup_evolution_for_ews {
+    my ($self, $mailbox, $mail_passwd) = @_;
+
+    mouse_hide(1);
+
+    # Clean and Start Evolution
+    x11_start_program("xterm -e \"killall -9 evolution; find ~ -name evolution | xargs rm -rf;\"");
+    x11_start_program("evolution");
+    assert_screen [qw(evolution-default-client-ask test-evolution-1)];
+    if (match_has_tag "evolution-default-client-ask") {
+        assert_and_click "evolution-default-client-agree";
+        assert_screen 'test-evolution-1';
+    }
+
+    # Follow the wizard to setup mail account
+    assert_screen "test-evolution-1";
+    send_key "alt-o";
+    assert_screen "evolution_wizard-restore-backup";
+    send_key "alt-o";
+    assert_screen "evolution_wizard-identity";
+    wait_screen_change {
+        send_key "alt-e";
+    };
+    type_string "SUSE Test";
+    wait_screen_change {
+        send_key "alt-a";
+    };
+    type_string "$mailbox";
+    sleep 1;
+    save_screenshot();
+
+    send_key "alt-o";
+    assert_screen [qw(evolution_wizard-skip-lookup evolution_wizard-receiving)];
+    if (match_has_tag "evolution_wizard-skip-lookup") {
+        send_key "alt-s";
+        assert_screen 'evolution_wizard-receiving';
+    }
+
+    wait_screen_change {
+        send_key "alt-t";
+    };
+    send_key "ret";
+    send_key_until_needlematch "evolution_wizard-receiving-ews", "up", 10, 3;
+    send_key "ret";
+    assert_screen "evolution_wizard-ews-prefill";
+    send_key "alt-u";
+    assert_screen "evolution_mail-auth";
+    type_string "$mail_passwd";
+    send_key "ret";
+    assert_screen "evolution_wizard-ews-oba", 300;
+    send_key "alt-o";
+    assert_screen "evolution_wizard-receiving-opts";
+    assert_and_click "evolution_wizard-ews-enable-gal";
+    assert_and_click "evolution_wizard-ews-fetch-abl";
+    assert_screen [qw/evolution_wizard-ews-view-gal evolution_mail-auth/], 120;
+    if (match_has_tag('evolution_mail-auth')) {
+        type_string "$mail_passwd";
+        send_key "ret";
+        assert_screen "evolution_wizard-ews-view-gal", 120;
+    }
+    send_key "alt-o";
+    assert_screen "evolution_wizard-account-summary";
+    send_key "alt-o";
+    assert_screen "evolution_wizard-done";
+    send_key "alt-a";
+    assert_screen "evolution_mail-auth";
+    type_string "$mail_passwd";
+    send_key "ret";
+    if (check_screen "evolution_mail-init-window") {
+        send_key "super-up";
+    }
+    assert_screen "evolution_mail-max-window";
+
+    # Make all existing mails as read
+    assert_screen "evolution_mail-online", 60;
+    assert_and_click "evolution_mail-inbox";
+    assert_screen "evolution_mail-ready", 60;
+    send_key "ctrl-/";
+    if (check_screen "evolution_mail-confirm-read") {
+        send_key "alt-y";
+    }
+    assert_screen "evolution_mail-ready", 60;
+}
+
 1;
 # vim: set sw=4 et:
