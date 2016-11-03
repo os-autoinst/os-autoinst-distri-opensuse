1d51ebd03e826bcdb1bb299631d02972d80b0245
Michal Nowak <mnowak@suse.com>
2016-11-03 08:48:37 +0100

    Change directory when sudo runs w/o permissions
    When command was run via sudo as `postgres` user bash warned that is
    'could not change directory to "/root": Permission denied'. Now the
    current working directory is chenged before sudo and re-set afterwards.
    
    Verification run: http://assam.suse.cz/tests/4101

--- tests/console/postgresql94server.pm
+++ tests/console/postgresql94server.pm
@@ -37,11 +37,17 @@ sub run() {
         assert_script_run "systemctl show -p SubState postgresql.service | grep SubState=running";
     }
 
+    # without changing current working directory we get:
+    # 'could not change directory to "/root": Permission denied'
+    assert_script_run 'pushd /tmp';
+
     # test basic functionality - require postgresql94
     assert_script_run "sudo -u postgres createdb openQAdb";
     assert_script_run "sudo -u postgres psql -d openQAdb -c \"CREATE TABLE test (id SERIAL PRIMARY KEY, entry VARCHAR)\"";
     assert_script_run "sudo -u postgres psql -d openQAdb -c \"INSERT INTO test (entry) VALUES ('openQA_test'), ('can you read this?');\"";
     assert_script_run "sudo -u postgres psql -d openQAdb -c \"SELECT * FROM test\" | grep \"can you read this\"";
+
+    assert_script_run 'popd';    # back to previous directory
 }
 
 1;
