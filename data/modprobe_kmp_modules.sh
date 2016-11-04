3f49174673bfa31fe9252ca54fb6e0a7420403df
Jozef Pupava <jpupava@suse.com>
2016-11-04 17:05:46 +0100

    Remove dropped compute kernel kmp modules
    https://fate.suse.com/320686

--- data/modprobe_kmp_modules.sh
+++ data/modprobe_kmp_modules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # load modules
-for pkg in $(rpm -qa \*-kmp-$1); do
+for pkg in $(rpm -qa \*-kmp-rt); do
   for mod in $(rpm -ql $pkg | grep '\.ko$'); do
     modname=$(basename $mod .ko)
-    modprobe -v $modname
+    modprobe $modname || fail=1
   done
 done
+if [ $fail ] ; then exit 1 ; fi
