ddca4e19ae8e25d65fbdd2197dd29c6e7ad739b4
Michal Nowak <mnowak@suse.com>
2017-01-31 15:47:00 +0100

    Run the test also without DBus

--- tests/console/snapper_undochange.pm
+++ tests/console/snapper_undochange.pm
@@ -13,26 +13,41 @@
 use base "consoletest";
 use strict;
 use testapi;
+use utils 'service_action';
 
 sub run() {
     select_console 'root-console';
 
-    my $snapfile = '/root/snapfile';
+    my $snapfile     = '/root/snapfile';
+    my @snapper_runs = 'snapper';
+    push @snapper_runs, 'snapper --no-dbus' if get_var('SNAPPER_NODBUS');
 
-    my $snapbf = script_output "snapper create -p -d 'before undochange test'", 90;
-    script_run "date > $snapfile";
-    my $snapaf = script_output "snapper create -p -d 'after undochange test'", 90;
+    foreach my $snapper (@snapper_runs) {
+        service_action('dbus', {type => ['socket', 'service'], action => ['stop', 'mask']}) if ($snapper =~ /dbus/);
 
-    # Delete snapfile
-    script_run "snapper undochange $snapbf..$snapaf $snapfile", 90;
-    script_run("test -f $snapfile || echo \"snap-ba-ok\" > /dev/$serialdev", 0);
-    wait_serial("snap-ba-ok", 30) || die "Snapper undochange $snapbf..$snapaf failed";
+        my $snapbf = script_output "$snapper create -p -d 'before undochange test'", 90;
+        script_run "date > $snapfile";
+        my $snapaf = script_output "$snapper create -p -d 'after undochange test'", 90;
 
-    # Restore snapfile
-    script_run "snapper undochange $snapaf..$snapbf $snapfile", 90;
-    assert_script_run("test -f $snapfile", timeout => 10, fail_message => "File $snapfile could not be found");
+        # Delete snapfile
+        script_run "$snapper undochange $snapbf..$snapaf $snapfile", 90;
+        script_run("test -f $snapfile || echo \"snap-ba-ok\" > /dev/$serialdev", 0);
+        wait_serial("snap-ba-ok", 30) || die "Snapper undochange $snapbf..$snapaf failed";
 
-    assert_screen 'snapper_undochange';
+        # Restore snapfile
+        script_run "$snapper undochange $snapaf..$snapbf $snapfile", 90;
+        assert_script_run("test -f $snapfile", timeout => 10, fail_message => "File $snapfile could not be found");
+
+        assert_screen 'snapper_undochange';
+        service_action('dbus', {type => ['socket', 'service'], action => ['unmask', 'start']}) if ($snapper =~ /dbus/);
+        assert_script_run "rm $snapfile";
+    }
+}
+
+sub post_fail_hook() {
+    my ($self) = @_;
+
+    upload_logs('/var/log/snapper.log');
 }
 
 sub test_flags() {
