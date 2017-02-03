46b22aec23c3e1d7a7ee855c5de770318551ed0d
Vladimir Nadvornik <nadvornik@suse.cz>
2017-02-03 15:13:45 +0100

    Boot of slepos image
    - do not wait for server on offline image
    - login after boot so we are in the same state as after normal
    installation

--- tests/slepos/boot_image.pm
+++ tests/slepos/boot_image.pm
@@ -20,6 +20,12 @@ use utils;
 sub run() {
     my $self = shift;
 
+    unless (get_var("SLEPOS") =~ /^terminal-offline/) {
+        mutex_lock("bs1_images_synced");
+        mutex_unlock("bs1_images_synced");
+        resume_vm();
+    }
+
     my $select_id = get_var("SLEPOS_SELECT_ID");
     if (defined $select_id) {
         assert_screen("slepos-select-id", 200);
@@ -39,6 +45,12 @@ sub run() {
     }
 
     assert_screen("slepos-image-login", 300);
+
+    type_string $username;
+    send_key "ret";
+    assert_screen "displaymanager-password-prompt";
+    type_string $password;
+    send_key "ret";
 }
 
 sub test_flags() {
