301347bd4cc6b827ddef6bd81d47368895340424
Oliver Kurz <okurz@suse.de>
2017-02-06 08:26:30 +0100

    Ensure vim has terminated before asserting clean console
    Verification run: http://lord.arch/tests/5618
    
    Related progress issue:
    https://progress.opensuse.org/issues/16494

--- tests/console/vim.pm
+++ tests/console/vim.pm
@@ -1,29 +1,25 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# G-Summary: refactor jeos tests a bit
-#    - simplyfy by using assert_script_run instead of
-#      validate_script_output
-#    - move some of the more generic ones to console/
-#    - make vim test actually run vim
-# G-Maintainer: Ludwig Nussel <ludwig.nussel@suse.de>
+# Summary: Test vim editor display including syntax highlighting
+# Maintainer: Oliver Kurz <okurz@suse.de>
 
 use base "consoletest";
 use strict;
 use testapi;
 
 sub run() {
-    assert_script_run("rpm -qi vim");
-    type_string("vim /etc/passwd\n");
-    assert_screen("vim-showing-passwd");
-    type_string(":q!\n");
+    assert_script_run 'rpm -qi vim';
+    type_string "vim /etc/passwd\n";
+    assert_screen 'vim-showing-passwd';
+    wait_screen_change { type_string ":q!\n" };
 }
 
 1;
