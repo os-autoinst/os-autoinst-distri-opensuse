d68b5030f4b933c2386cecf1dd824297950b8fd7
Oliver Kurz <okurz@suse.de>
2017-02-14 21:47:02 +0100

    Add test for openqa-review
    Verification run: http://lord.arch/tests/5647#step/openqa_review/5
    
    The verification run shows an expected problem because of underspecified
    requirements in the spec file of openqa-review therefore the test is working
    as expected.

---
+++ tests/console/openqa_review.pm
@@ -0,0 +1,23 @@
+# SUSE's openQA tests
+#
+# Copyright © 2017 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Test openqa-review can be started (with runtime dependencies)
+# Maintainer: Oliver Kurz <okurz@suse.de>
+
+use base "consoletest";
+use strict;
+use testapi;
+use utils;
+
+sub run() {
+    zypper_call('in python-openqa_review');
+    assert_script_run 'openqa-review --help';
+}
+
+1;
