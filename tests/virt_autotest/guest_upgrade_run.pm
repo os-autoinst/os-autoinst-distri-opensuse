3c2b9e73266639c74f180adaa5ef87b26b5c1e4e
alice <xlai@suse.com>
2017-02-27 10:24:50 +0800

    Virtualization: change assert pattern to tell test result for guest upgrade test

--- tests/virt_autotest/guest_upgrade_run.pm
+++ tests/virt_autotest/guest_upgrade_run.pm
@@ -34,7 +34,7 @@ sub run {
     my $self = shift;
     my $timeout = get_var("MAX_TEST_TIME", "36000") + 10;
     script_run("echo \"Debug info: max_test_time is $timeout\"");
-    $self->run_test($timeout, "Test run completed successfully", "no", "yes", "/var/log/qa/", "guest-upgrade-logs");
+    $self->run_test($timeout, "guest_upgrade_test ... ... PASSED", "no", "yes", "/var/log/qa/", "guest-upgrade-logs");
 }
 
 sub test_flags {
