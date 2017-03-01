9bf9ac0f40fbad2ecdc49424b24f83019557dfe9
mkravec <mkravec@suse.com>
2017-03-01 16:25:46 +0100

    Simplify mtab test

--- tests/console/mtab.pm
+++ tests/console/mtab.pm
@@ -1,15 +1,15 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# G-Summary: Rework the tests layout.
-# G-Maintainer: Alberto Planas <aplanas@suse.com>
+# Summary: Rework the tests layout.
+# Maintainer: Alberto Planas <aplanas@suse.com>
 
 use base "consoletest";
 use strict;
@@ -17,8 +17,7 @@ use testapi;
 
 sub run() {
     select_console 'user-console';
-    script_run('test -L /etc/mtab && echo OK || echo fail');
-    assert_screen "test-mtab-1", 3;
+    assert_script_run 'test -L /etc/mtab';
     script_run('cat /etc/mtab');
     save_screenshot;
 }
