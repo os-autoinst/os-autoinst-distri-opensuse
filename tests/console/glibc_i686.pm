a8892e908c7ad1bc840276dc15835883d5f9f082
Jozef Pupava <jpupava@suse.com>
2017-03-03 12:49:41 +0100

    Remove needle check in console glibc_i686 test

--- tests/console/glibc_i686.pm
+++ tests/console/glibc_i686.pm
@@ -1,30 +1,28 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# G-Summary: Rework the tests layout.
-# G-Maintainer: Alberto Planas <aplanas@suse.com>
+# Summary: Check /lib/libc.so.* content
+# Maintainer: Jozef Pupava <jpupava@suse.com>
 
 use base "consoletest";
-use testapi;
 use strict;
+use testapi;
+use utils 'zypper_call';
 
 # this part contains the steps to run this test
 sub run() {
     select_console 'root-console';
 
-    assert_script_run("zypper -n in -C libc.so.6", 500);
-
-    # select user console for our needles to match
-    select_console 'user-console';
-    script_run("/lib/libc.so.*", 0);
-    assert_screen 'test-glibc_i686-1';
+    zypper_call 'in -C libc.so.6';
+    script_run "/lib/libc.so.* | tee /dev/$serialdev", 0;
+    wait_serial("\QGNU C Library (GNU libc) stable release\E.*\n.*\n.*\n.*\n.*\n\QConfigured for i686-suse-linux.\E") || die '/lib/libc.so.* did not match';
 }
 
 1;
