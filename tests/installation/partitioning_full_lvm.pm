487fab3e33cdbfb7a8649ddc8165b4de5980594d
Jozef Pupava <jpupava@suse.com>
2017-03-10 13:29:41 +0100

    Export partition operations

--- tests/installation/partitioning_full_lvm.pm
+++ tests/installation/partitioning_full_lvm.pm
@@ -15,107 +15,26 @@ use strict;
 use warnings;
 use base 'y2logsstep';
 use testapi;
-
-my %role = qw(
-  OS alt-o
-  data alt-d
-  swap alt-s
-  raw alt-a
-);
-
-sub addpart {
-    my (%args) = @_;
-    assert_screen 'expert-partitioner';
-    send_key $cmd{addpart};
-    if (!get_var('UEFI')) {    # partitioning type does not appear when GPT disk used, GPT is default for UEFI
-        assert_screen 'partitioning-type';
-        send_key $cmd{next};
-    }
-    assert_screen 'partition-size';
-    if ($args{size}) {
-        for (1 .. 10) {
-            send_key 'backspace';
-        }
-        type_string $args{size} . 'mb';
-    }
-    send_key $cmd{next};
-    assert_screen 'partition-role';
-    send_key $role{$args{role}};
-    send_key $cmd{next};
-    assert_screen 'partition-format';
-    if ($args{format}) {
-        if ($args{format} eq 'donotformat') {
-            send_key $cmd{donotformat};
-            send_key 'alt-u';
-        }
-        else {
-            send_key 'alt-s';
-            send_key_until_needlematch "partition-selected-$args{format}-type", 'down';
-        }
-    }
-    if ($args{fsid}) {    # $args{fsid} will describe needle tag below
-        send_key 'alt-i';    # select File system ID
-        send_key_until_needlematch "partition-selected-$args{fsid}-type", 'down';
-    }
-    if ($args{mount}) {
-        send_key 'alt-m';
-        type_string "$args{mount}";
-    }
-    if ($args{encrypt}) {
-        send_key $cmd{encrypt};
-        assert_screen 'partition-lvm-encrypt';
-        send_key $cmd{next};
-        assert_screen 'partition-lvm-password-prompt';
-        send_key 'alt-e';    # select password field
-        type_password;
-        send_key 'tab';
-        type_password;
-    }
-    send_key $cmd{finish};
-}
-
-sub addlv {
-    my (%args) = @_;
-    send_key $cmd{addpart};
-    send_key 'down';
-    send_key 'down';
-    wait_still_screen 2;
-    save_screenshot;
-    send_key 'ret';    # create logical volume
-    assert_screen 'partition-lv-type';
-    type_string $args{name};
-    send_key $cmd{next};
-    assert_screen 'partition-lv-size';
-    if ($args{size}) {    # use default max size if not defined
-        send_key 'alt-c';    # custom size
-        type_string $args{size} . 'mb';
-    }
-    send_key $cmd{next};
-    assert_screen 'partition-role';
-    send_key $role{$args{role}};    # swap role
-    send_key $cmd{next};
-    assert_screen 'partition-format';
-    send_key $cmd{finish};
-}
+use partition_setup;
 
 sub run() {
     send_key $cmd{expertpartitioner};
     assert_screen 'expert-partitioner';
     wait_still_screen;
     for (1 .. 4) {
-        send_key 'right';           # select vda hard disk
+        send_key 'right';    # select vda hard disk
     }
-    send_key 'alt-x';               # expert menu
+    send_key 'alt-x';        # expert menu
     send_key 'down';
     wait_still_screen 2;
     save_screenshot;
-    send_key 'ret';                 # create new partition table
-    if (!get_var('UEFI')) {         # only GPT partition table
+    send_key 'ret';          # create new partition table
+    if (!get_var('UEFI')) {  # only GPT partition table
         assert_screen 'partition-table-MSDOS-selected';
-        send_key 'alt-o';           # OK
+        send_key 'alt-o';    # OK
     }
     assert_screen 'partition-create-new-table';
-    send_key 'alt-y';               # yes
+    send_key 'alt-y';        # yes
     if (check_var('ARCH', 's390x')) {    # s390x need /boot/zipl on ext partition
         addpart(role => 'OS', size => 500, format => 'ext2', mount => '/boot');
     }
