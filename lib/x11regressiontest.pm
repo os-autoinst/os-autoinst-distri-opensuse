os-autoinst/os-autoinst-distri-opensuse

commit 96e1b0287858f3c8e07eeedd07c376be262a60c3

More DRY in x11regression tests

lib/x11regressiontest.pm
@@ -296,6 +296,26 @@ sub start_evolution {
     send_key $self->{next};
 }
 
+sub evolution_add_self_signed_ca {
+    my ($self, $account) = @_;
+    # add self-signed CA with internal account
+    if ($account =~ m/internal/) {
+        assert_and_click "evolution_wizard-receiving-checkauthtype";
+        assert_screen "evolution_mail_meeting_trust_ca";
+        send_key "alt-a";
+        wait_screen_change {
+            send_key $self->{next};
+            send_key "ret";
+        }
+    }
+    else {
+        send_key $self->{next};
+    }
+    if (sle_version_at_least('12-SP2')) {
+        send_key "ret";    #only need in SP2 or later
+    }
+}
+
 sub setup_mail_account {
     my ($self, $proto, $account) = @_;
 
@@ -351,22 +371,7 @@ sub setup_mail_account {
     wait_screen_change {
         send_key "ret";
     };
-    # add self-signed CA with internal account
-    if ($account =~ m/internal/) {
-        assert_and_click "evolution_wizard-receiving-checkauthtype";
-        assert_screen "evolution_mail_meeting_trust_ca";
-        send_key "alt-a";
-        wait_screen_change {
-            send_key $self->{next};
-            send_key "ret";
-        }
-    }
-    else {
-        send_key $self->{next};
-    }
-    if (sle_version_at_least('12-SP2')) {
-        send_key "ret";    #only need in SP2 or later
-    }
+    $self->evolution_add_self_signed_ca($account);
     save_screenshot;
     assert_screen "evolution_wizard-receiving-opts";
     send_key $self->{next};
@@ -419,22 +424,7 @@ sub setup_mail_account {
     };
     sleep 1;
     type_string "$mail_user";
-    # add self-signed CA with internal account
-    if ($account =~ m/internal/) {
-        assert_and_click "evolution_wizard-sending-checkauthtype";
-        assert_screen "evolution_mail_meeting_trust_ca";
-        send_key "alt-a";
-        wait_screen_change {
-            send_key $self->{next};
-            send_key "ret";
-        }
-    }
-    else {
-        send_key $self->{next};
-    }
-    if (sle_version_at_least('12-SP2')) {
-        send_key "ret";    #only in sp2, send ret to next page
-    }
+    $self->evolution_add_self_signed_ca($account);
     assert_screen "evolution_wizard-account-summary";
     send_key $self->{next};
     if (sle_version_at_least('12-SP2')) {
@@ -634,6 +624,64 @@ sub evolution_send_message {
     }
 
     return $mail_subject;
+}
+
+sub pidgin_remove_account {
+    send_key "ctrl-a";
+    sleep 2;
+    send_key "right";
+    sleep 2;
+    send_key "ret";
+    sleep 2;
+    send_key "alt-d";
+    sleep 2;
+    send_key "alt-d";
+}
+
+sub tomboy_logout_and_login {
+    wait_screen_change { send_key 'alt-f4' };
+
+    # logout
+    send_key "alt-f2";
+    sleep 1;
+    type_string "gnome-session-quit --logout --force\n";
+    sleep 20;
+    wait_idle;
+
+    # login
+    send_key "ret";
+    sleep 2;
+    wait_still_screen;
+    type_password();
+    sleep 2;
+    send_key "ret";
+    sleep 20;
+    wait_idle;
+
+    # open start note again and take screenshot
+    x11_start_program("tomboy note");
+}
+
+sub gnote_start_with_new_note {
+    x11_start_program("gnote");
+    assert_screen 'gnote-first-launched', 10;
+    send_key "ctrl-n";
+    assert_screen 'gnote-new-note', 5;
+}
+
+# clean: remove the created new note
+sub gnote_cleanup {
+    send_key "ctrl-tab";    #jump to toolbar
+    sleep 2;
+    send_key "ret";         #back to all notes interface
+    send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
+    send_key "delete";
+    sleep 2;
+    send_key "tab";
+    sleep 2;
+    send_key "ret";
+    sleep 2;
+    send_key "ctrl-w";
 }
 
 1;

tests/x11regressions/gnote/gnote_link_note.pm
@@ -18,10 +18,8 @@ use testapi;
 
 
 sub run() {
-    x11_start_program("gnote");
-    assert_screen 'gnote-first-launched', 10;
-    send_key "ctrl-n";
-    assert_screen 'gnote-new-note', 5;
+    my ($self) = @_;
+    $self->gnote_start_with_new_note;
     type_string "Start Here\n";
     assert_screen 'gnote-new-note-link', 5;
     send_key "up";
@@ -42,18 +40,7 @@ sub run() {
     send_key "ctrl-w";      #close the note "Start Here"
     sleep 2;
 
-    #clean: remove the created new note
-    send_key "ctrl-tab";    #jump to toolbar
-    sleep 2;
-    send_key "ret";         #back to all notes interface
-    send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
-    send_key "delete";
-    sleep 2;
-    send_key "tab";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "ctrl-w";
+    $self->cleanup_gnote;
 }
 
 1;

tests/x11regressions/gnote/gnote_edit_format.pm
@@ -18,10 +18,8 @@ use testapi;
 
 
 sub run() {
-    x11_start_program("gnote");
-    assert_screen "gnote-first-launched", 10;
-    send_key "ctrl-n";
-    assert_screen 'gnote-new-note', 5;
+    my ($self) = @_;
+    $self->gnote_start_with_new_note;
     type_string "opensuse\n";
     send_key "ctrl-h";    #hightlight on
     type_string "opensuse\n";
@@ -37,18 +35,7 @@ sub run() {
     send_key "ctrl-i";    #italic off
     assert_screen 'gnote-edit-format', 5;
 
-    #clean: remove the created new note
-    send_key "ctrl-tab";    #jump to toolbar
-    sleep 2;
-    send_key "ret";         #back to all notes interface
-    send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
-    send_key "delete";
-    sleep 2;
-    send_key "tab";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "ctrl-w";
+    $self->cleanup_gnote;
 }
 
 1;

tests/x11regressions/gnote/gnote_undo_redo.pm
@@ -17,12 +17,7 @@ use strict;
 use testapi;
 
 
-sub run() {
-    x11_start_program("gnote");
-    assert_screen "gnote-first-launched", 10;
-    send_key "ctrl-n";
-    assert_screen 'gnote-new-note', 5;
-    type_string "opensuse\nOPENSUSE\n";
+sub undo_redo_once {
     assert_screen 'gnote-new-note-1', 5;
     send_key "ctrl-z";    #undo
     assert_screen 'gnote-new-note', 5;
@@ -30,25 +25,29 @@ sub run() {
     sleep 2;
     send_key "left";            #unselect text
     assert_screen 'gnote-new-note-1', 5;
+}
+
+sub run() {
+    my ($self) = @_;
+    x11_start_program("gnote");
+    assert_screen "gnote-first-launched", 10;
+    send_key "ctrl-n";
+    assert_screen 'gnote-new-note', 5;
+    type_string "opensuse\nOPENSUSE\n";
+    $self->undo_redo_once;
 
     #assure undo and redo take effect after save note and re-enter note
-    send_key "ctrl-tab";        #jump to toolbar
+    send_key "ctrl-tab";    #jump to toolbar
     sleep 2;
-    send_key "ret";             #back to all notes interface
+    send_key "ret";         #back to all notes interface
     send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
     send_key "ret";
-    assert_screen 'gnote-new-note-1', 5;
-    send_key "ctrl-z";          #undo
-    assert_screen 'gnote-new-note', 5;
-    send_key "ctrl-shift-z";    #redo
-    sleep 2;
-    send_key "left";            #unselect text
-    assert_screen 'gnote-new-note-1', 5;
+    $self->undo_redo_once;
 
     #clean: remove the created new note
-    send_key "ctrl-tab";        #jump to toolbar
+    send_key "ctrl-tab";    #jump to toolbar
     sleep 2;
-    send_key "ret";             #back to all notes interface
+    send_key "ret";         #back to all notes interface
     send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
     send_key "delete";
     sleep 2;

tests/x11regressions/pidgin/pidgin_IRC.pm
@@ -18,6 +18,7 @@ use testapi;
 
 
 sub run() {
+    my ($self) = @_;
     my $CHANNELNAME = "susetesting";
     x11_start_program("pidgin");
 
@@ -75,15 +76,7 @@ sub run() {
     sleep 2;
 
     # Cleaning
-    send_key "ctrl-a";
-    sleep 2;
-    send_key "right";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "alt-d";
-    sleep 2;
-    send_key "alt-d";
+    $self->pidgin_remove_account;
 
     # Should not have any account and show welcome window
     assert_screen 'pidgin-welcome';

tests/x11regressions/pidgin/pidgin_aim.pm
@@ -17,6 +17,7 @@ use strict;
 use testapi;
 
 sub run() {
+    my ($self) = @_;
     my $USERNAME  = "nooops_test3";
     my $USERNAME1 = "nooops_test4";
     my $DOMAIN    = "aim";
@@ -94,28 +95,10 @@ sub run() {
     send_key "ctrl-w";
     sleep 2;
 
-    # Remove one account
-    send_key "ctrl-a";
-    sleep 2;
-    send_key "right";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "alt-d";
-    sleep 2;
-    send_key "alt-d";
-    sleep 2;
-
-    # Remove the other account
-    send_key "ctrl-a";
-    sleep 2;
-    send_key "right";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "alt-d";
-    sleep 2;
-    send_key "alt-d";
+    # Remove both accounts
+    for (1 .. 2) {
+        $self->pidgin_remove_account;
+    }
 
     # Should not have any account
     assert_screen 'pidgin-welcome';

tests/x11regressions/tomboy/tomboy_Hotkeys.pm
@@ -18,10 +18,10 @@ use testapi;
 
 
 sub run() {
+    my ($self) = @_;
     # open Hotkeys sheet
     x11_start_program("tomboy note");
-    send_key "alt-e";
-    sleep 1;
+    wait_screen_change { send_key 'alt-e' };
     send_key "p";
     sleep 1;
     send_key "right";
@@ -34,27 +34,9 @@ sub run() {
     type_string "<Alt>F10\t<Alt>F9";
     assert_screen 'test-tomboy_Hotkeys-1', 3;
     sleep 2;
-    send_key "esc";
-    wait_idle;
-    send_key "alt-f4";
+    wait_screen_change { send_key 'esc' };
 
-    # logout
-    send_key "alt-f2";
-    sleep 1;
-    type_string "gnome-session-quit --logout --force\n";
-    sleep 20;
-    wait_idle;
-
-    # login and open tomboy again
-    send_key "ret";
-    sleep 2;
-    wait_still_screen;
-    type_password();
-    sleep 2;
-    send_key "ret";
-    sleep 20;
-    wait_idle;
-    x11_start_program("tomboy note");
+    $self->tomboy_logout_and_login;
 
     # test hotkeys
     send_key "alt-f12";
@@ -118,7 +100,6 @@ sub run() {
     send_key "esc";
     sleep 2;
     send_key "alt-f4";
-    sleep 2;
 }
 
 1;

tests/x11regressions/tomboy/tomboy_Open.pm
@@ -18,6 +18,7 @@ use testapi;
 
 
 sub run() {
+    my ($self) = @_;
     # open start note and take screenshot
     x11_start_program("tomboy note");
     send_key "alt-f11";
@@ -41,28 +42,8 @@ sub run() {
     sleep 2;
     send_key "ctrl-w";
     sleep 2;
-    send_key "alt-f4";
-    sleep 2;
+    $self->tomboy_logout_and_login;
 
-    # logout
-    send_key "alt-f2";
-    sleep 1;
-    type_string "gnome-session-quit --logout --force\n";
-    sleep 20;
-    wait_idle;
-
-    # login
-    send_key "ret";
-    sleep 2;
-    wait_still_screen;
-    type_password();
-    sleep 2;
-    send_key "ret";
-    sleep 20;
-    wait_idle;
-
-    # open start note again and take screenshot
-    x11_start_program("tomboy note");
     send_key "alt-f11";
     sleep 2;
     send_key "up";
@@ -71,8 +52,6 @@ sub run() {
     send_key "ctrl-w";
     sleep 2;
     send_key "alt-f4";
-    sleep 2;
-    wait_idle;
 }
 
 1;

commit 01987280f9e99641c09dc392808c9013ad4a52f2

gnote: Fix regression introduced by 96e1b02
Verification run: http://lord.arch/tests/6107

lib/x11regressiontest.pm
@@ -662,26 +662,22 @@ sub tomboy_logout_and_login {
     x11_start_program("tomboy note");
 }
 
+# remove the created new note
+sub cleanup_gnote {
+    wait_screen_change { send_key 'ctrl-tab' };    #jump to toolbar
+    send_key 'ret';                                #back to all notes interface
+    send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
+    wait_screen_change { send_key 'delete' };
+    wait_screen_change { send_key 'tab' };
+    wait_screen_change { send_key 'ret' };
+    send_key 'ctrl-w';
+}
+
 sub gnote_start_with_new_note {
     x11_start_program("gnote");
     assert_screen 'gnote-first-launched', 10;
     send_key "ctrl-n";
     assert_screen 'gnote-new-note', 5;
-}
-
-# clean: remove the created new note
-sub gnote_cleanup {
-    send_key "ctrl-tab";    #jump to toolbar
-    sleep 2;
-    send_key "ret";         #back to all notes interface
-    send_key_until_needlematch 'gnote-new-note-matched', 'down', 6;
-    send_key "delete";
-    sleep 2;
-    send_key "tab";
-    sleep 2;
-    send_key "ret";
-    sleep 2;
-    send_key "ctrl-w";
 }
 
 1;

tests/x11regressions/gnote/gnote_link_note.pm
@@ -12,7 +12,7 @@
 # Maintainer: Xudong Zhang <xdzhang@suse.com>
 # Tags: tc#1436158
 
-use base "x11regressiontest";
+use base 'x11regressiontest';
 use strict;
 use testapi;
 
@@ -21,25 +21,24 @@ sub run() {
     my ($self) = @_;
     $self->gnote_start_with_new_note;
     type_string "Start Here\n";
-    assert_screen 'gnote-new-note-link', 5;
-    send_key "up";
-    sleep 2;
-    send_key "ctrl-ret";    #switch to link
-    assert_screen 'gnote-note-start-here', 5;
+    assert_screen 'gnote-new-note-link';
+    wait_screen_change { send_key 'up' };
+    send_key 'ctrl-ret';    #switch to link
+    assert_screen 'gnote-note-start-here';
 
-    send_key "ctrl-tab";    #jump to toolbar
-    for (1 .. 6) { send_key "right" }
-    sleep 2;
-    send_key "ret";
-    send_key "down";
-    if (get_var("SP2ORLATER")) {
-        send_key "ret";
+    wait_screen_change { send_key 'ctrl-tab' };    #jump to toolbar
+    wait_screen_change {
+        for (1 .. 6) { send_key 'right' }
+    };
+    wait_screen_change { send_key 'ret' };
+    wait_screen_change { send_key 'down' };
+    if (get_var('SP2ORLATER')) {
+        wait_screen_change { send_key 'ret' };
     }
-    assert_screen 'gnote-what-link-here', 5;
-    send_key "esc";
-    send_key "ctrl-w";      #close the note "Start Here"
-    sleep 2;
-
+    assert_screen 'gnote-what-link-here';
+    wait_screen_change { send_key 'esc' };
+    #close the note "Start Here"
+    wait_screen_change { send_key 'ctrl-w' };
     $self->cleanup_gnote;
 }
 