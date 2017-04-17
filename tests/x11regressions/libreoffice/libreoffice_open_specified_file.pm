42455a121fddb08c85ea1d45c00fd1f60e949271
Chingkai <qkzhu@suse.com>
2017-04-17 11:22:06 +0800

    Update two libreoffice cases to fix poo#18410
    libreoffice_open_specified_file and libreoffice_double_click_file are
    unreliable. It seems that the VM's performance doesn't support opening
    so many test files at the same time.
    
    - libreoffice_double_click_file
      Open a test file then close it immediately.
    - libreoffice_open_specified_file
      Close every 3 files to reduce the VM's burden and test open multiple files.
    
      see also: poo18410

--- tests/x11regressions/libreoffice/libreoffice_open_specified_file.pm
+++ tests/x11regressions/libreoffice/libreoffice_open_specified_file.pm
@@ -26,7 +26,8 @@ sub run() {
     assert_screen("welcome-to-libreoffice");
     $self->check_libreoffice_dialogs();
 
-    # open below qw(doc docx fodg fodp fods fodt odf odg odp ods odt pdf pptx xlsx) to check whether can be work
+    # open test files of different formats
+    my $i = 0;
     for my $tag (qw(doc docx fodg fodp fods fodt odf odg odp ods odt pdf pptx xlsx)) {
         send_key "ctrl-o";
         wait_still_screen 3;
@@ -35,8 +36,10 @@ sub run() {
         type_string "/home/$username/Documents/ooo-test-doc-types/test.$tag\n";
         wait_still_screen 3;
         assert_screen("libreoffice-test-$tag", 90);
+        # Close every 3 files to reduce the VM's burden
+        if ($i % 3 == 0) { send_key_until_needlematch('libreoffice-test-doc', 'alt-f4', 3, 5); }
+        $i++;
     }
-    send_key "ctrl-q";
     if (!check_screen("generic-desktop")) {
         send_key "ctrl-q";
     }
