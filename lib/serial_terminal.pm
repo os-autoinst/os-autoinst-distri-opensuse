0a6bc4c4b1327a514bbead0d628ebeacca610e4e
Richard Palethorpe <rpalethorpe@suse.com>
2017-05-04 14:46:38 +0200

    serial_terminal: Export serial_term_prompt variable

--- lib/serial_terminal.pm
+++ lib/serial_terminal.pm
@@ -17,8 +17,22 @@ use Exporter;
 use bmwqemu ();
 
 BEGIN {
-    our @EXPORT = qw(login);
+    our @EXPORT = qw($serial_term_prompt login);
 }
+
+=head2 serial_term_prompt
+
+   wait_serial($serial_term_prompt);
+
+A simple undecorated prompt for serial terminals. ANSI escape characters only
+serve to create log noise in most tests which use the serial terminal, so
+don't use them here. Also avoid using characters which have special meaning in
+a regex. Note that our common prompt character '#' denotes a comment in a
+regex with '/z' on the end, but if you are using /z you will need to wrap the
+prompt in \Q and \E anyway otherwise the whitespace will be ignored.
+
+=cut
+our $serial_term_prompt = '# ';
 
 =head2 login
 
@@ -44,9 +58,8 @@ sub login {
     type_password;
     type_string("\n");
     wait_serial(qr/$escseq* \w+:~\s\# $escseq* \s*$/x);
-    # TODO: use distribution::set_standard_prompt
-    type_string(qq/PS1="# "\n/);
-    wait_serial(qr/PS1="# "\s+# $/);
+    type_string(qq/PS1="$serial_term_prompt"\n/);
+    wait_serial(qr/PS1="$serial_term_prompt"\s+# $/);
     # TODO: Send 'tput rmam' instead/also
     assert_script_run('stty cols 2048');
     assert_script_run('echo Logged into $(tty)', $bmwqemu::default_timeout, result_title => 'vconsole_login');
