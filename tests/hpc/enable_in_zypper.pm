5caef8a1e85f1f4d5498d4c0bdf4b2b22d265367
soulofdestiny <mgriessmeier@suse.com>
2017-05-05 15:00:05 +0200

    hpc: Add test for enable HPC through yast
    This test is supposed to enable hpc devel repo through yast on a fully patched image

---
+++ tests/hpc/enable_in_zypper.pm
@@ -0,0 +1,41 @@
+# SUSE's openQA tests
+#
+# Copyright © 2017 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Enable HPC repo through yast
+# Maintainer: mgriessmeier <mgriessmeier@suse.com>
+
+use base "opensusebasetest";
+use strict;
+use warnings;
+use testapi;
+use utils;
+use lockapi;
+
+sub run() {
+
+    select_console 'root-console';
+
+    # disable packagekitd
+    pkcon_quit();
+
+    my $repo     = get_required_var('HPC_REPO');
+    my $reponame = get_required_var('HPC_REPONAME');
+    zypper_call("ar -f $repo");
+    assert_script_run "zypper lr | grep $reponame  | tee /dev/$serialdev";
+
+    zypper_call("--gpg-auto-import-keys ref");
+    zypper_call 'up';
+}
+
+sub test_flags() {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
+# vim: set sw=4 et:
