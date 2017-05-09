751e2ede08625faa70fd654883a621d18968311b
Oliver Kurz <okurz@suse.de>
2017-05-09 10:32:51 +0200

    wait_boot: Add proper method description comment

--- lib/opensusebasetest.pm
+++ lib/opensusebasetest.pm
@@ -238,9 +238,24 @@ sub handle_uefi_boot_disk_workaround {
     wait_screen_change { send_key 'ret' };
 }
 
-# makes sure bootloader appears and then boots to desktop resp text
-# mode. Handles unlocking encrypted disk if needed.
-# arguments: bootloader_time => seconds # now long to wait for bootloader to appear
+
+=head2 wait_boot
+
+  wait_boot([bootloader_time => $bootloader_time] [, textmode => $textmode] [,ready_time => $ready_time] [,in_grub => $in_grub]);
+
+Makes sure the bootloader appears and then boots to desktop or text mode
+correspondingly. Returns successfully when the system is ready on a login
+prompt or logged in desktop. Set C<$textmode> to 1 when the text mode login
+prompt should be expected rather than a desktop or display manager.
+C<wait_boot> also handles unlocking encrypted disks if needed as well as
+various exceptions during the boot process. Also, before the bootloader menu
+or login prompt various architecture or machine specific handlings are in
+place. The time waiting for the bootloader can be configured with
+C<$bootloader_time> in seconds as well as the time waiting for the system to
+be fully booted with C<$ready_time> in seconds. Set C<$in_grub> to 1 when the
+SUT is already expected to be within the grub menu. C<wait_boot> continues
+from there.
+=cut
 sub wait_boot {
     my ($self, %args) = @_;
     my $bootloader_time = $args{bootloader_time} // 100;
