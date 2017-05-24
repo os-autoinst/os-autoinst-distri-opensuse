os-autoinst/os-autoinst-distri-opensuse

commit 6c95f04aa41a91686a4602793da61b01a706b61b

Also set unpartitioned btrfs experiment disk in other modules
Followup to 3387aa0b.

Verification run: http://lord.arch/t6310

Related progress issue: https://progress.opensuse.org/issues/19056

lib/btrfs_test.pm
@@ -1,0 +1,17 @@
+package btrfs_test;
+use base 'consoletest';
+
+use strict;
+use testapi;
+
+=head2 unpartitioned_disk_in_bash
+
+Choose the disk without a partition table for btrfs experiments.
+Defines the variable C<$disk> in a bash session.
+=cut
+sub set_unpartitioned_disk_in_bash {
+    assert_script_run 'disk=$(parted --machine -l |& sed -n \'s@^\(/dev/vd[ab]\):.*unknown.*$@\1@p\')';
+}
+
+1;
+# vim: set sw=4 et:

tests/console/btrfs_qgroups.pm
@@ -12,7 +12,7 @@
 #	 putting data into them and then running btrfsck on the hard disk
 # Maintainer: mkravec <mkravec@suse.com>
 
-use base "consoletest";
+use base 'btrfs_test';
 use strict;
 use testapi;
 
@@ -20,12 +20,12 @@ my $dest = "/mnt/qg";
 
 # poo#11446
 sub run() {
+    my ($self) = @_;
     select_console 'root-console';
 
     # Set up
     assert_script_run "mkdir $dest";
-    # choose the disk without a partition table for btrfs experiments
-    assert_script_run 'disk=$(parted --machine -l |& sed -n \'s@^\(/dev/vd[ab]\):.*unknown.*$@\1@p\')';
+    $self->set_unpartitioned_disk_in_bash;
     assert_script_run "mkfs.btrfs -f \$disk && mount \$disk $dest && cd $dest";
     assert_script_run "btrfs quota enable .";
 

tests/console/btrfs_send_receive.pm
@@ -10,7 +10,7 @@
 # Summary: Btrfs send & receive snapshots
 # Maintainer: mkravec <mkravec@suse.com>
 
-use base "consoletest";
+use base 'btrfs_test';
 use strict;
 use testapi;
 use utils;
@@ -44,13 +44,15 @@ sub compare_data {
 
 # poo#11792
 sub run() {
+    my ($self) = @_;
     select_console 'root-console';
 
     # Set up
     assert_script_run "mkdir $src";
     assert_script_run "btrfs subvolume create $src/sv";
     assert_script_run "mkdir $dest";
-    assert_script_run "mkfs.btrfs -f /dev/vdb && mount /dev/vdb $dest";
+    $self->set_unpartitioned_disk_in_bash;
+    assert_script_run "mkfs.btrfs -f \$disk && mount \$disk $dest";
     #make sure that pax is installed
     zypper_call('in -C pax');
 
@@ -67,7 +69,7 @@ sub run() {
         assert_script_run "btrfs send -p $src/snap" . ($i - 1) . " $src/snap$i | btrfs receive $dest";
         compare_data $i;
     }
-    assert_script_run("umount -fl /dev/vdb");
+    assert_script_run("umount -fl \$disk");
 }
 
 1;

tests/console/snapper_thin_lvm.pm
@@ -11,12 +11,13 @@
 # poo#15944 FATE#321049
 # Maintainer: Michal Nowak <mnowak@suse.com>
 
-use base "consoletest";
+use base 'btrfs_test';
 use testapi;
 use strict;
 use utils 'service_action';
 
 sub run() {
+    my ($self) = @_;
     select_console 'root-console';
 
     my @snapper_runs = 'snapper';
@@ -24,15 +25,16 @@ sub run() {
     my $mnt_thin          = '/mnt/thin';
     my $mnt_thin_snapshot = $mnt_thin . '-snapshot';
 
+    $self->set_unpartitioned_disk_in_bash;
     foreach my $snapper (@snapper_runs) {
         service_action('dbus', {type => ['socket', 'service'], action => ['stop', 'mask']}) if ($snapper =~ /dbus/);
 
-        # Create partition on /dev/vbd
-        assert_script_run 'echo -e "g\nn\n\n\n\nt\n8e\np\nw" | fdisk /dev/vdb';
+        # Create partition on unpartitioned
+        assert_script_run 'echo -e "g\nn\n\n\n\nt\n8e\np\nw" | fdisk $disk';
         assert_script_run 'lsblk';
 
         # Create a volume group named 'test'
-        assert_script_run 'vgcreate test /dev/vdb1';
+        assert_script_run 'vgcreate test ${disk}1';
         # Follow guide at https://lizards.opensuse.org/2012/07/25/snapper-lvm/
         assert_script_run 'lvcreate --thin test/pool --size 3G';
         assert_script_run 'lvcreate --thin test/pool --virtualsize 5G --name thin';
@@ -61,7 +63,7 @@ sub run() {
         assert_script_run "umount $mnt_thin";
         assert_script_run "rm -rf $mnt_thin_snapshot $mnt_thin";
         assert_script_run 'vgremove -f test';
-        assert_script_run 'echo -e "g\np\nw" | fdisk /dev/vdb';
+        assert_script_run 'echo -e "g\np\nw" | fdisk $disk';
         assert_script_run 'lsblk';
 
         service_action('dbus', {type => ['socket', 'service'], action => ['unmask', 'start']}) if ($snapper =~ /dbus/);

tests/slenkins/slenkins_control.pm
@@ -111,7 +111,7 @@ sub run {
         my $disk_size = $settings{$p}->{HDDSIZEGB} || 10;
         my $num_disks = $settings{$p}->{NUMDISKS}  || 1;
 
-        # In SLEnkins DISK_NAME_NODE_DISK0 is used for first additional disk /dev/vdb and so on
+        # In SLEnkins DISK_NAME_NODE_DISK0 is used for first additional disk, e.g. /dev/vdb and so on
         # Drive /dev/vda is always in use as bootdrive and has no dedicated variable
         for (my $d = 0; $d < ($num_disks - 1); $d++) {
             $conf .= "export DISK_NAME_" . uc($node) . "_DISK$d='/dev/$disk_name" . chr(ord('b') + $d) . "'\n";

commit f473a56d8a3c2e60cb7a8ab3f5758de52f2ba580

Fix snapper_thin_lvm regression after 6c95f04a (poo#19322)
Wipe the filesystem after test modules accessing the unpartitioned disk to
make sure subsequent modules, in this case snapper_thin_lvm, can also find the
unpartitioned disk again reliably. This also prevents a potentially misleading
warning as we had in before that snapper_thin_lvm is forcefully overwriting an
existing filesystem on the disk reserved for filesystem related experiments.

Verification run: http://lord.arch/tests/6327

Related progress issue: https://progress.opensuse.org/issues/19322

lib/btrfs_test.pm
@@ -11,6 +11,11 @@ Defines the variable C<$disk> in a bash session.
 =cut
 sub set_unpartitioned_disk_in_bash {
     assert_script_run 'disk=$(parted --machine -l |& sed -n \'s@^\(/dev/vd[ab]\):.*unknown.*$@\1@p\')';
+    assert_script_run 'echo $disk';
+}
+
+sub cleanup_partition_table {
+    assert_script_run 'wipefs --all $disk';
 }
 
 1;

tests/console/btrfs_qgroups.pm
@@ -87,6 +87,7 @@ sub run() {
 
     assert_script_run "cd; umount $dest";
     assert_script_run 'btrfsck $disk';
+    $self->cleanup_partition_table;
 }
 
 1;

tests/console/btrfs_send_receive.pm
@@ -69,7 +69,8 @@ sub run() {
         assert_script_run "btrfs send -p $src/snap" . ($i - 1) . " $src/snap$i | btrfs receive $dest";
         compare_data $i;
     }
-    assert_script_run("umount -fl \$disk");
+    assert_script_run 'umount -fl $disk';
+    $self->cleanup_partition_table;
 }
 
 1;