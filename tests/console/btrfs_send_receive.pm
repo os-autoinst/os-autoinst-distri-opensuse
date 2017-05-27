77532556dc1e8986b0a0c3edad2ad6f8a7e2aa85
Oliver Kurz <okurz@suse.de>
2017-05-27 13:08:10 +0200

    btrfs_test: Try to avoid a busy filesystem when trying to wipe (#2964)
    Using '--force' on unmount would only make sense for NFS, '--lazy' would
    cleanup references after unmount but keep the device busy so get rid of these
    options, be explicit about the cleanup and wait longer if necessary.
    
    Related progress issue: https://progress.opensuse.org/issues/19370

--- tests/console/btrfs_send_receive.pm
+++ tests/console/btrfs_send_receive.pm
@@ -69,7 +69,7 @@ sub run() {
         assert_script_run "btrfs send -p $src/snap" . ($i - 1) . " $src/snap$i | btrfs receive $dest";
         compare_data $i;
     }
-    assert_script_run 'umount -fl $disk';
+    assert_script_run 'umount $disk', 600;
     $self->cleanup_partition_table;
 }
 
