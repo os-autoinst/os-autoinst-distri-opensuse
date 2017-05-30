8dc1981fabea17e522c6a4d36d21eab6df62da3b
Oliver Kurz <okurz@suse.de>
2017-05-30 22:31:16 +0200

    yast2_lang: Avoid unnecessary tab characters

--- tests/yast2_gui/yast2_lang.pm
+++ tests/yast2_gui/yast2_lang.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -25,20 +25,20 @@ sub run() {
     $self->launch_yast2_module_x11($module);
     assert_screen "yast2-$module-ui", 60;
 
-    #	check language details and change detailed locale setting
+    # check language details and change detailed locale setting
     assert_and_click 'yast2-lang_details';
     assert_and_click 'yast2-lang_detailed-locale-setting';
     assert_and_click 'yast2-lang_detailed-locale-setting_en_GB';
     assert_screen 'yast2-lang_detailed-locale-setting_changed';
     send_key 'alt-o';
 
-    #	change adapt time zone to German
+    # change adapt time zone to German
     assert_and_click 'yast2-lang_adapt-timezone';
     assert_and_click 'yast2-lang_secondary-language';
     assert_screen 'yast2-lang_settings_done';
 
-    #	Now it will install required language packages and exit
-    send_key "alt-o";
+    # Now it will install required language packages and exit
+    send_key 'alt-o';
 }
 
 1;
