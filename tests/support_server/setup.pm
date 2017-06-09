8437f994204ca8e0fc679048be5a2d8ebbf8ec30
Tomas Hehejik <thehejik@suse.com>
2017-06-09 12:34:10 +0200

    GRE tunnel support by MTU change in support server (#3054)

--- tests/support_server/setup.pm
+++ tests/support_server/setup.pm
@@ -85,6 +85,8 @@ sub setup_networks {
         $setup_script .= "IPADDR=$server_ip\n";
         $setup_script .= "NETMASK=$net_conf->{$network}->{subnet_mask}\n";
         $setup_script .= "STARTMODE='auto'\n";
+        # TCP cannot pass GRE tunnel with default MTU value 1500 in combination of DF flag set in L3 for ovs bridge
+        $setup_script .= "MTU='1458'\n";
         $setup_script .= "EOT\n";
     }
     $setup_script .= "rcnetwork restart\n";
@@ -160,6 +162,8 @@ sub setup_dhcp_server {
         $setup_script .= "  range  " . ip_in_subnet($net_conf->{$network}, 15) . "  " . ip_in_subnet($net_conf->{$network}, 100) . ";\n";
         $setup_script .= "  default-lease-time 14400;\n";
         $setup_script .= "  max-lease-time 172800;\n";
+        # dhcp clients have to use MTU 1458 to be able pass GRE Tunnel
+        $setup_script .= "  option interface-mtu 1458;\n";
         $setup_script .= "  option domain-name \"openqa.test\";\n";
         if ($dns) {
             $setup_script .= "  option domain-name-servers  $server_ip,  $server_ip;\n";
