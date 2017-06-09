0b9a79960eefb3ab8c43fafd109b796780098a57
Michal Nowak <mnowak@suse.com>
2017-06-09 12:54:57 +0200

    Shorten wait_still_screen and remove redundant code
    Shortening wait_still_screen from 7 -> 3 seconds requested in
    bsc#981299#c41.
    
    Also remove code duplication.

--- lib/utils.pm
+++ lib/utils.pm
@@ -490,9 +490,9 @@ sub reboot_x11 {
 
         if (get_var("SHUTDOWN_NEEDS_AUTH")) {
             assert_screen 'reboot-auth';
-            wait_still_screen;
+            wait_still_screen(3);                                               # 981299#c41
             type_string $testapi::password, max_interval => 5;
-            wait_still_screen;
+            wait_still_screen(3);                                               # 981299#c41
             wait_screen_change {
                 # Extra assert_and_click (with right click) to check the correct number of characters is typed and open up the 'show text' option
                 assert_and_click 'reboot-auth-typed', 'right';
@@ -507,26 +507,7 @@ sub reboot_x11 {
             # because after pressing return, the system is down
             prepare_system_reboot;
 
-            # Pure 'ret' is sometimes not enough, taking the more labourish way to do it
-            for (1 .. 2) { send_key 'tab'; }
-            send_key 'ret';
-
-            # run only on qemu backend, e.g. svirt backend is fast enough to reboot properly
-            if (check_var('BACKEND', 'qemu')) {
-                my $delay = check_var('ARCH', 'x86_64') ? 5 : 10;
-                sleep $delay;    # wait to make authentication window disappear after successful authentication
-                if (check_screen 'reboot-auth', 2) {
-                    record_soft_failure 'bsc#981299';
-                    send_key_until_needlematch 'generic-desktop', 'esc', 7, 10;    # close timed out authentication window
-                    if (check_var('DISTRI', 'sle') && !sle_version_at_least('12-SP2')) {
-                        # retrying does not help on SP1 - once it fails, it will always fail
-                        # so just keep it as soft failure
-                        return;
-                    }
-                    send_key_until_needlematch 'logoutdialog', 'ctrl-alt-delete', 7, 10;    # reboot
-                    assert_and_click 'logoutdialog-reboot-highlighted';
-                }
-            }
+            send_key 'ret';    # Confirm
         }
     }
 }
