11a68bab2e8be7599c0f8d8f524a87ee668bae95
Oliver Kurz <okurz@suse.de>
2017-06-29 08:20:25 +0200

    Add support for packagehub activation over SCC
    Needs needle MR:
    https://gitlab.suse.de/openqa/os-autoinst-needles-sles/merge_requests/402
    
    Verification run: http://lord.arch/tests/6739
    Verification run for zypper_lr step: http://lord.arch/tests/6749
    
    Related progress issue: https://progress.opensuse.org/issues/20136

--- tests/installation/releasenotes.pm
+++ tests/installation/releasenotes.pm
@@ -47,7 +47,7 @@ sub run() {
     }
 
     # no release-notes for WE and all modules
-    my @no_relnotes = qw(we lgm asmm certm contm pcm tcm wsm hpcm ids idu);
+    my @no_relnotes = qw(we lgm asmm certm contm pcm tcm wsm hpcm ids idu phub);
 
     # no relnotes for ltss in QAM_MINIMAL
     push @no_relnotes, qw(ltss) if get_var('QAM_MINIMAL');
