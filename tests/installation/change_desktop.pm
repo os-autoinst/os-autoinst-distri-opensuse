dd103d6256b9abad2baef3f3ab1c312ab76523f2
Joachim Rauch <jrauch@suse.com>
2017-06-30 08:57:32 +0200

    Improve installation/change_desktop
    Added wait_screen_change around the send_key calls to avoid
    race conditions.

--- tests/installation/change_desktop.pm
+++ tests/installation/change_desktop.pm
@@ -22,30 +22,32 @@ sub change_desktop() {
     if (check_var('VIDEOMODE', 'text')) {
         send_key 'alt-c';
         assert_screen 'inst-overview-options';
-        send_key 'alt-s';
+        wait_screen_change { send_key 'alt-s'; };
     }
     else {
         send_key_until_needlematch 'packages-section-selected', 'tab', 10;
-        send_key 'ret';
+        wait_screen_change { send_key 'ret'; };
     }
 
     if (check_screen('dependancy-issue', 10) && get_var("WORKAROUND_DEPS")) {
         while (check_screen 'dependancy-issue', 5) {
             if (check_var('VIDEOMODE', 'text')) {
-                send_key 'alt-s', 3;
+                wait_screen_change { send_key 'alt-s'; };
             }
             else {
-                send_key 'alt-1', 3;
+                wait_screen_change { send_key 'alt-1'; };
             }
-            send_key 'spc',   3;
-            send_key 'alt-o', 3;
+            wait_screen_change { send_key 'spc'; };
+            send_key 'alt-o';
         }
     }
 
     assert_screen 'pattern_selector';
     if (check_var('VIDEOMODE', 'text')) {
-        send_key 'alt-f';
-        for (1 .. 4) { send_key 'up'; }
+        wait_screen_change { send_key 'alt-f'; };
+        for (1 .. 4) {
+            wait_screen_change { send_key 'up'; };
+        }
         send_key 'ret';
         send_key_until_needlematch 'patterns-list-selected', 'tab', 10;
     }
@@ -59,15 +61,15 @@ sub change_desktop() {
     else {
         if (!check_var('DESKTOP', 'gnome')) {
             send_key_until_needlematch 'gnome-selected', 'down', 10;
-            wait_screen_change { send_key ' '; };
+            send_key ' ';
         }
         if (check_var('DESKTOP', 'kde')) {
             send_key_until_needlematch 'kde-unselected', 'down', 10;
-            wait_screen_change { send_key ' '; };
+            send_key ' ';
         }
         if (check_var('DESKTOP', 'textmode')) {
             send_key_until_needlematch 'x11-selected', 'down', 10;
-            wait_screen_change { send_key ' '; };
+            send_key ' ';
         }
         assert_screen "desktop-selected";
     }
