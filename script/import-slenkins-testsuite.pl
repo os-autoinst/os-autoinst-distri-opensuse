2ea59d7af47b5534e8b5661e683ab128108ae6d2
Tomas Hehejik <thehejik@suse.com>
2017-07-17 08:14:39 +0200

    SLEnkins import script update (#3290)
    Slenkins control nodes uses openqa_support_server_sles12sp2 now

--- script/import-slenkins-testsuite.pl
+++ script/import-slenkins-testsuite.pl
@@ -10,7 +10,7 @@ my $template_control = pp(
 
     {key => "DESKTOP", value => "textmode"},
 
-    {key => "HDD_1", value => "openqa_support_server_sles12sp1.x86_64.qcow2"},
+    {key => "HDD_1", value => "openqa_support_server_sles12sp2.x86_64.qcow2"},
 
     {key => "NICTYPE", value => "tap"},
 
