74eea8c1743d839fefb1b3f30eb6d4fb496f5cd4
Petr Vorel <pvorel@suse.cz>
2017-07-24 12:09:16 +0200

    LTP: networking: Unify syntax

--- tests/kernel/ltp_setup_networking.pm
+++ tests/kernel/ltp_setup_networking.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2016 SUSE LLC
+# Copyright © 2016-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -19,13 +19,13 @@ use utils;
 
 sub install {
     # utils
-    zypper_call("in iputils psmisc tcpdump", log => 'utils.log');
+    zypper_call('in iputils psmisc tcpdump', log => 'utils.log');
 
     # clients
-    zypper_call("in dhcp-client finger telnet", log => 'clients.log');
+    zypper_call('in dhcp-client finger telnet', log => 'clients.log');
 
     # services
-    zypper_call("in dhcp-server dnsmasq finger-server nfs-kernel-server rdist rpcbind rsync tcpd telnet-server vsftpd xinetd", log => 'services.log');
+    zypper_call('in dhcp-server dnsmasq finger-server nfs-kernel-server rdist rpcbind rsync tcpd telnet-server vsftpd xinetd', log => 'services.log');
 }
 
 sub setup {
@@ -43,32 +43,32 @@ pts/7
 pts/8
 pts/9
 EOF
-    assert_script_run "echo \"$content\" >> '/etc/securetty'";
+    assert_script_run("echo \"$content\" >> '/etc/securetty'");
 
     # xinetd (echo)
     foreach my $xinetd_conf (qw(echo finger telnet)) {
-        assert_script_run 'sed -i \'s/\(disable\s*=\s\)yes/\1no/\' /etc/xinetd.d/' . $xinetd_conf;
+        assert_script_run('sed -i \'s/\(disable\s*=\s\)yes/\1no/\' /etc/xinetd.d/' . $xinetd_conf);
     }
-    assert_script_run 'sed -i \'s/^#\(\s*bind\s*=\)\s*$/\1 0.0.0.0/\' /etc/xinetd.conf';
+    assert_script_run('sed -i \'s/^#\(\s*bind\s*=\)\s*$/\1 0.0.0.0/\' /etc/xinetd.conf');
 
     # ftp
-    assert_script_run 'sed -i \'s/^\s*\(root\)\s*$/# \1/\' /etc/ftpusers';
+    assert_script_run('sed -i \'s/^\s*\(root\)\s*$/# \1/\' /etc/ftpusers');
 
     # getaddrinfo_01: missing hostname in /etc/hosts
-    assert_script_run 'h=`hostname`; grep -q $h /etc/hosts || printf "# ltp\n127.0.0.1\t$h\n::1\t$h\n" >> /etc/hosts';
+    assert_script_run('h=`hostname`; grep -q $h /etc/hosts || printf "# ltp\n127.0.0.1\t$h\n::1\t$h\n" >> /etc/hosts');
 
     # boo#1017616: missing link to ping6 in iputils >= s20150815
-    assert_script_run 'which ping6 >/dev/null 2>&1 || ln -s `which ping` /usr/local/bin/ping6';
+    assert_script_run('which ping6 >/dev/null 2>&1 || ln -s `which ping` /usr/local/bin/ping6');
 
     # dhcpd
-    assert_script_run 'touch /var/lib/dhcp/db/dhcpd.leases /var/lib/dhcp6/db/dhcpd6.leases';
+    assert_script_run('touch /var/lib/dhcp/db/dhcpd.leases /var/lib/dhcp6/db/dhcpd6.leases');
 
     # echo/echoes, getaddrinfo_01
-    assert_script_run 'sed -i \'s/^\(hosts:\s+files\s\+dns$\)/\1 myhostname/\' /etc/nsswitch.conf';
+    assert_script_run('sed -i \'s/^\(hosts:\s+files\s\+dns$\)/\1 myhostname/\' /etc/nsswitch.conf');
 
-    my $services = "dnsmasq nfsserver rpcbind vsftpd xinetd";
-    assert_script_run "systemctl enable $services";
-    assert_script_run "systemctl start $services";
+    my $services = 'dnsmasq nfsserver rpcbind vsftpd xinetd';
+    assert_script_run("systemctl enable $services");
+    assert_script_run("systemctl start $services");
 }
 
 # poo#14402
