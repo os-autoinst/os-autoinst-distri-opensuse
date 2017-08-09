8183fb824e023c70f0b94029be586575c23ea555
dehai <dhkong@suse.com>
2017-08-09 18:03:08 +0800

    Enhance gpg case to encrypt file and support fips
    this case can be running at fips environment and none fips environment.
    and also can be changed the generated key size

--- tests/console/gpg.pm
+++ tests/console/gpg.pm
@@ -7,21 +7,29 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: gpg key generation and passphrase test
-# Maintainer: Petr Cervinka <pcervinka@suse.com>
+# Summary: gpg key generation, passphrase test, encrypt file and support fips test
+# Maintainer: Petr Cervinka <pcervinka@suse.com>, Dehai Kong <dhkong@suse.com>
 
 use base "consoletest";
 use strict;
 use testapi;
+use utils;
 
-sub run {
-    select_console 'user-console';
+my $username = $testapi::username;
+my $passwd   = $testapi::password;
+my $email    = "user\@example.com";
+
+sub gpg_generate_key {
+    my ($key_size) = @_;
+    my $user_name = $username . " " . $key_size;
 
     # Get gpg version and base on the result choose different test
     my $gpg_version_output = script_output("gpg --version");
     my ($gpg_version) = $gpg_version_output =~ /gpg \(GnuPG\) (\d\.\d)/;
+
     # generate gpg key
     if ($gpg_version eq "2.0") {
+
         # gpg version 2.0.x
         type_string "gpg2 --gen-key\n";
     }
@@ -32,33 +40,89 @@ sub run {
     wait_still_screen 1;
     type_string "1\n";
     wait_still_screen 1;
-    type_string "2048\n";
+    type_string "$key_size\n";
     wait_still_screen 1;
     type_string "0\n";
     wait_still_screen 1;
     type_string "y\n";
     wait_still_screen 1;
-    type_string "Test User\n";
+    type_string "$user_name\n";
     wait_still_screen 1;
-    type_string "user\@example.com\n", 1;
+    type_string "$email\n", 1;
     type_string "\n";
     wait_still_screen 1;
     save_screenshot;
     type_string "O\n";
-
     assert_screen("gpg-enter-passphrase");
+
     # enter wrong passphrase
     type_string "REALSECRETPHRASE\n";
     assert_screen("gpg-incorrect-passphrase");
     type_string "\t\n", 1;
+
     # enter correct passphrase
-    type_string "R34LS3CR3TPHR4S3\n";
+    type_string "$passwd\n";
     wait_still_screen 1;
-    type_string "R34LS3CR3TPHR4S3\n";
+    type_string "$passwd\n";
     wait_still_screen 1;
-    # list gpg keys
-    validate_script_output("gpg --list-keys", sub { m/\[ultimate\] Test User <user\@example\.com>/ });
+    if (get_var("FIPS") || get_var("FIPS_TS") && $key_size == 1024) {
+        assert_screen("key-generation-failed");
+    }
+    else {
+        # list gpg keys
+        validate_script_output("gpg --list-keys", sub { m/\[ultimate\] $user_name \<$email\>/ });
+    }
+}
+
+sub gpg_encrypt_file {
+    my ($key_size) = @_;
+    my $user_name  = $username . " " . $key_size;
+    my $user_id    = $user_name . " " . "\<$email\>";
+
+    # prepare a text file
+    assert_script_run("touch gtest.txt");
+
+    # Should generate encrypted file gtest.txt.gpg
+    assert_script_run("gpg2 -r \"$user_id\" -e gtest.txt");
+
+    # check encrypted file gtest.txt.gpg
+    assert_script_run("ls | grep gtest.txt.gpg");
+
+    # Decrypt function can be work
+    script_run("gpg2 -u \"$user_id\" -d gtest.txt.gpg", 0);
+    wait_still_screen 1;
+    type_string "$passwd\n";
+    wait_still_screen 1;
+
+    # Should generate signature file gtest.txt.asc
+    script_run("gpg2 -u \"$user_id\" --clearsign gtest.txt", 0);
+    wait_still_screen 1;
+    type_string "$passwd\n";
+    wait_still_screen 1;
+    assert_script_run("ls | grep gtest.txt.asc");
+
+    # Verify the signature
+    assert_script_run("gpg2 -u \"$user_id\" --verify gtest.txt.asc");
+
+    # clean up
+    assert_script_run("ls | grep gtest | xargs -i rm -f {}");
+}
+
+sub run {
+    select_console 'user-console';
+
+    # gpg key generated and file encrypted with two size of key
+    for my $key_size (2048, 3072) {
+        gpg_generate_key($key_size);
+        gpg_encrypt_file($key_size);
+    }
+
+    # fips environment, 1024 bit RSA size key should NOT work
+    if (get_var("FIPS") || get_var("FIPS_TS")) {
+        gpg_generate_key(1024);
+    }
 }
 
 1;
+
 # vim: set sw=4 et:
