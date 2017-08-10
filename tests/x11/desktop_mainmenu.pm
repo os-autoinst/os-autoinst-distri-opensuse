4224c77d3a32312cd45bbd305d6da48a313ba8de
Andreas Stieger <astieger@suse.com>
2017-08-10 10:39:33 +0200

    After removal of wait_idle, add sleep to reduce desktop_mainmenu fails
    Follow-up to 28ac756.

--- tests/x11/desktop_mainmenu.pm
+++ tests/x11/desktop_mainmenu.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2017 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -18,6 +18,7 @@ use testapi;
 
 
 sub run {
+    sleep 5;
     if (check_var("DESKTOP", "lxde")) {
         x11_start_program("lxpanelctl menu");    # or Super_L or Windows key
     }
