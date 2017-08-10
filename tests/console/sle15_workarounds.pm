90205f320edf423671c60ac63628848255faeb7c
Rodion Iafarov <riafarov@suse.com>
2017-08-10 14:02:01 +0200

    Add standard repo for sle 15 in the end of installation
    Part of implemented workaround didn't work well as expected kernel dev
    packages are not available in dev-tools module. To resolve this issue we
    now add this repo manually using zypper call, as due to bsc#1053222 this
    doesn't work on add on products page.
    
    See poo#20984.

---
+++ tests/console/sle15_workarounds.pm
@@ -0,0 +1,34 @@
+# Copyright (C) 2014-2017 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+# Summary: performing extra actions specific to sle 15 which are not available normally
+# Maintainer: Rodion Iafarov <riafarov@suse.com>
+
+use base "consoletest";
+use strict;
+use testapi;
+use utils qw(zypper_call sle_version_at_least);
+
+
+sub run {
+    return unless sle_version_at_least('15');
+    select_console 'root-console';
+    # Kernel devel packages are not in the dev tools module, so add standard repos
+    record_soft_failure('bsc#1053222');    # Once bug is resolved, this code can be removed
+    zypper_call('ar http://download.suse.de/ibs/SUSE:/SLE-15:/GA/standard/SUSE:SLE-15:GA.repo');
+    zypper_call('--gpg-auto-import-keys ref');
+}
+
+1;
+# vim: set sw=4 et:
