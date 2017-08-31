644a21537ddc62fdc65310c45e865a4f0eec6a2b
Rodion Iafarov <riafarov@suse.com>
2017-08-31 11:45:26 +0200

    Adjust tests for sle 15 welcome screen
    With new architecture we show license after product is selected. Hence,
    it cannot be shown before this step. This adjusts flow for SLE15.

---
+++ tests/installation/accept_license.pm
@@ -0,0 +1,31 @@
+# Copyright (C) 2017 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Summary: Test module is used to accept license during SLE 15 installation
+#          Should be improved to be more product specific
+# Maintainer: Rodion Iafarov <riafarov@suse.com>
+
+use strict;
+use base "y2logsstep";
+use testapi;
+
+sub run {
+    my ($self) = @_;
+    $self->verify_license_has_to_be_accepted;
+    send_key $cmd{next};
+}
+
+1;
+# vim: set sw=4 et:
