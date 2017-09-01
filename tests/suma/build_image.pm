fedeac61c8b906b48ae6609d1bbcf7b8ac9f7623
Vladimir Nadvornik <nadvornik@suse.cz>
2017-09-01 13:37:39 +0200

    Test multiple image versions

--- tests/suma/build_image.pm
+++ tests/suma/build_image.pm
@@ -55,18 +55,22 @@ sub run {
   }
   else {
     select_console 'root-console';
-    
+
     assert_script_run "mkdir -p /etc/salt/master.d ; echo 'file_recv_max_size: 10000' >/etc/salt/master.d/openqa_test_image.conf";
     assert_script_run 'systemctl restart salt-master';
     barrier_create('image_registered', 2);
     barrier_create('image_synced', 2);
     barrier_wait('build_image');
-    
+
     assert_script_run 'salt -t 1000 suma-branch\* cp.push /built-image/POS_Image_JeOS5.x86_64-6.0.0', 1000;
     assert_script_run 'salt -t 1000 suma-branch\* cp.push /built-image/initrd-netboot-suse-SLES12.x86_64-2.1.1.gz', 1000;
     assert_script_run 'salt -t 1000 suma-branch\* cp.push /built-image/initrd-netboot-suse-SLES12.x86_64-2.1.1.kernel', 1000;
     script_output 'find /var/cache/salt/master/minions -type f ';
-    assert_script_run 'cp -r /var/cache/salt/master/minions/suma-branch*/files/built-image /srv/www/htdocs/pub/';
+    assert_script_run 'mv /var/cache/salt/master/minions/suma-branch*/files/built-image /srv/www/htdocs/pub/';
+    assert_script_run 'cp -r /srv/www/htdocs/pub/built-image /srv/www/htdocs/pub/built-image2';
+    assert_script_run 'mount -o loop -t ext3 /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.0 /mnt ; echo POS_Image_JeOS5-6.0.1 > /mnt/etc/ImageVersion ; umount /mnt ; sync';
+    assert_script_run 'e2fsck -f -y /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.0 ; sync';
+    assert_script_run 'mv /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.0 /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.1';
 
     # share one pillar file between tests, each test appends its own config
     script_output 'cat >> /srv/pillar/suma_test.sls << EOT
@@ -81,6 +85,16 @@ images:
         boot_image: default
         sync:
           url: http://10.0.2.10/pub/built-image/POS_Image_JeOS5.x86_64-6.0.0
+
+    - 6.0.1:
+        url: ftp://ftp/image/POS_Image_JeOS5.x86_64-6.0.1
+        name: POS_Image_JeOS5
+        fstype: ext3
+        size: `stat -c%s /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.1`
+        hash: `sha256sum /srv/www/htdocs/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.1 |cut -d \' \' -f 1`
+        boot_image: default
+        sync:
+          url: http://10.0.2.10/pub/built-image2/POS_Image_JeOS5.x86_64-6.0.1
 
 boot_images:
   default:
