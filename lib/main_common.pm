4e0e34dbfa3933c5c37e7cacbd7d47126444955c
Rodion Iafarov <riafarov@suse.com>
2017-09-15 08:38:02 +0200

    Do not load zypper_lr on sle 15 as part of console tests
    zypper_lr test module takes 1 hour for execution on SLE 15. And we don't
    need full validation in every run of console tests. So we execute
    validation part only for extra tests and in all other scenarios we
    simply list all repos to simplify investigation.

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -500,7 +500,7 @@ sub load_extra_tests {
         loadtest 'x11/yast2_lan_restart' if check_var('DISTRI', 'gnome');
     }
     else {
-        loadtest "console/zypper_lr";
+        loadtest "console/zypper_lr_validate";
         loadtest "console/openvswitch";
         # dependency of git test
         loadtest "console/sshd";
