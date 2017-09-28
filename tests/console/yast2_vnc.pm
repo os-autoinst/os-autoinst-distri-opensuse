430a5224d70a66deabd5cc973c4da93461cfe621
Oliver Kurz <okurz@suse.de>
2017-09-28 06:45:01 +0200

    yast2_vnc: Adapt to SLE15
    Verification run: http://lord.arch/tests/7543
    
    Related progress issue: https://progress.opensuse.org/issues/23820

--- tests/console/yast2_vnc.pm
+++ tests/console/yast2_vnc.pm
@@ -13,7 +13,7 @@
 use strict;
 use base "console_yasttest";
 use testapi;
-use utils 'zypper_call';
+use utils;
 
 
 sub run {
@@ -25,16 +25,20 @@ sub run {
     # install components to test plus dependencies for checking
     my $packages = 'vncmanager xorg-x11';
     # netstat is deprecated in newer versions, use 'ss' instead
-    my $use_ss = check_var('VERSION', 'Tumbleweed');
-    $packages .= ' net-tools' unless $use_ss;
+    my $use_nettools = (is_sle && !sle_version_at_least('15')) || (is_leap && !leap_version_at_least('15'));
+    $packages .= ' net-tools' if $use_nettools;
     zypper_call("in $packages");
 
     # start Remote Administration configuration
     script_run("yast2 remote; echo yast2-remote-status-\$? > /dev/$serialdev", 0);
 
     # check Remote Administration VNC got started
-    assert_screen 'yast2_vnc_remote_administration';
-    wait_still_screen;
+    assert_screen([qw(yast2_vnc_remote_administration yast2_still_susefirewall2)], 90);
+    if (match_has_tag 'yast2_still_susefirewall2') {
+        record_soft_failure "bsc#1059569";
+        send_key 'alt-i';
+        assert_screen 'yast2_vnc_remote_administration';
+    }
 
     # enable remote administration
     send_key 'alt-a';
@@ -62,7 +66,7 @@ sub run {
     wait_serial('yast2-remote-status-0', 60) || die "'yast2 remote' didn't finish";
 
     # check vnc port is listening
-    assert_script_run $use_ss ? 'ss' : 'netstat' . ' -tl | grep 5901 | grep LISTEN';
+    assert_script_run $use_nettools ? 'netstat' : 'ss' . ' -tl | grep 5901 | grep LISTEN';
 }
 1;
 
