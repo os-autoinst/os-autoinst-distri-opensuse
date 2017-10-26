dc405cd1d5aaaf3c2c3a725e4123264b95233fb1
Anton Smorodskyi <asmorodskyi@suse.com>
2017-10-26 10:35:42 +0200

    Move function to setup static IP to utils

--- lib/hpcbase.pm
+++ lib/hpcbase.pm
@@ -2,18 +2,6 @@ package hpcbase;
 use base "opensusebasetest";
 use strict;
 use testapi;
-use mm_network;
-
-sub setup_static_mm_network {
-    my ($self) = @_;
-    my $host_ip = get_required_var('HPC_HOST_IP');
-    configure_default_gateway;
-    configure_static_ip($host_ip);
-    configure_static_dns(get_host_resolv_conf());
-
-    # check if gateway is reachable
-    assert_script_run "ping -c 1 10.0.2.2 || journalctl -b --no-pager > /dev/$serialdev";
-}
 
 sub exec_and_insert_password {
     my ($self, $cmd) = @_;
