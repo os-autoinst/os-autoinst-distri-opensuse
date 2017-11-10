d5d1c3bc6340b803ef6ff5dde48d717b86ca95ad
Jiawei Sun <JiawWei.Sun@suse.com>
2017-11-10 17:40:02 +0800

    Adjust partitioning_raid for new shortcuts
    Add version check for SLES15
    Add needle check for expert partitioner drop down menu
    Select expert partitioner option for version latest then 15
    
    - see POO#27436

--- tests/installation/partitioning_raid.pm
+++ tests/installation/partitioning_raid.pm
@@ -159,6 +159,11 @@ sub run {
     # create partitioning
     send_key(is_storage_ng() ? $cmd{expertpartitioner} : $cmd{createpartsetup});
 
+    if (sle_version_at_least '15') {
+        assert_screen 'expertpartitioner_list';
+        send_key 'tab';
+        send_key 'ret';
+    }
     # With storage ng, we go directly to expert partitioner and invalidate configuration by rescan
     if (is_storage_ng) {
         send_key $cmd{rescandevices};              # Rescan devices
