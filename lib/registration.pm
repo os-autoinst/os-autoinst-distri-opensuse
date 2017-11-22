os-autoinst/os-autoinst-distri-opensuse

commit 3a9f6afe5b4216462201f02773500cffd1506b79

registration: Add helper for activating product with SUSEConnect

lib/registration.pm
@@ -24,6 +24,7 @@ use testapi;
 use utils qw(addon_decline_license assert_screen_with_soft_timeout sle_version_at_least);
 
 our @EXPORT = qw(
+  add_suseconnect_product
   fill_in_registration_data
   registration_bootloader_cmdline
   registration_bootloader_params
@@ -80,6 +81,19 @@ sub accept_addons_license {
             send_key $cmd{next};
         }
     }
+}
+
+=head2 add_suseconnect_product
+
+    add_suseconnect_product($name, [$version, [$arch, [$params]]]);
+
+Wrapper for SUSEConnect -p $name.
+=cut
+sub add_suseconnect_product {
+    my ($name, $version, $arch, $params) = @_;
+    $version //= scc_version();
+    $arch //= get_required_var('ARCH');
+    assert_script_run("SUSEConnect -p $name/$version/$arch $params");
 }
 
 sub register_addons {

lib/suseconnect_register.pm
@@ -84,12 +84,12 @@ sub command_register {
 
         # register extension without reg code.
         if (!$addon_regcode) {
-            assert_script_run "SUSEConnect -p $addon_name/$addon_version/$arch";
+            add_suseconnect_product($addon_name, $addon_version);
         }
 
         # register extension with reg code.
         else {
-            assert_script_run "SUSEConnect -p $addon_name/$version/$arch --regcode $addon_regcode";
+            add_suseconnect_product($addon_name, $version, $arch, "--regcode $addon_regcode");
         }
     }
     zypper_call 'lr';

tests/console/suseconnect_scc.pm
@@ -25,8 +25,6 @@ use registration;
 
 sub run {
     return if get_var('HDD_SCC_REGISTERED');
-    my $version  = get_required_var('VERSION');
-    my $arch     = get_required_var('ARCH');
     my $reg_code = get_required_var('SCC_REGCODE');
     my $scc_url  = get_required_var('SCC_URL');
     my $scc_addons;
@@ -40,7 +38,7 @@ sub run {
 
     # add modules
     foreach (split(',', $registration::SLE15_DEFAULT_MODULES{get_required_var('SLE_PRODUCT')} . $scc_addons)) {
-        assert_script_run "SUSEConnect -p sle-module-" . lc($registration::SLE15_MODULES{$_}) . "/$version/$arch";
+        add_suseconnect_product("sle-module-" . lc($registration::SLE15_MODULES{$_}));
     }
     # check repos actually work
     zypper_call('refresh');

tests/feature/feature_console/deregister.pm
@@ -35,12 +35,11 @@ sub run {
     my $abbrv   = "tcm";
     my $module  = get_addon_fullname("$abbrv");
     my $version = get_required_var('VERSION') =~ s/([0-9]+).*/$1/r;
-    my $arch    = get_required_var('ARCH');
     die "$module needs to be part of SCC_ADDONS for this test" unless check_var_array('SCC_ADDONS', $abbrv);
     assert_script_run("SUSEConnect --status-text | grep -A 3 $module | grep \"^\\s*Registered\"", 200);
 
     # deregister the module and check if it is successful
-    assert_script_run "SUSEConnect --de-register --product $module/$version/$arch";
+    add_suseconnect_product($module, $version, get_required_var('ARCH'), "--de-register");
 
     # check if ONE module is deregistered
     my $count_dereg = script_output("SUSEConnect --status-text | grep -c \"^\\s*Registered\"", 200);

tests/kernel/install_ltp.pm
@@ -29,9 +29,7 @@ sub scc_we_enabled {
 
 sub add_desktop_productivity_module {
     if (check_var('DISTRI', 'sle') and sle_version_at_least('15')) {
-        my $version = get_required_var('VERSION') =~ s/-SP/./gr;
-        my $arch    = get_required_var('ARCH');
-        assert_script_run("SUSEConnect -p sle-module-desktop-productivity/" . $version . "/" . $arch);
+        add_suseconnect_product("sle-module-desktop-productivity");
     }
 }
 

tests/kernel/qa_test_klp.pm
@@ -15,6 +15,7 @@ use warnings;
 use base 'opensusebasetest';
 use testapi;
 use utils;
+use registration;
 
 sub run {
     my $git_repo = get_required_var('QA_TEST_KLP_REPO');
@@ -25,9 +26,7 @@ sub run {
     zypper_call('in -l bats hiworkload');
 
     if (check_var('DISTRI', 'sle') and get_var('INCIDENT_PATCH', '')) {
-        my $version = get_required_var('VERSION') =~ s/-SP/\./gr;
-        my $arch    = get_required_var('ARCH');
-        assert_script_run("SUSEConnect -p sle-sdk/" . $version . "/" . $arch);
+        add_suseconnect_product("sle-sdk");
     }
 
     zypper_call('in -l git gcc kernel-devel make');

commit 75af52f6fd5e12f4537869b5f6282ddb09b5863d

registration: Add missing module
Fixes: 3a9f6afe ("registration: Add helper for activating product with SUSEConnect")

lib/registration.pm
@@ -21,7 +21,7 @@ use Exporter;
 use strict;
 
 use testapi;
-use utils qw(addon_decline_license assert_screen_with_soft_timeout sle_version_at_least);
+use utils qw(addon_decline_license assert_screen_with_soft_timeout scc_version sle_version_at_least);
 
 our @EXPORT = qw(
   add_suseconnect_product