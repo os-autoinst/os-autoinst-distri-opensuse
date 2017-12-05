a74e2b0a12d82a5454b3721482f4559caf56f723
mkravec <mkravec@suse.com>
2017-12-05 10:58:24 +0100

    Improve handling of login for bsc#1035968

--- tests/caasp/first_boot.pm
+++ tests/caasp/first_boot.pm
@@ -26,42 +26,38 @@ sub run {
     assert_screen 'linux-login-casp', 300;
 
     # Workers installed using autoyast have no password - bsc#1030876
-    unless (get_var('AUTOYAST')) {
-        # Workaround for bsc#1035968
-        if (is_caasp 'VMX') {
-            # FreeRDP is not sending 'Ctrl' as part of 'Ctrl-Alt-Fx', 'Alt-Fx' is fine though.
-            my $ctrl = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? '' : 'ctrl-';
-            my $tty2 = wait_screen_change(sub { send_key "${ctrl}alt-f2"; }, 1);
-            unless ($tty2) {
-                wait_screen_change(undef, 180);
-                wait_still_screen;
-            }
-        }
-        select_console 'root-console';
+    return if get_var('AUTOYAST');
 
-        # On Hyper-V we need to add special framebuffer provisions
-        if (check_var('VIRSH_VMM_FAMILY', 'hyperv') || check_var('VIRSH_VMM_TYPE', 'linux')) {
-            set_framebuffer_resolution;
-            assert_script_run 'transactional-update grub.cfg';
-            process_reboot 1;
-        }
+    # Login is displayed before cloud-init finishes - bsc#1035968
+    if (is_caasp 'VMX') {
+        # FreeRDP is not sending 'Ctrl' as part of 'Ctrl-Alt-Fx', 'Alt-Fx' is fine though.
+        my $key = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f2' : 'ctrl-alt-f2';
+        send_key_until_needlematch 'tty2-selected', $key, 10, 30;
+    }
+    select_console 'root-console';
 
-        # Restart network to push hostname to dns
-        if (is_caasp('VMX') && get_var('STACK_ROLE')) {
-            script_run "systemctl restart network", 60;
-            # Workaround for bsc#1062717 when admin node has no fqdn hostname
-            record_soft_failure 'bsc#1062717';
-            get_var('TEST') =~ /.*-(\w+)$/;
-            my $fake_hostname = $1;
-            script_run "hostnamectl set-hostname $fake_hostname.openqa.test";
-            if (check_var('STACK_ROLE', 'admin')) {
-                script_run "rm /etc/pki/ldap.crt /etc/pki/velum.crt";
-                script_run "systemctl restart admin-node-setup.service";
-                script_run "docker rm -f \$(docker ps -f \"name=k8s_velum-dashboard\" -q)";
-                script_run "docker rm -f \$(docker ps -f \"name=k8s_openldap_velum\" -q)";
-            }
-            # End of workaround
+    # On Hyper-V we need to add special framebuffer provisions
+    if (check_var('VIRSH_VMM_FAMILY', 'hyperv') || check_var('VIRSH_VMM_TYPE', 'linux')) {
+        set_framebuffer_resolution;
+        assert_script_run 'transactional-update grub.cfg';
+        process_reboot 1;
+    }
+
+    # Restart network to push hostname to dns
+    if (is_caasp('VMX') && get_var('STACK_ROLE')) {
+        script_run "systemctl restart network", 60;
+        # Workaround for bsc#1062717 when admin node has no fqdn hostname
+        record_soft_failure 'bsc#1062717';
+        get_var('TEST') =~ /.*-(\w+)$/;
+        my $fake_hostname = $1;
+        script_run "hostnamectl set-hostname $fake_hostname.openqa.test";
+        if (check_var('STACK_ROLE', 'admin')) {
+            script_run "rm /etc/pki/ldap.crt /etc/pki/velum.crt";
+            script_run "systemctl restart admin-node-setup.service";
+            script_run "docker rm -f \$(docker ps -f \"name=k8s_velum-dashboard\" -q)";
+            script_run "docker rm -f \$(docker ps -f \"name=k8s_openldap_velum\" -q)";
         }
+        # End of workaround
     }
 }
 
