ef9dec919cc97302a3c6240659c852d0b80a5c8d
Michal Nowak <mnowak@suse.com>
2017-12-15 11:36:48 +0100

    Rewrite and make unzip test more robust
    Serial line text matching is sometimes not robust enough:
    https://openqa.suse.de/tests/1320275#step/unzip/11. Adding magic string
    there so we can recognize the expected result.
    
    Also rewrote the test a bit to get rid of the absolute patch.
    
    Validation run: http://assam.suse.cz/tests/71

--- tests/console/unzip.pm
+++ tests/console/unzip.pm
@@ -24,30 +24,30 @@ use testapi;
 sub run {
     # Preparation
     select_console 'root-console';
-    assert_script_run 'mkdir -p /tmp/unzip-test/';
+    assert_script_run 'mkdir -p /tmp/unzip-test/ && pushd /tmp/unzip-test';
 
     # 1. Unzip (basic usage)
-    assert_script_run 'wget --quiet ' . data_url('console/test_unzip.zip') . ' -O /tmp/unzip-test/archive.zip';
-    assert_script_run 'cd /tmp/unzip-test; unzip archive.zip';
+    assert_script_run 'wget --quiet ' . data_url('console/test_unzip.zip');
+    assert_script_run 'unzip test_unzip.zip';
 
-    my $entries = script_output('ls -1Uq /tmp/unzip-test/ | wc -l');
-    die "Extract produced too few values: $entries instead of 6" unless ($entries =~ /^6$/);
+    my $entries = 'entries1st=' . script_output('ls -1Uq /tmp/unzip-test/ | wc -l');
+    die "Extract produced too few values: $entries instead of 6" unless ($entries =~ /entries1st=6$/);
 
     # Go one step further and verify the md5sum of each file
-    assert_script_run 'wget --quiet ' . data_url('console/checklist.md5') . ' -O /tmp/unzip-test/checklist.md5';
-    assert_script_run 'md5sum -c /tmp/unzip-test/checklist.md5';
+    assert_script_run 'wget --quiet ' . data_url('console/checklist.md5');
+    assert_script_run 'md5sum -c checklist.md5';
 
     # 2. Unzip into a new directory
-    assert_script_run 'cd /tmp/unzip-test/ ; unzip -d extract/ /tmp/unzip-test/archive.zip';
-    $entries = script_output('ls -1Uq /tmp/unzip-test/extract/ | wc -l');
-    die "Extract produced too few values: $entries instead of 5" unless ($entries =~ /^5$/);
+    assert_script_run 'unzip -d extract/ test_unzip.zip';
+    $entries = 'entries2nd=' . script_output('ls -1Uq extract/ | wc -l');
+    die "Extract produced too few values: $entries instead of 5" unless ($entries =~ /entries2nd=5$/);
     # Go one step further and verify the md5sum in extract folder of each file.
-    assert_script_run 'cp /tmp/unzip-test/archive.zip /tmp/unzip-test/extract/archive.zip';
-    assert_script_run "sed -Ei 'sXunzip-testXunzip-test/extractXg' /tmp/unzip-test/checklist.md5";
-    assert_script_run 'md5sum -c /tmp/unzip-test/checklist.md5';
+    assert_script_run 'cp test_unzip.zip extract/test_unzip.zip';
+    assert_script_run 'md5sum -c checklist.md5';
 
     # 3. Test the credibility of the zip archive
-    assert_script_run 'cd /tmp/unzip-test/ ; unzip -tq archive.zip';
+    assert_script_run 'unzip -tq test_unzip.zip';
+    assert_script_run 'popd';
 }
 
 1;
