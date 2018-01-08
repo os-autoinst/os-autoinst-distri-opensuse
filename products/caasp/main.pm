0bf3c7f244b03d96c7cad72f3efeeeada7f38cde
mkravec <mkravec@suse.com>
2018-01-08 16:31:48 +0100

    Enable overlayfs test on kubic

--- products/caasp/main.pm
+++ products/caasp/main.pm
@@ -108,10 +108,7 @@ sub load_feature_tests {
     loadtest 'caasp/create_autoyast' unless check_var('VIRSH_VMM_FAMILY', 'hyperv');
     loadtest 'caasp/libzypp_config';
     loadtest 'caasp/filesystem_ro';
-
-    # bsc#1074515 - Incorrect nsswitch.conf
-    loadtest 'caasp/overlayfs' unless check_var('DISTRI', 'kubic');
-
+    loadtest 'caasp/overlayfs';
     loadtest 'caasp/services_enabled';
     loadtest 'caasp/one_line_checks';
     loadtest 'caasp/nfs_client' if get_var('NFS_SHARE');
