f19ce39569f8e466877245551690094ab9877fe1
Alvaro Carvajal <acarvajal@suse.de>
2018-01-16 12:20:12 +0100

    sapconf: Add SAP profile name verification

--- tests/sles4sap/sapconf.pm
+++ tests/sles4sap/sapconf.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2017 SUSE LLC
+# Copyright © 2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -13,6 +13,21 @@
 use base "sles4sap";
 use testapi;
 use strict;
+
+my %profiles = (
+    hana   => 'sap-hana',
+    b1     => 'sap-hana',
+    ase    => 'sap-ase',
+    sybase => 'sap-ase',
+    bobj   => 'sap-bobj'
+);
+
+sub check_profile {
+    my $current = shift;
+    my $output  = script_output "tuned-adm active";
+    die "Tuned profile change failed. Expected 'Current active profile: $profiles{$current}', got: [$output]"
+        unless ($output =~ /Current active profile: $profiles{$current}/);
+}
 
 sub run {
     my ($self) = @_;
@@ -28,10 +43,20 @@ sub run {
       . 'Started Dynamic System Tuning Daemon.$';
     die "Command 'sapconf status' output is not recognized" unless ($output =~ m|$statusregex|s);
 
-    foreach my $cmd (qw(start hana b1 ase sybase bobj)) {
+    $output = script_output "tuned-adm active";
+    $output =~ /Current active profile: ([a-z\-]+)/;
+    my $default_profile = $1;
+    record_info("Current profile", "Current default profile: $default_profile");
+
+    foreach my $cmd ('start', keys %profiles) {
         $output = script_output "sapconf $cmd";
         die "Command 'sapconf $cmd' output is not recognized" unless ($output =~ /Forwarding action to tuned\-adm.$/);
+        next if ($cmd eq 'start');
+        check_profile($cmd);
     }
+
+    # Set default profile again
+    assert_script_run "tuned-adm profile $default_profile";
 }
 
 1;
