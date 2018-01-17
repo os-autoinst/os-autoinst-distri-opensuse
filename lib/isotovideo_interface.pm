4dbc1101453e0541da21e30481fe17e96ba4dbc3
dasantiago <dasantiago@suse.cz>
2018-01-17 18:05:16 +0100

    Stop relying on isotovideo binary for the interface version
    Binary call replace, with symbol table lookup for the interface version.

--- lib/isotovideo_interface.pm
+++ lib/isotovideo_interface.pm
@@ -6,10 +6,7 @@ use warnings;
 sub VERSION {
     my ($package, $version) = @_;
 
-    qx/isotovideo --version/ =~ /interface v(\d+)/;
-    my $last_version = $1 // 0;
-
-    die "isotovideo interface version $version required--this is version $last_version" if $version != $last_version;
+    die "isotovideo interface version $version required--this is version $main::INTERFACE" if $version != $main::INTERFACE;
 }
 
 1;
