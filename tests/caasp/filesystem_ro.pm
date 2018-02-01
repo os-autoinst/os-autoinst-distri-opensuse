5998ba7e20c6594f7f149c1fedd1b983be969f73
mkravec <mkravec@suse.com>
2018-02-01 16:54:24 +0100

    Modify filesystem test for Kubic

--- tests/caasp/filesystem_ro.pm
+++ tests/caasp/filesystem_ro.pm
@@ -14,6 +14,7 @@
 use base "opensusebasetest";
 use strict;
 use testapi;
+use version_utils 'is_caasp';
 
 sub run {
     assert_script_run "! touch /should_fail";
@@ -21,7 +22,14 @@ sub run {
     assert_script_run "touch /var/log/should_succeed";
 
     assert_script_run 'btrfs property get / ro | grep "ro=true"';
-    assert_script_run 'btrfs property get /var/log ro | grep "ro=false"';
+
+    if (is_caasp '4.0+') {
+        assert_script_run 'btrfs property get /var ro | grep "ro=false"';
+        assert_script_run 'lsattr -ld /var | grep No_COW';
+    }
+    else {
+        assert_script_run 'btrfs property get /var/log ro | grep "ro=false"';
+    }
 
     assert_script_run "grep '/ btrfs ro' /etc/fstab";
     assert_script_run "mount | grep 'on / type btrfs (ro,'";
