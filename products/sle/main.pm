0432ee9b07e13913cd1eabaad781c5f91d06fd9a
Joachim Rauch <jrauch@suse.com>
2018-02-05 10:57:42 +0100

    Remove unused setting SCC_Module
    Removed the unused setting SCC_MODULE and adapted the behaviour to
    SCC_ADDONS

--- products/sle/main.pm
+++ products/sle/main.pm
@@ -341,10 +341,10 @@ if (is_update_test_repo_test && !get_var('MAINT_TEST_REPO')) {
     }
 }
 
-if (get_var('ENABLE_ALL_SCC_MODULES') && !get_var('SCC_MODULES')) {
+if (get_var('ENABLE_ALL_SCC_MODULES') && !get_var('SCC_ADDONS')) {
     if (sle_version_at_least('15')) {
         # Add only modules which are not pre-selected
-        my $addons = (get_var('SCC_ADDONS') ? get_var('SCC_ADDONS') . ',' : '') . 'legacy,sdk,pcm';
+        my $addons = 'legacy,sdk,pcm';
         set_var('SCC_ADDONS', $addons);
         set_var('PATTERNS',   'default,asmm,pcm');
     }
