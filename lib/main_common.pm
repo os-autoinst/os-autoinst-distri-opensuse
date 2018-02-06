c7b077fda0f16ac99230a1ca2bfa4cd3f9707aef
Qingming Su <QingMing.Su@suse.com>
2018-02-06 20:01:04 +0800

    Skip version switching for zdup without patching (#4360)
    Version switch to upgrade target is not required during zdup
    without patching test.
    - see poo#31387 for details

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -224,7 +224,7 @@ sub load_zdup_tests {
     loadtest 'installation/zdup';
     loadtest 'installation/post_zdup';
     # Restrict version switch to sle until opensuse adopts it
-    loadtest "migration/version_switch_upgrade_target" if is_sle;
+    loadtest "migration/version_switch_upgrade_target" if is_sle and get_var("UPGRADE_TARGET_VERSION");
     loadtest 'boot/boot_to_desktop';
 }
 
