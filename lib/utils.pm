os-autoinst/os-autoinst-distri-opensuse

commit 18a65b9b8d62446117ac1bc02cc412a7ab865768

Fix unlocking of encrypted boot partition in storage-ng setups
I encountered multiple conflicting locations where the password prompt for
encrypted disks is handled depending on architecture, storage-ng or not, etc.
This commit makes sure that the unlocking only hapens in one location in a
utils function `workaround_type_encrypted_passphrase` which is used in the
calling locations.

Also adding documentation to functions, extracting smaller methods within the
complex grub_test module,

Verification runs:
* SLE15 x86_64 cryptlvm: http://lord.arch/tests/480
* SLE12SP4 x86_64 cryptlvm: http://lord.arch/tests/468
* SLE12SP4 x86_64 lvm-full-encrypt: http://lord.arch/tests/476
* SLE15 x86_64 staging: http://lord.arch/tests/475

Related progress issue: https://progress.opensuse.org/issues/27829

lib/utils.pm
@@ -22,7 +22,7 @@ use strict;
 
 use testapi qw(is_serial_terminal :DEFAULT);
 use mm_network;
-use version_utils qw(is_caasp is_leap is_tumbleweed is_sle is_sle12_hdd_in_upgrade leap_version_at_least sle_version_at_least);
+use version_utils qw(is_caasp is_leap is_tumbleweed is_sle is_sle12_hdd_in_upgrade leap_version_at_least sle_version_at_least is_storage_ng);
 
 our @EXPORT = qw(
   check_console_font
@@ -312,15 +312,31 @@ sub minimal_patch_system {
     }
 }
 
+=head2 workaround_type_encrypted_passphrase
+
+    workaround_type_encrypted_passphrase()
+
+Record soft-failure for unresolved feature fsc#320901 which we think is
+important and then unlock encrypted boot partitions if we expect it to be
+encrypted. This condition is met on 'storage-ng' which by default puts the
+boot partition within the encrypted LVM same as in test scenarios where we
+explicitly create an LVM including boot (C<FULL_LVM_ENCRYPT>). C<ppc64le> was
+already doing the same by default also in the case of pre-storage-ng but not
+anymore for storage-ng.
+
+=cut
 sub workaround_type_encrypted_passphrase {
-    if (
-        get_var('FULL_LVM_ENCRYPT')
-        || (check_var('ARCH', 'ppc64le')
-            && (get_var('ENCRYPT') && !get_var('ENCRYPT_ACTIVATE_EXISTING') || get_var('ENCRYPT_FORCE_RECOMPUTE'))))
-    {
-        record_soft_failure 'workaround https://fate.suse.com/320901' if sle_version_at_least('12-SP4');
-        unlock_if_encrypted;
-    }
+    # nothing to do if the boot partition is not encrypted in FULL_LVM_ENCRYPT
+    return if get_var('UNENCRYPTED_BOOT');
+    return if !get_var('ENCRYPT') && !get_var('FULL_LVM_ENCRYPT');
+    # ppc64le is always doing the opposite by default :)
+    # ppc storage-ng encrypt
+    return if (is_storage_ng && check_var('ARCH', 'ppc64le')) || (!is_storage_ng && !check_var('ARCH', 'ppc64le'));
+    # If the encrypted disk is "just activated" it does not mean that the
+    # installer would propose an encrypted installation again
+    return if get_var('ENCRYPT_ACTIVATE_EXISTING') && !get_var('ENCRYPT_FORCE_RECOMPUTE');
+    record_soft_failure 'workaround https://fate.suse.com/320901' if sle_version_at_least('12-SP4');
+    unlock_if_encrypted;
 }
 
 # if stay under tty console for long time, then check

tests/installation/grub_test.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -16,25 +16,51 @@ use strict;
 use base "opensusebasetest";
 use testapi;
 use utils;
+use version_utils 'is_sle';
 use bootloader_setup qw(stop_grub_timeout boot_into_snapshot);
 
-sub run {
-    my ($self) = @_;
 
-    if (get_var('LIVECD')) {
-        mouse_hide;
+=head2 handle_livecd_reboot_failure
+
+Handle a potential failure on a live CD related to boo#993885 that the reboot
+action from a desktop session does not work and we are stuck on the desktop.
+=cut
+sub handle_livecd_reboot_failure {
+    mouse_hide;
+    wait_still_screen;
+    assert_screen([qw(generic-desktop-after_installation grub2)]);
+    if (match_has_tag('generic-desktop-after_installation')) {
+        record_soft_failure 'boo#993885 Kde-Live net installer does not reboot after installation';
+        select_console 'install-shell';
+        type_string "reboot\n";
+        save_screenshot;
+        assert_screen 'grub2', 300;
+    }
+}
+
+=head2 set_vmware_videomode
+
+bsc#997263 - VMware screen resolution defaults to 800x600
+By default VMware starts with Grub2 in 640x480 mode and then boots the system to
+800x600. To avoid that we need to reconfigure Grub's gfxmode and gfxpayload.
+Permanent - system-wise - solution is in console/consoletest_setup.pm.
+=cut
+sub set_vmware_videomode {
+    if (check_var('VIRSH_VMM_FAMILY', 'vmware')) {
+        send_key 'c';
+        type_string "gfxmode=1024x768x32; gfxpayload=1024x768x32; terminal_output console; terminal_output gfxterm\n";
         wait_still_screen;
-        assert_screen([qw(generic-desktop-after_installation grub2)]);
-        if (match_has_tag('generic-desktop-after_installation')) {
-            record_soft_failure 'boo#993885 Kde-Live net installer does not reboot after installation';
-            select_console 'install-shell';
-            type_string "reboot\n";
-            save_screenshot;
-            assert_screen 'grub2', 300;
-        }
+        send_key 'esc';
     }
 
-    # due to pre-installation setup, qemu boot order is always booting from CD-ROM
+}
+
+=head2 handle_installer_medium_bootup
+
+Due to pre-installation setup, qemu boot order is always booting from CD-ROM.
+=cut
+sub handle_installer_medium_bootup {
+    my ($self) = @_;
     if (check_var("BOOTFROM", "d")) {
         assert_screen 'inst-bootmenu';
         if (check_var("AUTOUPGRADE") && check_var("PATCH")) {
@@ -57,68 +83,44 @@ sub run {
         send_key_until_needlematch 'inst-bootmenu-boot-harddisk', 'up';
         send_key 'ret';
     }
+}
 
-    if (get_var("STORAGE_NG") && get_var("ENCRYPT") && !get_var('UNENCRYPTED_BOOT')) {
-        if (check_var('ARCH', 'ppc64le')) {
-            # bootloader timeout is disable so hit 'ret' is needed
-            assert_screen 'grub2';    # grub appear first in powerpc before the password
-            send_key 'ret';
-        }
-        else {
-            my @tags = ();
-            for (my $disk = 0; $disk < get_var("NUMDISKS", 1); $disk++) {
-                push @tags, "grub-encrypted-disk$disk-password-prompt";
-            }
+sub bug_workaround_bsc1005313 {
+    record_soft_failure "Running with plymouth:debug to catch bsc#1005313" if get_var('PLYMOUTH_DEBUG');
 
-            foreach my $tag (@tags) {
-                assert_screen($tag, 100);
-                type_password;        # enter PW at boot
-                send_key "ret";
-            }
-        }
-    }
-    unless (get_var("STORAGE_NG") && get_var("ENCRYPT") && check_var('ARCH', 'ppc64le')) {
-        workaround_type_encrypted_passphrase unless get_var('UNENCRYPTED_BOOT');
-        # 60 due to rare slowness e.g. multipath poo#11908
-        assert_screen "grub2", 60;
-        stop_grub_timeout;
-    }
+    send_key 'e';
+    # Move to end of kernel boot parameters line
+    send_key_until_needlematch "linux-line-selected", "down";
+    send_key "end";
 
-    # BSC#997263 - VMware screen resolution defaults to 800x600
-    # By default VMware starts with Grub2 in 640x480 mode and then boots the system to
-    # 800x600. To avoid that we need to reconfigure Grub's gfxmode and gfxpayload.
-    # Permanent - system-wise - solution is in console/consoletest_setup.pm.
-    if (check_var('VIRSH_VMM_FAMILY', 'vmware')) {
-        send_key 'c';
-        type_string "gfxmode=1024x768x32; gfxpayload=1024x768x32; terminal_output console; terminal_output gfxterm\n";
-        wait_still_screen;
-        send_key 'esc';
+    assert_screen "linux-line-matched";
+    if (get_var('PLYMOUTH_DEBUG')) {
+        # remove "splash=silent quiet showopts"
+        for (1 .. 28) { send_key "backspace" }
+        type_string 'plymouth:debug';
     }
+    type_string " " . get_var('GRUB_KERNEL_OPTION_APPEND') if get_var('GRUB_KERNEL_OPTION_APPEND');
 
-    boot_into_snapshot if (get_var("BOOT_TO_SNAPSHOT"));
-    if (get_var("XEN")) {
-        send_key_until_needlematch("bootmenu-xen-kernel", 'down', 10, 5);
-    }
-    if ((check_var('ARCH', 'aarch64') && check_var('DISTRI', 'sle') && get_var('PLYMOUTH_DEBUG'))
+    save_screenshot;
+    send_key 'ctrl-x';
+}
+
+sub run {
+    my ($self) = @_;
+
+    $self->handle_livecd_reboot_failure if get_var('LIVECD');
+    $self->handle_installer_medium_bootup;
+    workaround_type_encrypted_passphrase;
+    # 60 due to rare slowness e.g. multipath poo#11908
+    assert_screen 'grub2', 60;
+    stop_grub_timeout;
+    set_vmware_videomode if check_var('VIRSH_VMM_FAMILY', 'vmware');
+    boot_into_snapshot if get_var("BOOT_TO_SNAPSHOT");
+    send_key_until_needlematch("bootmenu-xen-kernel", 'down', 10, 5) if get_var('XEN');
+    if ((check_var('ARCH', 'aarch64') && is_sle && get_var('PLYMOUTH_DEBUG'))
         || get_var('GRUB_KERNEL_OPTION_APPEND'))
     {
-        record_soft_failure "Running with plymouth:debug to catch bsc#995310" if get_var('PLYMOUTH_DEBUG');
-
-        send_key 'e';
-        # Move to end of kernel boot parameters line
-        send_key_until_needlematch "linux-line-selected", "down";
-        send_key "end";
-
-        assert_screen "linux-line-matched";
-        if (get_var('PLYMOUTH_DEBUG')) {
-            # remove "splash=silent quiet showopts"
-            for (1 .. 28) { send_key "backspace" }
-            type_string 'plymouth:debug';
-        }
-        type_string " " . get_var('GRUB_KERNEL_OPTION_APPEND') if get_var('GRUB_KERNEL_OPTION_APPEND');
-
-        save_screenshot;
-        send_key 'ctrl-x';
+        $self->bug_workaround_bsc1005313;
     }
     else {
         # avoid timeout for booting to HDD

lib/opensusebasetest.pm
@@ -374,15 +374,11 @@ sub wait_boot {
             send_key "ret";
             assert_screen "grub2", 15;
         }
-        elsif (match_has_tag("migration-source-system-grub2") or match_has_tag('grub2')) {
-            send_key "ret";    # boot to source system
-        }
         elsif (get_var("LIVETEST")) {
             # prevent if one day booting livesystem is not the first entry of the boot list
             if (!match_has_tag("boot-live-" . get_var("DESKTOP"))) {
                 send_key_until_needlematch("boot-live-" . get_var("DESKTOP"), 'down', 10, 5);
             }
-            send_key "ret";
         }
         elsif (match_has_tag('inst-bootmenu')) {
             # assuming the cursor is on 'installation' by default and 'boot from
@@ -390,8 +386,6 @@ sub wait_boot {
             send_key_until_needlematch 'inst-bootmenu-boot-harddisk', 'up';
             boot_local_disk;
             assert_screen 'grub2', 15;
-            # confirm default choice
-            send_key 'ret';
         }
         elsif (match_has_tag('encrypted-disk-password-prompt')) {
             # unlock encrypted disk before grub
@@ -402,6 +396,8 @@ sub wait_boot {
             # check_screen timeout
             die "needle 'grub2' not found";
         }
+        # confirm default choice
+        send_key 'ret';
     }
 
     # On Xen we have to re-connect to serial line as Xen closed it after restart

tests/installation/boot_encrypt.pm
@@ -8,7 +8,8 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Unlock encrypted partitions during bootup
+# Summary: Unlock encrypted partitions during bootup after the bootloader
+#   passed, e.g. from plymouth
 # Maintainer: Oliver Kurz <okurz@suse.de>
 
 use strict;

commit ec9075c2f79233a9371d748bf9e6a045dbda1078

Fix workaround of live-CD reboot failure handling
Fixes a regression introduced by 18a65b9b in combination with a649d1e6.
Whenever we reset the consoles we have to ensure that the system was really
rebooted so that the next `select_console` call finds a not logged in session
as is expected for non-activated consoles. In the case of the KDE
Live-Installer however the reboot requested by the installer does not work
(boo#993885) but we already reset the consoles causing the test to look for
a 'text-login' needle when we would just encounter a still active
'install-shell'. This commit fixes this by moving the
`handle_livecd_reboot_failure` call within the `power_action` command where
the actual reboot is requested but failing to reboot the system so that after
exiting the `power_action` call we ensured that the system really rebooted as
well as that the consoles are reset.

Verification run: http://lord.arch/tests/505

Related progress issue: https://progress.opensuse.org/issues/31480

lib/utils.pm
@@ -598,6 +598,23 @@ sub poweroff_x11 {
     }
 }
 
+=head2 handle_livecd_reboot_failure
+
+Handle a potential failure on a live CD related to boo#993885 that the reboot
+action from a desktop session does not work and we are stuck on the desktop.
+=cut
+sub handle_livecd_reboot_failure {
+    mouse_hide;
+    wait_still_screen;
+    assert_screen([qw(generic-desktop-after_installation grub2)]);
+    if (match_has_tag('generic-desktop-after_installation')) {
+        record_soft_failure 'boo#993885 Kde-Live net installer does not reboot after installation';
+        select_console 'install-shell';
+        type_string "reboot\n";
+        save_screenshot;
+    }
+}
+
 =head2 power_action
 
     power_action($action [,observe => $observe] [,keepconsole => $keepconsole] [,textmode => $textmode]);
@@ -651,6 +668,10 @@ sub power_action {
     }
     else {
         assert_shutdown($shutdown_timeout) if $action eq 'poweroff';
+        # We should only reset consoles if the system really rebooted.
+        # Otherwise the next select_console will check for a login prompt
+        # instead of handling the still logged in system.
+        handle_livecd_reboot_failure if get_var('LIVECD');
         reset_consoles;
         if (check_var('BACKEND', 'svirt') && $action ne 'poweroff') {
             console('svirt')->start_serial_grab;

tests/installation/grub_test.pm
@@ -20,24 +20,6 @@ use version_utils 'is_sle';
 use bootloader_setup qw(stop_grub_timeout boot_into_snapshot);
 
 
-=head2 handle_livecd_reboot_failure
-
-Handle a potential failure on a live CD related to boo#993885 that the reboot
-action from a desktop session does not work and we are stuck on the desktop.
-=cut
-sub handle_livecd_reboot_failure {
-    mouse_hide;
-    wait_still_screen;
-    assert_screen([qw(generic-desktop-after_installation grub2)]);
-    if (match_has_tag('generic-desktop-after_installation')) {
-        record_soft_failure 'boo#993885 Kde-Live net installer does not reboot after installation';
-        select_console 'install-shell';
-        type_string "reboot\n";
-        save_screenshot;
-        assert_screen 'grub2', 300;
-    }
-}
-
 =head2 set_vmware_videomode
 
 bsc#997263 - VMware screen resolution defaults to 800x600
@@ -108,7 +90,6 @@ sub bug_workaround_bsc1005313 {
 sub run {
     my ($self) = @_;
 
-    $self->handle_livecd_reboot_failure if get_var('LIVECD');
     $self->handle_installer_medium_bootup;
     workaround_type_encrypted_passphrase;
     # 60 due to rare slowness e.g. multipath poo#11908