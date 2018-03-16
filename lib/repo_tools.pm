d1c4786bbc553590674822d5305e938a7c8010b1
Jiawei Sun <JiawWei.Sun@suse.com>
2018-03-16 16:38:43 +0800

    RMT installation and mirror repos from SCC
    Install RMT pattern for SLES15
    Config RMT server with SCC account
    Mirror repos from SCC (default is sle-module-legacy/15/x86_64)
    Support Define mirrors by use parameter RMT_REPO
    Supprt SMT to RMT migration
    
    -See Poo#28552 & Poo#28693

--- lib/repo_tools.pm
+++ lib/repo_tools.pm
@@ -8,7 +8,7 @@
 # without any warranty.
 
 # Summary: common parts on SMT and RMT
-# Maintainer: Dehai Kong <dhkong@suse.com>
+# Maintainer: Dehai Kong <dhkong@suse.com> Jaiwei Sun <jwsun@suse.com>
 
 package repo_tools;
 
@@ -18,8 +18,9 @@ use base "x11test";
 use strict;
 use warnings;
 use testapi;
+use utils;
 
-our @EXPORT = qw (smt_wizard smt_mirror_repo);
+our @EXPORT = qw (smt_wizard smt_mirror_repo rmt_wizard rmt_mirror_repo);
 
 sub smt_wizard {
     type_string "yast2 smt-wizard;echo yast2-smt-wizard-\$? > /dev/$serialdev\n";
@@ -68,6 +69,48 @@ sub smt_mirror_repo {
     assert_script_run 'smt-mirror', 600;
     save_screenshot;
 }
+
+sub rmt_wizard {
+    my $SCC_Password = get_var('SCC_PWD');
+    my $SCC_User     = get_var('SCC_USER');
+
+    # install RMT and mariadb
+    zypper_call 'in rmt-server';
+    zypper_call 'in mariadb';
+
+    # check mysql status and config mysql for RMT
+    systemctl 'start mysql.service';
+    systemctl 'status mysql.service';
+    my $cmd = 'mysql -u root -p <<EOFF 
+GRANT ALL PRIVILEGES ON \`rmt\`.* TO rmt@localhost IDENTIFIED BY \'rmt\'; 
+FLUSH PRIVILEGES; 
+EOFF';
+    type_string "$cmd\n";
+    assert_screen('rmt-sqladmin-password', 40);
+    send_key 'ret';
+
+    # Modify rmt config file
+    assert_script_run "sed -i '/^[][ ]*username:[][ ]*\$/d' /etc/rmt.conf";
+    assert_script_run "sed -i '/^[][ ]*password/d' /etc/rmt.conf";
+    assert_script_run "sed -i '/adapter/i\ \\ \\ password: rmt' /etc/rmt.conf";
+    assert_script_run "sed -i '/scc/a\ \\ \\ password: $SCC_Password' /etc/rmt.conf";
+    assert_script_run "sed -i '/scc/a\ \\ \\ username: $SCC_User' /etc/rmt.conf";
+
+    # Start rmt server
+    systemctl 'start rmt';
+    systemctl 'status rmt';
+}
+
+sub rmt_mirror_repo {
+    my $repo_list = get_var('RMT_REPO') || 'sle-module-legacy/15/x86_64';
+    assert_script_run 'rmt-cli sync', 1800;
+    for my $repo (split(/,/, $repo_list)) {
+        assert_script_run "rmt-cli products enable $repo", 600;
+    }
+    assert_script_run 'rmt-cli mirror', 1800;
+    assert_script_run 'rmt-cli repo list';
+}
+
 
 sub test_flags {
     return {fatal => 1};
