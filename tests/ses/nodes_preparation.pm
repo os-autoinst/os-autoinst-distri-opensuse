cec7146cab0c353377a0f6b0fb497780adc5796f
Jozef Pupava <jpupava@suse.com>
2018-03-22 16:21:39 +0100

    SES5 deploy and openattic test with support server
    poo#31960

--- tests/ses/nodes_preparation.pm
+++ tests/ses/nodes_preparation.pm
@@ -24,6 +24,7 @@ sub run {
     if (check_var('HOSTNAME', 'master')) {
         my $num_nodes = get_var('NODE_COUNT');
         barrier_create('network_configured', $num_nodes + 1);
+        barrier_create('deployment_done',    $num_nodes + 1);
         barrier_create('all_tests_done',     $num_nodes + 1);
         assert_script_run 'echo \'server ntp1.suse.de burst iburst\' >> /etc/ntp.conf';
     }
@@ -33,37 +34,45 @@ sub run {
     systemctl 'restart ntpd';
     # disable ipv6
     assert_script_run 'echo \'net.ipv6.conf.all.disable_ipv6 = 1\' >> /etc/sysctl.conf';
+    systemctl 'restart network' unless get_var('DEEPSEA_TESTSUITE');
     # firewall and apparmor should not run
     systemctl 'stop SuSEfirewall2';
     systemctl 'disable SuSEfirewall2';
     systemctl 'stop apparmor';
     systemctl 'disable apparmor';
-    # set node hostname
-    my $node_hostname = get_var('HOSTNAME');
-    assert_script_run "hostnamectl set-hostname $node_hostname";
-    # configure network
-    configure_default_gateway;
-    my $node_ip = get_var('NODE_IP');
-    configure_static_ip("$node_ip/24");
-    configure_static_dns(get_host_resolv_conf());
-    # add node entries to /etc/hosts
-    my $hosts = <<'EOF';
+    if (get_var('DEEPSEA_TESTSUITE')) {
+        # set node hostname
+        my $node_hostname = get_var('HOSTNAME');
+        assert_script_run "hostnamectl set-hostname $node_hostname";
+        # configure network
+        configure_default_gateway;
+        my $node_ip = get_var('NODE_IP');
+        configure_static_ip("$node_ip/24");
+        configure_static_dns(get_host_resolv_conf());
+        # add node entries to /etc/hosts
+        my $hosts = <<'EOF';
 echo -e '10.0.2.100\tmaster.openqa.de master' >> /etc/hosts
 echo -e '10.0.2.101\tnode1.openqa.de node1' >> /etc/hosts
 echo -e '10.0.2.102\tnode2.openqa.de node2' >> /etc/hosts
 echo -e '10.0.2.103\tnode3.openqa.de node3' >> /etc/hosts
 echo -e '10.0.2.104\tnode4.openqa.de node4' >> /etc/hosts
 EOF
-    script_run($_) foreach (split /\n/, $hosts);
-    if (get_var('EDGECAST')) {
-        record_info 'Netfix', 'Go through Europe Microfocus info-bloxx';
-        my $edgecast_europe = get_var('EDGECAST');
-        assert_script_run "echo $edgecast_europe updates.suse.com >> /etc/hosts";
+        script_run($_) foreach (split /\n/, $hosts);
+        if (get_var('EDGECAST')) {
+            record_info 'Netfix', 'Go through Europe Microfocus info-bloxx';
+            my $edgecast_europe = get_var('EDGECAST');
+            assert_script_run "echo $edgecast_europe updates.suse.com >> /etc/hosts";
+        }
+        assert_script_run 'cat /etc/hosts';
+        barrier_wait {name => 'network_configured', check_dead_job => 1};
+        # nodes will ping each other to test connection
+        assert_script_run 'fping -c2 -q $(grep \'openqa.de\' /etc/hosts|awk \'{print$2}\'|tr "\n" " ")';
     }
-    assert_script_run 'cat /etc/hosts';
-    barrier_wait {name => 'network_configured', check_dead_job => 1};
-    # nodes will ping each other to test connection
-    assert_script_run 'fping -c2 -q $(grep \'openqa.de\' /etc/hosts|awk \'{print$2}\'|tr "\n" " ")';
+    else {
+        barrier_wait {name => 'network_configured', check_dead_job => 1};
+        assert_script_run 'ip a';
+        assert_script_run 'for i in {1..7}; do echo "try $i" && fping -c2 -q updates.suse.com 10.0.2.2 && sleep 2 && break; done';
+    }
 }
 
 sub test_flags {
