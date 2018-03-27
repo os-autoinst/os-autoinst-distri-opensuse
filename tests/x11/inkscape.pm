264e6303193908b69676a9ff5597dc8a48cccff4
Joachim Rauch <jrauch@suse.com>
2018-03-27 09:55:43 +0200

    Add startup verification to inkscape test
    Now verifies the typed command and whether inkscape started correctly

--- tests/x11/inkscape.pm
+++ tests/x11/inkscape.pm
@@ -17,7 +17,7 @@ use testapi;
 
 sub run {
     ensure_installed('inkscape', timeout => 300);
-    x11_start_program('inkscape');
+    x11_start_program('inkscape', target_match => 'inkscape', match_typed => 'inkscape_typed');
     send_key "alt-f4";    # Exit
 }
 
