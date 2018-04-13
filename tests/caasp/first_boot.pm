2cc671316a65830d194bc378a88196f2683c8745
mkravec <mkravec@suse.com>
2018-04-13 15:27:16 +0200

    Separate DVD & VMX logic in first_boot module

--- tests/caasp/first_boot.pm
+++ tests/caasp/first_boot.pm
@@ -20,40 +20,45 @@ use bootloader_setup 'set_framebuffer_resolution';
 use caasp qw(process_reboot script_retry);
 
 sub run {
-    # On VMX images bootloader_uefi eats grub2 needle
-    # On DVD images stall prevents reliable matching of BIOS needle - poo#28648
-    if (is_caasp('DVD') && !get_var('AUTOYAST')) {
-        assert_screen 'grub2';
-        send_key 'ret';
+    if (is_caasp 'DVD') {
+        # On DVD images stall prevents reliable matching of BIOS needle - poo#28648
+        unless (get_var 'AUTOYAST') {
+            assert_screen 'grub2';
+            send_key 'ret';
+        }
+
+        assert_screen 'linux-login-casp', 100;
+
+        # Workers installed using autoyast have no password - bsc#1030876
+        unless (get_var 'AUTOYAST') {
+            select_console 'root-console';
+        }
     }
 
-    # Check ssh keys & ip information are displayed
-    assert_screen 'linux-login-casp', 300;
-
-    # Workers installed using autoyast have no password - bsc#1030876
-    return if get_var('AUTOYAST');
-
-    # First attempts to select tty2 are ignored - bsc#1035968
     if (is_caasp 'VMX') {
+        assert_screen 'linux-login-casp', 100;
+
         # FreeRDP is not sending 'Ctrl' as part of 'Ctrl-Alt-Fx', 'Alt-Fx' is fine though.
         my $key = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f2' : 'ctrl-alt-f2';
+        # First attempts to select tty2 are ignored - bsc#1035968
         send_key_until_needlematch 'tty2-selected', $key, 10, 30;
-    }
-    select_console 'root-console';
 
-    # On Hyper-V we need to add special framebuffer provisions
-    if (check_var('VIRSH_VMM_FAMILY', 'hyperv') || check_var('VIRSH_VMM_TYPE', 'linux')) {
-        set_framebuffer_resolution;
-        assert_script_run 'transactional-update grub.cfg';
-        process_reboot 1;
-    }
+        select_console 'root-console';
 
-    # Help cloud-init on cluster tests
-    if (is_caasp('VMX') && get_var('STACK_ROLE')) {
-        # Wait for cloud-init initialization - bsc#1088654
-        script_retry 'ls /run/cloud-init/result.json';
-        # Restart network to push hostname to dns
-        systemctl 'restart network', timeout => 60;
+        # Help cloud-init on cluster tests
+        if (get_var 'STACK_ROLE') {
+            # Wait for cloud-init initialization - bsc#1088654
+            script_retry 'ls /run/cloud-init/result.json';
+            # Restart network to push hostname to dns
+            systemctl 'restart network', timeout => 60;
+        }
+
+        # On Hyper-V we need to add special framebuffer provisions
+        if (check_var('VIRSH_VMM_FAMILY', 'hyperv') || check_var('VIRSH_VMM_TYPE', 'linux')) {
+            set_framebuffer_resolution;
+            assert_script_run 'transactional-update grub.cfg';
+            process_reboot 1;
+        }
     }
 }
 
@@ -62,4 +67,3 @@ sub test_flags {
 }
 
 1;
-
