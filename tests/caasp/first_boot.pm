59d882d1f4df65661483e1214bf68d6c93d6420a
mkravec <mkravec@suse.com>
2018-04-17 14:25:14 +0200

    Enable login workarounds also after reboots

--- tests/caasp/first_boot.pm
+++ tests/caasp/first_boot.pm
@@ -17,34 +17,18 @@ use testapi;
 use utils 'systemctl';
 use version_utils 'is_caasp';
 use bootloader_setup 'set_framebuffer_resolution';
-use caasp qw(process_reboot script_retry);
+use caasp qw(process_reboot script_retry microos_login);
 
 sub run {
-    if (is_caasp 'DVD') {
-        # On DVD images stall prevents reliable matching of BIOS needle - poo#28648
-        unless (get_var 'AUTOYAST') {
-            assert_screen 'grub2';
-            send_key 'ret';
-        }
-
-        assert_screen 'linux-login-casp', 100;
-
-        # Workers installed using autoyast have no password - bsc#1030876
-        unless (get_var 'AUTOYAST') {
-            select_console 'root-console';
-        }
+    # On DVD images stall prevents reliable matching of BIOS needle - poo#28648
+    if (is_caasp('DVD') && !get_var('AUTOYAST')) {
+        assert_screen 'grub2';
+        send_key 'ret';
     }
 
+    microos_login;
+
     if (is_caasp 'VMX') {
-        assert_screen 'linux-login-casp', 100;
-
-        # FreeRDP is not sending 'Ctrl' as part of 'Ctrl-Alt-Fx', 'Alt-Fx' is fine though.
-        my $key = check_var('VIRSH_VMM_FAMILY', 'hyperv') ? 'alt-f2' : 'ctrl-alt-f2';
-        # First attempts to select tty2 are ignored - bsc#1035968
-        send_key_until_needlematch 'tty2-selected', $key, 10, 30;
-
-        select_console 'root-console';
-
         # Help cloud-init on cluster tests
         if (get_var 'STACK_ROLE') {
             # Wait for cloud-init initialization - bsc#1088654
