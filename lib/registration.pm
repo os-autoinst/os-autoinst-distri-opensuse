65c7dc516d5a143cf7fb05c38432df9858e564aa
Jozef Pupava <jpupava@suse.com>
2018-04-18 13:04:52 +0200

    Workaround missing SES6 license
    bsc#1090012

--- lib/registration.pm
+++ lib/registration.pm
@@ -81,13 +81,14 @@ sub accept_addons_license {
     #   isc co SUSE:SLE-15:GA 000product
     #   grep -l EULA SUSE:SLE-15:GA/000product/*.product | sed 's/.product//'
     # All shown products have a license that should be checked.
-    my @addons_with_license = qw(geo live rt idu ids lgm hpcm ses);
+    my @addons_with_license = qw(geo live rt idu ids lgm hpcm);
 
     # In SLE 15 some modules do not have license or have the same
     # license (see bsc#1089163) and so are not be shown twice
     push @addons_with_license, qw(ha sdk wsm we) unless is_sle('15+');
 
     for my $addon (@scc_addons) {
+        record_soft_failure 'bsc#1090012 - missing SES6 license' if $addon eq 'ses';
         # most modules don't have license, skip them
         next unless grep { $addon eq $_ } @addons_with_license;
         while (check_screen('scc-downloading-license', 5)) {
