3f341147c2f3f0667a8aebed08a71be522941b72
Dominique Leuenberger <dimstar@opensuse.org>
2018-04-25 08:41:25 +0200

    install_otherDE: do not err in case files to be removed do not exist
    The last fix was a bit ambitious: only Leap so far brings lightdm with minimalX.
    On Tumbleweed, the patterns are desynced and still bring xdm for this case, which in
    turn means that after switching, there are no lightdm state/cache files yet to be removed.

--- tests/console/install_otherDE_pattern.pm
+++ tests/console/install_otherDE_pattern.pm
@@ -31,7 +31,7 @@ sub run {
     assert_script_run("zypper -n in -t $zypp_type $pattern", 600);
 
     # Reset the state of lightdm, to have the new default in use (lightdm saves what the user's last session was)
-    assert_script_run("rm ~lightdm/.cache/lightdm-gtk-greeter/state /var/lib/AccountsService/users/*");
+    assert_script_run("rm -f ~lightdm/.cache/lightdm-gtk-greeter/state /var/lib/AccountsService/users/*");
 }
 
 sub test_flags {
