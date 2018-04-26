953b6ebb682e01e593f91f95f5ec0383cbdd8cd9
mkravec <mkravec@suse.com>
2018-04-26 15:37:28 +0200

    Introduce xy sub

--- lib/caasp_controller.pm
+++ lib/caasp_controller.pm
@@ -7,7 +7,7 @@ use lockapi;
 use mmapi 'wait_for_children';
 
 use Exporter 'import';
-our @EXPORT = qw(confirm_insecure_https velum_login switch_to download_kubeconfig click_click);
+our @EXPORT = qw(confirm_insecure_https velum_login switch_to download_kubeconfig click_click xy);
 
 # 10% of clicks are lost because ajax refreshes Velum during click
 sub click_click {
@@ -20,6 +20,24 @@ sub click_click {
     }
     mouse_hide;
     record_info 'bsc#1048975', 'User interaction is lost after page refresh';
+}
+
+# Get xy coordinates for:
+# - 1 needle  1 area    : center of a needle
+# - 1 needle  2 areas   : intersection of 2 areas   (n1a1 => x, n1aN => y)
+# - 2 needles 1|2 areas : intersection of 2 needles (n1a1 => x, n2aN => y)
+sub xy {
+    my ($tag0, $tag1) = @_;
+    my $as;
+
+    # Get x position from first needle & first area
+    $as = assert_screen($tag0)->{area};
+    my $x = $as->[0]{x} + int($as->[0]{w} / 2);
+    # Get y position from last needle & last area
+    $as = assert_screen($tag1)->{area} if $tag1;
+    my $y = $as->[-1]{y} + int($as->[-1]{h} / 2);
+
+    return ($x, $y);
 }
 
 # Easier switching between applications
