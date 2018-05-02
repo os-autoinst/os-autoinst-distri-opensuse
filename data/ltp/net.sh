f17bf2dd80dbe4f8b8cbdd963c8e82fd7ac772d4
Petr Vorel <pvorel@suse.cz>
2018-05-02 16:09:19 +0200

    LTP/net.sh: Put code into function instead calling exit
    This helps us to get exit code (result) of sourcing the library without
    calling exit. Without exit we can also source script (next commit).
    
    Generally, we need some variables to be set before sourcing the library
    (these variables differ for new and legacy LTP shell API) and they need
    to be unset after sourcing (not to affect behavior of tests).

--- data/ltp/net.sh
+++ data/ltp/net.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 # emulate $LTPROOT/testscripts/network.sh
 
-export TCID="network_settings"
-export TST_ID="$TCID"
+load_tst_net()
+{
+	export TCID="network_settings"
+	export TST_ID="$TCID"
 
-if [ -f "$LTPROOT/testcases/bin/tst_net.sh" ]; then
-	export TST_NO_DEFAULT_RUN=1
-	file="tst_net.sh"
-	echo "Using new API ($file)"
-else
-	export TST_TOTAL=1
-	file="test_net.sh"
-	echo "Using legacy API ($file)"
-fi
+	if [ -f "$LTPROOT/testcases/bin/tst_net.sh" ]; then
+		export TST_NO_DEFAULT_RUN=1
+		file="tst_net.sh"
+		echo "Using new API ($file)"
+	else
+		export TST_TOTAL=1
+		file="test_net.sh"
+		echo "Using legacy API ($file)"
+	fi
 
-. $file
-ret=$?
-echo $ret
+	. $file
+	ret=$?
 
-# new API
-unset TST_ID TST_NO_DEFAULT_RUN
-# legacy API
-unset TCID TST_TOTAL TST_LIB_LOADED
+	# new API
+	unset TST_ID TST_NO_DEFAULT_RUN
+	# legacy API
+	unset TCID TST_TOTAL TST_LIB_LOADED
 
-exit $ret
+	return $ret
+}
+
+load_tst_net
