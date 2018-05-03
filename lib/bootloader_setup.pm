fc01821718c6ba437bcb9d76b31fb4c47874af4a
lemon.li <leli@suse.com>
2018-05-03 09:20:01 +0800

    Wait to avoid send down key early in grub_test_snapshot

--- lib/bootloader_setup.pm
+++ lib/bootloader_setup.pm
@@ -81,7 +81,8 @@ sub boot_local_disk {
 sub boot_into_snapshot {
     send_key_until_needlematch('boot-menu-snapshot', 'down', 10, 5);
     send_key 'ret';
-    wait_still_screen 10;
+    # wait to avoid send down key early in grub_test_snapshot.
+    wait_still_screen 1;
     # in upgrade/migration scenario, we want to boot from snapshot 1 before migration.
     if ((get_var('UPGRADE') && !get_var('ONLINE_MIGRATION', 0)) || get_var('ZDUP')) {
         send_key_until_needlematch('snap-before-update', 'down', 40, 5);
