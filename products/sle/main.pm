fd314c1e7a9e6086c4f26ae8884597105f632cb4
Oliver Kurz <okurz@suse.de>
2018-05-09 16:03:15 +0200

    Fix SLE12 module installation by enabling SLE updates again
    Tested locally with isotovideo schedule evaluation:
    * SLE12SP4 validation: online updates enabled
    * SLE15 validation: online updates disabled
    * SLE12SP3 Incidents: online updates enabled
    * SLE12 Updates: online updates enabled
    
    Related progress issue: https://progress.opensuse.org/issues/34099

--- products/sle/main.pm
+++ products/sle/main.pm
@@ -131,6 +131,12 @@ if (sle_version_at_least('15')) {
     }
 }
 
+# don't want updates, as we don't test it or rely on it in any tests, if is executed during installation
+# Proxy SCC replaces all update repo urls and we end up having invalid update repos, and we
+# also do not want to have official update repos, which will lead to inconsistent SUT.
+# For released products we want install updates during installation, only in minimal workflow disable
+set_var('DISABLE_SLE_UPDATES', get_var('DISABLE_SLE_UPDATES', get_var('QAM_MINIMAL') || (is_sle('15+') && !is_updates_tests)));
+
 # Set serial console for Xen PV
 if (check_var('VIRSH_VMM_FAMILY', 'xen') && check_var('VIRSH_VMM_TYPE', 'linux')) {
     if (sle_version_at_least('12-SP2')) {
