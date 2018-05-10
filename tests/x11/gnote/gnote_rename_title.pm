a852c8213048a6a8e1a6837bc053993d6306909b
GraceWang <gwang@suse.com>
2018-05-10 18:39:00 +0800

    Fix x11-desktopapps-documentation failed test cases
    Updated gnote_rename_title.pm
    Updated gnote_undo_redo.pm
    Updated libreoffice_recent_documents.pm
    poo#35272

--- tests/x11/gnote/gnote_rename_title.pm
+++ tests/x11/gnote/gnote_rename_title.pm
@@ -25,9 +25,7 @@ sub run {
     send_key "up";
     send_key "up";
     type_string "new title-opensuse\n";
-    send_key "ctrl-tab";    #jump to toolbar
-    send_key "ctrl-tab" if is_sle('15+');    #jump to toolbar for SLED15
-    send_key "ret";                          #back to all notes interface
+    send_key 'esc';    #back to all notes interface
     send_key_until_needlematch 'gnote-new-note-title-matched', 'down', 6;
     send_key "delete";
     send_key "tab";
