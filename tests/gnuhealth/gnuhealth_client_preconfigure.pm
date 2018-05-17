d9cabdb15602677f75c1d735252f3e3341d513b1
Oliver Kurz <okurz@suse.de>
2018-05-17 09:21:45 +0200

    New tests for gnuhealth-client (poo#30044)
    In TW and Leap 15, the tryton frontend (package tryton) is replaced with
    the gnuhealth-client (package gnuhealth-client). These changes replace all the
    tryton tests by gnuhealth-client for oS Leap 15.0 and newer.
    
    Verification run: http://lord.arch/tests/1142
    
    Related progress ticket: https://progress.opensuse.org/issues/30044

--- tests/gnuhealth/gnuhealth_client_preconfigure.pm
+++ tests/gnuhealth/gnuhealth_client_preconfigure.pm
@@ -1,13 +1,13 @@
 # SUSE's openQA tests
 #
-# Copyright © 2017 SUSE LLC
+# Copyright © 2017-2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: first time administration and setup work for gnuhealth tryton
+# Summary: first time administration and setup work for gnuhealth client
 # Maintainer: Oliver Kurz <okurz@suse.de>
 
 use base 'x11test';
@@ -16,22 +16,23 @@ use testapi;
 use version_utils qw(is_leap is_tumbleweed);
 
 sub run {
-    x11_start_program('tryton');
-    assert_and_click 'tryton-manage_profiles';
+    my $gnuhealth = get_var('GNUHEALTH_CLIENT', 'gnuhealth-client');
+    x11_start_program($gnuhealth);
+    assert_and_click "$gnuhealth-manage_profiles";
     # wait for indexing to be done
     wait_still_screen(3);
-    assert_and_click 'tryton-manage_profiles-add';
+    assert_and_click "$gnuhealth-manage_profiles-add";
     type_string 'localhost';
-    send_key_until_needlematch 'tryton-manage_profiles-host_textfield_selected', 'tab';
+    send_key_until_needlematch "$gnuhealth-manage_profiles-host_textfield_selected", 'tab';
     type_string 'localhost';
     send_key 'tab';
     if (is_tumbleweed || is_leap('42.3+')) {
-        assert_screen 'tryton-manage_profiles-database_selected';
+        assert_screen "$gnuhealth-manage_profiles-database_selected";
         type_string 'admin';
     }
     else {
         # button 'create' should appear, weird GUI behaviour
-        assert_and_click 'tryton-manage_profiles-create_database';
+        assert_and_click "$gnuhealth-manage_profiles-create_database";
         # tryton server password
         type_string 'susetesting';
         send_key 'tab';
@@ -44,14 +45,14 @@ sub run {
         send_key 'tab';
         type_string 'susetesting';
         # wait for create button to be active
-        assert_and_click 'tryton-manage_profiles-create_database-create';
+        assert_and_click "$gnuhealth-manage_profiles-create_database-create";
 
     }
     # back to profiles menue
-    assert_screen 'tryton-manage_profiles-add', 300;
+    assert_screen "$gnuhealth-manage_profiles-add", 300;
     send_key 'ret';
     # back to login dialog
-    assert_screen 'tryton-startup';
+    assert_screen "$gnuhealth-startup";
 }
 
 sub test_flags {
