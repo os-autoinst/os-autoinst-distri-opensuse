os-autoinst/os-autoinst-distri-opensuse

commit 20ec803e6ba63710bd3c628d951a57e27dc99907

LTP: Use load_bootloader_s390x() + refactor loading kernel tests
load_bootloader_s390x() is needed for fixing tests run on s390 arch.

As we need to determine whether the test is kernel test, use
is_kernel_test() also for loading kernel tests in relevant main.pm files.

lib/main_common.pm
@@ -46,7 +46,8 @@ our @EXPORT = qw(
   any_desktop_is_applicable
   console_is_applicable
   boot_hdd_image
-  maybe_load_kernel_tests
+  is_kernel_test
+  load_kernel_tests
   load_bootloader_s390x
   load_yast2_ncurses_tests
   load_yast2_gui_tests
@@ -162,6 +163,16 @@ sub is_bridged_networking {
     return $ret;
 }
 
+sub is_kernel_test {
+    return ( get_var('INSTALL_LTP')
+          || get_var('LTP_SETUP_NETWORKING')
+          || get_var('LTP_COMMAND_FILE')
+          || get_var('INSTALL_KOTD')
+          || get_var('QA_TEST_KLP_REPO')
+          || get_var('INSTALL_KOTD')
+          || get_var('VIRTIO_CONSOLE_TEST'));
+}
+
 # Isolate the loading of LTP tests because they often rely on newer features
 # not present on all workers. If they are isolated then only the LTP tests
 # will fail to load when there is a version mismatch instead of all tests.
@@ -172,14 +183,8 @@ sub is_bridged_networking {
     if ($@) {
         bmwqemu::fctwarn("Failed to load main_ltp.pm:\n$@", 'main_common.pm');
         eval q%
-            sub maybe_load_kernel_tests {
-                if (get_var('INSTALL_LTP')
-                    || get_var('LTP_SETUP_NETWORKING')
-                    || get_var('LTP_COMMAND_FILE')
-                    || get_var('INSTALL_KOTD')
-                    || get_var('QA_TEST_KLP_REPO')
-                    || get_var('INSTALL_KOTD')
-                    || get_var('VIRTIO_CONSOLE_TEST'))
+            sub load_kernel_tests {
+                if (is_kernel_test())
                 {
                     die "Can not run kernel tests because evaluating main_ltp.pm failed";
                 }

lib/main_ltp.pm
@@ -1,4 +1,4 @@
-# Copyright © 2017 SUSE LLC
+# Copyright © 2017-2018 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,9 +22,10 @@ use autotest;
 use utils;
 use LTP::TestInfo qw(testinfo);
 use File::Basename 'basename';
+use main_common 'load_bootloader_s390x';
 use 5.018;
 
-our @EXPORT = qw(maybe_load_kernel_tests);
+our @EXPORT = 'load_kernel_tests';
 
 sub loadtest {
     my ($test, %args) = @_;
@@ -104,7 +105,9 @@ sub stress_snapshots {
     }
 }
 
-sub maybe_load_kernel_tests {
+sub load_kernel_tests {
+    load_bootloader_s390x();
+
     if (get_var('INSTALL_LTP')) {
         if (get_var('INSTALL_KOTD')) {
             loadtest 'install_kotd';
@@ -137,8 +140,4 @@ sub maybe_load_kernel_tests {
     elsif (get_var('VIRTIO_CONSOLE_TEST')) {
         loadtest 'virtio_console';
     }
-    else {
-        return 0;
-    }
-    return 1;
 }

products/opensuse/main.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2017 SUSE LLC
+# Copyright © 2012-2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -755,7 +755,8 @@ sub load_slenkins_tests {
 }
 
 # load the tests in the right order
-if (maybe_load_kernel_tests()) {
+if (is_kernel_test()) {
+    load_kernel_tests();
 }
 elsif (get_var("WICKED")) {
     boot_hdd_image();

products/sle/main.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2017 SUSE LLC
+# Copyright © 2012-2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -1192,7 +1192,8 @@ require $distri;
 testapi::set_distribution(susedistribution->new());
 
 # load the tests in the right order
-if (maybe_load_kernel_tests()) {
+if (is_kernel_test()) {
+    load_kernel_tests();
 }
 elsif (get_var("WICKED")) {
     boot_hdd_image();

commit 0612c5f1c94a9b81775932c6ac21c7ea55fc6390

Remove duplicate variable
Fixes: 20ec803e6

lib/main_common.pm
@@ -218,7 +218,6 @@ sub is_kernel_test {
     return ( get_var('INSTALL_LTP')
           || get_var('LTP_SETUP_NETWORKING')
           || get_var('LTP_COMMAND_FILE')
-          || get_var('INSTALL_KOTD')
           || get_var('QA_TEST_KLP_REPO')
           || get_var('INSTALL_KOTD')
           || get_var('VIRTIO_CONSOLE_TEST')