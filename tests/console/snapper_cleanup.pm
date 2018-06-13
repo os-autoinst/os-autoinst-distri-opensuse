f9828d5350f55c03c2593278875c9db8980d9dfc
Klaus G Wagner <kgw@suse.com>
2018-06-13 16:23:34 +0200

    Avoid to uselessly run into ENOSPC, poo#36838
    The current hardcoded test parameters combined with the default
    setting of NUMBER_LIMIT=2-10 are prone to making the test run out
    of disk space during snapshot creation if less than about 9 GB
    free filesystem space is left at the beginning. The resulting
    fails are useless.
    
    This patch adds initial free disk space detection. It then adjusts
    snapper setting NUMBER_LIMIT such that the cleanups (if working
    properly) free sufficiently much so as to prevent filesystem
    overfill even before the "snapper cleanup" invocations. It also
    adapts the number of invocations of snapper_cleanup() such that
    some actual cleanup action is to be expected in any case.
    
    Independently of the above, the test will now remove its leftovers
    after a successful run (tidy-up).

--- tests/console/snapper_cleanup.pm
+++ tests/console/snapper_cleanup.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2012-2017 SUSE LLC
+# Copyright © 2012-2018 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -33,12 +33,12 @@ sub get_space {
     return max(@numbers);
 }
 sub snapper_cleanup {
+    my ($scratch_size_gb,$scratchfile_mb) = @_;
     my $snaps_numb = "snapper list | grep number | wc -l";
 
     script_run($btrfs_fs_usage);
     # we want to fill up disk enough so that snapper cleanup triggers
-    my $scratch_size_gb = 3;
-    my $fill_space      = 'dd if=/dev/urandom of=data bs=1M count=1024';
+    my $fill_space      = "dd if=/dev/urandom of=data bs=1M count=$scratchfile_mb";
     my $snap_create     = "snapper create --cleanup number --command '$fill_space'";
     assert_script_run "btrfs filesystem show --mbytes /";
 
@@ -51,11 +51,11 @@ sub snapper_cleanup {
     assert_script_run("snapper list");
     clear_console;
     script_run($btrfs_fs_usage);
-    # Get actual exckusive disk space to verify exclusive disk space is taken into account
+    # Get actual exclusive disk space to verify exclusive disk space is taken into account
     my $qgroup_excl_space = get_space("btrfs qgroup show  / --raw | grep 1/0 | awk -F ' ' '{print\$3}'");
     if ($qgroup_excl_space > $exp_excl_space) {
-        my $msg = "bsc#998360: Exclusive space is above user-defined limit: "
-          . "$exp_excl_space (expected exclusive disk space) < $qgroup_excl_space (expected exclusive disk space)";
+        my $msg = "bsc#998360: qgroup 1/0: Exclusive space is above user-defined limit:\n"
+          . "$exp_excl_space (expected exclusive disk space) < $qgroup_excl_space (consumed exclusive disk space)";
         if (check_var('VERSION', '12-SP2')) {
             record_soft_failure $msg;
         }
@@ -73,17 +73,51 @@ sub run {
         assert_script_run "snapper set-config NUMBER_LIMIT=2-10 NUMBER_LIMIT_IMPORTANT=4-10 SPACE_LIMIT=0.5";
     }
 
+    my ($n_scratch_prepost, $scratchfile_mb, $safety_margin_mb, $initially_free);
+    my ($nmax_snapshots, $number_limit_upper_orig, $number_limit_upper, $n);
+
+    # three test parameters; hardcoded values found appropriate in past tests
+    $n_scratch_prepost = 3;
+    $scratchfile_mb = 1024;
+    # Expected to keep the test from filling the last 300 MB of the filesystem, poo#36838
+    $safety_margin_mb = 300 + $scratchfile_mb;
+
     assert_script_run("snapper get-config");    # get initial cfg
+    $number_limit_upper_orig = script_output("snapper get-config | awk '/^NUMBER_LIMIT\\>/{ print \$3 }'");
     assert_script_run("snapper list");          # get initial list of snap's
-    assert_script_run("snapper set-config NUMBER_MIN_AGE=0");
+
+    # check amount of free fs disk space and adapt to it
+    $initially_free = get_space("$btrfs_fs_usage | awk -F ' ' '/Free .estimated.:.*min:/{print\$3}'"); # bytes
+    $nmax_snapshots = int(($initially_free/1_048_576 - $safety_margin_mb)/$scratchfile_mb) * 2;
+
+    # NUMBER_LIMIT setting such that disk space after cleanup
+    # allows another $n_scratch_prepost pre/post snapshot pairs
+    $number_limit_upper = $nmax_snapshots - $n_scratch_prepost * 2;
+    if ($number_limit_upper > 0) {
+        # plenty of free space? No ambition to fill it all, then.
+        $number_limit_upper = 18 if ($number_limit_upper > 18);
+        assert_script_run "snapper set-config NUMBER_LIMIT=2-$number_limit_upper NUMBER_MIN_AGE=0";
+    }  elsif ($number_limit_upper == 0) {
+        assert_script_run "snapper set-config NUMBER_LIMIT=0 NUMBER_MIN_AGE=0";
+    }  else  {
+        # no cleanup test at all will be run in this case, see $n below
+        record_soft_failure('WARNING: insufficient disk space left on / for this test');
+    }
+    assert_script_run("snapper get-config");    # report customized cfg
     assert_script_run("btrfs qgroup show -pc /", 3);
     # Exclusive disk space of qgroup should be ~50% of the fs space as set with SPACE_LIMIT
     $exp_excl_space = get_space("$btrfs_fs_usage | sed -n '2p' | awk -F ' ' '{print\$3}'") / 2;
     # We need to run snapper at least couple of times to ensure it cleans up properly
-    # '4' is an arbitrary value proven by test
-    for (1 .. 4) { snapper_cleanup; }
+    # Specifically: Iteration $n - 3 to be the first to actually do clean-ups
+    $n = ($number_limit_upper >= 0) ? int(($number_limit_upper+0.5)/($n_scratch_prepost*2)) + 3 : 0;
 
-    assert_script_run("snapper set-config NUMBER_MIN_AGE=1800");
+    for (1 .. $n) { snapper_cleanup($n_scratch_prepost,$scratchfile_mb); }
+
+    # tidy up and restore default settings
+    assert_script_run("snapper set-config NUMBER_LIMIT=0; snapper cleanup number; rm -fv data",  300);
+    assert_script_run("snapper set-config NUMBER_LIMIT=$number_limit_upper_orig NUMBER_MIN_AGE=1800");
+    assert_script_run("snapper get-config; snapper ls");    # final report
+    assert_script_run("$btrfs_fs_usage");                   # final report
 }
 
 1;
