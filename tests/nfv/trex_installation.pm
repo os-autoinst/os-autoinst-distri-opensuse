f0e2243dfa42e4a76fe4859ea06394bd8e7c4981
Jose Lausch <jalausuch@suse.com>
2018-07-02 23:42:26 +0200

    [NFV] Add config file for T-Rex

--- tests/nfv/trex_installation.pm
+++ tests/nfv/trex_installation.pm
@@ -16,22 +16,33 @@ use testapi;
 use strict;
 use utils;
 use mmapi;
-use serial_terminal 'select_virtio_console';
 
 sub run {
+    select_console 'root-ssh';
+
     my $trex_version = get_required_var('TG_VERSION');
     my $tarball      = "$trex_version.tar.gz";
     my $url          = "http://trex-tgn.cisco.com/trex/release/$tarball";
     my $trex_dest    = "/tmp/trex-core";
+    my $trex_conf    = "/etc/trex_cfg.yaml";
+    my $PORT_1       = get_required_var('PORT_1');
+    my $PORT_2       = get_required_var('PORT_2');
 
-    select_virtio_console();
-
+    # Download and extract T-Rex package
     assert_script_run("wget $url", 900);
     assert_script_run("tar -xzf $tarball");
     assert_script_run("mv $trex_version $trex_dest");
 
-    # Copy sample config file to default location
-    assert_script_run("cp $trex_dest/cfg/simple_cfg.yaml /etc/trex_cfg.yaml");
+    # Copy config file and replace port values
+    assert_script_run("curl " . data_url('nfv/trex_cfg.yaml') . " -o $trex_conf");
+    assert_script_run("sed -i 's/PORT_0/$PORT_1/' -i $trex_conf");
+    assert_script_run("sed -i 's/PORT_1/$PORT_2/' -i $trex_conf");
+    assert_script_run("cat $trex_conf");
+
+    # Start daemon
+    assert_script_run("cd $trex_dest");
+    assert_script_run("./trex_daemon_server start");
 }
 
 1;
+
