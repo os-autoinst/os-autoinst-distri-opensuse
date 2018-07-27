b72f8eabc36f0bce5ee5e87b3e0eabdab8e376ba
Jozef Pupava <jpupava@suse.com>
2018-07-27 08:49:04 +0200

    Add SLES4SAP addon as it is not prsent in SCC_ADDONS
    SLES4SAP addon does not exist on SLE12SP3, have to add it at this step
    to create needed QAM variables

--- products/sle/main.pm
+++ products/sle/main.pm
@@ -323,6 +323,9 @@ if (is_updates_test_repo && !get_var('MAINT_TEST_REPO')) {
     # set SCC_ADDONS before push to slenkins
     set_var('SCC_ADDONS', join(',', @addons));
 
+    # SLES4SAP does not have addon on SLE12SP3
+    push(@addons, 'sles4sap') if is_sle('<15') && check_var('FLAVOR', 'Server-DVD-SLES4SAP-Updates');
+
     # push sdk addon to slenkins tests
     if (get_var('TEST', '') =~ /^slenkins/) {
         push(@addons, 'sdk');
