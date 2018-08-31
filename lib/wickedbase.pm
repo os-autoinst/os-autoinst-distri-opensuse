dda12d3c5c65ed9ad98350704407d27a4f55d319
Rodion Iafarov <riafarov@suse.com>
2018-08-31 12:08:19 +0200

    Revert "Recover network if cannot reach worker"

--- lib/wickedbase.pm
+++ lib/wickedbase.pm
@@ -14,7 +14,6 @@ package wickedbase;
 
 use base 'opensusebasetest';
 use utils 'systemctl';
-use network_utils 'setup_static_network';
 use testapi;
 
 sub assert_wicked_state {
@@ -118,7 +117,7 @@ sub before_scenario {
         assert_script_run("ifbind.sh unbind $iface");
         script_run("rm /etc/sysconfig/network/ifcfg-$iface");
         assert_script_run("ifbind.sh bind $iface");
-        $self->setup_static_network(ip => $self->get_ip(is_wicked_ref => check_var('IS_WICKED_REF', 1), type => 'host'));
+        $self->setup_static_network($self->get_ip(is_wicked_ref => check_var('IS_WICKED_REF', 1), type => 'host'));
     }
     record_info($title, $text);
 }
