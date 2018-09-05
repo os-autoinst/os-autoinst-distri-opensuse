06fb0f7980e6c38636e74ad4ddd86fff95b64334
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2018-09-05 13:39:43 +0200

    Separate ipa installation, run ipa for azure
    Add testmodule (install_ipa.pm) to install ipa and publish hdd.
    This module has test_flags->fatal set.
    The PUBLIC_CLOUD_TOOLS_REPO parameter is now optional. The repo
    url gets generated depending on VERSION and DISTRI.
    Add parameter to ipa.pm to specify instance type.
    Add support to run IPA for azure.
    Don't call 'aws ec2 delete-key-pair', when we didn't created one.

--- tests/publiccloud/ipa.pm
+++ tests/publiccloud/ipa.pm
@@ -15,13 +15,33 @@ use base "opensusebasetest";
 use strict;
 use testapi;
 use utils;
-use File::Basename;
-use registration 'add_suseconnect_product';
 use serial_terminal 'select_virtio_console';
-use version_utils qw(is_sle is_opensuse);
 
+sub get_ipa_tests {
+    my $ipa_tests = get_required_var('PUBLIC_CLOUD_IPA_TESTS');
+    $ipa_tests =~ s/,/ /g;
+    return $ipa_tests;
+}
 
-sub find_secgroup {
+sub save_logs {
+    my $ret = script_run("test -d ipa_results");
+    return if (!defined($ret) || $ret != 0);
+
+    my $image = get_required_var('PUBLIC_CLOUD_IMAGE_ID');
+
+    my $output = script_output("find ipa_results -type f");
+    for my $file (split(/\n/, $output)) {
+        if ($file =~ m"ipa_results/ec2/ami-[0-9a-z]+/i-[0-9a-z]+/[0-9]{14}\.(log|results)" or
+            $file =~ m"ipa_results/azure/$image/azure-ipa-test-\w+/[0-9]{14}\.(log|results)") {
+            upload_logs($file, failok => 1);
+            if ($file =~ /results$/) {
+                parse_extra_log(IPA => $file);
+            }
+        }
+    }
+}
+
+sub ec2_find_secgroup {
     my ($name) = @_;
 
     my $out = script_output("aws ec2 describe-security-groups --group-names '$name'", 30, proceed_on_failure => 1);
@@ -31,97 +51,71 @@ sub find_secgroup {
     return;
 }
 
-sub create_secgroup {
+sub ec2_create_secgroup {
     my ($name) = @_;
 
-    my $secgroup_id = find_secgroup($name);
+    my $secgroup_id = ec2_find_secgroup($name);
     return $secgroup_id if (defined($secgroup_id));
 
     assert_script_run("aws ec2 create-security-group --group-name '$name' --description 'SSH_OPEN'");
     assert_script_run("aws ec2 authorize-security-group-ingress --group-name '$name' --protocol tcp --port 22 --cidr 0.0.0.0/0");
     assert_script_run("aws ec2 authorize-security-group-ingress --group-name '$name' --protocol icmp --cidr 0.0.0.0/0 --port 0 ");
-    return find_secgroup($name);
+    return ec2_find_secgroup($name);
 }
 
-sub create_ssh_key {
+sub ec2_create_ssh_key {
     my ($prefix, $out_file) = @_;
 
     for my $i (0 .. 9) {
         my $key_name = $prefix . "_" . $i;
         my $cmd      = "aws ec2 create-key-pair --key-name '" . $key_name . "' --query 'KeyMaterial' --output text > " . $out_file;
-        if (script_run($cmd) == 0) {
+        my $ret      = script_run($cmd);
+        if (defined($ret) && $ret == 0) {
             return $key_name;
         }
     }
     die("Unable to create SSH key on aws with prefix '$prefix'");
 }
 
-sub save_logs {
-    my $ret = script_run("test -d ipa_results");
-    return if (!defined($ret) || $ret != 0);
+sub ec2_cleanup {
+    my ($self) = @_;
 
-    my $output = script_output("find ipa_results -type f");
-    for my $file (split(/\n/, $output)) {
-        if ($file =~ m'ipa_results/ec2/ami-[0-9a-z]+/i-[0-9a-z]+/[0-9]{14}\.(log|results)') {
-            upload_logs($file, failok => 1);
-            if ($file =~ /results$/) {
-                parse_extra_log(IPA => $file);
-            }
-        }
+    # Terminate instance
+    if (defined($self->{'ipa_instance_id'})) {
+        assert_script_run("aws ec2 terminate-instances --instance-ids " . $self->{'ipa_instance_id'});
+    }
+
+    if (defined($self->{'ipa_ssh_key_name'})) {
+        assert_script_run("aws ec2 delete-key-pair --key-name " . $self->{'ipa_ssh_key_name'});
     }
 }
 
-sub run {
+sub ec2_run_ipa {
     my ($self) = @_;
 
-    select_virtio_console();
-
-    die "Public cloud provider isn't supported" unless check_var('PUBLIC_CLOUD_PROVIDER', "EC2");
-
-    if (is_sle) {
-        my $modver = get_required_var('VERSION') =~ s/-SP\d+//gr;
-        add_suseconnect_product('sle-module-public-cloud', $modver);
-    }
-    zypper_call('ar ' . get_required_var('PUBLIC_CLOUD_TOOLS_REPO'));
-    zypper_call('--gpg-auto-import-keys in python3-ipa python3-ipa-tests');
-
-    # WAR install awscli from pip instead of using the package bcs#1095041
-    zypper_call('in gcc python3-pip');
-    if (is_opensuse) {
-        zypper_call('in python3-devel');
-        assert_script_run("pip3 install pycrypto");
-    }
-    assert_script_run("pip3 install awscli");
-    assert_script_run("pip3 install keyring");
+    my $region = get_var('PUBLIC_CLOUD_REGION', 'eu-central-1');
 
     assert_script_run("export AWS_ACCESS_KEY_ID=" . get_required_var('PUBLIC_CLOUD_KEY_ID'));
     assert_script_run("export AWS_SECRET_ACCESS_KEY=" . get_required_var('PUBLIC_CLOUD_KEY_SECRET'));
-    assert_script_run("export AWS_DEFAULT_REGION='eu-central-1'");
+    assert_script_run('export AWS_DEFAULT_REGION="' . $region . '"');
 
     # Create SSH key.
-    my $ssh_key_name = create_ssh_key("openqa_" . time, 'QA_SSH_KEY.pem');
+    my $ssh_key_name = ec2_create_ssh_key("openqa_" . time, 'QA_SSH_KEY.pem');
     $self->{'ipa_ssh_key_name'} = $ssh_key_name;
 
     # Create security group
-    my $secgroup_id = create_secgroup("qa_secgroup");
+    my $secgroup_id = ec2_create_secgroup("qa_secgroup");
     die "Failed on creating security group" unless $secgroup_id;
 
     #Prestart instance, cause IPA might use the wrong security group
     my $ami_id = get_required_var("PUBLIC_CLOUD_IMAGE_ID");
     my ($instance_id)
-      = script_output("aws ec2 run-instances --image-id $ami_id --instance-type t2.large --key-name " . $ssh_key_name . " --security-group-ids '$secgroup_id'")
+      = script_output("aws ec2 run-instances --image-id $ami_id --instance-type '"
+          . get_var('PUBLIC_CLOUD_INSTANCE_TYPE', 't2.large')
+          . "' --key-name " . $ssh_key_name . " --security-group-ids '$secgroup_id'")
       =~ /"InstanceId":\s*"([^"]+)"/;
     $self->{'ipa_instance_id'} = $instance_id;
 
-    # Create some folders, ipa will need them
-    assert_script_run("mkdir -p ~/ipa/tests/");
-    assert_script_run("mkdir -p .config/ipa");
-    assert_script_run("touch .config/ipa/config");
-    assert_script_run("ipa list");
-    assert_script_run("ipa --version");
-
-    my $ipa_tests = get_required_var('PUBLIC_CLOUD_IPA_TESTS');
-    $ipa_tests =~ s/,/ /g;
 
     assert_script_run(
         "ipa test ec2 "
@@ -132,17 +126,80 @@ sub run {
           . "-D 'IPA test $ami_id' "
           . "--distro sles "
           . "-R '$instance_id' "
-          . "--region 'eu-central-1' "
+          . "--region '$region' "
           . "-u ec2-user "
           . "--ssh-private-key-file QA_SSH_KEY.pem "
           . "--ssh-key-name '" . $ssh_key_name . "' "
           . "--results-dir ipa_results "
-          . $ipa_tests,
+          . get_ipa_tests(),
         timeout => 600
     );
     delete $self->{'ipa_instance_id'};
 
-    assert_script_run("aws ec2 delete-key-pair --key-name " . $ssh_key_name);
+    ec2_cleanup;
+}
+
+sub az_run_ipa {
+    my ($self) = @_;
+
+    my $clientid      = get_required_var('PUBLIC_CLOUD_KEY_ID');
+    my $clientsecret  = get_required_var('PUBLIC_CLOUD_KEY_SECRET');
+    my $subscription  = get_required_var('PUBLIC_CLOUD_SUBSCRIPTION_ID');
+    my $tenantid      = get_required_var('PUBLIC_CLOUD_TENANT_ID');
+    my $image         = get_required_var('PUBLIC_CLOUD_IMAGE_ID');
+    my $instance_type = get_var('PUBLIC_CLOUD_INSTANCE_TYPE', 'Standard_A2');
+    my $region        = get_var('PUBLIC_CLOUD_REGION', 'westeurope');
+
+    my $credentials = <<EOT;
+{
+  "clientId": "$clientid", 
+  "clientSecret": "$clientsecret", 
+  "subscriptionId": "$subscription", 
+  "tenantId": "$tenantid", 
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com", 
+  "resourceManagerEndpointUrl": "https://management.azure.com/", 
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/", 
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/", 
+  "galleryEndpointUrl": "https://gallery.azure.com/", 
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+EOT
+
+    save_tmp_file("azure_credentials.txt", $credentials);
+    assert_script_run('curl -O ' . autoinst_url . "/files/azure_credentials.txt");
+
+    assert_script_run('ssh-keygen -b 2048 -t rsa -q -N "" -f ~/.ssh/id_rsa');
+
+    assert_script_run(
+        "ipa test azure "
+          . "--service-account-file azure_credentials.txt "
+          . "-D 'IPA test $image' "
+          . "--distro sles "
+          . "--ssh-private-key-file ~/.ssh/id_rsa "
+          . "--region '$region' "
+          . "--results-dir ipa_results "
+          . "--cleanup "
+          . "--instance-type '$instance_type' "
+          . "-i '$image' "
+          . get_ipa_tests(),
+        timeout => 1200
+    );
+}
+
+sub run {
+    my ($self) = @_;
+
+    select_virtio_console();
+
+    if (check_var('PUBLIC_CLOUD_PROVIDER', 'EC2')) {
+        ec2_run_ipa($self);
+    }
+    elsif (check_var('PUBLIC_CLOUD_PROVIDER', 'AZURE')) {
+        az_run_ipa($self);
+    }
+    else {
+        die('Unknown PUBLIC_CLOUD_PROVIDER given');
+    }
     save_logs;
 }
 
@@ -151,12 +208,9 @@ sub post_fail_hook {
 
     save_logs;
 
-    # Terminate instance
-    if (defined($self->{'ipa_instance_id'})) {
-        assert_script_run("aws ec2 terminate-instances --instance-ids " . $self->{'ipa_instance_id'});
+    if (check_var('PUBLIC_CLOUD_PROVIDER', 'EC2')) {
+        ec2_cleanup($self);
     }
-
-    assert_script_run("aws ec2 delete-key-pair --key-name " . $self->{'ipa_ssh_key_name'});
 }
 
 1;
@@ -177,15 +231,12 @@ IPA get installed in and not to the public cloud image.
 =head2 PUBLIC_CLOUD_IMAGE_ID
 
 The image ID which is used to instantiate a VM and run tests on it.
+For azure, the name of the image, e.g. B<SUSE:SUSE-Manager-Server-BYOS:3.1:2018.08.27>.
+For ec2 the AMI, e.g. B<ami-067a77ef88a35c1a5>.
 
 =head2 PUBLIC_CLOUD_PROVIDER
 
-The type of the CSP (Cloud service provider). Currently only EC2 is allowed.
-
-=head2 PUBLIC_CLOUD_TOOLS_REPO
-
-The URL to the cloud:tools repo. 
-(e.g. http://download.opensuse.org/repositories/Cloud:/Tools/openSUSE_Tumbleweed/Cloud:Tools.repo)
+The type of the CSP (Cloud service provider). 
 
 =head2 PUBLIC_CLOUD_KEY_ID
 
@@ -194,6 +245,27 @@ The CSP credentials key-id to used to access API.
 =head2 PUBLIC_CLOUD_KEY_SECRET
 
 The CSP credentials secret used to access API.
+
+=head2 PUBLIC_CLOUD_REGION
+
+The region to use. (default-azure: westeurope, default-ec2: eu-central-1)
+
+=head2 PUBLIC_CLOUD_INSTANCE_TYPE
+
+Specify the instance type. Which instance types exists depends on the CSP.
+(default-azure: Standard_A2, default-ec2: t2.large )
+
+More infos:
+Azure: https://docs.microsoft.com/en-us/rest/api/compute/virtualmachinesizes/list
+EC2: https://aws.amazon.com/ec2/instance-types/
+
+=head2 PUBLIC_CLOUD_TENANT_ID
+
+This is B<only for azure> and used to create the service account file.
+
+=head2 PUBLIC_CLOUD_SUBSCRIPTION_ID
+
+This is B<only for azure> and used to create the service account file.
 
 =cut
 
