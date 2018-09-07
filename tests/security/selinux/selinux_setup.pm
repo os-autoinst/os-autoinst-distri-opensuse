6d3a041f570a3f6ae66e1e3cb93dfd8e8c3ae2a8
lilyeyes <llzhao@suse.com>
2018-09-07 18:15:18 +0800

    Add SELinux new test cases

---
+++ tests/security/selinux/selinux_setup.pm
@@ -0,0 +1,55 @@
+# Copyright (C) 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Summary: Setup environment for selinux tests
+# Maintainer: llzhao <llzhao@suse.com>
+# Tags: poo#40358
+
+use base "consoletest";
+use strict;
+use testapi;
+use utils;
+use version_utils qw(is_sle is_leap);
+
+sub run {
+    select_console "root-console";
+
+    # for opensuse, e.g, Tumbleweed, add the repo in case rather than by default
+    if (!is_sle && !is_leap) {
+        zypper_call("ar http://download.opensuse.org/tumbleweed/repo/oss/ tumbleweed-Oss");
+    }
+
+    # program 'sestatus' can be found in policycoreutils pkgs
+    zypper_call("in policycoreutils policycoreutils-python");
+
+    my $pkgs = script_output("echo `zypper se selinux | grep -i selinux | grep -v -w srcpackage | cut -d '|' -f 2`");
+    zypper_call("in $pkgs", timeout => 3000);
+
+    # for opensuse, e.g, Tumbleweed install selinux_policy pkgs as needed
+    # NOTE: have to install "selinux-policy-minimum-*" pkg due to this bug: bsc#1108949
+    if (!is_sle && !is_leap) {
+        my @files = ("selinux-policy-20140730-103.3.noarch.rpm", "selinux-policy-minimum-20140730-103.3.noarch.rpm");
+        foreach my $file (@files) {
+            assert_script_run "wget --quiet " . data_url("selinux/$file");
+            assert_script_run("rpm -ivh --nosignature $file");
+        }
+    }
+}
+
+sub test_flags {
+    return {milestone => 1, fatal => 1};
+}
+
+1;
---
+++ tests/security/selinux/selinux_smoke.pm
@@ -0,0 +1,57 @@
+# Copyright (C) 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Summary: Test with SELinux is "enabled" and in "permissive" mode system still
+#          works, can access files, directories, ports and processes, e.g.,
+#          no problem with refreshing and updating,
+#          packages should be installed and removed without any problems,
+#          patterns could be installed.
+# Maintainer: llzhao <llzhao@suse.com>
+# Tags: poo#40361, tc#1682591
+
+use base "consoletest";
+use strict;
+use testapi;
+use utils;
+use registration "add_suseconnect_product";
+use version_utils "is_sle";
+
+sub run {
+    select_console "root-console";
+
+    # make sure SELinux is "enabled" and in "permissive" mode
+    validate_script_output("sestatus", sub { m/SELinux\ status: .*enabled.* Current\ mode: .*permissive/sx });
+
+    # refresh & update
+    zypper_call("ref", timeout => 1200);
+    zypper_call("up",  timeout => 1200);
+
+    # install & remove pkgs, e.g., apache2
+    zypper_call("in apache2");
+    zypper_call("rm apache2");
+
+    # for sle, register available extensions and modules, e.g., free addons
+    if (is_sle) {
+        my $SCC_REGCODE = get_required_var("SCC_REGCODE");
+        assert_script_run("SUSEConnect -r $SCC_REGCODE");
+        add_suseconnect_product("sle-sdk");
+    }
+
+    # install & remove patterns, e.g., mail_server
+    zypper_call("in -t pattern mail_server");
+    zypper_call("rm -t pattern mail_server");
+}
+
+1;
