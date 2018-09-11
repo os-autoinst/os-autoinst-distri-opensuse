e0a0c243730a55cd74a2034ac239a1ddc8eec861
Ettore Di Giacinto <mudler@users.noreply.github.com>
2018-09-11 11:41:18 +0200

    Adapt isotovideo to recent os-autoinst changes (#5746)
    The interface version was moved in
    https://github.com/os-autoinst/os-autoinst/commit/cebdde289fd2dfacb29eff95a9127778f3dc930e

--- lib/isotovideo.pm
+++ lib/isotovideo.pm
@@ -5,12 +5,14 @@ use warnings;
 
 sub VERSION {
     my ($package, $version) = @_;
+    my $v = $main::INTERFACE // $OpenQA::Isotovideo::Interface::version;
 
-    die "isotovideo interface version $version required--this is version $main::INTERFACE" if $version != $main::INTERFACE;
+    die "isotovideo interface version $version required--this is version $v" if $version != $v;
 }
 
 sub get_version {
-    my $version = $main::INTERFACE;
+    my $version = $main::INTERFACE // $OpenQA::Isotovideo::Interface::version;
+    return unless defined $version;
     ($version) =~ /\d+/;
     return $version;
 }
