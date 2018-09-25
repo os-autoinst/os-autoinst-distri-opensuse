os-autoinst/os-autoinst-distri-opensuse

commit bca493a1b380e3a4e255aed33c39269a51b32215

LTP: Adjust tags for bare metal tests
LTP tests on Bare metal don't setup HDD_1, which is required for getting
tag. Let's emulate it with more variables which should be set on both
qemu and bare metal tests.

+ extract code to function get_ltp_tag() to comply DRY.

This is a preparation for LTP bare metal tests.

lib/main_common.pm
@@ -34,6 +34,7 @@ our @EXPORT = qw(
   console_is_applicable
   consolestep_is_applicable
   default_desktop
+  get_ltp_tag
   gnomestep_is_applicable
   guiupdates_is_applicable
   have_scc_repos
@@ -232,6 +233,14 @@ sub is_kernel_test {
         || get_var('VIRTIO_CONSOLE_TEST')
         || get_var('NVMFTESTS')
         || get_var('TRINITY'));
+}
+
+sub get_ltp_tag {
+    my $tag = get_var('LTP_RUNTEST_TAG');
+    if (!defined $tag) {
+        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '-' . get_var('LTP_RELEASE', 'master') . '@' . get_var('MACHINE');
+    }
+    return $tag . '.txt';
 }
 
 # Isolate the loading of LTP tests because they often rely on newer features

lib/main_ltp.pm
@@ -22,7 +22,7 @@ use autotest;
 use utils;
 use LTP::TestInfo qw(testinfo);
 use File::Basename 'basename';
-use main_common qw(load_bootloader_s390x boot_hdd_image);
+use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag);
 use 5.018;
 
 our @EXPORT = 'load_kernel_tests';
@@ -74,7 +74,7 @@ sub parse_runtest_file {
 sub loadtest_from_runtest_file {
     my $name               = get_var('LTP_COMMAND_FILE');
     my $path               = get_var('ASSETDIR') . '/other';
-    my $tag                = (get_var('LTP_RUNTEST_TAG') || basename(get_var('HDD_1'))) . '.txt';
+    my $tag                = get_ltp_tag();
     my $cmd_pattern        = get_var('LTP_COMMAND_PATTERN') || '.*';
     my $cmd_exclude        = get_var('LTP_COMMAND_EXCLUDE') || '$^';
     my $test_result_export = {

tests/kernel/install_ltp.pm
@@ -21,6 +21,7 @@ use power_action_utils 'power_action';
 use bootloader_setup 'add_custom_grub_entries';
 use serial_terminal qw(add_serial_console select_virtio_console);
 use version_utils qw(is_sle sle_version_at_least is_opensuse);
+use main_common 'get_ltp_tag';
 
 sub add_repos {
     my $qa_head_repo = get_required_var('QA_HEAD_REPO');
@@ -248,7 +249,7 @@ EOF
 sub run {
     my $self     = shift;
     my $inst_ltp = get_var 'INSTALL_LTP';
-    my $tag      = (get_var('LTP_RUNTEST_TAG') || basename(get_var('PUBLISH_HDD_1'))) . '.txt';
+    my $tag      = get_ltp_tag();
 
     if ($inst_ltp !~ /(repo|git)/i) {
         die 'INSTALL_LTP must contain "git" or "repo"';

commit 5d4e80ac2a271034e7a96f0c02cc7c9fd94d7f80

LTP: Fix get_ltp_tag() for non-master branches
LTP_RELEASE is defined only for install_ltp.pm. Expecting it's defined
for the tests leads to using wrong tag (=> searching for wrong file)
for non-master branches.

That leads going back to previous version for QEMU based tests:
* PUBLISH_HDD_1 must be used for install_ltp.pm
* HDD_1 must be used for actual tests.

Bare metal tests (IPMI based), which don't use qcow images, need to use
complicated way of tag using DISTRI, VERSION, ... variables (for both
install_ltp.pm and actual tests). This version is not able to
distinguish between different git branches in the same architecture,
that's why it's not used for QEMU based tests (see poo#35041).

Fixes: bca493a1b ("LTP: Adjust tags for bare metal tests")

lib/main_common.pm
@@ -238,8 +238,12 @@ sub is_kernel_test {
 
 sub get_ltp_tag {
     my $tag = get_var('LTP_RUNTEST_TAG');
-    if (!defined $tag) {
-        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '-' . get_var('LTP_RELEASE', 'master') . '@' . get_var('MACHINE');
+    if (!defined $tag && defined get_var('HDD_1')) {
+        $tag = get_var('PUBLISH_HDD_1');
+        $tag = get_var('HDD_1') if (!defined $tag);
+        $tag = basename($tag);
+    } else {
+        $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
     }
     return $tag . '.txt';
 }