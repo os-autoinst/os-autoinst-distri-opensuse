a1008b0743ed25059dcf207bd5284e9c2ce7207a
Martin Loviska <mloviska@suse.com>
2018-09-27 10:03:00 +0200

    Handle select hard disk screen in leap

--- tests/installation/partitioning_togglehome.pm
+++ tests/installation/partitioning_togglehome.pm
@@ -21,7 +21,11 @@ sub run {
     record_soft_failure 'boo#1093372' if (!get_var('TOGGLEHOME') && is_leap('15.1+'));
     send_key $cmd{guidedsetup};
     if (is_storage_ng) {
-        assert_screen 'partition-scheme';
+        assert_screen [qw(existing-partitions partition-scheme)];
+        if (match_has_tag 'existing-partitions') {
+            send_key $cmd{next};
+            assert_screen 'partition-scheme';
+        }
         send_key $cmd{next};
         installation_user_settings::await_password_check if get_var('ENCRYPT');
     }
