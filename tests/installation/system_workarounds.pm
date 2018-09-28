078660148114bf4c50d1ef92678f637d37e200dd
Guillaume GARDET <guillaume.gardet@opensuse.org>
2018-09-28 10:52:37 +0200

    Add installation/system_workarounds to handle specific system workarounds

---
+++ tests/installation/system_workarounds.pm
@@ -0,0 +1,31 @@
+# SUSE's openQA tests
+#
+# Copyright © 2018 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Special handling to get workarounds applied ASAP
+# Maintainer: Guillaume GARDET <guillaume@opensuse.org>
+
+use strict;
+use testapi;
+use base 'opensusebasetest';
+
+sub run {
+    select_console('root-console');
+
+    # boo#1105302 - Disable kernel watchdog on aarch64
+    if (check_var('ARCH', 'aarch64')) {
+        record_info('boo#1105302', "Disable kernel watchdog to avoid test failures due to 'watchdog: BUG: soft lockup - CPU#0 stuck for XXs!'");
+        assert_script_run('echo 0 > /proc/sys/kernel/watchdog_thresh');
+    }
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
