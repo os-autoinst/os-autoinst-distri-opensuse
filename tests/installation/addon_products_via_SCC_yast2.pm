e06294fc897a43d452d3e88a97e9e2adaf474df2
Joaquín Rivera <jeriveramoya@suse.com>
2018-10-24 15:21:23 +0200

    Fix scrolling in yast2 scc

--- tests/installation/addon_products_via_SCC_yast2.pm
+++ tests/installation/addon_products_via_SCC_yast2.pm
@@ -15,26 +15,26 @@ use base qw(y2logsstep y2x11test);
 use strict;
 use testapi;
 use registration 'fill_in_registration_data';
-use version_utils 'sle_version_at_least';
+use version_utils 'is_sle';
 use utils 'turn_off_gnome_screensaver';
 
+=head2 test_setup
+Define proxy SCC. For SLE 15 we need to clean existing registration
+=cut
 sub test_setup {
     select_console 'root-console';
-    my $proxy_scc;
-    if (sle_version_at_least '15') {
-        # Remove registration from the system
-        assert_script_run 'SUSEConnect --clean';
-        # Define proxy SCC
-        assert_script_run 'echo "url: ' . get_var('SCC_URL') . '" > /etc/SUSEConnect';
-    }    # add every used addon to regurl for proxy SCC
+    if (is_sle('>=15')) {
+        assert_script_run 'SUSEConnect --clean';                                          # Remove registration from the system
+        assert_script_run 'echo "url: ' . get_var('SCC_URL') . '" > /etc/SUSEConnect';    # Define proxy SCC
+    }
     elsif (get_var('SCC_ADDONS')) {
+        # Add every used addon to regurl for proxy SCC
         my @addon_proxy = ("url: http://server-" . get_var('BUILD_SLE'));
         for my $addon (split(/,/, get_var('SCC_ADDONS', ''))) {
-            my $uc_addon = uc $addon;    # change to uppercase to match variable
+            my $uc_addon = uc $addon;                                                     # change to uppercase to match variable
             push(@addon_proxy, "\b.$addon-" . get_var("BUILD_$uc_addon"));
         }
-        # Define proxy SCC
-        assert_script_run "echo \"@addon_proxy.proxy.scc.suse.de\" > /etc/SUSEConnect";
+        assert_script_run "echo \"@addon_proxy.proxy.scc.suse.de\" > /etc/SUSEConnect";    # Define proxy SCC
     }
     turn_off_gnome_screensaver;
     select_console 'x11';
@@ -42,7 +42,8 @@ sub test_setup {
 
 sub run {
     my ($self) = @_;
-    test_setup;                          # Define proxy SCC. For SLE 15 we need to clean existing registration.
+
+    test_setup;
     $self->launch_yast2_module_x11('scc', target_match => [qw(scc-registration packagekit-warning)], maximize_window => 1);
     if (match_has_tag 'packagekit-warning') {
         send_key 'alt-y';
