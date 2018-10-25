121ed4e8b227144859f18b4d2e45a116c072567b
Stephan Kulow <stephan@kulow.org>
2018-10-25 07:17:45 +0200

    Revert "Fix assertion of the screen in releasenotes_origin" (#6044)

--- tests/installation/releasenotes_origin.pm
+++ tests/installation/releasenotes_origin.pm
@@ -14,7 +14,7 @@
 use strict;
 use base "y2logsstep";
 use testapi;
-use version_utils ':VERSION';
+use version_utils 'is_sle';
 
 sub run {
     assert_screen('release-notes-button');
@@ -31,9 +31,7 @@ sub run {
         }
     }
     type_string "exit\n";
-    # If we don't have system role screen, release notes origin is verified on partitioning screen
-    my $current_screen = is_using_system_role ? 'system-role-default-system' : 'partitioning-edit-proposal-button';
-    assert_screen $current_screen, 180;
+    assert_screen 'system-role-default-system', 180;
 }
 
 1;
