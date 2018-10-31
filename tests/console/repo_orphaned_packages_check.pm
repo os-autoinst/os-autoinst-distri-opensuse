ebb36ee922b481b17f52ecb8ca7b1693a61d0d4f
Michal Nowak <mnowak@suse.com>
2018-10-31 16:28:37 +0100

    Make sure no -mini package is in the image
    https://bugzilla.suse.com/show_bug.cgi?id=1114175
    
    Approach from poo#32800 was broken, let's check only for -mini packages.
    
    Validation runs:
    * Expected fail: SLES 12-sp4 http://nilgiri.suse.cz/tests/1694#step/repo_orphaned_packages_check/4
    * Expected pass: SLES 15-sp1 http://nilgiri.suse.cz/tests/1695#step/repo_orphaned_packages_check/2
    * Expected fail: Leap 15.0: http://nilgiri.suse.cz/tests/1692#step/repo_orphaned_packages_check/4

--- tests/console/repo_orphaned_packages_check.pm
+++ tests/console/repo_orphaned_packages_check.pm
@@ -7,35 +7,18 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Check for packages orphaned from present repos
+# Summary: *-mini packages must not be delivered in the image
 # Maintainer: Michal Nowak <mnowak@suse.com>
 
 use base 'consoletest';
 use strict;
 use testapi;
-use utils 'zypper_call';
 
 sub run {
     select_console 'root-console';
 
-    # Pipe-separated list of packages we whitelist for orphan checking
-    my $orphan_whitelist = get_var('ORPHAN_WHITELIST', 'gpg-pubkey');
-    zypper_call('ref');
-    my $script = 'ret=0
-for name in $(rpmquery -a --qf "%{NAME}\n" | grep -v -E ^"' . $orphan_whitelist . '"$)
-do
-  if ! zypper --no-refresh info $name | grep -wq ^Repository; then
-    echo "ERROR: $name package is not present in available repos"
-    ret=1
-  fi
-done
-if [ $ret -ne 0 ]; then false; fi
-';
-    my $script_name = 'repo_orphaned_packages_check.sh';
-    save_tmp_file($script_name, $script);
-    assert_script_run('curl -O ' . autoinst_url . "/files/$script_name");
-    assert_script_run("sh $script_name", 600);
-    assert_script_run("rm $script_name");
+    # Check that no *-mini RPM package is present
+    assert_script_run('found="$(rpmquery -a \'*-mini\' \'*-MINI\')"; if [[ "$found" ]]; then echo -e "Found -mini package:\n" $found; false; fi');
 }
 
 1;
