2c633c52f06f38e43b4ef29ec8392f0cd86834a6
Martin Kravec <mkravec@suse.com>
2018-11-06 16:27:30 +0100

    Fix snapshot column number for caasp

--- tests/caasp/transactional_update.pm
+++ tests/caasp/transactional_update.pm
@@ -65,7 +65,7 @@ sub run {
     check_package 'in';
 
     # Find snapshot number for rollback
-    my $f = is_caasp('kubic') ? 1 : 3;
+    my $f = is_caasp('kubic') ? 1 : 2;
     my $snap = script_output "snapper list | tail -1 | cut -d'|' -f$f | tr -d ' *'";
 
     record_info 'Update #1', 'Add repository and update - snapshot #2';
