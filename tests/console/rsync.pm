c664899c57d375e85707126038a051cf25f4c8bd
Ciprian Cret <ccret@suse.com>
2018-11-12 11:07:59 +0100

    Update rsync test to avoid needle mismatch

--- tests/console/rsync.pm
+++ tests/console/rsync.pm
@@ -42,12 +42,9 @@ sub run {
     }
 
     if (is_opensuse) {
-        my $output = script_output('cat /etc/ssh/sshd_config | grep "#PermitRootLogin"');
-        if ($output =~ 'prohibit-password') {
-            clear_console;
-            assert_script_run("sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config");
-            assert_script_run("systemctl restart sshd");
-        }
+        clear_console;
+        assert_script_run("sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config");
+        systemctl 'restart sshd';
     }
 
     type_string("rsync -avzr /tmp/rsync_test_folder_a/ root\@localhost:/tmp/rsync_test_folder_b; echo \$\? > /tmp/rsync_return_code.txt\n");
@@ -83,11 +80,8 @@ sub post_run_hook {
     assert_script_run('rm /tmp/rsync_return_code.txt');
 
     if (is_opensuse) {
-        my $output = script_output('cat /etc/ssh/sshd_config | grep PermitRootLogin');
-        if ($output =~ 'yes') {
-            assert_script_run("sed -i 's/PermitRootLogin yes/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config");
-            assert_script_run("systemctl restart sshd");
-        }
+        assert_script_run("sed -i 's/PermitRootLogin yes/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config");
+        systemctl 'restart sshd';
     }
 }
 
