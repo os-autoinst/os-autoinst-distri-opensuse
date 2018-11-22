3b21ff97d8737eec19d260c3931b47d3b7850b0c
Jozef Pupava <jpupava@suse.com>
2018-11-22 11:56:41 +0100

    Fix bind suite on SLE12SP4
    - extend condition of dependencies to cover SP4
    - enable source pool repositories to get latest bind sources
    - upload log when suite fail

--- tests/console/bind.pm
+++ tests/console/bind.pm
@@ -24,17 +24,23 @@ sub run {
     # script to add missing dependency repos and in second run remove only added products/repos
     assert_script_run 'curl -v -o /tmp/script.sh ' . data_url('qam/handle_bind_source_dependencies.sh');
     assert_script_run 'bash /tmp/script.sh';
-    if (is_sle('<=12-SP3')) {
-        # preinstall libmysqlclient-devel because on 12* are multiple versions and zypper can't decide, perl-IO-Socket-INET6 for reclimit test
-        zypper_call 'in libmysqlclient-devel bind rpm-build perl-IO-Socket-INET6';
+    if (is_sle('<=12-SP4')) {
+        # preinstall libopenssl-devel & libmysqlclient-devel because on 12* are multiple versions and zypper can't decide,
+        # perl-IO-Socket-INET6 for reclimit test
+        zypper_call 'in libopenssl-devel libmysqlclient-devel bind rpm-build perl-IO-Socket-INET6';
     }
     elsif (is_sle('>=15')) {
-        # bind-utils for dig, net-tools-deprecated for ifconfig, perl-IO-Socket-INET6 for reclimit, perl-Net-DNS for xfer, dnspython for chain test
+        # bind-utils for dig, net-tools-deprecated for ifconfig, perl-IO-Socket-INET6 for reclimit,
+        # perl-Net-DNS for xfer, dnspython for chain test
         zypper_call 'in bind rpm-build bind-utils net-tools-deprecated perl-IO-Socket-INET6 perl-Socket6 perl-Net-DNS python3-dnspython';
     }
+    # enable source repositories to get latest source packages
+    assert_script_run 'for r in `zypper lr|awk \'/Source-Pool/ {print$5}\'`;do zypper mr -e -f $r;done';
     # install bind sources to build and run testsuite
     zypper_call 'si bind';
     assert_script_run 'rpm -q bind';
+    # disable previously enabled source repositories
+    assert_script_run 'for r in `zypper lr|awk \'/Source-Pool/ {print$5}\'`;do zypper mr -d -F $r;done';
     assert_script_run 'cd /usr/src/packages';
     # build the bind package with tests
     assert_script_run 'rpmbuild -bc SPECS/bind.spec', 500;
@@ -71,6 +77,7 @@ sub post_run_hook {
 sub post_fail_hook {
     # print out what tests failed
     assert_script_run 'egrep "^A|^R" systests.output|grep -B1 FAIL';
+    upload_logs 'systests.output';
 }
 
 1;
