a9c98cc7cc2bae3950d5af000cdbb960a2db917b
Anton Smorodskyi <asmorodskyi@suse.com>
2018-12-12 14:03:37 +0100

    wicked: Add vlan_ifdown_modify_one_config scenario

--- tests/wicked/startandstop/sut/t11_vlan_ifdown_modify_one_config.pm
+++ tests/wicked/startandstop/sut/t11_vlan_ifdown_modify_one_config.pm
@@ -17,22 +17,24 @@ use strict;
 use testapi;
 use network_utils qw(iface ifc_exists);
 
+
 sub run {
     my ($self) = @_;
     my $iface  = iface();
     my $config = "/etc/sysconfig/network/ifcfg-$iface.42";
-    $self->get_from_data('wicked/ifcfg/eth0.42', $config);
-    my $local_ip = $self->get_ip(type => 'vlan', netmask => 1);
+    my $local_ip = $self->get_ip(type => 'vlan_changed', netmask => 1);
     $local_ip =~ s'/'\\/';
-    assert_script_run("sed 's/ip_address/$local_ip/' -i $config");
-    assert_script_run("sed 's/interface/$iface/' -i $config");
+    my $previous_ip = $self->get_ip(type => 'vlan', netmask => 1);
+    $previous_ip =~ s'/'\\/';
+    assert_script_run("sed 's/$previous_ip/$local_ip/' -i $config");
     script_run("cat $config");
-    $self->wicked_command('ifup', 'all');
+    $self->wicked_command('ifreload', 'all');
     assert_script_run('ip a');
-    die if (!ifc_exists($iface . '.42'));
-    $self->wicked_command('ifdown', "$iface.42");
-    die if (ifc_exists($iface . '.42'));
-    die if (!ifc_exists($iface));
+    die('VLAN interface does not exists') if (!ifc_exists($iface . '.42'));
+    die('IP is unreachable')
+      if (!$self->ping_with_timeout(ip => $self->get_remote_ip(type => 'vlan_changed'), timeout => '5'));
+    $self->wicked_command('ifdown', "all");
+    die('VLAN interface exists') if (ifc_exists($iface . '.42'));
 }
 
 
