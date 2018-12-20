ded6b64958f6ce5f2957107413c8f66f6d1f1fc9
Jozef Pupava <jpupava@suse.com>
2018-12-20 12:43:37 +0100

    Adapt for IPMI backend and cleanup
    - removed unused packages and specified used packages
    - declare variable at beginning when used in whole file
    - removed $testapi::serialdev = get_var('SERIALDEV') if get_var('SERIALDEV');
      same is done in testapi, not needed here
    - expecting (sshd|vnc)-server-started on IPMI
    - added PXE_ENTRY for Prague PXE to use entry, previously used with SES5_DEPLOY
    - changed a bit Prague regex to be able to run also developed (non-GM) products

--- tests/boot/boot_from_pxe.pm
+++ tests/boot/boot_from_pxe.pm
@@ -14,17 +14,18 @@ use base 'opensusebasetest';
 
 use strict;
 use warnings;
-use bootloader_setup;
-use File::Basename;
 use lockapi;
-use registration;
 use testapi;
+use bootloader_setup qw(bootmenu_default_params specific_bootmenu_params);
+use registration 'registration_bootloader_cmdline';
 use utils 'type_string_slow';
 
 sub run {
+    my ($image_path, $image_name, $cmdline);
+    my $arch = get_var('ARCH');
     my $interface = get_var('SUT_NETDEVICE', 'eth0');
     # In autoyast tests we need to wait until pxe is available
-    if (get_var('AUTOYAST') && get_var('DELAYED_START')) {
+    if (get_var('AUTOYAST') && get_var('DELAYED_START') && !check_var('BACKEND', 'ipmi')) {
         mutex_lock('pxe');
         mutex_unlock('pxe');
         resume_vm();
@@ -32,7 +33,6 @@ sub run {
     if (check_var('BACKEND', 'ipmi')) {
         select_console 'sol', await_console => 0;
     }
-    my $image_path = "";
     assert_screen([qw(pxe-menu-bei pxe-menu-nue pxe-menu-prg pxe-menu)], 300);
     #detect pxe location
     if (match_has_tag('pxe-menu-bei')) {    # BeiJing
@@ -48,7 +48,6 @@ sub run {
             $image_name = get_var("REPO_0");
         }
 
-        my $arch       = get_var('ARCH');
         my $path       = "/mnt/openqa/repo/${image_name}/boot/${arch}/loader";
         my $openqa_url = get_required_var('OPENQA_URL');
         $openqa_url = 'http://' . $openqa_url unless $openqa_url =~ /http:\/\//;
@@ -58,23 +57,21 @@ sub run {
         }
         $image_path = "$path/linux initrd=$path/initrd install=$repo ";
     }
-        send_key_until_needlematch 'pxe-stable-iso-entry', 'down';
-        send_key 'ret';
-        send_key_until_needlematch 'pxe-sle-12-sp3-entry', 'down';
-        send_key 'tab';
-        my $node = get_var('WORKER_CLASS');
-    }
-        # Fix problem with sol on ttyS2
-        $testapi::serialdev = get_var('SERIALDEV') if get_var('SERIALDEV');
-
-        my $arch = get_var('ARCH');
-        my ($image_name) = get_var('ISO') =~ s/^.*?([^\/]+)-DVD-${arch}-([^-]+)-DVD1\.iso/$1-$2/r;
-        $image_path .= "/mounts/dist/install/SLP/${image_name}/${arch}/DVD1/boot/${arch}/loader/linux ";
-        $image_path .= "initrd=/mounts/dist/install/SLP/${image_name}/${arch}/DVD1/boot/${arch}/loader/initrd ";
     elsif (match_has_tag('pxe-menu-prg')) {      # Prague
         send_key_until_needlematch 'pxe-edit-prompt', 'esc', 8, 3;
+        if (get_var('PXE_ENTRY')) {
+            my $entry = get_var('PXE_ENTRY');
+            send_key_until_needlematch "pxe-$entry-entry", 'down';
+            send_key 'tab';
+        }
+        else {
             my $device = check_var('BACKEND', 'ipmi') ? "?device=$interface" : '';
+            my $release = get_var('BETA') ? 'LATEST' : 'GM';
+            $image_name = get_var('ISO') =~ s/.*\/(.*)-DVD-${arch}-.*\.iso/$1-$release/r;
+            $image_path = "/mounts/dist/install/SLP/${image_name}/${arch}/DVD1/boot/${arch}/loader/linux ";
+            $image_path .= "initrd=/mounts/dist/install/SLP/${image_name}/${arch}/DVD1/boot/${arch}/loader/initrd ";
             $image_path .= "install=http://mirror.suse.cz/install/SLP/${image_name}/${arch}/DVD1$device ";
+        }
     }
     elsif (match_has_tag('pxe-menu')) {
         # select network (second entry)
@@ -109,9 +106,8 @@ sub run {
     send_key 'ret';
     save_screenshot;
 
-    if ((check_var('BACKEND', 'ipmi') && !get_var('AUTOYAST')) || get_var('SES5_DEPLOY')) {
-        my $ssh_vnc_wait_time = 600;
-        assert_screen((check_var('VIDEOMODE', 'text') ? 'sshd' : 'vnc') . '-server-started', $ssh_vnc_wait_time);
+    if (check_var('BACKEND', 'ipmi')) {
+        assert_screen((check_var('VIDEOMODE', 'text') ? 'sshd' : 'vnc') . '-server-started', 600);
         select_console 'installation';
 
         # We have textmode installation via ssh and the default vnc installation so far
