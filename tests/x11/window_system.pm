96adb9eb4b75bb2f714619c99d468d329d13d775
Jan Baier <jbaier@suse.cz>
2019-01-03 14:15:27 +0100

    Make window system test more robust

--- tests/x11/window_system.pm
+++ tests/x11/window_system.pm
@@ -4,21 +4,30 @@
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
+# notice and this notice are preserved. This file is offered as-is,
 # without any warranty.
 
-# Summary: Information about current window system
+# Summary: Show information about current session (window system)
 # Maintainer: Jan Baier <jbaier@suse.cz>
 
-use base "x11test";
+use base "opensusebasetest";
 use strict;
+use warnings;
 use testapi;
 
 sub run {
-    x11_start_program('xterm');
-    my $window_system = script_output('echo $XDG_SESSION_TYPE');
-    script_run('exit', 0);
-    record_info("$window_system", "Current window system is $window_system");
+    my $self = shift;
+    $self->select_serial_terminal;
+
+    my $session_type = script_output "loginctl show-session \$(loginctl list-sessions | awk '/$testapi::username/ {print \$1};') -p Type | cut -f2 -d=";
+
+    if ($session_type) {
+        record_info("$session_type", "Current session type is $session_type");
+    } else {
+        record_soft_fail("Session type is not defined");
+    }
+
+    select_console 'x11' unless check_var('DESKTOP', 'textmode');
 }
 
 1;
