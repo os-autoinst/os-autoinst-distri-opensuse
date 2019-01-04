c4c9a1f60e861a09c64af2569af0810a346eca51
Anton Smorodskyi <asmorodskyi@suse.com>
2019-01-04 13:24:29 +0100

    wicked: Add VLAN xml config test case

--- tests/wicked/startandstop/ref/t12_vlan_xml_config.pm
+++ tests/wicked/startandstop/ref/t12_vlan_xml_config.pm
@@ -1,13 +1,13 @@
 # SUSE's openQA tests
 #
-# Copyright © 2018-2019 SUSE LLC
+# Copyright © 2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: VLAN - ifdown, modify one config, ifreload, ifdown, ifup
+# Summary: VLAN - Create a VLAN from wicked XML files
 # Maintainer: Anton Smorodskyi <asmorodskyi@suse.com>
 #             Jose Lausuch <jalausuch@suse.com>
 #             Clemens Famulla-Conrad <cfamullaconrad@suse.de>
@@ -17,13 +17,11 @@ use strict;
 use testapi;
 use network_utils 'iface';
 
+
 sub run {
     my ($self) = @_;
-    record_info('Info', 'VLAN - ifdown, modify one config, ifreload, ifdown, ifup');
-    $self->setup_vlan('vlan_changed');
+    record_info('Info', 'VLAN - Create a VLAN from wicked XML files');
+    $self->setup_vlan('vlan');
 }
 
-sub test_flags {
-    return {always_rollback => 1};
-}
 1;
