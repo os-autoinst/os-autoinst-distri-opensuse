234fd94d70bac8c00f11ffe262a90e522f4a2c08
GraceWang <gwang@suse.com>
2019-01-25 15:49:25 +0800

    Log out root user in window_system.pm (poo#46655)

--- tests/x11/window_system.pm
+++ tests/x11/window_system.pm
@@ -30,6 +30,8 @@ sub run {
         record_soft_fail("Session type is not defined");
     }
 
+    type_string "exit\n";    # logout
+
     select_console 'x11' unless check_var('DESKTOP', 'textmode');
 }
 
