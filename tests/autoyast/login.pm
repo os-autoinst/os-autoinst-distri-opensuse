66fef8a2024cc916a79602f387fda1029c9716a7
Oliver Kurz <okurz@suse.de>
2019-01-25 11:49:00 +0100

    autoyast: Take maintainership for "login"
    psladek was not contributing in past years and is not reacting to tickets.

--- tests/autoyast/login.pm
+++ tests/autoyast/login.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2018 SUSE LLC
+# Copyright (C) 2015-2019 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
 # Summary: Log into system installed with autoyast
-# Maintainer: Pavel Sladek <psladek@suse.cz>
+# Maintainer: Oliver Kurz <okurz@suse.de>
 
 use strict;
 use base 'y2logsstep';
