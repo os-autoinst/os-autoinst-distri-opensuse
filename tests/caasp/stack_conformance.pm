8aaa32d23b7bb31f27c2a055405389e7e9eafec4
Martin Kravec <mkravec@suse.com>
2019-02-01 11:21:28 +0100

    Fix conformance tests for CaaSP 3.0

--- tests/caasp/stack_conformance.pm
+++ tests/caasp/stack_conformance.pm
@@ -28,24 +28,24 @@ my $config_json = <<'EOF';
 EOF
 
 sub run {
-    # The repository for the sonobuoy package
-    my $repo = get_var('SONOBUOY_REPO', 'https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo');
-
     my $json_name = "sonobuoy.json";
     my $logs_dir  = "sonobuoy_logs";
 
     my $sb_pass = '"SUCCESS! -- [1-9][0-9]\+ Passed | 0 Failed | 0 Pending.*PASS"';
     my $sb_test = '"Test Suite Passed"';
 
+    # Install sonobuoy cli
     switch_to 'xterm';
     become_root;
-
-    # Add the repo
-    zypper_call " ar -Gf $repo";
-
-    # Install the sonobuoy package on the controller node
-    zypper_call "-n in sonobuoy";
-
+    if (is_caasp '=3.0') {
+        assert_script_run 'curl -O ' . data_url("caasp/sonobuoy_0.12.1_linux_amd64.tar.gz");
+        assert_script_run 'tar -xzf sonobuoy_0.12.1_linux_amd64.tar.gz';
+        assert_script_run 'mv sonobuoy /usr/bin/';
+    } else {
+        my $repo = get_var('SONOBUOY_REPO', 'https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo');
+        zypper_call "ar -Gf $repo";
+        zypper_call 'in sonobuoy';
+    }
     type_string "exit\n";
 
     # Create config file
@@ -66,8 +66,6 @@ sub run {
     upload_logs 'plugins/e2e/results/e2e.log';
     assert_script_run "tail -10 plugins/e2e/results/e2e.log | tee /dev/tty | grep $sb_pass";
     assert_script_run "tail -10 plugins/e2e/results/e2e.log | grep $sb_test";
-
-    switch_to 'velum';
 }
 
 1;
