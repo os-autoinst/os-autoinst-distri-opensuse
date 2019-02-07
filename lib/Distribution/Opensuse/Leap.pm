d42262c9fcdaad3342d8b05919d7f3254b44167f
Oleksandr Orlov <oorlov@suse.de>
2019-02-07 10:22:34 +0100

    Add factory to provide distributions
    The commit adds Factory to provide distributions depending on their
    properties (like 'DISTRI', 'VERSION'). That allows to use the
    distributions in test modules without explicitly defining them.

---
+++ lib/Distribution/Opensuse/Leap.pm
@@ -0,0 +1,23 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: The class represents Leap distribution and provides access to its
+# features.
+# Follows the "Factory first" rule. So that the feature first appears in
+# Tumbleweed distribution, and only if it behaves different in Leap then it
+# should be overriden here.
+
+# Maintainer: Oleksandr Orlov <oorlov@suse.de>
+
+package Distribution::Opensuse::Leap;
+use strict;
+use warnings FATAL => 'all';
+use parent 'Distribution::Opensuse::Tumbleweed';
+
+1;
---
+++ lib/DistributionProvider.pm
@@ -0,0 +1,41 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: A library that provides the certain distribution depending on the
+# version of the product that is specified for a Test Suite.
+
+# Maintainer: Oleksandr Orlov <oorlov@suse.de>
+
+package DistributionProvider;
+use strict;
+use warnings FATAL => 'all';
+use version_utils;
+
+use Distribution::Sle::15;
+use Distribution::Sle::12;
+use Distribution::Opensuse::Leap;
+use Distribution::Opensuse::Tumbleweed;
+
+=head2 provide
+
+  provide();
+
+Returns the certain distribution depending on the version of the product.
+
+If there is no matched version, then returns Tumbleweed as the default one.
+
+=cut
+sub provide {
+    return Distribution::Sle::15->new()        if version_utils::is_sle('15+');
+    return Distribution::Sle::12->new()        if version_utils::is_sle('12+');
+    return Distribution::Opensuse::Leap->new() if version_utils::is_leap();
+    return Distribution::Opensuse::Tumbleweed->new();
+}
+
+1;
