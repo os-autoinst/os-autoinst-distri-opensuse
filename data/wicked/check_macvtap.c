6c2f85f659ef87a6ecc33346cfcfc98e25588618
Anton Smorodskyi <asmorodskyi@suse.com>
2019-02-19 14:19:46 +0100

    wicked: Add legacy macvtap test case

---
+++ data/wicked/check_macvtap.c
@@ -0,0 +1,128 @@
+// Small program to test macvtap interfaces
+// It assumes that  MAC=0E:0E:0E:0E:0E:0E
+// the following command has been issued: arping -c 1 -I macvtap1 <destination
+// IP> It then listens on /dev/tapX to check whether the correct answer was
+// received.
+
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PACKET_SIZE 70
+#define SOURCE_IP_POS_IN_ARRAY 38
+#define DEST_IP_POS_IN_ARRAY 48
+#define FILENAME_LENGTH 16
+// Expected ARP packet
+// -1 stands for unknown data
+unsigned char
+    arp_packet[PACKET_SIZE] =
+        {0x00, 0x00, 0x00, 0x00, 0x00, // 10 bytes preamble
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x0e,
+         0x0e, 0x0e, 0x0e, 0x0e, 0x0e,       // destination MAC address
+         -1,   -1,   -1,   -1,   -1,   -1,   // source MAC address
+         0x08, 0x06,                         // ARP protocol
+         0x00, 0x01, 0x08, 0x00, 0x06, 0x04, // Ethernet <=> IP
+         0x00, 0x02,                         // "is at" answer
+         -1,   -1,   -1,   -1,   -1,   -1,   // source MAC address
+         -2,   -2,   -2,   -2, // source IP address (replaced with 1st input
+                               // param )
+         0x0e, 0x0e, 0x0e, 0x0e, 0x0e, 0x0e, // destination MAC address
+         -2,   -2,   -2,   -2, // destination IP address ( replaced with 2nd
+                               // input param)
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 18 bytes postamble
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+// macvtap file name
+char fn[FILENAME_LENGTH];
+
+// Read buffer
+unsigned char buffer[PACKET_SIZE];
+
+// Search for /dev/tapX
+void search_tap() {
+  char *hint = "/sys/class/net/macvtap1/ifindex";
+  FILE *fp = fopen(hint, "r");
+  int index;
+
+  if (fp == NULL) {
+    printf("Can't open %s\n", hint);
+    exit(-1);
+  }
+  fscanf(fp, "%d", &index);
+  fclose(fp);
+
+  snprintf(fn, 15, "/dev/tap%d", index);
+  fn[15] = '\0';
+  printf("Type device was found - %s \n", fn);
+}
+
+// Read 70 bytes from the tap device
+void read_packet() {
+  int fd;
+
+  fd = open(fn, O_RDONLY);
+  if (fd == -1) {
+    printf("Can't open tap device %s\n", fn);
+    exit(-2);
+  }
+  if (read(fd, &buffer, PACKET_SIZE) < 0) {
+    printf("Error reading %s\n", fn);
+    close(fd);
+    exit(-3);
+  } else {
+    printf("ARP packet catched \n");
+  }
+  close(fd);
+}
+
+void analyze_packet() {
+  unsigned char *pbuffer;
+  unsigned char *ppacket;
+  int differ = 0;
+
+  for (ppacket = arp_packet, pbuffer = buffer;
+       ppacket < arp_packet + PACKET_SIZE; ppacket++, pbuffer++) {
+    if (*ppacket != 255) {
+      if (*pbuffer != *ppacket) {
+        printf("arp[%u] buffer[%u] DIFFERS!\n", *ppacket, *pbuffer);
+        differ = 1;
+      } else {
+        printf("arp[%u] buffer[%u] \n", *ppacket, *pbuffer);
+      }
+    }
+  }
+  if (differ == 1) {
+    exit(-4);
+  } else {
+    printf("Success listening to tap device %s, received the expected ARP "
+           "packet\n",
+           fn);
+  }
+}
+
+// Main program
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("No input parameters. Exit \n");
+    exit(-5);
+  }
+  // getting source and destination ips from command line and inserting them
+  // into dedicated places in packet
+  struct in_addr *source_ip =
+      (struct in_addr *)&arp_packet[SOURCE_IP_POS_IN_ARRAY];
+  inet_pton(AF_INET, argv[1], source_ip);
+  struct in_addr *dest_ip = (struct in_addr *)&arp_packet[DEST_IP_POS_IN_ARRAY];
+  inet_pton(AF_INET, argv[2], dest_ip);
+  search_tap();
+  read_packet();
+  analyze_packet();
+
+  return 0;
+}
---
+++ tests/wicked/advanced/sut/t15_macvtap_legacy.pm
@@ -0,0 +1,62 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Advanced test cases for wicked
+# Test 15: Create a macvtap interface from legacy ifcfg files
+# Maintainer: Anton Smorodskyi <asmorodskyi@suse.com>
+#             Jose Lausuch <jalausuch@suse.com>
+#             Clemens Famulla-Conrad <cfamullaconrad@suse.de>
+
+use base 'wickedbase';
+use strict;
+use warnings;
+use testapi;
+use network_utils 'iface';
+use utils 'zypper_call';
+
+our $macvtap_log = '/tmp/macvtap_results.txt';
+
+sub run {
+    my ($self) = @_;
+    record_info('Info', 'Create a macvtap interface from legacy ifcfg files');
+    my $config     = '/etc/sysconfig/network/ifcfg-macvtap1';
+    my $iface      = iface();
+    my $ip_address = $self->get_ip(type => 'macvtap', netmask => 1);
+    $ip_address =~ s'/'\\/';
+    my $ref_ip = $self->get_ip(type => 'host', netmask => 0, is_wicked_ref => 1);
+    $self->get_from_data('wicked/ifcfg/macvtap1',    $config);
+    $self->get_from_data('wicked/ifcfg/macvtap_eth', '/etc/sysconfig/network/ifcfg-' . $iface);
+    $self->get_from_data('wicked/check_macvtap.c',   'check_macvtap.c', executable => 1);
+    zypper_call('in gcc');
+    assert_script_run('gcc ./check_macvtap.c -o check_macvtap');
+    script_run('chmod +x ./check_macvtap');
+    assert_script_run("sed 's/iface/$iface/' -i $config");
+    assert_script_run("sed 's/ip_address/$ip_address/' -i $config");
+    $self->wicked_command('ifreload', $iface);
+    $self->wicked_command('ifup',     'macvtap1');
+    $ip_address = $self->get_ip(type => 'macvtap', netmask => 0);
+    my $cmd_text = "./check_macvtap $ref_ip $ip_address > $macvtap_log 2>&1 &";
+    type_string($cmd_text);
+    wait_serial($cmd_text, undef, 0, no_regex => 1);
+    type_string("\n");
+    # arping not getting packet back it is expected because check_macvtap
+    # executable is consume it from tap device before it actually reaches arping
+    script_run("arping -c 1 -I macvtap1 $ref_ip");
+    validate_script_output("cat $macvtap_log", sub { m/Success listening to tap device/ });
+    upload_logs($macvtap_log);
+}
+
+sub post_fail_hook {
+    my ($self) = shift;
+    select_console('log-console');
+    upload_logs($macvtap_log);
+    $self->SUPER::post_fail_hook;
+}
+
+1;
