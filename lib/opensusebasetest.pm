os-autoinst/os-autoinst-distri-opensuse

commit a42ed584bd400b0a37b6cb6df01b124cbb222edc

Boot from local disk on installation on aarch64

lib/opensusebasetest.pm
@@ -365,24 +365,7 @@ sub wait_grub {
         }
     }
     elsif (match_has_tag('inst-bootmenu')) {
-        # assuming the cursor is on 'installation' by default and 'boot from
-        # harddisk' is above
-        send_key_until_needlematch 'inst-bootmenu-boot-harddisk', 'up';
-        boot_local_disk;
-
-        my @tags = qw(grub2 tianocore-mainmenu);
-        push @tags, 'encrypted-disk-password-prompt' if (get_var('ENCRYPT'));
-
-        check_screen(\@tags, 15)
-          || die 'neither grub2 nor tianocore-mainmenu needles found';
-        if (match_has_tag('tianocore-mainmenu')) {
-            $self->handle_uefi_boot_disk_workaround();
-            check_screen('encrypted-disk-password-prompt', 10);
-        }
-        if (match_has_tag('encrypted-disk-password-prompt')) {
-            workaround_type_encrypted_passphrase;
-            assert_screen('grub2');
-        }
+        $self->wait_boot_on_local_disk;
     }
     elsif (match_has_tag('encrypted-disk-password-prompt')) {
         # unlock encrypted disk before grub
@@ -396,6 +379,31 @@ sub wait_grub {
         die "needle '$failneedle' not found";
     }
     mutex_wait 'support_server_ready' if get_var('USE_SUPPORT_SERVER');
+}
+
+=head2 wait_grub_to_boot_on_local_disk
+
+  wait_grub_to_boot_on_local_disk
+
+When bootloader appears, make sure to boot from local disk when it is on aarch64. 
+=cut
+sub wait_grub_to_boot_on_local_disk {
+   # assuming the cursor is on 'installation' by default and 'boot from
+   # harddisk' is above
+   send_key_until_needlematch 'inst-bootmenu-boot-harddisk', 'up';
+   boot_local_disk;
+   my @tags = qw(grub2 tianocore-mainmenu);
+   push @tags, 'encrypted-disk-password-prompt' if (get_var('ENCRYPT'));
+
+   assert_screen(\@tags, 15);
+   if (match_has_tag('tianocore-mainmenu')) {
+       opensusebasetest::handle_uefi_boot_disk_workaround();
+       check_screen('encrypted-disk-password-prompt', 10);
+    }
+   if (match_has_tag('encrypted-disk-password-prompt')) {
+       workaround_type_encrypted_passphrase;
+       assert_screen('grub2');
+   }
 }
 
 =head2 wait_boot

tests/autoyast/installation.pm
@@ -128,6 +128,8 @@ sub run {
         push(@needles, 'autoyast-license');
     }
 
+    # Push needle 'inst-bootmenu' to ensure boot from hard disk on aarch64
+    push(@needles, 'inst-bootmenu') if (check_var('ARCH', 'aarch64') && get_var('UPGRADE'));
     # Kill ssh proactively before reboot to avoid half-open issue on zVM, do not need this on zKVM
     prepare_system_shutdown if check_var('BACKEND', 's390x');
     my $postpartscript = 0;
@@ -142,7 +144,8 @@ sub run {
     until (match_has_tag('reboot-after-installation')
           || match_has_tag('bios-boot')
           || match_has_tag('autoyast-stage1-reboot-upcoming')
-          || match_has_tag('linux-login-casp'))
+          || match_has_tag('linux-login-casp')
+          || match_has_tag('inst-bootmenu'))
     {
         #Verify timeout and continue if there was a match
         next unless verify_timeout_and_check_screen(($timer += $check_time), \@needles);
@@ -256,7 +259,7 @@ sub run {
     $stage   = 'stage2';
 
     check_screen \@needles, $check_time;
-    @needles = qw(reboot-after-installation autoyast-postinstall-error autoyast-boot warning-pop-up autoyast-error);
+    @needles = qw(reboot-after-installation autoyast-postinstall-error autoyast-boot warning-pop-up autoyast-error inst-bootmenu);
     # There will be another reboot for IPMI backend
     push @needles, qw(prague-pxe-menu qa-net-selection) if check_var('BACKEND', 'ipmi');
     until (match_has_tag 'reboot-after-installation') {
@@ -279,6 +282,9 @@ sub run {
         }
         elsif (match_has_tag('autoyast-error')) {
             die 'Error detected during second stage of the installation';
+        }
+        elsif (match_has_tag('inst-bootmenu')) {
+            $self->wait_grub_to_boot_on_local_disk;
         }
     }
 

commit eb8c53550bbb3d33314bd6fae7abd8bef91c3cd4

Replace wait_boot_on_local_disk
Regression from a42ed584bd400b0a37b6cb6df01b124cbb222edc.

Fails here:
* https://openqa.suse.de/tests/2478650#step/boot_to_desktop/3
* https://openqa.suse.de/tests/overview?distri=sle&version=15-SP1&build=173.1&groupid=111

Validation run: http://nilgiri.suse.cz/tests/300

lib/opensusebasetest.pm
@@ -365,7 +365,7 @@ sub wait_grub {
         }
     }
     elsif (match_has_tag('inst-bootmenu')) {
-        $self->wait_boot_on_local_disk;
+        $self->wait_grub_to_boot_on_local_disk;
     }
     elsif (match_has_tag('encrypted-disk-password-prompt')) {
         # unlock encrypted disk before grub