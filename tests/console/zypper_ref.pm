5cf902109d76bd2b46a3d4ef6f5557a7fff58e96
Santiago Zarate <santiago.zarate@suse.com>
2019-02-20 11:32:34 +0100

    Enable repos when install medium are dvds

--- tests/console/zypper_ref.pm
+++ tests/console/zypper_ref.pm
@@ -1,14 +1,15 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Ensure zypper can refresh repos
+# Summary: Ensure zypper can refresh repos and enable them if the install
+# medium used was a dvd
 # Maintainer: Oliver Kurz <okurz@suse.de>
 
 use base "consoletest";
@@ -18,6 +19,12 @@ use utils 'zypper_call';
 
 sub run {
     select_console 'root-console';
+
+    # see FATE#325541
+    zypper_call 'mr -e -l'
+        if is_sle('15+')
+        and get_var('ISO_1', '') =~ /SLE-.*-Packages-.*\.iso/;
+
     zypper_call '--gpg-auto-import-keys ref';
 }
 
