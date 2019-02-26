c3e1eb97033b3be5d7a8341325db7edd0cf04b4f
Oleksandr Orlov <oorlov@suse.de>
2019-02-26 10:09:13 +0100

    Add test modules for toggling separate home
    The commit adds test modules that explicitly enable/disable separate
    /home.

---
+++ tests/installation/partitioning/lvm_no_separate_home.pm
@@ -0,0 +1,23 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: The test module creates a partition with LVM and explicitly disables
+# separate /home partition.
+# Maintainer: Oleksandr Orlov <oorlov@suse.de>
+
+use strict;
+use warnings FATAL => 'all';
+use parent "installbasetest";
+
+sub run {
+    my $partitioner = $testapi::distri->get_partitioner();
+    $partitioner->edit_proposal(is_lvm => 1, has_separate_home => 0);
+}
+
+1;
