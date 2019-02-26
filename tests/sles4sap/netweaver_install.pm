79228bf7892a6bc666c9259c32b13bd70e04d67b
Oliver Kurz <okurz@suse.de>
2019-02-26 21:48:39 +0100

    Align netweaver_install to fixed tidy checks

--- tests/sles4sap/netweaver_install.pm
+++ tests/sles4sap/netweaver_install.pm
@@ -175,18 +175,18 @@ sub run {
 }
 
 {
-no warnings 'redefine';
-sub post_fail_hook {
-    my $self = shift;
+    no warnings 'redefine';
+    sub post_fail_hook {
+        my $self = shift;
 
-    $self->export_logs();
-    upload_logs "/tmp/check-nw-media";
-    $self->save_and_upload_log('ls -alF /sapinst/unattended', '/tmp/nw_unattended_ls.log');
-    $self->save_and_upload_log('ls -alF /sbin/mount*',        '/tmp/sbin_mount_ls.log');
-    upload_logs "/sapinst/unattended/sapinst.log";
-    upload_logs "/sapinst/unattended/sapinst_dev.log";
-    upload_logs "/sapinst/unattended/start_dir.cd";
-}
+        $self->export_logs();
+        upload_logs "/tmp/check-nw-media";
+        $self->save_and_upload_log('ls -alF /sapinst/unattended', '/tmp/nw_unattended_ls.log');
+        $self->save_and_upload_log('ls -alF /sbin/mount*',        '/tmp/sbin_mount_ls.log');
+        upload_logs "/sapinst/unattended/sapinst.log";
+        upload_logs "/sapinst/unattended/sapinst_dev.log";
+        upload_logs "/sapinst/unattended/start_dir.cd";
+    }
 }
 
 1;
