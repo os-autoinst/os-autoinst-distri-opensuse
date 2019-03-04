ae71c0f45c213926b2cde3fb19d4bd98ebd0b970
Oliver Kurz <okurz@suse.de>
2019-03-04 08:56:00 +0100

    Revert "In patch_sle we should wait a while to get the password."

--- lib/opensusebasetest.pm
+++ lib/opensusebasetest.pm
@@ -483,8 +483,6 @@ sub wait_boot {
             type_line_svirt '', expect => $login_ready, timeout => $ready_time + 100, fail_message => 'Could not find login prompt';
             type_line_svirt "root", expect => 'Password';
             type_line_svirt "$testapi::password";
-            # we need wait a while before we got the password, poo#45515
-            sleep 10;
             type_line_svirt "systemctl is-active network", expect => 'active';
             type_line_svirt 'systemctl is-active sshd',    expect => 'active';
 
