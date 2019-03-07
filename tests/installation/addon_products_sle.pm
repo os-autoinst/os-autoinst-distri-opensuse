6b9ff8bff64b5b127011f1ffd752b4c0bba77d9d
Yutao wang <yuwang@suse.com>
2019-03-07 14:39:47 +0800

    Add python2 module for media upgrade

--- tests/installation/addon_products_sle.pm
+++ tests/installation/addon_products_sle.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -42,6 +42,9 @@ sub handle_all_packages_medium {
     # Refer to https://bugzilla.suse.com/show_bug.cgi?id=1078958#c4
     push @addons, 'we' if check_var('SLE_PRODUCT', 'sled') && !grep(/^we$/, @addons);
 
+    # Add python2 module, refer to https://jira.suse.de/browse/SLE-3167
+    push @addons, 'python2' if get_var('MEDIA_UPGRADE') && is_sle('<15', get_var('HDDVERSION')) && !check_var('SLE_PRODUCT', 'rt');
+
     # For SLES12SPx and SLES11SPx to SLES15 migration, need add the demand module at least for media migration manually
     # Refer to https://fate.suse.com/325293
     if (get_var('MEDIA_UPGRADE') && is_sle('<15', get_var('HDDVERSION')) && !check_var('SLE_PRODUCT', 'sled')) {
@@ -64,6 +67,11 @@ sub handle_all_packages_medium {
     # there will be problem to enable the same addon twice
     for my $a (split(/,/, get_var('SCC_ADDONS', ''))) {
         push @addons, $a if !grep(/^$a$/, @addons);
+    }
+
+    # Add python2 module, refer to https://jira.suse.de/browse/SLE-3167
+    if (get_var('MEDIA_UPGRADE') && check_var('HDDVERSION', '15') && grep(/^lgm$|^legacy$/, @addons)) {
+        push @addons, 'python2' if !grep(/^python2$/, @addons);
     }
 
     # Record the addons to be enabled for debugging
