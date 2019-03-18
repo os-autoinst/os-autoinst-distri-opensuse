a72982a70af39521d36bdaf87a54f89312a4c9a7
Anton Smorodskyi <asmorodskyi@suse.com>
2019-03-18 16:18:55 +0100

    wicked: Create macvtap test with XML config

--- tests/wicked/advanced/sut/t16_macvtap_xml.pm
+++ tests/wicked/advanced/sut/t16_macvtap_xml.pm
@@ -8,7 +8,7 @@
 # without any warranty.
 
 # Summary: Advanced test cases for wicked
-# Test 15: Create a macvtap interface from legacy ifcfg files
+# Test 16: Create a macvtap interface from wicked XML files
 # Maintainer: Anton Smorodskyi <asmorodskyi@suse.com>
 #             Jose Lausuch <jalausuch@suse.com>
 #             Clemens Famulla-Conrad <cfamullaconrad@suse.de>
@@ -17,26 +17,22 @@ use base 'wickedbase';
 use strict;
 use warnings;
 use testapi;
+use utils 'zypper_call';
 
 our $macvtap_log = '/tmp/macvtap_results.txt';
 
 sub run {
     my ($self, $ctx) = @_;
-    record_info('Info', 'Create a macvtap interface from legacy ifcfg files');
-    my $config     = '/etc/sysconfig/network/ifcfg-macvtap1';
+    record_info('Info', 'Create a macvtap interface from wicked XML files');
+    my $config     = '/etc/wicked/ifconfig/macvtap.xml';
     my $ip_address = $self->get_ip(type => 'macvtap', netmask => 1);
     $ip_address =~ s'/'\\/';
-    $self->get_from_data('wicked/ifcfg/macvtap1',    $config);
-    $self->get_from_data('wicked/ifcfg/macvtap_eth', '/etc/sysconfig/network/ifcfg-' . $ctx->iface());
+    $self->get_from_data('wicked/xml/macvtap.xml', $config);
     $self->prepare_check_macvtap($config, $ctx->iface(), $ip_address);
     $self->wicked_command('ifreload', $ctx->iface());
     $self->wicked_command('ifup',     'macvtap1');
     $self->validate_macvtap($macvtap_log);
     upload_logs($macvtap_log);
-}
-
-sub test_flags {
-    return {always_rollback => 1};
 }
 
 sub post_fail_hook {
