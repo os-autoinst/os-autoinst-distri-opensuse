f82d706d2064c7f8204d556ff479df75b69c703b
Jonathan Rivrain <JRivrain@suse.com>
2019-04-02 16:14:45 +0200

    Fix validate_lvm_encrypt on aarch64+sle12

--- tests/console/validate_lvm_encrypt.pm
+++ tests/console/validate_lvm_encrypt.pm
@@ -17,6 +17,8 @@ use testapi;
 use utils;
 use y2logsstep;
 use Test::Assert ':all';
+use version_utils qw(is_aarch64 is_storage_ng);
+
 
 sub run {
     my $self              = shift;
@@ -81,10 +83,10 @@ sub run {
     }
 
     record_info('INFO', 'Check partitions');
-    if (get_var('NAME') =~ m/separate/) {
+    if ((get_var('NAME') =~ m/separate/) || is_aarch64 && !is_storage_ng) {    # Separate boot partition, not encrypted. aarch64+!storage_ng: see poo#49718
         assert_script_run q[df | grep -P "^\/dev\/.{3}\d{1}"| grep boot];
     }
-    else {
+    else {                                                                     # Encrypted boot partition with lvm
         assert_script_run q[df | grep -P "^\/dev/\mapper\/\w+"| grep boot];
     }
 }
