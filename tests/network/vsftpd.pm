081467d76a20e1b64b47be1e309b20de0d9a56c5
Jozef Pupava <jpupava@suse.com>
2019-04-10 10:10:28 +0200

    Use assert_script_run with expect for ssh-copy-id

--- tests/network/vsftpd.pm
+++ tests/network/vsftpd.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2018 SUSE LLC
+# Copyright © 2018-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -19,25 +19,25 @@ use lockapi qw(barrier_create barrier_wait);
 use utils 'zypper_call';
 
 sub run {
+    my $password = $testapi::password;
     select_console 'root-console';
     if (check_var('HOSTNAME', 'server')) {
         zypper_call 'in vsftpd';
     }
+    else {
+        zypper_call 'in expect';
+    }
     # export slenkins variables
     assert_script_run 'export SERVER=10.0.2.101';
     assert_script_run 'export CLIENT=10.0.2.102';
+    # hosts entry for ssh-copy-id with expect
+    assert_script_run 'echo "10.0.2.101 server" >>/etc/hosts';
+    assert_script_run 'echo "10.0.2.102 client" >>/etc/hosts';
     # copy ssh keys on server and client
     assert_script_run 'ssh-keygen -f /root/.ssh/id_rsa -N ""';
-    type_string "ssh-copy-id -o StrictHostKeyChecking=no \$SERVER\n";
-    wait_still_screen 3;
-    type_password;
-    send_key 'ret';
-    wait_still_screen 3;
-    type_string "ssh-copy-id -o StrictHostKeyChecking=no \$CLIENT\n";
-    wait_still_screen 3;
-    type_password;
-    send_key 'ret';
-    wait_still_screen 3;
+    my $ssh_copy = 'ssh-copy-id -o StrictHostKeyChecking=no';
+    assert_script_run "expect -c 'spawn $ssh_copy server;expect \"Password\";send \"$password\\r\";interact'";
+    assert_script_run "expect -c 'spawn $ssh_copy client;expect \"Password\";send \"$password\\r\";interact'";
     # extract vsftpd testsuite in /tmp/vsftpd
     assert_script_run 'cd /tmp';
     assert_script_run 'wget ' . data_url('qam/vsftpd.tar.gz');
