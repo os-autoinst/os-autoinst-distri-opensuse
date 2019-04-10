e4b14acf080393bc07a4cbf4390940b2bbf050a0
Alvaro Carvajal <acarvajal@suse.de>
2019-04-10 17:00:21 +0200

    Fix comment to reflect removal of get_node_to_join

--- tests/ha/fencing.pm
+++ tests/ha/fencing.pm
@@ -28,7 +28,7 @@ sub run {
     # Fence the master node
     assert_script_run 'crm -F node fence ' . get_node_to_join if is_node(2);
 
-    # Wait for fencing to start only if running in get_node_to_join
+    # Wait for fencing to start only if running in the master node
     if (get_var('HA_CLUSTER_INIT')) {
         my $loop_count = 120;    # Wait at most for 120 seconds
         while (check_screen('root-console', 0, no_wait => 1)) {
