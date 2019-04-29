d23a6beb8217efa5495ed3ddef5ee938737e4df9
Joachim Rauch <jrauch@suse.com>
2019-04-29 14:22:06 +0200

    Fixed Travis condition
    Travis now only deploy conditional when something has changed in the
    lib/utils.
    For this a DEPLOY_NEEDED global variable is needed in the travis
    settings

--- script/generateUtilsDoc.sh
+++ script/generateUtilsDoc.sh
@@ -14,8 +14,26 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
+NEW_DEPLOY_NEEDED=0
+
+if [ -z $GITHUB_TOKEN  ]
+then
+    NEW_DEPLOY_NEEDED=0
+    exit 0
+fi
+
 cd $TRAVIS_BUILD_DIR
 
 touch docs/utils.html
 pod2html --infile=lib/utils.pm --outfile=docs/utils.html
+
+#checkout old docs and compare to new ones, then toggle flag accordingly
+git diff gh-pages -- docs/utils.html
+ret_val=$?
+if [ ${ret_val} -ne 0 ]
+then
+    NEW_DEPLOY_NEEDED=1
+else
+    NEW_DEPLOY_NEEDED=0
+fi
 
