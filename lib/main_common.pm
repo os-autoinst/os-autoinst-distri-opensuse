os-autoinst/os-autoinst-distri-opensuse

commit 8a3f66bed12f0eaa4bb803a1b2e302391ec4410c

Update lib/main_common.pm
Implement a more elegant check for SLE and Leap version

Co-Authored-By: atanno <41161035+atanno@users.noreply.github.com>

lib/main_common.pm
@@ -1594,7 +1594,7 @@ sub load_extra_tests_console {
     loadtest "console/lshw" if ((is_sle('15+') && (check_var('ARCH', 'ppc64le') || check_var('ARCH', 'x86_64'))) || is_opensuse);
     loadtest 'console/quota' unless is_jeos;
     loadtest 'console/zziplib' if (is_sle('12-SP3+') && !is_jeos);
-    loadtest 'console/firewalld' if (is_sle('15+') || is_leap || is_tumbleweed);
+    loadtest 'console/firewalld' unless is_sle('<15') || is_leap('<15');
 }
 
 sub load_extra_tests_docker {

commit 644d8b3dc159ed43a1fd24eb13215df22b6f9623

Fix Schedule for Leap 15.1
Commit 8a3f66bed12f0eaa4bb803a1b2e302391ec4410c broke the schedule by
using a non permitted version

lib/main_common.pm
@@ -1590,7 +1590,7 @@ sub load_extra_tests_console {
     loadtest "console/lshw" if ((is_sle('15+') && (check_var('ARCH', 'ppc64le') || check_var('ARCH', 'x86_64'))) || is_opensuse);
     loadtest 'console/quota' unless is_jeos;
     loadtest 'console/zziplib' if (is_sle('12-SP3+') && !is_jeos);
-    loadtest 'console/firewalld' unless is_sle('<15') || is_leap('<15');
+    loadtest 'console/firewalld' if is_sle('15+') || is_leap('15.0+');
 }
 
 sub load_extra_tests_docker {