d53ae5b116f018c469b9e7403a3b3247eee63487
openQA web UI <openqa@linux-qftt>
2019-05-17 10:41:18 +0800

    add full run case for kernel performance testing

--- tests/kernel_performance/install_qatestset.pm
+++ tests/kernel_performance/install_qatestset.pm
@@ -12,7 +12,7 @@
 # Maintainer: Joyce Na <jna@suse.de>
 
 package install_qatestset;
-use base "y2logsstep";
+use base "y2_installbase";
 use power_action_utils 'power_action';
 use strict;
 use warnings;
@@ -40,7 +40,7 @@ sub setup_environment {
 sub run {
     install_pkg;
     setup_environment;
-    power_action('poweroff');
+    power_action('poweroff', keepconsole => 1, textmode => 1);
 }
 
 sub test_flags {
