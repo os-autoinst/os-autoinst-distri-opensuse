os-autoinst/os-autoinst-distri-opensuse

commit 5c79a6d326bbacc4af5ad950810bc3b509597dcc

Boot and install LTP on baremetal

products/sle/main.pm
@@ -677,8 +677,7 @@ if (is_jeos) {
 
 # load the tests in the right order
 if (is_kernel_test()) {
-    if (get_var('LTP_BAREMETAL')) {
-        set_var('ADDONURL', 'sdk') if is_sle('=12-SP4');
+    if (get_var('LTP_BAREMETAL') && get_var('INSTALL_LTP')) {
         load_baremetal_tests();
     }
     load_kernel_tests();

lib/opensusebasetest.pm
@@ -332,6 +332,13 @@ sub wait_boot {
 
     # Reset the consoles after the reboot: there is no user logged in anywhere
     reset_consoles;
+    # For IPMI machines PXE boot menu will appear first
+    if (check_var('BACKEND', 'ipmi')) {
+        select_console 'sol', await_console => 0;
+        # boot from harddrive
+        assert_screen([qw(virttest-pxe-menu qa-net-selection prague-pxe-menu pxe-menu)], 200);
+        send_key 'ret';
+    }
     # reconnect s390
     if (check_var('ARCH', 's390x')) {
         my $login_ready = get_login_message();
@@ -385,12 +392,6 @@ sub wait_boot {
         else {
             select_console('x11', await_console => 0);
         }
-    }
-    elsif (check_var('BACKEND', 'ipmi')) {
-        select_console 'sol', await_console => 0;
-        # boot from harddrive
-        assert_screen([qw(virttest-pxe-menu qa-net-selection prague-pxe-menu pxe-menu)], 200);
-        send_key 'ret';
     }
     # On Xen PV and svirt we don't see a Grub menu
     elsif (!(check_var('VIRSH_VMM_FAMILY', 'xen') && check_var('VIRSH_VMM_TYPE', 'linux') && check_var('BACKEND', 'svirt'))) {

tests/kernel/install_ltp.pm
@@ -23,6 +23,7 @@ use power_action_utils 'power_action';
 use serial_terminal 'add_serial_console';
 use upload_system_log;
 use version_utils qw(is_sle is_opensuse is_jeos);
+use Utils::Backends 'use_ssh_serial_console';
 
 sub add_repos {
     my $qa_head_repo = get_required_var('QA_HEAD_REPO');
@@ -272,7 +273,13 @@ sub run {
         select_console('root-console');
         add_serial_console('hvc1');
     }
-    $self->select_serial_terminal;
+
+    if (check_var('BACKEND', 'ipmi')) {
+        use_ssh_serial_console;
+    }
+    else {
+        $self->select_serial_terminal;
+    }
 
     if (script_output('cat /sys/module/printk/parameters/time') eq 'N') {
         script_run('echo 1 > /sys/module/printk/parameters/time');

commit 5c3b3063021b848d993abeb99da480d8be3fdc41

IPMI,LTP: Set again ADDONURL=sdk for not yet released SLE12
*Not yet released* versions of SLE12 need ADDONURL=sdk settings for SCC
registration. But this setting is conflicting for SLE15 (both released
and not released) and SLE12 *released* versions.

Because this single test suite setup cannot be used for both SLE12 and
SLE 15 commit 8a1a108b0 ("LTP: Set ADDONURL=sdk for SLE12 SP4") added
ADDONURL=sdk in code just for SLE12 SP4 (at the time not released).
Later 5c79a6d32 ("Boot and install LTP on baremetal") removed it to
fix SLE12 SP4 after it being released, but this broke SLE12 SP5 which
haven't been released. Therefore use is_released to detect non-released
SLE12 versions.

Adding it to load_baremetal_tests() so it should fix not only
install_ltp_baremetal [1], but also ibtest which use ipxe_install.pm [2].

[1] http://quasar.suse.cz/tests/2999
[2] https://openqa.suse.de/tests/2991216

Fixes: 5c79a6d32 ("Boot and install LTP on baremetal")

products/sle/main.pm
@@ -17,7 +17,7 @@ use registration;
 use utils;
 use mmapi 'get_parents';
 use version_utils
-  qw(is_vmware is_hyperv is_hyperv_in_gui is_caasp is_installcheck is_rescuesystem is_desktop_installed is_jeos is_sle is_staging is_upgrade);
+  qw(is_vmware is_hyperv is_hyperv_in_gui is_caasp is_installcheck is_rescuesystem is_desktop_installed is_jeos is_released is_sle is_staging is_upgrade);
 use File::Find;
 use File::Basename;
 use LWP::Simple 'head';
@@ -531,6 +531,8 @@ sub mellanox_config {
 }
 
 sub load_baremetal_tests {
+    set_var('ADDONURL', 'sdk') if (is_sle('>=12') && is_sle('<15')) && !is_released;
+
     loadtest "autoyast/prepare_profile" if get_var "AUTOYAST_PREPARE_PROFILE";
     if (get_var('IPXE')) {
         loadtest 'installation/ipxe_install';