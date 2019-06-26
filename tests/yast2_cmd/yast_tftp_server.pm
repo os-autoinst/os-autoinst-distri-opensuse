61a1c301073af9b0f46825ada92d1c615be5a780
skliu <liushukuicn1771@gmail.com>
2019-06-26 13:18:36 +0800

    Create yast_tftp_server.pm

---
+++ tests/yast2_cmd/yast_tftp_server.pm
@@ -0,0 +1,31 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: yast tftp-server, list, set and show summary
+# Maintainer: shukui <skliu@suse.com>
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run {
+    select_console 'root-console';
+    zypper_call "in tftp yast2-tftp-server";
+    assert_script_run 'yast tftp-server directory path=/srv/tftpboot';
+    validate_script_output 'yast tftp-server directory list 2>&1', sub { m/tftpboot/ };
+    assert_script_run 'yast tftp-server status enable';
+    validate_script_output 'yast tftp-server status show 2>&1', sub { m/true/ };
+    assert_script_run 'yast tftp-server status disable';
+    validate_script_output 'yast tftp-server status show 2>&1', sub { m/false/ };
+}
+
+#sub post_run_hook {}
+1;
