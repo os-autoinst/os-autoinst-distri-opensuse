c406e69086966574aef40db7b5f06dfec0a514cf
skliu <liushukuicn1771@gmail.com>
2019-06-26 12:08:47 +0800

    Update yast2_tftp_server.pm

--- tests/yast2_gui/yast2_tftp_server.pm
+++ tests/yast2_gui/yast2_tftp_server.pm
@@ -22,29 +22,29 @@ sub run {
     my $self = shift;
 
     select_console 'root-console';
-#   assert_script_run 'echo Hello World!';
-#   assert_script_run 'ls -l';
-#   assert_script_run 'ip a';
-#   assert_script_run 'systemctl stop firewalld';
-#   assert_script_run 'systemctl start sshd.service';
+    #   assert_script_run 'echo Hello World!';
+    #   assert_script_run 'ls -l';
+    #   assert_script_run 'ip a';
+    #   assert_script_run 'systemctl stop firewalld';
+    #   assert_script_run 'systemctl start sshd.service';
     assert_script_run 'zypper -n in tftp yast2-tftp-server', 200;
     script_run 'systemctl status tftp.socket';
 
     select_console 'x11';
     $self->launch_yast2_module_x11('tftp-server', match_timeout => 60);
     send_key "alt-n";
-    assert_screen "yast2_tftp_server_enabled";# here is the tag name of the needle.
-    send_key "alt-o";    # OK => Exit
+    assert_screen "yast2_tftp_server_enabled";    # here is the tag name of the needle.
+    send_key "alt-o";                             # OK => Exit
 
     select_console 'root-console';
     assert_script_run 'systemctl status tftp.socket';
-    assert_screen "yast2_tftp_server_check_socket";  
+    assert_screen "yast2_tftp_server_check_socket";
     assert_script_run 'chmod 777 /srv/tftpboot';
     assert_script_run 'echo "hello world" > /srv/tftpboot/tmp.txt';
     assert_script_run 'chmod 777 /srv/tftpboot/tmp.txt';
     assert_script_run 'tftp -v 127.0.0.1 -c get tmp.txt';
     assert_script_run 'tftp -v 127.0.0.1 -c put tmp.txt';
-    assert_screen "yast2_tftp_server_result";  
+    assert_screen "yast2_tftp_server_result";
     select_console 'x11';
 }
 
