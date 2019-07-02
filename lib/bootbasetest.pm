b753639fe169bb8000abf4136ba1b544139b2edc
Guillaume GARDET <guillaume.gardet@free.fr>
2019-07-02 10:15:36 +0200

    bootbasetest: Rework post_fail_hook

--- lib/bootbasetest.pm
+++ lib/bootbasetest.pm
@@ -11,19 +11,17 @@ sub post_fail_hook {
         record_info 'Seems that the display manager failed';
     }
 
-    # if we found a shell, we do not need the memory dump
-    if (!(match_has_tag('emergency-shell') or match_has_tag('emergency-mode'))) {
-        die "save_memory_dump not implemented, no way to save memory_dump" unless check_var('BACKEND', 'qemu');
-        select_console 'root-console';
-        diag 'Save memory dump to debug bootup problems, e.g. for bsc#1005313';
-        save_memory_dump;
-    }
-
     # crosscheck for text login on tty1
     select_console 'root-console';
 
     # collect and upload some stuff
     $self->SUPER::export_logs();
+
+    # Dump memory on qemu backend
+    if (check_var('BACKEND', 'qemu')) {
+        diag 'Save memory dump to debug bootup problems, e.g. for bsc#1005313';
+        save_memory_dump;
+    }
 }
 
 1;
