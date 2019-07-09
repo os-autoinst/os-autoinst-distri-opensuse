55d2ec5da708a12ed6d970d55eb42a45b7eebd78
coolgw <wegao@suse.com>
2019-07-09 13:38:29 +0800

    Add 12-sp5 check for http server

--- lib/service_check.pm
+++ lib/service_check.pm
@@ -57,7 +57,7 @@ our $default_services = {
     #    apache => {
     #    srv_pkg_name  => 'apache2',
     #    srv_proc_name => 'apache2',
-    #    support_ver   => '12-SP3,12-SP4,15,15-SP1'
+    #    support_ver   => '12-SP3,12-SP4,12-SP5,15,15-SP1'
     #},
     bind => {
         srv_pkg_name  => 'bind',
