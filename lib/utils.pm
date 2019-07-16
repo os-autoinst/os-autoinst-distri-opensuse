os-autoinst/os-autoinst-distri-opensuse

commit d310be7dbec3c7d9bc6f27c06e522288657ec41f

utils: zypper_ar() enhancements
* name is now optional parameter (if not used, $url must be a URI
  pointing to a .repo file). Check before adding whether repo exists is
  only when name is used.
* add optional parameter priority
* possibility to add other ar subcommand parameters
* refresh in single command (with -f)
* doc

lib/utils.pm
@@ -456,15 +456,42 @@ sub zypper_enable_install_dvd {
 
 =head2 zypper_ar
 
+    zypper_ar($url,[ name => NAME ], [ priority => N ]);
+
 Add repository (with zypper ar) unless it's already repo C<$name> already added.
 
+Options:
+
+C<$name> alias for repository, optional
+When used, additional check if repo not yet exists is done, and adding
+only if it doesn't exist.
+NOTE: if not used, $url must be a URI pointing to a .repo file.
+
+C<$priority> set repo priority, optional
+
+C<$params> other ar subcommand parameters, optional
+
+Examples:
+    zypper_ar('https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo', priority => 90);
+    zypper_ar('http://dist.nue.suse.com/ibs/QA:/Head/SLE-15-SP1', name => 'qa-head);
 =cut
 sub zypper_ar {
-    my ($url, $name) = @_;
+    my ($url, %args) = @_;
+    my $name = $args{name} // '';
+    my $priority = $args{priority} // '';
+    my $params = $args{params} // '';
 
+    $priority = "-p $priority" if ($priority);
+    my $cmd = "--gpg-auto-import-keys ar -f $priority $params $url";
+
+    # repo file
+    if (!$name) {
+        return zypper_call($cmd, dumb_term => 1);
+    }
+
+    # URI alias
     if (script_run("zypper lr $name")) {
-        zypper_call("ar $url $name",                           dumb_term => 1);
-        zypper_call("--gpg-auto-import-keys ref --repo $name", dumb_term => 1);
+        return zypper_call("$cmd $name", dumb_term => 1);
     }
 }
 

commit ff723a898d1689874da498c9337a79636728af9f

zypper_ar: Put back zypper ref
zypper ar -f isn't equivalent of zypper ref.
(It just enable auto-refresh of the repository).

Fixes: d310be7db utils: zypper_ar() enhancements

lib/utils.pm
@@ -458,13 +458,14 @@ sub zypper_enable_install_dvd {
 
     zypper_ar($url,[ name => NAME ], [ priority => N ]);
 
-Add repository (with zypper ar) unless it's already repo C<$name> already added.
+Add repository (with zypper ar) unless it's already repo C<$name> already added
+and refresh repositories.
 
 Options:
 
 C<$name> alias for repository, optional
 When used, additional check if repo not yet exists is done, and adding
-only if it doesn't exist.
+only if it doesn't exist. Also zypper ref is run only on this repository.
 NOTE: if not used, $url must be a URI pointing to a .repo file.
 
 C<$no_gpg_check> pass --no-gpgcheck for repos with not valid GPG key, optional
@@ -486,16 +487,19 @@ sub zypper_ar {
 
     $priority     = "-p $priority"  if ($priority);
     $no_gpg_check = "--no-gpgcheck" if ($no_gpg_check);
-    my $cmd = "--gpg-auto-import-keys ar -f $priority $no_gpg_check $params $url";
+    my $cmd_ar  = "--gpg-auto-import-keys ar -f $priority $no_gpg_check $params $url";
+    my $cmd_ref = "--gpg-auto-import-keys ref";
 
     # repo file
     if (!$name) {
-        return zypper_call($cmd, dumb_term => 1);
+        zypper_call($cmd_ar, dumb_term => 1);
+        return zypper_call($cmd_ref, dumb_term => 1);
     }
 
     # URI alias
     if (script_run("zypper lr $name")) {
-        return zypper_call("$cmd $name", dumb_term => 1);
+        zypper_call("$cmd_ar $name", dumb_term => 1);
+        return zypper_call("$cmd_ref --repo $name", dumb_term => 1);
     }
 }
 