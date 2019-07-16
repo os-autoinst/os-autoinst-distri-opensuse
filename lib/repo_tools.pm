os-autoinst/os-autoinst-distri-opensuse

commit eaf7c6fb49c8167cd1fd27501f2098ead3e4db88

Use zypper_ar() in some tests

lib/repo_tools.pm
@@ -44,7 +44,7 @@ our @EXPORT = qw(
 Helper to add QA:HEAD repository repository (usually from IBS).
 =cut
 sub add_qa_head_repo {
-    zypper_ar(get_required_var('QA_HEAD_REPO'), 'qa-head');
+    zypper_ar(get_required_var('QA_HEAD_REPO'), name => 'qa-head');
 }
 
 =head2 add_qa_web_repo
@@ -54,7 +54,7 @@ sub add_qa_head_repo {
 Helper to add QA web repository repository.
 =cut
 sub add_qa_web_repo {
-    zypper_ar(get_required_var('QA_WEB_REPO'), 'qa-web');
+    zypper_ar(get_required_var('QA_WEB_REPO'), name => 'qa-web');
 }
 
 =head2 get_repo_var_name

lib/utils.pm
@@ -477,9 +477,9 @@ Examples:
 =cut
 sub zypper_ar {
     my ($url, %args) = @_;
-    my $name = $args{name} // '';
+    my $name     = $args{name}     // '';
     my $priority = $args{priority} // '';
-    my $params = $args{params} // '';
+    my $params   = $args{params}   // '';
 
     $priority = "-p $priority" if ($priority);
     my $cmd = "--gpg-auto-import-keys ar -f $priority $params $url";

tests/caasp/stack_conformance.pm
@@ -43,7 +43,7 @@ sub run {
         assert_script_run 'mv sonobuoy /usr/bin/';
     } else {
         my $repo = get_var('SONOBUOY_REPO', 'https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo');
-        zypper_call "ar -Gf $repo";
+        zypper_ar($repo);
         zypper_call 'in sonobuoy';
     }
     type_string "exit\n";

tests/kernel/ib_tests.pm
@@ -64,8 +64,7 @@ sub ibtest_master {
     my $hpc_testing_branch = get_var('IBTEST_GITBRANCH', 'master');
 
     # do all test preparations and setup
-    zypper_call('ar -f -G ' . get_required_var('DEVEL_TOOLS_REPO'));
-    zypper_call('--gpg-auto-import-keys ref');
+    zypper_ar(get_required_var('DEVEL_TOOLS_REPO'));
     zypper_call('in git-core twopence bc iputils python');
 
     # create symlinks, the package is (for now) broken

tests/kernel/install_kotd.pm
@@ -33,8 +33,8 @@ sub run {
     # Remove all installed kernel and related packages
     remove_kernel_packages;
     # Enable kotd/kmp repositories
-    zypper_call("ar -G -f -p 90 $kotd_repo KOTD");
-    zypper_call("ar -G -f -p 90 $kmp_repo KMP") if $kmp_repo;
+    zypper_ar($kotd_repo, name => 'KOTD', priority => 90);
+    zypper_ar($kmp_repo,  name => 'KMP',  priority => 90) if $kmp_repo;
     # Install latest kernel
     zypper_call("in -l kernel-default");
     # Check for multiple kernel installation

tests/kernel/update_kernel.pm
@@ -252,7 +252,7 @@ sub install_kotd {
     my $repo = shift;
     fully_patch_system;
     remove_kernel_packages;
-    zypper_call("ar -G -f -p 90 $repo KOTD");
+    zypper_ar($repo, name => 'KOTD', priority => 90);
     zypper_call("in -l kernel-default");
 }
 

tests/boot/grub2_test.pm
@@ -34,7 +34,7 @@ sub run {
     record_info 'grub2 menu entry', 'install another kernel, another kernel will the previous one';
     select_console 'root-console';
     zypper_call 'in -t pattern yast2_basis' if is_sle('15+');
-    zypper_ar 'http://download.suse.de/ibs/Devel:/Kernel:/master/standard/', 'KERNEL_DEVEL';
+    zypper_ar 'http://download.suse.de/ibs/Devel:/Kernel:/master/standard/', name => 'KERNEL_DEVEL';
     zypper_call 'in kernel-vanilla';
     assert_script_run 'uname -r >kernel.txt';
     reboot;

tests/console/zziplib.pm
@@ -26,7 +26,7 @@ sub run {
     # development module needed for dependencies, released products are tested with sdk module
     if (get_var('BETA')) {
         my $sdk_repo = is_sle('15+') ? get_var('REPO_SLE_MODULE_DEVELOPMENT_TOOLS') : get_var('REPO_SLE_SDK');
-        zypper_ar 'http://' . get_var('OPENQA_URL') . "/assets/repo/$sdk_repo", 'SDK';
+        zypper_ar 'http://' . get_var('OPENQA_URL') . "/assets/repo/$sdk_repo", name => 'SDK';
     }
     # maintenance updates are registered with sdk module
     elsif (get_var('FLAVOR') !~ /Updates|Incidents/) {

commit ddcc38cceefc11deefc2c46797e8e244ed0d272e

zypper_ar: Add option to pass --no-gpgcheck (default off)
and use it in tests which used it before (had zypper ar -G,
--no-gpgcheck is alias for -G).

Fixes: eaf7c6fb4 Use zypper_ar() in some tests

Reported-by: Jozef Pupava <jpupava@suse.com>

lib/repo_tools.pm
@@ -44,7 +44,7 @@ our @EXPORT = qw(
 Helper to add QA:HEAD repository repository (usually from IBS).
 =cut
 sub add_qa_head_repo {
-    zypper_ar(get_required_var('QA_HEAD_REPO'), name => 'qa-head');
+    zypper_ar(get_required_var('QA_HEAD_REPO'), name => 'qa-head', no_gpg_check => 1);
 }
 
 =head2 add_qa_web_repo
@@ -54,7 +54,7 @@ sub add_qa_head_repo {
 Helper to add QA web repository repository.
 =cut
 sub add_qa_web_repo {
-    zypper_ar(get_required_var('QA_WEB_REPO'), name => 'qa-web');
+    zypper_ar(get_required_var('QA_WEB_REPO'), name => 'qa-web', no_gpg_check => 1);
 }
 
 =head2 get_repo_var_name

lib/utils.pm
@@ -467,22 +467,26 @@ When used, additional check if repo not yet exists is done, and adding
 only if it doesn't exist.
 NOTE: if not used, $url must be a URI pointing to a .repo file.
 
+C<$no_gpg_check> pass --no-gpgcheck for repos with not valid GPG key, optional
+
 C<$priority> set repo priority, optional
 
 C<$params> other ar subcommand parameters, optional
 
 Examples:
-    zypper_ar('https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo', priority => 90);
     zypper_ar('http://dist.nue.suse.com/ibs/QA:/Head/SLE-15-SP1', name => 'qa-head);
+    zypper_ar('https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo', no_gpg_check => priority => 90);
 =cut
 sub zypper_ar {
     my ($url, %args) = @_;
-    my $name     = $args{name}     // '';
-    my $priority = $args{priority} // '';
-    my $params   = $args{params}   // '';
+    my $name         = $args{name}         // '';
+    my $priority     = $args{priority}     // '';
+    my $params       = $args{params}       // '';
+    my $no_gpg_check = $args{no_gpg_check} // '';
 
-    $priority = "-p $priority" if ($priority);
-    my $cmd = "--gpg-auto-import-keys ar -f $priority $params $url";
+    $priority     = "-p $priority"  if ($priority);
+    $no_gpg_check = "--no-gpgcheck" if ($no_gpg_check);
+    my $cmd = "--gpg-auto-import-keys ar -f $priority $no_gpg_check $params $url";
 
     # repo file
     if (!$name) {

tests/caasp/stack_conformance.pm
@@ -42,7 +42,7 @@ sub run {
         assert_script_run 'tar -xzf sonobuoy_0.12.1_linux_amd64.tar.gz';
         assert_script_run 'mv sonobuoy /usr/bin/';
     } else {
-        my $repo = get_var('SONOBUOY_REPO', 'https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo');
+        my $repo = get_var('SONOBUOY_REPO', 'https://download.opensuse.org/repositories/devel:/kubic/openSUSE_Tumbleweed/devel:kubic.repo', no_gpg_check => 1);
         zypper_ar($repo);
         zypper_call 'in sonobuoy';
     }

tests/kernel/ib_tests.pm
@@ -64,7 +64,7 @@ sub ibtest_master {
     my $hpc_testing_branch = get_var('IBTEST_GITBRANCH', 'master');
 
     # do all test preparations and setup
-    zypper_ar(get_required_var('DEVEL_TOOLS_REPO'));
+    zypper_ar(get_required_var('DEVEL_TOOLS_REPO'), no_gpg_check => 1);
     zypper_call('in git-core twopence bc iputils python');
 
     # create symlinks, the package is (for now) broken

tests/kernel/install_kotd.pm
@@ -33,8 +33,8 @@ sub run {
     # Remove all installed kernel and related packages
     remove_kernel_packages;
     # Enable kotd/kmp repositories
-    zypper_ar($kotd_repo, name => 'KOTD', priority => 90);
-    zypper_ar($kmp_repo,  name => 'KMP',  priority => 90) if $kmp_repo;
+    zypper_ar($kotd_repo, name => 'KOTD', priority => 90, no_gpg_check => 1);
+    zypper_ar($kmp_repo,  name => 'KMP',  priority => 90, no_gpg_check => 1) if $kmp_repo;
     # Install latest kernel
     zypper_call("in -l kernel-default");
     # Check for multiple kernel installation

tests/kernel/update_kernel.pm
@@ -252,7 +252,7 @@ sub install_kotd {
     my $repo = shift;
     fully_patch_system;
     remove_kernel_packages;
-    zypper_ar($repo, name => 'KOTD', priority => 90);
+    zypper_ar($repo, name => 'KOTD', priority => 90, no_gpg_check => 1);
     zypper_call("in -l kernel-default");
 }
 