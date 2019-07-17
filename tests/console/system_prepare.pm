37e8e4b270d7c4d53120e93b56c2bed17f0374bb
tinawang123 <yuwang@suse.com>
2019-07-17 17:24:47 +0800

    Delete the unused function name and add subroutine

--- tests/console/system_prepare.pm
+++ tests/console/system_prepare.pm
@@ -17,6 +17,7 @@ use Utils::Backends 'use_ssh_serial_console';
 use version_utils 'is_sle';
 use serial_terminal 'add_serial_console';
 use bootloader_setup qw(change_grub_config grub_mkconfig);
+use registration;
 use strict;
 use warnings;
 
