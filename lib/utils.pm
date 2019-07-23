os-autoinst/os-autoinst-distri-opensuse

commit 969d2b93eff0b6f44a47e3af1fe7c0e217d036f0

zypper_ar: Check repo exists before adding it

lib/utils.pm
@@ -456,13 +456,16 @@ sub zypper_enable_install_dvd {
 
 =head2 zypper_ar
 
-Works exactly like zypper_ar on console
+Add repository (with zypper ar) unless it's already repo C<$name> already added.
+
 =cut
 sub zypper_ar {
     my ($url, $name) = @_;
 
-    zypper_call("ar $url $name",                           dumb_term => 1);
-    zypper_call("--gpg-auto-import-keys ref --repo $name", dumb_term => 1);
+    if (script_run("zypper lr $name")) {
+        zypper_call("ar $url $name",                           dumb_term => 1);
+        zypper_call("--gpg-auto-import-keys ref --repo $name", dumb_term => 1);
+    }
 }
 
 =head2 fully_patch_system

commit 727f3aea74fd606893b3149edd551866170a1f22

Add workaround for zypper lr check on SLE11 SP3
Detection whether repo has been installed does not work on SLE11 SP3
(zypper lr foo exits 0 in old zypper 1.6.x). Thus detect with error
message.

Fixes: 969d2b93e ("zypper_ar: Check repo exists before adding it")
Fixes: bsc#1141504

lib/utils.pm
@@ -497,7 +497,8 @@ sub zypper_ar {
     }
 
     # URI alias
-    if (script_run("zypper lr $name")) {
+    my $out = script_output("LC_ALL=C zypper lr $name 2>&1", proceed_on_failure => 1);
+    if ($out =~ /Repository.*$name.*not found/i) {
         zypper_call("$cmd_ar $name", dumb_term => 1);
         return zypper_call("$cmd_ref --repo $name", dumb_term => 1);
     }