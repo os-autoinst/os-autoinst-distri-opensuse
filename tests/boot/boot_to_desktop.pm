9a9952de50bc418f8faef73f860290d81fca7362
Anton Smorodskyi <asmorodskyi@suse.com>
2019-07-24 09:23:13 +0200

    wait_boot: Add another case when we waiting for boot longer

--- tests/boot/boot_to_desktop.pm
+++ tests/boot/boot_to_desktop.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -20,12 +20,12 @@ use version_utils qw(is_upgrade is_sles4sap);
 sub run {
     my ($self) = @_;
     $self->{in_boot_desktop} = 1;
-    # We have tests that boot from HDD and wait for DVD boot menu's timeout, so
-    # the timeout here must cover it. UEFI DVD adds some 60 seconds on top.
-    my $timeout = get_var('UEFI') ? 140 : 80;
-    # Add additional 60 seconds if the test suite is migration as reboot from
-    # pre-migration system may take an additional time.
-    $timeout += 60 if get_var('PATCH') || get_var('ONLINE_MIGRATION');
+    # In some cases default wait for boot should be increased due to different reasons.
+    # currently we awere about such reasons :
+    # - tests which boot from HDD and wait for boot menu's timeout ( UEFI DVD)
+    # - reboot from pre-migration system
+    # - skipping GRUB menu detection for unknown reason slowdown boot process
+    my $timeout = get_var('UEFI') || get_var('KEEP_GRUB_TIMEOUT') || get_var('PATCH') || get_var('ONLINE_MIGRATION') ? 140 : 80;
     # Do not attempt to log into the desktop of a system installed with SLES4SAP
     # being prepared for upgrade, as it does not have an unprivileged user to test
     # with other than the SAP Administrator
