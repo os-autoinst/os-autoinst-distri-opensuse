f4f4978fea3d9a74b655e87de7487defce659c15
Paolo Stivanin <info@paolostivanin.com>
2019-07-24 14:51:25 +0200

    clean_pidgin: update summary with a more descriptive text
    for usage with https://confluence.suse.com/pages/viewpage.action?spaceKey=~martinsmac&title=Script+collecting+testcase+coverage

--- tests/x11/pidgin/clean_pidgin.pm
+++ tests/x11/pidgin/clean_pidgin.pm
@@ -1,14 +1,16 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2017 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Cleaning for testing pidgin
+# Summary: Cleanup before testing pidgin
+# - remove pidgin package
+# - ensure that package was really removed
 # Maintainer: Chingkai <qkzhu@suse.com>
 
 use base "x11test";
