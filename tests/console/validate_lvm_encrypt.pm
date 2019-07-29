75c2d89a6cd0366ee6c3847ce0e5e5824b971568
Rodion Iafarov <riafarov@suse.com>
2019-07-29 09:56:20 +0200

    Revert "Validate cryptlvm test suite"

--- tests/console/validate_lvm_encrypt.pm
+++ tests/console/validate_lvm_encrypt.pm
@@ -18,80 +18,8 @@ use utils;
 use y2_module_basetest 'workaround_suppress_lvm_warnings';
 use Test::Assert ':all';
 use version_utils 'is_storage_ng';
-use Utils::Architectures qw(is_aarch64 is_s390x);
-use Mojo::JSON 'decode_json';
+use Utils::Architectures 'is_aarch64';
 
-# pass all block devices from json lsblk output
-sub extract_drives {
-    my $bd = shift;
-    (ref($bd) eq 'HASH' and ref($bd->{blockdevices}) eq 'ARRAY') ?
-      return $bd->{blockdevices} : die "Block devices not found among json data";
-}
-
-sub _get_children {
-    my $drive = shift;
-    return (
-        (ref($drive) eq 'HASH') and
-          defined($drive->{children})) ? $drive->{children} : undef;
-}
-
-sub extract_partitions_from_disk {
-    my $block_devices = shift;
-    return unless (grep($_->{type} eq 'disk', @{$block_devices}));
-    my @parts;
-    # create list of partitions
-    foreach my $drive (@{$block_devices}) {
-        push(@parts, grep { $_->{type} eq 'part' } @{_get_children($drive)});
-    }
-    return \@parts;
-}
-
-# container is created on top of a partition
-# container is always type crypt and without a mountpoint
-sub get_crypt_containers {
-    my $partitions = shift;
-    my @crypt_containers;
-    foreach my $part (@{$partitions}) {
-        push(@crypt_containers, grep { $_->{type} eq 'crypt' } @{_get_children($part)})
-          if (!defined $part->{mountpoint} and (ref(_get_children($part)) eq 'ARRAY'));
-    }
-    return \@crypt_containers;
-}
-
-sub get_encrypted_volumes {
-    my $containers = shift;
-    return unless (grep($_->{type} eq 'crypt', @{$containers}));
-    my @volumes;
-    foreach my $crypt (@{$containers}) {
-        push(@volumes, @{_get_children($crypt)});
-    }
-    return \@volumes;
-}
-
-sub cryptsetup_test {
-    my $containers = shift;
-    my $bkp_file   = '/root/bkp_luks_header';
-
-    die "No /etc/crypttab found!\n" if (script_run('test -f /etc/crypttab'));
-    my @crypttab = script_output q[cat /etc/crypttab | awk '{print $1}'];
-
-    foreach (@crypttab) {
-        assert_script_run qq[cryptsetup status $_ | grep "is active"];
-    }
-    record_info('crypttab', @crypttab);
-
-    foreach (@{$containers}) {
-        unless (script_output('cryptsetup -v status ' . $_->{name}) =~ m/device:\s+(?<crypt_partition>\/.*\b)/) {
-            die "$_->{name} is not active!\n";
-        }
-        next if (script_run('cryptsetup -v isLuks ' . $+{crypt_partition}) != 0);
-        assert_script_run('cryptsetup -v luksUUID ' . $+{crypt_partition});
-        assert_script_run('cryptsetup -v luksDump ' . $+{crypt_partition});
-        assert_script_run('cryptsetup -v luksHeaderBackup ' . $+{crypt_partition} . ' --header-backup-file ' . $bkp_file);
-        validate_script_output("file $bkp_file", sub { m/\bLUKS\sencrypted\sfile\b/ });
-        assert_script_run('cryptsetup -v --batch-mode luksHeaderRestore ' . $+{crypt_partition} . ' --header-backup-file ' . $bkp_file);
-    }
-}
 
 sub run {
     my $self              = shift;
@@ -110,9 +38,12 @@ sub run {
     record_info('INFO', 'Validate LVM config');
     assert_script_run "lvmconfig --mergedconfig --validate | grep \"LVM configuration valid.\"";
 
+    record_info('INFO', 'Find encrypted volumes');
+    my $encrypted_partition = script_output q[cat /etc/crypttab | awk '{print $1}'];
+    assert_script_run qq[cryptsetup status /dev/mapper/$encrypted_partition | grep "is active"];
+
     record_info('INFO', 'Validate setup');
     assert_script_run 'lvmdiskscan -v';
-
     my @active_vols = split(/\n/, script_output q[lvscan | awk '{print $1}']);
     foreach my $vol_status (@active_vols) {
         assert_equals($vol_status, 'ACTIVE', "Volume is Inactive");
@@ -120,6 +51,7 @@ sub run {
 
     my $pvTotalPE = script_output q[pvdisplay|grep "Total PE" | awk '{print $3}'];
     my $pvFreePE  = script_output q[pvdisplay|grep "Free PE" | awk '{print $3}'];
+    my $pe_size   = $pvTotalPE - $pvFreePE;
 
     assert_script_run 'pvs -a';
 
@@ -141,7 +73,7 @@ sub run {
         die "Partitions not found in $volume configuration: \n $results" if ($results);
     }
 
-    assert_equals($pvTotalPE - $pvFreePE, $lv_size, "Sum of Logical Extends differs!");
+    assert_equals($pe_size, $lv_size, "Sum of Logical Extends differs!");
 
     record_info('INFO', 'Create a file on home volume');
     my $test_file = '/home/bernhard/test_file.txt';
@@ -152,71 +84,13 @@ sub run {
         die "LVM usage stats do not differ!";
     }
 
-    record_info('LAYOUT', 'Partition layout overview');
-    my $drives = extract_drives(decode_json(script_output qq[lsblk -p -o NAME,TYPE,MOUNTPOINT -J -e 11]));
-    record_info('DRIVES', 'Total = ' . scalar @{$drives});
-    my $parts = extract_partitions_from_disk($drives);
-    record_info('PARTS', 'Total = ' . scalar @{$parts});
-    my $crypts = get_crypt_containers($parts);
-    record_info('CRYPT', 'Total = ' . scalar @{$crypts});
-    my $encrypted_volumes = get_encrypted_volumes($crypts);
-    record_info('VOLUMES',  'Total = ' . scalar @{$encrypted_volumes});
-    record_info($_->{name}, "Mounted in $_->{mountpoint} ($_->{type})") foreach (@{$encrypted_volumes});
-
-    # read kernel boot parameters, determine root partition
-    my ($rootfs_part) = grep { /root=[^\s]*/ }
-      split(/\s/, script_output qq[cat /proc/cmdline | tee /dev/$serialdev]);
-    # possible formats
-    # root=UUID=aaf272eb-7817-4e04-9f4d-da4e4e354706
-    # root=/dev/mapper/vg+lv
-    # root=/dev/sdaX
-    die "No rootfs found in /proc/cmdline!\n" unless defined($rootfs_part);
-    record_info('rootfs', $rootfs_part = (split(/\=/, $rootfs_part, 2))[1]);
-
-    # get partition which holds initrd, or where does /boot live?
-    my $initrd_record = script_output('df -T /boot/initrd-$(uname -a | cut -f 3 -d\' \') | tail -n 1| tee /dev/' . "$serialdev");
-    my $boot_part     = (split(/\s/, $initrd_record))[0];
-    assert_script_run("df | grep -P \'^$boot_part\'| grep boot");
-    record_info('boot', $boot_part);
-
-    ### rootfs != boot
-    # a) explicitly separated /boot
-    # b) UEFI - old storage stack puts whole /boot on separate partition
-    #         - storage-ng separates only /boot/efi
-    my $efi_zipl_boot;
-    if (get_var('UEFI')) {
-        my $efi_zipl_boot = script_output('df -T /boot/efi| tail -n 1| awk \'{print $1}\' |tee /dev/' . "$serialdev");
-        record_info('EFI', $efi_zipl_boot);
-    } elsif (is_s390x) {
-        my $efi_zipl_boot = script_output('df -T /boot/zipl| tail -n 1| awk \'{print $1}\' |tee /dev/' . "$serialdev");
-        record_info('ZIPL', $efi_zipl_boot);
-    } else {
-        bmwqemu::diag("x86_64 and ppc64le don't have any additional boot partition to be mounted");
+    record_info('INFO', 'Check partitions');
+    if ((get_var('NAME') =~ m/separate/) || is_aarch64 && !is_storage_ng) {    # Separate boot partition, not encrypted. aarch64+!storage_ng: see poo#49718
+        assert_script_run q[df | grep -P "^\/dev\/.{3}\d{1}"| grep boot];
     }
-
-    # all installations can separate /boot
-    if ((get_var('NAME') =~ m/separate/) or
-        ((get_var('NAME') =~ m/cryptlvm/) and !is_storage_ng)) {
-        assert_not_equals($boot_part, $rootfs_part, 'Dedicated partition for /boot is expected!');
-    } else {
-        assert_equals($boot_part, $rootfs_part, '/boot is expected to be part of rootfs!');
+    else {                                                                     # Encrypted boot partition with lvm
+        assert_script_run q[df | grep -P "^\/dev/\mapper\/\w+"| grep boot];
     }
-
-    # handle special boot partition
-    if (get_var('UEFI') || is_aarch64 || is_s390x) {
-        assert_not_equals($efi_zipl_boot, $rootfs_part, "/boot/zipl or /boot/efi should be located on a separate partition");
-    }
-
-    record_info('CRYPT', 'Test cryptsetup functions');
-    cryptsetup_test($crypts);
-
-    record_info('PARTED', 'Partition align-check');
-    foreach my $dev (@{$drives}) {
-        for (my $i = 1; $i <= scalar @{_get_children($dev)}; $i++) {
-            assert_script_run("parted $dev->{name} align-check optimal $i");
-        }
-    }
-    record_info('PARTED', script_output qq[parted -lms]);
 }
 
 1;
