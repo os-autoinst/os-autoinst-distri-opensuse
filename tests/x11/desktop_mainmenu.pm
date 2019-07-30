f1dd58671516dd59bd17dd718f7399b18d17fa7e
Paolo Stivanin <info@paolostivanin.com>
2019-07-30 15:35:37 +0200

    desktop_mainmenu: update summary with a more descriptive text
    for usage with https://confluence.suse.com/pages/viewpage.action?spaceKey=~martinsmac&title=Script+collecting+testcase+coverage

--- tests/x11/desktop_mainmenu.pm
+++ tests/x11/desktop_mainmenu.pm
@@ -1,15 +1,15 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Test desktop main menue shows up.
-#   Support for different WMs/DEs
+# Summary: Test that desktop main menu shows up (support multiple DEs/WMs)
+# - open main menu and check that it matches the needle
 # Maintainer: Oliver Kurz <okurz@suse.de>
 
 use base "x11test";
