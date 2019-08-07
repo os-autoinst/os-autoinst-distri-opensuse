6ba2ed34d6005d17df8ba6dbf1614e3a8dddbdd8
Jozef Pupava <jpupava@suse.com>
2019-08-07 15:38:06 +0200

    Add sdk module for soundtouch test
    - schedule for opensuse too
    - exclude from LTSS products

--- tests/console/soundtouch.pm
+++ tests/console/soundtouch.pm
@@ -18,9 +18,25 @@ use strict;
 use warnings;
 use testapi;
 use utils;
+use registration qw(cleanup_registration register_product add_suseconnect_product get_addon_fullname remove_suseconnect_product);
+use version_utils 'is_sle';
 
 sub run {
     select_console 'root-console';
+    # development module needed for dependencies, released products are tested with sdk module
+    if (is_sle) {
+        if (get_var('BETA')) {
+            my $sdk_repo = is_sle('15+') ? get_var('REPO_SLE_MODULE_DEVELOPMENT_TOOLS') : get_var('REPO_SLE_SDK');
+            zypper_ar 'http://' . get_var('OPENQA_URL') . "/assets/repo/$sdk_repo", name => 'SDK';
+        }
+        # maintenance updates are registered with sdk module
+        elsif (get_var('FLAVOR') !~ /Updates|Incidents/) {
+            cleanup_registration;
+            register_product;
+            add_suseconnect_product('sle-module-desktop-applications') if is_sle('15+');
+            add_suseconnect_product(get_addon_fullname('sdk'));
+        }
+    }
     zypper_call "in soundtouch";
     zypper_call "in alsa-utils";
 
@@ -37,5 +53,15 @@ sub run {
     assert_script_run 'aplay soundtouch/1d5d9dD_rate.wav soundtouch/1d5d9dD_tempo-and-pitch.wav soundtouch/bar_bpm.wav';
     assert_recorded_sound 'soundtouch';
     assert_script_run 'rm -rf soundtouch';
+    # unregister SDK
+    if (is_sle) {
+        select_console 'root-console';
+        if (get_var('BETA')) {
+            zypper_call "rr SDK";
+        }
+        elsif (get_var('FLAVOR') !~ /Updates|Incidents/) {
+            remove_suseconnect_product(get_addon_fullname('sdk'));
+        }
+    }
 }
 1;
