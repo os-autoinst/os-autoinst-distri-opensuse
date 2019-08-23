b8609332cca0d1c71d7498dc456d826d485808bb
hjluo <hluo@suse.com>
2019-08-23 09:54:19 +0800

    Add autofs service check

--- tests/console/autofs.pm
+++ tests/console/autofs.pm
@@ -26,32 +26,13 @@ use base "consoletest";
 use strict;
 use warnings;
 use testapi;
-use version_utils qw(is_sle is_jeos);
-use autofs_utils qw(setup_autofs_server check_autofs_service);
-use utils qw(systemctl zypper_call);
+use autofs_utils;
 
 sub run {
     select_console 'root-console';
-    # mkisofs is not distributed in JeOS based on sle12
-    my $mk_iso_tool = (is_jeos and is_sle('<15')) ? 'genisoimage' : 'mkisofs';
-    zypper_call("in autofs $mk_iso_tool") if (is_sle('15+') or is_jeos);
-    my $autofs_conf_file       = '/etc/auto.master';
-    my $autofs_map_file        = '/etc/auto.master.d/autofs_regression_test.autofs';
-    my $test_conf_file         = '/etc/auto.iso';
-    my $test_mount_dir         = '/mnt/test_autofs_local';
-    my $file_to_mount          = '/tmp/test-iso.iso';
-    my $test_conf_file_content = "echo  iso     -fstype=auto,ro         :$file_to_mount > $test_conf_file";
-    assert_script_run("mkdir -p $test_mount_dir");
-    assert_script_run("dd if=/dev/urandom of=$test_mount_dir/README bs=4024 count=1");
-    assert_script_run("$mk_iso_tool -o $file_to_mount $test_mount_dir");
-    assert_script_run("ls -lh $file_to_mount");
-    check_autofs_service();
-    assert_script_run("test -f $autofs_conf_file");
-    setup_autofs_server(autofs_conf_file => $autofs_conf_file, autofs_map_file => $autofs_map_file, test_conf_file => $test_conf_file, test_conf_file_content => $test_conf_file_content, test_mount_dir => $test_mount_dir);
-    systemctl 'restart autofs';
-    assert_script_run("ls $test_mount_dir/iso");
-    my $mount_output_triggered = script_output("mount | grep -e $file_to_mount");
-    die "Something went wrong, target is already mounted" unless $mount_output_triggered =~ /$file_to_mount/;
+    autofs_utils::configure_service('function');
+    autofs_utils::check_function();
+    autofs_utils::do_cleanup();
 }
 
 1;
