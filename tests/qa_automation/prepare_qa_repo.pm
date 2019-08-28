os-autoinst/os-autoinst-distri-opensuse

commit f1c4edc7eab04f758505902fa9810b897fb39a5f

repo_tools: Add add_qa_{head,web}_repo() and use it
using get_required_var() as QA_HEAD_REPO and QA_WEB_REPO are usually
defined for SLES on medium level for all jobs, if it is missing, it is
incorrect configuration at medium level.
(That's why it's now required in tests/virt_autotest/pvusb_run.pm).

tests/qa_automation/prepare_qa_repo.pm completely replaced by
add_qa_head_repo() and add_qa_web_repo().

Use add_qa_head_repo() also in tests/kernel_performance/install_qatestset.pm,
instead of custom QA_REPO variable.

NOTE: not using add_qa_head_repo() in:
* virt_autotest/install_package.pm
add_qa_head_repo() cannot be used on lpar_cmd()
+ it'd benefit to reduce duplicity of commands used lpar vs. other arch:
there should be some wrapper to remove duplicity, but there are a few
subtle differences which needs to be kept. According to @alice-suse
'zypper rr' needs to be kept, or it will cause trouble in second
triggering, especially s390, which reuses host.

* tests/console/systemd_testsuite.pm
According to @SergioAtSUSE systemd testsuite uses different QA_HEAD_REPO
than the one from QAM. And this module is going to be removed/replaced
anyway:
https://progress.opensuse.org/issues/45158
https://github.com/os-autoinst/os-autoinst-distri-opensuse/pull/7863

products/sle/main.pm
@@ -21,6 +21,7 @@ use version_utils
 use File::Find;
 use File::Basename;
 use LWP::Simple 'head';
+use repo_tools qw(add_qa_head_repo add_qa_web_repo);
 use scheduler 'load_yaml_schedule';
 use Utils::Backends qw(is_hyperv is_hyperv_in_gui);
 use Utils::Architectures;
@@ -720,7 +721,8 @@ elsif (get_var("QA_TESTSET")) {
 }
 elsif (get_var("QA_TESTSUITE")) {
     boot_hdd_image;
-    loadtest "qa_automation/prepare_qa_repo";
+    add_qa_head_repo;
+    add_qa_web_repo;
     loadtest "qa_automation/install_test_suite";
     loadtest "qa_automation/execute_test_run";
 }

tests/qa_automation/prepare_qa_repo.pm
@@ -1,44 +1,0 @@
-# SUSE's openQA tests
-#
-# Copyright © 2017 SUSE LLC
-#
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
-#
-# Summary: the step to prepare QA:Head repository
-# Maintainer: Yong Sun <yosun@suse.com>
-
-use strict;
-use warnings;
-use testapi;
-use utils;
-use base "opensusebasetest";
-
-# Add qa head repo for kernel testing. If QA_HEAD_REPO is set,
-# remove all existing zypper repos first
-sub prepare_repos {
-    my $self         = shift;
-    my $qa_head_repo = get_required_var('QA_HEAD_REPO', '');
-    my $qa_web_repo  = get_var('QA_WEB_REPO', '');
-
-    zypper_call("--no-gpg-check ar -f $qa_head_repo qa-ibs");
-    if ($qa_web_repo) {
-        zypper_call("--no-gpg-check ar -f $qa_web_repo qa-web");
-    }
-
-    # sometimes updates.suse.com is busy, so we need to wait for possiblye retries
-    zypper_call("--gpg-auto-import-keys ref");
-}
-
-sub run {
-    select_console 'root-console';
-    prepare_repos();
-}
-
-sub test_flags {
-    return {milestone => 1, fatal => 1};
-}
-
-1;

lib/repo_tools.pm
@@ -19,10 +19,12 @@ use strict;
 use warnings;
 use testapi;
 use utils;
-use version_utils qw(is_sle is_leap is_tumbleweed);
+use version_utils qw(is_leap is_sle is_tumbleweed);
 use y2_module_consoletest;
 
 our @EXPORT = qw(
+  add_qa_head_repo
+  add_qa_web_repo
   smt_wizard
   smt_mirror_repo
   rmt_wizard
@@ -34,6 +36,26 @@ our @EXPORT = qw(
   prepare_oss_repo
   disable_oss_repo
   generate_version);
+
+=head2 add_qa_head_repo
+
+    add_qa_head_repo();
+
+Helper to add QA:HEAD repository repository (usually from IBS).
+=cut
+sub add_qa_head_repo {
+    zypper_ar(get_required_var('QA_HEAD_REPO'), 'qa-head');
+}
+
+=head2 add_qa_web_repo
+
+    add_qa_web_repo();
+
+Helper to add QA web repository repository.
+=cut
+sub add_qa_web_repo {
+    zypper_ar(get_required_var('QA_WEB_REPO'), 'qa-web');
+}
 
 =head2 get_repo_var_name
 This takes something like "MODULE_BASESYSTEM_SOURCE" as parameter

lib/systemd_testsuite_test.pm
@@ -10,7 +10,6 @@
 # Summary: library functions for setting up the tests and uploading logs in error case.
 # Maintainer: Thomas Blume <tblume@suse.com>
 
-
 package systemd_testsuite_test;
 use base "opensusebasetest";
 
@@ -19,33 +18,11 @@ use warnings;
 use known_bugs;
 use testapi;
 use power_action_utils 'power_action';
+use repo_tools 'add_qa_head_repo';
 use utils 'zypper_call';
 use version_utils qw(is_opensuse is_sle is_tumbleweed);
 
-
 sub testsuiteinstall {
-    # The isotovideo setting QA_HEAD_REPO is not mandatory.
-    # QA_HEAD_REPO is meant to override the default repos with a custom OBS repo to test changes on the test suite package.
-    my $qa_head_repo = get_var('QA_HEAD_REPO', '');
-    if (!$qa_head_repo) {
-        if (is_opensuse()) {
-            my $sub_project;
-            if (is_tumbleweed()) {
-                $sub_project = 'Tumbleweed/openSUSE_Tumbleweed/';
-            }
-            else {
-                (my $version, my $service_pack) = split('\.', get_required_var('VERSION'));
-                $sub_project = "Leap:/$version/openSUSE_Leap_$version.$service_pack/";
-            }
-            $qa_head_repo = 'https://download.opensuse.org/repositories/devel:/openSUSE:/QA:/' . $sub_project;
-        }
-        else {
-            (my $version, my $service_pack) = split('-', get_required_var('VERSION'));
-            $qa_head_repo = "http://download.suse.de/ibs/QA:/$version$service_pack/standard/";
-        }
-        die '$qa_head_repo is not set' unless ($qa_head_repo);
-    }
-
     select_console 'root-console';
 
     if (is_sle('15+')) {
@@ -57,8 +34,7 @@ sub testsuiteinstall {
     zypper_call 'in strace';
 
     # install systemd testsuite
-    zypper_call "ar $qa_head_repo systemd-testrepo";
-    zypper_call '--gpg-auto-import-keys ref';
+    add_qa_head_repo;
     zypper_call 'in systemd-qa-testsuite';
 }
 

tests/kernel/install_ltp.pm
@@ -22,15 +22,11 @@ use utils;
 use bootloader_setup qw(add_custom_grub_entries add_grub_cmdline_settings);
 use main_common 'get_ltp_tag';
 use power_action_utils 'power_action';
+use repo_tools 'add_qa_head_repo';
 use serial_terminal 'add_serial_console';
 use upload_system_log;
 use version_utils qw(is_sle is_opensuse is_jeos);
 use Utils::Backends 'use_ssh_serial_console';
-
-sub add_repos {
-    my $qa_head_repo = get_required_var('QA_HEAD_REPO');
-    zypper_ar($qa_head_repo, 'qa_repo');
-}
 
 sub add_we_repo_if_available {
     # opensuse doesn't have extensions
@@ -318,7 +314,7 @@ sub run {
         install_from_git($tag);
     }
     else {
-        add_repos;
+        add_qa_head_repo;
         install_from_repo($tag);
     }
 

tests/kernel/qa_test_klp.pm
@@ -16,6 +16,7 @@ use base 'opensusebasetest';
 use testapi;
 use utils;
 use registration;
+use repo_tools 'add_qa_head_repo';
 use version_utils 'is_sle';
 
 sub run {
@@ -29,7 +30,7 @@ sub run {
     my ($test_type) = $git_repo =~ /qa_test_(\w+).git/;
 
     (is_sle(">12-sp1") || !is_sle) ? $self->select_serial_terminal() : select_console('root-console');
-    zypper_call('ar -f -G ' . get_required_var('QA_HEAD_REPO') . ' qa_head');
+    add_qa_head_repo;
     zypper_call('in -l bats hiworkload', exitcode => [0, 106, 107]);
 
     add_suseconnect_product("sle-sdk") if (is_sle('<12-SP5'));

tests/kernel/update_kernel.pm
@@ -20,6 +20,7 @@ use version_utils 'is_sle';
 use qam;
 use kernel 'remove_kernel_packages';
 use power_action_utils 'power_action';
+use repo_tools 'add_qa_head_repo';
 use Utils::Backends 'use_ssh_serial_console';
 
 
@@ -278,21 +279,19 @@ sub run {
     }
 
     if (get_var('KGRAFT')) {
-        my $qa_head = get_required_var('QA_HEAD_REPO');
         prepare_kgraft($repo, $incident_id);
         boot_to_console($self);
 
         # dependencies for heavy load script
         if (!$wk_ker) {
-            zypper_call("ar $qa_head qa_repo");
-            zypper_call("--gpg-auto-import-keys ref");
+            add_qa_head_repo;
             zypper_call("in qa_lib_ctcs2 qa_test_ltp qa_test_newburn");
         }
         # update kgraft patch under heavy load
         update_kgraft($repo, $incident_id);
 
         if (!$wk_ker) {
-            zypper_call("rr qa_repo");
+            zypper_call("rr qa-head");
             zypper_call("rm qa_lib_ctcs2 qa_test_ltp qa_test_newburn");
         }
         power_action('reboot', textmode => 1);

tests/kernel_performance/install_qatestset.pm
@@ -7,7 +7,6 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 #
-#
 # Summary: setup performance run environment
 # Maintainer: Joyce Na <jna@suse.de>
 
@@ -18,13 +17,10 @@ use strict;
 use warnings;
 use utils;
 use testapi;
+use repo_tools 'add_qa_head_repo';
 
 sub install_pkg {
-    my $qa_server_repo = get_required_var('QA_REPO');
-    zypper_call("rr qa-ibs");
-    zypper_call(
-        "--no-gpg-check ar -f '$qa_server_repo' qa-ibs");
-    zypper_call("--no-gpg-check ref");
+    add_qa_head_repo;
     zypper_call("install qa_testset_automation");
 }
 

commit c1f043bee87be54d5a053cb76a0bc89dedea137e

Reintroduce prepare_qa_repo.pm
By putting add_qa_{head,web}_repo() helpers into
qa_automation/prepare_qa_repo.pm.

The problem wasn't with add_qa_{head,web}_repo() helpers itself, but
with the need to have select_console() call before (at least for JeOS).

This fixes JeOS testing
(sle-12-SP5-JeOS-for-kvm-and-xen-x86_64-Build4.61-jeos-fs_stress@64bit-virtio-vga)

Fixes: f1c4edc7e ("repo_tools: Add add_qa_{head,web}_repo() and use it")
Reported-by: Martin Loviska <mloviska@suse.com>

products/sle/main.pm
@@ -21,7 +21,6 @@ use version_utils
 use File::Find;
 use File::Basename;
 use LWP::Simple 'head';
-use repo_tools qw(add_qa_head_repo add_qa_web_repo);
 use scheduler 'load_yaml_schedule';
 use Utils::Backends qw(is_hyperv is_hyperv_in_gui);
 use Utils::Architectures;
@@ -730,8 +729,7 @@ elsif (get_var("QA_TESTSET")) {
 }
 elsif (get_var("QA_TESTSUITE")) {
     boot_hdd_image;
-    add_qa_head_repo;
-    add_qa_web_repo;
+    loadtest "qa_automation/prepare_qa_repo";
     loadtest "qa_automation/install_test_suite";
     loadtest "qa_automation/execute_test_run";
 }

tests/qa_automation/prepare_qa_repo.pm
@@ -1,0 +1,31 @@
+# SUSE's openQA tests
+#
+# Copyright © 2017-2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: the step to prepare QA:Head repository
+# Maintainer: Yong Sun <yosun@suse.com>
+
+use strict;
+use warnings;
+use testapi;
+use utils;
+use base "opensusebasetest";
+use repo_tools qw(add_qa_head_repo add_qa_web_repo);
+
+sub run {
+    my $self = shift;
+    $self->select_serial_terminal;
+    add_qa_head_repo;
+    add_qa_web_repo;
+}
+
+sub test_flags {
+    return {milestone => 1, fatal => 1};
+}
+
+1;