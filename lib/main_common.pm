fca708a554a6cbb2cddedc0e0bca211600990baa
Wang Jun <badboywj@gmail.com>
2019-09-04 11:42:12 +0800

    Add yast_users test module in cli mode.     - modified:   lib/main_common.pm     - new file:   tests/yast2_cmd/yast_users.pm

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1417,6 +1417,7 @@ sub load_extra_tests_y2uitest_cmd {
     loadtest 'yast2_cmd/yast_tftp_server';
     loadtest 'yast2_cmd/yast_ftp_server';
     loadtest 'yast2_cmd/yast_rdp' if is_sle('15+');
+    loadtest 'yast2_cmd/yast_users';
     loadtest 'yast2_cmd/yast_sysconfig';
     loadtest 'yast2_cmd/yast_keyboard';
     loadtest 'yast2_cmd/yast_nfs_server';
