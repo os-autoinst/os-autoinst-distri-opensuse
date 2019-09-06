2efb5f640e6d919288d6592636afbc64ec2654fe
Martin Loviska <mloviska@suse.com>
2019-09-06 15:03:40 +0200

    Adapt Win10 test suite to build 1903

--- tests/installation/win10_installation.pm
+++ tests/installation/win10_installation.pm
@@ -20,37 +20,43 @@ use testapi;
 
 sub run {
     # This test works onlywith CDMODEL=ide-cd due to windows missing scsi drivers which are installed via scsi iso
-    if (get_var('UEFI')) {
-        assert_screen 'windows-boot';
-        send_key 'spc';    # boot from CD or DVD
-    }
-    assert_screen 'windows-setup', 60;
-    send_key 'alt-n';      # next
-    send_key 'alt-i';      # install Now
-    send_key 'alt-n';      # next
+    assert_screen 'windows-setup', 1000;
+    send_key 'alt-n';    # next
+    save_screenshot;
+    send_key 'alt-i';    # install Now
+    save_screenshot;
+    send_key 'alt-n';    # next
     assert_screen 'windows-activate';
-    send_key 'alt-i';      # I dont have the product key
+    send_key 'alt-i';    # I dont have the product key
     assert_screen 'windows-select-system';
-    send_key 'alt-n';      # select OS (Win 10 Pro)
+    send_key_until_needlematch('windows-10-pro', 'down');
+    send_key 'alt-n';    # select OS (Win 10 Pro)
     assert_screen 'windows-license';
-    send_key 'alt-a';      # accept eula
-    send_key 'alt-n';      # next
+    send_key 'alt-a';    # accept eula
+    send_key 'alt-n';    # next
     assert_screen 'windows-installation-type';
-    send_key 'alt-c';      # custom
+    send_key 'alt-c';    # custom
     assert_screen 'windows-disk-partitioning';
-    send_key 'alt-l';      # load driver
+    send_key 'alt-l';    # load driver
     assert_screen 'windows-load-driver';
-    send_key 'alt-b';      # browse button
+    send_key 'alt-b';    # browse button
     send_key 'c';
-    send_key 'c';          # go to second CD drive with drivers
-    send_key 'ret';        # ok
+    save_screenshot;
+    send_key 'c';        # go to second CD drive with drivers
+    send_key 'right';    # ok
+    sleep 0.5;
+    send_key 'ret';
     send_key_until_needlematch 'windows-all-drivers-selected', 'shift-down', 5;    # select all drivers
-    wait_still_screen;
+    sleep 0.5;
     send_key 'alt-n';                                                              # next
     wait_still_screen;
     send_key 'alt-n';                                                              # next ->Installing windows!
     assert_screen 'windows-restart', 600;
     send_key 'alt-r';                                                              # restart
+}
+
+sub test_flags {
+    return {fatal => 1};
 }
 
 1;
