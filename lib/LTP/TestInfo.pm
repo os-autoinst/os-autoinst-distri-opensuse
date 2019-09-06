0ac097db21975009aa7b36c0b3635ee6d2c50940
Martin Doucha <martin.doucha@suse.com>
2019-09-06 16:42:57 +0200

    Use correct runfile name in ltp_run

--- lib/LTP/TestInfo.pm
+++ lib/LTP/TestInfo.pm
@@ -18,6 +18,7 @@ use Mojo::Base 'OpenQA::Test::RunArgs';
 our @EXPORT_OK = qw(testinfo);
 use Exporter 'import';
 
+has 'runfile';
 has 'test';
 has test_result_export => sub { die 'Require test_result_export hashref'; };
 
