43c59f17d8b9e9590cc409444109e691b283415f
Alynx Zhou <alynx.zhou@gmail.com>
2019-09-11 11:44:48 +0800

    Fix issue while file does not exist

--- lib/services/postfix.pm
+++ lib/services/postfix.pm
@@ -40,7 +40,7 @@ sub check_service {
 # check postfix function
 sub check_function {
     # Clear mailbox
-    assert_script_run("rm /var/spool/mail/$testapi::username");
+    type_string("rm /var/spool/mail/$testapi::username\n");
     # Send testing mail
     assert_script_run("echo 'Mail body' | mailx -v -s 'openQA Testing' $testapi::username\@localhost");
     # Flush mail queue to ensure mail has been sent
