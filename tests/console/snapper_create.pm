a85d33aa06eb2d97a990348fca9ec34684b5b7c6
Dominik Heidler <dheidler@suse.de>
2019-09-11 14:20:09 +0200

    Fix snapper_create test

--- tests/console/snapper_create.pm
+++ tests/console/snapper_create.pm
@@ -21,12 +21,12 @@ use utils;
 # that we got what we expect. See poo#25716
 sub get_last_snap_number {
     # get snapshot id column, parse output in perl to avoid SIGPIPE
-    my $snap_head = (split(/\n/, script_output("snapper list")))[0];
+    my $snap_head = script_output("snapper list");
     # strip kernel messages - for some reason we always get something like this at this very position:
     # [ 1248.663412] BTRFS info (device vda2): qgroup scan completed (inconsistency flag cleared)
     my @lines = split(/\n/, $snap_head);
     @lines = grep(/\|/, @lines);
-    die "Unable to receive snapshot list column header line" unless (@lines);
+    die "Unable to receive snapshot list column header line - got this output: $snap_head" unless (@lines);
     $snap_head = $lines[0];
 
     my $snap_col_found    = 0;
