0560a02e49f4b927f0284c5efc6ee07abab668f8
root <root@d503.qam.suse.de>
2019-09-11 12:55:11 +0200

    Add test for suse-module-tools

---
+++ tests/console/suse_module_tools.pm
@@ -0,0 +1,45 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Testing functionality of modhash and modsign-verify commands.
+# A random module from available loadable modules on the system is picked
+# for testing purposes.
+# * modhash: the output is checked for correct format
+# * modsign-verify: the output is checked for correct format
+# Maintainer: Vasileios Anastasiadis <vasilios.anastasiadis@suse.com>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use utils 'zypper_call';
+use version_utils qw(is_sle is_tumbleweed);
+
+sub run {
+    select_console 'root-console';
+
+    # Find a random module available on the system
+    # Get a list of all modules
+    my $mds = script_output("find /lib/modules/\$(uname -r) -type f -name '*.ko'");
+    # Get the first module on the list
+    my $pth = (split '\n', $mds)[0];
+
+    # Test modhash command
+    if (!is_sle('=12-sp1') && !is_sle('=12-sp5') && !is_tumbleweed()) {
+        # Get the output and test it against correct output
+        assert_script_run("modhash $pth | grep -E \"$pth: [0-9a-fA-F]+\"");
+    }
+    # testing modsign-verify command
+    if (is_sle('<15-sp1')) {
+        # Run command and grep correct output
+        assert_script_run("modsign-verify $pth | grep 'good signature\\|bad signature\\|certificate not found\\|module not signed\\|other error'");
+    }
+}
+
+1;
