1fae4dcd58c6b547c557d243ca5deabbe201a341
Jonathan Rivrain <JRivrain@suse.com>
2019-09-19 15:05:16 +0200

    fake

--- tests/boot/fake.pm
+++ tests/boot/fake.pm
@@ -1,6 +1,8 @@
-
+use base "bootbasetest";
 use strict;
 use warnings;
+use testapi;
+use bootloader_setup qw(select_bootmenu_more ensure_shim_import);
 
 sleep(65468713216761321354654130546785254656465468764321541314653146846521544686546846453203219887464161674621143);
 
