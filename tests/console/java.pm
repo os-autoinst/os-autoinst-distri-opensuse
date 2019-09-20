f10c5f0261b3ad0733840c9db3f439a0c6600692
Jozef Pupava <jpupava@suse.com>
2019-09-20 12:55:46 +0200

    Use zypper_call instead of script_retry to pass on exit 0
    Move record_soft_failure into zypper_call as exception for this java bug is needed

--- tests/console/java.pm
+++ tests/console/java.pm
@@ -30,8 +30,7 @@ sub run {
     pkcon_quit;
 
     if (check_var("DISTRI", "sle")) {
-        if (script_retry('zypper -n in --auto-agree-with-licenses java-*', timeout => 500, expect => 4, retry => 5) == 4) {
-            record_soft_failure 'bsc#1137466';
+        if (zypper_call('in --auto-agree-with-licenses java-*', timeout => 500, exitcode => [0, 4])) {
             # install only java-11-openjdk* & java-*-ibm*
             zypper_call('in --auto-agree-with-licenses java-11-openjdk* java-*-ibm*');
         }
