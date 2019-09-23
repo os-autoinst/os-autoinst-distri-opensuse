43a4ed4f7c15c79cd2a29a536dee6cc1ce8dabf3
Alynx Zhou <alynx.zhou@gmail.com>
2019-09-23 10:29:19 +0800

    Update summary

--- tests/console/check_system_info.pm
+++ tests/console/check_system_info.pm
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 #
-# Summary: Check system info like `/etc/issue` or `/etc/os-release`.
+# Summary: Verify milestone version and display some info.
 # Maintainer: Alynx Zhou <alynx.zhou@suse.com>
 
 use base "consoletest";
