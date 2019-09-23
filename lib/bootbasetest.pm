77f5b73939b7eef9e4685a7d5c804d20a45e33cd
Santiago Zarate <santiago.zarate@suse.com>
2019-09-23 13:02:00 +0200

    Call post_fail_hooks in order

--- lib/bootbasetest.pm
+++ lib/bootbasetest.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 sub post_fail_hook {
+    my ($self) = @_;
     # check for text login to check if X has failed
     if (check_screen('generic-login')) {
         record_info 'Seems that the display manager failed';
@@ -25,9 +26,11 @@ sub post_fail_hook {
 
     # crosscheck for text login on tty1
     select_console 'root-console';
-
     # collect and upload some stuff
-    export_logs();
+    $self->export_logs();
+    # call parent's post fail hook
+    $self->SUPER::post_fail_hook;
+
 }
 
 1;
