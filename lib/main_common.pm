79cf460c007e5af796e3719fb65358862336d060
Tony yuan <tyuan@suse.com>
2019-09-24 19:59:54 +0800

    Regression test for yast dns server command line

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1431,6 +1431,7 @@ sub load_extra_tests_y2uitest_cmd {
     loadtest 'yast2_cmd/yast_keyboard';
     loadtest 'yast2_cmd/yast_nfs_server';
     loadtest 'yast2_cmd/yast_nfs_client';
+    loadtest 'yast2_cmd/yast_dns_server';
 
     #temporary runs for QAM while tests under y2uitest_ncurses are being ported
     loadtest "console/yast2_apparmor";
