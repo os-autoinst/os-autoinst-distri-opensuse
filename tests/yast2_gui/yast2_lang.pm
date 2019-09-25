719b6d06ae36b1fa85c261291b64df4c23526c07
Oleksandr Orlov <oleksandr.orlov1@gmail.com>
2019-09-25 16:45:30 +0200

    Fix language selection in yast2_lan on aarch64
    Select "Detailed Locale Settings" drop down and press 'up' until the preferred
    language is selected, as the module sporadically failed to click the drop down
    with assert_and_click on aarch64.
    
    Related ticket: https://progress.opensuse.org/issues/56039

--- tests/yast2_gui/yast2_lang.pm
+++ tests/yast2_gui/yast2_lang.pm
@@ -26,9 +26,9 @@ sub run {
 
     # check language details and change detailed locale setting
     assert_and_click 'yast2-lang_details';
-    assert_and_click 'yast2-lang_detailed-locale-setting';
-    assert_and_click 'yast2-lang_detailed-locale-setting_en_GB';
-    assert_screen 'yast2-lang_detailed-locale-setting_changed';
+    assert_screen 'yast2-lang_detailed-locale-setting';
+    send_key 'alt-d';
+    send_key_until_needlematch 'yast2-lang_detailed-locale-setting_changed', 'up';
     send_key 'alt-o';
 
     # change adapt time zone to German
