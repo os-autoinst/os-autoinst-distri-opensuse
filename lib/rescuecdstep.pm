079dc161d62ad0e8ba5f3a5b0845665f722d6cf5
Zaoliang Luo <zluo@suse.de>
2019-09-25 10:41:23 +0200

    Add documentation for lib/rescuecdstep.pm
    see https://progress.opensuse.org/issues/57332

--- lib/rescuecdstep.pm
+++ lib/rescuecdstep.pm
@@ -1,11 +1,21 @@
+=head1 rescuecdstep
+
+Base class for all RESCUECD tests
+
+=cut
 package rescuecdstep;
 use base "opensusebasetest";
 use testapi;
 use strict;
 use warnings;
 
-# Base class for all RESCUECD tests
+=head2 test_flags
 
+ test_flags();
+
+Return test flag fatal => 1
+
+=cut
 sub test_flags {
     return {fatal => 1};
 }
