os-autoinst/os-autoinst-distri-opensuse

commit ea1ac9f410a0de573a720522d5d64b7f270c1de0

Separate tidy targets
Calling 'make tidy' seems more natural than tools/tidy.

Makefile
@@ -32,8 +32,12 @@ tools/lib/: os-autoinst/
 check-links: tools/tidy tools/lib/ os-autoinst/
 
 .PHONY: check-links
-tidy: check-links
+tidy-check: check-links
 	tools/tidy --check
+
+.PHONY: tidy
+tidy:
+	tools/tidy
 
 .PHONY: unit-test
 unit-test:
@@ -77,7 +81,7 @@ test-no-wait_idle:
 	@! git --no-pager grep wait_idle lib/ tests/
 
 .PHONY: test-static
-test-static: tidy test-merge test-dry test-no-wait_idle test-unused-modules test-soft_failure-no-reference
+test-static: tidy-check test-merge test-dry test-no-wait_idle test-unused-modules test-soft_failure-no-reference
 
 .PHONY: test
 ifeq ($(TESTS),compile)

commit dab5cd0e4d717014245ca4fa96c4828dd1100cce

Makefile: Fix dependencies of 'tidy' target (regression in ea1ac9f41)

Makefile
@@ -36,8 +36,8 @@ tidy-check: check-links
 	tools/tidy --check
 
 .PHONY: tidy
-tidy:
-	tools/tidy
+tidy: tools/tidy
+	$<
 
 .PHONY: unit-test
 unit-test: