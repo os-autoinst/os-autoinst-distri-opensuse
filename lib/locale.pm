ec0b546e9ec51de32cdf0bdc869aeefb5af1c413
Zaoliang Luo <zluo@suse.de>
2019-10-18 09:23:41 +0200

    Add post_fail_hook in lib/locale.pm
    see https://progress.opensuse.org/issues/56636
    The problem is that no logs collected and uploaded for test module
    keymap_or_locale.
    
    verification test:
    http://f40.suse.de/tests/5167#step/keymap_or_locale/12
    check uploaded logs at Logs& Assets.

--- lib/locale.pm
+++ lib/locale.pm
@@ -75,4 +75,13 @@ sub get_keystroke_list {
     return $keystrokes{$layout};
 }
 
+# post_fail_hook for upload logs for test module which uses it as base
+# see https://progress.opensuse.org/issues/56636
+sub post_fail_hook {
+    my ($self) = shift;
+    select_console('log-console');
+    $self->SUPER::post_fail_hook;
+    $self->export_logs_basic;
+}
+
 1;
