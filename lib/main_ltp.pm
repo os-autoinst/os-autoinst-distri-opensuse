02d2e76d1778ac2e236a7b6d61bac2ff7f6b8899
Petr Cervinka <pcervinka@suse.com>
2019-10-31 12:34:05 +0100

    Add spvm boot support for LTP
    Enhancement poo#44309: LTP has own independent boot procedure, this
    change will allow to boot LTP tests on PowerVM.

--- lib/main_ltp.pm
+++ lib/main_ltp.pm
@@ -25,6 +25,7 @@ use LTP::TestInfo 'testinfo';
 use File::Basename 'basename';
 use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag load_boot_tests load_inst_tests load_reboot_tests);
 use 5.018;
+use Utils::Backends 'is_spvm';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"
 # use warnings;
 
@@ -132,6 +133,7 @@ sub load_kernel_tests {
     load_bootloader_s390x();
 
     if (get_var('INSTALL_LTP')) {
+        loadtest "../installation/bootloader" if is_spvm;
         if (get_var('INSTALL_KOTD')) {
             loadtest 'install_kotd';
         }
@@ -150,6 +152,7 @@ sub load_kernel_tests {
         shutdown_ltp();
     }
     elsif (get_var('LTP_COMMAND_FILE')) {
+        loadtest "../installation/bootloader" if is_spvm;
         if (get_var('INSTALL_KOTD')) {
             loadtest 'install_kotd';
         }
