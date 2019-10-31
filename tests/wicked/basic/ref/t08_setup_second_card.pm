983b93caaf34e745deca53bf445115827a15f549
Jose Lausuch <jalausuch@suse.com>
2019-10-31 11:45:43 +0100

    Wicked: fix t08 from basic suite

--- tests/wicked/basic/ref/t08_setup_second_card.pm
+++ tests/wicked/basic/ref/t08_setup_second_card.pm
@@ -22,11 +22,14 @@ use lockapi;
 sub run {
     my ($self, $ctx) = @_;
     record_info('Info', 'Set up a second card');
-    assert_script_run(sprintf("ip a a %s/15 dev %s", $self->get_ip(type => 'second_card'), $ctx->iface()));
+    assert_script_run(sprintf("ip a a %s/24 dev %s", $self->get_ip(type => 'dhcp_2nic'),   $ctx->iface()));
+    assert_script_run(sprintf("ip a a %s/24 dev %s", $self->get_ip(type => 'second_card'), $ctx->iface()));
     systemctl 'stop dhcpd.service';
     $self->get_from_data('wicked/dhcp/dhcpd_2nics.conf', '/etc/dhcpd.conf');
     systemctl 'start dhcpd.service';
     $self->wait_for_dhcpd();
+    # Enable forwarding to reach the gateway from SUT
+    assert_script_run('sysctl -w net.ipv4.ip_forward=1');
     die("Create mutex failed") unless mutex_create('t08_dhcpd_setup_complete');
 }
 
