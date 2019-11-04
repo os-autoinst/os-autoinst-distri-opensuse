e79799f2099d0c1bc2df657c01c07f94a3ff79bb
Jozef Pupava <jpupava@suse.com>
2019-11-04 11:06:03 +0100

    Fix bind test on s390x

--- data/qam/handle_bind_source_dependencies.sh
+++ data/qam/handle_bind_source_dependencies.sh
@@ -3,6 +3,7 @@ set -eox pipefail
 . /etc/os-release
 SUSECONNECT=/tmp/SUSEConnect.txt
 MAJOR_VERSION=$(echo $VERSION_ID|cut -c1-2)
+ARCH=$(uname -m)
 
 function is_sle12 {
     [[ $VERSION_ID =~ 12 ]]
@@ -26,14 +27,14 @@ if [ -e /tmp/REMOVE_ADDED_PRODUCTS ]; then
     if [ -e /tmp/REMOVE_PC ]; then
         # starting with 15.1 public cloud module started to use VERSION_ID instead of just MAJOR_VERSION
         if [[ $VERSION_ID = 15.[12345] ]]; then
-            SUSEConnect -d -p sle-module-public-cloud/$VERSION_ID/x86_64
+            SUSEConnect -d -p sle-module-public-cloud/$VERSION_ID/$ARCH
         else
-            SUSEConnect -d -p sle-module-public-cloud/$MAJOR_VERSION/x86_64
+            SUSEConnect -d -p sle-module-public-cloud/$MAJOR_VERSION/$ARCH
         fi
         rm -f /tmp/REMOVE_PC
     fi
     if [ -e /tmp/REMOVE_SDK ]; then
-        SUSEConnect -d -p $SDK/$VERSION_ID/x86_64
+        SUSEConnect -d -p $SDK/$VERSION_ID/$ARCH
         rm -f /tmp/REMOVE_SDK
     fi
     if is_sle12; then
@@ -48,14 +49,14 @@ else
     if ! grep sle-module-public-cloud $SUSECONNECT; then
         # starting with 15.1 public cloud module started to use VERSION_ID instead of just MAJOR_VERSION
         if [[ $VERSION_ID = 15.[12345] ]]; then
-            SUSEConnect -p sle-module-public-cloud/$VERSION_ID/x86_64
+            SUSEConnect -p sle-module-public-cloud/$VERSION_ID/$ARCH
         else
-            SUSEConnect -p sle-module-public-cloud/$MAJOR_VERSION/x86_64
+            SUSEConnect -p sle-module-public-cloud/$MAJOR_VERSION/$ARCH
         fi
         touch /tmp/REMOVE_PC
     fi
     if ! grep $SDK $SUSECONNECT; then
-        SUSEConnect -p $SDK/$VERSION_ID/x86_64
+        SUSEConnect -p $SDK/$VERSION_ID/$ARCH
         touch /tmp/REMOVE_SDK
     fi
     if is_sle12; then
