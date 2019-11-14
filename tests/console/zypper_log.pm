6b0c6ddba6be27db237f0e87616f489a884670a7
Dominik Heidler <dheidler@suse.de>
2019-11-14 10:46:31 +0100

    Fix zypper_log for jeos and live

--- tests/console/zypper_log.pm
+++ tests/console/zypper_log.pm
@@ -15,14 +15,21 @@ use strict;
 use warnings;
 use testapi;
 use utils;
+use version_utils;
 
 sub run {
     select_console 'root-console';
+
+    # zypper-log is recommended by zypper (but that doesn't count on livesystems and on jeos)
+    if (is_jeos || is_livecd) {
+        zypper_call 'in zypper-log';
+    }
 
     if (script_run('zypper-log') != 0) {
         record_soft_failure 'bsc#1156158: zypper-log needs python2 but only python3 is installed by default';
         zypper_call 'in python2';
     }
+
     script_run("zypper some_wrong_arg_that_will_show_up_in_the_log");
     assert_script_run("zypper-log | grep some_wrong_arg_that_will_show_up_in_the_log");
     assert_script_run("zypper-log -r3");
