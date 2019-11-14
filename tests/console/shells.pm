c6acd2ce40f88b8b41064bb554493b746afb8ade
slemke <slemke@suse.cz>
2019-11-14 16:19:35 +0100

    Add specialization for tcsh test

--- tests/console/shells.pm
+++ tests/console/shells.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2017 SUSE LLC
+# Copyright 2017-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -10,10 +10,15 @@
 # Summary: Test all officially SLE supported shells
 # - Install tcsh and zsh (when supported)
 # - Run ksh -c "print hello" | grep hello (when supported)
-# - Run 'tcsh -c "printf \'hello\n\'" | grep hello'
 # - Run 'csh -c "printf \'hello\n\'" | grep hello'
 # - Run 'zsh -c "echo hello" | grep hello'
 # - Run 'sh -c "echo hello" | grep hello'
+#
+# - tcsh:
+# - Run 'tcsh -c "printf \'hello\n\'" | grep hello'
+# - Check if ~ is expanding correctly
+# - Check if ~/.tcsh is correctly sourced
+# - Check if directories are listed correctly with ls -d /*
 # Maintainer: Oliver Kurz <okurz@suse.de>
 
 use base "consoletest";
@@ -35,6 +40,36 @@ sub run() {
     assert_script_run 'csh -c "printf \'hello\n\'" | grep hello';
     assert_script_run 'zsh -c "echo hello" | grep hello';
     assert_script_run 'sh -c "echo hello" | grep hello';
+    tcsh_extra_tests();
+}
+
+#tcsh specializated test for bsc#1154877 && poo#59354:
+sub tcsh_extra_tests {
+    select_console 'root-console';
+    script_run 'useradd -s /usr/bin/tcsh -d /home/tux -m tux';
+    script_run 'su - tux', 0;
+
+    #Generate some outputs for the new created user:
+    type_string "echo \"echo Sourced!\" >> ~/.tcsh\n";
+
+    type_string "grep tux /etc/passwd > /tmp/tcsh\n";
+    type_string "echo \$SHELL >> /tmp/tcsh\n";
+    type_string "echo ~ >> /tmp/tcsh\n";
+    type_string "source ~/.tcsh >> /tmp/tcsh\n";
+    type_string "ls -d /* |grep -wc '/bin\\|/home\\|/root\\|/lib\\|/usr\\|/var\\|/proc\\|/sys\\|/boot\\|/sbin\\|/tmp\\|/opt' >> /tmp/tcsh\n";
+
+    #Go back to root/openqa and do the validations:
+    script_run 'logout', 0;
+
+    validate_script_output 'grep -c tux:x:1001:100::/home/tux:/usr/bin/tcsh /tmp/tcsh', sub { /1/ };
+    validate_script_output 'grep -c ^/usr/bin/tcsh /tmp/tcsh',                          sub { /1/ };
+    validate_script_output 'grep -c ^/home/tux /tmp/tcsh',                              sub { /1/ };
+    validate_script_output 'grep -c Sourced! /tmp/tcsh',                                sub { /1/ };
+    validate_script_output 'grep 12 /tmp/tcsh',                                         sub { /12/ };
+
+    #cleanup:
+    script_run 'rm /tmp/tcsh ~/.tcsh';
+    script_run 'userdel tux';
 }
 
 1;
