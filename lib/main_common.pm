dbe9356ce554ee03299a08a558d650672198b223
Fabian Vogt <fvogt@suse.com>
2019-11-16 11:54:00 +0100

    Don't run plasma_browser_integration on live tests
    It's incredibly unreliable, probably because the browser cache (so also the video data) ends up in the ramdisk.
    
    Fixes poo#59867

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1295,7 +1295,9 @@ sub load_x11tests {
         }
     }
     if (kdestep_is_applicable()) {
-        loadtest "x11/plasma_browser_integration" if (is_tumbleweed || is_leap("15.1+"));
+        if((is_tumbleweed || is_leap("15.1+")) && !get_var('LIVECD')) {
+            loadtest "x11/plasma_browser_integration";
+        }
         loadtest "x11/khelpcenter";
         if (get_var("PLASMA5")) {
             loadtest "x11/systemsettings5";
