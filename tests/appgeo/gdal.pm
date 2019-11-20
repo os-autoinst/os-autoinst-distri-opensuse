7c4495260b6aa15f9799dcc0b15c40326b709b0c
Guillaume Gardet <guillaume.gardet@arm.com>
2019-11-20 15:16:48 +0100

    Add tests of geo applications

---
+++ tests/appgeo/gdal.pm
@@ -0,0 +1,25 @@
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Install gdal and perform tests
+# Maintainer: Guillaume <guillaume@opensuse.org>
+
+use strict;
+use warnings;
+use base "consoletest";
+use testapi;
+use utils;
+
+sub run {
+    my ($self) = @_;
+
+    $self->select_serial_terminal;
+    zypper_call 'in gdal';
+
+    select_console 'user-console';
+    validate_script_output("gdalinfo --formats", sub { m/BMP -raster-/ });
+}
+
+1;
