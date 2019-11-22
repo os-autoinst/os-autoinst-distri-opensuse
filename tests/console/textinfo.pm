2524928a13a0f7a89c2b3f188d521924df2f5f4d
Jozef Pupava <jpupava@suse.com>
2019-11-22 10:21:00 +0100

    Increase timout for collection of system informations

--- tests/console/textinfo.pm
+++ tests/console/textinfo.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2016 SUSE LLC
+# Copyright © 2012-2019 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -42,7 +42,7 @@ sub run {
     # If we're doing this test as the user root, we will not find the textinfo script
     # in /home/root, so we'll set $home with the appropiate home directory
     my $home = $username eq 'root' ? '/root' : "/home/$username";
-    assert_script_run("$home/data/textinfo 2>&1 | tee /tmp/info.txt");
+    assert_script_run("$home/data/textinfo 2>&1 | tee /tmp/info.txt", 150);
     upload_logs("/tmp/info.txt");
     upload_logs("/tmp/logs.tar.bz2");
 }
