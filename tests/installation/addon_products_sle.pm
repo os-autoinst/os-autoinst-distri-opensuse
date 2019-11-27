df4561e7bdb77eea458b515cc1b6d07810e161f2
Sergio Lindo <sergiolindo.empresa@gmail.com>
2019-11-27 14:16:35 +0100

    Extract next statement from the function
    Put next inside the visible loop block.

--- tests/installation/addon_products_sle.pm
+++ tests/installation/addon_products_sle.pm
@@ -78,7 +78,7 @@ sub handle_all_packages_medium {
     my @addons_with_license = qw(ha we);
     my @addons_license_tags = ();
     for my $i (@addons) {
-        skip_package_hub_if_necessary($i);
+        next if (skip_package_hub_if_necessary($i));
         push @addons_license_tags, "addon-license-$i" if grep(/^$i$/, @addons_with_license);
         send_key 'home';
         send_key_until_needlematch "addon-products-all_packages-$i-highlighted", 'down', 30;
@@ -112,7 +112,7 @@ sub handle_all_packages_medium {
     assert_screen "addon-products-nonempty";
     # Confirm all required addons are properly added
     foreach (@addons) {
-        skip_package_hub_if_necessary($_);
+        next if (skip_package_hub_if_necessary($_));
         send_key 'home';
         send_key_until_needlematch "addon-products-$_", 'down';
     }
