os-autoinst/os-autoinst-distri-opensuse

commit e288e39cd1b6293fdd71bbec188ee586ec90aab6

Fix gnome case due to is_sle api change

lib/opensusebasetest.pm
@@ -7,7 +7,7 @@ use strict;
 use utils;
 use lockapi 'mutex_wait';
 use serial_terminal 'get_login_message';
-use version_utils qw(is_sle is_leap is_upgrade is_aarch64_uefi_boot_hdd);
+use version_utils qw(is_sle is_leap is_upgrade is_aarch64_uefi_boot_hdd is_tumbleweed);
 use isotovideo;
 use IO::Socket::INET;
 
@@ -499,6 +499,9 @@ sub wait_boot {
             #assert_screen "dm-password-input", 10;
             elsif (check_var('DESKTOP', 'gnome')) {
                 # In GNOME/gdm, we do not have to enter a username, but we have to select it
+                if (is_tumbleweed) {
+                    send_key 'tab';
+                }
                 send_key 'ret';
             }
 

lib/x11test.pm
@@ -608,8 +608,7 @@ sub firefox_check_default {
     my $stilltime = get_var('TIMEOUT_SCALE') ? 100 : 10;
     wait_still_screen $stilltime, 100;
     # Set firefox as default browser if asked
-    assert_screen [qw(firefox_default_browser firefox-url-loaded)], 300;
-    if (match_has_tag('firefox_default_browser')) {
+    if (check_screen('firefox_default_browser')) {
         wait_screen_change {
             assert_and_click 'firefox_default_browser_yes';
         };

tests/x11/gnomecase/application_starts_on_login.pm
@@ -18,20 +18,18 @@ use version_utils qw(is_leap is_sle is_tumbleweed);
 
 sub tweak_startupapp_menu {
     my ($self) = @_;
-    unless (is_sle('15+')) {
+    if (is_tumbleweed) {
+        x11_start_program 'gnome-tweaks';
+    }
+    elsif (is_sle('15+')) {
+        # tweak-tool entry is not in gnome-control-center of SLE15;
+        x11_start_program 'gnome-tweak-tool';
+    }
+    else {
         $self->start_gnome_settings;
         type_string "tweak";
         assert_screen "settings-tweak-selected";
         send_key "ret";
-    }
-    else {
-        # tweak-tool entry is not in gnome-control-center of SLE15;
-        if (is_tumbleweed) {
-            x11_start_program 'gnome-tweaks';
-        }
-        else {
-            x11_start_program 'gnome-tweak-tool';
-        }
     }
     assert_screen "tweak-tool";
     # increase the default timeout - the switching can be slow
@@ -40,19 +38,20 @@ sub tweak_startupapp_menu {
 
 sub start_dconf {
     my ($self) = @_;
-    unless (is_sle('15+')) {
+
+    if (is_tumbleweed || is_sle('15+')) {
+        # dconf-editor entry is not in gnome-control-center of SLE15;
+        x11_start_program 'dconf-editor';
+    }
+    else {
         $self->start_gnome_settings;
         type_string "dconf";
         assert_screen "settings-dconf";
         send_key "ret";
     }
-    else {
-        # dconf-editor entry is not in gnome-control-center of SLE15;
-        x11_start_program 'dconf-editor';
-    }
+
     # dconf-editor can show the notice to be careful after the main window
     # popped up so we have to wait for it to settle down
-    wait_still_screen(3);
     assert_screen([qw(dconf-editor will-be-careful)]);
     if (match_has_tag('will-be-careful')) {
         assert_and_click 'will-be-careful';
@@ -108,7 +107,7 @@ sub run {
     $self->tweak_startupapp_menu;
     assert_and_click "tweak-startapp-add";
     assert_screen "tweak-startapp-applist";
-    if (is_sle('12-SP2+')) {
+    if (is_sle('12-SP2+') || is_tumbleweed) {
         assert_and_click "startupApp-searching";
         wait_still_screen;
         assert_screen "focused-on-search";
@@ -137,13 +136,7 @@ sub run {
     #remove firefox from startup application
     $self->tweak_startupapp_menu;
     assert_screen "startapp-firefox-added";
-    # assert_and_click "startapp-delete";
-    check_screen "startapp-delete";
-    send_key "right";
-    send_key "down";
-    send_key "right";
-    send_key "ret";
-    send_key "alt-f4";
+    assert_and_click "startapp-delete";
     wait_still_screen;
     send_key "alt-f4";
     assert_screen "generic-desktop";
@@ -157,14 +150,7 @@ sub run {
     ##auto-save-session functionality has been abandoned;
     ##current status: just firefox works
     ##so in the future will consider remove openqa code for this session
-    unless (is_sle('15+')) {
-        # Install dconf-editor for TW
-        if (check_var('VERSION', 'Tumbleweed')) {
-            select_console('root-console');
-            pkcon_quit;
-            zypper_call('in dconf-editor');
-            select_console('x11');
-        }
+    unless (is_sle('15+') || is_tumbleweed) {
         $self->alter_status_auto_save_session;
 
         x11_start_program('firefox');

tests/x11/gnomecase/change_password.pm
@@ -157,7 +157,7 @@ sub run {
 
     #delete the added user: test
     # We should kill the active user test in SLE15
-    assert_script_run 'loginctl terminate-user test' if is_sle('15+');
+    assert_script_run 'loginctl terminate-user test' if is_sle('15+') || is_tumbleweed;
     wait_still_screen;
     type_string "userdel -f test\n";
     assert_screen "user-test-deleted";

tests/x11/gnomecase/nautilus_cut_file.pm
@@ -23,11 +23,19 @@ sub run {
     send_key "ctrl-x";
     send_key_until_needlematch 'nautilus-Downloads-matched', 'left', 5;
     send_key "ret";
-    send_key "ctrl-v";    #paste to dir ~/Downloads
-    assert_screen "nautilus-newfile-moved";
-    send_key "alt-up";                      #back to home dir from ~/Downloads
-    assert_screen 'nautilus-no-newfile';    #assure newfile moved
-    send_key "ctrl-w";                      #close nautilus
+    # paste to ~/Downloads
+    send_key "ctrl-v";
+    # assure file moved, no matter file is highlighted or not
+    assert_screen([qw(nautilus-newfile-moved nautilus-newfile-moved-no-focus)]);
+    if (match_has_tag('nautilus-newfile-moved-no-focus')) {
+        record_soft_failure 'poo#45527 [tw][desktop] pasted files not always highlighted in openqa';
+        save_screenshot;
+    }
+    # back to home dir
+    send_key "alt-up";
+    assert_screen 'nautilus-no-newfile';
+    # close nautilus
+    send_key "ctrl-w";
 
     #remove the newfile, rm via cmd to avoid file moving to trash
     x11_start_program("rm Downloads/newfile", valid => 0);

tests/x11/gnomecase/nautilus_open_ftp.pm
@@ -16,7 +16,7 @@
 use base 'x11test';
 use strict;
 use testapi;
-use version_utils 'is_sle';
+use version_utils qw(is_sle is_tumbleweed);
 
 sub run {
     x11_start_program('nautilus');
@@ -25,7 +25,7 @@ sub run {
     assert_screen 'nautilus-ftp-login';
     send_key 'ret';
     assert_screen 'nautilus-ftp-suse-com';
-    if (is_sle('12-SP2+')) {
+    if (is_sle('12-SP2+') || is_tumbleweed) {
         assert_and_click 'unselected-pub';
         assert_and_click 'ftp-path-selected';
         wait_still_screen(2);

commit 46a5f9673aab1923ab384b3fe33decc115159811

Fix different handling for gdm password depending on product version
This should fix the password handling in the gdm login prompt depending on
product versions. In general "is_tumbleweed" should be avoided to handle new
features which are expected to come to SLE and Leap eventually anyway.
This problem was introduced in e288e39c.

This now handles older products as the exception as it should be, not the
other way around.

Related progress issue: https://progress.opensuse.org/issues/60332

lib/opensusebasetest.pm
@@ -788,7 +788,7 @@ sub wait_boot {
             #assert_screen "dm-password-input", 10;
             elsif (check_var('DESKTOP', 'gnome')) {
                 # In GNOME/gdm, we do not have to enter a username, but we have to select it
-                if (is_tumbleweed) {
+                unless (is_sle('<=15-sp1') || is_leap('<=15.1')) {
                     send_key 'tab';
                 }
                 send_key 'ret';