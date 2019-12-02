82a944c0e91a8d27fbe53614733be0df9036433c
Richard Palethorpe <rpalethorpe@suse.com>
2019-12-02 15:18:34 +0100

    CD into a drive with space and use tar-up logs script

--- tests/kernel/runltp_support.pm
+++ tests/kernel/runltp_support.pm
@@ -27,12 +27,13 @@ sub run {
 
     zypper_call('in git-core qemu squashfs xz');
 
+    assert_script_run('mount /dev/nvme0n1p2 /mnt && cd /mnt/var/tmp');
     assert_script_run('git clone --recurse-submodules https://gitlab.suse.de/kernel-qa/runltp-support.git');
     assert_script_run('cd runltp-support && curl -OL https://openqa.suse.de/assets/iso/' . get_var('ISO'));
     assert_script_run('./install-setup-run-syzkaller.sh ' . get_var('SCC_REGCODE') . ' ' . get_var('ISO'),
         timeout => 3600);
-    assert_script_run('cd runltp-ng && tar -c syzkaller* | xz -T 8 > results.tar.xz');
-    upload_logs('results.tar.xz');
+    assert_script_run('./tar-up-results.sh');
+    upload_logs('runltp-ng/results.tar.xz');
 }
 
 1;
