7bb46fc5401d979d3c3954e336c4df0d71301704
Richard Palethorpe <rpalethorpe@suse.com>
2019-12-04 12:08:28 +0100

    Just try powering on with IPMI instead of using install module

--- tests/kernel/runltp_support.pm
+++ tests/kernel/runltp_support.pm
@@ -20,20 +20,62 @@ use power_action_utils 'power_action';
 use lockapi;
 use ipmi_backend_utils;
 
+sub ipmitool {
+    my ($cmd) = @_;
+
+    my @cmd = ('ipmitool', '-I', 'lanplus', '-H', $bmwqemu::vars{IPMI_HOSTNAME}, '-U', $bmwqemu::vars{IPMI_USER}, '-P', $bmwqemu::vars{IPMI_PASSWORD});
+    push(@cmd, split(/ /, $cmd));
+
+    my ($stdin, $stdout, $stderr, $ret);
+    print @cmd;
+    $ret = IPC::Run::run(\@cmd, \$stdin, \$stdout, \$stderr);
+    chomp $stdout;
+    chomp $stderr;
+
+    die join(' ', @cmd) . ": $stderr" unless ($ret);
+    bmwqemu::diag("IPMI: $stdout");
+    return $stdout;
+}
+
+sub poweroff_host {
+    ipmitool("chassis power off");
+    while (1) {
+        sleep(3);
+        my $stdout = ipmitool('chassis power status');
+        last if $stdout =~ m/is off/;
+        ipmitool('chassis power off');
+    }
+}
+
+sub poweron_host {
+    ipmitool("chassis power on");
+    while (1) {
+        sleep(3);
+        my $stdout = ipmitool('chassis power status');
+        last if $stdout =~ m/is on/;
+        ipmitool('chassis power on');
+    }
+}
+
 sub run {
     my $self = shift;
 
+    poweron_host;
+    select_console 'sol', await_console => 0;
+    assert_screen('linux-login', 1800);
     $self->select_serial_terminal;
 
-    zypper_call('in git-core qemu squashfs xz');
+    zypper_call('in git-core');
 
     assert_script_run('mount /dev/nvme0n1p2 /mnt && cd /mnt/var/tmp');
     assert_script_run('git clone --recurse-submodules https://gitlab.suse.de/kernel-qa/runltp-support.git');
-    assert_script_run('cd runltp-support && curl -OL https://openqa.suse.de/assets/iso/' . get_var('ISO'));
+    assert_script_run('cd runltp-support && ./host-install.sh');
     assert_script_run('./install-setup-run-syzkaller.sh ' . get_var('SCC_REGCODE') . ' ' . get_var('ISO'),
         timeout => 3600);
     assert_script_run('./tar-up-results.sh');
     upload_logs('runltp-ng/results.tar.xz');
+
+    power_action('poweroff');
 }
 
 1;
