a4612add882198ba2b5ee75f88a33473d2c53041
sofiasyria <sofiasyria@gmail.com>
2019-12-11 15:45:22 +0100

    Add changes to validate_ext4_fs module
    Add changes to validate_ext4_fs module

--- tests/console/validate_ext4_fs.pm
+++ tests/console/validate_ext4_fs.pm
@@ -25,19 +25,18 @@ sub run {
 
     foreach (@partitions) {
         if ($_->{mnt_point} eq '[SWAP]') {
+            record_info("Check $_->{mnt_point}", "Verify the '$_->{mnt_point}' partition was created and the filesystem is '$_->{fs_type}'");
             # Validate that the partition is created.
-            record_info("Check $_->{mnt_point}", "Verify the '$_->{mnt_point}' partition was created.");
             assert_script_run("lsblk -n | grep SWAP", fail_message => "Partition SWAP was not created.");
             # Validate that the filesystem type is the expected one.
             my $fstype = script_output("cat /etc/fstab | grep swap | awk '{print \$3}'");
             assert_equals($_->{fs_type}, $fstype, "File system on '$_->{mnt_point}' partition does not correspond to the expected one.");
         }
         else {
+            record_info("Check $_->{mnt_point}", "Verify the '$_->{mnt_point}' partition was created and the filesystem is '$_->{fs_type}'");
             # Validate that the partition is created.
-            record_info("Check $_->{mnt_point}", "Verify the '$_->{mnt_point}' partition was created.");
             assert_script_run("lsblk -n | grep $_->{mnt_point}\$", fail_message => "Partition $_->{mnt_point} was not created.");
             # Validate that the filesystem type is the expected one.
-            record_info("Check fs $_->{mnt_point}", "Verify the '$_->{mnt_point}' partition filesystem is '$_->{fs_type}'");
             my $fstype = script_output("df -PT $_->{mnt_point} | grep -v \"Filesystem\" | awk '{print \$2}'");
             assert_equals($_->{fs_type}, $fstype, "File system on '$_->{mnt_point}' partition does not correspond to the expected one.");
             # Test the ability to read and write files on the partition.
@@ -45,8 +44,11 @@ sub run {
             assert_script_run("cat $_->{mnt_point}/emptyfile",          fail_message => 'Failure while reading from ' . $_->{mnt_point});
         }
     }
+
     # Validate that there is no unpartitioned space.
-    my $unpartitioned = script_output("parted $test_data->{device} unit GB print free | grep 'Free Space' | tail -n1 | awk '{print \$3}'");
+    my $device = $test_data->{device};
+    record_info("Check $device partitioning", "Verify the '$device' does not have unpartitioned disk space");
+    my $unpartitioned = script_output("parted $device unit GB print free | grep 'Free Space' | tail -n1 | awk '{print \$3}'");
     die "There is $unpartitioned unpartitioned disk space." if ($unpartitioned ne $test_data->{allowed_unpartitioned});
 
 }
