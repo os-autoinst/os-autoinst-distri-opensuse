3b5449a46a1dedd1441ea9966358c3ace142a5fe
Guillaume Gardet <guillaume.gardet@arm.com>
2019-12-12 15:39:20 +0100

    Add some scripts to be used with generalhw backend

---
+++ data/generalhw_scripts/get_sol_ttyUSB0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# exit when any command fails
+# set -e
+
+device=/dev/ttyUSB0
+speed=115200
+
+# Set up device
+stty -F $device $speed
+
+# Read the device $device in the background
+# tail -f $device &
+cat < $device &
+
+# Capture PID of background process so it is possible to terminate it when done
+bgPid=$!
+
+trap cleanup SIGINT SIGTERM SIGKILL
+
+cleanup()
+{
+  # Terminate background read process
+  kill $bgPid
+  exit 1
+}
+
+# Read commands from user, send them to device $device
+while read cmd
+do
+   echo "$cmd" 
+done > $device
+
+cleanup;
