9bf90b8783253c6f3df79527a218d08cf1e974e2
Oleksandr Orlov <oleksandr.orlov1@gmail.com>
2019-12-18 10:07:17 +0100

    Add tests for bond, bridge and vlan restart
    The commit adds the three separate test modules that are using OOP
    framework.
    
    Related ticket: [poo#59315](https://progress.opensuse.org/issues/59315)

--- tests/yast2_gui/yast2_lan_restart_bond.pm
+++ tests/yast2_gui/yast2_lan_restart_bond.pm
@@ -7,11 +7,59 @@
 # notice and this notice are preserved. This file is offered as-is,
 # without any warranty.
 
-# Summary: TODO
+# Summary: The test verifies that network is not restarted if no changes were
+# made to the configuration of Bridged device but "Ok" button was pressed in
+# Network Settings. Related tasks: fate#318787 poo#11450
+#
+# Pre-conditions:
+# Create Bond device.
+#
+# Test:
+# 1. Open Network Settings Dialog;
+# 2. Press "Edit" button to view the configuration of Bond device;
+# 3. Proceed through the Edit configuration wizard, but do not change anything;
+# 4. Save the changes by pressing "Ok" button in Network Settings dialog;
+# 5. Verify the Network is not restarted.
+#
+# Post-condition:
+# Delete the Bond device.
 # Maintainer: Oleksandr Orlov <oorlov@suse.de>
 
-package yast2_lan_restart_bond;
+use base 'y2_installbase';
 use strict;
 use warnings;
+use testapi;
+use y2lan_restart_common qw(initialize_y2lan open_network_settings check_network_status wait_for_xterm_to_be_visible clear_journal_log close_xterm);
+
+my $network_settings;
+
+sub pre_run_hook {
+    initialize_y2lan;
+    open_network_settings;
+    $network_settings = $testapi::distri->get_network_settings();
+    $network_settings->add_bond_slave();
+    $network_settings->save_changes();
+    wait_for_xterm_to_be_visible();
+}
+
+sub run {
+    record_info('bond', 'Verify network is not restarted after saving bond device settings without changes.');
+    open_network_settings;
+    $network_settings->view_bond_slave_without_editing();
+    $network_settings->save_changes();
+    wait_for_xterm_to_be_visible();
+    clear_journal_log();
+    check_network_status('', 'bond');
+}
+
+sub post_run_hook {
+    open_network_settings;
+    $network_settings->delete_bond_device();
+    # return ethernet card settings to the default ones
+    $network_settings->select_dynamic_address_for_ethernet();
+    $network_settings->save_changes();
+    wait_for_xterm_to_be_visible();
+    close_xterm();
+}
 
 1;
