os-autoinst/os-autoinst-distri-opensuse

commit a0b10d08d6a19cf3a299c2004183ea0ccb280a9a

Fix regexp for linux command in some archs
add_custom_grub_entries() adds new boot options into /boot/grub2/grub.cfg
and check changes. Some archs has different name of kernel image:

* aarch64-virtio
linux	/boot/Image-4.12.14-23-default root=UUID=f2eab56d-b670-4dde-b7d4-eb766834a2ca  ${extra_cmdline} plymouth.ignore-serial-consoles console=ttyAMA0 console=tty splash=silent showopts
* s390x-kvm-sle12:
linux	${btrfs_subvol}/boot/image-4.12.14-94.27-default root=/dev/vda3 ima_policy=tcb  ${extra_cmdline} hvc_iucv=8 TERM=dumb resume=/dev/disk/by-path/ccw-0.0.0000-part2 crashkernel=112M

=> we need to grep just for linux.*/boot/.*

lib/bootloader_setup.pm
@@ -106,7 +106,7 @@ sub add_custom_grub_entries {
     die("Unexpected number of grub entries: $cnt_new, expected: " . ($cnt_orig * 2)) if ($cnt_orig * 2 != $cnt_new);
     $cnt_new = script_output("grep -c 'menuentry .$distro.*($grub_param)' /boot/grub2/grub.cfg");
     die("Unexpected number of new grub entries: $cnt_new, expected: " . ($cnt_orig)) if ($cnt_orig != $cnt_new);
-    $cnt_new = script_output("grep -c 'linux.*/boot/vmlinu.* $grub_param ' /boot/grub2/grub.cfg");
+    $cnt_new = script_output("grep -c 'linux.*/boot/.* $grub_param ' /boot/grub2/grub.cfg");
     die("Unexpected number of new grub entries with '$grub_param': $cnt_new, expected: " . ($cnt_orig)) if ($cnt_orig != $cnt_new);
 }
 

commit d68a99f08e04a2f02d75312f0e828fd9ed3a268d

add_custom_grub_entries: Fix regexp for SLE12 SP3
by checking also /vmlinu[xz]-

SLE12 SP3 doesn't use /boot/ directory (images are directly in root
directory, not in /boot/).

SLES use /vmlinuz, but expect also /vmlinux to avoid other failures
in the future.

Fixes: a0b10d08d Fix regexp for linux command in some archs

lib/bootloader_setup.pm
@@ -139,7 +139,7 @@ sub add_custom_grub_entries {
     die("Unexpected number of grub entries: $cnt_new, expected: $cnt_old") if ($cnt_old != $cnt_new);
     $cnt_new = script_output("grep -c 'menuentry .$distro.*($grub_param)' " . GRUB_CFG_FILE);
     die("Unexpected number of new grub entries: $cnt_new, expected: " . ($cnt_old)) if ($cnt_old != $cnt_new);
-    $cnt_new = script_output("grep -c 'linux.*/boot/.* $grub_param ' " . GRUB_CFG_FILE);
+    $cnt_new = script_output("grep -c -E 'linux.*(/boot|/vmlinu[xz]-).* $grub_param ' " . GRUB_CFG_FILE);
     die("Unexpected number of new grub entries with '$grub_param': $cnt_new, expected: " . ($cnt_old)) if ($cnt_old != $cnt_new);
 }
 