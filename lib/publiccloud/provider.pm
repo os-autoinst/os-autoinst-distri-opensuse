81a48f044cf812a1b9c3f9fe73495d56a3ea7bbc
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2020-01-07 15:18:11 +0100

    publiccloud: Adjust vault token ttl
    In case the authentication token expire, _all_ created leased which was
    created in that session, are expired as well.
    From [1] you can found an explanation:
    ```
    Revocation can happen manually via the API, via the vault revoke cli command,
    or automatically by Vault. When a lease is expired, Vault will automatically
    revoke that lease. When a token is revoked, Vault will revoke all leases that
    were created using that token.
    ```
    
    So we use [2] to increase our token expire time.
    As there is a other concept of "Explicit maximum lifetime" [3]. If I
    understood this concept right, there is no way to have an unlimited token.
    
    [1] https://www.vaultproject.io/docs/concepts/lease.html
    [2] https://www.vaultproject.io/api/auth/token/index.html#renew-a-token
    [3] https://www.vaultproject.io/docs/concepts/tokens.html#explicit-max-ttls

--- lib/publiccloud/provider.pm
+++ lib/publiccloud/provider.pm
@@ -490,6 +490,7 @@ sub vault_api {
 
     $ua->insecure(get_var('_SECRET_PUBLIC_CLOUD_REST_SSL_INSECURE', 0));
     $url = $url . $path;
+    bmwqemu::diag("Request Vault REST API: $url");
     if ($method eq 'get') {
         $res = $ua->get($url =>
               {'X-Vault-Token' => $self->vault_token()})->result;
@@ -512,6 +513,23 @@ sub vault_api {
     }
 
     return $res->json;
+}
+
+=head2 vault_get_secrets
+
+  my $data = $csp->vault_get_secrets('/azure/creds/openqa-role')
+
+This is a wrapper around C<vault_api()> to retrieve secrets from aws, gce or
+azure secret engine.
+It prepend C<'/v1/' + $NAMESPACE> to the given path before sending the request.
+It stores lease_id and also adjust the token-live-time.
+=cut
+sub vault_get_secrets {
+    my ($self, $path) = @_;
+    my $res = $self->vault_api('/v1/' . get_var('PUBLIC_CLOUD_VAULT_NAMESPACE', '') . $path, method => 'get');
+    $self->vault_lease_id($res->{lease_id});
+    $self->vault_api('/v1/auth/token/renew-self', method => 'post', data => {increment => $res->{lease_duration} . 's'});
+    return $res->{data};
 }
 
 =head2 vault_revoke
