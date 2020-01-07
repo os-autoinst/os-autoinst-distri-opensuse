b49fe9ee2a7af4c384e010e33d06200175dc5163
Ednilson Miura <emiura@suse.com>
2020-01-07 17:45:38 +0100

    Enhanced tests for dracut
    This tests run a patched version of the dracut testsuite inside dracut package.
    The patch was necessary to run custom test initramfs inside openqa virtual
    machine rather in another qemu instance.
    
    This commit contains xml files for autoyast necessary to custom partitioning,
    also contains a set of patches for basic test, systemd, separated usr, full
    systemd and lvm tests.  Also included yaml files for testsuites.
    
    Versions before SLE12-SP2 are not supported due to dracut testsuite being very
    different.
    
    Partially covers poo#58460 [qam][maint][dracut] extend dracut test coverage
    using its testsuite
    (Support for multimachine based tests to be added later)
    
    Test run:
    SLE15-SP1
    systemd: http://10.161.229.180/tests/3126
    full-systemd: http://10.161.229.180/tests/3124
    lvm: http://10.161.229.180/tests/3037
    usr-mount: http://10.161.229.180/tests/3011
    basic: http://10.161.229.180/tests/3004
    
    SLE15:
    systemd: http://10.161.229.180/tests/2933
    full-systemd: http://10.161.229.180/tests/2974
    lvm: http://10.161.229.180/tests/2991
    usr-mount: http://10.161.229.180/tests/2972
    basic: http://10.161.229.180/tests/2942
    
    SLE12-SP5:
    systemd: http://10.161.229.180/tests/3053
    full-systemd: http://10.161.229.180/tests/3051
    lvm: http://10.161.229.180/tests/3045
    usr-mount: http://10.161.229.180/tests/3055
    basic: http://10.161.229.180/tests/3057
    
    SLE12-SP4:
    systemd: http://10.161.229.180/tests/3065
    full-systemd: http://10.161.229.180/tests/3067
    lvm: http://10.161.229.180/tests/3089
    usr-mount: http://10.161.229.180/tests/3069
    basic: http://10.161.229.180/tests/3063
    
    SLE12-SP3:
    systemd: http://10.161.229.180/tests/3079
    full-systemd: http://10.161.229.180/tests/3150
    lvm: http://10.161.229.180/tests/3087
    usr-mount: http://10.161.229.180/tests/3077
    basic: http://10.161.229.180/tests/3071
    
    SLE12-SP2:
    systemd: http://10.161.229.180/tests/3116
    full-systemd: http://10.161.229.180/tests/3136
    lvm: http://10.161.229.180/tests/3114
    usr-mount: http://10.161.229.180/tests/3112
    basic: http://10.161.229.180/tests/3102

---
+++ tests/console/dracut_enhanced.pm
@@ -0,0 +1,70 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Run dracut testsuite
+# - Test suitable only for dracut 0.44 (SLE12SP2+)
+# - Fetches a tarball containing patches to be applied to dracut tests
+# - Install dracut src.rpm and some dependencies
+# - Patch dracut tests (remove references for qemu)
+# - Creates a initramfs and boots the system
+# - Cleanup
+# Based on code from Sergio Lindo Mansilla <slindomansilla@suse.com> and
+# Thomas Blume <tblume@suse.com>
+
+# Maintainer: Ednilson Miura <emiura@suse.com>
+
+use base "consoletest";
+use warnings;
+use strict;
+use testapi;
+use utils 'zypper_call';
+use power_action_utils 'power_action';
+use version_utils 'is_sle';
+
+sub run {
+    select_console 'root-console';
+    my $dracut_test = get_var('DRACUT_TEST');
+    if (is_sle('<12-SP2')) {
+        die "Unsupported dracut version";
+    }
+    else {
+        assert_script_run 'curl -v -o /tmp/dracut-patches.tar.gz ' . data_url('qam/dracut/dracut-patches.tar.gz');
+        assert_script_run 'tar xvf /tmp/dracut-patches.tar.gz -C /tmp';
+        zypper_call "in rpmbuild dhcp-client strace";
+        zypper_call "si -D dracut";
+        assert_script_run('cd /usr/src/packages/SPECS');
+        assert_script_run('rpmbuild -bp dracut.spec --nodeps');
+        assert_script_run('cd /usr/src/packages/BUILD/dracut-*');
+        assert_script_run('cp /tmp/dracut*.patch .');
+        assert_script_run('for p in *patch; do patch -p0 < $p; done');
+        assert_script_run "cd /usr/src/packages/BUILD/dracut-*/test/$dracut_test";
+        assert_script_run 'mkdir /tmp/logs';
+        assert_script_run "./test.sh --setup |& tee /tmp/logs/$dracut_test-setup.log", 300;
+        assert_script_run "grep -q dracut-root-block-created /tmp/logs/$dracut_test-setup.log";
+        power_action('reboot', textmode => 1);
+        sleep 10;
+        assert_screen("linux-login", 600);
+        type_string "root\n";
+        wait_still_screen 3;
+        type_password;
+        wait_still_screen 3;
+        send_key 'ret';
+        assert_script_run "cd /usr/src/packages/BUILD/dracut-*/test/$dracut_test";
+        assert_script_run './test.sh --clean';
+    }
+}
+
+sub post_fail_hook {
+    my ($self) = shift;
+    $self->SUPER::post_fail_hook;
+    assert_script_run('tar -cjf dracut-testsuite-logs.tar.bz2 /tmp/logs', 600);
+    upload_logs('dracut-testsuite-logs.tar.bz2');
+}
+
+1;
