ba6b82aab18c53abca2e76bd087ccde21fd98298
Sebastian Chlad <sebastian.chlad@gmail.com>
2020-01-21 16:51:36 +0100

    HPC: Clean up of HPC specific libs

--- lib/hpc/migration.pm
+++ lib/hpc/migration.pm
@@ -1,3 +1,15 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019-2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: Base module for HPC migration tests
+# Maintainer: Sebastian Chlad <schlad@suse.de>
+
 package hpc::migration;
 use strict;
 use warnings;
