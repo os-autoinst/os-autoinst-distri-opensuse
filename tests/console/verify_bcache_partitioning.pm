3aed8e9dcbe5a438b1f31a5f8f41ef858f2783c3
ybonatakis <ybonatakis@suse.com>
2020-02-27 10:33:15 +0100

    Add autoyast xml and yaml scheduler for bcache test job
    Create a new yaml scheduler and the xml for the autoyast.
    The yaml scheduler adds verification for the partitioning
    and of the AY profile.
    Added a method to retrieve specific value from the test data.
    The reason is to avoid dublicate entries for some of the data.

---
+++ tests/console/verify_bcache_partitioning.pm
@@ -0,0 +1,55 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: Validate bcache in writethrough
+# Usually the caching device is a fast device (ssd). In this scenario a rotational device is used.
+# The whole second disk is used as a caching device with /home mounted as a backing partition.
+# This doesnt give any significant performance advantages but it is better to test bcache with this setup
+# than use bcache without caching(which yast provides a option if we want to do so)
+# Scenarios covered:
+# - Verify that certain values are set correctly in a bcache setup after installation.
+# - Write operation in a backing device can be performed and validate that cache is working watching the cache hits.
+# Maintainer: yiannis bonatakis <ybonatakis@suse.com>
+
+use strict;
+use warnings;
+use base "opensusebasetest";
+use testapi;
+use scheduler 'get_test_suite_data';
+use Test::Assert 'assert_true';
+
+sub run {
+    my $test_data  = get_test_suite_data();
+    my $cachingdev = $test_data->{profile}->{partitioning}->[1]->{drive}->{device};
+    select_console 'root-console';
+
+    # assert the registered backing dev
+    assert_script_run "cat /sys/block/bcache0/bcache/backing_dev_name | grep $test_data->{backingdev}";
+    # assert bcache is running
+    assert_script_run "cat /sys/block/bcache0/bcache/running | grep 1";
+    # assert bcache is setup and is not cached dirty data
+    assert_script_run "cat /sys/block/bcache0/bcache/state | grep clean";
+
+    record_info("bcache info", "Show info of bcache $cachingdev");
+    assert_script_run "bcache-super-show $cachingdev";
+    assert_script_run "cat /proc/partitions";
+
+    record_info("write operation");
+    my $hit_i = script_output "cat /sys/block/bcache0/bcache/stats_total/cache_hits";
+    record_info "cache_hits before", "$hit_i";
+    my $path_in_backingdev = "/home/bernhard/one/two/three";
+    assert_script_run "mkdir -p $path_in_backingdev";
+    assert_script_run "touch ${path_in_backingdev}/data";
+    assert_script_run "dd bs=1024 count=1000000 < /dev/random > ${path_in_backingdev}/data";
+
+    my $hits_f = script_output "cat /sys/block/bcache0/bcache/stats_total/cache_hits";
+    assert_true($hit_i < $hits_f, "$hits_f should be bigger than $hit_i");
+}
+
+1;
