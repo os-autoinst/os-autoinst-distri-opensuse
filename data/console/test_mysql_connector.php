7ced791c1749ce8b5a4dd16811327d38d268ff8c
Santiago Zarate <santiago.zarate@suse.com>
2020-03-10 16:01:30 +0100

    Enable error reporting in PHP+MySQL test code

--- data/console/test_mysql_connector.php
+++ data/console/test_mysql_connector.php
@@ -1,9 +1,17 @@
 <?php
-    $link = mysqli_connect('localhost', 'root', '', 'openQAdb');
-    $result = mysqli_query($link, "SELECT * FROM test");
-    while($row = mysqli_fetch_assoc($result)){
-        echo $row['entry'];
-    }
-    mysqli_query($link, "INSERT INTO test (entry) VALUE ('can php write this?')");
+ini_set('display_errors', 1);
+ini_set('html_errors', 0);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$link = mysqli_connect('localhost', 'root', '', 'openQAdb');
+$result = mysqli_query($link, "SELECT * FROM test");
+
+while($row = mysqli_fetch_assoc($result)){
+    echo $row['entry'];
+}
+
+mysqli_query($link, "INSERT INTO test (entry) VALUE ('can php write this?')");
+
 ?>
 
