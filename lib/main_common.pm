os-autoinst/os-autoinst-distri-opensuse

commit 3674a01d9ef4a1fd1a4ded9ddfdfc4b4a499037e

LTP: Fix tag name for verification builds via github PR
Github pull requests done via openqa-clone-custom-git-refspec script set
BUILD variable as user/branch-name#GITHUB_PR. BUILD variable is used as
part of tag (via get_ltp_tag()) which is used as filename. Thus we need
to replace special characters so that filename is valid. It'd be enough
to replace # and /, but just for case replace all non-alpha numeric
and _@ characters with - (dash).

lib/main_common.pm
@@ -258,6 +258,7 @@ sub get_ltp_tag {
     } else {
         $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
     }
+    $tag =~ s/[^a-zA-Z0-9_@]+/-/g;
     return $tag . '.txt';
 }
 

commit a5c55811cddf82fc32e77b9414f68af748c575da

LTP: Keep dot in runtest file
It's perfectly safe character and runtest file is more readable with it
(keeping dot version instead replacing it with dash).

Fixes: 3674a01d9 ("LTP: Fix tag name for verification builds via github PR")

lib/main_common.pm
@@ -279,7 +279,7 @@ sub get_ltp_tag {
             $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
         }
     }
-    $tag =~ s/[^a-zA-Z0-9_@]+/-/g;
+    $tag =~ s/[^a-zA-Z0-9_@.]+/-/g;
     return $tag;
 }
 