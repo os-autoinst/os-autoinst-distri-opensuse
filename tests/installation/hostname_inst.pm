87d86583e5ee08514e6bf1c5ee90871fed89140b
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-03-16 12:00:13 +0100

    Expect hostname by DHCP on SLE15-SP2+
    Before SLE15-SP2, yast didn't take during installation the hostname by
    DHCP. See fate#319639

--- tests/installation/hostname_inst.pm
+++ tests/installation/hostname_inst.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2016 SUSE LLC
+# Copyright © 2016-2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -18,18 +18,22 @@ use base 'y2_installbase';
 use strict;
 use warnings;
 use testapi;
+use version_utils;
 
 sub run {
     assert_screen "before-package-selection";
     select_console 'install-shell';
-    if (my $expected_install_hostname = get_var('EXPECTED_INSTALL_HOSTNAME')) {
-        # EXPECTED_INSTALL_HOSTNAME contains expected hostname YaST installer
-        # got from environment (DHCP, 'hostname=' as a kernel cmd line argument
-        assert_script_run "test \"\$(hostname)\" == \"$expected_install_hostname\"";
-    }
-    else {
-        # 'install' is the default hostname if no hostname is get from environment
-        assert_script_run 'test "$(hostname)" == "install"';
+    # NICTYPE_USER_OPTIONS="hostname=myguest" causes a fake DHCP hostname provided to SUT
+    my $NICTYPE_USER_OPTIONS      = get_required_var('NICTYPE_USER_OPTIONS');
+    my $expected_install_hostname = ($NICTYPE_USER_OPTIONS =~ s/hostname=//r);
+    # Before SLE15-SP2, yast didn't take during installation the hostname by DHCP
+    # See fate#319639
+    # 'install' is the default hostname if no hostname is get from environment
+    $expected_install_hostname = 'install' if (is_sle('<15-SP2'));
+    if (is_sle('<15-SP2') && (script_run(qq{test "\$(hostname)" == "linux"}) == 0)) {
+        record_soft_failure('bsc#1166778 - Default hostname in SLE15-SP1 is not "install"');
+    } else {
+        assert_script_run(qq{test "\$(hostname)" == "$expected_install_hostname"});
     }
     save_screenshot;
     # cleanup
