os-autoinst/os-autoinst-distri-opensuse

commit 08d9ec4d747448f7302d544ba7c09c1fe75e2e6d

Update kernel test scheduling for spvm backend
Fix poo#44309: Tuned was removed, it is already part of
extra_tests_kernel. Initialisation of bootloader was simplified to
cover more tests on spvm backend.

lib/main_ltp.pm
@@ -118,25 +118,11 @@ sub stress_snapshots {
     }
 }
 
-sub prepare_target {
-    if (get_var("BOOT_HDD_IMAGE")) {
-        boot_hdd_image;
-    }
-    elsif (check_var('IPXE', '1')) {
-        return;
-    }
-    else {
-        load_boot_tests();
-        load_inst_tests();
-        load_reboot_tests();
-    }
-}
-
 sub load_kernel_tests {
     load_bootloader_s390x();
+    loadtest "../installation/bootloader" if is_spvm;
 
     if (get_var('INSTALL_LTP')) {
-        loadtest "../installation/bootloader" if is_spvm;
         if (get_var('INSTALL_KOTD')) {
             loadtest 'install_kotd';
         }
@@ -159,7 +145,6 @@ sub load_kernel_tests {
         }
     }
     elsif (get_var('LTP_COMMAND_FILE')) {
-        loadtest "../installation/bootloader" if is_spvm;
         if (get_var('INSTALL_KOTD')) {
             loadtest 'install_kotd';
         }
@@ -201,10 +186,6 @@ sub load_kernel_tests {
     } elsif (get_var('NUMA_IRQBALANCE')) {
         boot_hdd_image();
         loadtest 'numa_irqbalance';
-    }
-    elsif (get_var('TUNED')) {
-        prepare_target;
-        loadtest "tuned";
     }
 
     if (check_var('BACKEND', 'svirt') && get_var('PUBLISH_HDD_1')) {

commit b2a7870c80e9c482fafce0c2241fcdf0ccfd3315

LTP: cleanup unused import
Fixes: 08d9ec4d7 ("Update kernel test scheduling for spvm backend")

lib/main_ltp.pm
@@ -24,7 +24,7 @@ use Archive::Tar;
 use utils;
 use LTP::TestInfo 'testinfo';
 use File::Basename 'basename';
-use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag load_boot_tests load_inst_tests load_reboot_tests);
+use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag);
 use 5.018;
 use Utils::Backends 'is_pvm';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"