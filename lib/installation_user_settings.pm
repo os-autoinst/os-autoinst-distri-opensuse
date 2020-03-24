55a01fb5e9a398981ef3ffa864c51da2650e2363
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-03-24 09:57:20 +0100

    Increase timeout for password check on pvm_hmc
    pvm_hmc performs slower than other machines.

--- lib/installation_user_settings.pm
+++ lib/installation_user_settings.pm
@@ -30,7 +30,7 @@ sub await_password_check {
     # PW too easy (cracklib)
     # bsc#937012 is resolved in > SLE 12, skip if VERSION=12
     return if (is_sle('=12') && check_var('ARCH', 's390x'));
-    assert_screen 'inst-userpasswdtoosimple';
+    assert_screen('inst-userpasswdtoosimple', (check_var('BACKEND', 'pvm_hmc')) ? 60 : 30);
     send_key 'ret';
 
 }
