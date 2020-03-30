80248b1e1007a4c6ba8bda8a67cd21b4928b2386
rfan1 <richard.fan@suse.com>
2020-03-30 12:06:37 +0800

    Implement test for tpm2_server,tpm2_engine and tpm2_tools

---
+++ tests/security/tpm2/tpm2_engine/tpm2_engine_ecdsa_operation.pm
@@ -0,0 +1,56 @@
+# Copyright (C) 2020 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Summary: Per TPM2 stack, we would like to add the tpm2-tss-engine,
+#          For tpm2_enginee tests, we need tpm2-abrmd serive active.
+#          We have several test modules, this test module will cover
+#          ECDSA operations.
+# Maintainer: rfan1 <richard.fan@suse.com>
+# Tags: poo#64902, tc#1742298
+
+use strict;
+use warnings;
+use base 'opensusebasetest';
+use testapi;
+
+sub run {
+    select_console "root-console";
+
+    # ECDSA operations
+    # There is an known issue bsc#1159508
+    # Please use the command below to carry out the tests
+    my $test_dir  = "tpm2_engine_ecdsa_sign";
+    my $test_file = "data";
+    my $my_key    = "mykey";
+    my $my_sig    = "mysig";
+    assert_script_run "mkdir $test_dir";
+    assert_script_run "cd $test_dir";
+    assert_script_run "echo tpm2test > $test_file";
+    assert_script_run "tpm2tss-genkey -a ecdsa -s 2048 $my_key";
+    assert_script_run "openssl ec -engine tpm2tss -inform engine -in $my_key -pubout -outform pem -out $my_key.pub";
+    assert_script_run "sha256sum $test_file | cut -d ' ' -f 1 | base64 -d > $test_file.hash";
+    assert_script_run "openssl pkeyutl -engine tpm2tss -keyform engine -inkey $my_key -sign -in $test_file.hash -out $my_sig";
+    validate_script_output "openssl pkeyutl -engine tpm2tss -keyform engine -inkey $my_key -verify -in $test_file.hash -sigfile $my_sig", sub {
+        m
+            /Signature\sVerified\sSuccessfully/
+    };
+    assert_script_run "cd";
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+1;
---
+++ tests/security/tpm2/tpm2_tools/tpm2_tools_sign_verify.pm
@@ -0,0 +1,53 @@
+# Copyright (C) 2020 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Summary: Per TPM2 stack, we would like to add the tpm2-tools tests,
+#          from sles15sp2, update tpm2.0-tools to the stable 4 release
+#          this test module will cover sign and verify function.
+# Maintainer: rfan1 <richard.fan@suse.com>
+# Tags: poo#64905, tc#1742297
+
+use strict;
+use warnings;
+use base 'opensusebasetest';
+use testapi;
+
+sub run {
+    select_console "root-console";
+
+    # Sign and verify with the TPM using the endorsement hierarchy
+    my $test_dir = "tpm2_tools_sign_verify";
+    my $prim_ctx = "primary.ctx";
+    my $rsa_priv = "rsa.priv";
+    my $rsa_pub  = "rsa.pub";
+    my $msg_dat  = "message.dat";
+    my $rsa_ctx  = "rsa.ctx";
+    my $sig_rsa  = "sig.rsa";
+    assert_script_run "mkdir $test_dir";
+    assert_script_run "cd $test_dir";
+    assert_script_run "tpm2_createprimary -C e -c $prim_ctx -T tabrmd";
+    assert_script_run "tpm2_create -G rsa -u $rsa_pub -r $rsa_priv -C $prim_ctx -T tabrmd";
+    assert_script_run "tpm2_load -C $prim_ctx -u $rsa_pub -r $rsa_priv -c $rsa_ctx -T tabrmd";
+    assert_script_run "echo \"my message\" > $msg_dat";
+    assert_script_run "tpm2_sign -c $rsa_ctx -g sha256 -o $sig_rsa $msg_dat -T tabrmd";
+    assert_script_run "tpm2_verifysignature -c $rsa_ctx -g sha256 -s $sig_rsa -m $msg_dat -T tabrmd";
+    assert_script_run "cd";
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+1;
