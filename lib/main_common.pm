b69b8222c542a6b21d8194cf15efebae96b06581
phoenix <felix.niederwanger@suse.com>
2020-04-01 11:45:37 +0200

    Implement wpa_supplicant test
    Adds a test for wpa_supplicant. The wpa_supplicant test is a
    self-sufficient zip archive that gets extracted and executed.
    
    The wpa_supplicant test creates two virtual wlan interfaces and tests
    the wlan functionality by setting up a hotspot via hostapd.
    wpa_supplicant is then tested, if able to obtain a network list and
    able to connect to an open and encrypted network. Lastly we test if
    wicked is able to get a ip address via dhcp on the wlan interface.

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1692,8 +1692,9 @@ sub load_extra_tests_console {
     loadtest 'console/osinfo_db' if (is_sle('12-SP3+') && !is_jeos);
     loadtest 'console/libgcrypt' if ((is_sle(">=12-SP4") && (check_var_array('ADDONS', 'sdk') || check_var_array('SCC_ADDONS', 'sdk'))) || is_opensuse);
     loadtest "console/gd";
-    loadtest 'console/valgrind'   unless is_sle('<=12-SP3');
-    loadtest 'console/sssd_samba' unless is_sle("<15");
+    loadtest 'console/valgrind'       unless is_sle('<=12-SP3');
+    loadtest 'console/sssd_samba'     unless is_sle("<15");
+    loadtest 'console/wpa_supplicant' unless (!is_x86_64 || is_sle('<15') || is_leap('<15.1'));
 }
 
 sub load_extra_tests_sdk {
