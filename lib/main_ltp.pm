2e3a17dd224eeb85bae030ca0fe808eb39f53dcc
Petr Vorel <pvorel@suse.cz>
2020-04-01 09:18:21 +0200

    LTP: Fix LTP_BAREMETAL=1 with s390x
    LTP_BAREMETAL=1 is used on o3 for z/VM to use installer (all tests needs
    to be chained to install_ltp with START_DIRECTLY_AFTER_TEST, because on
    o3 there is only z/VM which does not handle qcow).
    
    Therefore don't load bootloader_s390 module again via
    load_bootloader_s390x() (loaded already in load_kernel_baremetal_tests()).

--- lib/main_ltp.pm
+++ lib/main_ltp.pm
@@ -129,9 +129,10 @@ sub stress_snapshots {
 sub load_kernel_tests {
     if (get_var('LTP_BAREMETAL') && get_var('INSTALL_LTP')) {
         load_kernel_baremetal_tests();
+    } else {
+        load_bootloader_s390x();
     }
 
-    load_bootloader_s390x();
     loadtest "../installation/bootloader" if is_pvm;
 
     if (get_var('INSTALL_LTP')) {
