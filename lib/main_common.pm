os-autoinst/os-autoinst-distri-opensuse

commit d8348605831723e65e78350ea8e027235a7e222e

LTP: Introduce bare metal tests
This install SLE from scratch and install LTP (qemu based tests reuse
qcow created by create_hdd_minimal_base+sdk).

+ sort imports.

products/sle/main.pm
@@ -684,6 +684,9 @@ if (is_jeos) {
 
 # load the tests in the right order
 if (is_kernel_test()) {
+    if (get_var('LTP_BAREMETAL')) {
+        load_baremetal_tests();
+    }
     load_kernel_tests();
 }
 elsif (get_var('IBTESTS')) {

tests/kernel/install_ltp.pm
@@ -17,11 +17,11 @@ use File::Basename 'basename';
 use testapi;
 use registration;
 use utils;
-use power_action_utils 'power_action';
 use bootloader_setup 'add_custom_grub_entries';
+use main_common 'get_ltp_tag';
+use power_action_utils 'power_action';
 use serial_terminal qw(add_serial_console select_virtio_console);
 use version_utils qw(is_sle sle_version_at_least is_opensuse);
-use main_common 'get_ltp_tag';
 
 sub add_repos {
     my $qa_head_repo = get_required_var('QA_HEAD_REPO');
@@ -255,7 +255,9 @@ sub run {
         die 'INSTALL_LTP must contain "git" or "repo"';
     }
 
-    $self->wait_boot;
+    if (!get_var('LTP_BAREMETAL')) {
+        $self->wait_boot;
+    }
 
     # poo#18980
     if (get_var('OFW') && check_var('VIRTIO_CONSOLE', 1)) {
@@ -272,6 +274,7 @@ sub run {
     upload_logs('/boot/config-$(uname -r)', failok => 1);
 
     add_we_repo_if_available;
+
     if (is_sle('12+') || is_opensuse) {
         add_custom_grub_entries;
     }

commit 3e7e469e4a766e1527a5226934e30dfa1549794c

LTP: Move kernel baremetal setup to main_common.pm
load_baremetal_tests() call for LTP_BAREMETAL && INSTALL_LTP
was moved to main_common.pm which enables this fix also for openSUSE.

This required to move also load_baremetal_tests(), which in turn causes
main_ltp.pm to load some modules from main_common.pm and
products/sle/main.pm to load load_baremetal_tests().

Although IPMI is not yet used in o3, be prepared for future.
But this code is reused for s390x installation on o3, which also needs
to use installer (all tests needs to be chained to install_ltp with
START_DIRECTLY_AFTER_TEST, because on o3 there is only z/VM which does
not handle qcow).

Fixes: d83486058 ("LTP: Introduce bare metal tests")

lib/main_common.pm
@@ -64,6 +64,7 @@ our @EXPORT = qw(
   load_autoyast_clone_tests
   load_autoyast_tests
   load_ayinst_tests
+  load_baremetal_tests
   load_bootloader_s390x
   load_boot_tests
   load_common_installation_steps_tests
@@ -3146,6 +3147,24 @@ sub load_mm_autofs_tests {
             loadtest "network/autofs_client";
         }
     }
+}
+
+sub load_baremetal_tests {
+    set_var('ADDONURL', 'sdk')          if (is_sle('>=12') && is_sle('<15')) && !is_released;
+    loadtest "kernel/ibtests_barriers"  if get_var("IBTESTS");
+    loadtest "autoyast/prepare_profile" if get_var("AUTOYAST_PREPARE_PROFILE");
+    if (get_var('IPXE')) {
+        loadtest "installation/ipxe_install";
+        loadtest "console/suseconnect_scc";
+    } else {
+        load_boot_tests();
+        get_var("AUTOYAST") ? load_ayinst_tests() : load_inst_tests();
+        load_reboot_tests();
+    }
+    # make sure we always have the toolchain installed
+    loadtest "toolchain/install";
+    # some tests want to build and run a custom kernel
+    loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
 }
 
 1;

lib/main_ltp.pm
@@ -24,7 +24,7 @@ use Archive::Tar;
 use utils;
 use LTP::TestInfo 'testinfo';
 use File::Basename 'basename';
-use main_common qw(load_bootloader_s390x boot_hdd_image get_ltp_tag);
+use main_common qw(boot_hdd_image get_ltp_tag load_baremetal_tests load_bootloader_s390x);
 use 5.018;
 use Utils::Backends 'is_pvm';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"
@@ -127,6 +127,10 @@ sub stress_snapshots {
 }
 
 sub load_kernel_tests {
+    if (get_var('LTP_BAREMETAL') && get_var('INSTALL_LTP')) {
+        load_baremetal_tests();
+    }
+
     load_bootloader_s390x();
     loadtest "../installation/bootloader" if is_pvm;
 

products/sle/main.pm
@@ -17,7 +17,7 @@ use registration;
 use utils;
 use mmapi 'get_parents';
 use version_utils
-  qw(is_vmware is_hyperv is_hyperv_in_gui is_caasp is_installcheck is_rescuesystem is_desktop_installed is_jeos is_released is_sle is_staging is_upgrade);
+  qw(is_vmware is_hyperv is_hyperv_in_gui is_caasp is_installcheck is_rescuesystem is_desktop_installed is_jeos is_sle is_staging is_upgrade);
 use File::Find;
 use File::Basename;
 use LWP::Simple 'head';
@@ -530,24 +530,6 @@ sub mellanox_config {
     load_reboot_tests() if (check_var('BACKEND', 'ipmi'));
 }
 
-sub load_baremetal_tests {
-    set_var('ADDONURL', 'sdk')               if (is_sle('>=12') && is_sle('<15')) && !is_released;
-    loadtest 'tests/kernel/ibtests_barriers' if get_var('IBTESTS');
-    loadtest "autoyast/prepare_profile"      if get_var "AUTOYAST_PREPARE_PROFILE";
-    if (get_var('IPXE')) {
-        loadtest 'installation/ipxe_install';
-        loadtest "console/suseconnect_scc";
-    } else {
-        load_boot_tests();
-        get_var("AUTOYAST") ? load_ayinst_tests() : load_inst_tests();
-        load_reboot_tests();
-    }
-    # make sure we always have the toolchain installed
-    loadtest "toolchain/install";
-    # some tests want to build and run a custom kernel
-    loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
-}
-
 sub load_nfv_tests {
     loadtest "nfv/hugepages_config" if get_var('HUGEPAGES');
     mellanox_config();
@@ -615,9 +597,6 @@ if (is_jeos) {
 
 # load the tests in the right order
 if (is_kernel_test()) {
-    if (get_var('LTP_BAREMETAL') && get_var('INSTALL_LTP')) {
-        load_baremetal_tests();
-    }
     load_kernel_tests();
 }
 elsif (get_var("NFV")) {