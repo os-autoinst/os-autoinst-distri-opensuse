os-autoinst/os-autoinst-distri-opensuse

commit 1e035ddb467f323eef13d07864e2f54a7c61f238

LTP: Use ASSET_1 as runtest file archive full path
instead of using ASSETDIR + /other (i.e. /var/lib/openqa/share/factory/other/),
which does not require to have mounted NFS /var/lib/openqa/share on
workers (which was often problematic on o3, see poo#63373, poo#51743).

Function parameter is now archive full path (previously it was a
path of a directory containing an archive).

This requires:
1) commit in openQA
d5c17138e ("Rewrite asset list generation for cache service")

2) and (obviously) to add ASSET_1=runtest-files-%HDD_1%.tar.gz to all
testsuites that have LTP_COMMAND_FILE (and which use HDD_1; for other
e.g. IPMI on osd and s390x on o3 it needs to use
ASSET_1=runtest-files-%DISTRI%-%VERSION%-%ARCH%-%BUILD%-%FLAVOR%@%MACHINE%.tar.gz)
This is the path set up by get_ltp_tag().

Also move get_ltp_tag() from main_common.pm into install_ltp.pm as it's
now used only there.

lib/main_ltp.pm
@@ -18,13 +18,13 @@
 package main_ltp;
 use base 'Exporter';
 use Exporter;
-use testapi qw(check_var get_var);
+use testapi qw(check_var get_required_var get_var);
 use autotest;
 use Archive::Tar;
 use utils;
 use LTP::TestInfo 'testinfo';
 use File::Basename 'basename';
-use main_common qw(boot_hdd_image get_ltp_tag load_bootloader_s390x load_kernel_baremetal_tests);
+use main_common qw(boot_hdd_image load_bootloader_s390x load_kernel_baremetal_tests);
 use 5.018;
 use Utils::Backends 'is_pvm';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"
@@ -80,9 +80,8 @@ sub parse_runtest_file {
 
 sub loadtest_from_runtest_file {
     my $namelist           = get_var('LTP_COMMAND_FILE');
-    my $path               = shift || get_var('ASSETDIR') . '/other';
+    my $archive            = shift || get_required_var('ASSET_1');
     my $unpack_path        = './runtest-files';
-    my $tag                = get_ltp_tag();
     my $cmd_pattern        = get_var('LTP_COMMAND_PATTERN') || '.*';
     my $cmd_exclude        = get_var('LTP_COMMAND_EXCLUDE') || '$^';
     my $test_result_export = {
@@ -97,7 +96,7 @@ sub loadtest_from_runtest_file {
 
     mkdir($unpack_path, 0755);
     my $tar = Archive::Tar->new();
-    $tar->read("$path/runtest-files-$tag.tar.gz") || die "tar read failed $? $!";
+    $tar->read($archive) || die "tar read failed $? $!";
     $tar->setcwd($unpack_path);
     $tar->extract() || die "tar extract failed $? $!";
 

lib/main_common.pm
@@ -39,7 +39,6 @@ our @EXPORT = qw(
   chromiumstep_is_applicable
   consolestep_is_applicable
   default_desktop
-  get_ltp_tag
   gnomestep_is_applicable
   guiupdates_is_applicable
   have_scc_repos
@@ -266,22 +265,6 @@ sub is_kernel_test {
         || get_var('TRINITY')
         || get_var('NUMA_IRQBALANCE')
         || get_var('TUNED'));
-}
-
-sub get_ltp_tag {
-    my $tag = get_var('LTP_RUNTEST_TAG');
-
-    if (!defined $tag) {
-        if (defined get_var('HDD_1')) {
-            $tag = get_var('PUBLISH_HDD_1');
-            $tag = get_var('HDD_1') if (!defined $tag);
-            $tag = basename($tag);
-        } else {
-            $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
-        }
-    }
-    $tag =~ s/[^a-zA-Z0-9_@.]+/-/g;
-    return $tag;
 }
 
 # Isolate the loading of LTP tests because they often rely on newer features

tests/kernel/install_ltp.pm
@@ -20,7 +20,6 @@ use testapi;
 use registration;
 use utils;
 use bootloader_setup qw(add_custom_grub_entries add_grub_cmdline_settings);
-use main_common 'get_ltp_tag';
 use main_ltp 'loadtest_from_runtest_file';
 use power_action_utils 'power_action';
 use repo_tools 'add_qa_head_repo';
@@ -298,6 +297,22 @@ sub setup_network {
     }
 }
 
+sub get_ltp_tag {
+    my $tag = get_var('LTP_RUNTEST_TAG');
+
+    if (!defined $tag) {
+        if (defined get_var('HDD_1')) {
+            $tag = get_var('PUBLISH_HDD_1');
+            $tag = get_var('HDD_1') if (!defined $tag);
+            $tag = basename($tag);
+        } else {
+            $tag = get_var('DISTRI') . '-' . get_var('VERSION') . '-' . get_var('ARCH') . '-' . get_var('BUILD') . '-' . get_var('FLAVOR') . '@' . get_var('MACHINE');
+        }
+    }
+    $tag =~ s/[^a-zA-Z0-9_@.]+/-/g;
+    return $tag;
+}
+
 sub run {
     my $self       = shift;
     my $inst_ltp   = get_var 'INSTALL_LTP';
@@ -361,7 +376,7 @@ sub run {
 
     if (get_var('LTP_COMMAND_FILE')) {
         # This assumes that current working directory is the worker's pool dir
-        loadtest_from_runtest_file('assets_public');
+        loadtest_from_runtest_file("assets_public/runtest-files-$tag.tar.gz");
     }
 
     is_jeos && zypper_call 'in system-user-bin system-user-daemon';

commit 0b68492ab2e9438ad0a58f9d8e20037697de4e95

LTP: Use unique variable ASSET_31 for runtest file
There are ASSET_1 and ASSET_2 already used on test suite
create_hdd_textmode. They are defined in test module (not in the test
suite), and it got propagated to child jobs: install_ltp and then to
actual LTP tests, although these define ASSET_1 in test suites.

Thus use unique number ASSET_31 (really bad solution, but asset
inheritance don't give us better solution).

This obviously expects to change the testsuite setup.

Fixes: 1e035ddb4 ("LTP: Use ASSET_1 as runtest file archive full path")

lib/main_ltp.pm
@@ -80,7 +80,7 @@ sub parse_runtest_file {
 
 sub loadtest_from_runtest_file {
     my $namelist           = get_var('LTP_COMMAND_FILE');
-    my $archive            = shift || get_required_var('ASSET_1');
+    my $archive            = shift || get_required_var('ASSET_31');
     my $unpack_path        = './runtest-files';
     my $cmd_pattern        = get_var('LTP_COMMAND_PATTERN') || '.*';
     my $cmd_exclude        = get_var('LTP_COMMAND_EXCLUDE') || '$^';