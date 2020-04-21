606bd326342979b21a1dc8ff516677626cd4eb6f
Jozef Pupava <jpupava@suse.com>
2020-04-21 08:46:57 +0200

    Let proc_sys_dump script generate white list and do upload

--- tests/kernel/proc_sys_dump.pm
+++ tests/kernel/proc_sys_dump.pm
@@ -27,22 +27,19 @@ require bmwqemu;
 use Utils::Architectures 'is_aarch64';
 
 sub run {
-    my ($self)         = @_;
-    my $tar_dir        = '/tmp/proc_sys_dump/';
-    my $tar            = $tar_dir . 'tar.xz';
-    my $ps_dump        = 'proc_sys_dump.sh';
-    my $white_list     = '~/proc_sys_whitelist.txt';
-    my $use_white_list = check_var('PROC_SYS_USE_WHITELIST', 1);
-    my $wl_opt  = $use_white_list ? 'u' : 'w';
-    my $timeout = $use_white_list ? 120 : 300;
+    my ($self)     = @_;
+    my $tar_dir    = '/tmp/proc_sys_dump/';
+    my $tar        = $tar_dir . 'tar.xz';
+    my $ps_dump    = 'proc_sys_dump.sh';
+    my $timeout    = 300;
     my $script_url = data_url("ltp/$ps_dump");
 
     $timeout *= 2 if is_aarch64;
 
     assert_script_run("curl -sS -o /tmp/$ps_dump $script_url");
-    assert_script_run("chmod u+x /tmp/$ps_dump", timeout => 300);
+    assert_script_run("chmod u+x /tmp/$ps_dump");
     assert_script_run("mkdir -p $tar_dir");
-    assert_script_run("/tmp/$ps_dump -c $tar -$wl_opt $white_list", timeout => $timeout);
+    assert_script_run("/tmp/$ps_dump -c $tar && sync", timeout => $timeout);
     upload_logs($tar);
 }
 
