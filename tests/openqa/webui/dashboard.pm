318359db01f378914a7ba23370ae3c174c0db397
Oliver Kurz <okurz@suse.de>
2020-05-07 11:08:44 +0200

    openqa: Prevent double check of same screen
    Just a record_info for a slow firefox will not trigger any action
    different than just a long timeout does.

--- tests/openqa/webui/dashboard.pm
+++ tests/openqa/webui/dashboard.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2018-2019 SUSE LLC
+# Copyright © 2018-2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -23,11 +23,7 @@ sub run {
     x11_start_program("firefox http://localhost", timeout => 60, valid => 0);
     # starting from git might take a bit longer to get and generated assets
     # workaround for poo#19798, basically doubles the timeout
-    if ((check_screen 'openqa-dashboard', 180) == undef) {
-        record_info 'ff took to long to start';
-    }
-    #wait few minutes for ff to start and then fail the test
-    assert_screen 'openqa-dashboard', 360;
+    assert_screen 'openqa-dashboard', 600;
 }
 
 sub test_flags {
