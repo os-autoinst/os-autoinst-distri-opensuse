21a30988a51800ac7c032052cfd83790be89bfd1
Jozef Pupava <jpupava@suse.com>
2020-05-28 12:25:04 +0200

    Handle evolution wizard until receiving within start_evolution
    -drop code for skip lookup, was not used and is not needed anymore
    -when evolution could not find details of nooop_test3@aim.com,
     cancel wizard and continue with smoke test poo#67408
    -drop loop for evolution_smoke-detect-folders-scanning, this code was not used,
     when evolution could not lookup details of entered email, tests did not pass
     evolution_wizard-account-summary and failed, this maybe worked long time ago

--- tests/x11/evolution/evolution_smoke.pm
+++ tests/x11/evolution/evolution_smoke.pm
@@ -24,18 +24,6 @@ use testapi;
 use utils;
 use version_utils qw(is_sle is_tumbleweed);
 
-sub evolution_wizard {
-    my ($self, $mail_box) = @_;
-    # Clean and Start Evolution
-    # Follow the wizard to setup mail account
-    $self->start_evolution($mail_box);
-    assert_screen "evolution_wizard-account-summary", 60;
-    assert_and_click "evolution-option-next";
-
-    assert_screen "evolution_wizard-done";
-    send_key "alt-a";
-}
-
 sub run {
     my $self        = shift;
     my $mail_box    = 'nooops_test3@aim.com';
@@ -43,26 +31,7 @@ sub run {
 
     mouse_hide(1);
     x11_start_program('xterm -e "gsettings set org.gnome.desktop.session idle-delay 0"', valid => 0);
-    evolution_wizard($self, $mail_box);
-
-    # init time counter
-    my $time_counter = 0;
-    while (1) {
-        # look for mail authentication window or folders scanning, fail in other situations
-        assert_screen([qw(evolution_smoke-detect-folders-scanning evolution_mail-auth)]);
-        # break loop and continue with test in case of mail authentication window
-        last if (match_has_tag('evolution_mail-auth'));
-        # if evolution still hangs on folders scanning after 10 tries, try another mail provider
-        if ($time_counter == 10) {
-            record_info('Connection problem', "Server is not responding, trying backup email provider.");
-            my $mail_box = 'nooops_test3@gmx.com';
-            evolution_wizard($self, $mail_box);
-            assert_screen "evolution_mail-auth";
-            last;
-        }
-        ++$time_counter;
-        sleep 1;
-    }
+    $self->start_evolution($mail_box);
 
     send_key "super-up" if (check_screen "evolution_mail-init-window", 2);
     if (check_screen "evolution_mail-auth", 5) {
