3f6c61596e4edb08c2b5597613febcdd59abc1b3
lemon.li <leli@suse.com>
2020-06-02 14:15:34 +0800

    Increase timeout value for snap-default

--- lib/bootloader_setup.pm
+++ lib/bootloader_setup.pm
@@ -260,7 +260,7 @@ sub boot_into_snapshot {
     send_key_until_needlematch('boot-menu-snapshot', 'down', 10, 5);
     send_key 'ret';
     # assert needle to avoid send down key early in grub_test_snapshot.
-    assert_screen('snap-default', 60) if get_var('OFW');
+    assert_screen('snap-default', 120) if get_var('OFW');
     # in upgrade/migration scenario, we want to boot from snapshot 1 before migration.
     if ((get_var('UPGRADE') && !get_var('ONLINE_MIGRATION', 0)) || get_var('ZDUP')) {
         send_key_until_needlematch('snap-before-update', 'down', 40, 5);
