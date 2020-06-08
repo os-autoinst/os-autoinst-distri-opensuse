os-autoinst/os-autoinst-distri-opensuse

commit ad30e6b024dbfde19aa32f328073603ae0d2c95b

main_common: Simplify some conditions

products/opensuse/main.pm
@@ -1,7 +1,7 @@
 # SUSE's openQA tests
 #
 # Copyright © 2009-2013 Bernhard M. Wiedemann
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2012-2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -30,37 +30,15 @@ init_main();
 
 sub cleanup_needles {
     remove_common_needles;
-    if (!get_var("LIVECD")) {
-        unregister_needle_tags("ENV-LIVECD-1");
+    unregister_needle_tags('ENV-LIVECD-' . get_var('LIVECD') ? 0 : 1);
+    for my $wm (qw(mate lxqt enlightenment awesome)) {
+        remove_desktop_needles($wm) unless check_var('DE_PATTERN', $wm);
     }
-    else {
-        unregister_needle_tags("ENV-LIVECD-0");
-    }
-    if (!check_var("DE_PATTERN", "mate")) {
-        remove_desktop_needles("mate");
-    }
-    if (!check_var("DE_PATTERN", "lxqt")) {
-        remove_desktop_needles("lxqt");
-    }
-    if (!check_var("DE_PATTERN", "enlightenment")) {
-        remove_desktop_needles("enlightenment");
-    }
-    if (!check_var("DE_PATTERN", "awesome")) {
-        remove_desktop_needles("awesome");
-    }
-    if (!is_jeos) {
-        unregister_needle_tags('ENV-FLAVOR-JeOS-for-kvm');
-    }
-    if (!is_leap) {
-        unregister_needle_tags('ENV-LEAP-1');
-    }
-    if (!is_tumbleweed) {
-        unregister_needle_tags('ENV-VERSION-Tumbleweed');
-    }
+    unregister_needle_tags('ENV-FLAVOR-JeOS-for-kvm') unless is_jeos;
+    unregister_needle_tags('ENV-LEAP-1')              unless is_leap;
+    unregister_needle_tags('ENV-VERSION-Tumbleweed')  unless is_tumbleweed;
     for my $flavor (qw(Krypton-Live Argon-Live)) {
-        if (!check_var('FLAVOR', $flavor)) {
-            unregister_needle_tags("ENV-FLAVOR-$flavor");
-        }
+        unregister_needle_tags("ENV-FLAVOR-$flavor") unless check_var('FLAVOR', $flavor);
     }
     # unregister christmas needles unless it is December where they should
     # appear. Unused needles should be disregarded by admin delete then
@@ -77,14 +55,7 @@ testapi::set_distribution(DistributionProvider->provide());
 $testapi::distri->set_expected_serial_failures(create_list_of_serial_failures());
 $testapi::distri->set_expected_autoinst_failures(create_list_of_autoinst_failures());
 
-unless (get_var("DESKTOP")) {
-    if (check_var("VIDEOMODE", "text")) {
-        set_var("DESKTOP", "textmode");
-    }
-    else {
-        set_var("DESKTOP", "kde");
-    }
-}
+set_var('DESKTOP', check_var('VIDEOMODE', 'text') ? 'textmode' : 'kde') unless get_var('DESKTOP');
 
 if (check_var('DESKTOP', 'minimalx')) {
     set_var("NOAUTOLOGIN", 1);
@@ -120,10 +91,7 @@ if (check_var('DESKTOP', 'kde') && !get_var('KDE4')) {
     set_var("PLASMA5", 1);
 }
 
-# ZDUP_IN_X imply ZDUP
-if (get_var('ZDUP_IN_X')) {
-    set_var('ZDUP', 1);
-}
+set_var('ZDUP', 1) if get_var('ZDUP_IN_X');
 
 if (is_updates_test_repo && !get_var('ZYPPER_ADD_REPOS')) {
     my $repos = map_incidents_to_repo({OS => get_required_var('OS_TEST_ISSUES')}, {OS => get_required_var('OS_TEST_TEMPLATE')});

lib/main_common.pm
@@ -700,15 +700,10 @@ sub remove_common_needles {
     remove_desktop_needles("minimalx");
     remove_desktop_needles("textmode");
 
-    if (!check_var("VIDEOMODE", "text")) {
-        unregister_needle_tags("ENV-VIDEOMODE-text");
-    }
-
+    unregister_needle_tags("ENV-VIDEOMODE-text") unless check_var("VIDEOMODE", "text");
+    unregister_needle_tags('ENV-ARCH-s390x')     unless check_var('ARCH',      's390x');
     if (get_var("INSTLANG") && get_var("INSTLANG") ne "en_US") {
         unregister_needle_tags("ENV-INSTLANG-en_US");
-    }
-    if (!check_var('ARCH', 's390x')) {
-        unregister_needle_tags('ENV-ARCH-s390x');
     }
     else {    # english default
         unregister_needle_tags("ENV-INSTLANG-de_DE");

commit 44ef6367dc92b6698d59d240259959adc8ef32f3

cleanup_needles: Fix ternary operator evaluation
Use of uninitialized value in concatenation (.) or string at
/var/lib/openqa/cache/openqa1-opensuse/tests/opensuse/products/opensuse/main.pm
line 36.

?: (ternary operator) has lower preference than . (string
concatenation), thus it must be wrapped in brackets.

man perlop is your friend :)

Fixes: ad30e6b02 ("main_common: Simplify some conditions")

products/opensuse/main.pm
@@ -33,7 +33,7 @@ sub cleanup_needles {
     for my $distri (qw(opensuse microos)) {
         unregister_needle_tags("ENV-DISTRI-$distri") unless check_var('DISTRI', $distri);
     }
-    unregister_needle_tags('ENV-LIVECD-' . get_var('LIVECD') ? 0 : 1);
+    unregister_needle_tags('ENV-LIVECD-' . (get_var('LIVECD') ? 0 : 1));
     for my $wm (qw(mate lxqt enlightenment awesome)) {
         remove_desktop_needles($wm) unless check_var('DE_PATTERN', $wm);
     }