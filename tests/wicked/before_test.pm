adb23d74d4184e254ee744635de9c4197634a6ff
Anton Smorodskyi <asmorodskyi@suse.com>
2020-06-08 22:14:31 +0200

    wicked: remove python-libteam which not used in tests

--- tests/wicked/before_test.pm
+++ tests/wicked/before_test.pm
@@ -109,7 +109,7 @@ sub run {
             $package_list .= ' ndisc6';
         }
         $package_list .= ' openvswitch iputils';
-        $package_list .= ' libteam-tools libteamdctl0 python-libteam' if check_var('WICKED', 'advanced') || check_var('WICKED', 'aggregate');
+        $package_list .= ' libteam-tools libteamdctl0 ' if check_var('WICKED', 'advanced') || check_var('WICKED', 'aggregate');
         $package_list .= ' gcc' if check_var('WICKED', 'advanced');
         zypper_call('-q in ' . $package_list, timeout => 400);
         $self->reset_wicked();
