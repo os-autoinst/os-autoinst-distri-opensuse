729f705f297838ec772323b61cb4afea6a738318
Sofia Syrianidou <ssyrianidou@suse.com>
2020-06-09 17:16:02 +0200

    Add validation modules for nis tests

---
+++ tests/x11/nis_client_validate.pm
@@ -0,0 +1,50 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Validate YaST configuration functionality for NIS
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base "opensusebasetest";
+use testapi;
+use scheduler 'get_test_suite_data';
+use lockapi 'mutex_wait';
+use x11utils 'turn_off_gnome_screensaver';
+use utils 'systemctl';
+
+sub run {
+    my ($self) = @_;
+    my $test_data = get_test_suite_data();
+
+    x11_start_program('xterm -geometry 155x45+5+5', target_match => 'xterm');
+    turn_off_gnome_screensaver if check_var('DESKTOP', 'gnome');
+    become_root;
+
+    assert_script_run "ls /etc/yp.conf",              fail_message => "File /etc/yp.conf doesn't exist";
+    assert_script_run "grep \"ypserv\" /etc/yp.conf", fail_message => "\"ypserv\" was not found in /etc/yp.conf";
+    assert_script_run "grep \"$test_data->{nis_domain}\" /etc/defaultdomain",
+      fail_message => "\"$test_data->{nis_domain}\" was not found in /etc/defaultdomain";
+    mutex_wait('nis_user_ready');
+    assert_script_run "ypmatch $test_data->{username} passwd",
+      fail_message => "New nis user is not visible from NIS client";
+    # In order for the client to use nis users, nscd needs to be restarted.
+    systemctl "restart nscd";
+    assert_script_run "grep -r netgroup /etc/nsswitch.conf | grep nis",
+      fail_message => "nsswitch.conf was not modified properly by NIS configuration";
+    assert_script_run "su - $test_data->{username} -c 'pwd | grep $test_data->{username}'",
+      fail_message => "Home directory of new NIS user is not the expected one";
+    assert_script_run "su - $test_data->{username} -c 'echo \"nis works\" > some_random_file'",
+      fail_message => "Cannot write in home directory of new NIS user from NIS client";
+    assert_script_run "su - $test_data->{username} -c 'grep \"nis works\" some_random_file'",
+      fail_message => "Failed to verify writability to home directory of new NIS user from NIS client";
+    type_string "killall xterm\n";
+}
+
+1;
---
+++ tests/x11/nis_server_validate.pm
@@ -0,0 +1,51 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Validate YaST configuration functionality for NIS
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base "opensusebasetest";
+use testapi;
+use scheduler 'get_test_suite_data';
+use lockapi 'mutex_create';
+use x11utils 'turn_off_gnome_screensaver';
+use mmapi 'wait_for_children';
+
+sub run {
+    my ($self) = @_;
+    my $test_data = get_test_suite_data();
+
+    x11_start_program('xterm -geometry 155x45+5+5', target_match => 'xterm');
+    turn_off_gnome_screensaver if check_var('DESKTOP', 'gnome');
+    become_root;
+
+    assert_script_run "grep \"$test_data->{nfs_domain}\" /etc/idmapd.conf",
+      fail_message => "Nfs domain was not found in idmapd.conf";
+    assert_script_run "grep \"$test_data->{nis_domain}\" /etc/defaultdomain",
+      fail_message => "Nis domain was not found in defaultdomain";
+    foreach my $map (@{$test_data->{map_names}}) {
+        assert_script_run "find /var/yp/$test_data->{nis_domain}/ -name $map*",
+          fail_message => "$map was not found in /var/yp/$test_data->{nis_domain}/";
+    }
+    assert_script_run "useradd $test_data->{username} -p $test_data->{password}",
+      fail_message => "Failed to add user $test_data->{username}";
+    assert_script_run "cd /var/yp/",
+      fail_message => "Failed to switch to directory /var/yp/";
+    assert_script_run "make",
+      fail_message => "Failed to make Makefile in /var/yp/";
+    assert_script_run "chown $test_data->{username}:users /home/$test_data->{username}/",
+      fail_message => "Failed to change ownership of /home/$test_data->{username}/";
+    mutex_create('nis_user_ready');
+    wait_for_children;
+    type_string "killall xterm\n";
+}
+
+1;
