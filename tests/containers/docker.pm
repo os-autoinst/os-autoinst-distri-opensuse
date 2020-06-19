30eac32e4be157dfa060bd12b6849fdd86d51083
Anna Minou <anna.minou@suse.com>
2020-06-19 15:24:23 +0200

    Refactor docker.pm and podman.pm

--- tests/containers/docker.pm
+++ tests/containers/docker.pm
@@ -33,133 +33,32 @@ use strict;
 use warnings;
 use containers::common;
 use version_utils qw(is_sle is_leap);
-use containers::docker;
-
-sub test_seccomp {
-    my $no_seccomp = script_run('docker info | tee /dev/tty | grep seccomp');
-    if ($no_seccomp) {
-        my $err_seccomp_support = 'boo#1072367 - Docker Engine does NOT have seccomp support';
-        if (is_sle('<15') || is_leap('<15.0')) {
-            record_info('WONTFIX', $err_seccomp_support);
-        }
-        else {
-            die($err_seccomp_support);
-        }
-    }
-    else {
-        record_info('seccomp', 'Docker Engine supports seccomp');
-    }
-}
+use containers::utils;
 
 sub run {
     my ($self) = @_;
     $self->select_serial_terminal;
 
     my $sleep_time = 90 * get_var('TIMEOUT_SCALE', 1);
+    my $dir        = "/root/DockerTest";
 
     install_docker_when_needed();
     test_seccomp();
 
-    # images can be searched on the Docker Hub
-    validate_script_output("docker search --no-trunc tumbleweed", sub { m/Official openSUSE Tumbleweed images/ });
-
-    # images can be pulled from the Docker Hub
-    #   - pull minimalistic alpine image of declared version using tag
-    #   - https://store.docker.com/images/alpine
-    my $alpine_image_version = '3.6';
-    assert_script_run("docker image pull alpine:$alpine_image_version", timeout => 300);
-    #   - pull typical docker demo image without tag. Should be latest.
-    #   - https://store.docker.com/images/hello-world
-    assert_script_run("docker image pull hello-world", timeout => 300);
-    #   - pull image of last released version of openSUSE Leap
-    assert_script_run("docker image pull opensuse/leap", timeout => 600) unless (check_var('ARCH', 's390x'));
-    #   - pull image of openSUSE Tumbleweed
-    assert_script_run('docker image pull opensuse/tumbleweed', timeout => 600);
-
-    # local images can be listed
-    assert_script_run('docker image ls none');
-    #   - filter with tag
-    assert_script_run(qq{docker image ls alpine:$alpine_image_version | grep "alpine\\s*$alpine_image_version"});
-    #   - filter without tag
-    assert_script_run(qq{docker image ls hello-world | grep "hello-world\\s*latest"});
-    #   - all local images
-    my $local_images_list = script_output('docker image ls');
-    die('docker image opensuse/tumbleweed not found') unless ($local_images_list =~ /opensuse\/tumbleweed\s*latest/);
-    die("docker image opensuse/leap not found") if (!check_var('ARCH', 's390x') && !$local_images_list =~ /opensuse\/leap\s*latest/);
-
-    # containers can be spawned
-    #   - using 'run'
-    assert_script_run('docker container run --name test_1 hello-world | grep "Hello from Docker\!"');
-    #   - using 'create', 'start' and 'logs' (background container)
-    assert_script_run("docker container create --name test_2 alpine:$alpine_image_version /bin/echo Hello world");
-    assert_script_run('docker container start test_2 | grep "test_2"');
-    assert_script_run('docker container logs test_2 | grep "Hello world"');
-    #   - using 'run --rm'
-    assert_script_run(qq{docker container run --name test_ephemeral --rm alpine:$alpine_image_version /bin/echo Hello world | grep "Hello world"});
-    #   - using 'run -d' and 'inspect' (background container)
-    my $container_name = 'tw';
-    assert_script_run("docker container run -d --name $container_name opensuse/tumbleweed tail -f /dev/null");
-    assert_script_run("docker container inspect --format='{{.State.Running}}' $container_name | grep true");
-    my $output_containers = script_output('docker container ls -a');
-    die('error: missing container test_1') unless ($output_containers =~ m/test_1/);
-    die('error: missing container test_2') unless ($output_containers =~ m/test_2/);
-    die('error: ephemeral container was not removed') if ($output_containers =~ m/test_ephemeral/);
-    die("error: missing container $container_name") unless ($output_containers =~ m/$container_name/);
-
-    # containers state can be saved to a docker image
-    my $exit_code = script_run("docker container exec $container_name zypper -n in curl", 600);
-    if ($exit_code) {
-        record_info('poo#40958 - curl install failure, try with force-resolution.');
-        my $output = script_output("docker container exec $container_name zypper in --force-resolution -y -n curl", 600);
-        die('error: curl not installed in the container') unless ($output =~ m/Installing: curl.*done/);
-    }
-    assert_script_run("docker container commit $container_name tw:saved");
-
-    # network is working inside of the containers
-    my $output = script_output('docker container run tw:saved curl -I google.de');
-    die("network is not working inside of the container tw:saved") unless ($output =~ m{Location: http://www\.google\.de/});
-
-    # Using an init process as PID 1
-    assert_script_run 'docker run --rm --init opensuse/tumbleweed ps --no-headers -xo "pid args" | grep "1 .*init"';
-
-    if (script_run('command -v man') == 0) {
-        assert_script_run('man -P cat docker build | grep "docker-build - Build an image from a Dockerfile"');
-        assert_script_run('man -P cat docker config | grep "docker-config - Manage Docker configs"');
-    }
-
-    # containers can be stopped
-    assert_script_run("docker container stop $container_name");
-    assert_script_run("docker container inspect --format='{{.State.Running}}' $container_name | grep false");
-
-    # containers can be deleted
-    my $cmd_docker_rm = 'docker container rm test_1';
-    assert_script_run("$cmd_docker_rm | grep test_1");
-    $output_containers = script_output('docker container ls -a');
-    die("error: container was not removed: $cmd_docker_rm") if ($output_containers =~ m/test_1/);
-    my $cmd_docker_container_prune = 'docker container prune -f';
-    assert_script_run("$cmd_docker_container_prune");
-    $output_containers = script_output('docker container ls -a');
-    die("error: container was not removed: $cmd_docker_container_prune") if ($output_containers =~ m/test_2/);
-
-    # images can be deleted
-    my $images_to_delete = "alpine:$alpine_image_version hello-world opensuse/tumbleweed tw:saved";
-    $images_to_delete .= ' opensuse/leap' if (!check_var('ARCH', 's390x'));
-    my $cmd_docker_rmi = "docker image rm $images_to_delete";
-    my $output_deleted = script_output($cmd_docker_rmi);
-    die("error: docker image rm opensuse/leap") if (!check_var('ARCH', 's390x') && $output_deleted !~ m/Untagged: opensuse\/leap/);
-    die('error: docker image rm opensuse/tumbleweed')          unless ($output_deleted =~ m/Untagged: opensuse\/tumbleweed/);
-    die('error: docker image rm tw:saved')                     unless ($output_deleted =~ m/Untagged: tw:saved/);
-    die("error: docker image rm alpine:$alpine_image_version") unless ($output_deleted =~ m/Untagged: alpine:$alpine_image_version/);
-    die('error: docker image rm hello-world:latest')           unless ($output_deleted =~ m/Untagged: hello-world:latest/);
+    # Run basic docker tests
+    basic_container_tests("docker");
 
     # Set up environment for building a new image
-    set_up();
+    set_up("$dir");
 
     # Build the image
-    build_img();
+    build_img("$dir", "docker");
 
     # Run the built image
-    test_built_img();
+    test_built_img("docker");
+
+    # Clean container
+    clean_container_host("docker");
 }
 
 1;
--- tests/containers/podman.pm
+++ tests/containers/podman.pm
@@ -30,102 +30,30 @@ use warnings;
 use registration;
 use containers::common;
 use version_utils qw(is_sle is_leap is_jeos);
+use containers::utils;
 
 sub run {
     my ($self) = @_;
     $self->select_serial_terminal;
 
+    my $dir = "/root/DockerTest";
+
     install_podman_when_needed();
 
-    # images can be searched on the default registry
-    validate_script_output("podman search --no-trunc tumbleweed", sub { m/Official openSUSE Tumbleweed images/ });
+    # Run basic tests for podman
+    basic_container_tests("podman");
 
-    # images can be pulled from the default registry
-    #   - pull minimalistic alpine image of declared version using tag
-    #   - https://store.docker.com/images/alpine
-    my $alpine_image_version = '3.6';
-    assert_script_run("podman image pull alpine:$alpine_image_version", timeout => 300);
-    #   - pull typical podman demo image without tag. Should be latest.
-    #   - https://store.docker.com/images/hello-world
-    assert_script_run("podman image pull hello-world", timeout => 300);
-    #   - pull image of last released version of openSUSE Leap
-    assert_script_run("podman image pull opensuse/leap", timeout => 600);
-    #   - pull image of openSUSE Tumbleweed
-    assert_script_run('podman image pull opensuse/tumbleweed', timeout => 600);
+    # Setup the environment
+    set_up("$dir");
 
-    # local images can be listed
-    assert_script_run('podman image ls');
-    #   - filter with tag
-    assert_script_run(qq{podman image ls alpine:$alpine_image_version | grep "alpine\\s*$alpine_image_version"});
-    #   - filter without tag
-    assert_script_run(qq{podman image ls hello-world | grep "hello-world\\s*latest"});
-    #   - all local images
-    my $local_images_list = script_output('podman image ls');
-    die('podman image opensuse/tumbleweed not found') unless ($local_images_list =~ /opensuse\/tumbleweed\s*latest/);
-    die("podman image opensuse/leap not found")       unless ($local_images_list =~ /opensuse\/leap\s*latest/);
+    # Build the image
+    build_img("$dir", "podman");
 
-    # containers can be spawned
-    #   - using 'run'
-    assert_script_run('podman container run --name test_1 hello-world | grep "Hello from Docker\!"');
-    #   - using 'create', 'start' and 'logs' (background container)
-    assert_script_run("podman container create --name test_2 alpine:$alpine_image_version /bin/echo Hello world");
-    assert_script_run('podman container start test_2 | grep "test_2"');
-    assert_script_run('podman container logs test_2 | grep "Hello world"');
-    #   - using 'run --rm'
-    assert_script_run(qq{podman container run --name test_ephemeral --rm alpine:$alpine_image_version /bin/echo Hello world | grep "Hello world"});
-    #   - using 'run -d' and 'inspect' (background container)
-    my $container_name = 'tw';
-    assert_script_run("podman container run -d --name $container_name opensuse/tumbleweed tail -f /dev/null");
-    assert_script_run("podman container inspect --format='{{.State.Running}}' $container_name | grep true");
-    my $output_containers = script_output('podman container ls -a');
-    die('error: missing container test_1') unless ($output_containers =~ m/test_1/);
-    die('error: missing container test_2') unless ($output_containers =~ m/test_2/);
-    die('error: ephemeral container was not removed') if ($output_containers =~ m/test_ephemeral/);
-    die("error: missing container $container_name") unless ($output_containers =~ m/$container_name/);
+    # Run the built image
+    test_built_img("podman");
 
-    # containers state can be saved to a podman image
-    my $exit_code = script_run("podman container exec $container_name zypper -n in curl", 300);
-    if ($exit_code) {
-        record_info('poo#40958 - curl install failure, try with force-resolution.');
-        my $output = script_output("podman container exec $container_name zypper in --force-resolution -y -n curl", 300);
-        die('error: curl not installed in the container') unless ($output =~ m/Installing: curl.*done/);
-    }
-    assert_script_run("podman container commit $container_name tw:saved");
-
-    # network is working inside of the containers
-    my $output = script_output('podman container run tw:saved curl -I google.de');
-    die("network is not working inside of the container tw:saved") unless ($output =~ m{Location: http://www\.google\.de/});
-
-    # Using an init process as PID 1
-    assert_script_run 'podman run --rm --init opensuse/tumbleweed ps --no-headers -xo "pid args" | grep "1 /dev/init"';
-
-    if (script_run('command -v man') == 0) {
-        assert_script_run('man -P cat podman build | grep "podman-build - Build a container image using a Containerfile"');
-    }
-
-    # containers can be stopped
-    assert_script_run("podman container stop $container_name");
-    assert_script_run("podman container inspect --format='{{.State.Running}}' $container_name | grep false");
-
-    # containers can be deleted
-    my $cmd_podman_rm = 'podman container rm test_1';
-    assert_script_run("$cmd_podman_rm");
-    $output_containers = script_output('podman container ls -a');
-    die("error: container was not removed: $cmd_podman_rm") if ($output_containers =~ m/test_1/);
-    my $cmd_podman_container_prune = 'podman container prune -f';
-    assert_script_run("$cmd_podman_container_prune");
-    $output_containers = script_output('podman container ls -a');
-    die("error: container was not removed: $cmd_podman_container_prune") if ($output_containers =~ m/test_2/);
-
-    # images can be deleted
-    my $cmd_podman_rmi = "podman rmi -a";
-    $output_containers = script_output('podman container ls -a');
-    die('error: podman rmi -a did not remove opensuse/leap')                if ($output_containers =~ m/Untagged: opensuse\/leap/);
-    die('error: podman rmi -a did not remove opensuse/tumbleweed')          if ($output_containers =~ m/Untagged: opensuse\/tumbleweed/);
-    die('error: podman rmi -a did not remove tw:saved')                     if ($output_containers =~ m/Untagged: tw:saved/);
-    die("error: podman rmi -a did not remove alpine:$alpine_image_version") if ($output_containers =~ m/Untagged: alpine:$alpine_image_version/);
-    die('error: podman rmi -a did not remove hello-world:latest')           if ($output_containers =~ m/Untagged: hello-world:latest/);
-    clean_container_host('podman');
+    # Clean container
+    clean_container_host("podman");
 }
 
 sub post_fail_hook {
