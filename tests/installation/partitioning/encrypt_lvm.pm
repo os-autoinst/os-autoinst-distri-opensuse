89ba5071168070d3c35b5adc5b54f5d3c1b3ec5c
Rodion I <riafarov@suse.com>
2020-07-01 15:35:52 +0200

    Revert "Add scenario cryptlvm_iscsi"

--- tests/installation/partitioning/encrypt_lvm.pm
+++ tests/installation/partitioning/encrypt_lvm.pm
@@ -18,7 +18,7 @@ use testapi;
 
 sub run {
     my $partitioner = $testapi::distri->get_partitioner();
-    $partitioner->edit_proposal(multiple_disks => 1, is_lvm => 1, is_encrypted => 1);
+    $partitioner->edit_proposal(is_lvm => 1, is_encrypted => 1);
     $partitioner->get_suggested_partitioning_page()->assert_encrypted_partition_with_lvm_shown_in_the_list();
 }
 
