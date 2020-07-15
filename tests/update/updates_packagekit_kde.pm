567c1dbb18ce645a4edf75d4bb16f4664c11e325
Antonios Pappas <apappas@suse.com>
2020-07-15 16:30:49 +0200

    Change test to be more plasma specific
    After feedback about the tags and needles being to similar, I simplified the
    test and merged the bsc/boo numbers that reference the same bug.

--- tests/update/updates_packagekit_kde.pm
+++ tests/update/updates_packagekit_kde.pm
@@ -16,7 +16,6 @@ use warnings;
 use utils;
 use testapi;
 use x11utils qw(ensure_unlocked_desktop turn_off_kde_screensaver);
-use power_action_utils 'power_action';
 
 # Update with Plasma applet for software updates using PackageKit
 
@@ -31,62 +30,47 @@ sub run {
     my ($self) = @_;
     setup_system;
 
-    my @updates_installed_tags = qw(updates_none updates_available updates_available-tray );
-    assert_screen [qw(updates_available-tray tray-without-updates-available)];
-    if (match_has_tag 'updates_available-tray') {
-        assert_and_click("updates_available-tray");
+    # There are two valid states.  Tray with updates or without.
+    my @updates_state_tags = qw(plasma-tray-without-updates plasma-tray-with-updates );
+    assert_screen \@updates_state_tags;
 
-        # First update package manager, then packages, then bsc#992773 (2x)
-        while (1) {
-            assert_and_click_until_screen_change('updates_click-install');
+    # If there are no updates, the test passe.
+    if (match_has_tag 'plasma-tray-with-updates') {
+        # There can be a maximum of two updates cycles.
+        my $update_count = 0;
+        do {
+            # Install the updates.
+            assert_and_click("plasma-tray-with-updates");
+            assert_and_click_until_screen_change('plasma-updates-click-install');
 
+            $update_count++;
             # Wait until installation starts, intended to time out
             wait_still_screen(stilltime => 4, timeout => 5);
-
             # Wait until installation is done
-            assert_screen \@updates_installed_tags, 3600;
+            assert_screen \@updates_state_tags, 3600;
+
             # Make sure the applet has fetched the current status from the backend
             # and has finished redrawing. In case the update status changed after
             # the assert_screen, record a soft failure
             wait_still_screen;
 
             #Check if the applet reports that a restart is needed.
-            if (check_screen 'updates_installed-restart') {
-                assert_and_click 'updates_installed-restart', 5;
+            if (check_screen 'plasma-updates_installed-restart') {
+                assert_and_click 'plasma-updates_installed-restart', 5;
                 $self->wait_boot;
                 setup_system;
-                # Look again
-                assert_screen \@updates_installed_tags;
+                next;
             }
-            wait_still_screen;
-            if (match_has_tag('updates_none')) {
-                if (check_screen 'updates_none', 30) {
-                    last;
-                } else {
-                    record_soft_failure 'boo#992773';
-                }
-            } elsif (match_has_tag('updates_available')) {
-                # look again
-                if (check_screen 'updates_none', 0) {
-                    record_soft_failure 'boo#1041112';
-                    last;
-                }
+            #Check if "Install new updates" pops up even though there are no updates. (boo#1041112)
+            if (match_has_tag 'plasma-tray-with-updates' and check_screen 'plasma-tray-without-updates') {
+                record_soft_failure 'boo#1041112';
             }
-            # Check, if there are more updates available
-            elsif (match_has_tag('updates_available-tray')) {
-                # look again
-                if (check_screen 'updates_available-tray', 30) {
-                    assert_and_click("updates_available-tray");
-                } else {
-                    # Make sure, that there are no updates, otherwise fail
-                    assert_screen 'updates_none';
-                    record_soft_failure 'boo#1041112';
-                    last;
-                }
-            }
+        }  while (  check_screen 'plasma-tray-with-updates' and  $update_count < 2);
+
+        if (match_has_tag 'plasma-tray-with-updates') {
+            die "Updates should already have been installed";
         }
-        # Close tray updater
-        send_key("alt-f4");
+        assert_screen 'plasma-tray-without-updates';
     }
 }
 
@@ -97,7 +81,9 @@ sub post_fail_hook {
 }
 
 sub test_flags {
-    return {milestone => 1, fatal => 1};
+    return {milestone => 1
+                , fatal => 1};
 }
 
 1;
+
