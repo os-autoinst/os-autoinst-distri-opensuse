69f4e8cbdcbfcae3b2da42a8ec0a584c85b417b0
XGWang0 <xgwang@suse.com>
2020-07-21 11:29:07 +0800

    Add expect script get_guest_ip to openqa data
    This script is used to get guest ip address

---
+++ data/mitigation/xen/get_guest_ip.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+
+function _get_ip_by_expect {
+
+local guestname=$1
+
+expect -c " 
+set timeout 3600
+
+#hide echo
+log_user 0
+spawn -noecho virsh console ${guestname}
+
+#wait connection
+sleep 3
+send \"\r\n\r\n\r\n\"
+
+#condition expect
+expect {
+        \"*login:\" {
+                send \"root\r\"
+		exp_continue
+        }
+        \"*assword\" {
+                send \"nots3cr3t\r\"
+		exp_continue
+        }
+        \"*:~ #\" {
+                send -- \"ip route get 1\r\"
+        }
+
+	    \"error: The domain is not running\" {
+	    	puts \"The domain $guestname is not running\"
+	    	exit 8
+	    }
+        timeout {
+                puts \"The guest $guestname is broken during installation\"
+                exit 9
+        }
+}
+
+#submatch for output
+expect -re {dev.*\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})[^0-9]}
+
+if {![info exists expect_out(1,string)]} {
+        puts \"Match did not happen :(\"
+        exit 1
+}
+
+# assign submatch to variable
+set output \$expect_out(1,string)
+#clear terminal, no work for current situation
+unset expect_out(buffer)
+send \"\\035\\r\"
+expect eof
+
+puts \"\$output\"
+"
+}
+
+function get_guest_ip_addr() {
+	local guestname=$1
+	local gip
+	local ret
+	gip=$(_get_ip_by_expect $guestname)
+	ret=$?
+	#echo $gip
+	if [ $ret != 0 ];then
+		if [ $ret -eq 8 ];then
+			echo "Error: Domian $guestname is not running,"  $w
+			exit -1
+		fi
+	fi
+
+	echo $gip
+
+}
+
+get_guest_ip_addr $1
