421d4125f2ad160a34013173c4407d94f672b35e
Oleksandr Orlov <oorlov@suse.com>
2020-07-24 10:44:45 +0200

    Add validation for disk encryption activation …
    The commit adds the appropriate test module and updates the related
    schedule.
    
    Related ticket: https://progress.opensuse.org/issues/68962

---
+++ tests/console/validate_activation_encrypted_partition.pm
@@ -0,0 +1,34 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
+# Summary: Validation module to check that previously encrypted partition is active.
+# Covered scenarios:
+# - Verify that activated during installation the existing encrypted partition is actually active;
+# - Verify that all properties of the activated partition are correct (using 'cryptsetup status')
+# encrypted volumes that encrypted volume is successfully activated during
+#
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base "installbasetest";
+use scheduler 'get_test_suite_data';
+use testapi;
+use validate_encrypt_utils;
+
+sub run {
+    my $test_data = get_test_suite_data();
+    select_console 'install-shell';
+    my $status = parse_cryptsetup_status($test_data->{mapped_device});
+    verify_cryptsetup_message($test_data->{device_status}->{message}, $status->{message});
+    verify_cryptsetup_properties($test_data->{device_status}->{properties}, $status->{properties});
+    select_console 'installation';
+}
+
+1;
