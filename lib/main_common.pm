f96188725e0270113b1042d5535b6f107ae00106
phoenix <felix.niederwanger@suse.com>
2020-07-28 08:17:36 +0200

    Fix wpa_supplicant for SLES and public cloud
    Skips the wpa_supplicant tests on public cloud test runs and schedules
    wpa_supplicant on SLES 15+

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1694,7 +1694,7 @@ sub load_extra_tests_console {
     loadtest "console/gd";
     loadtest 'console/valgrind'       unless is_sle('<=12-SP3');
     loadtest 'console/sssd_samba'     unless (is_sle("<15") || is_sle(">=15-sp2"));
-    loadtest 'console/wpa_supplicant' unless (!is_x86_64 || is_sle('<15') || is_leap('<15.1') || is_jeos);
+    loadtest 'console/wpa_supplicant' unless (!is_x86_64 || is_sle('<15') || is_leap('<15.1') || is_jeos || is_public_cloud);
 }
 
 sub load_extra_tests_sdk {
