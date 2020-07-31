89f42fef6eb46adddfb210d3623344fbdb33c1e8
Julien ADAMEK <julien.adamek@suse.com>
2020-07-31 10:17:07 +0200

    Add new cluster rolling upgrade test

---
+++ tests/ha/check_cluster_integrity.pm
@@ -0,0 +1,27 @@
+# SUSE's openQA tests
+#
+# Copyright (c) 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Check cluster integrity
+# Maintainer: Christian Lanig <clanig@suse.com>
+
+use base 'opensusebasetest';
+use strict;
+use warnings;
+use testapi;
+use lockapi;
+use hacluster;
+use utils 'systemctl';
+
+sub run {
+    select_console 'root-console';
+    # Check for the state of the whole cluster
+    check_cluster_state;
+}
+
+1;
---
+++ tests/ha/cluster_state_mgmt.pm
@@ -0,0 +1,33 @@
+# SUSE's openQA tests
+#
+# Copyright (c) 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Manage cluster stack
+#          Stop the cluster if pacemaker is active, otherwise start it.
+# Maintainer: Julien Adamek <jadamek@suse.com>
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils 'systemctl';
+
+sub run {
+    my $self = shift;
+    $self->select_serial_terminal;
+    if (systemctl('-q is-active pacemaker', ignore_failure => 1)) {
+        record_info("Start cluster", "Cluster is starting");
+        script_run "crm cluster start";
+    }
+    else {
+        record_info("Stop cluster", "Cluster is stopping");
+        script_run "crm cluster stop";
+    }
+}
+
+1;
