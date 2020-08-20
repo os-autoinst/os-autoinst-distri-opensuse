5fcb450c663f9a1fd706f792e4aee29a45a60df9
Jonathan Rivrain <JRivrain@suse.com>
2020-08-20 18:52:23 +0200

    Enable firstboot_hostname in first boot test
    Related ticket https://progress.opensuse.org/issues/69175
    This change also aims to allow testing other firstboot clients, see
    motivation here https://progress.opensuse.org/issues/69130

--- tests/installation/enable_y2_firstboot.pm
+++ tests/installation/enable_y2_firstboot.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2019 SUSE LLC
+# Copyright © 2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -9,18 +9,24 @@
 
 # Summary: Enable YaST2 Firstboot module - Desktop workstation configuration utility
 # Doc: https://en.opensuse.org/YaST_Firstboot
-# Maintainer: Martin Loviska <mloviska@suse.com>
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
 
 use base 'y2_installbase';
 use strict;
 use warnings;
 use testapi;
 use utils qw(zypper_call clear_console);
+use scheduler 'get_test_suite_data';
 
 sub run {
+    my $test_data = get_test_suite_data();
     select_console 'root-console';
     zypper_call "in yast2-firstboot";
     assert_script_run 'touch /var/lib/YaST2/reconfig_system';
+    # Use default file from package if no custom file was specified
+    if ($test_data->{custom_control_file}) {
+        assert_script_run 'wget ' . data_url($test_data->{custom_control_file}) . ' -O /etc/YaST2/firstboot.xml';
+    }
     clear_console;
 }
 
