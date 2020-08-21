60487b95face59054c62172f29a9e7272f24ad95
Rodion Iafarov <riafarov@suse.com>
2020-08-21 15:09:39 +0200

    Reuse parse_repo_data to validate repos
    We have nice method to get all the repo properties which we then can use
    to validate repositories. Reusing this method allows to validate repo
    properties granularly.
    
    See [poo#69598](https://progress.opensuse.org/issues/69586).

--- tests/console/validate_mirror_repos.pm
+++ tests/console/validate_mirror_repos.pm
@@ -15,7 +15,8 @@ use strict;
 use warnings;
 use base "opensusebasetest";
 use testapi;
-use repo_tools 'validate_repo_enablement';
+use repo_tools 'validate_repo_properties';
+use registration 'scc_version';
 
 sub run {
     select_console 'root-console';
@@ -24,9 +25,17 @@ sub run {
     my $mirror_src = get_required_var("MIRROR_$method");
     $mirror_src .= '?ssl_verify=no' if ($method eq 'HTTPS');
     my $sle_prod = uc get_var('SLE_PRODUCT') . get_var('VERSION');
+    my $name     = $sle_prod . '-' . scc_version() . '-0';
 
     record_info("Mirror Validation", "Validate $mirror_src used for installation is added in the installed system");
-    validate_repo_enablement(alias => $sle_prod, name => $sle_prod, uri => $mirror_src);
+    validate_repo_properties({
+            Filter      => $name,
+            Alias       => $sle_prod,
+            Name        => $sle_prod,
+            URI         => $mirror_src,
+            Enabled     => 'Yes',
+            Autorefresh => 'On'
+    });
 }
 
 1;
