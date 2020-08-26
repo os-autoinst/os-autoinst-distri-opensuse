os-autoinst/os-autoinst-distri-opensuse

commit 37ffec08815bd0bd948bf93074ac0756fd6f018d

Schedule perf test on all versions
- Remove condition to run perf test only on sle<15-sp1
- Run on bare metal as well

lib/main_common.pm
@@ -1640,7 +1640,7 @@ sub load_extra_tests_console {
     loadtest "console/sqlite3";
     loadtest "console/ant" if is_sle('<15-sp1');
     loadtest "console/gdb";
-    loadtest "console/perf" if is_sle('<15-sp1');
+    loadtest "console/perf" unless is_sle;
     loadtest "console/sysctl";
     loadtest "console/sysstat";
     loadtest "console/curl_ipv6" unless get_var('PUBLIC_CLOUD');
@@ -3207,6 +3207,7 @@ sub load_kernel_baremetal_tests {
     }
     # make sure we always have the toolchain installed
     loadtest "toolchain/install";
+    loadtest "console/perf";
     # some tests want to build and run a custom kernel
     loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
 }

schedule/qam/15-SP1/mau-extratests.yaml
@@ -26,6 +26,7 @@ schedule:
 - console/java
 - console/sqlite3
 - console/gdb
+- console/perf
 - console/sysctl
 - console/sysstat
 - console/krb5

schedule/qam/15-SP2/mau-extratests.yaml
@@ -27,6 +27,7 @@ schedule:
 - console/java
 - console/sqlite3
 - console/gdb
+- console/perf
 - console/sysctl
 - console/sysstat
 - console/krb5

commit 650cd694ed1d74a763f367e8ba253c6a40c8cb5c

LTP baremetal: Don't run perf and oprofile
The purpose of install_ltp_baremetal* isn't to test installation, but
prepare SLES for kernel tests as quick as possible. Thus removing these
to speedup LTP bare metal testing (about 4 min).

console/oprofile is not now used anywhere, it should be either put
somewhere else or removed.

Fixes: 37ffec088 ("Schedule perf test on all versions")
Fixes: af2454ba0 ("Add bare metal test for oprofile")

lib/main_common.pm
@@ -3180,8 +3180,6 @@ sub load_kernel_baremetal_tests {
     }
     # make sure we always have the toolchain installed
     loadtest "toolchain/install";
-    loadtest "console/perf";
-    loadtest "console/oprofile";
     # some tests want to build and run a custom kernel
     loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
 }