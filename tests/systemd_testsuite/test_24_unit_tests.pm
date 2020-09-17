95b7b6277010c3f1c1a05809dbe2cfbbe573d56e
root <root@mcbrain-2.arch.suse.de>
2020-09-17 15:07:17 +0200

    add missing tests 24, 25 and 43

---
+++ tests/systemd_testsuite/test_24_unit_tests.pm
@@ -0,0 +1,36 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Run test executed by TEST-24-UNIT-TESTS from upstream after openSUSE/SUSE patches.
+# Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
+
+use base 'systemd_testsuite_test';
+use warnings;
+use strict;
+use testapi;
+
+sub pre_run_hook {
+    my ($self) = @_;
+    #prepare test
+    $self->testsuiteprepare('TEST-24-UNIT-TESTS');
+}
+
+sub run {
+    #run test
+    assert_script_run 'cd /var/opt/systemd-tests';
+    assert_script_run './run-tests.sh TEST-24-UNIT-TESTS --run 2>&1 | tee /tmp/testsuite.log', 60;
+    assert_script_run 'grep "PASS: ...TEST-24-UNIT-TESTS" /tmp/testsuite.log';
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+
+1;
