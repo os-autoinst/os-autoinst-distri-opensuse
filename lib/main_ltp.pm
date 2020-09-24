os-autoinst/os-autoinst-distri-opensuse

commit f743e19b574fb13a622633166ce0bd973afc54fc

Modify scheduling code for install_klp_product
The install_klp_product test will not work with KOTD builds so creating
a separate job for it would waste worker resources. Append it to the existing
livepatch job with exception for KOTD builds.

lib/main_ltp.pm
@@ -88,6 +88,9 @@ sub load_kernel_tests {
         }
         loadtest_kernel 'boot_ltp';
         loadtest_kernel 'qa_test_klp';
+        unless (get_var('KOTD_REPO') || get_var('INSTALL_KOTD')) {
+            loadtest_kernel 'install_klp_product';
+        }
     }
     elsif (get_var('INSTALL_KLP_PRODUCT')) {
         loadtest_kernel 'boot_ltp';

tests/kernel/install_klp_product.pm
@@ -25,6 +25,13 @@ sub run {
     my $kver;
     my $kflavor;
 
+    # Running in the same job as qa_test_klp, reboot to fix kernel state
+    if (get_var('QA_TEST_KLP_REPO')) {
+        power_action('reboot', textmode => 1);
+        $self->wait_boot;
+        $self->select_serial_terminal;
+    }
+
     my $output = script_output('uname -r');
     if ($output =~ /^([0-9]+([-.][0-9a-z]+)*)-([a-z][a-z0-9]*)/i) {
         $kver    = $1;

commit 992bd9f2b93f00a64f5bab2414c7a18166edc060

install_klp_product: Use only for SLE
sle-live-patching repo is not available for openSUSE (and module was not
expecting to be used on openSUSE), thus it broke Tumbleweed
kernel-live-patching tests.

Fixes: f743e19b5 ("Modify scheduling code for install_klp_product")

lib/main_ltp.pm
@@ -25,6 +25,7 @@ use utils;
 use main_common qw(boot_hdd_image load_bootloader_s390x load_kernel_baremetal_tests);
 use 5.018;
 use Utils::Backends 'is_pvm';
+use version_utils 'is_opensuse';
 # FIXME: Delete the "## no critic (Strict)" line and uncomment "use warnings;"
 # use warnings;
 
@@ -90,7 +91,8 @@ sub load_kernel_tests {
         loadtest_kernel 'qa_test_klp';
         unless (get_var('KOTD_REPO') ||
             get_var('INSTALL_KOTD') ||
-            get_var('AZURE')) {
+            get_var('AZURE')        ||
+            is_opensuse) {
             loadtest_kernel 'install_klp_product';
         }
     }