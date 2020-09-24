58d52363bb27f8ccd73598b2d1bf21eb30c2fc24
Joaquín Rivera <jeriveramoya@suse.com>
2020-09-24 09:34:16 +0200

    Add test data for xfs scenario in PowerVM
    Additionally, move partition table inside disk in test data for
    test suites: msdos,xfs,nvme

--- tests/installation/partitioning/msdos_partition_table.pm
+++ tests/installation/partitioning/msdos_partition_table.pm
@@ -6,29 +6,30 @@
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
-
+#
 # Summary: Create new partition table during installation.
-# Maintainer: Sofia Syrianidou <ssyrianidou@suse.com>
+#
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
 
 use strict;
 use warnings;
 use parent 'installbasetest';
 use testapi;
-use version_utils ':VERSION';
 use scheduler 'get_test_suite_data';
 
 sub run {
-    my $test_data   = get_test_suite_data();
-    my $partitioner = $testapi::distri->get_expert_partitioner();
-    $partitioner->run_expert_partitioner();
-    $partitioner->create_new_partition_table({disk => $test_data->{partition_table_disk}, table_type => $test_data->{table_type}});
+    my $test_data   = get_test_suite_data;
+    my $partitioner = $testapi::distri->get_expert_partitioner;
+    $partitioner->run_expert_partitioner;
     foreach my $disk (@{$test_data->{disks}}) {
+        $partitioner->create_new_partition_table({disk => $disk->{name},
+                table_type => $disk->{table_type}});
         foreach my $partition (@{$disk->{partitions}}) {
-            $partitioner->add_partition_msdos({disk => $disk->{name}, partition => $partition});
+            $partitioner->add_partition_msdos({disk => $disk->{name},
+                    partition => $partition});
         }
     }
-    $partitioner->accept_changes_and_press_next();
+    $partitioner->accept_changes_and_press_next;
 }
+
 1;
-
-
