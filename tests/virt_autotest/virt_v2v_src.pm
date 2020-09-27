ddcebd9519cd3254d0445c67bc0a1e0297f9b550
Leon Guo <xguo@suse.com>
2020-09-27 13:09:49 +0200

    [virt_autotest] Fix up log directory cleanup in prj6 virt_v2v

--- tests/virt_autotest/virt_v2v_src.pm
+++ tests/virt_autotest/virt_v2v_src.pm
@@ -22,13 +22,13 @@ use virt_utils;
 sub run {
     my ($self) = @_;
 
-    my $ip_out = $self->execute_script_run('ip route show|grep kernel|cut -d" " -f9|head -1', 30);
+    my $ip_out = script_output('ip route show|grep kernel|cut -d" " -f9|head -1', 30);
     set_var('SRC_IP',   $ip_out);
     set_var('SRC_USER', "root");
     set_var('SRC_PASS', $password);
     bmwqemu::save_vars();
 
-    $self->execute_script_run("rm -r /var/log/qa/ctcs2/* /tmp/virt-v2v/* -r", 30);
+    script_run("rm -r /var/log/qa/ctcs2/* /tmp/virt-v2v/* -r", 30);
 
     mutex_create('SRC_READY_TO_START');
 
