9278c2fd5aa89868565f4484678714886e1570a7
ybonatakis <ybonatakis@suse.com>
2020-09-30 10:10:09 +0200

    Upload collected files to the logs

--- tests/console/system_state.pm
+++ tests/console/system_state.pm
@@ -1,16 +1,25 @@
 # SUSE's openQA tests
 #
-# Copyright © 2012-2018 SUSE LLC
+# Copyright © 2020 SUSE LLC
 #
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+#
 # Summary:  Export the existing status of running tasks and system load
 # for future reference
 # - Collect running process list
 # - Collect system load average
+# - Upload the gatherings to the job's logs
 # Maintainer: Zaoliang Luo <zluo@suse.de>
 
 use base "consoletest";
@@ -21,9 +30,12 @@ use strict;
 use warnings;
 
 sub run {
+    my ($self) = shift;
     check_var("BACKEND", "ipmi") ? use_ssh_serial_console : select_console 'root-console';
-    script_run "ps axf > /tmp/psaxf.log";
-    script_run "cat /proc/loadavg > /tmp/loadavg_consoletest_setup.txt";
+    script_run "mkdir /tmp/system_state";
+    script_run "ps axf > /tmp/system_state/psaxf.log";
+    script_run "cat /proc/loadavg > /tmp/system_state/loadavg_consoletest_setup.txt";
+    $self->tar_and_upload_log('/tmp/system_state', '/tmp/stats_during_installation.tar.bz2');
 }
 
 1;
