941822f3b7c6df39ef4ec0644755752b0229c2d3
Sofia Syrianidou <ssyrianidou@suse.com>
2020-10-01 16:32:19 +0200

    Increase assert_screen timeout in yast2_network_settings

--- tests/yast2_gui/yast2_network_settings.pm
+++ tests/yast2_gui/yast2_network_settings.pm
@@ -34,7 +34,7 @@ sub run {
     $cmd{routing_tab}        = 'alt-u';
 
     select_console 'x11';
-    $self->launch_yast2_module_x11('lan', target_match => [qw(yast2-lan-ui yast2_still_susefirewall2 yast2-lan-warning-network-manager)], match_timeout => 60);
+    $self->launch_yast2_module_x11('lan', target_match => [qw(yast2-lan-ui yast2_still_susefirewall2 yast2-lan-warning-network-manager)], match_timeout => 120);
     if (match_has_tag 'yast2_still_susefirewall2') {
         send_key $cmd{install};
         wait_still_screen;
