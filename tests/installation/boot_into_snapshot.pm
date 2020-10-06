82ae484783ba5bce8895c1db157592d5b44a017e
Zaoliang Luo <zluo@suse.de>
2020-10-06 09:56:47 +0200

    Boot to snapshot after upgrade and then rollback
    update leap to tw, boot into snap and rollback to leap.
    rename boot_into_snapshot to rollback_from_ro_snapshot.
    create new boot_to_snapshot and rollback_from_ro_snapshot
    create YAML_SCHEDULE boot_into_snapshot_and_rollback_after_upgrade.yaml
    see https://progress.opensuse.org/issues/12964 for details.
    replace grub_test by boot_into_desktop.
    verifications:
    http://10.162.23.47/tests/8495 (Rollback_after_upgarde)
    http://10.162.23.47/tests/8384 (sles boot_into_snapsot new)
    http://10.162.23.47/tests/8383 (tw boot_into_snapshot new)

--- tests/installation/boot_into_snapshot.pm
+++ tests/installation/boot_into_snapshot.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2016-2018 SUSE LLC
+# Copyright © 2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -8,53 +8,50 @@
 # without any warranty.
 
 # Summary: Boot into root filesystem snapshot from boot menu
-#  this module tests the read-only snapshot, from console. (1)
-#  we print some console output only for nice debugging (1b)
-#  if variable UPGRADE is set, we want to check that the 2 snapshots
-#  before_upgrade and after upgrade are not identical. test is made
-#  by checking the /etc/os-release, this is compatible also for openSUSE-TW (2).
-#  The test is also used for testing the snapper rollback functionality.
-# Maintainer: Oliver Kurz <okurz@suse.de>
+# uses grub menu to boot into the last RO snapshot made by snapper
+# and verifies that `before_upgrade` and `after upgrade` are not identical.
+# Maintainer: Zaoliang Luo <zluo@suse.de>
 
 use strict;
 use warnings;
 use testapi;
 use base "opensusebasetest";
+use bootloader_setup qw(stop_grub_timeout boot_into_snapshot);
 
 sub run {
     my ($self) = @_;
-    assert_screen 'linux-login', 200;
-    select_console 'root-console';
-    # 1)
-    assert_script_run('touch /etc/NOWRITE;test ! -f /etc/NOWRITE');
-    # 1b) just debugging infos
-    assert_script_run("snapper --iso list");
-    assert_script_run("cat /etc/os-release");
-    if (get_var("UPGRADE")) {
-        # if we made a migration, the version should be for example opensuse before migr. 42.1 > 42.2
-        # extract number of version id: example SlES 12.2 -> 12.2. for opensuse also ok
-        my $OS_VERSION     = script_output("grep VERSION_ID /etc/os-release | cut -c13- | head -c -2");
-        my $OLD_OS_VERSION = script_output("grep VERSION_ID /.snapshots/2/snapshot/etc/os-release | cut -c13- | head -c -2");
-        # grub_bug bug:956046. menu entry not stable. we could boot in wrong menu-entry.
-        if ($OS_VERSION eq $OLD_OS_VERSION) {
-            die "OS_VERSION after Rollback matches OS_VERSION before Rollback";
-        }
+    if (check_var('DESKTOP', 'gnome')) {
+        assert_screen('generic-desktop', 200);
+        select_console 'root-console';
+        script_run("systemctl reboot", 0);
+        # start from cdrom boot menu and it should boot from hard disk and select start bootloader for snapshot
+        assert_screen('bootloader', 200);
+        send_key 'ret';
+        # boot into leap snapshot
+        send_key_until_needlematch('start-bootloader-from-snapshot', 'down', 10, 5);
+        send_key 'ret';
+        send_key_until_needlematch('snapshot-before-upgrade', 'down', 40, 5);
+        send_key 'ret';
+        send_key_until_needlematch('opensuse-leap', 'down', 10, 5);
+        send_key 'ret';
+        save_screenshot;
+        reset_consoles;
+        assert_screen('generic-desktop', 200);
     }
-    assert_script_run('snapper rollback');
-    assert_script_run('snapper --iso list');
-    assert_script_run('snapper --iso list | grep \'number.*first root filesystem.*|\s*$\'',
-        fail_message => 'first root filesystem should be marked for cleanup as well');
-    assert_script_run('snapper --iso list | grep \'number.*after installation\'.*important=yes');
-    assert_script_run('snapper --iso list | grep \'number.*rollback\'.*important=yes',
-        fail_message => 'rollback backup should be marked for cleanup fate#321773');
-    assert_script_run('snapper --iso list | tail -n 1 | grep \'|\s*| [^|]\+ |\s*$\'', fail_message => 'last snapshot should not be cleaned up but is not-important');
-    script_run("systemctl reboot", 0);
-    reset_consoles;
-    $self->wait_boot;
-}
-
-sub test_flags {
-    return {fatal => 1, milestone => 1};
+    else {
+        assert_screen('linux-login', 200);
+        select_console 'root-console';
+        script_run("systemctl reboot", 0);
+        reset_consoles;
+        stop_grub_timeout;
+        boot_into_snapshot;
+        assert_screen('linux-login', 200);
+        select_console 'root-console';
+        assert_script_run('touch /etc/NOWRITE;test ! -f /etc/NOWRITE');
+        script_run("systemctl reboot", 0);
+        reset_consoles;
+        $self->wait_boot;
+    }
 }
 
 sub post_fail_hook {
---
+++ tests/installation/rollback_from_ro_snapshot.pm
@@ -0,0 +1,69 @@
+# SUSE's openQA tests
+#
+# Copyright © 2016-2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Expects SUT booted into RO snapshot with state "before upgrade" (See `boot_into_snapshot`)
+#  (1) It prints some console output only for nice debugging
+#  if variable UPGRADE is set, it checks that the 2 snapshots
+#  `before_upgrade` and `after upgrade` are not identical. Test is made
+#  by checking the /etc/os-release, this is compatible also for openSUSE-TW.
+#  (2) It tests the snapper rollback functionality.
+# Maintainer: Zaoliang Luo <zluo@suse.de>
+
+use strict;
+use warnings;
+use testapi;
+use base "opensusebasetest";
+
+sub run {
+    my ($self) = @_;
+    if (check_var('DESKTOP', 'gnome')) {
+        assert_screen 'generic-desktop', 200;
+    }
+    else {
+        assert_screen 'linux-login', 200;
+    }
+    select_console 'root-console';
+    # 1) just debugging infos
+    assert_script_run("snapper --iso list");
+    assert_script_run("cat /etc/os-release");
+    if (get_var("UPGRADE")) {
+        # if we made a migration, the version should be for example opensuse before migr. 42.1 > 42.2
+        # extract number of version id: example SlES 12.2 -> 12.2. for opensuse also ok
+        my $OS_VERSION     = script_output("grep VERSION_ID /etc/os-release | cut -c13- | head -c -2");
+        my $OLD_OS_VERSION = script_output("grep VERSION_ID /.snapshots/2/snapshot/etc/os-release | cut -c13- | head -c -2");
+        # grub_bug bug:956046. menu entry not stable. we could boot in wrong menu-entry.
+    }
+    # 2) Rollback
+    assert_script_run('snapper rollback');
+    assert_script_run('snapper --iso list');
+    assert_script_run('snapper --iso list | grep \'number.*first root filesystem.*|\s*$\'',
+        fail_message => 'first root filesystem should be marked for cleanup as well');
+    assert_script_run('snapper --iso list | grep \'number.*after installation\'.*important=yes');
+    assert_script_run('snapper --iso list | grep \'number.*rollback\'.*important=yes',
+        fail_message => 'rollback backup should be marked for cleanup fate#321773');
+    assert_script_run('snapper --iso list | tail -n 1 | grep \'|\s*| [^|]\+ |\s*$\'', fail_message => 'last snapshot should not be cleaned up but is not-important');
+    script_run("systemctl reboot", 0);
+    reset_consoles;
+    $self->wait_boot;
+    select_console 'root-console';
+    assert_script_run("systemctl poweroff");
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+sub post_fail_hook {
+    my $self = shift;
+    $self->SUPER::post_fail_hook;
+    $self->export_logs;
+}
+
+1;
+
