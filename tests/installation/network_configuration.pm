a5cc93b85586e7ba19bf5d40194b4daad43fe62e
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-10-14 10:51:29 +0200

    Check install.inf
    Check that linuxrc set the expected settings in /etc/install.inf

--- tests/installation/network_configuration.pm
+++ tests/installation/network_configuration.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2018 SUSE LLC
+# Copyright © 2018-2020 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -9,6 +9,10 @@
 
 # Summary: Verify default options in Network Configuration during installation
 # and modify some of these options.
+# Related documentation:
+# - https://documentation.suse.com/sles/15-SP2/html/SLES-all/appendix-linuxrc.html
+# - https://github.com/openSUSE/linuxrc/blob/master/linuxrc_hostname.md
+# - https://github.com/yast/yast-network/blob/master/doc/hostname.md
 # Maintainer: Joaquín Rivera <jeriveramoya@suse.de>
 
 use base 'y2_installbase';
@@ -17,7 +21,31 @@ use warnings;
 use testapi;
 use registration 'assert_registration_screen_present';
 
+sub check_install_inf_settings {
+    my %install_inf_settings = (
+        # linuxrc writes by default /etc/install.inf::SetHostnameUsed=0
+        # linuxrc writes /etc/install.inf::SetHostnameUsed=1 if `hostname` boot option is used
+        SetHostnameUsed => script_output('grep "^SetHostnameUsed:" /etc/install.inf') =~ s/SetHostnameUsed: //r,
+
+        # linuxrc writes by default /etc/install.inf::SetHostname=1
+        # linuxrc writes /etc/install.inf::SetHostnameUsed=1 if `hostname` boot option is used
+        SetHostname => script_output('grep "^SetHostname:" /etc/install.inf') =~ s/SetHostname: //r,
+
+        # linuxrc doesn't write /etc/install.inf::Hostname by default. So, it is not present by default
+        # linuxrc takes the value of `hostname` boot option and writes it to /etc/install.inf::Hostname if `hostname` boot option is used
+        Hostname => script_output('grep "^Hostname:" /etc/install.inf', proceed_on_failure => 1) =~ s/Hostname: //r
+    );
+
+    die "Parameter `hostname` not used in cmdline, but `/etc/install.inf::SetHostnameUsed` is set to " . $install_inf_settings{SetHostnameUsed} if ($install_inf_settings{SetHostnameUsed} != 0);
+    die "By default hostname by DHCP should be enabled, but `/etc/install.inf::SetHostname` is set to " . $install_inf_settings{SetHostname} if ($install_inf_settings{SetHostname} != 1);
+    die "By default linuxrc should not specify a static hostname, but `/etc/install.inf::Hostname` is set to " . $install_inf_settings{Hostname} if ($install_inf_settings{Hostname});
+}
+
 sub run {
+    select_console('install-shell');
+    check_install_inf_settings();
+    select_console 'installation';
+
     if (get_var 'OFFLINE_SUT') {
         assert_screen 'inst-networksettings';
     }
