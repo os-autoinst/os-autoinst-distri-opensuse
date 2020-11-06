204c294bc80c8b4e69f1bdbb529b8cd631575b06
Rodion Iafarov <riafarov@suse.com>
2020-11-06 11:06:02 +0100

    Setup liyui REST API on qemu backend

---
+++ tests/installation/setup_libyui.pm
@@ -0,0 +1,33 @@
+# Copyright © 2020 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Summary: Modules sets up the environment for using libyui REST API with the
+# installer, which requires enabling libyui-rest-api packages.
+
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base "installbasetest";
+use testapi;
+
+sub run {
+    record_info('PORT', "Used port for libyui: " . get_var('YUI_PORT'));
+    assert_screen('startshell', timeout => 500);
+    assert_script_run('extend libyui-rest-api');
+    type_string "exit\n";
+}
+
+1;
