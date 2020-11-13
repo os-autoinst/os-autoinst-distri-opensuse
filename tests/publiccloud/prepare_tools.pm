17a2bed4d59f7f7979f5da6a6c35e8072cb2dc4b
Anton Smorodskyi <asmorodskyi@suse.com>
2020-11-13 12:07:22 +0100

    toolsimage: revert workaround for ec2uploadimg

--- tests/publiccloud/prepare_tools.pm
+++ tests/publiccloud/prepare_tools.pm
@@ -75,14 +75,13 @@ sub run {
     # Install ec2imgutils
     install_in_venv('ec2imgutils', 'ec2uploadimg');
     assert_script_run("curl " . data_url('publiccloud/ec2utils.conf') . " -o /root/.ec2utils.conf");
-    # TODO: revert to 'ec2uploadimg --version' when it will be fixed
-    record_info('ec2imgutils', 'ec2uploadimg:' . script_output('ec2uploadimg -h'));
+    record_info('ec2imgutils', 'ec2uploadimg:' . script_output('ec2uploadimg --version'));
 
     # Install Azure cli
     install_in_venv('azure-cli', 'az');
     my $azure_error = '/tmp/azure_error';
     record_info('Azure', script_output('az -v 2>' . $azure_error));
-    script_run('cat ' . $azure_error);
+    assert_script_run('cat ' . $azure_error);
     assert_script_run('test ! -s ' . $azure_error);
 
     # Install Google Cloud SDK
