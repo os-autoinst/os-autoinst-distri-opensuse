f84cba9fa41ea7d1c720dde7ea5329f0c75639a9
Rodion Iafarov <riafarov@suse.com>
2020-11-20 12:22:54 +0100

    Add method to setup volume group to expert partitioner

--- lib/Installation/Partitioner/LibstorageNG/v4_3/ExpertPartitionerController.pm
+++ lib/Installation/Partitioner/LibstorageNG/v4_3/ExpertPartitionerController.pm
@@ -19,10 +19,12 @@ use strict;
 use warnings;
 use testapi;
 use parent 'Installation::Partitioner::LibstorageNG::v4::ExpertPartitionerController';
+use Installation::Partitioner::LibstorageNG::v4_3::AddVolumeGroupPage;
 use Installation::Partitioner::LibstorageNG::v4_3::ClonePartitionsDialog;
 use Installation::Partitioner::LibstorageNG::v4_3::CreatePartitionTablePage;
 use Installation::Partitioner::LibstorageNG::v4_3::DeletingCurrentDevicesDialog;
 use Installation::Partitioner::LibstorageNG::v4_3::ExpertPartitionerPage;
+
 use YuiRestClient;
 
 sub new {
@@ -38,8 +40,14 @@ sub init {
     $self->{ClonePartitionsDialog}        = Installation::Partitioner::LibstorageNG::v4_3::ClonePartitionsDialog->new({app => YuiRestClient::get_app()});
     $self->{CreatePartitionTablePage}     = Installation::Partitioner::LibstorageNG::v4_3::CreatePartitionTablePage->new({app => YuiRestClient::get_app()});
     $self->{DeletingCurrentDevicesDialog} = Installation::Partitioner::LibstorageNG::v4_3::DeletingCurrentDevicesDialog->new({app => YuiRestClient::get_app()});
+    $self->{AddVolumeGroupPage}    = Installation::Partitioner::LibstorageNG::v4_3::AddVolumeGroupPage->new({app => YuiRestClient::get_app()});
 
     return $self;
+}
+
+sub get_add_volume_group_page {
+    my ($self) = @_;
+    return $self->{AddVolumeGroupPage};
 }
 
 sub get_clone_partition_dialog {
@@ -98,6 +106,18 @@ sub create_new_partition_table {
     $self->get_expert_partitioner_page()->select_create_partition_table();
     $self->get_deleting_current_devices_dialog()->press_yes();
     $self->get_create_new_partition_table_page()->press_next();
+}
+
+sub add_volume_group {
+    my ($self, $args) = @_;
+    $self->get_expert_partitioner_page()->select_lvm();
+    $self->get_expert_partitioner_page()->press_add_volume_group_button();
+    $self->get_add_volume_group_page()->set_volume_group_name($args->{name});
+    foreach my $device (@{$args->{devices}}) {
+        $self->get_add_volume_group_page()->select_available_device($device);
+    }
+    $self->get_add_volume_group_page()->press_add_button();
+    $self->get_add_volume_group_page()->press_next_button();
 }
 
 1;
