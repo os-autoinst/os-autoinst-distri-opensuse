f991e2eb47953ab2d332acbfc8733799338f503e
Martin Doucha <mdoucha@suse.cz>
2020-11-20 16:28:59 +0100

    Move schedule_tests() from boot_ltp to LTP::utils

--- tests/kernel/boot_ltp.pm
+++ tests/kernel/boot_ltp.pm
@@ -18,11 +18,8 @@ use base 'opensusebasetest';
 use testapi;
 use LTP::WhiteList 'download_whitelist';
 use LTP::utils;
-use LTP::TestInfo 'testinfo';
 use version_utils 'is_jeos';
 use utils 'assert_secureboot_status';
-use main_ltp qw(loadtest_kernel shutdown_ltp);
-use File::Basename 'basename';
 
 sub run {
     my ($self) = @_;
@@ -137,110 +134,6 @@ EOF
     # If the command file (runtest file) is set then we dynamically schedule
     # the test and shutdown modules.
     schedule_tests($cmd_file) if $cmd_file;
-}
-
-sub read_runfile {
-    my ($runfile_path) = @_;
-    my $basename = basename($runfile_path);
-    my @ret;
-
-    upload_asset($runfile_path);
-    open my $rf, "assets_private/$basename" or die "Cannot open runfile $basename: $!";
-
-    while (my $line = <$rf>) {
-        push @ret, $line;
-    }
-
-    close($rf);
-    return \@ret;
-}
-
-sub schedule_tests {
-    my ($cmd_file) = @_;
-
-    my $test_result_export = {
-        format      => 'result_array:v2',
-        environment => {},
-        results     => []};
-    my $cmd_pattern = get_var('LTP_COMMAND_PATTERN') || '.*';
-    my $cmd_exclude = get_var('LTP_COMMAND_EXCLUDE') || '$^';
-    my $environment = {
-        product     => get_var('DISTRI') . ':' . get_var('VERSION'),
-        revision    => get_var('BUILD'),
-        flavor      => get_var('FLAVOR'),
-        arch        => get_var('ARCH'),
-        backend     => get_var('BACKEND'),
-        kernel      => '',
-        libc        => '',
-        gcc         => '',
-        harness     => 'SUSE OpenQA',
-        ltp_version => ''
-    };
-    my $ver_linux_out = script_output("cat /tmp/ver_linux_before.txt");
-    if ($ver_linux_out =~ qr'^Linux\s+(.*?)\s*$'m) {
-        $environment->{kernel} = $1;
-    }
-    if ($ver_linux_out =~ qr'^Linux C Library\s*>?\s*(.*?)\s*$'m) {
-        $environment->{libc} = $1;
-    }
-    if ($ver_linux_out =~ qr'^Gnu C\s*(.*?)\s*$'m) {
-        $environment->{gcc} = $1;
-    }
-    $environment->{ltp_version}        = script_output('touch /opt/ltp_version; cat /opt/ltp_version');
-    $test_result_export->{environment} = $environment;
-
-    if ($cmd_file =~ m/ltp-aiodio.part[134]/) {
-        loadtest_kernel 'create_junkfile_ltp';
-    }
-
-    if ($cmd_file =~ m/lvm\.local/) {
-        loadtest_kernel 'ltp_init_lvm';
-    }
-
-    for my $name (split(/,/, $cmd_file)) {
-        if ($name eq 'openposix') {
-            parse_openposix_runfile($name,
-                read_runfile('/root/openposix-test-list'),
-                $cmd_pattern, $cmd_exclude, $test_result_export);
-        }
-        else {
-            parse_runtest_file($name, read_runfile("/opt/ltp/runtest/$name"),
-                $cmd_pattern, $cmd_exclude, $test_result_export);
-        }
-    }
-
-    shutdown_ltp(run_args => testinfo($test_result_export));
-}
-
-sub parse_openposix_runfile {
-    my ($name, $cmds, $cmd_pattern, $cmd_exclude, $test_result_export) = @_;
-
-    for my $line (@$cmds) {
-        chomp($line);
-        if ($line =~ m/$cmd_pattern/ && !($line =~ m/$cmd_exclude/)) {
-            my $test  = {name => basename($line, '.run-test'), command => $line};
-            my $tinfo = testinfo($test_result_export, test => $test, runfile => $name);
-            loadtest_kernel('run_ltp', name => $test->{name}, run_args => $tinfo);
-        }
-    }
-}
-
-sub parse_runtest_file {
-    my ($name, $cmds, $cmd_pattern, $cmd_exclude, $test_result_export) = @_;
-
-    for my $line (@$cmds) {
-        next if ($line =~ /(^#)|(^$)/);
-
-        #Command format is "<name> <command> [<args>...] [#<comment>]"
-        if ($line =~ /^\s* ([\w-]+) \s+ (\S.+) #?/gx) {
-            next if (check_var('BACKEND', 'svirt') && ($1 eq 'dnsmasq' || $1 eq 'dhcpd'));    # poo#33850
-            my $test  = {name => $1, command => $2};
-            my $tinfo = testinfo($test_result_export, test => $test, runfile => $name);
-            if ($test->{name} =~ m/$cmd_pattern/ && !($test->{name} =~ m/$cmd_exclude/)) {
-                loadtest_kernel('run_ltp', name => $test->{name}, run_args => $tinfo);
-            }
-        }
-    }
 }
 
 sub test_flags {
