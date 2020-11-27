0273faba80934a63f88a7407352bcd4d9ba0a610
Sergio Lindo <sergiolindo.empresa@gmail.com>
2020-11-27 08:40:53 +0100

    Give slow hyper-v 15 minutes more for installation
    Total of 1h 45min

--- tests/installation/await_install.pm
+++ tests/installation/await_install.pm
@@ -101,7 +101,7 @@ sub run {
     # our Hyper-V server is just too slow
     # SCC might mean we install everything from the slow internet
     if (check_var('VIRSH_VMM_FAMILY', 'hyperv') || (check_var('SCC_REGISTER', 'installation') && !get_var('SCC_URL'))) {
-        $timeout = 5500;
+        $timeout = 6300;
     }
     # aarch64 can be particularily slow depending on the hardware
     $timeout *= 2 if check_var('ARCH', 'aarch64') && get_var('MAX_JOB_TIME');
