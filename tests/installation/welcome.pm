e14bd8ec38af170976e66ef62b3c0372b26d776d
Julien ADAMEK <julien.adamek@suse.com>
2020-12-02 17:42:16 +0100

    Change sles4sap shortcut for quarterly iso tests

--- tests/installation/welcome.pm
+++ tests/installation/welcome.pm
@@ -70,7 +70,7 @@ sub get_product_shortcuts {
             : 'i',
             sled => 'x',
             sles4sap => is_ppc64le() ? 'i'
-            : (is_sle('15-SP2+') && is_x86_64()) ? 't'
+            : (is_sle('15-SP2+') && is_x86_64() && !is_quarterly_iso()) ? 't'
             : 'p',
             hpc => is_x86_64() ? 'g' : 'u',
             rt  => is_x86_64() ? 't' : undef
