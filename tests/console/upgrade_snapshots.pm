f4072af6924b3910cdcd80494c7e3741a10d942c
hjluo <hluo@suse.com>
2020-12-10 14:57:49 +0800

    Fix snapper list timeout issue in upgrade_snapshots
    Snapper list will cost lots of time if we have 40+ snapshots.
    Add time will not always work. We can use --disable-used-space
    to make it quicker to get the snapper list.

--- tests/console/upgrade_snapshots.pm
+++ tests/console/upgrade_snapshots.pm
@@ -23,12 +23,17 @@ sub run {
     select_console 'root-console';
 
     my $waittime = 200 * get_var('TIMEOUT_SCALE', 1);
-    script_run("snapper list | tee /dev/$serialdev", 0);
+
+    # 'snapper list' will cost a lots of time to finish if btrfs-balance.sh was triggered
+    # To make it faster, we'd check if snapper have disable-used-space option.
+    # This is a workaround for bsc#1167353.
+    record_info('workaround for bsc#1167353');
+    script_run("(snapper --help | grep -q -- --disable-used-space && snapper list --disable-used-space || snapper list) | tee /dev/$serialdev", 0);
     # Check if the snapshot called 'before update' is there
     wait_serial('pre\s*(\|[^|]*){4,}\s*\|\s*number\s*\|\s*before (update|online migration)\s*\|\s*important=yes', $waittime)
       || die 'upgrade snapshots test failed';
 
-    script_run("snapper list | tee /dev/$serialdev", 0);
+    script_run("(snapper --help | grep -q -- --disable-used-space && snapper list --disable-used-space || snapper list) | tee /dev/$serialdev", 0);
     # Check if the snapshot called 'after update' is there
     wait_serial('post\s*(\|[^|]*){4,}\s*\|\s*number\s*\|\s*after (update|online migration)\s*\|\s*important=yes', $waittime)
       || die 'upgrade snapshots test failed';
