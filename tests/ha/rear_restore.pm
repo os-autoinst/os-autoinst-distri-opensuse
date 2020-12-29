2e5fa6323e480b3e638a2fefc1ccd879d8f5b72d
Loic Devulder <ldevulder@suse.com>
2020-12-29 18:00:41 +0100

    HA: add ReaR tests
    ReaR is a tool used to backup/restore OS image. It is part of the HA
    module but not specific to HA.
    
    This commit adds the first version of automated tests.

---
+++ tests/ha/rear_restore.pm
@@ -0,0 +1,62 @@
+# SUSE's openQA tests
+#
+# Copyright (c) 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Restore a ReaR backup
+# Maintainer: Loic Devulder <ldevulder@suse.com>
+
+use base 'opensusebasetest';
+use strict;
+use warnings;
+use testapi;
+use power_action_utils 'power_action';
+
+sub upload_rear_logs {
+    # Create tarball with logfiles and upload it
+    my $logfile = '/tmp/rear-recover-logs.tar.bz2';
+    script_run("tar cjf $logfile /var/log/rear/rear-*.log /var/lib/rear/layout/* /var/lib/rear/recovery/*");
+    upload_logs("$logfile", failok => 1);
+}
+
+sub run {
+    my ($self) = @_;
+    my $hostname = get_var('HOSTNAME', 'susetest');
+
+    # Select recovering entry and wait for OS boot
+    assert_screen('rear-boot-screen');
+    send_key_until_needlematch('rear-recover-selected', 'up');
+    send_key 'ret';
+    $self->wait_boot_past_bootloader;
+
+    # Restore the OS backup
+    set_var('LIVETEST', 1);                               # Because there is no password in Rear miniOS
+    select_console('root-console', skip_setterm => 1);    # Serial console is not configured in ReaR miniOS
+    assert_script_run('export USER_INPUT_TIMEOUT=5; rear -d -D recover');
+    upload_rear_logs;
+    set_var('LIVETEST', 0);
+
+    # Reboot into the restored OS
+    power_action('reboot', keepconsole => 1);
+    $self->wait_boot;
+
+    # Test login to ensure that the based OS configuration is correctly restored
+    $self->select_serial_terminal;
+    assert_script_run('cat /etc/os-release ; uname -a');
+}
+
+sub post_fail_hook {
+    my ($self) = shift;
+    upload_rear_logs;
+    $self->SUPER::post_fail_hook unless get_var('LIVETEST', 0);
+}
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+1;
