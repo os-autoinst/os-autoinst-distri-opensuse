0969aeac44ccd79ba86159ab99a72d5e1ae8ad3c
Joaquín Rivera <jeriveramoya@suse.com>
2021-01-08 08:50:30 +0100

    Enable libyui in installed system (only x86_64)

--- lib/y2_module_guitest.pm
+++ lib/y2_module_guitest.pm
@@ -48,7 +48,7 @@ sub launch_yast2_module_x11 {
         script_run('pkill -TERM -e yast2');
         select_console('x11');
     }
-    my $yast_env_variables = y2_module_basetest::with_yast_env_variables();
+    my $yast_env_variables = y2_module_basetest::with_yast_env_variables($args{extra_vars});
     # the command started with 'sh -c' to be able to execute 'echo' in Desktop Runner on Gnome
     x11_start_program("sh -c 'xdg-su -c \"env $yast_env_variables /sbin/yast2 $module\"; echo \"yast2-$module-status-\$?\" > /dev/$serialdev'", target_match => @tags, match_timeout => $args{match_timeout});
     foreach ($args{target_match}) {
