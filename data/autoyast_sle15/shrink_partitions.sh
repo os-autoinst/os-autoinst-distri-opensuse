633e75238cf42c890eee5c2c0a51e0cf60a45a20
Sofia Syrianidou <ssyrianidou@suse.com>
2021-01-23 14:08:36 +0100

    Modify system validation for autoyast_lvm

--- data/autoyast_sle15/shrink_partitions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-# Check if AutoYaST displayed a warning about shrinking partitions to make them
-# fit (bsc#1078418).
-zgrep "Some additional space" /var/log/YaST2/y2log*.gz
-echo "AUTOYAST OK"
---
+++ tests/console/verify_firewall_services.pm
@@ -0,0 +1,24 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+#
+# Summary: Verification of firewall being inactive and allowing services http https.
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use parent 'y2_module_consoletest';
+use testapi;
+
+sub run {
+    validate_script_output("systemctl is-active firewalld",                        sub { /inactive/ });
+    validate_script_output("firewall-offline-cmd --zone=external --list-services", sub { /http https/ });
+
+}
+
+1;
