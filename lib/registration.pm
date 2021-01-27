44d6984f15d7ba09c03ba53fc74c60e97d707238
Jozef Pupava <jpupava@suse.com>
2021-01-27 17:43:24 +0100

    Wait for accepted license

--- lib/registration.pm
+++ lib/registration.pm
@@ -650,9 +650,9 @@ sub fill_in_registration_data {
                 send_key 'alt-y';
                 next;
             }
-            elsif (match_has_tag("license-agreement") || match_has_tag("license-agreement-accepted")) {
-                send_key 'alt-a' unless match_has_tag("license-agreement-accepted");
-                record_soft_failure 'bsc#1080450: license agreement is shown twice' if match_has_tag("license-agreement-accepted");
+            elsif (match_has_tag("license-agreement")) {
+                send_key 'alt-a';
+                assert_screen('license-agreement-accepted');
                 send_key $cmd{next};
                 assert_screen "remove-repository";
                 send_key $cmd{next};
