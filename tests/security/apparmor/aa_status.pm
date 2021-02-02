a92357c7974c7113513084769438831f76eae679
lilyeyes <llzhao@suse.com>
2021-02-02 17:29:37 +0800

    Automate new feature SLE-15464 - Update Apparmor
    Feature: SLE-15464 - QA: Update Apparmor to 2.13.4
    poo#80200 - [sle][security][sle15sp3][feature][automation] SLE-15464: QA: Update Apparmor to 2.13.4
    (parent poo)
    poo#81727 - [sle][security][sle15sp3][feature][automation] AppArmor SLE-15464: verify "aa-status" can "handle profile names containing '(' "
    poo#81730 - [sle][security][sle15sp3][feature][automation] AppArmor SLE-15464: verify bug 1848227
    poo#81732 - [sle][security][sle15sp3][feature][automation] AppArmor SLE-15464: verify " Fix crash when log message contains a filename with unbalanced parenthesis"

--- tests/security/apparmor/aa_status.pm
+++ tests/security/apparmor/aa_status.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 SUSE LLC
+# Copyright (C) 2018-2021 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,9 +17,10 @@
 # aa-status.
 # - Check if apparmor is active
 # - Run aa-status, check the output for strings about modules/profiles/processes
+#   and verify "aa-status" can "handle profile with name contains '('".
 # and strings enforced, complain, unconfined and loaded.
 # Maintainer: llzhao <llzhao@suse.com>
-# Tags: poo#36874, poo#44912
+# Tags: tc#1767574, poo#81727, poo#36874, poo#44912
 
 use base "consoletest";
 use strict;
@@ -27,11 +28,25 @@ use warnings;
 use testapi;
 use utils;
 use services::apparmor;
+use apparmortest qw(create_a_test_profile aa_status_stdout_check);
 
 sub run {
+    my ($self) = @_;
+
     select_console 'root-console';
     services::apparmor::check_service();
     services::apparmor::check_aa_status();
+
+    # Verify "aa-status" can "handle profile with name contains '('"
+    my $testfile    = "/usr/bin/ls";
+    my $str_special = '\(test\)';
+
+    apparmortest::create_a_test_profile_name_is_special($testfile, $str_special);
+    systemctl("restart apparmor");
+    services::apparmor::check_aa_status();
+    validate_script_output 'aa-status', sub { m/.*$str_special.*/ };
+    apparmortest::aa_tmp_prof_clean($self, "$testfile" . "$str_special");
+    apparmortest::aa_tmp_prof_clean($self, "/etc/apparmor.d/.*$str_special.*");
 }
 
 1;
