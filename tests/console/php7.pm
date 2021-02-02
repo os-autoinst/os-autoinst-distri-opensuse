6ca7afd624a01fcb0f889a418e8ce534ee2c295e
Dominique Leuenberger <dimstar@opensuse.org>
2021-02-02 09:51:15 +0100

    Revert "php7: be specific to install php7-* packages"

--- tests/console/php7.pm
+++ tests/console/php7.pm
@@ -30,7 +30,7 @@ sub run {
     assert_script_run('grep "PHP Version 7" /tmp/tests-console-php7.txt');
 
     # test function provided by external module (php7-json RPM)
-    zypper_call 'in php7-json php7-cli';
+    zypper_call 'in php-json';
     assert_script_run('php -r \'echo json_encode(array("foo" => true))."\n";\' | grep :true');
 
     # test reading file
