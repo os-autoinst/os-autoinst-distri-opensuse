d1a1d44bf1f8e14f0e0fe05a73aec23cd50d8a22
Sarah-Kriesch <sarah.kriesch@ibm.com>
2021-02-22 18:47:27 +0100

    Confirm installation

--- tests/installation/installation_overview.pm
+++ tests/installation/installation_overview.pm
@@ -42,9 +42,9 @@ sub ensure_ssh_unblocked {
                 send_key_until_needlematch 'ssh-blocked-selected', 'tab', 25;
                 send_key 'ret';
                 send_key_until_needlematch 'ssh-open', 'tab';
-                send_key_until_needlematch 'ssh-service-disabled', 'tab', 25;
-                send_key 'ret';
 		send_key_until_needlematch 'ssh-service-enabled', 'tab';
+		send_key_until_needlematch 'confirm-installation', 'tab';
+		send_key 'ret';
             }
         }
         #performance ci need disable firewall
