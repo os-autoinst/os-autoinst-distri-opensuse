ac5e8b32b94d92b1914d4bae8ae3e53eac3e887d
Petr Vorel <pvorel@suse.cz>
2021-03-08 11:38:05 +0100

    module_build: Fix missing MODULE_LICENSE
    Required since 1d6cd3929360 ("modpost: turn missing MODULE_LICENSE() into error")
    from v5.11-rc1.
    
    While at it, modernize code a bit:
    * add __init, __exit
    * add static
    * add module_init(), module_exit()
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- data/kernel/module/hello.c
+++ data/kernel/module/hello.c
@@ -1,13 +1,23 @@
-/* Example from http://www.tldp.org/LDP/lkmpg/2.6/html/x121.html */
+/*
+ * Example from http://www.tldp.org/LDP/lkmpg/2.6/html/x121.html
+ * Updated to the current standard
+ */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
-int init_module(void)
+
+static int __init hello_init_module(void)
 {
- printk(KERN_INFO "Hello world.\n");
- return 0;
+	printk(KERN_INFO "Hello world.\n");
+
+	return 0;
 }
-void cleanup_module(void)
+
+static void __exit hello_cleanup_module(void)
 {
- printk(KERN_INFO "Goodbye world.\n");
+	printk(KERN_INFO "Goodbye world.\n");
 }
+
+module_init(hello_init_module)
+module_exit(hello_cleanup_module)
+MODULE_LICENSE("GPL");
