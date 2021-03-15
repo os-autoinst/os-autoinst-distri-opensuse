21ff6e75b3b820edb2159cf6d00a4be5998ef20f
Julien ADAMEK <julien.adamek@suse.com>
2021-03-15 16:02:45 +0100

    Disable Kdump from the AutoYaST installer

---
+++ tests/installation/disable_kdump.pm
@@ -0,0 +1,52 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Disable kdump from the Installer
+#   Since 15-SP3, the kdump module is part of the first installer stage
+#   and it is enabled by default.
+#   We need to disable it for properly testing sysctl settings linked
+#   to the RAM amount.
+# Maintainer: Julien Adamek <jadamek@suse.com>
+
+use strict;
+use warnings;
+use base 'y2_installbase';
+use testapi;
+
+sub run {
+    my ($self) = shift;
+
+    # Verify Installation Settings overview is displayed as starting point
+    assert_screen "installation-settings-overview-loaded";
+
+    if (check_var('VIDEOMODE', 'text')) {
+        # Select section booting on Installation Settings overview on text mode
+        send_key $cmd{change};
+        assert_screen 'inst-overview-options';
+        send_key 'alt-k';
+    }
+    else {
+        # Select section kdump on Installation Settings overview (video mode)
+        send_key_until_needlematch 'kdump-section-selected', 'tab';
+        send_key 'ret';
+    }
+
+    assert_screen 'inst-kdump-settings';
+    # Disable kdump
+    send_key 'alt-d';
+    wait_still_screen(1);
+
+    save_screenshot;
+    send_key $cmd{ok};
+
+    # Adapting system setting needs longer time in case of installing/upgrading with multi-addons
+    assert_screen 'installation-settings-overview-loaded', 220;
+}
+
+1;
