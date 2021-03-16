7e2f5e29da3d4ddaba11439bc69deb4bb8a97b6c
Ioannis Bonatakis <ybonatakis@suse.com>
2021-03-16 17:56:47 +0100

    Replace totest images with orginal ones from registry
    This commit includes changes that we requested in the PR.
    The buildah_image is removed and becomes two individual modules
    for each runtime. Also The test will test original images rather the
    ones from the totest repo.
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/containers/buildah_docker.pm
+++ tests/containers/buildah_docker.pm
@@ -28,39 +28,28 @@ sub run {
     my ($image_names, $stable_names) = get_suse_container_urls();
     my ($running_version, $sp, $host_distri) = get_os_release;
     install_buildah_when_needed($host_distri);
-    install_podman_when_needed($host_distri);
     install_docker_when_needed($host_distri);
     allow_selected_insecure_registries(runtime => 'docker');
-    allow_selected_insecure_registries(runtime => 'podman');
     scc_apply_docker_image_credentials() if (get_var('SCC_DOCKER_IMAGE'));
 
-    for my $iname (@{$image_names}) {
+    for my $iname (@{$stable_names}) {
+        record_info 'testing image', $iname;
         test_container_image(image => $iname, runtime => 'buildah');
         if (check_os_release('suse', 'PRETTY_NAME')) {
-            # sle15-working-container is the default name given to a container. it is created in test_container_image
-            my $prefix_img_name = +(split /\//, +(split /:/, $iname)[0])[-1];
+            # Use container which it is created in test_container_image
+            # Buildah default name is conducted by <image-name>-working-container
+            my ($prefix_img_name) = $iname =~ /([^\/:]+)(:.+)?$/;
             test_opensuse_based_image(image => "${prefix_img_name}-working-container", runtime => 'buildah');
             # Due to the steps from the test_opensuse_based_image previously,
             # the image has been committed as refreshed
-            test_containered_app(runtime => 'podman',
-                buildah    => 1,
-                dockerfile => 'Dockerfile.suse',
-                base       => 'refreshed');
-            assert_script_run "podman rmi -f myapp";
             test_containered_app(runtime => 'docker',
                 buildah    => 1,
                 dockerfile => 'Dockerfile.suse',
                 base       => 'refreshed');
-            if (is_sle) {
-                my $container_id = script_output "docker ps -aqf 'ancestor=myapp'";
-                assert_script_run "docker cp /etc/SUSEConnect $container_id:/etc/SUSEConnect";
-                assert_script_run "docker cp /etc/zypp/credentials.d/SCCcredentials $container_id:/etc/zypp/credentials.d/SCCcredentials";
-                assert_script_run "docker exec -it $container_id zypper lr";
-            }
         }
     }
+
     scc_restore_docker_image_credentials();
-    clean_container_host(runtime => 'podman');
     clean_container_host(runtime => 'docker');
     clean_container_host(runtime => 'buildah');
 }
