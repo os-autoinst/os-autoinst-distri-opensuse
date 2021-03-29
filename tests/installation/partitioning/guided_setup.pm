891c3411a688ad3268b4b13bd73ba857b0b599aa
rwx788 <riafarov@suse.com>
2021-03-29 14:42:27 +0200

    Adjust guided setup module to handle multiple disks
    Adding changes to the test module to use REST API in order to handle
    multidisk partitioning, including cases for the existing partitions.
    Those methods can be reused in future for the single disk setups
    with existing partitions.

--- tests/installation/partitioning/guided_setup.pm
+++ tests/installation/partitioning/guided_setup.pm
@@ -19,7 +19,8 @@ use scheduler 'get_test_suite_data';
 sub run {
     my $test_data    = get_test_suite_data()->{guided_partitioning};
     my $guided_setup = $testapi::distri->get_guided_partitioner();
-
+    # Select disks to use, if multiple disks are available
+    $guided_setup->setup_disks_to_use(@{$test_data->{disks}}) if $test_data->{disks};
     $guided_setup->setup_partitioning_scheme($test_data->{partitioning_scheme});
     $guided_setup->setup_filesystem_options($test_data->{filesystem_options});
 }
