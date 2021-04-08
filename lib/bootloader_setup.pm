467c667a9448e210a9acc7f3b11779cb64bb28c4
Pavel Dostál <pdostal@suse.cz>
2021-04-08 15:33:18 +0200

    IPMI: Create GPT partition table after wiping drives

--- lib/bootloader_setup.pm
+++ lib/bootloader_setup.pm
@@ -1348,16 +1348,20 @@ sub prepare_disks {
     my $disks = script_output('lsblk -n -l -o NAME -d -e 7,11');
     for my $d (split('\n', $disks)) {
         script_run "wipefs -af /dev/$d";
-        if (get_var('ENCRYPT_ACTIVATE_EXISTING') || get_var('ENCRYPT_CANCEL_EXISTING'))
-        {
+        script_run "sync";
+        if (get_var('ENCRYPT_ACTIVATE_EXISTING') || get_var('ENCRYPT_CANCEL_EXISTING')) {
             create_encrypted_part(disk => $d);
             if (get_var('ETC_PASSWD') && get_var('ETC_SHADOW')) {
                 mimic_user_to_import(disk => $d,
                     passwd => get_var('ETC_PASSWD'),
                     shadow => get_var('ETC_SHADOW'));
             }
+        } else {
+            script_run "parted /dev/$d mklabel gpt";
+            script_run "sync";
         }
     }
+    script_run "lsblk";
 }
 
 1;
