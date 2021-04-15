os-autoinst/os-autoinst-distri-opensuse

commit cbfb4dcc7a43b134f471e59f6366f10d6eaa3e55

LTP: Allow install more than one package in LTP_PKG
+ move default package logic into own procedure get_default_pkg(),
update docs.

NOTE: there's no point running LVM tests in compat mode thus we're not
generating the runtest file for 32 bit packages.

lib/LTP/utils.pm
@@ -30,6 +30,7 @@ use File::Basename 'basename';
 
 our @EXPORT = qw(
   get_ltproot
+  get_ltp_version_file
   init_ltp_tests
   loadtest_kernel
   prepare_ltp_env
@@ -48,7 +49,18 @@ sub shutdown_ltp {
 }
 
 sub get_ltproot {
-    return get_required_var('TEST_SUITE_NAME') =~ m/[-_]m32$/ ? '/opt/ltp-32' : '/opt/ltp';
+    # TEST_SUITE_NAME is for running 32bit tests (e.g. ltp_syscalls_m32),
+    # checking LTP_PKG is for install_ltp.pm which also uses prepare_ltp_env()
+    my $want_32bit = shift // (get_required_var('TEST_SUITE_NAME') =~ m/[-_]m32$/
+          || get_var('LTP_PKG', '') =~ m/^(ltp|qa_test_ltp)-32bit$/);
+
+    return $want_32bit ? '/opt/ltp-32' : '/opt/ltp';
+}
+
+sub get_ltp_version_file {
+    my $want_32bit = shift // get_required_var('TEST_SUITE_NAME') =~ m/[-_]m32$/;
+
+    return get_ltproot($want_32bit) . '/version';
 }
 
 # Set up basic shell environment for running LTP tests
@@ -203,7 +215,9 @@ sub schedule_tests {
     if ($ver_linux_out =~ qr'^Gnu C\s*(.*?)\s*$'m) {
         $environment->{gcc} = $1;
     }
-    $environment->{ltp_version}        = script_output('touch /opt/ltp_version; cat /opt/ltp_version');
+
+    my $file = get_ltp_version_file();
+    $environment->{ltp_version}        = script_output("touch $file; cat $file");
     $test_result_export->{environment} = $environment;
 
     if ($cmd_file =~ m/ltp-aiodio.part[134]/) {

tests/kernel/install_ltp.pm
@@ -189,15 +189,15 @@ sub install_from_git {
         assert_script_run("git clone -q $url" . $rel, timeout => 360);
     }
     assert_script_run 'cd ltp';
-    # It is a shallow clone so 'git describe' won't work
-    script_run 'git log -1 --pretty=format:"git-%h" | tee /opt/ltp_version';
-
     assert_script_run 'make autotools';
     assert_script_run("$configure $extra_flags", timeout => 300);
     assert_script_run 'make -j$(getconf _NPROCESSORS_ONLN)', timeout => $timeout;
     script_run 'export CREATE_ENTRIES=1';
     assert_script_run 'make install', timeout => 360;
     assert_script_run "find $prefix -name '*.run-test' > ~/openposix-test-list";
+
+    # It is a shallow clone so 'git describe' won't work
+    script_run 'git log -1 --pretty=format:"git-%h" | tee ' . get_ltp_version_file();
 }
 
 sub add_ltp_repo {
@@ -220,12 +220,24 @@ sub add_ltp_repo {
     zypper_ar($repo, name => 'ltp_repo');
 }
 
-sub install_from_repo {
-    my $pkg = get_var('LTP_PKG', (is_sle && is_released) ? 'qa_test_ltp' : 'ltp');
+sub get_default_pkg {
+    my $pkg = 'ltp';
 
-    zypper_call("in --recommends $pkg");
-    script_run "rpm -qi $pkg | tee /opt/ltp_version";
-    assert_script_run "find " . get_ltproot() . q(/testcases/bin/openposix/conformance/interfaces/ -name '*.run-test' > ~/openposix-test-list);
+    return 'qa_test_ltp' if (is_sle && is_released);
+    return $pkg;
+}
+
+sub install_from_repo {
+    my @pkgs = split(/\s* \s*/, get_var('LTP_PKG', get_default_pkg));
+
+    zypper_call("in --recommends " . join(' ', @pkgs));
+
+    for my $pkg (@pkgs) {
+        my $want_32bit = $pkg =~ m/32bit/;
+        script_run "rpm -qi $pkg | tee " . get_ltp_version_file($want_32bit);
+        assert_script_run "find " . get_ltproot($want_32bit) .
+          q(/testcases/bin/openposix/conformance/interfaces/ -name '*.run-test' > ~/openposix-test-list);
+    }
 }
 
 sub setup_network {
@@ -331,6 +343,8 @@ sub run {
     add_custom_grub_entries if (is_sle('12+') || is_opensuse) && !is_jeos;
     setup_network;
 
+    # we don't run LVM tests in 32bit, thus not generating the runtest file
+    # for 32 bit packages
     if (!is_sle('<12')) {
         prepare_ltp_env();
         assert_script_run('generate_lvm_runfile.sh');
@@ -447,6 +461,9 @@ LTP_EXTRA_CONF_FLAGS="CFLAGS=-m32 LDFLAGS=-m32").
 
 LTP_PKG=qa_test_ltp
 Stable LTP package in QA head repository.
+
+LTP_PKG=ltp ltp-32bit
+Install both 64bit and 32bit LTP packages from nightly build.
 
 =head3 Available LTP packages
 https://confluence.suse.com/display/qasle/LTP+repositories

commit 3cd79f36aaa390500219d037bd8b28408900370e

LTP: Fix openposix on 32bit
Fixes: cbfb4dcc7 ("LTP: Allow install more than one package in LTP_PKG")

lib/LTP/utils.pm
@@ -30,6 +30,7 @@ use File::Basename 'basename';
 
 our @EXPORT = qw(
   get_ltproot
+  get_ltp_openposix_test_list_file
   get_ltp_version_file
   init_ltp_tests
   loadtest_kernel
@@ -48,13 +49,23 @@ sub shutdown_ltp {
     loadtest_kernel('shutdown_ltp', @_);
 }
 
-sub get_ltproot {
+sub want_ltp_32bit {
     # TEST_SUITE_NAME is for running 32bit tests (e.g. ltp_syscalls_m32),
     # checking LTP_PKG is for install_ltp.pm which also uses prepare_ltp_env()
-    my $want_32bit = shift // (get_required_var('TEST_SUITE_NAME') =~ m/[-_]m32$/
+    return (get_required_var('TEST_SUITE_NAME') =~ m/[-_]m32$/
           || get_var('LTP_PKG', '') =~ m/^(ltp|qa_test_ltp)-32bit$/);
+}
+
+sub get_ltproot {
+    my $want_32bit = shift // want_ltp_32bit;
 
     return $want_32bit ? '/opt/ltp-32' : '/opt/ltp';
+}
+
+sub get_ltp_openposix_test_list_file {
+    my $want_32bit = shift // want_ltp_32bit;
+
+    return get_ltproot($want_32bit) . '/runtest/openposix-test-list';
 }
 
 sub get_ltp_version_file {
@@ -271,6 +282,7 @@ sub parse_runtest_file {
     }
 }
 
+# NOTE: current implementation does not allow to run tests on both archs
 sub parse_runfiles {
     my ($cmd_file, $test_result_export, $suffix) = @_;
 
@@ -282,7 +294,7 @@ sub parse_runfiles {
     for my $name (split(/,/, $cmd_file)) {
         if ($name eq 'openposix') {
             parse_openposix_runfile($name,
-                read_runfile('/root/openposix-test-list'),
+                read_runfile(get_ltp_openposix_test_list_file()),
                 $cmd_pattern, $cmd_exclude, $test_result_export, $suffix);
         }
         else {

tests/kernel/install_ltp.pm
@@ -194,7 +194,8 @@ sub install_from_git {
     assert_script_run 'make -j$(getconf _NPROCESSORS_ONLN)', timeout => $timeout;
     script_run 'export CREATE_ENTRIES=1';
     assert_script_run 'make install', timeout => 360;
-    assert_script_run "find $prefix -name '*.run-test' > ~/openposix-test-list";
+    assert_script_run "find $prefix -name '*.run-test' > "
+      . get_ltp_openposix_test_list_file();
 
     # It is a shallow clone so 'git describe' won't work
     record_info("LTP git", script_output('git log -1 --pretty=format:"git-%h" | tee '
@@ -246,7 +247,8 @@ sub install_from_repo {
         record_info("LTP pkg: $pkg", script_output("rpm -qi $pkg | tee "
                   . get_ltp_version_file($want_32bit)));
         assert_script_run "find " . get_ltproot($want_32bit) .
-          q(/testcases/bin/openposix/conformance/interfaces/ -name '*.run-test' > ~/openposix-test-list);
+          q(/testcases/bin/openposix/conformance/interfaces/ -name '*.run-test' > )
+          . get_ltp_openposix_test_list_file($want_32bit);
     }
 }
 