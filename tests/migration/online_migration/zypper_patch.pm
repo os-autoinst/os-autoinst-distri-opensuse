a7edf0e35458123bd6dcde42039b52700f85b7af
chcao <chcao@suse.com>
2021-04-19 15:55:27 +0800

    Do not remove ESPOS before migration
    ESPOS is an integral part of the product, no need to deregister it
    before migration.

--- tests/migration/online_migration/zypper_patch.pm
+++ tests/migration/online_migration/zypper_patch.pm
@@ -31,7 +31,6 @@ sub run {
     fully_patch_system;
     install_patterns() if (get_var('PATTERNS'));
     remove_ltss;
-    remove_espos;
     power_action('reboot', keepconsole => 1, textmode => 1);
     reconnect_mgmt_console if is_pvm;
 
