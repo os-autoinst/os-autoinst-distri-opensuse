5c1b8a44086a025e04f98a5892ef376549a6834b
Sofia Syrianidou <ssyrianidou@suse.com>
2021-04-19 16:15:55 +0200

    Install tar package for leap transactional_server

---
+++ tests/transactional/trup_install_tar.pm
@@ -0,0 +1,34 @@
+# Copyright © 2021 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# Summary: Module to install tar package via transactional-update. The system is
+# rebooted so changes take effect.
+
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base "opensusebasetest";
+use testapi;
+use transactional;
+
+sub run {
+    select_console 'root-console';
+
+    record_info 'Install tar', 'Install package tar using transactional server and reboot';
+    trup_install "tar";
+}
+
+1;
