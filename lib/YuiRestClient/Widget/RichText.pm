6d1178e94349d6a79809394dce7f1e1cfb1e2668
rwx788 <riafarov@suse.com>
2021-04-20 13:23:03 +0200

    Add method to select link in the RichText widget
    This commits adds the method to press links in the rich text. This
    widget is widely used on the Overview page.

--- lib/YuiRestClient/Widget/RichText.pm
+++ lib/YuiRestClient/Widget/RichText.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright © 2020 SUSE LLC
+# Copyright © 2020-2021 SUSE LLC
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -13,12 +13,18 @@ package YuiRestClient::Widget::RichText;
 
 use strict;
 use warnings;
+use YuiRestClient::Action;
 
 use parent 'YuiRestClient::Widget::Base';
 
 sub text {
     my ($self) = @_;
     return $self->property('text');
+}
+
+sub activate_link {
+    my ($self, $link) = @_;
+    return $self->action(action => YuiRestClient::Action::YUI_SELECT, value => $link);
 }
 
 1;
