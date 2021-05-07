81759cc3946aa7c59cc1c86727169fc2f55acd3c
Oleksandr Orlov <oorlov@suse.com>
2021-05-07 12:52:44 +0200

    Initialize libyui in yast2_gui@svirt-xen-hvm
    yast2_lan_restart_* test modules are using libyui, but it was not set
    up.
    
    The commit sets up libyui.
    
    Related ticket: https://progress.opensuse.org/issues/90773

--- tests/yast2_gui/yast2_lan_restart_vlan.pm
+++ tests/yast2_gui/yast2_lan_restart_vlan.pm
@@ -26,11 +26,12 @@
 # Delete the VLAN device.
 # Maintainer: Oleksandr Orlov <oorlov@suse.de>
 
-use base 'y2_installbase';
+use base 'y2_module_guitest';
 use strict;
 use warnings;
 use testapi;
 use y2lan_restart_common qw(initialize_y2lan open_network_settings check_network_status wait_for_xterm_to_be_visible clear_journal_log close_xterm);
+use YuiRestClient;
 
 my $network_settings;
 
@@ -41,7 +42,12 @@ sub pre_run_hook {
     $network_settings = $testapi::distri->get_network_settings();
     $network_settings->add_vlan_device();
     $network_settings->save_changes();
+    # After network settings were changed, DHCP may assign another IP address (e.g. on xen-hvm).
+    # Init IP address again, so that libyui will be able to communicate with the YaST module.
+    YuiRestClient::set_host(YuiRestClient::init_host());
+    select_console('x11', await_console => 0);
     wait_for_xterm_to_be_visible();
+    clear_journal_log();
     $self->SUPER::pre_run_hook;
 }
 
@@ -51,7 +57,6 @@ sub run {
     $network_settings->view_vlan_device_without_editing();
     $network_settings->save_changes();
     wait_for_xterm_to_be_visible();
-    clear_journal_log();
     check_network_status('no_restart_or_reload', 'vlan');
 }
 
