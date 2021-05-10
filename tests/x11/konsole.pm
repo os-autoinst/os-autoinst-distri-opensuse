238e86d3dcd4c17ae47c09b9fad489ab7bf1a9f3
Fabian Vogt <fvogt@suse.de>
2021-05-10 10:17:17 +0200

    Add test for konsole

---
+++ tests/x11/konsole.pm
@@ -0,0 +1,24 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Package: konsole
+# Summary: Basic functionality of konsole
+# Maintainer: Fabian Vogt <fvogt@suse.de>
+
+use base "x11test";
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    my ($self) = @_;
+    $self->test_terminal('konsole');
+}
+
+1;
