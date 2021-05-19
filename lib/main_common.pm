os-autoinst/os-autoinst-distri-opensuse

commit 8b24687d57eeb835d5940b2499e68082c73139e3

pynfs: Initialize pynfs test

products/sle/main.pm
@@ -739,6 +739,14 @@ elsif (get_var("BTRFS_PROGS")) {
     loadtest "btrfs-progs/run";
     loadtest "btrfs-progs/generate_report";
 }
+elsif (get_var("PYNFS")) {
+    prepare_target;
+    if (check_var('PYNFS', 'installation')) {
+        loadtest "pynfs/install";
+        loadtest "pynfs/run";
+        loadtest "pynfs/generate_report";
+    }
+}
 elsif (get_var("VIRT_AUTOTEST")) {
     if (get_var('REPO_0_TO_INSTALL', '')) {
         #Before host installation starts, swtich to version REPO_0_TO_INSTALL if it is set

tests/pynfs/generate_report.pm
@@ -1,0 +1,47 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: Upload logs and generate report
+# Maintainer: Yong Sun <yosun@suse.com>
+package generate_report;
+
+use strict;
+use warnings;
+use base 'opensusebasetest';
+use File::Basename;
+use testapi;
+use upload_system_log;
+
+sub upload_log {
+    my $log_file = "./log.txt";
+    my $timeout  = 90;
+    my $folder   = get_required_var('PYNFS') || 'nfs4.0';
+    assert_script_run("cd ~/pynfs/$folder");
+
+    #show failures and save to log
+    script_run('../nfs4.0/showresults.py log.txt | grep -A 2 FAILURE | grep -v PASS | tee fails.txt');
+    upload_logs('fails.txt', timeout => $timeout, log_name => 'upload-failure');
+
+    #raw log
+    upload_logs($log_file, timeout => $timeout, log_name => 'upload-raw');
+
+    #analys log
+    script_run('./showresults.py log.txt > result.txt');
+    upload_logs('result.txt', timeout => $timeout, log_name => 'upload-analys');
+}
+
+sub run {
+    my $self = shift;
+    $self->select_serial_terminal;
+
+    upload_log();
+    upload_system_logs();
+}
+
+1;

tests/pynfs/install.pm
@@ -1,0 +1,57 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: Install pynfs
+# Maintainer: Yong Sun <yosun@suse.com>
+package install;
+
+use 5.018;
+use strict;
+use warnings;
+use base 'opensusebasetest';
+use utils;
+use testapi;
+
+sub install_dependencies {
+    my @deps = qw(
+      git
+      krb5-devel
+      python3-devel
+      swig
+      python3-gssapi
+      python3-ply
+      nfs-client
+      nfs-kernel-server
+    );
+    zypper_call('in ' . join(' ', @deps));
+}
+
+sub install_pynfs {
+    install_dependencies;
+    assert_script_run('git clone -q --depth 1 git://git.linux-nfs.org/projects/bfields/pynfs.git && cd ./pynfs');
+    record_info('pynfs git version', script_output('git log -1 --pretty=format:"git-%h" | tee version.txt'));
+    assert_script_run('./setup.py build && ./setup.py build_ext --inplace');
+}
+
+sub setup_nfs_server {
+    assert_script_run('mkdir -p /exportdir && echo \'/exportdir *(rw,no_root_squash,insecure)\' >/etc/exports && systemctl restart nfs-server');
+}
+
+sub run {
+    my $self = shift;
+    $self->select_serial_terminal;
+
+    # Disable PackageKit
+    quit_packagekit;
+
+    install_pynfs;
+    setup_nfs_server;
+}
+
+1;

tests/pynfs/run.pm
@@ -1,0 +1,51 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: Run tests
+# Maintainer: Yong Sun <yosun@suse.com>
+package run;
+
+use 5.018;
+use strict;
+use warnings;
+use base 'opensusebasetest';
+use File::Basename;
+use testapi;
+use utils;
+use power_action_utils 'power_action';
+
+sub server_test_all {
+    my $self   = shift;
+    my $folder = get_required_var('PYNFS') || 'nfs4.0';
+    assert_script_run("cd ./$folder");
+    script_run('./testserver.py --outfile ${logf:-log.txt} --maketree ${serv:-localhost}:${export:-/exportdir} all', 3600);
+}
+
+
+sub run {
+    my $self = shift;
+    select_console('root-console');
+    script_run('cd ~/pynfs');
+    server_test_all;
+}
+
+1;
+
+=head1 Configuration
+
+Example configuration for SLE:
+
+BOOT_HDD_IMAGE=1
+DESKTOP=textmode
+HDD_1=SLES-%VERSION%-%ARCH%-%BUILD%@%MACHINE%-minimal_with_sdk%BUILD_SDK%_installed.qcow2
+PYNFS=nfs4.0
+UEFI_PFLASH_VARS=SLES-%VERSION%-%ARCH%-%BUILD%@%MACHINE%-minimal_with_sdk%BUILD_SDK%_installed-uefi-vars.qcow2
+START_AFTER_TEST=create_hdd_minimal_base+sdk
+
+=cut

commit f899674fdeec14502be896c366617658deae21ed

Load pynfs also on o3
This requires to move loading pynfs to new function load_pynfs_tests().

Also remove check_var('PYNFS', 'installation'), which is wrong
(previously where was separate install job.

TODO: migrate to yaml schedule. That effort will have to solve
prepare_target() on SLE which does more than simple loading
boot_to_desktop.pm on opensuse.

Fixes: 8b24687d5 ("pynfs: Initialize pynfs test")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

lib/main_common.pm
@@ -77,6 +77,7 @@ our @EXPORT = qw(
   load_nfv_master_tests
   load_nfv_trafficgen_tests
   load_public_cloud_patterns_validation_tests
+  load_pynfs_tests
   load_transactional_role_tests
   load_reboot_tests
   load_rescuecd_tests
@@ -3238,6 +3239,12 @@ sub load_kernel_baremetal_tests {
     loadtest "toolchain/install";
     # some tests want to build and run a custom kernel
     loadtest "kernel/build_git_kernel" if get_var('KERNEL_GIT_TREE');
+}
+
+sub load_pynfs_tests {
+    loadtest "pynfs/install";
+    loadtest "pynfs/run";
+    loadtest "pynfs/generate_report";
 }
 
 1;

products/opensuse/main.pm
@@ -268,6 +268,10 @@ if (is_kernel_test()) {
 elsif (get_var('NFV')) {
     load_nfv_tests();
 }
+elsif (get_var("PYNFS")) {
+    loadtest "boot/boot_to_desktop";
+    load_pynfs_tests;
+}
 elsif (get_var("REGRESSION")) {
     load_common_x11;
 }

products/sle/main.pm
@@ -744,11 +744,7 @@ elsif (get_var("BTRFS_PROGS")) {
 }
 elsif (get_var("PYNFS")) {
     prepare_target;
-    if (check_var('PYNFS', 'installation')) {
-        loadtest "pynfs/install";
-        loadtest "pynfs/run";
-        loadtest "pynfs/generate_report";
-    }
+    load_pynfs_tests;
 }
 elsif (get_var("VIRT_AUTOTEST")) {
     if (get_var('REPO_0_TO_INSTALL', '')) {