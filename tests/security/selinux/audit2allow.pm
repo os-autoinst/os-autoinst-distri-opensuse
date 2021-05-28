180622f0950e7c576c37c9f6eb34f92ee3ee0c36
Michael Grifalconi <mgrifalconi@suse.com>
2021-05-28 10:30:24 +0200

    audit2allow rename logs to txt

--- tests/security/selinux/audit2allow.pm
+++ tests/security/selinux/audit2allow.pm
@@ -29,8 +29,8 @@ use registration qw(add_suseconnect_product);
 sub run {
     my $testfile        = "test_file";
     my $test_module     = "test_module";
-    my $audit_log       = "/var/log/audit/audit.log";
-    my $audit_log_short = "/var/log/audit/audit.log.short";
+    my $audit_log       = "/var/log/audit/audit.txt";
+    my $audit_log_short = "/var/log/audit/audit.short.txt";
 
     select_console "root-console";
 
