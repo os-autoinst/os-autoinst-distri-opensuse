2c78cc233817dc63dfe0a4d6226e489e81fe4636
mloviska <mloviska@suse.com>
2021-06-02 12:07:12 +0200

    Adapt zypp.conf file check for JeOS

--- tests/microos/libzypp_config.pm
+++ tests/microos/libzypp_config.pm
@@ -15,11 +15,12 @@ use base "opensusebasetest";
 use strict;
 use warnings;
 use testapi;
+use version_utils qw(is_jeos);
 
 sub run {
     assert_script_run 'egrep -x "^solver.onlyRequires ?= ?true" /etc/zypp/zypp.conf';
     assert_script_run 'egrep -x "^rpm.install.excludedocs ?= ?yes" /etc/zypp/zypp.conf';
-    assert_script_run 'egrep -x "^multiversion ?=" /etc/zypp/zypp.conf';
+    assert_script_run sprintf('egrep -x "^multiversion ?=%s" /etc/zypp/zypp.conf', is_jeos ? ' ?provides:multiversion\(kernel\)' : '');
 }
 
 sub post_fail_hook {
