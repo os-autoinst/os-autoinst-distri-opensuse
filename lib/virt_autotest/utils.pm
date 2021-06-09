12eb5d987dc577fac6f0bac2000ab1aa88cdcb8a
phoenix <felix.niederwanger@suse.com>
2021-06-09 17:07:52 +0200

    Increase waiting timeout
    With more guests being present, we need to further increase the already
    bad wait timeout for the guest installation.

--- lib/virt_autotest/utils.pm
+++ lib/virt_autotest/utils.pm
@@ -402,9 +402,12 @@ sub wait_guests_shutdown {
 
 # Start all guests and wait until they are online
 sub start_guests {
-    script_run "virsh start $_" foreach (keys %virt_autotest::common::guests);
-    # Wait for guests to show up as running
-    script_retry("virsh list | grep $_ | grep running", delay => 5, retry => 10) foreach (keys %virt_autotest::common::guests);
+    foreach my $guest (keys %virt_autotest::common::guests) {
+        if (script_run("virsh list | grep '$guest' | grep running") != 0) {
+            assert_script_run("virsh start '$guest'");
+            script_retry("virsh list | grep '$guest' | grep running", delay => 5, retry => 10);
+        }
+    }
     wait_guest_online($_) foreach (keys %virt_autotest::common::guests);
 }
 
