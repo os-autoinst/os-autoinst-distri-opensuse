3e2c72002166bbdddfd789dfcac1faa58ed26422
Joaquín Rivera <jeriveramoya@suse.com>
2021-06-29 10:26:48 +0200

    Increase timeout for libyui to 60 seconds
    For aarch64 seems that 30 seconds is not enough.

--- tests/installation/setup_libyui.pm
+++ tests/installation/setup_libyui.pm
@@ -27,7 +27,7 @@ use testapi;
 use YuiRestClient;
 
 sub run {
-    my $app  = YuiRestClient::get_app(installation => 1, timeout => 30, interval => 1);
+    my $app  = YuiRestClient::get_app(installation => 1, timeout => 60, interval => 1);
     my $port = $app->get_port();
     record_info('SERVER', "Used host for libyui: " . $app->get_host());
     record_info('PORT',   "Used port for libyui: " . $port);
