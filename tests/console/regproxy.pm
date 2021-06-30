55af4807731650c539aec99bdc5174e7106e05e9
Fabian Vogt <fvogt@suse.de>
2021-06-30 15:12:30 +0200

    console/regproxy: Start update-ca-certificates through systemd
    The copy of the file already triggers update-ca-certificates.path, which
    starts update-ca-certificates.service. Starting update-ca-certificates manually
    as well can lead to conflicts. Simply start the service manually to ensure that
    it completed.

--- tests/console/regproxy.pm
+++ tests/console/regproxy.pm
@@ -34,7 +34,8 @@ sub run {
     assert_script_run('echo "PREFIX=' . $opensuse_prefix . '" > /etc/regproxy.conf');
     assert_script_run('systemctl daemon-reload && systemctl enable --now regproxy.service');
     # Install the MITM cert
-    assert_script_run('ln -s $PWD/regproxy-cert.pem /etc/pki/trust/anchors && update-ca-certificates');
+    assert_script_run('ln -s $PWD/regproxy-cert.pem /etc/pki/trust/anchors');
+    systemctl('start ca-certificates');
     assert_script_run('popd');
 
     # Redirect to localhost
