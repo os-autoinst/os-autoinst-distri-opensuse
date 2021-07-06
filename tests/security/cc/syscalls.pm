25123e53e34807762a3b55265bb95889006a2c96
Amrysliu <xiaojing.liu@suse.com>
2021-07-06 18:42:42 +0800

    Integrate audit test 'syscalls' into openQA
    Relate ticket: https://progress.opensuse.org/issues/94684
    
    Verification run:
     https://openqa.suse.de/tests/6388923 (pass)
     https://openqa.suse.de/tests/6388752 (fail)

---
+++ tests/security/cc/syscalls.pm
@@ -0,0 +1,41 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+# Summary: Run 'syscalls' test case of 'audit-test' test suite
+# Maintainer: Liu Xiaojing <xiaojing.liu@suse.com>
+# Tags: poo#94684
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+use audit_test qw(run_testcase compare_run_log);
+
+sub run {
+    my ($self) = shift;
+
+    select_console "root-console";
+
+    if (my $pprofile = get_var('PPROFILE')) {
+        assert_script_run("export PPROFILE=$pprofile");
+    }
+    # Run test case
+    run_testcase('syscalls', (make => 1, timeout => 720));
+
+    # Compare current test results with baseline
+    my $result = compare_run_log('syscalls');
+    $self->result($result);
+}
+
+sub test_flags {
+    return {no_rollback => 1};
+}
+
+1;
