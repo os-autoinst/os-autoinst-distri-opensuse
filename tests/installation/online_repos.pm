d47fe7b5fb23ccd41bc39ddde0281348aed0d220
Guillaume Gardet <guillaume.gardet@arm.com>
2021-07-21 10:07:17 +0200

    online_repos: handle network-not-configured case - poo#87719

--- tests/installation/online_repos.pm
+++ tests/installation/online_repos.pm
@@ -57,6 +57,19 @@ sub run {
     my @needles = qw(online-repos-popup before-role-selection inst-networksettings partitioning-edit-proposal-button inst-instmode network-not-configured list-of-online-repositories);
     assert_screen(\@needles, timeout => 60);
 
+    if (match_has_tag('network-not-configured')) {
+        # On slow workers the network may be unconfigured - poo#87719
+        send_key("alt-i");                                       # Edit button
+        assert_screen('static-ip-address-set');
+        send_key("alt-y");                                       # Select Dynamic address
+        assert_screen('dynamic-ip-address-set');
+        send_key $cmd{next};                                     # Next
+        assert_screen('inst-networksettings');
+        send_key $cmd{next};                                     # Next
+        @needles = grep { !/inst-networksettings/ } @needles;    # Do not match the previous screen
+        assert_screen(\@needles, timeout => 60);                 # Check the screen again with network up and running
+    }
+
     # Do nothing if pop-up is not found
     return unless match_has_tag('online-repos-popup');
 
