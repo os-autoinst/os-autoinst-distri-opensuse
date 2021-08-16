c67e68d5002976c992713f7be60d8056607728ce
Anna Minou <anna.minou@suse.com>
2021-08-16 14:57:54 +0200

    Add missing information in test modules

--- tests/virt_autotest/cleanup_service.pm
+++ tests/virt_autotest/cleanup_service.pm
@@ -1,3 +1,12 @@
+# SUSE's openQA tests
+#
+# Copyright © 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
 # Summary: revert or cleanup the configuration files and restart services
 # Maintainer: Julie CAO <jcao@suse.com>
 package cleanup_service;
--- tests/x11/awesome_xterm.pm
+++ tests/x11/awesome_xterm.pm
@@ -1,3 +1,12 @@
+# SUSE's openQA tests
+#
+# Copyright © 2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+
 # Package: awesome
 # Summary: Test for xterm started in awesome window manager
 # Maintainer: Dominik Heidler <dheidler@suse.de>
