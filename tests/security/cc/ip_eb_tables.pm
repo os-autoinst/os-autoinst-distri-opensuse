c9b80b4c4f8365fe9be84186a9a45e0b24f6ad16
root <root@localhost.localdomain>
2021-08-20 09:44:48 +0100

    Fix for POO97076
    Minor fixes
    
    Fixes based on review
    
    perltidy fixed minir formatting
    
    Fix to allow run audit tests from different CODE_BASE
    
    Perltidy fix of chnges in audit_test.pm

--- tests/security/cc/ip_eb_tables.pm
+++ tests/security/cc/ip_eb_tables.pm
@@ -19,12 +19,35 @@ use utils;
 use audit_test qw(run_testcase compare_run_log);
 
 sub run {
-    my ($self) = shift;
+    my ($self)          = shift;
+    my $f_ifcfg_br0     = '/etc/sysconfig/network/ifcfg-br0';
+    my $f_ifcfg_eth0    = '/etc/sysconfig/network/ifcfg-eth0';
+    my $bakf_ifcfg_eth0 = '/etc/sysconfig/network/ifcfg-eth0.bak';
+    my $br0_config      = "BOOTPROTO='dhcp'\nSTARTMODE='auto'\nBRIDGE='yes'\nBRIDGE_PORTS='eth0'\nBRIDGE_STP='off'\nBRIDGE_FORWARDDELAY='15'\n";
+    my $eth0_config     = "IPADDR='0.0.0.0'\nBOOTPROTO='none'\nSTARTMODE='auto'\n";
 
     select_console 'root-console';
 
+    # Configure bridge for ip_eb_tables workaround
+    assert_script_run("cat > $f_ifcfg_br0 <<'END'\n$br0_config\nEND\n( exit \$?)");
+
+    # Creating backup for eth0 configuration
+    assert_script_run("cp $f_ifcfg_eth0 $bakf_ifcfg_eth0");
+
+    # Configure eth0 for ip_eb_tables workaround
+    assert_script_run("cat > $f_ifcfg_eth0 <<'END'\n$eth0_config\nEND\n( exit \$?)");
+
+    assert_script_run("service network restart");
+    assert_script_run("bridge link show");
+
     # Run test case
     run_testcase('ip+eb-tables', timeout => 300);
+
+    # Clean-up and restore configuration
+    assert_script_run("rm $f_ifcfg_br0");
+    assert_script_run("rm $f_ifcfg_eth0");
+    assert_script_run("cp $bakf_ifcfg_eth0 $f_ifcfg_eth0");
+    assert_script_run("service network restart");
 
     # Compare current test results with baseline
     my $result = compare_run_log('ip_eb_tables');
