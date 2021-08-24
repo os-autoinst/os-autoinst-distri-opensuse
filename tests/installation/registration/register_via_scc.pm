25da43079e1f27323a99d359b472d432632d7f7b
Joaquín Rivera <jeriveramoya@suse.com>
2021-08-24 09:34:58 +0200

    Rename test registration_reg_code to register_via_scc

--- tests/installation/registration/register_via_scc.pm
+++ tests/installation/registration/register_via_scc.pm
@@ -7,19 +7,21 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Register the system with scc in the installer with registration code.
+# Summary: Register the system via SCC in the installer with registration
+# code, optional email and enabling update repositories.
 #
 # Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
 
 use base 'y2_installbase';
 use strict;
 use warnings;
-use testapi 'get_var';
+use testapi;
 
 sub run {
-    my $reg_code = get_var('SCC_REGCODE');
-    $testapi::distri->get_registration()->register_product_with_regcode($reg_code);
-    $testapi::distri->get_registration()->enable_update_repositories('The registration server offers update repos.*');
+    $testapi::distri->get_registration()->register_via_scc({
+            email    => get_var('SCC_EMAIL'),
+            reg_code => get_var('SCC_REGCODE')});
+    $testapi::distri->get_registration()->enable_update_repositories();
 }
 
 1;
