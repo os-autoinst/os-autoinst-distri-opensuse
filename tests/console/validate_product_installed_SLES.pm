7e680e0381551406257b8a7519e3b8301597865e
Joaquín Rivera <jeriveramoya@suse.com>
2021-09-02 14:05:37 +0200

    Rename validate_product_name_installed to validate_product_installed_SLES

--- tests/console/validate_product_installed_SLES.pm
+++ tests/console/validate_product_installed_SLES.pm
@@ -7,24 +7,22 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 
-# Summary: Validate name of the installed product via /etc/os-release
+# Summary: Validate that the product installed is SLES via /etc/os-release
 # Maintainer: QA SLE YaST <qa-sle-yast@suse.de>
 
 use base "consoletest";
 use strict;
 use warnings;
 use testapi;
-use scheduler 'get_test_suite_data';
 use Config::Tiny;
 use Test::Assert ':all';
 
 sub run {
-    my $product = get_test_suite_data()->{product};
     select_console 'root-console';
 
     my $os_release_output = script_output('cat /etc/os-release');
     my $os_release_name   = Config::Tiny->read_string($os_release_output)->{_}->{NAME};
-    assert_equals("\"$product\"", $os_release_name, 'Wrong product NAME in /etc/os-release');
+    assert_equals('"SLES"', $os_release_name, 'Wrong product NAME in /etc/os-release');
 }
 
 1;
