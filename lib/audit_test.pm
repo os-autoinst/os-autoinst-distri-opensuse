7eb2853660f6f83fafd356707cb2eee9973d8820
Viktors Trubovics <viktors.trubovics@suse.com>
2021-09-03 11:27:37 +0100

    Perltidy fix of chnges in audit_test.pm

--- lib/audit_test.pm
+++ lib/audit_test.pm
@@ -35,9 +35,9 @@ our @EXPORT = qw(
   upload_audit_test_logs
 );
 
-our $testdir      = '/tmp/';
-our $code_repo    = get_var('CODE_BASE', 'https://gitlab.suse.de/security/audit-test-sle15/-/archive/master/audit-test-sle15-master.tar');
-my @lines         = split(/[\/\.]+/, $code_repo);
+our $testdir   = '/tmp/';
+our $code_repo = get_var('CODE_BASE', 'https://gitlab.suse.de/security/audit-test-sle15/-/archive/master/audit-test-sle15-master.tar');
+my @lines = split(/[\/\.]+/, $code_repo);
 our $testfile_tar = $lines[-2];
 our $mode         = get_var('MODE', 64);
 
