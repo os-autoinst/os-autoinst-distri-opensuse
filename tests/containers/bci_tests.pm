6d3a163579fefc508b3cc96c2dc299f10f0a1de0
Jose Lausuch <jlausuch@gmail.com>
2021-10-08 13:22:48 +0200

    BCI: run bci-tests in parallel
    Also, make distinction between different SLE versions to install
    certain packages

--- tests/containers/bci_tests.pm
+++ tests/containers/bci_tests.pm
@@ -36,8 +36,7 @@ our $test_envs = get_var('BCI_TEST_ENVS', 'base,init,dotnet,python,node,go,multi
 sub parse_logs {
     my $self = @_;
 
-    upload_logs('junit_build_serial.xml', failok => 1);
-    upload_logs('junit_build_parallel.xml', failok => 1);
+    upload_logs('junit_build.xml', failok => 1);
 
     # bci-tests produce separate XUnit results files for each environment.
     # We need tp merge all together into a single xml file that will
@@ -48,8 +47,7 @@ sub parse_logs {
 
     # Dump xml contents to a location where we can access later using data_url
     for my $env (split(/,/, $test_envs)) {
-        upload_logs('junit_' . $env . '_serial.xml', failok => 1);
-        my $log_file = upload_logs('junit_' . $env . '_parallel.xml', failok => 1);
+        my $log_file = upload_logs('junit_' . $env . '.xml', failok => 1);
         if ($log_file) {
             my $dom = XML::LibXML->load_xml(location => "ulogs/$log_file");
             for my $node ($dom->findnodes('//testsuite')) {
@@ -74,7 +72,7 @@ sub run {
     my $engine = get_required_var('CONTAINER_RUNTIME');
     my $bci_tests_repo = get_required_var('BCI_TESTS_REPO');
     my $bci_devel_repo = get_var('BCI_DEVEL_REPO');
-    my $bci_timeout = get_var('BCI_TIMEOUT', 900);
+    my $bci_timeout = get_var('BCI_TIMEOUT', 5400);
 
     ensure_ca_certificates_suse_installed;
 
@@ -90,33 +88,48 @@ sub run {
         die("Runtime $engine not given or not supported");
     }
 
+    # Show some info about the SLE host
+    record_info('SCCcredentials', script_output('cat /etc/zypp/credentials.d/SCCcredentials', proceed_on_failure => 1));
+    record_info('SUSEConnect -l', script_output('SUSEConnect -l', proceed_on_failure => 1));
+    record_info('SUSEConnect -l', script_output('SUSEConnect --status-text', proceed_on_failure => 1));
+
     record_info('Install', 'Install needed packages');
-    zypper_call('--quiet in git-core python3 python3-devel gcc skopeo', timeout => 600);
-    assert_script_run('pip3.6 --quiet install tox pytest', timeout => 600);
+    my @packages = ('git-core', 'python3', 'python3-devel', 'gcc');
+    if (check_var('HOST_VERSION', '12-SP5')) {
+        # pip is not installed in 12-SP5 by default in our hdds
+        push @packages, 'python36-pip';
+    } else {
+        # skopeo is not available in <=12-SP5
+        push @packages, 'skopeo';
+    }
+    foreach my $pkg (@packages) {
+        zypper_call("--quiet in $pkg", timeout => 300);
+    }
+    assert_script_run('pip3.6 --quiet install --upgrade pip', timeout => 600);
+    assert_script_run("pip3.6 --quiet install tox --ignore-installed six", timeout => 600);
 
     record_info('Clone', 'Clone BCI tests repository');
     assert_script_run("git clone -q --depth 1 $bci_tests_repo");
 
     record_info('Build', 'Build bci-tests project');
     assert_script_run('cd bci-tests');
+    assert_script_run("export TOX_PARALLEL_NO_SPINNER=1");
     assert_script_run("export CONTAINER_RUNTIME=$engine");
     assert_script_run("export BCI_DEVEL_REPO=$bci_devel_repo") if $bci_devel_repo;
-    assert_script_run('tox -e build', timeout => 900);
+    my $build_options = check_var('HOST_VERSION', '12-SP5') ? '' : '-- -n auto';
+    assert_script_run("tox -e build $build_options", timeout => 900);
 
-    # Run the tests for each environment
-    my $error_count = 0;
-    for my $env (split(/,/, $test_envs)) {
-        record_info("Test: $env");
-        my $ret = script_run("tox -e $env", timeout => $bci_timeout);
-        if ($ret != 0) {
-            $error_count += 1;
-            record_soft_failure("There was a problem running the test: $env");
-        }
-    }
+    # Run the tests in parallel
+    record_info('Tests', 'Build bci-tests project');
+    my $ret = script_run("tox -e $test_envs --parallel", timeout => $bci_timeout);
+    record_info('Result', "The test run command returned: $ret");
+
+    # script_run returns undef if it hits timeout
+    die('There was a timeout running some tests') if (!defined($ret));
 
     $self->parse_logs();
 
-    die("$error_count tests failed.") if ($error_count != 0);
+    die("Some tests failed.") if ($ret != 0);
 }
 
 sub test_flags {
