8eaaa4f53624532f48cf6d2716dc7669802a05ad
Jose Lausuch <jlausuch@gmail.com>
2021-10-13 16:46:55 +0200

    WIP: Buildah refactor

--- lib/containers/engine.pm
+++ lib/containers/engine.pm
@@ -55,7 +55,7 @@ sub _engine_script_retry {
 Creates a container.
 C<image> the name of the image we create the container from.
 C<name> is the given name of the created container.
-C<cmd> can be anything you want to run in the container, similar to C<run>. for 
+C<cmd> can be anything you want to run in the container, similar to C<run>. for
 instance: C<docker create -it tumbleweed bash>
 
 =cut
@@ -118,7 +118,7 @@ sub build {
 
 Run a container.
 C<image_name> is required and can be the image id, the name or name with tag.
-If C<daemon> is enabled then container will run in the detached mode. Otherwise will be in the 
+If C<daemon> is enabled then container will run in the detached mode. Otherwise will be in the
 interactive mode.
 Set C<name> is you want to name the container.
 if C<cmd> found then it will execute the given command into the container.
@@ -324,95 +324,6 @@ sub configure_insecure_registries {
     assert_script_run "curl " . data_url('containers/registries.conf') . " -o /etc/containers/registries.conf";
     assert_script_run "chmod 644 /etc/containers/registries.conf";
     file_content_replace("/etc/containers/registries.conf", REGISTRY => $registry);
-}
-
-
-package containers::engine::buildah;
-use Mojo::Base 'containers::engine';
-use testapi;
-use containers::utils qw(registry_url);
-use utils qw(systemctl file_content_replace);
-use Test::Assert 'assert_equals';
-has runtime => "buildah";
-
-sub cleanup_system_host {
-    my ($self, $assert) = @_;
-    $assert //= 1;
-    $self->_engine_assert_script_run("rm --all");
-    $self->_engine_assert_script_run("rmi --all --force");
-
-    if ($assert) {
-        assert_equals(0, scalar @{$self->enum_containers()}, "containers have not been removed");
-        assert_equals(0, scalar @{$self->enum_images()}, "images have not been removed");
-    }
-}
-
-sub get_images_by_repo_name {
-    my ($self) = @_;
-    my $repo_images;
-
-    $repo_images = $self->_engine_script_output("images --format '{{.Name}}'");
-    my @images = split /[\n\t ]/, $repo_images;
-    return \@images;
-}
-
-sub run_container {
-    my ($self, $image_name, %args) = @_;
-    die 'image name or id is required' unless $image_name;
-    my $remote = $args{cmd} ? "-- $args{cmd}" : '';
-    $image_name = $args{name} ? "\$(buildah from $args{name})" : '$image_name';
-    my $ret = $self->_engine_script_run(sprintf qq(run %s %s %s), $image_name, $remote, timeout => $args{timeout});
-    record_info "cmd_info", "Container executes:\noptions $image_name $remote";
-    return $ret;
-}
-
-sub pull {
-    my ($self, $image_name, %args) = @_;
-    if (my $rc = $self->_engine_script_run("images | grep '$image_name'") == 0) {
-        return;
-    }
-    # At least on publiccloud, this image pull can take long and occasinally fails due to network issues
-    return $self->_engine_script_run("pull $image_name", timeout => $args{timeout} // 300);
-}
-
-sub create_container {
-    my ($self) = shift;
-    my %args = (
-        image => '',
-        name => '',
-        cmd => '',
-        @_
-    );
-    die('Must provide an image') unless ($args{image});
-    die('Must provide an name') unless ($args{name});
-    my $container = $self->_engine_script_output("from $args{image} 2>/dev/null");
-    record_info 'Container', qq[Testing:\nContainer "$container" based on image "$args{image}"];
-}
-
-sub start_container {
-    my ($self, $image_name) = @_;
-    $self->_engine_assert_script_run("run $image_name");
-    record_info "$image_name container started", "";
-}
-
-sub build {
-    my ($self, $dockerfile_path, $container_tag, %args) = @_;
-    die 'wrong number of arguments' if @_ < 3;
-    $self->_engine_assert_script_run("bud -t $container_tag $dockerfile_path", $args{timeout} // 300);
-    record_info "$container_tag created", "";
-}
-
-sub commit {
-    my ($self, $mycontainer, $new_image_name, %args) = @_;
-    $self->_engine_assert_script_run("commit --rm $mycontainer $new_image_name", timeout => $args{timeout});
-}
-
-sub enum_containers {
-    my ($self) = shift;
-    my $containers_s = $self->_engine_script_output("ls -q");
-    record_info "Containers", $containers_s;
-    my @containers = split /[\n\t]/, $containers_s;
-    return \@containers;
 }
 
 1;
