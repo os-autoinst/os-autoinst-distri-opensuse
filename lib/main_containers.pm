1fa9460c33a645f2d0feae0b2942a649dbd6db66
Jose Lausuch <jlausuch@gmail.com>
2021-10-14 16:13:35 +0200

    Allow running podman tests for openSUSE using main_containers.pm
    By changing from `if` to `unless` we only skip the tests when the
    condition is fullfilled and not in any other case.
    
    Also, change the methods names to make them more self descriptive.

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -36,11 +36,11 @@ sub is_res_host {
     return get_var("HDD_1") =~ /(res82.qcow2|res79.qcow2)/;
 }
 
-sub load_tests_podman_image {
+sub load_image_tests_podman {
     loadtest 'containers/podman_image';
 }
 
-sub load_tests_docker_image {
+sub load_image_tests_docker {
     loadtest 'containers/docker_image';
     # container_diff package is not avaiable for <=15 in aarch64
     # Also, we don't want to run it on 3rd party hosts
@@ -49,8 +49,8 @@ sub load_tests_docker_image {
     }
 }
 
-sub load_tests_podman_common {
-    if (is_sle('>=15-SP1')) {
+sub load_host_tests_podman {
+    unless (is_sle('<15-SP1')) {
         # podman package is only available as of 15-SP1
         loadtest 'containers/podman';
         loadtest 'containers/podman_image';
@@ -60,13 +60,14 @@ sub load_tests_podman_common {
     }
 }
 
-sub load_tests_docker_common {
+sub load_host_tests_docker {
     loadtest 'containers/docker';
     loadtest 'containers/docker_image';
     loadtest 'containers/docker_3rd_party_images';
     unless (is_sle("<=15") && is_aarch64) {
         # these 2 packages are not avaiable for <=15 (aarch64 only)
-        loadtest 'containers/zypper_docker';
+        # zypper-docker is not available in factory
+        loadtest 'containers/zypper_docker' unless is_tumbleweed;
         loadtest 'containers/docker_runc';
     }
     unless (check_var('BETA', 1)) {
@@ -89,12 +90,12 @@ sub load_container_tests {
     if (is_container_image_test()) {
         # Container Image tests
         loadtest 'containers/host_configuration' unless is_res_host;
-        load_tests_podman_image() if ($runtime =~ 'podman');
-        load_tests_docker_image() if ($runtime =~ 'docker');
+        load_image_tests_podman() if ($runtime =~ 'podman');
+        load_image_tests_docker() if ($runtime =~ 'docker');
     } else {
         # Container Host tests
-        load_tests_podman_common() if ($runtime =~ 'podman');
-        load_tests_docker_common() if ($runtime =~ 'docker');
+        load_host_tests_podman() if ($runtime =~ 'podman');
+        load_host_tests_docker() if ($runtime =~ 'docker');
     }
     loadtest 'console/coredump_collect';
 }
