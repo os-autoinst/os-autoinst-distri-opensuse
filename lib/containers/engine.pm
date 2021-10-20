ae300939d509a726c817192f7e1f4f4de5f73a92
Ioannis Bonatakis <ybonatakis@suse.com>
2021-10-20 20:32:50 +0200

    Reschedule jeos yaml to run engines in separate jobs
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- lib/containers/engine.pm
+++ lib/containers/engine.pm
@@ -345,6 +345,12 @@ has runtime_instance => undef;
 
 sub get_instance {
     my ($self, $runargs) = @_;
+    if (!keys %{$runargs}) {
+        # Hack for the jobs that use yaml scheduler
+        # We have move to main_*.pm in most of the cases but there is one or two which uses yaml.
+        # This will not work if the module used in the same job twice for both docker and podman
+        check_var('CONTAINER_RUNTIME', 'docker') ? $runargs->{docker} = 1 : $runargs->{podman} = 1;
+    }
     if (defined $runargs->{docker}) {
         $self->runtime_instance(containers::engine::docker->new());
     }
