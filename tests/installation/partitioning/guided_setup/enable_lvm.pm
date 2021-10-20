4a3b60557765d9c6b47f1d7c2a61be6cea3a5353
Oleksandr Orlov <oorlov@suse.com>
2021-10-20 17:54:28 +0200

    Add test module for enabling LVM

---
+++ tests/installation/partitioning/guided_setup/enable_lvm.pm
@@ -0,0 +1,18 @@
+# Copyright SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: The test module enables LVM on Partitioning Scheme Screen of Guided Setup
+# and navigates to the next screen.
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
+
+use parent 'y2_installbase';
+use strict;
+use warnings;
+
+sub run {
+    my $partitioning_scheme = $testapi::distri->get_partitioning_scheme();
+    $partitioning_scheme->enable_lvm();
+    $partitioning_scheme->go_forward();
+}
+
+1;
