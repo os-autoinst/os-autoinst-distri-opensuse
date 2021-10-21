25ccbe94f73b473c540525cb39574360db9e1e75
Dominik Heidler <dheidler@suse.de>
2021-10-21 13:54:31 +0200

    Increase coverage of postfix tests

--- tests/console/mta.pm
+++ tests/console/mta.pm
@@ -35,8 +35,33 @@ sub run {
         systemctl 'start postfix';
     }
 
-    # test email transmission
+    record_info 'send email', 'send e-mail to a local user and check that it was delivered';
     assert_script_run 'echo "FOOBAR123" | mail root';
+    assert_script_run 'postqueue -p';
+    assert_script_run 'until postqueue -p|grep "Mail queue is empty";do sleep 1;done';
+    assert_script_run 'grep FOOBAR123 /var/mail/root';
+
+    record_info 'send bad email', 'send e-mail to a non-existent local user and check that it was bounced';
+    assert_script_run 'echo "FOOBAR456" | mail agent_smith';
+    assert_script_run 'postqueue -p';
+    assert_script_run 'until postqueue -p|grep "Mail queue is empty";do sleep 1;done';
+    assert_script_run 'grep "^Subject: Undelivered Mail Returned to Sender" /var/mail/root';
+    assert_script_run 'journalctl -n1000 | grep "unknown user: \"agent_smith\""';
+
+    record_info 'send attachement', 'Send mail with attachment';
+    assert_script_run 'echo > /var/mail/root';
+    assert_script_run 'dd bs=1024 count=10 if=/dev/urandom of=/tmp/foo';
+    assert_script_run 'date | mail -a /tmp/foo -s "mail with attachement" root';
+    assert_script_run 'postqueue -p';
+    assert_script_run 'until postqueue -p|grep "Mail queue is empty";do sleep 1;done';
+    assert_script_run 'grep "^Subject: mail with attachement" /var/mail/root';
+    assert_script_run 'echo w1 | mail';
+    assert_script_run 'diff foo /tmp/foo';
+
+    record_info 'aliases', 'send e-mail to alias';
+    assert_script_run 'newaliases';
+    assert_script_run 'echo > /var/mail/root';
+    assert_script_run 'echo "FOOBAR123" | mail nobody';
     assert_script_run 'postqueue -p';
     assert_script_run 'until postqueue -p|grep "Mail queue is empty";do sleep 1;done';
     assert_script_run 'grep FOOBAR123 /var/mail/root';
