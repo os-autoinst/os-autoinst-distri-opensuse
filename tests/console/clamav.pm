7fb4b9429b1f3076230e832007018b0d044faad9
Santiago Zarate <santiago.zarate@suse.com>
2021-10-21 11:25:52 +0200

    console/clamav: Avoid upstream database

--- tests/console/clamav.pm
+++ tests/console/clamav.pm
@@ -54,8 +54,6 @@ sub run {
     my $host = is_sle ? 'openqa.suse.de' : 'openqa.opensuse.org';
     assert_script_run("sed -i '/mirror1/i PrivateMirror $host/assets/repo/cvd' /etc/freshclam.conf");
     assert_script_run('freshclam');
-    assert_script_run("sed -i '/PrivateMirror $host/d' /etc/freshclam.conf");
-    assert_script_run('freshclam');
 
     # clamd takes a lot of memory at startup so a swap partition is needed on JeOS
     # But openSUSE aarch64 JeOS has already a swap and BTRFS does not support swapfile
