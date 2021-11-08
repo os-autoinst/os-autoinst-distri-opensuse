3f581c6cafc502739c2cd4e1afa479bb74c9f96c
Oleksandr Orlov <oorlov@suse.com>
2021-11-08 16:48:57 +0100

    Split guided_setup test module by pages
    The commit uses test modules that represent each screen of Guided Setup
    unstead of a single guided_setup test module. This makes the test suite
    schedule to be more transparent.

--- tests/installation/partitioning/guided_setup/select_disks.pm
+++ tests/installation/partitioning/guided_setup/select_disks.pm
@@ -8,10 +8,11 @@
 use parent 'y2_installbase';
 use strict;
 use warnings;
+use scheduler 'get_test_suite_data';
 
 sub run {
     my $test_data = get_test_suite_data()->{guided_partitioning};
-    $testapi::distri->get_select_hard_disks()->select_disks($test_data->{disks});
+    $testapi::distri->get_select_hard_disks()->select_disks(@{$test_data->{disks}});
     $testapi::distri->get_select_hard_disks()->go_forward();
 }
 
