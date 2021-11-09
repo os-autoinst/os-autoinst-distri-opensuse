fb94b767058f774c70a12ec486dd8957ddd8b8cb
Amrysliu <xiaojing.liu@suse.com>
2021-11-09 12:23:02 +0800

    Create qcow2 file without bridge for CC multi-machine test
    In CC system role, the bridge is setup by default in aarch64 and x86_64.
    This will cause the failure when configuring multi-machine network.
    So we could simply remove the bridge network and then do following
    tests.
    
    We also move cc_audit_test_setup to this process because some packages
    are also needed by multi-machine configuration.
    
    related: https://progress.opensuse.org/issues/102116

---
+++ tests/security/cc/remove_bridge_network.pm
@@ -0,0 +1,30 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: Remove bridge network in CC system role for multi-machine test
+# Maintainer: Liu Xiaojing <xiaojing.liu@suse.com>
+# Tags: poo#102116
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run {
+    my ($self) = shift;
+    select_console 'root-console';
+
+    my $bridge_info = script_output('bridge link');
+    if ($bridge_info =~ /\d+:\s+(\S+):.*master\s(br\d).*/) {
+        my $eth_interface = $1;
+        my $br_interface = $2;
+        assert_script_run("rm -f /etc/sysconfig/network/ifcfg-$br_interface");
+        assert_script_run("sed -i 's/none/dhcp/' /etc/sysconfig/network/ifcfg-$eth_interface");
+        assert_script_run('systemctl restart network.service');
+    }
+}
+
+1;
