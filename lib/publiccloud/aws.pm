1f765c74b1f6fe221d9f0b8cdec7cae30a43dc8a
openQA web UI <openqa@fromm>
2021-11-09 14:08:34 +0100

    Independize vault client from the publiccloud::provider
    Now vault client actions are part of the provider class
    this forces to use code of instance creation (e.g. terraform)
    in tests where are not necessary create instances (e.g. kubernetes)
    
    https://progress.opensuse.org/issues/101491

--- lib/publiccloud/aws.pm
+++ lib/publiccloud/aws.pm
@@ -24,7 +24,7 @@ sub vault_create_credentials {
     my ($self) = @_;
 
     record_info('INFO', 'Get credentials from VAULT server.');
-    my $data = $self->vault_get_secrets('/aws/creds/openqa-role');
+    my $data = $self->vault->get_secrets('/aws/creds/openqa-role');
     $self->key_id($data->{access_key});
     $self->key_secret($data->{secret_key});
     die('Failed to retrieve key') unless (defined($self->key_id) && defined($self->key_secret));
--- lib/publiccloud/provider.pm
+++ lib/publiccloud/provider.pm
@@ -11,6 +11,7 @@ package publiccloud::provider;
 use testapi qw(is_serial_terminal :DEFAULT);
 use Mojo::Base -base;
 use publiccloud::instance;
+use publiccloud::vault;
 use Carp;
 use List::Util qw(max);
 use Data::Dumper;
@@ -28,9 +29,8 @@ has username => undef;
 has prefix => 'openqa';
 has terraform_env_prepared => 0;
 has terraform_applied => 0;
-has vault_token => undef;
-has vault_lease_id => undef;
 has resource_name => sub { get_var('PUBLIC_CLOUD_RESOURCE_NAME', 'openqa-vm') };
+has vault => undef;
 
 =head1 METHODS
 
@@ -42,6 +42,7 @@ Needs provider specific credentials, e.g. key_id, key_secret, region.
 sub init {
     my ($self) = @_;
     $self->create_ssh_key();
+    $self->vault(publiccloud::vault->new());
 }
 
 =head2 conv_openqa_tf_name
@@ -513,151 +514,6 @@ sub escape_single_quote {
     return $s;
 }
 
-sub vault_retry {
-    my ($self, $code, %args) = @_;
-    my $max_tries = max(1, get_var('PUBLIC_CLOUD_VAULT_TRIES', $args{max_tries} // 3));
-    my $sleep_duration = get_var('PUBLIC_CLOUD_VAULT_TIMEOUT', $args{sleep_duration} // 60);
-    $args{name} //= Carp::shortmess("vault_retry()");
-
-    my $ret;
-    my $try_cnt = 0;
-
-    while ($try_cnt++ < $max_tries) {
-        eval {
-            $ret = $code->();
-        };
-        return $ret unless ($@);
-        sleep $sleep_duration;
-    }
-    die($args{name} . " call failed after $max_tries attempts -- " . $@);
-}
-
-=head2 __vault_login
-
-Login to vault using C<_SECRET_PUBLIC_CLOUD_REST_USER> and
-C<_SECRET_PUBLIC_CLOUD_REST_PW>. The retrieved VAULT_TOKEN is stored in this
-instance and used for further C<publiccloud::provider::vault_api()> calls.
-=cut
-sub __vault_login
-{
-    my ($self) = @_;
-    my $url = get_required_var('_SECRET_PUBLIC_CLOUD_REST_URL');
-    my $user = get_required_var('_SECRET_PUBLIC_CLOUD_REST_USER');
-    my $password = get_required_var('_SECRET_PUBLIC_CLOUD_REST_PW');
-    my $ua = Mojo::UserAgent->new;
-
-    $ua->insecure(get_var('_SECRET_PUBLIC_CLOUD_REST_SSL_INSECURE', 0));
-    $url = $url . '/v1/auth/userpass/login/' . $user;
-    my $res = $ua->post($url => json => {password => $password})->result;
-    if (!$res->is_success) {
-        my $err_msg = 'Request ' . $url . ' failed with: ' . $res->message . ' (' . $res->code . ')';
-        $err_msg .= "\n" . join("\n", @{$res->json->{errors}}) if ($res->code == 400);
-        record_info('Vault login', $err_msg, result => 'fail');
-        die("Vault login failed - $url");
-    }
-
-    return $self->vault_token($res->json('/auth/client_token'));
-}
-
-=head2 vault_login
-
-Wrapper arround C<<$self->vault_login()>> to have retry capability.
-=cut
-sub vault_login {
-    my $self = shift;
-    return $self->vault_retry(
-        sub { $self->__vault_login() },
-        name => 'vault_login()', sleep_duration => 10
-    );
-}
-
-=head2 __vault_api
-
-Invoke a vault API call. It use _SECRET_PUBLIC_CLOUD_REST_URL as base
-url.
-Depending on the method (get|post) you can pass additional data as json.
-=cut
-sub __vault_api {
-    my ($self, $path, %args) = @_;
-    my $method = $args{method} // 'get';
-    my $data = $args{data} // {};
-    my $ua = Mojo::UserAgent->new;
-    my $url = get_required_var('_SECRET_PUBLIC_CLOUD_REST_URL');
-    my $res;
-
-    $self->vault_login() unless ($self->vault_token);
-
-    $ua->insecure(get_var('_SECRET_PUBLIC_CLOUD_REST_SSL_INSECURE', 0));
-    $ua->request_timeout(40);
-    $url = $url . $path;
-    bmwqemu::diag("Request Vault REST API: $url");
-    if ($method eq 'get') {
-        $res = $ua->get($url =>
-              {'X-Vault-Token' => $self->vault_token()})->result;
-    } elsif ($method eq 'post') {
-        $res = $ua->post($url =>
-              {'X-Vault-Token' => $self->vault_token()} =>
-              json => $data)->result;
-    } else {
-        die("Unknown method $method");
-    }
-
-    if (!$res->is_success) {
-        my $err_msg = 'Request ' . $url . ' failed with: ' . $res->message . ' (' . $res->code . ')';
-        $err_msg .= "\n" . join("\n", @{$res->json->{errors}}) if ($res->code == 400);
-        record_info('Vault API', $err_msg, result => 'fail');
-        die("Vault REST api call failed - $url");
-    }
-
-    return $res->json;
-}
-
-=head2 vault_api
-
-Wrapper around C<<$self->vault_api()>> to get retry capability.
-=cut
-sub vault_api {
-    my ($self, $path, %args) = @_;
-    my $max_tries = delete($args{max_tries}) // 3;
-    my $sleep_duration = delete($args{sleep_duration}) // 60;
-
-    return $self->vault_retry(
-        sub { $self->__vault_api($path, %args) },
-        name => 'vault_api()', max_tries => $max_tries, sleep_duration => $sleep_duration
-    );
-}
-
-=head2 vault_get_secrets
-
-  my $data = $csp->vault_get_secrets('/azure/creds/openqa-role' [, max_tries => 3][, sleep_duration => 60])
-
-This is a wrapper around C<vault_api()> to retrieve secrets from aws, gce or
-azure secret engine.
-It prepend C<'/v1/' + $NAMESPACE> to the given path before sending the request.
-It stores lease_id and also adjust the token-live-time.
-=cut
-sub vault_get_secrets {
-    my ($self, $path, %args) = @_;
-    my $res = $self->vault_api('/v1/' . get_var('PUBLIC_CLOUD_VAULT_NAMESPACE', '') . $path, method => 'get', %args);
-    $self->vault_lease_id($res->{lease_id});
-    $self->vault_api('/v1/auth/token/renew-self', method => 'post', data => {increment => $res->{lease_duration} . 's'}, %args);
-    return $res->{data};
-}
-
-=head2 vault_revoke
-
-Revoke a previous retrieved credential
-=cut
-sub vault_revoke {
-    my ($self) = @_;
-
-    return unless (defined($self->vault_lease_id));
-
-    $self->vault_api('/v1/sys/leases/revoke', method => 'post', data => {lease_id => $self->vault_lease_id});
-    $self->vault_lease_id(undef);
-}
-
-
 =head2 cleanup
 
 This method is called called after each test on failure or success.
@@ -666,7 +522,7 @@ This method is called called after each test on failure or success.
 sub cleanup {
     my ($self) = @_;
     $self->terraform_destroy();
-    $self->vault_revoke();
+    $self->vault->revoke();
     assert_script_run "cd";
 }
 
