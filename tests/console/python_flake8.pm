e859085e2437b7a13bb3752dd3a0805892d24223
Avinesh Kumar <akumar@suse.de>
2021-11-25 10:38:59 +0530

    Add regression test for python-flake8 package
    Running flake8 on a sample python file and validating the
    expected formatting errors.

---
+++ tests/console/python_flake8.pm
@@ -0,0 +1,38 @@
+# SUSE's openQA tests
+#
+# Copyright (C) 2021 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+# Summary: Regression test for python-flake8
+# Maintainer: QE-Core <qe-core@suse.de>
+
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use utils 'zypper_call';
+
+sub run {
+    my $self = shift;
+    $self->select_serial_terminal;
+
+    # Make sure that python-flake8 is installed.
+    # On SLE, this requires phub extenstion
+    zypper_call 'in python3-flake8';
+
+    # Test case 1: check if flake8 is functional
+    assert_script_run('mkdir /tmp/empty_dir');
+    assert_script_run('flake8 /tmp/empty_dir');
+
+    # Test case 2: check if flake8 is working as expected
+    assert_script_run('curl -O ' . data_url("python/sample.py"));
+    validate_script_output('flake8 --exit-zero sample.py', sub { m/E265 block comment should start with '# '/ && m/F401 'os' imported but unused/ });
+    script_run('rm sample.py');
+    script_run('rmdir /tmp/empty_dir');
+}
+
+1;
