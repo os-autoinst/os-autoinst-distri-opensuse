cca53bb267526f5fd01be50e79bd077edabc99bc
Rainer Koenig <rainer.konig@suse.com>
2021-11-29 09:58:57 +0100

    openQA test modules for ZFCP disk activation
    - Activation workflow is split up in 5 steps:
      - installation/disk_activation/select_configure_zfcp_disks
      - installation/disk_activation/select_add_zfcp_device
      - installation/disk_activation/configure_zfcp_device
      - installation/disk_activation/accept_configured_zfcp_devices
      - installation/disk_activation/finish_disk_activation

---
+++ tests/installation/disk_activation/select_add_zfcp_device.pm
@@ -0,0 +1,20 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: This test module handles ZFCP disk activation
+#          through libyui-rest-client.
+
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base 'y2_installbase';
+
+sub run {
+    my $zfcp_configuration_overview = $testapi::distri->get_configured_zfcp_devices();
+    $zfcp_configuration_overview->add();
+}
+
+1;
