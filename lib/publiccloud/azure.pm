cebf5591a9c78d839170fb8425acecea12ad2081
openQA web UI <openqa@fromm>
2021-12-03 12:47:22 +0100

    Add Azure connexion client
    Extracts the code for Azure connection from provider to azure_client to be used in k8s providers
    
    https://progress.opensuse.org/issues/103419

--- lib/publiccloud/azure.pm
+++ lib/publiccloud/azure.pm
@@ -14,6 +14,7 @@ use Term::ANSIColor 2.01 'colorstrip';
 use Data::Dumper;
 use testapi qw(is_serial_terminal :DEFAULT);
 use utils qw(script_output_retry);
+use publiccloud::azure_client;
 
 has tenantid => undef;
 has subscription => undef;
@@ -22,61 +23,18 @@ has storage_account => 'openqa';
 has container => 'sle-images';
 has lease_id => undef;
 has vault => undef;
-
-=head2 decode_azure_json
-
-    my $json_obj = decode_azure_json($str);
-
-Helper function to decode json string, retrieved from C<az>, into a json
-object.
-Due to https://github.com/Azure/azure-cli/issues/9903 we need to strip all
-color codes from that string first.
-=cut
-sub decode_azure_json {
-    return decode_json(colorstrip(shift));
-}
+has provider_client => undef;
 
 sub init {
     my ($self) = @_;
     $self->SUPER::init();
-    $self->vault(publiccloud::vault->new());
-    $self->vault_create_credentials() unless ($self->key_id);
-    $self->az_login();
-    assert_script_run("az account set --subscription " . $self->subscription);
-    assert_script_run("export ARM_SUBSCRIPTION_ID=" . $self->subscription);
-    assert_script_run("export ARM_CLIENT_ID=" . $self->key_id);
-    assert_script_run("export ARM_CLIENT_SECRET=" . $self->key_secret);
-    assert_script_run('export ARM_TENANT_ID="' . $self->tenantid . '"');
-    assert_script_run('export ARM_ENVIRONMENT="public"');
-    assert_script_run('export ARM_TEST_LOCATION="' . $self->region . '"');
-}
-
-sub az_login {
-    my ($self) = @_;
-    my $login_cmd = sprintf(q(while ! az login --service-principal -u '%s' -p '%s' -t '%s'; do sleep 10; done),
-        $self->key_id, $self->key_secret, $self->tenantid);
-    assert_script_run($login_cmd, timeout => 5 * 60);
-    #Azure infra need some time to propagate given by Vault credentials
-    # Running some verification command does not prove anything because
-    # at the beginning failures can happening sporadically
-    sleep(get_var('AZURE_LOGIN_WAIT_SECONDS', 0));
-}
-
-sub vault_create_credentials {
-    my ($self) = @_;
-
-    record_info('INFO', 'Get credentials from VAULT server.');
-    my $data = $self->vault->get_secrets('/azure/creds/openqa-role');
-    $self->key_id($data->{client_id});
-    $self->key_secret($data->{client_secret});
-
-    my $res = $self->vault->api('/v1/' . get_var('PUBLIC_CLOUD_VAULT_NAMESPACE', '') . '/secret/azure/openqa-role', method => 'get');
-    $self->tenantid($res->{data}->{tenant_id});
-    $self->subscription($res->{data}->{subscription_id});
-
-    for my $i (('key_id', 'key_secret', 'tenantid', 'subscription')) {
-        die("Failed to retrieve key - missing $i") unless (defined($self->$i));
-    }
+    $self->provider_client(publiccloud::azure_client->new(
+            key_id => $self->key_id,
+            key_secret => $self->key_secret,
+            subscription => $self->subscription,
+            tenantid => $self->tenantid,
+            region => $self->region));
+    $self->provider_client->init();
 }
 
 sub resource_exist {
@@ -174,10 +132,10 @@ sub img_proof {
 
     my $credentials_file = 'azure_credentials.txt';
     my $credentials = "{" . $/
-      . '"clientId": "' . $self->key_id . '", ' . $/
-      . '"clientSecret": "' . $self->key_secret . '", ' . $/
-      . '"subscriptionId": "' . $self->subscription . '", ' . $/
-      . '"tenantId": "' . $self->tenantid . '", ' . $/
+      . '"clientId": "' . $self->provider_client->key_id . '", ' . $/
+      . '"clientSecret": "' . $self->provider_client->key_secret . '", ' . $/
+      . '"subscriptionId": "' . $self->provider_client->subscription . '", ' . $/
+      . '"tenantId": "' . $self->provider_client->tenantid . '", ' . $/
       . '"activeDirectoryEndpointUrl": "https://login.microsoftonline.com", ' . $/
       . '"resourceManagerEndpointUrl": "https://management.azure.com/", ' . $/
       . '"activeDirectoryGraphResourceId": "https://graph.windows.net/", ' . $/
