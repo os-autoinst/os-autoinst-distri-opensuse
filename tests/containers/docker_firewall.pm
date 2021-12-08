os-autoinst/os-autoinst-distri-opensuse

commit 3ac053b0397fda1434c1a85293a6fc980b583e40

Containers: Check forewall with podman

lib/containers/docker.pm
@@ -9,8 +9,9 @@
 package containers::docker;
 use Mojo::Base 'containers::engine';
 use testapi;
-use containers::utils qw(registry_url);
+use containers::utils qw(registry_url get_docker_version check_runtime_version container_ip);
 use containers::common qw(install_docker_when_needed);
+use version_utils qw(is_sle is_leap);
 use utils qw(systemctl file_content_replace);
 use version_utils qw(get_os_release);
 has runtime => 'docker';
@@ -30,6 +31,46 @@ sub configure_insecure_registries {
     assert_script_run('cat /etc/docker/daemon.json');
     systemctl('restart docker');
     record_info "setup $self->runtime", "deamon.json ready";
+}
+
+sub check_containers_firewall {
+    record_info "firewall", "Checking that firewall is enabled, properly configured and containers can reach the Internet";
+    my $container_name = 'sut_container';
+    my $docker_version = get_docker_version();
+    systemctl('is-active firewalld');
+    my $running = script_output qq(docker ps -q | wc -l);
+    validate_script_output('ip a s docker0', sub { /state DOWN/ }) if $running == 0;
+    # Docker zone is created for docker version >= 20.10 (Tumbleweed), but it
+    # is backported to docker 19 for SLE15-SP3 and for Leap 15.3
+    if (check_runtime_version($docker_version, ">=20.10") || is_sle('>=15-sp3') || is_leap(">=15.3")) {
+        assert_script_run "firewall-cmd --list-all --zone=docker";
+        validate_script_output "firewall-cmd --list-interfaces --zone=docker", sub { /docker0/ };
+        validate_script_output "firewall-cmd --get-active-zones", sub { /docker/ };
+    }
+    # Rules applied before DOCKER. Default is to listen to all tcp connections
+    # ex. output: "1           0        0 RETURN     all  --  *      *       0.0.0.0/0            0.0.0.0/0"
+    validate_script_output "iptables -L DOCKER-USER -nvx --line-numbers", sub { /1.+all.+0\.0\.0\.0\/0\s+0\.0\.0\.0\/0/ };
+
+    # Run container in the background
+    assert_script_run "docker run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine');
+    my $container_ip = container_ip($container_name, 'docker');
+
+    # Each running container should have added a new entry to the DOCKER zone.
+    # ex. output: "1           0        0 ACCEPT     tcp  --  !docker0 docker0  0.0.0.0/0            172.17.0.2           tcp dpt:1234"
+    validate_script_output "iptables -L DOCKER -nvx --line-numbers", sub { /1.+ACCEPT.+!docker0 docker0.+$container_ip\s+tcp dpt:1234/ };
+
+    # Connectivity to host check
+    my $default_route = script_output "docker run " . registry_url('alpine') . " ip route show default | awk \'/default/ {print \$3}\'";
+    assert_script_run "docker run --rm " . registry_url('alpine') . " ping -c3 " . $default_route;
+
+    # Cross-container connectivity check
+    assert_script_run "docker run --rm " . registry_url('alpine') . " ping -c3 " . $container_ip;
+
+    # Outisde connectivity check
+    assert_script_run "docker run --rm " . registry_url('alpine') . " wget google.com";
+
+    # Kill the container running on background
+    assert_script_run "docker kill $container_name ";
 }
 
 1;

lib/containers/podman.pm
@@ -9,9 +9,10 @@
 package containers::podman;
 use Mojo::Base 'containers::engine';
 use testapi;
-use containers::utils qw(registry_url);
+use containers::utils qw(registry_url container_ip);
 use containers::common qw(install_podman_when_needed);
 use utils qw(file_content_replace);
+use Utils::Systemd 'systemctl';
 use version_utils qw(get_os_release);
 has runtime => "podman";
 
@@ -28,6 +29,40 @@ sub configure_insecure_registries {
     assert_script_run "curl " . data_url('containers/registries.conf') . " -o /etc/containers/registries.conf";
     assert_script_run "chmod 644 /etc/containers/registries.conf";
     file_content_replace("/etc/containers/registries.conf", REGISTRY => $registry);
+}
+
+sub check_containers_firewall {
+    my ($runtime) = @_;
+    record_info "firewall", "Checking that firewall is enabled, properly configured and containers can reach the Internet";
+    my $container_name = 'sut_container';
+    systemctl('is-active firewalld');
+
+    # cni-podman0 interface is created when running the first container
+    assert_script_run "podman pull " . registry_url('alpine');
+    assert_script_run "podman run --rm " . registry_url('alpine');
+    validate_script_output('ip a s cni-podman0', sub { /,UP/ });
+
+    # Run container in the background
+    assert_script_run "podman pull " . registry_url('alpine');
+    assert_script_run "podman run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine');
+    my $container_ip = container_ip $container_name, 'podman';
+
+    # Cheking rules of specific running container
+    validate_script_output("iptables -vn -t nat -L PREROUTING", sub { /CNI-HOSTPORT-DNAT/ });
+    validate_script_output("iptables -vn -t nat -L POSTROUTING", sub { /CNI-HOSTPORT-MASQ/ });
+
+    # Connectivity to host check
+    my $default_route = script_output "podman run " . registry_url('alpine') . " ip route show default | awk \'/default/ {print \$3}\'";
+    assert_script_run "podman run --rm " . registry_url('alpine') . " ping -c3 " . $default_route;
+
+    # Cross-container connectivity check
+    assert_script_run "podman run --rm " . registry_url('alpine') . " ping -c3 " . $container_ip;
+
+    # Outside connectivity check
+    assert_script_run "podman run --rm " . registry_url('alpine') . " wget google.com";
+
+    # Kill the container running on background
+    assert_script_run "podman kill $container_name";
 }
 
 1;

lib/containers/utils.pm
@@ -20,7 +20,7 @@ use warnings;
 use version_utils;
 use Mojo::Util 'trim';
 
-our @EXPORT = qw(test_seccomp basic_container_tests get_vars can_build_sle_base check_docker_firewall
+our @EXPORT = qw(test_seccomp basic_container_tests get_vars can_build_sle_base
   get_docker_version check_runtime_version container_ip registry_url);
 
 sub test_seccomp {
@@ -38,29 +38,6 @@ sub test_seccomp {
     else {
         record_info('seccomp', 'Docker Engine supports seccomp');
     }
-}
-
-sub check_docker_firewall {
-    my $container_name = 'sut_container';
-    my $docker_version = get_docker_version();
-    systemctl('is-active firewalld');
-    my $running = script_output qq(docker ps -q | wc -l);
-    validate_script_output('ip a s docker0', sub { /state DOWN/ }) if $running == 0;
-    # Docker zone is created for docker version >= 20.10 (Tumbleweed), but it
-    # is backported to docker 19 for SLE15-SP3 and for Leap 15.3
-    if (check_runtime_version($docker_version, ">=20.10") || is_sle('>=15-sp3') || is_leap(">=15.3")) {
-        assert_script_run "firewall-cmd --list-all --zone=docker";
-        validate_script_output "firewall-cmd --list-interfaces --zone=docker", sub { /docker0/ };
-    }
-    # Rules applied before DOCKER. Default is to listen to all tcp connections
-    # ex. output: "1           0        0 RETURN     all  --  *      *       0.0.0.0/0            0.0.0.0/0"
-    validate_script_output "iptables -L DOCKER-USER -nvx --line-numbers", sub { /1.+all.+0\.0\.0\.0\/0\s+0\.0\.0\.0\/0/ };
-    assert_script_run "docker run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine');
-    my $container_ip = container_ip($container_name, 'docker');
-    # Each running container should have added a new entry to the DOCKER zone.
-    # ex. output: "1           0        0 ACCEPT     tcp  --  !docker0 docker0  0.0.0.0/0            172.17.0.2           tcp dpt:1234"
-    validate_script_output "iptables -L DOCKER -nvx --line-numbers", sub { /1.+ACCEPT.+!docker0 docker0.+$container_ip\s+tcp dpt:1234/ };
-    assert_script_run "docker kill $container_name ";
 }
 
 sub get_docker_version {

tests/containers/docker.pm
@@ -32,6 +32,7 @@ use version_utils qw(is_jeos);
 use containers::utils;
 use containers::container_images;
 use publiccloud::utils;
+use Utils::Systemd qw(systemctl disable_and_stop_service);
 
 my $stop_firewall = 0;    # Post-run flag to stop the firewall (failsafe)
 
@@ -46,9 +47,11 @@ sub run {
     test_seccomp();
 
     if ($self->firewall() eq 'firewalld') {
-        # on publiccloud we need to install firewalld first
-        install_and_start_firewalld() if (is_publiccloud || is_jeos);
-        check_docker_firewall();
+        zypper_call('in ' . $self->firewall()) if (is_publiccloud || is_jeos);
+        systemctl('restart ' . $self->firewall());
+        systemctl('restart docker');
+        $stop_firewall = 1;
+        $engine->check_containers_firewall();
     }
 
     # Run basic runtime tests
@@ -62,26 +65,21 @@ sub run {
 
 sub post_fail_hook {
     my $self = shift;
-    cleanup();
+    cleanup($self->firewall());
     $self->SUPER::post_fail_hook;
 }
 
 sub post_run_hook {
     my $self = shift;
-    cleanup();
+    cleanup($self->firewall());
     $self->SUPER::post_run_hook;
-}
-
-sub install_and_start_firewalld() {
-    zypper_call('install firewalld');
-    systemctl('start firewalld');
-    systemctl('restart docker');
-    $stop_firewall = 1;
 }
 
 # must ensure firewalld is stopped, if it is only enabled in this test (e.g. publiccloud test runs)
 sub cleanup() {
-    script_run('systemctl stop firewalld; systemctl restart docker') if $stop_firewall;
+    my $firewall = shift;
+    disable_and_stop_service($firewall) if $stop_firewall;
+    systemctl('restart docker');
 }
 
 1;

tests/containers/podman.pm
@@ -22,10 +22,15 @@
 use Mojo::Base 'containers::basetest';
 use testapi;
 use utils;
+use version_utils;
 use registration;
 use containers::common;
 use containers::utils;
 use containers::container_images;
+use publiccloud::utils;
+use Utils::Systemd qw(systemctl disable_and_stop_service);
+
+my $stop_firewall = 0;    # Post-run flag to stop the firewall (failsafe)
 
 sub run {
     my ($self) = @_;
@@ -33,6 +38,13 @@ sub run {
 
     my $dir = "/root/DockerTest";
     my $podman = $self->containers_factory('podman');
+
+    if ($self->firewall() eq 'firewalld') {
+        zypper_call('in ' . $self->firewall()) if (is_publiccloud || is_jeos);
+        systemctl('restart ' . $self->firewall());
+        $stop_firewall = 1;
+        $podman->check_containers_firewall();
+    }
 
     # Run basic runtime tests
     basic_container_tests(runtime => $podman->runtime);
@@ -45,10 +57,23 @@ sub run {
 
 sub post_fail_hook {
     my ($self) = @_;
+    cleanup($self->firewall());
     select_console 'log-console';
     script_run "podman version | tee /dev/$serialdev";
     script_run "podman info --debug | tee /dev/$serialdev";
     $self->SUPER::post_fail_hook;
+}
+
+sub post_run_hook {
+    my $self = shift;
+    cleanup($self->firewall());
+    $self->SUPER::post_run_hook;
+}
+
+# must ensure firewalld is stopped, if it is only enabled in this test (e.g. publiccloud test runs)
+sub cleanup() {
+    my $firewall = shift;
+    disable_and_stop_service($firewall) if $stop_firewall;
 }
 
 1;

commit ccbc9647eb364a7651962df7b13d31bf2a793798

Containers: Test podman and docker with firewall and check connectivity
This restores 3ac053b introduced in #13666 reverted by #13744 due to bug

lib/containers/docker.pm
@@ -9,7 +9,7 @@
 package containers::docker;
 use Mojo::Base 'containers::engine';
 use testapi;
-use containers::utils qw(registry_url);
+use containers::utils qw(registry_url get_docker_version check_runtime_version container_ip);
 use containers::common qw(install_docker_when_needed);
 use utils qw(systemctl file_content_replace);
 use version_utils qw(get_os_release);
@@ -30,6 +30,28 @@ sub configure_insecure_registries {
     assert_script_run('cat /etc/docker/daemon.json');
     systemctl('restart docker');
     record_info "setup $self->runtime", "deamon.json ready";
+}
+
+sub check_containers_connectivity {
+    record_info "connectivity", "Checking that containers can connect to the host, to each other and outside of the host";
+    my $container_name = 'sut_container';
+
+    # Run container in the background
+    assert_script_run "docker run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine') . " sleep 30d";
+    my $container_ip = container_ip($container_name, 'docker');
+
+    # Connectivity to host check
+    my $default_route = script_output "docker run " . registry_url('alpine') . " ip route show default | awk \'/default/ {print \$3}\'";
+    assert_script_run "docker run --rm " . registry_url('alpine') . " ping -c3 " . $default_route;
+
+    # Cross-container connectivity check
+    assert_script_run "docker run --rm " . registry_url('alpine') . " ping -c3 " . $container_ip;
+
+    # Outisde connectivity check
+    assert_script_run "docker run --rm " . registry_url('alpine') . " wget google.com";
+
+    # Kill the container running on background
+    assert_script_run "docker kill $container_name ";
 }
 
 1;

lib/containers/podman.pm
@@ -9,9 +9,10 @@
 package containers::podman;
 use Mojo::Base 'containers::engine';
 use testapi;
-use containers::utils qw(registry_url);
+use containers::utils qw(registry_url container_ip);
 use containers::common qw(install_podman_when_needed);
 use utils qw(file_content_replace);
+use Utils::Systemd 'systemctl';
 use version_utils qw(get_os_release);
 has runtime => "podman";
 
@@ -28,6 +29,29 @@ sub configure_insecure_registries {
     assert_script_run "curl " . data_url('containers/registries.conf') . " -o /etc/containers/registries.conf";
     assert_script_run "chmod 644 /etc/containers/registries.conf";
     file_content_replace("/etc/containers/registries.conf", REGISTRY => $registry);
+}
+
+sub check_containers_connectivity {
+    record_info "connectivity", "Checking that containers can connect to the host, to each other and outside of the host";
+    my $container_name = 'sut_container';
+
+    # Run container in the background
+    assert_script_run "podman pull " . registry_url('alpine');
+    assert_script_run "podman run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine') . " sleep 30d";
+    my $container_ip = container_ip $container_name, 'podman';
+
+    # Connectivity to host check
+    my $default_route = script_output "podman run " . registry_url('alpine') . " ip route show default | awk \'/default/ {print \$3}\'";
+    assert_script_run "podman run --rm " . registry_url('alpine') . " ping -c3 " . $default_route;
+
+    # Cross-container connectivity check
+    assert_script_run "podman run --rm " . registry_url('alpine') . " ping -c3 " . $container_ip;
+
+    # Outside connectivity check
+    assert_script_run "podman run --rm " . registry_url('alpine') . " wget google.com";
+
+    # Kill the container running on background
+    assert_script_run "podman kill $container_name";
 }
 
 1;

lib/containers/utils.pm
@@ -20,7 +20,7 @@ use warnings;
 use version_utils;
 use Mojo::Util 'trim';
 
-our @EXPORT = qw(test_seccomp basic_container_tests get_vars can_build_sle_base check_docker_firewall
+our @EXPORT = qw(test_seccomp basic_container_tests get_vars can_build_sle_base
   get_docker_version check_runtime_version container_ip registry_url);
 
 sub test_seccomp {
@@ -38,29 +38,6 @@ sub test_seccomp {
     else {
         record_info('seccomp', 'Docker Engine supports seccomp');
     }
-}
-
-sub check_docker_firewall {
-    my $container_name = 'sut_container';
-    my $docker_version = get_docker_version();
-    systemctl('is-active firewalld');
-    my $running = script_output qq(docker ps -q | wc -l);
-    validate_script_output('ip a s docker0', sub { /state DOWN/ }) if $running == 0;
-    # Docker zone is created for docker version >= 20.10 (Tumbleweed), but it
-    # is backported to docker 19 for SLE15-SP3 and for Leap 15.3
-    if (check_runtime_version($docker_version, ">=20.10") || is_sle('>=15-sp3') || is_leap(">=15.3")) {
-        assert_script_run "firewall-cmd --list-all --zone=docker";
-        validate_script_output "firewall-cmd --list-interfaces --zone=docker", sub { /docker0/ };
-    }
-    # Rules applied before DOCKER. Default is to listen to all tcp connections
-    # ex. output: "1           0        0 RETURN     all  --  *      *       0.0.0.0/0            0.0.0.0/0"
-    validate_script_output "iptables -L DOCKER-USER -nvx --line-numbers", sub { /1.+all.+0\.0\.0\.0\/0\s+0\.0\.0\.0\/0/ };
-    assert_script_run "docker run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine');
-    my $container_ip = container_ip($container_name, 'docker');
-    # Each running container should have added a new entry to the DOCKER zone.
-    # ex. output: "1           0        0 ACCEPT     tcp  --  !docker0 docker0  0.0.0.0/0            172.17.0.2           tcp dpt:1234"
-    validate_script_output "iptables -L DOCKER -nvx --line-numbers", sub { /1.+ACCEPT.+!docker0 docker0.+$container_ip\s+tcp dpt:1234/ };
-    assert_script_run "docker kill $container_name ";
 }
 
 sub get_docker_version {

lib/main_containers.pm
@@ -59,6 +59,7 @@ sub load_host_tests_podman {
         loadtest 'containers/podman';
         loadtest 'containers/podman_image';
         loadtest 'containers/podman_3rd_party_images';
+        loadtest 'containers/podman_firewall';
         loadtest 'containers/buildah';
         loadtest 'containers/rootless_podman';
     }
@@ -68,6 +69,7 @@ sub load_host_tests_docker {
     loadtest 'containers/docker';
     loadtest 'containers/docker_image';
     loadtest 'containers/docker_3rd_party_images';
+    loadtest 'containers/docker_firewall';
     unless (is_sle("<=15") && is_aarch64) {
         # these 2 packages are not avaiable for <=15 (aarch64 only)
         # zypper-docker is not available in factory

schedule/jeos/sle/jeos-container-engines_and_tools.yaml
@@ -30,12 +30,14 @@ schedule:
     - console/zypper_ref
     - containers/podman
     - containers/podman_image
+    - containers/podman_3rd_party_images
+    - containers/podman_firewall
+    - containers/rootless_podman
     - containers/buildah
     - containers/docker
     - containers/docker_runc
     - containers/docker_image
     - containers/docker_3rd_party_images
-    - containers/podman_3rd_party_images
+    - containers/docker_firewall
     - containers/registry
     - containers/zypper_docker
-    - containers/rootless_podman

tests/containers/docker.pm
@@ -28,12 +28,8 @@ use Mojo::Base 'containers::basetest';
 use testapi;
 use utils;
 use containers::common;
-use version_utils qw(is_jeos);
 use containers::utils;
 use containers::container_images;
-use publiccloud::utils;
-
-my $stop_firewall = 0;    # Post-run flag to stop the firewall (failsafe)
 
 sub run {
     my ($self) = @_;
@@ -45,11 +41,8 @@ sub run {
     my $engine = $self->containers_factory('docker');
     test_seccomp();
 
-    if ($self->firewall() eq 'firewalld') {
-        # on publiccloud we need to install firewalld first
-        install_and_start_firewalld() if (is_publiccloud || is_jeos);
-        check_docker_firewall();
-    }
+    # Test the connectivity of Docker containers
+    $engine->check_containers_connectivity();
 
     # Run basic runtime tests
     basic_container_tests(runtime => $engine->runtime);
@@ -58,30 +51,6 @@ sub run {
 
     # Clean container
     $engine->cleanup_system_host();
-}
-
-sub post_fail_hook {
-    my $self = shift;
-    cleanup();
-    $self->SUPER::post_fail_hook;
-}
-
-sub post_run_hook {
-    my $self = shift;
-    cleanup();
-    $self->SUPER::post_run_hook;
-}
-
-sub install_and_start_firewalld() {
-    zypper_call('install firewalld');
-    systemctl('start firewalld');
-    systemctl('restart docker');
-    $stop_firewall = 1;
-}
-
-# must ensure firewalld is stopped, if it is only enabled in this test (e.g. publiccloud test runs)
-sub cleanup() {
-    script_run('systemctl stop firewalld; systemctl restart docker') if $stop_firewall;
 }
 
 1;

tests/containers/docker_firewall.pm
@@ -1,0 +1,85 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package: docker firewalld
+# Summary: Test docker with enabled firewall
+# Maintainer: qa-c team <qa-c@suse.de>
+
+use Mojo::Base 'containers::basetest';
+use testapi;
+use utils 'script_retry';
+use version_utils qw(is_sle is_leap);
+use containers::utils qw(registry_url container_ip);
+use Utils::Systemd 'systemctl';
+
+my $stop_firewall = 0;
+
+sub run {
+    my ($self) = @_;
+    $self->select_serial_terminal;
+
+    my $engine = $self->containers_factory('docker');
+    my $container_name = 'sut_container';
+
+    # Test firewall only on systems where it's installed
+    die('Firewall is not present.') unless ($self->firewall() == 'firewalld' && script_run('which ' . $self->firewall()) == 0);
+
+    # Start firewall if it was not running before
+    if (script_run('systemctl is-active ' . $self->firewall()) != 0) {
+        systemctl('start ' . $self->firewall());
+        systemctl('restart docker');
+        $stop_firewall = 1;
+    }
+
+    # Network interface docker0 is DOWN when no containers are running
+    die 'No containers should be running!' if (script_output('docker ps -q | wc -l') != 0);
+    validate_script_output('ip a s docker0', sub { /state DOWN/ });
+
+    # Docker zone is created for docker version >= 20.10 (Tumbleweed), but it
+    # is backported to docker 19 for SLE15-SP3 and for Leap 15.3
+    unless (is_sle('<15-sp3') || is_leap("<15.3")) {
+        assert_script_run "firewall-cmd --list-all --zone=docker";
+        validate_script_output "firewall-cmd --list-interfaces --zone=docker", sub { /docker0/ };
+        validate_script_output "firewall-cmd --get-active-zones", sub { /docker/ };
+    }
+    # Rules applied before DOCKER. Default is to listen to all tcp connections
+    # ex. output: "1           0        0 RETURN     all  --  *      *       0.0.0.0/0            0.0.0.0/0"
+    validate_script_output "iptables -L DOCKER-USER -nvx --line-numbers", sub { /1.+all.+0\.0\.0\.0\/0\s+0\.0\.0\.0\/0/ };
+
+    # Run container in the background
+    assert_script_run "docker run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine') . " sleep 30d";
+    my $container_ip = container_ip($container_name, 'docker');
+
+    # Each running container should have added a new entry to the DOCKER zone.
+    # ex. output: "1           0        0 ACCEPT     tcp  --  !docker0 docker0  0.0.0.0/0            172.17.0.2           tcp dpt:1234"
+    validate_script_output "iptables -L DOCKER -nvx --line-numbers", sub { /1.+ACCEPT.+!docker0 docker0.+$container_ip\s+tcp dpt:1234/ };
+
+    # Kill the container running on background (this may take some time)
+    assert_script_run "docker kill $container_name ";
+    script_retry "docker ps -q | wc -l | grep 0", delay => 5, retry => 12;
+
+    # Test the connectivity of Docker containers
+    $engine->check_containers_connectivity();
+
+    # Stop the firewall if it was started by this test module
+    if ($stop_firewall == 1) {
+        systemctl('stop ' . $self->firewall());
+        systemctl('restart docker');
+    }
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+
+    # Stop the firewall if it was started by this test module
+    if ($stop_firewall == 1) {
+        systemctl('stop ' . $self->firewall());
+        systemctl('restart docker');
+    }
+
+    $self->SUPER::post_fail_hook;
+}
+
+1;

tests/containers/podman.pm
@@ -34,6 +34,9 @@ sub run {
     my $dir = "/root/DockerTest";
     my $podman = $self->containers_factory('podman');
 
+    # Test the connectivity of Docker containers
+    $podman->check_containers_connectivity();
+
     # Run basic runtime tests
     basic_container_tests(runtime => $podman->runtime);
     # Build an image from Dockerfile and run it

tests/containers/podman_firewall.pm
@@ -1,0 +1,67 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package: podman firewalld
+# Summary: Test podman with enabled firewall
+# Maintainer: qac team <qa-c@suse.de>
+
+use Mojo::Base 'containers::basetest';
+use testapi;
+use utils 'script_retry';
+use containers::utils qw(registry_url container_ip);
+use Utils::Systemd 'systemctl';
+
+my $stop_firewall = 0;
+
+sub run {
+    my ($self) = @_;
+    $self->select_serial_terminal;
+
+    my $podman = $self->containers_factory('podman');
+    my $container_name = 'sut_container';
+
+    # Test firewall only on systems where it's installed
+    die('Firewall is not present.') unless ($self->firewall() == 'firewalld' && script_run('which ' . $self->firewall()) == 0);
+
+    # Start firewall if it was not running before
+    if (script_run('systemctl is-active ' . $self->firewall()) != 0) {
+        systemctl('start ' . $self->firewall());
+        $stop_firewall = 1;
+    }
+
+    # cni-podman0 interface is created when running the first container
+    assert_script_run "podman pull " . registry_url('alpine');
+    assert_script_run "podman run --rm " . registry_url('alpine');
+    validate_script_output('ip a s cni-podman0', sub { /,UP/ });
+
+    # Run container in the background
+    assert_script_run "podman pull " . registry_url('alpine');
+    assert_script_run "podman run -id --rm --name $container_name -p 1234:1234 " . registry_url('alpine') . " sleep 30d";
+
+    # Cheking rules of specific running container
+    validate_script_output("iptables -vn -t nat -L PREROUTING", sub { /CNI-HOSTPORT-DNAT/ });
+    validate_script_output("iptables -vn -t nat -L POSTROUTING", sub { /CNI-HOSTPORT-MASQ/ });
+
+    # Kill the container running on background (this may take some time)
+    assert_script_run "podman kill $container_name ";
+    script_retry "podman ps -q | wc -l | grep 0", delay => 5, retry => 12;
+
+    # Test the connectivity of Podman containers
+    $podman->check_containers_connectivity();
+
+    # Stop the firewall if it was started by this test module
+    systemctl('stop ' . $self->firewall()) if $stop_firewall;
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+
+    # Stop the firewall if it was started by this test module
+    systemctl('stop ' . $self->firewall()) if $stop_firewall;
+
+    $self->SUPER::post_fail_hook;
+}
+
+1;