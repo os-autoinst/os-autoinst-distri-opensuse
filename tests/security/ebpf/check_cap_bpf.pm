83a24f0297746b6acc7d5395c025eb17437c926d
lilyeyes <llzhao@suse.com>
2021-12-16 13:41:56 +0800

    Add ebpf test into openQA
    poo#103932 - [sle][security][sle15sp4][feature][automation] SLE-SLE-22587 - QA: Disable unprivileged eBPF due to security issues

---
+++ tests/security/ebpf/check_cap_bpf.pm
@@ -0,0 +1,28 @@
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Summary: Test 'CAP_BPF' capability is available:
+# Maintainer: llzhao <llzhao@suse.com>
+# Tags: poo#103932, tc#1769831
+
+use base 'opensusebasetest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run {
+    my $f_tcpdump = '/usr/sbin/tcpdump';
+    my $capability = 'cap_bpf';
+
+    select_console 'root-console';
+
+    # Install packages
+    zypper_call('in libcap-progs tcpdump');
+
+    assert_script_run("getcap $f_tcpdump");
+    assert_script_run("setcap $capability+eip $f_tcpdump");
+    validate_script_output("getcap $f_tcpdump", sub { m/.*$capability.*/ });
+}
+
+1;
