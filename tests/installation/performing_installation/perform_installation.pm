77baee550ba0b00df9f5b4b1a34ff6d2b0619180
Joaquín Rivera <jeriveramoya@suse.com>
2021-12-20 09:05:49 +0100

    Add/modify tests for performing installation phase

--- tests/installation/performing_installation/perform_installation.pm
+++ tests/installation/performing_installation/perform_installation.pm
@@ -3,7 +3,7 @@
 # Copyright 2021 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
-# Summary: Wait for the installation to be finished.
+# Summary: Wait for the installation to be finished successfully.
 # Use TIMEOUT_SCALE so expected installation time can be adjusted
 # for slower architectures.
 # Maintainer: QE YaST <qa-sle-yast@suse.de>
@@ -12,12 +12,21 @@ use strict;
 use warnings;
 use base 'y2_installbase';
 use testapi 'get_var';
+use Test::Assert ':all';
 
 sub run {
-    # Estimated timeout of 5500 seconds (we need to fix the client in order
-    # to introduce here the right value, because of that we set the half of it)
+    my $performing_install = $testapi::distri->get_performing_installation();
+
+    $performing_install->get_performing_installation_page();
+
+    # 2750 represents a huge timeout of 5500 seconds for installation to succeed
     my $timeout = 2750 * get_var('TIMEOUT_SCALE', 1);
-    $testapi::distri->get_performing_installation()->perform($timeout);
+    $performing_install->wait_installation_popup($timeout);
+
+    assert_matches(qr/The system will reboot now/,
+        $performing_install->get_system_reboot_popup()->text(),
+        'System reboot popup is not displayed');
+
 }
 
 1;
---
+++ tests/installation/performing_installation/stop_timeout_system_reboot_now.pm
@@ -0,0 +1,17 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Stop timeout for system reboot now.
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
+
+use strict;
+use warnings;
+use base 'y2_installbase';
+
+sub run {
+    $testapi::distri->get_performing_installation()->stop_timeout_system_reboot_now();
+}
+
+1;
