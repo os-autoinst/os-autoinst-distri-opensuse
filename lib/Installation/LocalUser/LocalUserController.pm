2ae1a96660987f08de86ddba61261a1c7b5e5cca
Jonathan Rivrain <JRivrain@suse.com>
2022-01-13 18:11:59 +0100

    Add a module to import users with restapi in installer
    Replaces the module users_import by one that uses libyui-rest-api.

--- lib/Installation/LocalUser/LocalUserController.pm
+++ lib/Installation/LocalUser/LocalUserController.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright 2021 SUSE LLC
+# Copyright 2021-2022 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
 # Summary: The class introduces business actions for Local user dialog
@@ -11,6 +11,7 @@ package Installation::LocalUser::LocalUserController;
 use strict;
 use warnings;
 use Installation::LocalUser::LocalUserPage;
+use Installation::LocalUser::SelectUsersPage;
 use Installation::Popups::YesNoPopup;
 use YuiRestClient;
 
@@ -23,6 +24,7 @@ sub new {
 sub init {
     my ($self, $args) = @_;
     $self->{LocalUserPage} = Installation::LocalUser::LocalUserPage->new({app => YuiRestClient::get_app()});
+    $self->{SelectUsersPage} = Installation::LocalUser::SelectUsersPage->new({app => YuiRestClient::get_app()});
     $self->{WeakPasswordWarning} = Installation::Popups::YesNoPopup->new({app => YuiRestClient::get_app()});
     return $self;
 }
@@ -31,6 +33,12 @@ sub get_local_user_page {
     my ($self) = @_;
     die 'Local User page is not displayed' unless $self->{LocalUserPage}->is_shown();
     return $self->{LocalUserPage};
+}
+
+sub get_select_users_page {
+    my ($self) = @_;
+    die 'User selection pop-up is not displayed' unless $self->{SelectUsersPage}->is_shown();
+    return $self->{SelectUsersPage};
 }
 
 sub get_weak_password_warning {
@@ -72,6 +80,15 @@ sub is_autologin {
 sub is_use_same_password_for_admin {
     my ($self) = @_;
     $self->get_local_user_page()->has_use_same_password_for_admin_checked();
+}
+
+sub import_existing_users {
+    my ($self) = @_;
+    $self->get_local_user_page()->select_import_users();
+    $self->get_local_user_page()->choose_users_to_import();
+    $self->get_select_users_page()->select_all();
+    $self->get_select_users_page()->press_ok();
+    $self->get_local_user_page()->press_next();
 }
 
 1;
