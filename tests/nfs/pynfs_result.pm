a897ccfa90f1ecbf010ab0938852ee1aa983413a
Petr Vorel <pvorel@suse.cz>
2022-01-19 13:35:00 +0100

    pynfs: Mark SATT15 and WRT18 as known failures
    for <= 15.3
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- tests/nfs/pynfs_result.pm
+++ tests/nfs/pynfs_result.pm
@@ -9,6 +9,7 @@ use strict;
 use warnings;
 use base 'opensusebasetest';
 use testapi;
+use version_utils qw(is_sle is_leap);
 
 sub run {
     my ($self, $args) = @_;
@@ -19,20 +20,25 @@ sub run {
     }
 
     my $data = $args->{data};
-    my $msg;
+    my $test = $data->{code};
+    my $message = $data->{failure}->{message};
+    my $is_v4 = get_required_var('PYNFS') eq 'nfs4.0';
+    my $log = "$message\n\n$data->{failure}->{err}";
 
     record_info('INFO', "name: $data->{name}\nclassname: $data->{classname}\ntime: $data->{time}\n");
 
     die 'failure does not exist' unless (exists($data->{failure}));
 
-    $msg = "$data->{failure}->{message}\n\n$data->{failure}->{err}";
 
-    if ($data->{code} eq 'LOCK24' && $data->{failure}->{message} eq
+    if ($test eq 'LOCK24' && $message eq
         'OP_LOCK should return NFS4_OK, instead got NFS4ERR_BAD_SEQID') {
-        $self->record_soft_failure_result("LOCK24 failure is known, verriding to softfail: bsc#1192211\n\n" . $msg);
+        $self->record_soft_failure_result("LOCK24 failure is known, verriding to softfail: bsc#1192211\n\n" . $log);
+    } elsif (($test eq 'SATT15' || $test eq 'WRT18') && $is_v4 && (is_sle('<=15-sp3') or is_leap('<=15.3')) &&
+        $message eq 'consecutive SETATTR(mode)\'s don\'t all change change attribute') {
+        $self->record_soft_failure_result("$test failure is known, verriding to softfail: bsc#1192210\n\n" . $log);
     } else {
         $self->{result} = 'fail';
-        record_info('ERROR', $msg);
+        record_info('ERROR', $log);
     }
 }
 
