os-autoinst/os-autoinst-distri-opensuse

commit a1badaffb8c1c571015e9d055abfac39e4c22d4c

utils::zypper_ar(): Fix repo priority on SLE-11
`zypper ar` on SLE-11 has no -p parameter. Priority has to be set using
`zypper mr` command after the repo has been added. Priority 0 (reset
to default) is also intentionally ignored because SLE-11 doesn't support it.

lib/utils.pm
@@ -579,14 +579,16 @@ sub zypper_ar {
     my $params       = $args{params}       // '';
     my $no_gpg_check = $args{no_gpg_check} // '';
 
-    $priority     = defined($priority) ? "-p $priority"  : "";
-    $no_gpg_check = $no_gpg_check      ? "--no-gpgcheck" : "";
-    my $cmd_ar  = "--gpg-auto-import-keys ar -f $priority $no_gpg_check $params $url";
+    $no_gpg_check = $no_gpg_check ? "--no-gpgcheck" : "";
+    my $prioarg = defined($priority) && !is_sle('<12') ? "-p $priority" : "";
+    my $cmd_ar  = "--gpg-auto-import-keys ar -f $prioarg $no_gpg_check $params $url";
+    my $cmd_mr  = "mr -p $priority $url";
     my $cmd_ref = "--gpg-auto-import-keys ref";
 
     # repo file
     if (!$name) {
         zypper_call($cmd_ar);
+        zypper_call($cmd_mr) if $priority && is_sle('<12');
         return zypper_call($cmd_ref);
     }
 
@@ -594,6 +596,7 @@ sub zypper_ar {
     my $out = script_output("LC_ALL=C zypper lr $name 2>&1", proceed_on_failure => 1);
     if ($out =~ /Repository.*$name.*not found/i) {
         zypper_call("$cmd_ar $name");
+        zypper_call($cmd_mr) if $priority && is_sle('<12');
         return zypper_call("$cmd_ref --repo $name");
     }
 }

commit 41507993297f8f6ec99ca0d486eda7c613d25126

Fixes: a1badaffb ("utils::zypper_ar(): Fix repo priority on SLE-11")
After above commit the $priority keep be used but then it hasnt proper value
as the correct variable is set to $prioarg.

Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

lib/utils.pm
@@ -639,13 +639,13 @@ sub zypper_ar {
     $no_gpg_check = $no_gpg_check ? "--no-gpgcheck" : "";
     my $prioarg = defined($priority) && !is_sle('<=12') ? "-p $priority" : "";
     my $cmd_ar = "--gpg-auto-import-keys ar -f $prioarg $no_gpg_check $params $url";
-    my $cmd_mr = "mr -p $priority $url";
+    my $cmd_mr = "mr $prioarg $url";
     my $cmd_ref = "--gpg-auto-import-keys ref";
 
     # repo file
     if (!$name) {
         zypper_call($cmd_ar);
-        zypper_call($cmd_mr) if $priority && is_sle('<12');
+        zypper_call($cmd_mr) if defined($priority) && is_sle('<12');
         return zypper_call($cmd_ref);
     }
 