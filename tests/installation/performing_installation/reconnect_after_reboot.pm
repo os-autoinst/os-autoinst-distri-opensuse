225408bfb906fc8e5f1cc8115c5744b32a2c8537
Oleksandr Orlov <oorlov@suse.com>
2022-01-20 16:01:14 +0100

    Add test modules for actions on reboot after installation
    The commit adds test module to press "Ok" after system installation.
    Also the commit adds test modules that are needed to process reboot
    after the "Ok" button is pressed on the systems where it is needed (e.g.
    xen, svirt-s390x).

---
+++ tests/installation/performing_installation/reconnect_after_reboot.pm
@@ -0,0 +1,22 @@
+# SUSE's openQA tests
+#
+# Copyright SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: VNC connection to SUT (the 'sut' console) is terminated via svirt backend on XEN and s390x
+# and it is required to re-connect *after* the restart, otherwise the job end up with stalled
+# VNC connection.
+#
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use base 'y2_installbase';
+use strict;
+use warnings;
+use power_action_utils qw(prepare_system_shutdown assert_shutdown_and_restore_system);
+
+sub run {
+    prepare_system_shutdown;
+    assert_shutdown_and_restore_system('reboot', 180);
+}
+
+1;
