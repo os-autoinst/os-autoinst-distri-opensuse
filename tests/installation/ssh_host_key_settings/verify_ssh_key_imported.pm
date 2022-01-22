90dbc019ec29593597ac83cbd60392f61ab5eb3a
Rainer Koenig <rainer.konig@suse.com>
2022-01-22 17:48:32 +0100

    New tests for import_ssh_host_keys handling

---
+++ tests/installation/ssh_host_key_settings/verify_ssh_key_imported.pm
@@ -0,0 +1,31 @@
+# SUSE's openQA tests
+#
+# Copyright 2022 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: verify that SSH host key got imported.
+#
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
+
+use base 'y2_installbase';
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    x11_start_program('xterm -geometry 150x45+5+5', target_match => 'xterm');
+    become_root;
+    script_run 'cd /etc/ssh';
+    enter_cmd "cat /etc/ssh/ssh_host_key | tee /dev/$serialdev";
+    wait_serial "SSHHOSTKEYFILE", 5 || die "/etc/ssh/ssh_host_key content doesn't match";
+    enter_cmd "cat /etc/ssh/ssh_host_key.pub | tee /dev/$serialdev";
+    wait_serial "SSHHOSTPUBKEYFILE", 5 || die "/etc/ssh/ssh_host_key.pub content doesn't match";
+    script_run "md5sum * | tee /dev/$serialdev";
+    enter_cmd "killall xterm";
+}
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+1;
