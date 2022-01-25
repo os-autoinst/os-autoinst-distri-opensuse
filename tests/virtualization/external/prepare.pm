b694dab7bcdc9745dfb3666ea2a8122c0c6daa24
tonyyuan <tyuan@suse.com>
2022-01-25 15:38:34 +0800

    Migrate vmware test to esxi7 and resolve softfails

--- tests/virtualization/external/prepare.pm
+++ tests/virtualization/external/prepare.pm
@@ -15,19 +15,24 @@ use version_utils;
 
 sub run {
     my ($self) = @_;
-    $self->select_serial_terminal;
-
+    select_console("root-console");
     script_run("SUSEConnect -r " . get_var('SCC_REGCODE'), timeout => 420);
-
     assert_script_run "rm /etc/zypp/repos.d/SUSE_Maintenance* || true";
     assert_script_run "rm /etc/zypp/repos.d/TEST* || true";
     zypper_call '-t in nmap iputils bind-utils', exitcode => [0, 102, 103, 106];
 
     # Fill the current pairs of hostname & address into /etc/hosts file
-    assert_script_run "echo \"\$(dig +short $virt_autotest::common::guests{$_}->{ip}) $_ # virtualization\" >> /etc/hosts" foreach (keys %virt_autotest::common::guests);
+    if (get_var("REGRESSION", '') =~ /vmware/) {
+        foreach my $guest (keys %virt_autotest::common::guests) {
+            my $ip = script_output(qq(ssh -o StrictHostKeyChecking=no root\@esxi7.qa.suse.cz "vim-cmd vmsvc/get.guest \\`vim-cmd vmsvc/getallvms | grep -w $guest|cut -d ' ' -f1\\`|grep -A 1 hostName|grep ipAddress|cut -d '\\"' -f2"));
+            record_info("$ip");
+            assert_script_run(qq(echo "$ip $guest" >> /etc/hosts));
+        }
+    } else {
+        assert_script_run "echo \"\$(dig +short $virt_autotest::common::guests{$_}->{ip}) $_ # virtualization\" >> /etc/hosts" foreach (keys %virt_autotest::common::guests);
+    }
     assert_script_run "cat /etc/hosts";
 }
-
 sub test_flags {
     return {fatal => 1, milestone => 1};
 }
