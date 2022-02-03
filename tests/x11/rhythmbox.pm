2bcab806071b4074952a067e6391a73f38f8604c
Anna Minou <anna.minou@suse.com>
2022-02-03 22:52:57 +0100

    Soft-fail rhythmbox

--- tests/x11/rhythmbox.pm
+++ tests/x11/rhythmbox.pm
@@ -12,8 +12,16 @@ use base "x11test";
 use strict;
 use warnings;
 use testapi;
+use utils "zypper_call";
+use x11utils 'start_root_shell_in_xterm';
 
 sub run {
+    if (!script_run "rpm -q libtdb1") {
+        record_soft_failure("Missing dependency for rhythmbox - bsc#1195510");
+        start_root_shell_in_xterm();
+        zypper_call "in libtdb1";
+        enter_cmd "killall xterm";
+    }
     x11_start_program('rhythmbox');
     send_key "alt-f4";
 }
