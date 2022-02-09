16b8eedd8edf20e68ca0f402aba9299ef7bf4c6e
Fabian Vogt <fvogt@suse.de>
2022-02-09 11:00:22 +0100

    installation/enable_selinux: Adapt to YaST UI changes
    Tumbleweed and presumably 15-SP4/15.4 have a separate combo box for selecting
    between SELinux and AppApparmor now which needs to be interacted with first.
    The module is only used on SLE Micro and TW as of now, so add that condition
    only for now.

--- tests/installation/enable_selinux.pm
+++ tests/installation/enable_selinux.pm
@@ -10,6 +10,7 @@ use strict;
 use warnings;
 use base 'y2_installbase';
 use testapi;
+use version_utils 'is_microos';
 
 sub run {
     my $textmode = check_var('VIDEOMODE', 'text');
@@ -28,9 +29,21 @@ sub run {
         send_key 'ret';
     }
 
-    send_key 'alt-m';
-    send_key_until_needlematch 'security-selinux-enforcing', 'down';
-    send_key 'ret' if $textmode;
+    if (is_microos('<5.3')) {
+        # Combobox for SELinux specifically
+        send_key 'alt-m';
+        send_key_until_needlematch 'security-selinux-enforcing', 'down';
+        send_key 'ret' if $textmode;
+    } else {
+        # Select SELinux first
+        send_key 'alt-s';
+        send_key_until_needlematch 'security-module-selinux', 'up';
+        send_key 'ret' if $textmode;
+        # Switch it into enforcing mode
+        send_key 'alt-u';
+        send_key_until_needlematch 'security-selinux-enforcing', 'down';
+        send_key 'ret' if $textmode;
+    }
 
     send_key $cmd{ok};
 
