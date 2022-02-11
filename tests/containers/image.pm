a8334d88f4bf767e91dffb3b668524a992301e3f
Ivan Lausuch <ilausuch@suse.com>
2022-02-11 14:15:59 +0100

    Merge podman_image and docker_image test
    Merge both tests in a single test
    
    https://progress.opensuse.org/issues/101210

--- tests/containers/image.pm
+++ tests/containers/image.pm
@@ -3,17 +3,10 @@
 # Copyright 2020-2021 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
-# Package: docker
-# Summary: Test installation and running of the docker image from the registry for this snapshot.
+# Package: podman
+# Summary: Test installation and running of the docker image from the registry for this snapshot
 # This module is unified to run independented the host os.
-# - if on SLE, enable internal registry
-# - load image
-# - run container
-# - run some zypper commands with zypper-decker if is sle/opensuse
-# - try to run a single cat command if not sle/opensuse
-# - commit the image
-# - remove the container, run it again and verify that the new image works
-# Maintainer: Pavel Dostál <pdostal@suse.cz>, qa-c team <qa-c@suse.de>
+# Maintainer: Fabian Vogt <fvogt@suse.com>, qa-c team <qa-c@suse.de>
 
 use Mojo::Base 'containers::basetest';
 use testapi;
@@ -23,12 +16,13 @@ use containers::container_images;
 use containers::urls 'get_suse_container_urls';
 
 sub run {
-    my $self = shift;
+    my ($self, $args) = @_;
     $self->select_serial_terminal();
 
-    my $engine = $self->containers_factory('docker');
+    my $runtime = $args->{runtime};
+    my $engine = $self->containers_factory($runtime);
 
-    scc_apply_docker_image_credentials() if (get_var('SCC_DOCKER_IMAGE'));
+    scc_apply_docker_image_credentials() if (get_var('SCC_DOCKER_IMAGE') && $runtime eq 'docker');
 
     # We may test either one specific image VERSION or comma-separated CONTAINER_IMAGE_VERSIONS
     my $versions = get_var('CONTAINER_IMAGE_VERSIONS', get_required_var('VERSION'));
@@ -43,7 +37,8 @@ sub run {
             test_opensuse_based_image(image => $iname, runtime => $engine, version => $version, beta => $beta);
         }
     }
-    scc_restore_docker_image_credentials();
+    scc_restore_docker_image_credentials() if ($runtime eq 'docker');
+
     $engine->cleanup_system_host();
 }
 
