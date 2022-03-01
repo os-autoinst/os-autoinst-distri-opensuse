9c7673f24b3450766f0ba1589a8cae6352c5fc52
Anton Smorodskyi <23258171+asmorodskyi@users.noreply.github.com>
2022-03-01 10:38:55 +0100

    Revert "Improve softreboot to work on TUNNELED instances"

--- tests/publiccloud/ssh_interactive_start.pm
+++ tests/publiccloud/ssh_interactive_start.pm
@@ -17,6 +17,23 @@ use publiccloud::utils "select_host_console";
 sub run {
     my ($self, $args) = @_;
 
+    # This ensure that we have used the setup console, even if no module was run before.
+    select_host_console();
+    my $setup_console = current_console();
+
+    # Establish the tunnel (it will stay active in foreground and occupy this console!)
+    select_console('tunnel-console');
+    ssh_interactive_tunnel($args->{my_instance});
+
+    # Enable ssh connection on setup console, this is done normally with the
+    # first activation hook in susedistribution:activate_console()
+    if ($setup_console !~ /tunnel/) {
+        select_console($setup_console);
+        # The verbose output is visible only at the tunnel-console -
+        #   it doesn't interfere with tests as it isn't piped to /dev/sshserial
+        script_run('ssh -E /var/tmp/ssh_sut.log -vt sut', timeout => 0);
+    }
+
     die("expect ssh serial") unless (get_var('SERIALDEV') =~ /ssh/);
 
     # Verify most important consoles
