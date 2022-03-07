58459774358cf6209495898138eccb93788cf92e
Jozef Pupava <jpupava@suse.com>
2022-03-07 20:56:15 +0100

    update_install: Install packages from each patch separately
    Each patch and involved packages, in the single patch, are installed and
    patched. This will result in less conflicts between packages.
    Added record info for overview what is happening.
    Upload also logs per patch, at the end merge same kind of log into one
    testreport template does expect prepare.log or zypper.log
    
    Most of the code is just moved into loop which will do the whole preinstall,
    patch and verify process. New hashes and arrays are created within the loop
    containing only packages/binaries of the patch.
    
    Update repos in the loop have to be disabled and enabled, added in following
    dbc6d09a8

--- tests/qam-updinstall/update_install.pm
+++ tests/qam-updinstall/update_install.pm
@@ -7,7 +7,8 @@
 # Summary: QAM incident install test in openQA
 #    1) boots prepared image / clean install
 #    2) update it to last released updates
-#    3) install packages mentioned in patch
+#    3) install packages mentioned in each patch at once
+#       update can contain multiple patches which could together conflict
 #    5) try install update and store install_logs
 #    6) try reboot
 #    7) all done
@@ -98,14 +99,15 @@ sub run {
     my ($self) = @_;
     my $incident_id = get_required_var('INCIDENT_ID');
     my $repos = get_required_var('INCIDENT_REPO');
+    # Sort binaries into:
+    my %installable;    #Binaries already released that can already be installed.
+    my @new_binaries;    #Binaries introduced by the update that will be installed after the repos are added.
+    my %bins;
 
     $self->select_serial_terminal;
 
     zypper_call(q{mr -d $(zypper lr | awk -F '|' '/NVIDIA/ {print $2}')}, exitcode => [0, 3]);
     zypper_call("ar -f http://dist.suse.de/ibs/SUSE/Updates/SLE-Live-Patching/12-SP3/" . get_var('ARCH') . "/update/ sle-module-live-patching:12-SP3::update") if is_sle('=12-SP3');
-
-    # Get packages affected by the incident.
-    my @packages = get_incident_packages($incident_id);
 
     # Extract module name from repo url.
     my @modules = split(/,/, $repos);
@@ -116,8 +118,18 @@ sub run {
         die 'Modules regex failed. Modules could not be extracted from repos variable.';
     }
 
+    # Patch the SUT to a released state;
+    fully_patch_system;
+
+    set_var('MAINT_TEST_REPO', $repos);
+    my $repos_count = add_test_repositories;
+
+    my $patches = get_patch($incident_id, $repos);
+
+    # Get packages affected by the incident.
+    my @packages = get_incident_packages($incident_id);
+
     # Get binaries that are in each package across the modules that are in the repos.
-    my %bins;
     foreach (@packages) {
         %bins = (%bins, get_packagebins_in_modules({package_name => $_, modules => \@modules}));
     }
@@ -127,117 +139,134 @@ sub run {
     my @l3 = grep { ($bins{$_}->{supportstatus} eq 'l3') } keys %bins;
     my @unsupported = grep { ($bins{$_}->{supportstatus} eq 'unsupported') } keys %bins;
 
-    # Patch the SUT to a released state;
-    fully_patch_system;
+    for my $patch (split(/\s+/, $patches)) {
+        my %patch_bins = %bins;
+        my (@patch_l2, @patch_l3, @patch_unsupported);
 
-    # Sort binaries into:
-    my %installable;    #Binaries already released that can already be installed.
-    my @new_binaries;    #Binaries introduced by the update that will be installed after the repos are added.
+        # Check if the patch was correctly configured.
+        # Get info about the patch included in the update.
+        my @patchinfo = split '\n', script_output("zypper -n info -t patch $patch", 200);
 
-    foreach my $b (@l2, @l3) {
-        if (zypper_call("se -t package -x $b", exitcode => [0, 104]) eq '104') {
-            push(@new_binaries, $b);
-        } else {
-            $installable{$b} = 1;
+        # Find the lines where the Conflict sections begins.
+        foreach (0 .. $#patchinfo) {
+            print "$_: $patchinfo[$_]\n";
         }
-    }
+        my @conflict_indexes = grep { $patchinfo[$_] =~ /^Conflicts\D*(\d+)/ } 0 .. $#patchinfo;
+        print "conflict_indexes @conflict_indexes\n";
 
-    for my $package (sort keys %installable) {
-        # check if we already skipped it
-        next unless defined $installable{$package};
-        # Remove binaries conflicting with the ones that are being tested.
-        my $conflict = has_conflict($package);
-        next unless $conflict;
-        if ($installable{$conflict}) {
-            record_info "CONFLICT!", "$package conflicts with $conflict. Skipping $conflict.";
-            delete $installable{$conflict};
-        } else {
-            record_info "CONFLICT!", "$package conflicts with $conflict. Removing $conflict.";
-            zypper_call("rm $conflict", exitcode => [0, 104]);
-            save_screenshot;
+        # Find the ranges where there are conflict sections.
+        my @ranges = map { $patchinfo[$_] =~ /Conflicts\D*(?<num>\d+)/; ($_ + 1, $_ + $+{num}) } @conflict_indexes;
+        print "ranges @ranges\n";
+
+        # Make a list of the conflicting binaries in this patch.
+        my @conflict_names = uniq pairmap {
+            map { $_ =~ /(^\s+(?<with_ext>\S*)(\.\S* <))|^\s+(?<no_ext>\S*)/; $+{with_ext} // $+{no_ext} } @patchinfo[$a .. $b] } @ranges;
+        print "Conflict names: @conflict_names\n";
+
+        # Get the l3 released binaries. Only installed binaries can conflict.
+        my @installable_l3 = grep { $bins{$_}->{supportstatus} eq 'l3' } keys %installable;
+
+        # If not all l3 released binaries are in the conflict binaries, fail.
+        print "\nInstallable L3 @installable_l3\n";
+        for my $package (@installable_l3) {
+            my $hit = first { $package eq $_ } @conflict_names;
+            unless ($hit) {
+                record_info "Error", "$package is l3 but does not exist in the patch. The update may have been misconfigured";
+            }
         }
-    }
 
-    # Install released version of installable binaries.
-    if (scalar(keys %installable)) {
-        zypper_call("in -l " . join(' ', keys %installable), exitcode => [0, 102, 103], log => 'prepare.log', timeout => 1500);
-    }
+        # separate binaries from this one patch based on patch info
+        for my $b (@l2) { push(@patch_l2, $b) if grep($b eq $_, @conflict_names); }
+        for my $b (@l3) { push(@patch_l3, $b) if grep($b eq $_, @conflict_names); }
+        for my $b (@unsupported) { push(@patch_unsupported, $b) if grep($b eq $_, @conflict_names); }
+        %patch_bins = map { $_ => ${bins}{$_} } (@patch_l2, @patch_l3);
 
-    # Store the version of the installed binaries before the update.
-    foreach (keys %bins) {
-        $bins{$_}->{old} = get_installed_bin_version($_);
-    }
-
-    set_var('MAINT_TEST_REPO', $repos);
-    add_test_repositories;
-
-    my $patches = get_patch($incident_id, $repos);
-
-    # Check if the patch was correctly configured.
-    # Get info about the patches included in the update.
-    my @patchinfo = split '\n', script_output("zypper -n info -t patch $patches", 200);
-    # Find the lines where the Conflict sections begins.
-    foreach (0 .. $#patchinfo) {
-        print "$_: $patchinfo[$_]\n";
-    }
-    my @conflict_indexes = grep { $patchinfo[$_] =~ /^Conflicts\D*(\d+)/ } 0 .. $#patchinfo;
-    print "conflict_indexes @conflict_indexes\n";
-    # Find the ranges where there are conflict sections.
-    my @ranges = map { $patchinfo[$_] =~ /Conflicts\D*(?<num>\d+)/; ($_ + 1, $_ + $+{num}) } @conflict_indexes;
-    print "ranges @ranges\n";
-    # Make a list of the conflicting binaries.
-    my @conflict_names = uniq pairmap {
-        map { $_ =~ /(^\s+(?<with_ext>\S*)(\.\S* <))|^\s+(?<no_ext>\S*)/; $+{with_ext} // $+{no_ext} } @patchinfo[$a .. $b] } @ranges;
-    print "Conflict names: @conflict_names\n";
-    # Get the l3 released binaries. Only installed binaries can conflict.
-    my @installable_l3 = grep { $bins{$_}->{supportstatus} eq 'l3' } keys %installable;
-    # If not all l3 released binaries are in the conflict binaries, fail.
-    print "\nInstallable L3 @installable_l3\n";
-    for my $package (@installable_l3) {
-        my $hit = first { $package eq $_ } @conflict_names;
-        unless ($hit) {
-            record_info "Error", "$package is l3 but does not exist in the patch. The update may have been misconfigured";
+        foreach my $b (@patch_l2, @patch_l3) {
+            if (zypper_call("se -t package -x $b", exitcode => [0, 104]) eq '104') {
+                push(@new_binaries, $b);
+            }
+            else {
+                $installable{$b} = 1;
+            }
         }
-    }
 
-    # Patch binaries already installed.
-    zypper_call("in -l -t patch ${patches}", exitcode => [0, 102, 103], log => 'zypper.log', timeout => 1500);
+        for my $package (sort keys %installable) {
 
-    # Install binaries newly added by the incident.
-    if (scalar @new_binaries) {
-        zypper_call("in -l @new_binaries", exitcode => [0, 102, 103], log => 'new.log', timeout => 1500);
-    }
+            # check if we already skipped it
+            next unless defined $installable{$package};
 
-
-    # After the patches have been applied and the new binaries have been
-    # installed, check the version again and based on that determine if the
-    # update was succesfull.
-    foreach (keys %bins) {
-        $bins{$_}->{new} = get_installed_bin_version($_);
-    }
-    my $l3_results = "L3 binaries must always be updated.\n";
-    foreach (@l3) {
-        if ($bins{$_}->{old} eq $bins{$_}->{new} or not $bins{$_}->{new}) {
-            $bins{$_}->{update_status} = 0;
-        } else {
-            $bins{$_}->{update_status} = 1;
+            # Remove binaries conflicting with the ones that are being tested.
+            my $conflict = has_conflict($package);
+            next unless $conflict;
+            if ($installable{$conflict}) {
+                record_info "CONFLICT!", "$package conflicts with $conflict. Skipping $conflict.";
+                delete $installable{$conflict};
+            }
+            else {
+                record_info "CONFLICT!", "$package conflicts with $conflict. Removing $conflict.";
+                zypper_call("rm $conflict", exitcode => [0, 104]);
+            }
         }
+
+        # Install released version of installable binaries.
+        if (scalar(keys %installable)) {
+            record_info 'Preinstall', 'Install affected packages before update repo is enabled';
+            zypper_call("in -l " . join(' ', keys %installable), exitcode => [0, 102, 103], log => "prepare_$patch.log", timeout => 1500);
+        }
+
+        # Store the version of the installed binaries before the update.
+        foreach (keys %patch_bins) {
+            $patch_bins{$_}->{old} = get_installed_bin_version($_);
+        }
+
+        # Patch binaries already installed.
+        record_info 'Install patch', "Install patch $patch";
+        zypper_call("in -l -t patch $patch", exitcode => [0, 102, 103], log => "zypper_$patch.log", timeout => 1500);
+
+        # Install binaries newly added by the incident.
+        if (scalar @new_binaries) {
+            record_info 'Install new packages', "New packages: @new_binaries";
+            zypper_call("in -l @new_binaries", exitcode => [0, 102, 103], log => "new_$patch.log", timeout => 1500);
+        }
+
+        # After the patches have been applied and the new binaries have been
+        # installed, check the version again and based on that determine if the
+        # update was succesfull.
+        foreach (keys %patch_bins) {
+            $patch_bins{$_}->{new} = get_installed_bin_version($_);
+        }
+        my $l3_results = "L3 binaries must always be updated.\n";
+        foreach (@l3) {
+            if ($patch_bins{$_}->{old} eq $patch_bins{$_}->{new} or not $patch_bins{$_}->{new}) {
+                $patch_bins{$_}->{update_status} = 0;
+            }
+            else {
+                $patch_bins{$_}->{update_status} = 1;
+            }
+        }
+        $l3_results = get_results({bins => \%patch_bins, package_list => \@patch_l3});
+        record_info('L3', $l3_results) if scalar(@patch_l3);
+
+        my $l2_results = "L2 binaries need not always be updated but they must be installed.\n";
+        $patch_bins{$_}->{update_status} = !!$patch_bins{$_}->{new} foreach (@patch_l2);
+        $l2_results = get_results({bins => \%patch_bins, package_list => \@patch_l2});
+        record_info('L2', $l2_results) if scalar(@patch_l2);
+
+        my $unsupported_results = "Unsupported binaries are ignored.\n";
+        $patch_bins{$_}->{update_status} = 1 foreach (@patch_unsupported);
+        $unsupported_results = get_results({bins => \%patch_bins, package_list => \@patch_unsupported});
+        record_info('UNSUPPORTED', $unsupported_results) if scalar(@patch_unsupported);
+
+        record_soft_failure 'poo#67357 Some L3 binaries were not updated.' if scalar(grep { !$patch_bins{$_}->{update_status} } @patch_l3);
+        record_soft_failure 'poo#67357 Some L2 binaries were not installed.' if scalar(grep { !$patch_bins{$_}->{update_status} } @patch_l2);
     }
-    $l3_results .= get_results({bins => \%bins, package_list => \@l3});
-    record_info('L3', $l3_results) if scalar(@l3);
 
-    my $l2_results = "L2 binaries need not always be updated but they must be installed.\n";
-    $bins{$_}->{update_status} = !!$bins{$_}->{new} foreach (@l2);
-    $l2_results .= get_results({bins => \%bins, package_list => \@l2});
-    record_info('L2', $l2_results) if scalar(@l2);
-
-    my $unsupported_results = "Unsupported binaries are ignored.\n";
-    $bins{$_}->{update_status} = 1 foreach (@unsupported);
-    $unsupported_results .= get_results({bins => \%bins, package_list => \@unsupported});
-    record_info('UNSUPPORTED', $unsupported_results) if scalar(@unsupported);
-
-    record_soft_failure 'poo#67357 Some L3 binaries were not updated.' if scalar(grep { !$bins{$_}->{update_status} } @l3);
-    record_soft_failure 'poo#67357 Some L2 binaries were not installed.' if scalar(grep { !$bins{$_}->{update_status} } @l2);
+    # merge logs from all patches into one which is testreport template expecting
+    foreach (qw(prepare zypper new)) {
+        next if script_run("ls /tmp|grep ${_}_", die_on_timeout => 0);
+        assert_script_run("cat /tmp/$_* > /tmp/$_.log");
+        upload_logs("/tmp/$_.log");
+    }
 
     prepare_system_shutdown;
     power_action("reboot");
