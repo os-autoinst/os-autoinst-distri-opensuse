205b3d42d68db811a82a9176cf2c510bdd3c6777
Pavel Dostál <pdostal@suse.cz>
2022-03-22 11:13:25 +0100

    Containers: nerdctl: Install apparmor pattern on SLE15-SP3

--- lib/containers/common.pm
+++ lib/containers/common.pm
@@ -140,6 +140,7 @@ sub install_buildah_when_needed {
 sub install_containerd_when_needed {
     my $registry = registry_url();
     zypper_call('in containerd cni-plugins', timeout => 300);
+    zypper_call('in -t pattern apparmor') if is_sle('=15-SP3');
     assert_script_run "curl " . data_url('containers/containerd.toml') . " -o /etc/containerd/config.toml";
     file_content_replace("/etc/containerd/config.toml", REGISTRY => $registry);
     assert_script_run('cat /etc/containerd/config.toml');
