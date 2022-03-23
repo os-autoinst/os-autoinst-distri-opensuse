25aba306ab44fd7c8c7de911745aee3c8579071b
jknphy <jeriveramoya@suse.com>
2022-03-23 13:29:05 +0100

    Fix hostname for newer product in network_hostname

--- tests/console/network_hostname.pm
+++ tests/console/network_hostname.pm
@@ -1,23 +1,27 @@
 # SUSE's openQA tests
 #
-# Copyright 2018 SUSE LLC
+# Copyright 2022 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
 # Summary: Verify hostname is setup according options selected in the installer.
 # - Verify variable DHCLIENT_SET_HOSTNAME is set to 'no'
 # - Verify hostname is of the form linux-xxxx https://github.com/openSUSE/linuxrc/blob/master/linuxrc_hostname.md,
 # which means that is not taken into account dhcp configuration provided for this test via NICTYPE_USER_OPTIONS=hostname=myguest
-# Maintainer: Joaquín Rivera <jeriveramoya@suse.de>
+# Maintainer: QE YaST <qa-sle-yast@suse.de>
 
 use base "consoletest";
 use strict;
 use warnings;
 use testapi;
+use version_utils 'is_sle';
 
 sub run {
     select_console 'root-console';
-    assert_script_run 'grep DHCLIENT_SET_HOSTNAME=\"no\" /etc/sysconfig/network/dhcp';
-    assert_script_run 'hostname | grep -E "linux-[[:alnum:]]{4}"';
+    assert_script_run
+      'grep DHCLIENT_SET_HOSTNAME=\"no\" /etc/sysconfig/network/dhcp';
+    assert_script_run 'hostname | grep ' . is_sle('<=15-SP1')
+      ? '-E "linux-[[:alnum:]]{4}"'
+      : 'localhost';
 }
 
 1;
