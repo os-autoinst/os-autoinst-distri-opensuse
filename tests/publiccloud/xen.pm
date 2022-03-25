651d649303e90e95299bfdc8e07553f96e3ce8bd
phoenix <felix.niederwanger@suse.com>
2022-03-25 11:44:44 +0100

    Add Xen smoketest to publiccloud
    Add new PUBLIC_CLOUD_XEN variable to indicate Xen test runs and add a
    simple smoke test for them.

---
+++ tests/publiccloud/xen.pm
@@ -0,0 +1,22 @@
+# SUSE's openQA tests
+#
+# Copyright 2022 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Run basic xen smoketest on a publiccloud test instance
+# Maintainer: qa-c team <qa-c@suse.de>
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run {
+    # Check if we are using Xen hypervisor by searching for matching output in dmesg
+    assert_script_run("sudo dmesg > /var/tmp/dmesg");
+    assert_script_run("grep -e 'Hypervisor detected:.*Xen' /var/tmp/dmesg");
+    assert_script_run("grep -i 'Booting paravirtualized kernel on Xen' /var/tmp/dmesg");
+}
+
+1;
