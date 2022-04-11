f4c195039d8e2cf923a32025282d0de0490e692d
mloviska <mloviska@suse.com>
2022-04-11 13:29:09 +0200

    s390x:Enable QEMU test for slem images
    In order to test QEMU on z/KVM images, we need to rebuild initrd
    without *ignition*, *ignition-microos*, *combustion* modules.
    
    - ticket: [Fix qemu test on s390](https://progress.opensuse.org/issues/104269)

---
+++ tests/microos/rebuild_initrd.pm
@@ -0,0 +1,25 @@
+# SUSE's openQA tests
+#
+# Copyright 2022 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Rebuild initrd without ignition and combustion
+#          KIWI provided initrd loads ignition and combustion
+#          modules that can interfere with tests
+#          e.g. qemu/qemu.pm (s390x)
+# Maintainer: qa-c team <qa-c@suse.de>
+
+use Mojo::Base "opensusebasetest";
+use testapi;
+use transactional;
+
+sub run {
+    my ($self) = @_;
+    select_console 'root-console';
+
+    assert_script_run(q|echo 'omit_dracutmodules+="ignition ignition-microos combustion"' > /etc/dracut.conf.d/20-disable_ignition.conf|);
+    trup_call('initrd');
+    check_reboot_changes;
+}
+
+1;
