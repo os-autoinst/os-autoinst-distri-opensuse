93ca4b9ad6465d0a21c5379d20ce6c76ec963150
lpalovsky <lpalovsky@suse.com>
2022-05-10 16:36:27 +0200

    Kill DB tests

---
+++ tests/sles4sap/publiccloud/hana_sr_kill_site_B.pm
@@ -0,0 +1,38 @@
+
+use strict;
+use warnings FATAL => 'all';
+use diagnostics;
+use Data::Dumper;
+use testapi;
+use Mojo::Base qw(publiccloud::basetest);
+use Mojo::JSON;
+use Mojo::File qw(path);
+use sles4sap_publiccloud;
+use publiccloud::utils;
+
+sub run {
+    my ($self, $run_args) = @_;
+    $self->{instances} = $run_args->{instances};
+    my $site_b = $run_args->{site_b};
+
+    $self->select_serial_terminal;
+
+    # Switch to control Site B (currently PROMOTED)
+    $self->{my_instance} = $site_b;
+
+    record_info("Stop DB", "Running 'HDB -kill' on Site B ('$site_b->{instance_id}')");
+    $self->stop_hana(method => "kill");
+
+    record_info("Takeover check");
+    $self->check_takeover;
+
+    record_info("Replication", "Enabling replication on Site B (DEMOTED)");
+    $self->enable_replication();
+
+    record_info("Site B start");
+    $self->cleanup_resource();
+
+    record_info("Done", "Test finished");
+}
+
+1;
