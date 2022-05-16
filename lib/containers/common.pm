6ad235cbab56ff859c4803210ad8a54be338858d
Jose Lausuch <jlausuch@gmail.com>
2022-05-16 14:34:07 +0200

    Containers: use retries for yum commands
    Similar to Ubuntu, CentOS fails to install packages from time to time.
    https://openqa.suse.de/tests/8755832#step/bci_prepare/28

--- lib/containers/common.pm
+++ lib/containers/common.pm
@@ -45,7 +45,7 @@ sub install_podman_when_needed {
     my @pkgs = qw(podman);
     if (script_run("which podman") != 0) {
         if ($host_os eq 'centos') {
-            assert_script_run "yum -y install @pkgs --nobest --allowerasing", timeout => 300;
+            script_retry "yum -y install @pkgs --nobest --allowerasing", timeout => 300;
         } elsif ($host_os eq 'ubuntu') {
             script_retry("apt-get -y install @pkgs", timeout => 300);
         } else {
@@ -133,8 +133,8 @@ sub install_buildah_when_needed {
             script_retry("apt-get update", timeout => 900);
             script_retry("apt-get -y install buildah", timeout => 300);
         } elsif ($host_os eq 'rhel') {
-            assert_script_run('yum update -y', timeout => 300);
-            assert_script_run('yum install -y buildah', timeout => 300);
+            script_retry('yum update -y', timeout => 300);
+            script_retry('yum install -y buildah', timeout => 300);
         } else {
             activate_containers_module if $host_os =~ 'sle';
             zypper_call('in buildah', timeout => 300);
