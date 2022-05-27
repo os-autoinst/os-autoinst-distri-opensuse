d46877e667233c8201aef939d52db9d9fa28aff6
Jonathan Rivrain <JRivrain@suse.com>
2022-05-27 16:16:43 +0200

    Stabilize yast2 keyboard
    Sometimes we type test before the prompt appears, the change seem to fix
    it as confirmed by 50 VRs.
    
    poo# : https://progress.opensuse.org/issues/111276

--- tests/yast2_gui/yast2_keyboard.pm
+++ tests/yast2_gui/yast2_keyboard.pm
@@ -60,7 +60,7 @@ sub run {
 
     # 4. simulate german keystrokes to switch back us keymap
     send_key "alt-f2";
-    type_string "xdg/su /c |&sbin&zast2 kezboard|";
+    wait_screen_change { type_string "xdg/su /c |&sbin&zast2 kezboard|" };
     send_key_until_needlematch('root-auth-dialog', 'ret', 3, 3);
     wait_still_screen 2;
     type_password;
