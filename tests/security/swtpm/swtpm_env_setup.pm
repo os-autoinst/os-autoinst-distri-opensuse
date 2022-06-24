10265fbbee6a061ed7414dadc025c676fd4f1653
Starry Wang <starry.wang@suse.com>
2022-06-24 17:05:57 +0800

    Add aarch64 support for swtpm test
    Updated test code to run swtpm tests on raspberry pi 4 for adding
    aarch64 support.

--- tests/security/swtpm/swtpm_env_setup.pm
+++ tests/security/swtpm/swtpm_env_setup.pm
@@ -5,18 +5,38 @@
 #          install required packages and download the pre-installed
 #          OS images for later tests, prepare a script for remote
 #          ssh login and execution
-# Maintainer: rfan1 <richard.fan@suse.com>
-# Tags: poo#81256, tc#1768671
+# Maintainer: rfan1 <richard.fan@suse.com> Starry Wang <starry.wang@suse.com>
+# Tags: poo#81256, tc#1768671, poo#100512
 
 use base 'opensusebasetest';
 use strict;
 use warnings;
 use testapi;
 use utils 'zypper_call';
+use Utils::Architectures;
+use power_action_utils qw(power_action);
 
 sub run {
     my $self = shift;
-    $self->select_serial_terminal;
+    $self->select_serial_terminal if !(get_var('MACHINE') =~ /RPi4/);
+
+    # Enable TPM on Raspberry Pi 4
+    # Refer: https://en.opensuse.org/HCL:Raspberry_Pi3_TPM
+    if (get_var('MACHINE') =~ /RPi4/) {
+        assert_script_run('echo -e "dtparam=spi=on\ndtoverlay=tpm-slb9670" >> /boot/efi/extraconfig.txt');
+        assert_script_run('cat /boot/efi/extraconfig.txt');
+        assert_script_run('echo "tpm_tis_spi" > /etc/modules-load.d/tpm.conf');
+        power_action('reboot', textmode => 1);
+
+        # Add some timeout to wait for reboot
+        sleep(60);
+        # Restore SSH connection
+        reset_consoles;
+        select_console('root-ssh');
+
+        assert_script_run('dmesg | grep tpm');
+        record_info('RPi4 TPM', 'Enabled TPM on Rasperry Pi 4');
+    }
 
     # Install the required packages for libvirt environment setup,
     # "expect" is used for later remote login test, so install here as well
@@ -28,21 +48,52 @@ sub run {
     assert_script_run("systemctl is-active libvirtd");
     assert_script_run("virsh net-list | grep default | grep active");
 
+    # Since machine:RPi4 and machine:aarch64 are using different flavors,
+    # and we can not add `START_AFTER_TEST` dependencies between jobs in
+    # different flavors, so we need to wait create_swtpm_hdd job finished
+    # here if this test is running on machine:RPi4
+    my $openqa_url = get_var('OPENQA_URL', autoinst_url);
+    if (get_var('MACHINE') =~ /RPi4/) {
+        if (!get_var('CREATE_SWTPM_HDD_TEST')) {
+            die 'CREATE_SWTPM_HDD_TEST settings are needed';
+        }
+        my $build = get_var('BUILD');
+        my $create_swtpm_test = get_var('CREATE_SWTPM_HDD_TEST');
+        my $counter = 100;
+        record_info('check status', "start checking job status of $create_swtpm_test");
+        while ($counter--) {
+            my $result = script_output(
+                "curl -s \"$openqa_url/api/v1/jobs?build=$build&machine=aarch64&flavor=DVD&latest=1&test=$create_swtpm_test&state=done&page=1&limit=3\""
+            );
+            if ($result =~ m/\"state\":\"done\"/) {
+                record_info('check status finished', "$create_swtpm_test job done");
+                last;
+            }
+            sleep(60);
+        }
+        if (!$counter) {
+            die "waiting for test $create_swtpm_test finished timeout";
+        }
+    }
+
     # Download the pre-installed guest images and sample xml files
     my $image_path = '/var/lib/libvirt/images';
     my $legacy_image = 'swtpm_legacy@64bit.qcow2';
     my $uefi_image = 'swtpm_uefi@64bit.qcow2';
     if (get_var('HDD_SWTPM_LEGACY')) {
         my $hdd_swtpm_legacy = get_required_var('HDD_SWTPM_LEGACY');
-        assert_script_run("wget -c -P $image_path " . autoinst_url("/assets/hdd/$hdd_swtpm_legacy"), 900);
+        my $sample_file = 'swtpm/swtpm_legacy.xml';
+        assert_script_run("wget -c -P $image_path $openqa_url/assets/hdd/$hdd_swtpm_legacy", 900);
         assert_script_run("mv $image_path/$hdd_swtpm_legacy $image_path/$legacy_image");
-        assert_script_run("wget --quiet " . data_url("swtpm/swtpm_legacy.xml") . " -P $image_path");
+        assert_script_run("wget --quiet " . data_url($sample_file) . " -P $image_path");
     }
     elsif (get_var('HDD_SWTPM_UEFI')) {
         my $hdd_swtpm_uefi = get_required_var('HDD_SWTPM_UEFI');
-        assert_script_run("wget -c -P $image_path " . autoinst_url("/assets/hdd/$hdd_swtpm_uefi"), 900);
+        my $sample_file = 'swtpm/swtpm_uefi';
+        $sample_file .= is_aarch64 ? '_aarch64.xml' : '.xml';
+        assert_script_run("wget -c -P $image_path $openqa_url/assets/hdd/$hdd_swtpm_uefi", 900);
         assert_script_run("mv $image_path/$hdd_swtpm_uefi $image_path/$uefi_image");
-        assert_script_run("wget --quiet " . data_url("swtpm/swtpm_uefi.xml") . " -P $image_path");
+        assert_script_run("wget --quiet " . data_url($sample_file) . " -P $image_path");
     }
 
     # Write expect script to implement ssh access into remote host and run some commands
