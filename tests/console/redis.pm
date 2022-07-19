03ebf56af47ceded92c7105821dd6030f22e5032
DeepthiYV <deepthiholla@gmail.com>
2022-07-19 10:43:38 +0200

    Fix redis test

--- tests/console/redis.pm
+++ tests/console/redis.pm
@@ -17,15 +17,8 @@ use strict;
 use warnings;
 use testapi;
 use utils 'zypper_call';
-
-sub wait_serial_or_die {
-    my ($feedback, %args) = @_;
-    $args{timeout} //= 10;
-
-    my $e = wait_serial($feedback, %args);
-
-    die('Unexpected serial output') unless (defined $e);
-}
+use version_utils;
+use registration qw(add_suseconnect_product get_addon_fullname);
 
 sub run {
     my $self = shift;
@@ -34,46 +27,21 @@ sub run {
     # install redis package
     zypper_call 'in redis';
 
-    # start redis server
+    # start redis server on port 6379 and 6380
     background_script_run('redis-server');
     wait_still_screen(5);
-
-    # test if redis cli is working
-    enter_cmd('redis-cli');
-    wait_serial_or_die('127.0.0.1:6379');
-    enter_cmd('quit');
-
-    # test some redis cli commands
-    validate_script_output('redis-cli ping', sub { m/PONG/ });
-    validate_script_output('redis-cli set foo bar', sub { m/OK/ });
-    validate_script_output('redis-cli get foo', sub { m/bar/ });
-    validate_script_output('redis-cli pfselftest', sub { m/OK/ });
-    validate_script_output('redis-cli flushdb', sub { m/OK/ });
-    validate_script_output('redis-cli get foo', sub { m/(nil)/ });
-
-    assert_script_run 'curl -O ' . data_url('console/movies.redis');
-    assert_script_run('redis-cli -h localhost -p 6379 < ./movies.redis');
-    wait_still_screen(5);
-
-    validate_script_output('redis-cli HMGET "movie:343" title', sub { m/Spider-Man/ });
-
-    # start another redis instance with a different port
     background_script_run('redis-server --port 6380');
     wait_still_screen(5);
+    assert_script_run 'curl -O ' . data_url('console/movies.redis');
+    run_script('redis_cli.sh');
+}
 
-    # get redis cli prompt of new instance
-    enter_cmd('redis-cli -p 6380');
-    wait_serial_or_die('127.0.0.1:6380');
-    enter_cmd('quit');
-
-    # make 6380 instance a replica of redis instance running on port 6379
-    assert_script_run('redis-cli -p 6380 replicaof localhost 6379');
-    wait_still_screen(5);
-
-    # test replication
-    validate_script_output('redis-cli info replication', sub { m/connected_slaves:1/ });
-    validate_script_output('redis-cli -p 6380 info replication', sub { m/role:slave/ });
-    validate_script_output('redis-cli -p 6380 HMGET "movie:343" title', sub { m/Spider-Man/ });
+sub run_script {
+    my $script = shift;
+    record_info($script, "Running shell script: $script");
+    assert_script_run("curl " . data_url("console/$script") . " -o '$script'");
+    assert_script_run("chmod a+rx '$script'");
+    assert_script_run("./$script");
 }
 
 sub post_fail_hook {
@@ -94,6 +62,8 @@ sub cleanup {
     script_run('killall redis-server');
     wait_still_screen(5);
     script_run('rm -f movies.redis');
+    wait_still_screen(5);
+    script_run('rm -f redis_cli.sh');
 }
 
 1;
