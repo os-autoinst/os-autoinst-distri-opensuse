afda54ed8275e452b33783e5c996ceb3daf8f4eb
lpalovsky <lpalovsky@suse.com>
2022-08-04 16:40:35 +0200

    Create crash test modules

---
+++ tests/sles4sap/publiccloud/hana_sr_crash_site_A.pm
@@ -0,0 +1,52 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: HANA SR - Crash OS on site A
+# Stop database on Site A by by crashing OS and forcing reboot.
+# Do takeover on Site B
+#
+# Maintainer: QE-SAP <qe-sap@suse.de>
+
+use strict;
+use warnings FATAL => 'all';
+use diagnostics;
+use Data::Dumper;
+use testapi;
+use Mojo::Base qw(publiccloud::basetest);
+use Mojo::JSON;
+use Mojo::File qw(path);
+use sles4sap_publiccloud;
+use publiccloud::utils;
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+sub run {
+    my ($self, $run_args) = @_;
+    $self->{instances} = $run_args->{instances};
+    my $site_a = $run_args->{site_a};
+
+    $self->select_serial_terminal;
+
+    # Switch to control Site A (currently PROMOTED)
+    $self->{my_instance} = $site_a;
+
+    record_info("Stop DB", "Running 'proc-systrigger' on Site A ('$site_a->{instance_id}')");
+    $self->stop_hana(method => "crash");
+
+    record_info("Takeover check");
+    $self->check_takeover;
+
+    record_info("Replication", "Enabling replication on Site A (DEMOTED)");
+    $self->enable_replication();
+
+    record_info("Site A start");
+    $self->cleanup_resource();
+
+    record_info("Done", "Test finished");
+}
+
+1;
---
+++ tests/sles4sap/publiccloud/hana_sr_crash_site_B.pm
@@ -0,0 +1,52 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: HANA SR - Crash OS on site B
+# Stop database on Site B by crashing OS and forcing reboot.
+# Do takeover on Site A
+#
+# Maintainer: QE-SAP <qe-sap@suse.de>
+
+use strict;
+use warnings FATAL => 'all';
+use diagnostics;
+use Data::Dumper;
+use testapi;
+use Mojo::Base qw(publiccloud::basetest);
+use Mojo::JSON;
+use Mojo::File qw(path);
+use sles4sap_publiccloud;
+use publiccloud::utils;
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+sub run {
+    my ($self, $run_args) = @_;
+    $self->{instances} = $run_args->{instances};
+    my $site_b = $run_args->{site_b};
+
+    $self->select_serial_terminal;
+
+    # Switch to control Site A (currently PROMOTED)
+    $self->{my_instance} = $site_b;
+
+    record_info("Stop DB", "Running 'proc-systrigger' on Site B ('$site_b->{instance_id}')");
+    $self->stop_hana(method => "crash");
+
+    record_info("Takeover check");
+    $self->check_takeover;
+
+    record_info("Replication", "Enabling replication on Site B (DEMOTED)");
+    $self->enable_replication();
+
+    record_info("Site B start");
+    $self->cleanup_resource();
+
+    record_info("Done", "Test finished");
+}
+
+1;
