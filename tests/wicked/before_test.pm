f9ca802fd2554b541fc2ec560ebc339ce8260795
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2022-08-16 13:46:00 +0200

    wicked: that installation from repo took place!
    If WICKED_REPO is set, we will install wicked and
    wicked-service from it. But this wasn't checked and thus we simple continue
    but the version under test is something different!

--- tests/wicked/before_test.pm
+++ tests/wicked/before_test.pm
@@ -120,6 +120,9 @@ sub run {
                     'The following \d+ packages? (are|is) going to be reinstalled:')) {
                 push(@installed_packages, split(/\s+/, $+{packages})) if ($zypper_in_output =~ m/(?s)($reg(?<packages>.*?))(?:\r*\n){2}/);
             }
+            for my $pkg ('wicked', 'wicked-service') {
+                die("Missing installation of package $pkg!") unless grep { $_ eq $pkg } @installed_packages;
+            }
             record_info('INSTALLED', join("\n", @installed_packages));
             my @zypper_ps_progs = split(/\s+/, script_output('zypper ps  --print "%s"', qr/^\s*$/));
             for my $ps_prog (@zypper_ps_progs) {
