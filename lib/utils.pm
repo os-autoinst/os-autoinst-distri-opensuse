f84691992d8883bce22477f4de3572abd1422498
Dominik Heidler <dheidler@suse.de>
2022-08-30 16:15:34 +0200

    Capture installed and uninstalled packages during test (PR v2)
    Ticket: https://progress.opensuse.org/issues/115478
    VR: http://kazhua.qa.suse.de/tests/149
    
    As suggested in the ticket this uses `save_ulog` to directly save
    the asset into the right folder on the worker filesystem.

--- lib/utils.pm
+++ lib/utils.pm
@@ -588,6 +588,50 @@ sub zypper_call {
         }
         last;
     }
+
+    # log all install and remove actions for later use by tests/console/zypper_log_packages.pm
+    my @packages = split(" ", $command);
+    my $dry_run = 0;
+    for (my $i = 0; $i < scalar(@packages); $i++) {
+        if ($packages[$i] eq "--root" || $packages[$i] eq "-R") {
+            splice(@packages, $i, 2);
+        }
+        elsif ($packages[$i] eq "--name" || $packages[$i] eq "-n") {
+            splice(@packages, $i, 2);
+        }
+        elsif ($packages[$i] eq "--from") {
+            splice(@packages, $i, 2);
+        }
+        elsif ($packages[$i] eq "--repo" || $packages[$i] eq "-r") {
+            splice(@packages, $i, 2);
+        }
+        elsif ($packages[$i] eq "--download") {
+            splice(@packages, $i, 2);
+        }
+        elsif ($packages[$i] eq "--dry-run" || $packages[$i] eq "--download-only" || $packages[$i] eq '-d') {
+            $dry_run = 1;
+        }
+        elsif ($packages[$i] eq "--solver-focus") {
+            splice(@packages, $i, 2);
+        }
+    }
+    @packages = grep(/^[^-]/, @packages);
+    my $zypper_action = shift(@packages);
+    $zypper_action = "install" if ($zypper_action eq "in");
+    $zypper_action = "remove" if ($zypper_action eq "rm");
+    if ($zypper_action =~ m/^(install|remove)$/ && !$dry_run) {
+        push(@{$testapi::distri->{zypper_packages}}, {
+                raw_command => $command,
+                action => $zypper_action,
+                packages => \@packages,
+                return_code => $ret,
+                test => {
+                    module => $autotest::current_test->{name},
+                    category => $autotest::current_test->{category}
+                }
+        });
+    }
+
     upload_logs("/tmp/$log") if $log;
 
     unless (grep { $_ == $ret } @$allow_exit_codes) {
