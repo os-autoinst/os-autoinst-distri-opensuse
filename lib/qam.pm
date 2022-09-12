4d69e59344256878fee76f771733ab2de52471f9
jknphy <jeriveramoya@suse.com>
2022-09-12 08:44:26 +0200

    blablab

--- lib/qam.pm
+++ lib/qam.pm
@@ -159,14 +159,14 @@ sub remove_test_repositories {
 
     type_string 'repos=($(zypper lr -e - | grep "name=TEST|baseurl=ftp" | cut -d= -f2)); if [ ${#repos[@]} -ne 0 ]; then zypper rr ${repos[@]}; fi';
     send_key 'ret';
-    type_string 'zypper -n ref';
-    send_key 'ret';
 }
 
 # Function that will remove all unreleased updated repos
 sub remove_unreleased_updates_repositories {
 
     type_string 'repos=($(zypper lr -e - | grep "name=SUSE_Updates" | cut -d= -f2)); if [ ${#repos[@]} -ne 0 ]; then zypper rr ${repos[@]}; fi';
+    send_key 'ret';
+    type_string 'zypper -n ref';
     send_key 'ret';
 }
 
