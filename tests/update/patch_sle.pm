1e9617f96b05cf7ee1e0d243f0ef9e33774137fc
jknphy <jeriveramoya@suse.com>
2022-09-12 09:46:03 +0200

    blablab

--- tests/update/patch_sle.pm
+++ tests/update/patch_sle.pm
@@ -45,8 +45,7 @@ sub patching_sle {
         set_scc_proxy_url if ((check_var('HDDVERSION', get_var('ORIGINAL_TARGET_VERSION')) && is_upgrade()));
         # remove_unreleased_updates_repositories;
         # assert_script_run 'SUSEConnect --cleanup';
-        # sle_register("register");
-        sleep 360;
+        sle_register("register");
         zypper_call('lr -d');
     }
 
