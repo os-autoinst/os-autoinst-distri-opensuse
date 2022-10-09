3c16038c8cc475fdd6cbb771192b62e4ce6787c4
openQA web UI <openqa@yutao-qe1>
2022-10-09 14:41:20 +0800

    Replace select_patterns when unselecting all patterns

---
+++ tests/installation/select_only_visible_patterns_from_top.pm
@@ -0,0 +1,33 @@
+# SUSE's openQA tests
+#
+# Copyright 2022 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Select given PATTERNS
+#    You can pass
+#    PATTERNS=minimal,base or
+#
+#    For this you need to have needles that provide base-pattern,
+#    minimal-pattern...
+# Maintainer: QA SLE YaST team <qa-sle-yast@suse.de>
+
+use base 'y2_installbase';
+use strict;
+use warnings;
+use testapi;
+use utils 'type_string_slow';
+
+sub run {
+    my ($self) = @_;
+    $self->go_to_patterns();
+    # Remove default patterns first
+    $self->select_not_install_any_pattern();
+    # go to the top of the list before looking for the pattern
+    send_key "home";
+    my @patterns = grep($_, split(/,/, get_required_var('PATTERNS')));
+    # Select visible patterns
+    $self->select_visible_unselected_patterns([@patterns]);
+    $self->accept_changes();
+}
+
+1;
