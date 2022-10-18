4b36eb8c6d5b682249903126601e9444b8ae804d
Ioannis Bonatakis <ybonatakis@suse.com>
2022-10-18 10:47:58 +0200

    Create test cases and xml report for rasdaemon
    Make rasdaemon to export results in xml and implement validation of the
    commands' outputs.
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/hpc/rasdaemon.pm
+++ tests/hpc/rasdaemon.pm
@@ -24,6 +24,8 @@ use testapi;
 use Utils::Architectures;
 use utils;
 
+our $file = 'tmpresults.xml';
+
 sub inject_error() {
     # Inject some software errors
     script_run('echo 0x9c00410000080f2b > /sys/kernel/debug/mce-inject/status');
@@ -33,16 +35,35 @@ sub inject_error() {
     script_run("echo \"sw\" > /sys/kernel/debug/mce-inject/flags");
 }
 
+our $ras_mc_ctl_results;
+
+sub subtestcase_output ($out, $regex, $description) {
+    foreach my $line (split('\n', $out)) {
+        next if ($line =~ /^\s*$/);
+        if (grep /$regex/, $line) {
+            $ras_mc_ctl_results = 0;
+        }
+        else {
+            $ras_mc_ctl_results = 1;
+        }
+        test_case($line, $description, $ras_mc_ctl_results);
+    }
+}
+
 sub run ($self) {
     # load kernel module
     assert_script_run('modprobe mce-inject') if (is_x86_64 && check_var('VERSION', '15-SP2'));
 
-    zypper_call('in rasdaemon');
+    my $rt = zypper_call('in rasdaemon');
+    test_case('Installation', 'rasdaemon', $rt);
 
-    assert_script_run('! ras-mc-ctl --status');
+    $rt = assert_script_run('! ras-mc-ctl --status');
+    test_case('Check rasdaemon --status', 'drivers are not loaded', $rt);
 
     # Try to start rasdaemon. May need a restart on aarch64.
-    systemctl('start rasdaemon');
+    $rt = systemctl('start rasdaemon');
+    test_case('Start rasdaemon service', 'service is started', $rt);
+
     if (systemctl('is-active rasdaemon', ignore_failure => 1)) {
         systemctl('restart rasdaemon');
         record_soft_failure('bsc#1170014 rasdaemon service needed to be restarted.');
@@ -52,12 +73,17 @@ sub run ($self) {
     # Validating output of 'ras-mc-ctl --mainboard'
     my $mainboard_output = script_output('ras-mc-ctl --mainboard');
     record_info('INFO', $mainboard_output);
+    test_case('ras-mc-ctl --mainboard', 'Check for errors', $ras_mc_ctl_results);
+    subtestcase_output($mainboard_output, 'mainboard', 'Check for errors from ras-mc-ctl --mainboard');
+
     die('Not expected mainboard - ' . $mainboard_output)
       unless ($mainboard_output =~ /mainboard/);
 
     # Validating output of 'ras-mc-ctl --summary' with assumption that no errors exists
     my $summary_output = script_output('ras-mc-ctl --summary');
     record_info('INFO', $summary_output);
+    test_case('Status ras-mc-ctl --summary', 'Status of the command', $summary_output);
+    subtestcase_output($summary_output, 'No .+ errors', 'Check for errors from ras-mc-ctl --summary');
 
     die('Not expected summary - ' . $summary_output)
       unless ($summary_output =~ /No Memory errors/
@@ -66,6 +92,8 @@ sub run ($self) {
     # Validating output of 'ras-mc-ctl --errors' with assumption that no errors exists
     my $empty_error_output = script_output('ras-mc-ctl --errors');
     record_info('INFO', $empty_error_output);
+    test_case('Status ras-mc-ctl --errors', 'Status of the command', $empty_error_output);
+    subtestcase_output($empty_error_output, 'No .+ errors', 'Check for errors from ras-mc-ctl --errors');
 
     die('Not expected error - ' . $empty_error_output)
       unless ($empty_error_output =~ /Memory errors/
@@ -82,6 +110,12 @@ sub run ($self) {
     }
 
     ##TODO: try to add error injection for ARM
+}
+
+sub post_run_hook ($self) {
+    pars_results('HPC rasdaemon tests', $file, @all_tests_results);
+    parse_extra_log('XUnit', $file);
+    $self->SUPER::post_run_hook();
 }
 
 sub post_fail_hook ($self) {
