03f465eb8f009d3e7c30b4f080d63cd10ffdd36c
coolgw <wegao@suse.com>
2022-10-21 04:53:25 -0400

    Record soft-failure in test suite installer_extended for product modules not available in early phase

--- tests/installation/module_registration/verify_module_registration.pm
+++ tests/installation/module_registration/verify_module_registration.pm
@@ -13,21 +13,60 @@ use base 'y2_installbase';
 use strict;
 use warnings;
 use scheduler 'get_test_suite_data';
-use utils 'arrays_differ';
+use utils 'arrays_subset';
+use testapi;
 
 sub run {
+    my %softfail_modules_data = (
+        PackageHub => "bsc#1202416 - packagehub cannot work at sles15sp5",
+        "sle-module-NVIDIA-compute" => "bsc#1204611 - no sle-module-NVIDIA-compute in 15sp5",
+        "sle-module-certifications" => "bsc#1204612 - no sle-module-certifications in 15sp5",
+    );
+
+    my @softfail_modules = keys %softfail_modules_data;
     my @expected_modules = @{get_test_suite_data()->{modules}};
     my @modules = @{$testapi::distri->get_module_registration()->get_modules()};
-    die "Modules do not match with the expected ones."
-      . "\nExpected:\n" . join(', ', @expected_modules)
-      . "\nActual:\n" . join(', ', @modules)
-      if arrays_differ(\@expected_modules, \@modules);
+    my @diff = arrays_subset(\@expected_modules, \@modules);
+    my @diff2 = arrays_subset(\@diff, \@softfail_modules);
+    if (scalar @diff > 0) {
+        if (scalar @diff2 > 0) {
+            die "Modules do not match with the expected ones."
+              . "\nExpected:\n"
+              . join(', ', @expected_modules)
+              . "\nActual:\n"
+              . join(', ', @modules)
+              . "\nFailed modules:\n"
+              . join(', ', @diff2);
+        }
+        else {
+            foreach (@diff) {
+                # Hack prefix string "BUG#0#BUG" for workaround CI check of record_soft_failure
+                record_soft_failure("BUG#0#BUG:" . $softfail_modules_data{$_});
+            }
+        }
+    }
+
     my @expected_registered_modules = @{get_test_suite_data()->{registered_modules}};
     my @registered_modules = @{$testapi::distri->get_module_registration()->get_registered_modules()};
-    die "Selected modules are not the default ones"
-      . "\nExpected:\n" . join(', ', @expected_registered_modules)
-      . "\nActual:\n" . join(', ', @registered_modules)
-      if arrays_differ(\@expected_registered_modules, \@registered_modules);
+    @diff = arrays_subset(\@expected_registered_modules, \@registered_modules);
+    @diff2 = arrays_subset(\@diff, \@softfail_modules);
+    if (scalar @diff > 0) {
+        if (scalar @diff2 > 0) {
+            die "Selected modules are not the default ones"
+              . "\nExpected:\n"
+              . join(', ', @expected_registered_modules)
+              . "\nActual:\n"
+              . join(', ', @registered_modules)
+              . "\nFailed modules:\n"
+              . join(', ', @diff2);
+        }
+        else {
+            foreach (@diff) {
+                # Hack prefix string "BUG#0#BUG" for workaround CI check of record_soft_failure
+                record_soft_failure("BUG#0#BUG:" . $softfail_modules_data{$_});
+            }
+        }
+    }
 }
 
 1;
