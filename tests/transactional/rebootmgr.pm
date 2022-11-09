a134559bf7d509a0b40b5eb92ace1f09f63b71af
Fabian Vogt <fvogt@suse.de>
2022-11-09 08:51:42 +0100

    transactional/rebootmgr: Some cleanups
    - Make the duration parameter mandatory
    - The etcd function is dead and the functionality removed in rebootmgr, drop it
    - Call get_utt_packages to not rely on previous modules
    - Drop "tput civis", shouldn't be needed and doesn't persist past reboots
      anyway

--- tests/transactional/rebootmgr.pm
+++ tests/transactional/rebootmgr.pm
@@ -42,7 +42,7 @@ sub rbm_check_status {
 # Sample time values: +1hour, -20minutes, now, 00:30
 sub rbm_set_window {
     my $time = shift;
-    my $duration = shift // '1h';
+    my $duration = shift;
     rbm_call "set-window \$(date -d $time +%T) $duration";
 }
 
@@ -59,7 +59,7 @@ sub check_strategy_maint_window {
     rbm_call "set-strategy maint-window";
 
     # Trigger reboot during maint-window
-    rbm_set_window '-5minutes';
+    rbm_set_window '-5minutes', '20m';
     trup_call "reboot pkg install" . rpmver('feature');
     process_reboot(expected_grub => is_pvm() ? 0 : 1);
 
@@ -71,38 +71,15 @@ sub check_strategy_maint_window {
     process_reboot;
 
     # Trigger & cancel reboot
-    rbm_set_window '+1hour';
+    rbm_set_window '+1hour', '20m';
     rbm_call 'reboot';
     rbm_check_status 2;
-    rbm_call "cancel";
-    rbm_check_status 0;
-}
-
-#3 Test etcd locking strategy
-sub check_strategy_etcd_lock {
-    rbm_call "set-strategy etcd-lock";
-    systemctl 'enable --now etcd';
-
-    # Unlock during maintenance window - bsc#1026274
-    rbm_call "lock lock1";
-    rbm_set_window 'now', '1h';
-    trup_call "reboot ptf install" . rpmver('reboot-needed');
-    rbm_check_status 3;
-    rbm_call "unlock lock1", 0;
-    process_reboot;
-
-    # Maintenance window passes while waiting for lock - bsc#1026298
-    rbm_call "lock lock2";
-    rbm_set_window '+1minute', '1m';
-    rbm_call 'reboot';
-    sleep 120;
-    rbm_check_status 3;
-    rbm_call "unlock lock2";
+    rbm_call 'cancel';
     rbm_check_status 0;
 }
 
 sub run {
-    enter_cmd "tput civis";
+    get_utt_packages;
 
     record_info 'Instantly', 'Test instant reboot';
     check_strategy_instantly;
