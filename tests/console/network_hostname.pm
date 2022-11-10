e41babae167bc50bea387ea0a06f44ff0d7e2b16
rfan1 <richard.fan@suse.com>
2022-11-10 03:18:16 +0100

    Check that hostname is 'localhost' if hostname boot option is not used on SLE15-SP2+
    https://progress.opensuse.org/issues/117424
    
    If hostname boot option is not used:
    linuxrc doesn't write /etc/install.inf::Hostname, so it is not present.
    linuxrc writes /etc/install.inf::SetHostnameUsed=0.
    linuxrc sets the transient hostname to install.

--- tests/console/network_hostname.pm
+++ tests/console/network_hostname.pm
@@ -13,11 +13,19 @@ use base "consoletest";
 use strict;
 use warnings;
 use testapi;
+use version_utils 'is_sle';
 
 sub run {
     select_console 'root-console';
     assert_script_run 'grep DHCLIENT_SET_HOSTNAME=\"no\" /etc/sysconfig/network/dhcp';
-    assert_script_run 'hostname | grep -E "linux-[[:alnum:]]{4}"';
+
+    # YaST tries to configure the local system hostname during installation.
+    # Currently (since SLE 15 SP2) YaST configures target system hostname only if
+    # it is explicitly set when booting installation with linuxrc's hostname option.
+    # In all other cases no hostname is proposed and you have to set the hostname
+    # later when booted into installed system.
+    my $hostname = is_sle('>=15-SP2') ? 'localhost' : 'linux-[[:alnum:]]{4}';
+    assert_script_run "hostname | grep -E '$hostname'";
 }
 
 1;
