9602f147400ba3eb6ac38c0256826527c2da3aad
Fabian Vogt <fvogt@suse.de>
2022-11-10 08:49:20 +0100

    installation/boot_encrypt: New grub2 is in more of Tumbleweed now

--- tests/installation/boot_encrypt.pm
+++ tests/installation/boot_encrypt.pm
@@ -13,12 +13,13 @@ use warnings;
 use base "installbasetest";
 use utils;
 use testapi qw(check_var get_var record_info);
+use version_utils qw(is_leap is_sle is_leap_micro is_sle_micro);
 
 sub run {
-    # In TW (Staging:M only for now), entering the passphrase in GRUB2
+    # With newer grub2 (in TW only currently), entering the passphrase in GRUB2
     # is enough. The key is passed on during boot, so it's not asked for
     # a second time.
-    return if is_boot_encrypted && check_var('VERSION', 'Staging:M');
+    return if is_boot_encrypted && !is_leap && !is_sle && !is_leap_micro && !is_sle_micro;
 
     unlock_if_encrypted(check_typed_password => 1);
 }
