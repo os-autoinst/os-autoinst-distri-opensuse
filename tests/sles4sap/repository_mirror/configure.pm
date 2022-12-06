7e5cce935d4442f0f27c4fb0e31903af25654492
Jan Kohoutek <jkohoutek@suse.com>
2022-12-06 09:20:43 +0100

    more verbose changes

--- tests/sles4sap/repository_mirror/configure.pm
+++ tests/sles4sap/repository_mirror/configure.pm
@@ -18,21 +18,21 @@ sub run {
     # Init al the PC gears (ssh keys)
     my $provider = $self->provider_factory();
 
-    my $resource_group_postfix = 'qesapval' . get_current_job_id();
+    my $resource_group_postfix = 'ibsmirror' . get_current_job_id();
     my $qesap_provider = lc get_required_var('PUBLIC_CLOUD_PROVIDER');
 
     my %variables;
     $variables{PROVIDER} = $qesap_provider;
     $variables{REGION} = $provider->provider_client->region;
-    $variables{DEPLOYMENTNAME} = $resource_group_postfix;
-    $variables{QESAP_CLUSTER_OS_VER} = get_required_var("QESAP_CLUSTER_OS_VER");
+    $variables{DEPLOYMENT_NAME} = $resource_group_postfix;
+    $variables{DEPLOYMENT_OS_VER} = get_required_var("DEPLOYMENT_OS_VER");
 #    $variables{SSH_KEY_PRIV} = '/root/.ssh/id_rsa';
 #    $variables{SSH_KEY_PUB} = '/root/.ssh/id_rsa.pub';
 #    $variables{SCC_REGCODE_SLES4SAP} = get_required_var('SCC_REGCODE_SLES4SAP');
 #   qesap_prepare_env(openqa_variables => \%variables, provider => $qesap_provider);
 
 # Clone the terraform and ansible files from the gitlab
-    my $work_dir = '~/deployement/';
+    my $work_dir = '~/deployment/';
     # Get the code for the Trento deployment
     my $gitlab_repo = get_var('GITLAB_REPO', 'gitlab.suse.de/jkohoutek/plan-b');
 
@@ -45,11 +45,14 @@ sub run {
     my $gitlab_clone_url = 'https://' . $gitlab_repo;
 
     record_info('CLONE', "Clone $gitlab_repo in $work_dir");
+    assert_script_run("mkdir $work_dir");
     assert_script_run("cd $work_dir");
     #assert_script_run("git clone $gitlab_clone_url .  2>&1 | tee " . GITLAB_CLONE_LOG);
     assert_script_run("git clone $gitlab_clone_url . 2>&1 | tee /tmp/gitlab_clone.log");
     assert_script_run("ls $work_dir");
+    assert_script_run("pws");
     assert_script_run("find ./");
+    assert_script_run("az account list");
 
 }
 
