da9f140915101265734f98692e04798cabe61785
mloviska <mloviska@suse.com>
2023-01-25 17:13:32 +0100

    Increase timeout to enable mandatory features
    MS features require more time to process in Windows 11.
    
    - failures:
      https://openqa.suse.de/tests/10363492#step/prepare_wsl_feature/20

--- tests/wsl/prepare_wsl_feature.pm
+++ tests/wsl/prepare_wsl_feature.pm
@@ -92,12 +92,12 @@ sub run {
     # reboot the SUT
     $self->run_in_powershell(
         cmd => $powershell_cmds->{enable_wsl_feature}->{wsl},
-        timeout => 120
+        timeout => 240
     );
     if (get_var('WSL2')) {
         $self->run_in_powershell(
             cmd => $powershell_cmds->{enable_wsl_feature}->{vm_platform},
-            timeout => 120
+            timeout => 240
         );
     }
 
