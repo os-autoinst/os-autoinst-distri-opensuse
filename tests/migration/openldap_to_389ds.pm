c25a35f7b4d69cd5c98a1bc9f1cecb7cea959ddd
Sofia Syrianidou <ssyrianidou@suse.com>
2023-03-01 09:54:48 +0100

    Split openldap_to_389ds

--- tests/migration/openldap_to_389ds.pm
+++ tests/migration/openldap_to_389ds.pm
@@ -12,9 +12,7 @@ use serial_terminal 'select_serial_terminal';
 use strict;
 use warnings;
 use utils;
-use lockapi;
-use Utils::Systemd qw(systemctl disable_and_stop_service);
-use registration qw(add_suseconnect_product);
+use Utils::Systemd qw(systemctl);
 use version_utils;
 use Utils::Logging 'tar_and_upload_log';
 
@@ -23,64 +21,16 @@ sub run {
     my $password = $testapi::password;
     select_serial_terminal;
 
-    # Install 389-ds and sssd on client
-    zypper_call("in 389-ds sssd sssd-tools");
+    # Install 389-ds
+    zypper_call("in 389-ds");
     zypper_call('info 389-ds');
-    # Install openldap since we need use slaptest tools
-    if (is_sle) {
-        my $openldap = get_required_var("OPENLDAP_PKG");
-        assert_script_run('wget ' . $openldap, 200);
-        my @ldap = split('/', $openldap);
-        assert_script_run 'rpm -i ' . $ldap[-1];
-    } else {
-        zypper_call("in openldap2");
-    }
-    zypper_call("in sssd-ldap openldap2-client");
-
-    # Disable and stop the nscd daemon because it conflicts with sssd
-    disable_and_stop_service("nscd");
-
-    # On newer environments, nsswitch.conf is located in /usr/etc
-    # Copy it to /etc directory
-    script_run 'f=/etc/nsswitch.conf; [ ! -f $f ] && cp /usr$f $f';
-    # Configure nsswitch with sssd
-    assert_script_run "sed -i 's/^passwd:.*/passwd: compat sss/' /etc/nsswitch.conf";
-    assert_script_run "sed -i 's/^group:.*/group: compat sss/' /etc/nsswitch.conf";
-    assert_script_run "cat /etc/nsswitch.conf";
-
-    # Prepare test env
-    assert_script_run "cd; curl -L -v " . autoinst_url . "/data/openldap_to_389ds > openldap_to_389ds.data && cpio -id < openldap_to_389ds.data && mv data test && ls test";
-    assert_script_run("cd test");
-
-    # We need start openldap to kick out date base file which stored in directory
-    assert_script_run "mkdir /tmp/ldap-sssdtest";
-    if (is_tumbleweed) {
-        assert_script_run "sed -i -e '/cachesize/d' ./slapd.conf";
-        assert_script_run "sed -i -e 's/hdb/mdb/g' ./slapd.conf";
-        # for openqa debug
-        permit_root_ssh;
-    }
-    assert_script_run "cat ./slapd.conf";
-    assert_script_run "slapd -h 'ldap:///' -f slapd.conf";
-
-    assert_script_run "ldapadd -x -D 'cn=root,dc=ldapdom,dc=net' -wpass -f db.ldif";
-    assert_script_run "killall slapd";
-    assert_script_run "ps -aux | grep slapd";
-
-    # setup sssd
-    assert_script_run "cp ./sssd.conf /etc/sssd/sssd.conf";
-    systemctl("stop sssd");
-    assert_script_run "rm -rf /var/lib/sss/db/*";
-    systemctl("restart sssd");
-    systemctl("status sssd");
 
     # Prepare data file for migration
+    assert_script_run("cd test");
     assert_script_run "sed -i 's/^root_password.*/root_password = $password/' ./instance.inf";
-    assert_script_run "mkdir slapd.d";
     assert_script_run("dscreate from-file ./instance.inf", timeout => 180);
     assert_script_run("echo '127.0.0.1    susetest' >> /etc/hosts");
     assert_script_run "dsctl localhost status";
-    assert_script_run "slaptest -f slapd.conf -F ./slapd.d";
 
     # Check migration tools
     tar_and_upload_log('./slapd.d', 'slapd.d.tar.bz2');
@@ -116,7 +66,6 @@ sub post_fail_hook {
     my ($self) = @_;
     select_console 'log-console';
     tar_and_upload_log('/var/log/dirsrv', '/tmp/ds389.tar.bz2');
-    tar_and_upload_log('/var/log/sssd', '/tmp/sssd.tar.bz2');
     $self->SUPER::post_fail_hook;
 }
 
