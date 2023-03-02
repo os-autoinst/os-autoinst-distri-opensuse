0687b96c5e6e8b16a47b32f646082535d87db4e2
Alvaro Carvajal <acarvajal@suse.de>
2023-03-02 10:43:02 +0100

    Remove testing of sap-b1/sap-bone pattern in 15-SP5+
    Per SR:290910, SLES for SAP Applications 15-SP5 will no longer have a
    sap-b1 (or sap-bone) pattern available, so the patterns test module
    should not look for this pattern starting with 15-SP5.

--- tests/sles4sap/patterns.pm
+++ tests/sles4sap/patterns.pm
@@ -19,7 +19,7 @@ use warnings;
 
 sub run {
     my @sappatterns = qw(sap-nw sap-b1 sap-hana);
-    $sappatterns[1] =~ s/b1/bone/ if (is_sle('15-SP5+'));
+    splice(@sappatterns, 1, 1) if (is_sle('15-SP5+'));    # sap-bone pattern is no longer part of SLES4SAP starting on 15-SP5
     my $output = '';
 
     select_serial_terminal;
