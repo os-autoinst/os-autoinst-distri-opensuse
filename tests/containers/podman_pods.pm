36d281f896fa3ef8bf1e5d7dba72c814d774ce67
Ivan Lausuch <ilausuch@suse.com>
2023-03-07 13:23:43 +0100

    Add podman kube generate test
    https://progress.opensuse.org/issues/124601

--- tests/containers/podman_pods.pm
+++ tests/containers/podman_pods.pm
@@ -12,6 +12,7 @@
 
 use Mojo::Base 'containers::basetest';
 use testapi;
+use utils qw(script_retry);
 use serial_terminal 'select_serial_terminal';
 use version_utils qw(is_sle is_opensuse);
 
@@ -40,6 +41,41 @@ sub run {
         record_info('Cleanup', 'Stop pods');
         assert_script_run('podman play kube --down hello-kubic.yaml');
     }
+
+    unless (is_sle('<=15-SP2')) {
+        # Kube generate
+        record_info('Test', 'Generate the yaml from a pod');
+        assert_script_run('podman pod create testing_pod');
+        my $image = "registry.suse.com/bci/bci-busybox:latest";
+        script_retry("podman pull $image", timeout => 300, delay => 60, retry => 3);
+        assert_script_run("podman container create --pod testing_pod --name container $image sh -c \"sleep 3600\"");
+        assert_script_run("podman kube generate testing_pod -f pod.yaml");
+        assert_script_run("grep 'image: $image' pod.yaml");
+        assert_script_run("podman pod rm testing_pod");
+
+        record_info('Test', 'Test the pod yaml creates a pod');
+        assert_script_run('podman play kube pod.yaml');
+        record_info('Test', 'Confirm pod is running');
+        record_info('pod ps', script_output('podman pod ps'));
+        validate_script_output('podman pod ps', sub { m/testing_pod/ });
+        validate_script_output('podman ps', sub { m/testing_pod-container/ });
+
+        record_info('Test', 'Removing one pod');
+        assert_script_run('podman pod rm -f testing_pod');
+
+        # kube play
+        record_info('Test', 'kube play');
+        assert_script_run('podman kube play pod.yaml');
+        validate_script_output('podman pod ps', sub { m/testing_pod/ });
+        validate_script_output('podman ps', sub { m/testing_pod-container/ });
+
+        # kube down
+        record_info('Test', 'kube down');
+        assert_script_run('podman kube down pod.yaml');
+        validate_script_output('podman pod ps', sub { !m/testing_pod/ });
+        validate_script_output('podman ps', sub { !m/testing_pod-container/ });
+    }
+
     $podman->cleanup_system_host();
 }
 
