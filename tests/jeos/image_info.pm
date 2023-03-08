37b100eb75a52a27be9c5776fc3d2d37cce9b48c
mloviska <mloviska@suse.com>
2023-03-08 15:09:05 +0100

    Gather image data in postgresql
    Moving away from InfluxDB required several code changes in the image
    data gathering module.
    Currently we use postgresql together with postgrest API in order to
    retrieve or push data from the test runs.
    
    - ticket: https://progress.opensuse.org/issues/124595
    - VR: http://kepler.suse.cz/tests/20413#

--- tests/jeos/image_info.pm
+++ tests/jeos/image_info.pm
@@ -19,86 +19,8 @@ use serial_terminal 'select_serial_terminal';
 use version_utils qw(is_sle is_opensuse is_openstack);
 use mmapi qw(get_current_job_id);
 use Cwd;
+use db_utils qw(push_image_data_to_db check_postgres_db);
 
-my $image;
-my $job_url;
-my $db_log = "/tmp/influxdb.txt";
-my $influxdb_server = get_var('INFLUXDB_SERVER');
-
-sub check_db {
-    # Check if the running job can push data to the Data Base
-    # We only allow this on certain conditions:
-    #  - The job must contain INFLUXDB_SERVER, _SECRET_INFLUXDB_USER and _SECRET_INFLUXDB_PWD
-    #  - The job shouldn't be a verification run
-    #  - The job must be executed from OSD or O3
-    return 0 unless ($influxdb_server);
-    my $influxdb_user = get_var('_SECRET_INFLUXDB_USER');
-    my $influxdb_pwd = get_var('_SECRET_INFLUXDB_PWD');
-    unless ($influxdb_user || $influxdb_pwd) {
-        record_info('INFO', 'INFLUXDB_SERVER variable is set but "_SECRET_INFLUXDB_USER" and "_SECRET_INFLUXDB_PWD" are missing.');
-        return 0;
-    }
-
-    # CASEDIR var is always set, and if when no specified, the value is "sle" for OSD and "opensuse" for O3
-    return 0 if (get_required_var('CASEDIR') !~ m/^sle$|^opensuse$|^(sle|leap)-micro$/);
-
-    my $openqa_host = get_required_var('OPENQA_HOSTNAME');
-    if ($openqa_host =~ /openqa1-opensuse|openqa.opensuse.org/) {    # O3 hostname
-        $job_url = 'https://openqa.opensuse.org/tests/' . get_current_job_id();
-    }
-    elsif ($openqa_host =~ /openqa.suse.de/) {    # OSD hostname
-        $job_url = 'https://openqa.suse.de/tests/' . get_current_job_id();
-    }
-    return 0 unless ($job_url);
-    # Store credentials in a file to be used by curl commands
-    script_run("read -s influx_creds", 0);
-    type_password("-u $influxdb_user:$influxdb_pwd\n");
-    assert_script_run('echo $influx_creds > influxdb_conf');
-
-    # Check if the data for this image has been already published before.
-    # This will avoid publishing the same data if a job is restarted.
-    my $query = 'curl -K influxdb_conf -i -G "http://' . $influxdb_server . ':8086/query?db=data"';
-    $query .= ' --data-urlencode "q=SELECT \"value\" FROM \"image_size\" WHERE \"image\" = \'' . $image . '\'"';
-    script_run("echo '$query' | tee -a $db_log");
-    my $query_output = script_output("$query 2>&1 | tee -a $db_log", proceed_on_failure => 1);
-
-    # Successful query should return 'HTTP/1.1 200 OK'
-    # Empty records will return  '{"results":[{"statement_id":0}]}'
-    # Existing records will return '{"results":[{"statement_id":0,"series":[{"name":"size","columns":["time","value"],"values"' ...
-    if ($query_output !~ /200 OK/) {
-        record_soft_failure("poo#110221 - There has been a problem with the query.");
-        return 0;
-    } elsif ($query_output =~ /values/) {
-        record_info('DB skip', "Skip pushing the data to the DB. There is already a DB record for this image.");
-        return 0;
-    }
-    record_info('DB ok', "Access is OK and the data will be pushed to the DB.\nquery = $query\nresult = \n$query_output");
-    return 1;
-}
-
-sub push_to_influxdb {
-    my ($self, %args) = @_;
-
-    my $query = 'curl -K influxdb_conf -i -XPOST "http://' . $influxdb_server . ':8086/write?db=data"';
-    $query .= ' --data-binary "' . $args{table};
-    $query .= ',distri=' . get_required_var('DISTRI');
-    $query .= ',version=' . get_required_var('VERSION');
-    $query .= ',arch=' . get_required_var('ARCH');
-    $query .= ',flavor=' . get_required_var('FLAVOR');
-    $query .= ',build=' . get_required_var('BUILD');
-    $query .= ',image=' . $image;
-    $query .= ',job_url=' . $job_url;
-    if (ref $args{extra} eq 'HASH') {
-        foreach (keys %{$args{extra}}) {
-            $query .= ',' . $_ . '=' . $args{extra}->{$_};
-        }
-    }
-    $query .= ' value=' . $args{value} . '"';
-    script_run("echo '$query' | tee -a $db_log");
-    my $query_output = script_output("$query 2>&1 | tee -a $db_log", proceed_on_failure => 1);
-    # if successful push, it should return 'HTTP/1.1 204 No Content'
-    record_soft_failure("poo#110221 - There has been a problem pushing data to InfluxDB.") unless ($query_output =~ /(?=.*204 No Content)/);
-}
 
 sub run {
     my $self = shift;
@@ -108,7 +30,7 @@ sub run {
     my $hdd;
     my $image_size;
     my $dir = getcwd;
-    my $can_push_to_db = check_db;
+    my $image;
 
     # Get the image size
     if (is_openstack) {
@@ -139,25 +61,30 @@ sub run {
         $image = $hdd;
         $image_size = -s $hdd;
     }
+
+    # DB availability check
+    unless (check_postgres_db) {
+        record_soft_failure("poo#110221 - DB is not available!");
+        return 1;
+    }
+
     my $size_mb = $image_size / 1024 / 1024;
     record_info('Image', "Image: $image\nSize: $size_mb");
-    $self->push_to_influxdb(table => 'image_size', value => $size_mb, extra => {type => 'uncompressed'}) if $can_push_to_db;
+    push_image_data_to_db('minimal-vm', $image, $size_mb, table => 'size', type => 'uncompressed');
 
     # Get list of packages installed in the system
-    my $packages = script_output('rpm -qa --queryformat "%{SIZE} %{NAME}\n" |sort -n -r');
+    my $packages = script_output('rpm -qa --queryformat "%{SIZE} %{NAME}-%{VERSION}-%{RELEASE}\n" |sort -n -r');
     my @rpm_array = split(/\n/, $packages);
     my $num_packages = scalar @rpm_array;
     record_info('rpm total', "Total number of installed packages: $num_packages");
     record_info('rpm list', $packages);
-    if ($can_push_to_db) {
-        $self->push_to_influxdb(table => 'num_packages', value => $num_packages);
-        # Push each rpm size
-        my @lines = split /\n/, $packages;
-        foreach my $line (@lines) {
-            my ($size) = $line =~ /^\d+/g;
-            my ($rpm) = $line =~ /\s(.*)/g;
-            $self->push_to_influxdb(table => 'rpms', value => $size, extra => {rpm => $rpm});
-        }
+    push_image_data_to_db('minimal-vm', $image, $num_packages, table => 'num_packages');
+    # Push each rpm size
+    my @lines = split /\n/, $packages;
+    foreach my $line (@lines) {
+        my ($size) = $line =~ /^\d+/g;
+        my ($rpm) = $line =~ /\s(.*)/g;
+        push_image_data_to_db('minimal-vm', $image, $size, table => 'rpms', rpm => $rpm);
     }
 
     # Get size of directories except those that are irrelevant
@@ -168,13 +95,12 @@ sub run {
     $cmd .= ' /';
     my $dirs = script_output($cmd);
     record_info("dirs", "$cmd\n$dirs");
-    my @lines = split /\n/, $dirs;
-    if ($can_push_to_db) {
-        foreach my $line (@lines) {
-            my ($size) = $line =~ /^\d+/g;
-            my ($dir) = $line =~ /\s(.*)/g;
-            $self->push_to_influxdb(table => 'directories', value => $size, extra => {dir => $dir});
-        }
+    @lines = ();
+    @lines = split /\n/, $dirs;
+    foreach my $line (@lines) {
+        my ($size) = $line =~ /^\d+/g;
+        my ($dir) = $line =~ /\s(.*)/g;
+        push_image_data_to_db('minimal-vm', $image, $size, table => 'directories', dir => $dir);
     }
 
     # Get the size of different file types
@@ -190,23 +116,11 @@ sub run {
         my ($metadata_mb) = $metadata =~ /(\d+\.\d+)/g;
         my ($globalreserve) = $btrfs_summary =~ /GlobalReserve.*/g;
         my ($globalreserve_mb) = $globalreserve =~ /(\d+\.\d+)/g;
-        if ($can_push_to_db) {
-            $self->push_to_influxdb(table => 'btrfs_df', value => $data_mb, extra => {type => 'Data'});
-            $self->push_to_influxdb(table => 'btrfs_df', value => $system_mb, extra => {type => 'System'});
-            $self->push_to_influxdb(table => 'btrfs_df', value => $metadata_mb, extra => {type => 'Metadata'});
-            $self->push_to_influxdb(table => 'btrfs_df', value => $globalreserve_mb, extra => {type => 'GlobalReserve'});
-        }
+        push_image_data_to_db('minimal-vm', $image, $data_mb, table => 'btrfs_df', type => 'Data');
+        push_image_data_to_db('minimal-vm', $image, $system_mb, table => 'btrfs_df', type => 'System');
+        push_image_data_to_db('minimal-vm', $image, $metadata_mb, table => 'btrfs_df', type => 'Metadata');
+        push_image_data_to_db('minimal-vm', $image, $globalreserve_mb, table => 'btrfs_df', type => 'GlobalReserve');
     }
-}
-
-sub post_run_hook {
-    upload_logs($db_log, failok => 1);
-    set_var('_QUIET_SCRIPT_CALLS', 0);
-}
-
-sub post_fail_hook {
-    upload_logs($db_log, failok => 1);
-    set_var('_QUIET_SCRIPT_CALLS', 0);
 }
 
 1;
