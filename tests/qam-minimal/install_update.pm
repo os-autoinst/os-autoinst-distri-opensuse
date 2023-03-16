964a5ac40626c220c4b3520f34b0b6a3ed13b86c
Jozef Pupava <jpupava@suse.com>
2023-03-16 13:04:11 +0100

    Reboot machine before kexec on UEFI kernel update
    Reboot UEFI machine when kernel package is part of update
    kexec will fail 'kexec_file_load failed: Required key not available'
    Old kernel does not have key of new kernel
    
    https://progress.opensuse.org/issues/125423

--- tests/qam-minimal/install_update.pm
+++ tests/qam-minimal/install_update.pm
@@ -80,6 +80,14 @@ sub run {
 
         capture_state('between', 1);
 
+        # old kernel does not have key of new kernel
+        my $repos_to_check = join(' ', map { "-r $_" } split(',', $repo));
+        if (check_var('MACHINE', 'uefi') && script_run("zypper se $repos_to_check kernel") == 0) {
+            power_action('reboot', textmode => 1);
+            $self->wait_boot(bootloader_time => get_var('BOOTLOADER_TIMEOUT', 200));
+            select_serial_terminal;
+        }
+
         # check if latest kernel has valid secure boot signature
         if (check_var('MACHINE', 'uefi') && is_sle('12-sp1+')) {
             assert_script_run 'kexec -l -s /boot/vmlinuz --initrd=/boot/initrd --reuse-cmdline';
