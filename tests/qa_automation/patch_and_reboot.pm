2a3264983d3b364ae78568058c2b644c35fa36ad
rfan1 <richard.fan@suse.com>
2023-03-20 13:49:22 +0100

    Handle reboot on powerVM for patch_and_reboot test
    https://progress.opensuse.org/issues/125186

--- tests/qa_automation/patch_and_reboot.pm
+++ tests/qa_automation/patch_and_reboot.pm
@@ -24,7 +24,7 @@ use testapi;
 use serial_terminal 'select_serial_terminal';
 use Utils::Architectures;
 use qam;
-use Utils::Backends 'use_ssh_serial_console';
+use Utils::Backends qw(use_ssh_serial_console is_pvm);
 use power_action_utils qw(power_action);
 use version_utils qw(is_sle);
 use serial_terminal qw(add_serial_console);
@@ -63,7 +63,8 @@ sub run {
 
     # DESKTOP can be gnome, but patch is happening in shell, thus always force reboot in shell
     power_action('reboot', textmode => 1);
-    $self->wait_boot(bootloader_time => get_var('BOOTLOADER_TIMEOUT', 150));
+    reconnect_mgmt_console if is_pvm;
+    $self->wait_boot(ready_time => 600, bootloader_time => get_var('BOOTLOADER_TIMEOUT', 300));
 }
 
 sub test_flags {
