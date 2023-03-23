809dac00303558565aec702d8245c18d48ec023d
Michele Pagot <101880010+mpagot@users.noreply.github.com>
2023-03-23 10:29:46 +0100

    Qesap collect logs in case of failure (#16661)
    * Collect more logs from the cluster at failure
    * Add failok to all Ansible api
    * Move the wait_ssh code from the test module to the qesap lib 
    * Add UT for all of them

--- tests/sles4sap/qesapdeployment/deploy.pm
+++ tests/sles4sap/qesapdeployment/deploy.pm
@@ -10,27 +10,6 @@ use Mojo::Base 'publiccloud::basetest';
 use testapi;
 use qesapdeployment;
 
-sub wait_for_ssh {
-    my ($host) = @_;
-    my $timeout //= bmwqemu::scale_timeout(600);
-    my $start_time = time();
-    my $check_port = 1;
-
-    # Looping until reaching timeout or passing two conditions :
-    # - SSH port 22 is reachable
-    # - journalctl got message about reaching one of certain targets
-    while ((my $duration = time() - $start_time) < $timeout) {
-        if ($check_port) {
-            $check_port = 0 if (script_run('nc -vz -w 1 ' . $host . ' 22', quiet => 1) == 0);
-        }
-        else {
-            return $duration;
-        }
-        sleep 5;
-    }
-    die 'Timed out while waiting for ssh to be available in the CSP instances';
-}
-
 sub run {
     my $ret = qesap_execute(cmd => 'terraform', verbose => 1, timeout => 1800);
     die "'qesap.py terraform' return: $ret" if ($ret);
@@ -38,9 +17,15 @@ sub run {
     upload_logs($inventory, failok => 1);
     my @remote_ips = qesap_remote_hana_public_ips;
     record_info 'Remote IPs', join(' - ', @remote_ips);
-    foreach my $host (@remote_ips) { wait_for_ssh $host; }
+    foreach my $host (@remote_ips) {
+        die 'Timed out while waiting for ssh to be available in the CSP instances' if qesap_wait_for_ssh(host => $host) == -1;
+    }
     $ret = qesap_execute(cmd => 'ansible', cmd_options => '--profile', verbose => 1, timeout => 3600);
-    die "'qesap.py ansible' return: $ret" if ($ret);
+    if ($ret)
+    {
+        qesap_cluster_logs();
+        die "'qesap.py ansible' return: $ret";
+    }
 }
 
 sub test_flags {
@@ -50,7 +35,8 @@ sub test_flags {
 sub post_fail_hook {
     my ($self) = shift;
     qesap_upload_logs();
-    qesap_execute(cmd => 'ansible', cmd_options => '-d', verbose => 1, timeout => 300);
+    my $inventory = qesap_get_inventory(get_required_var('PUBLIC_CLOUD_PROVIDER'));
+    qesap_execute(cmd => 'ansible', cmd_options => '-d', verbose => 1, timeout => 300) unless (script_run("test -e $inventory"));
     qesap_execute(cmd => 'terraform', cmd_options => '-d', verbose => 1, timeout => 1200);
     $self->SUPER::post_fail_hook;
 }
--- tests/sles4sap/qesapdeployment/test_cluster.pm
+++ tests/sles4sap/qesapdeployment/test_cluster.pm
@@ -18,11 +18,12 @@ sub run {
 
     my $chdir = qesap_get_terraform_dir();
     assert_script_run("terraform -chdir=$chdir output");
-    qesap_ansible_cmd(cmd => $_, provider => $prov, $_) for ('pwd', 'uname -a', 'cat /etc/os-release');
+    qesap_ansible_cmd(cmd => $_, provider => $prov) for ('pwd', 'uname -a', 'cat /etc/os-release');
     qesap_ansible_cmd(cmd => 'ls -lai /hana/', provider => $prov, filter => 'hana');
     my $cmr_status = qesap_ansible_script_output(cmd => 'crm status', provider => $prov, host => 'vmhana01', root => 1);
     record_info("crm status", $cmr_status);
     qesap_ansible_cmd(cmd => $crm_mon_cmd, provider => $prov, filter => 'vmhana01');
+    qesap_cluster_logs();
 }
 
 sub post_fail_hook {
