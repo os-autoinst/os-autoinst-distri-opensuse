070ae52fdb0d7ada7a596d7892b57ab93b06d8eb
Paolo Stivanin <info@paolostivanin.com>
2023-04-04 16:12:43 +0200

    fixfiles: correctly parse path with spaces

--- tests/security/selinux/fixfiles.pm
+++ tests/security/selinux/fixfiles.pm
@@ -23,30 +23,24 @@ sub run {
     # test `fixfiles check` can print any incorrect file context labels
     assert_script_run("fixfiles check > $file_output 2>&1", timeout => 300);
 
-    # pick up a test file to test
-    my $file_info = script_output("grep -i 'Would relabel' $file_output | tail -1");
-    my $file_name = script_output("echo $file_info | cut -d ' ' -f3");
-    my $fcontext_pre = script_output("echo $file_info | cut -d ' ' -f5");
-    my $fcontext_post = script_output("echo $file_info | cut -d ' ' -f7");
+    # pick up a test file to test. We need to use sed+awk to deal with potential spaces in the file path.
+    my $file_info = script_output("grep -i 'Would relabel' $file_output | tail -1 | sed 's/Would relabel //'");
+    my $file_name = script_output("echo $file_info | awk \'{split(\$0, array, \" from \"); print array[1]}\'");
+    my $fcontext_pre = script_output("echo $file_info | cut -d ' ' -f1");
+    my $fcontext_post = script_output("echo $file_info | cut -d ' ' -f3");
 
     # test `fixfiles restore`: run fixfiles restore on the test file and check the results
     $self->fixfiles_restore("$file_name", "$fcontext_pre", "$fcontext_post");
 
     # test `fixfiles verify/check`: to double confirm, there should be nothing to do with $file_name
     my $script_output = script_output("fixfiles verify $file_name", proceed_on_failure => 1);
-    # On ALP, there is always a note about excluded fixfiles directory overlay
-    if (is_alp) {
-        $script_output =~ s/skipping the directory \/var\/lib\/overlay//;
-    }
-    if ($script_output) {
+    if ($script_output =~ m/$file_name/) {
         record_info("ERROR", "verify $file_name, it is not well restored: $script_output", result => "fail");
         $self->result("fail");
     }
+
     $script_output = script_output("fixfiles check $file_name", proceed_on_failure => 1);
-    if (is_alp) {
-        $script_output =~ s/skipping the directory \/var\/lib\/overlay//;
-    }
-    if ($script_output) {
+    if ($script_output =~ m/$file_name/) {
         record_info("ERROR", "check $file_name, it is not well restored: $script_output", result => "fail");
         $self->result("fail");
     }
