f507676faf05a3aa3b418c82e02d051de92d3f16
Rainer Koenig <rainer.konig@suse.com>
2023-04-06 11:01:00 +0200

    ALP test profile for automated installation
    - Related tickets: https://progress.opensuse.org/issues/126857
      (also included parts of): https://progress.opensuse.org/issues/127349
    
    - Using `agama.auto` boot parameter to launch the automatic installation
    - Currently using a shell skript to execute agama commands

---
+++ data/yam/agama/auto/alp.sh
@@ -0,0 +1,8 @@
+set -ex
+
+/usr/bin/agama config set software.product=ALP-Bedrock
+/usr/bin/agama config set user.userName=joe user.password=doe
+/usr/bin/agama config set root.password=nots3cr3t
+/usr/bin/sleep 30
+/usr/bin/agama install
+/sbin/reboot
