063dfbfbf946ff2ca0588ac294b96db4f1d1819d
yarunachalam <yarunachalam@suse.com>
2023-04-25 01:45:20 +0000

    Add details to the programd and cleanup debug stmt
    Program Header
    cleanup
    cleanup azure_runcmd.sh

--- data/publiccloud/azure_lib_fn.sh
+++ data/publiccloud/azure_lib_fn.sh
@@ -1,25 +1,28 @@
+##################################################################################
 # File: azure_lib_fn.sh
 # Description: Holds shared functions for azcli testing
-# Funtions:
+# Functions:
 #   cmd_status: executes the given command and stores the status of the
 #               execution in test_status array.
-#   final_exit: create azure_cli testing report from test_status array.
-#
+#   cli_test_report: create azure_cli testing report from test_status array.
+##################################################################################
+
 # GLOBAL VARIABLES
+# TEST_STATUS stores testname and pass/fail status
 declare -A TEST_STATUS
-final_exit_status=0
 
 ##################################################################################
-#Description: save pass/fail status result in global TEST_STATUS associative array
-#Globals : TEST_STATUS
-#Arguments : test_name and command
-#Outputs: save pass/fail to TEST_STATUS array
-#Errors: None
-#Exits : when no parameter
+# Globals : TEST_STATUS
+# Description: save pass/fail status result in global TEST_STATUS associative array
+# Arguments : test_name and command
+# Outputs: save pass/fail to TEST_STATUS array
+#          prints test name and test command
+# Errors: None
+# Exits : when passed fewer than two parameter
 ##################################################################################
 cmd_status()
 { 
- if (( $# < 0 )); then
+ if (( $# < 2 )); then
     echo "illegal number of parameter"
     echo "Usage: cmd_status <test nameG> <test command>"
     echo "Example: cmd_status az_account_set az account set -s account"
@@ -29,25 +32,27 @@ cmd_status()
  local test_name="${1}"
  shift 1 # consume the first argument
  echo "[Running test: '${test_name}' and command : '${@}']"
+ # Executing command 
  "${@}"
  exit_status=$?
- if (( exit_status > 0 )); then
-    echo EXIT_STATUS: ${exit_status}
-    final_exit_status=1
- fi
-TEST_STATUS[${test_name}]=${exit_status}
+ TEST_STATUS[${test_name}]=${exit_status}
 }
 
 ########################################################################
-#Description: print azure_cli tests report status
-#Arguments : none
-#Outputs: Report cli command test status
+# Description: print azure_cli tests report status
+# Globals : TEST_STATUS
+# Arguments : none
+# Outputs: Report cli command test status
+# Errors: None
+# Exits : 0 if all test pass
+#         1 if any test fails
 ########################################################################
 cli_test_report()
 {
 tcmd=0
 pcmd=0
 fcmd=0
+final_exit_status=0
  for ind in "${!TEST_STATUS[@]}"
  do 
     let "tcmd+=1"
@@ -55,11 +60,12 @@ fcmd=0
     then
       echo "  Test ${ind} FAILED "
       let "fcmd+=1"
+      final_exit_status=1
     else
       echo "  Test ${ind} Passed "
       let "pcmd+=1"
     fi
  done
 echo "${tcmd} tests run, ${pcmd} tests passed and ${fcmd} failed"
-exit $final_exit_status
+exit ${final_exit_status}
 }
