a0e32961e96027db981b2c35f6bd88299cd56ed0
lpalovsky <lpalovsky@suse.com>
2023-04-28 15:33:36 +0200

    Remove static sbd delay value

--- tests/sles4sap/publiccloud/hana_sr_test_secondary.pm
+++ tests/sles4sap/publiccloud/hana_sr_test_secondary.pm
@@ -11,46 +11,46 @@ use warnings FATAL => 'all';
 use sles4sap_publiccloud;
 use testapi;
 use serial_terminal 'select_serial_terminal';
+use Time::HiRes 'sleep';
 
 sub run {
     my ($self, $run_args) = @_;
     $self->{instances} = $run_args->{instances};
     my $hana_start_timeout = bmwqemu::scale_timeout(600);
     # $site_b = $instance of secondary instance located in $run_args->{$instances}
-    croak("site_b is missing or undefined in run_args") if (!$run_args->{site_b});
+    croak('site_b is missing or undefined in run_args') if (!$run_args->{site_b});
     my $site_b = $run_args->{site_b};
     select_serial_terminal;
 
     # Switch to control Site B (currently replica mode)
     $self->{my_instance} = $site_b;
-    my $cluster_status = $self->run_cmd(cmd => "crm status");
-    record_info("Cluster status", $cluster_status);
+    my $cluster_status = $self->run_cmd(cmd => 'crm status');
+    record_info('Cluster status', $cluster_status);
     # Check initial state: 'site B' = replica mode
     die("Site B '$site_b->{instance_id}' is NOT in replication mode.") if
       $self->get_promoted_hostname() eq $site_b->{instance_id};
 
     # Stop DB
     # check variable DB_ACTION in case of separate usage of the test.
-    my $db_action = get_var("DB_ACTION", $run_args->{hana_test_definitions}{$self->{name}});
-    croak("Database action unknown or not defined.") if ($db_action !~ /^(stop|kill|crash)$/);
+    my $db_action = get_var('DB_ACTION', $run_args->{hana_test_definitions}{$self->{name}});
+    croak('Database action unknown or not defined.') if ($db_action !~ /^(stop|kill|crash)$/);
 
-    if ($db_action eq "crash") {
+    if (($db_action eq 'crash') && get_var('HA_SBD_START_DELAY')) {
         # Setup sbd delay in case of crash OS to prevent cluster starting too quickly after reboot.
-        $self->setup_sbd_delay("30s");
-        record_info("Crash DB", "Crashing OS on Site B ('$site_b->{instance_id}')");
+        $self->setup_sbd_delay(get_var('HA_SBD_START_DELAY'));
+        record_info('Crash DB', "Crashing OS on Site B ('$site_b->{instance_id}')");
     }
     else {
-        my $action = $db_action eq "stop" ? "stopp" : $db_action;
+        # 'stopp' is not a typo - 'ing' is appended later
+        my $action = $db_action eq 'stop' ? 'stopp' : $db_action;
         record_info(ucfirst($db_action) . ' DB', ucfirst($action) . "ing Site B ('$site_b->{instance_id}')");
     }
-
     $self->stop_hana(method => $db_action);
+    $self->{my_instance}->wait_for_ssh(username => 'cloudadmin');
 
-    if ($db_action eq "crash") {
-        $self->{my_instance}->wait_for_ssh(username => 'cloudadmin');
-        sleep 10;
-        $self->wait_for_pacemaker();
-    }
+    # SBD delay is active only after reboot
+    sleep $self->sbd_delay_formula if $db_action eq "crash";
+    $self->wait_for_pacemaker();
 
     # wait for DB to start with resources
     $self->is_hana_online(wait_for_start => 'true');
