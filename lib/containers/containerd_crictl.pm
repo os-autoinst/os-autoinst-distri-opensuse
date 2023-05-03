f16cd6647b869ca134514b1197566dc00e6fa9b6
Fabian Vogt <fvogt@suse.de>
2023-05-03 17:28:29 +0200

    lib/containers: Use proper packages in tests
    Don't download random garbage from the internet, instead test what we
    actually distribute.

--- lib/containers/containerd_crictl.pm
+++ lib/containers/containerd_crictl.pm
@@ -11,18 +11,24 @@ use Mojo::Base 'containers::engine';
 use testapi;
 use containers::common 'install_containerd_when_needed';
 use containers::utils 'registry_url';
+use utils qw(zypper_call);
+use version_utils qw(is_leap is_sle);
 has runtime => 'crictl';
 
 sub init {
     install_containerd_when_needed();
 
-    # The crictl validation test suite is a plug-in required for this test and needs to be installed from an external source.
-    my $version = get_var('CONTAINERS_CRICTL_VERSION', 'v1.23.0');
-    my $url = "https://github.com/kubernetes-sigs/cri-tools/releases/download/$version/crictl-$version-linux-amd64.tar.gz";
-    my $filename = "/tmp/crictl-$version-linux-amd64.tar.gz";
-    assert_script_run("curl -L $url -o $filename");
-    assert_script_run("tar zxvf $filename -C /usr/local/bin");
-    assert_script_run("rm $filename");
+    unless (is_sle || is_leap) {
+        zypper_call("in cri-tools");
+    } else {
+        # The crictl validation test suite is a plug-in required for this test and needs to be installed from an external source.
+        my $version = get_var('CONTAINERS_CRICTL_VERSION', 'v1.23.0');
+        my $url = "https://github.com/kubernetes-sigs/cri-tools/releases/download/$version/crictl-$version-linux-amd64.tar.gz";
+        my $filename = "/tmp/crictl-$version-linux-amd64.tar.gz";
+        assert_script_run("curl -L $url -o $filename");
+        assert_script_run("tar zxvf $filename -C /usr/local/bin");
+        assert_script_run("rm $filename");
+    }
     assert_script_run "curl " . data_url('containers/crictl.yaml') . " -o /etc/crictl.yaml";
     assert_script_run('crictl info');
 }
