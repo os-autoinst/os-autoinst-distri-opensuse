0f3d12767f18df14f9d76610b11d2889167476d1
Jonathan Rivrain <JRivrain@suse.com>
2023-05-18 14:56:02 +0200

    k

--- lib/yam/agama/agama_base.pm
+++ lib/yam/agama/agama_base.pm
@@ -8,15 +8,51 @@ package yam::agama::agama_base;
 use base 'opensusebasetest';
 use strict;
 use warnings;
+use distribution;
 use testapi 'select_console';
-use y2_base 'save_upload_y2logs';
 use Utils::Logging 'save_and_upload_log';
+#use y2_base 'save_upload_y2logs';
+use network_utils qw(can_upload_logs recover_network);
+ 
+sub save_upload_y2logs_without_investigation {
+    my (%args) = @_;
+
+    return if (get_var('NOLOGS') || get_var('Y2LOGS_UPLOADED'));
+    $args{suffix} //= '';
+
+    # Do not test/recover network if collect from installation system, as it won't work anyway with current approach
+    # Do not recover network on non-qemu backend, as not implemented yet
+    $args{no_ntwrk_recovery} //= (get_var('BACKEND') !~ /qemu/);
+
+    # Try to recover network if cannot reach gw and upload logs if everything works
+    if (can_upload_logs() || (!$args{no_ntwrk_recovery} && recover_network())) {
+        script_run 'sed -i \'s/^tar \(.*$\)/tar --warning=no-file-changed -\1 || true/\' /usr/sbin/save_y2logs';
+        my $filename = "/tmp/y2logs$args{suffix}.tar" . get_available_compression();
+        script_run "save_y2logs $filename", 180;
+        upload_logs($filename, failok => 1);
+    } else {    # Redirect logs content to serial
+        script_run("journalctl -b --no-pager -o short-precise > /dev/$serialdev");
+        script_run("dmesg > /dev/$serialdev");
+        script_run("cat /var/log/YaST/y2log > /dev/$serialdev");
+    }
+    save_screenshot();
+}
+
+sub save_upload_y2logs {
+    my ($self, %args) = @_;
+    $self->save_upload_y2logs_without_investigation(%args);
+
+    # We skip parsing yast2 logs in each installation scenario, but only if
+    # test has failed or we want to explicitly identify failures
+    $self->investigate_yast2_failure() unless $args{skip_logs_investigation};
+}
 
 sub post_fail_hook {
+    my ($self) = @_;
     $testapi::password = 'linux';
     select_console 'root-console';
     Utils::Logging::save_and_upload_log('journalctl -u agama-auto', '/tmp/agama-auto-log.txt');
-    y2_base::save_upload_y2logs;
+    save_upload_y2logs;
 }
 
 1;
