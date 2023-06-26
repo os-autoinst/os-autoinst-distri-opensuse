409c1242931c3ed841e0657493252ffeebd19ffe
Sudarshan Mhalas <sudarshan@smhalas.udp.ovpn1.nue.suse.de>
2023-06-26 17:34:26 +0530

    Added a test module which is a simple hello_world program to test workflow of openqa.

---
+++ tests/installation/hello_world.pm
@@ -0,0 +1,39 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: This test module is a simple hello world program for 
+# testing the workflow of openqa.
+
+# Maintainer: Sudarshan Mhalas <sudarshan.mhalas@suse.com>
+
+package helloworld;
+
+use strict;
+use warnings;
+use base "opensusebasetest";
+use testapi;
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = $class->SUPER::new($args);
+    return $self;
+}
+
+sub run {
+    my ($self) = @_;
+
+    if (get_var('HW')) {
+        # Print a greeting message
+        record_info("Hello World", "********** Hello World! **********");
+    } else {
+        record_info("Hello World", "Hello World message skipped");
+    }
+}
+
+sub test_flags {
+    return { fatal => 1 };
+}
+
+1;
