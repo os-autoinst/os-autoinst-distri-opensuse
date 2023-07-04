da5fdb0b687346954dc9eded82632198fe2bded3
openQA web UI <openqa@yutao-qe1>
2023-07-04 16:43:43 +0800

    Enhance how we open/close YaST modules in containers to detect potential problems

--- tests/yam/yast_modules/control_center.pm
+++ tests/yam/yast_modules/control_center.pm
@@ -10,12 +10,15 @@
 use base 'y2_installbase';
 use strict;
 use warnings;
-use testapi qw(select_console enter_cmd wait_still_screen save_screenshot get_var);
+use y2_module_consoletest;
+use testapi qw(select_console wait_serial save_screenshot get_var);
 
 sub run {
-    my $podman_cmd = "podman container runlabel run registry.opensuse.org/suse/alp/workloads/tumbleweed_containerfiles/suse/alp/workloads/yast-mgmt-ncurses-test-api";
     select_console('root-console');
-    enter_cmd(get_var('YUI_PARAMS') . " $podman_cmd");
+
+    my $podman_cmd = " podman container runlabel run registry.opensuse.org/suse/alp/workloads/tumbleweed_containerfiles/suse/alp/workloads/yast-mgmt-ncurses-test-api;";
+    my $module_name = y2_module_consoletest::yast2_console_exec(extra_vars => get_var('YUI_PARAMS'), podman => $podman_cmd);
+    save_screenshot();
     my $control_center = $testapi::distri->get_control_center();
     my $release_notes = $testapi::distri->get_release_notes();
     $control_center->open_release_notes();
@@ -23,6 +26,8 @@ sub run {
     $release_notes->close();
     save_screenshot();
     $control_center->quit();
+    wait_serial("$module_name-0", timeout => 60) ||
+      die "Fail! $podman_cmd is not closed or non-zero code returned.";
 }
 
 1;
