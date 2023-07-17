bc733457384c335074cca12e4ed983d2f94af24f
Fabian Vogt <fvogt@suse.de>
2023-07-17 18:55:13 +0200

    Update bootloader_s390.pm

--- tests/installation/bootloader_s390.pm
+++ tests/installation/bootloader_s390.pm
@@ -297,8 +297,6 @@ sub run {
     }
 
     my $exception = $@;
-
-    # add y2start/log output if exception is happening
     die join("\n", '#' x 67, $exception, '#' x 67) if $exception;
 
     # activate console so we can call wait_serial later
