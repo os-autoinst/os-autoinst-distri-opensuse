4f20f63afdc5af9b88062ca2fe33c1a170a36158
openQA web UI <openqa@yutao-qe1>
2023-08-14 11:32:41 +0800

    Create new libyui-rest-api based test modules for ntp_config_settings

---
+++ tests/yam/ntp_configuration.pm
@@ -0,0 +1,22 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: This test module launches the installation from
+#          the installation settings page.
+
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use strict;
+use warnings;
+use base 'y2_installbase';
+use Test::Assert 'assert_matches';
+
+sub run {
+    my $ntp_settings = $testapi::distri->get_ntp_configuration()->get_ntp_servers();
+    assert_matches(qr/suse\.pool\.ntp\.org|10\.160\.0\.45\ 10\.160\.0\.44\ 10\.160\.255\.254/, $ntp_settings, "NTP setting is not correct");
+    $testapi::distri->get_ntp_configuration()->press_next();
+}
+
+1;
