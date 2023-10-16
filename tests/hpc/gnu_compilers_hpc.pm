8b1222b57a5ec206821326fcc045f0fbe6659d1a
Ioannis Bonatakis <ybonatakis@suse.com>
2023-10-16 14:23:33 +0200

    Enhance validation of gcc version with regex
    Make the check more robust with a more precise regex. As a simple number it
    could much anything as it was before.
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/hpc/gnu_compilers_hpc.pm
+++ tests/hpc/gnu_compilers_hpc.pm
@@ -12,12 +12,17 @@ use Mojo::Base qw(hpcbase), -signatures;
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use utils;
+use version_utils;
 use Utils::Logging qw(export_logs);
 
 sub run ($self) {
     select_serial_terminal();
     my $version = get_var('GNU_COMPILERS_HPC_VERSION', '');
-    my $expected_version = $version ? $version : '7';
+    # When non-versioned gnu_compilers_hpc is installed
+    # The default gcc version for SLE12 is 4.8
+    # Since SLE15 the default is gcc7
+    my $default_version = check_version('>=15-SP0', get_var('VERSION'), qr/\d{2}(?:-sp\d)?/) ? '7' : '4.8';
+    my $expected_version = $version ? $version : $default_version;
 
     zypper_call("in gnu$version-compilers-hpc");
     type_string('pkill -u root', lf => 1);
@@ -30,7 +35,7 @@ sub run ($self) {
     zypper_call("in gnu$version-compilers-hpc-devel");
     assert_script_run 'module load gnu';
     assert_script_run qq{env |grep "PATH=/usr/lib/hpc/compiler/gnu/$expected_version/bin"};
-    assert_script_run qq{gcc --version | grep $expected_version};
+    assert_script_run qq{gcc --version | grep -E "^gcc \\(SUSE Linux\\) $expected_version\\.[0-9]*"};
 }
 
 sub post_fail_hook ($self) {
