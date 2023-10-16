1a0f682a31836cb47ca1ae0b3727a933232db38c
Ioannis Bonatakis <ybonatakis@suse.com>
2023-10-16 13:56:53 +0200

    Make gnu_compilers_hpc module to work with default package
    At least for all the SLE15 the default gnu_compilers_hpc should use gcc7. The
    tests for non-verioned package should work without the
    GNU_COMPILERS_HPC_VERSION by default.
    
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>

--- tests/hpc/gnu_compilers_hpc.pm
+++ tests/hpc/gnu_compilers_hpc.pm
@@ -16,19 +16,21 @@ use Utils::Logging qw(export_logs);
 
 sub run ($self) {
     select_serial_terminal();
-    my $version = get_required_var('GNU_COMPILERS_HPC_VERSION');
+    my $version = get_var('GNU_COMPILERS_HPC_VERSION', '');
+    my $expected_version = $version ? $version : '7';
+
     zypper_call("in gnu$version-compilers-hpc");
     type_string('pkill -u root', lf => 1);
     $self->{serial_term_prompt} = '# ';
     serial_terminal::login('root', $self->{serial_term_prompt});
 
-    assert_script_run qq{module av | grep gnu/$version};
+    assert_script_run qq{module av | grep gnu/$expected_version};
     assert_script_run 'module load gnu';
-    assert_script_run qq{env |grep "MODULEPATH=/usr/share/lmod/moduledeps/gnu-$version"};
+    assert_script_run qq{env |grep "MODULEPATH=/usr/share/lmod/moduledeps/gnu-$expected_version"};
     zypper_call("in gnu$version-compilers-hpc-devel");
     assert_script_run 'module load gnu';
-    assert_script_run qq{env |grep "PATH=/usr/lib/hpc/compiler/gnu/$version/bin"};
-    assert_script_run qq{gcc --version | grep $version};
+    assert_script_run qq{env |grep "PATH=/usr/lib/hpc/compiler/gnu/$expected_version/bin"};
+    assert_script_run qq{gcc --version | grep $expected_version};
 }
 
 sub post_fail_hook ($self) {
