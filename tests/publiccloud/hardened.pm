31543fb122b7cd9555614bac5aef1d29af874db9
Ricardo Branco <rbranco@suse.de>
2023-10-17 13:08:54 +0200

    pc: Add workaround for https://github.com/OpenSCAP/openscap/issues/1796

--- tests/publiccloud/hardened.pm
+++ tests/publiccloud/hardened.pm
@@ -22,15 +22,18 @@ sub run {
     assert_script_run('sudo grep always,exit /etc/audit/rules.d/access.rules /etc/audit/rules.d/delete.rules');
     # Check that at least one account has password age
     assert_script_run("sudo awk -F: '\$5 ~ /[0-9]/ { print \$1, \$5; }' /etc/shadow  | grep '[0-9]'");
-    # NOTE: Cannot run full evaluation with --fetch-remote-resources because of https://github.com/OpenSCAP/openscap/issues/1796
     assert_script_run("mkdir oscap");
     my $xml_path = "pub/projects/security/oval/suse.linux.enterprise.15.xml";
     # Downloaded file should have slashes replaced by hyphens
     my $xml_file = $xml_path =~ s/\//-/gr;
     assert_script_run("curl -o- https://ftp.suse.com/$xml_path.gz | gunzip -c > oscap/$xml_file", timeout => 300);
     my $ret = script_run("sudo oscap xccdf eval --report report.html --local-files oscap/ --profile pcs-hardening /usr/share/xml/scap/ssg/content/ssg-sle15-ds.xml", timeout => 300);
-    upload_logs("report.html");
-    record_soft_failure("bsc#1216088 - Public Cloud Hardened image fail SCAP test") if ($ret);
+    if ($ret == 137) {
+        record_soft_failure("gh#OpenSCAP/openscap#1796 - Killed because of OOM");
+    } else {
+        record_soft_failure("bsc#1216088 - Public Cloud Hardened image fail SCAP test") if ($ret != 0);
+        upload_logs("report.html");
+    }
 }
 
 1;
