806220919bd5477f624dbdb9cb6dc977e01efa65
lilyeyes <llzhao@suse.com>
2023-11-01 10:48:09 +0800

    Fix Azure broken cluster in qesap regression
    Fix Azure broken cluster in qesap regression:
    do a crm resource cleanup, wait for cs_wait_for_idle and then check crm status again
    TEAM-8442 - Azure: broken cluster in qesap regression as "Stopped: [ vmhana01 ]"

--- tests/sles4sap/qesapdeployment/test_cluster.pm
+++ tests/sles4sap/qesapdeployment/test_cluster.pm
@@ -19,26 +19,41 @@ sub run {
     my $chdir = qesap_get_terraform_dir(provider => $provider_setting);
     assert_script_run("terraform -chdir=$chdir output");
     my @remote_cmd = (
-        'pwd',
-        'uname -a',
+        'pwd', 'uname -a',
         'cat /etc/os-release',
         'sudo SUSEConnect --status-text',
-        'zypper ref',
-        'zypper lr',
-        'zypper in -f -y vim'
+        'zypper ref', 'zypper lr',
+        'zypper in -f -y vim',
+        'zypper -n in ClusterTools2'
     );
-    qesap_ansible_cmd(cmd => $_, provider => $provider_setting) for @remote_cmd;
+    qesap_ansible_cmd(cmd => $_, provider => $provider_setting, timeout => 300) for @remote_cmd;
     qesap_ansible_cmd(cmd => 'ls -lai /hana/', provider => $provider_setting, filter => 'hana');
-    my $cmr_status = qesap_ansible_script_output(cmd => 'crm status',
+    my $crm_status = qesap_ansible_script_output(
+        cmd => 'crm status',
         provider => $provider_setting,
         host => '"hana[0]"',
-        root => 1);
-    record_info("crm status", $cmr_status);
+        root => 1
+    );
+    record_info("crm status", $crm_status);
+    if ($provider_setting eq 'AZURE') {
+        if (cluster_status_matches_regex($crm_status)) {
+            record_info('Retry', 'Found issue, do crm resource cleanup and retry');
+            qesap_ansible_cmd(cmd => 'sudo crm resource cleanup', provider => $provider_setting, filter => 'hana');
+            qesap_ansible_cmd(cmd => 'cs_wait_for_idle --sleep 5', provider => $provider_setting, filter => 'hana');
+            $crm_status = qesap_ansible_script_output(
+                cmd => 'crm status',
+                provider => $provider_setting,
+                host => '"hana[0]"',
+                root => 1
+            );
+            record_info('Retry crm status', $crm_status);
+            die 'Cluster resources throwing errors' if cluster_status_matches_regex($crm_status);
+        }
+    }
+
     qesap_ansible_cmd(cmd => $crm_mon_cmd, provider => $provider_setting, filter => '"hana[0]"');
     qesap_cluster_logs();
-    if ($provider_setting eq 'AZURE') {
-        die 'Cluster resources throwing errors' if cluster_status_matches_regex($cmr_status);
-    }
+
     if ($provider_setting eq 'AZURE') {
         if (get_var("QESAPDEPLOY_IBSMIRROR_RESOURCE_GROUP")) {
             my $rg = qesap_az_get_resource_group();
@@ -47,7 +62,8 @@ sub run {
             qesap_add_server_to_hosts(name => 'download.suse.de', ip => get_required_var("QESAPDEPLOY_IBSMIRROR_IP"));
             qesap_az_vnet_peering_delete(source_group => $rg, target_group => $ibs_mirror_rg);
         }
-    } elsif ($provider_setting eq 'EC2') {
+    }
+    elsif ($provider_setting eq 'EC2') {
         if (get_var("QESAPDEPLOY_IBSMIRROR_IP_RANGE")) {
             my $deployment_name = qesap_calculate_deployment_name('qesapval');
             my $vpc_id = qesap_aws_get_vpc_id(resource_group => $deployment_name);
@@ -69,7 +85,8 @@ sub post_fail_hook {
             my $ibs_mirror_rg = get_required_var('QESAPDEPLOY_IBSMIRROR_RESOURCE_GROUP');
             qesap_az_vnet_peering_delete(source_group => $rg, target_group => $ibs_mirror_rg);
         }
-    } elsif (check_var('PUBLIC_CLOUD_PROVIDER', 'EC2')) {
+    }
+    elsif (check_var('PUBLIC_CLOUD_PROVIDER', 'EC2')) {
         if (get_var("QESAPDEPLOY_IBSMIRROR_IP_RANGE")) {
             qesap_aws_delete_transit_gateway_vpc_attachment(name => qesap_calculate_deployment_name('qesapval') . '*');
         }
