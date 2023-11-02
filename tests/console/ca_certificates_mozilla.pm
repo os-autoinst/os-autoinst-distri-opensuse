ad381b210c2f6866fc5dfcbc7a10375e7b1b12e9
Andrea Manzini <andrea.manzini@suse.com>
2023-11-02 14:26:45 +0100

    Explicit servername in ca-certificates test

--- tests/console/ca_certificates_mozilla.pm
+++ tests/console/ca_certificates_mozilla.pm
@@ -1,13 +1,13 @@
 # SUSE's openQA tests
 #
-# Copyright 2018-2020 SUSE LLC
+# Copyright 2018-2023 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
 # Package: ca-certificates-mozilla openssl
 # Summary: Install ca-certificates-mozilla and test connection to a secure website
 # - install ca-certificates-mozilla and openssl
 # - connect to static.opensuse.org:443 using openssl and verify that the return code is 0
-# Maintainer: Orestis Nalmpantis <onalmpantis@suse.de>
+# Maintainer: QE Core <qe-core@suse.de>
 
 use base "consoletest";
 use strict;
@@ -19,7 +19,8 @@ use utils 'zypper_call';
 sub run {
     select_serial_terminal;
     zypper_call 'in ca-certificates-mozilla openssl';
-    assert_script_run('echo "x" | openssl s_client -connect static.opensuse.org:443 | grep "Verify return code: 0"');
+    my $server = "static.opensuse.org";    # due to infra setup, need to pass explicit servername for older openssl
+    assert_script_run(qq[echo "x" | openssl s_client -connect $server:443 -servername $server | grep "Verify return code: 0"]);
 }
 
 1;
