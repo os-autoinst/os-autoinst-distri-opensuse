68b1d6bdfb7a0289c0052f92ded76ff8688bb24d
Andrea Manzini <andrea.manzini@suse.com>
2023-11-27 11:55:34 +0100

    New grub on 15SP6 don't need to type LUKS passphrase

--- tests/installation/boot_encrypt.pm
+++ tests/installation/boot_encrypt.pm
@@ -16,10 +16,11 @@ use testapi qw(check_var get_var record_info);
 use version_utils qw(is_leap is_sle is_leap_micro is_sle_micro is_alp);
 
 sub run {
-    # With newer grub2 (in TW only currently), entering the passphrase in GRUB2
+    # With newer grub2 (in TW and SLE 15SP6 currently), entering the passphrase in GRUB2
     # is enough. The key is passed on during boot, so it's not asked for
     # a second time.
-    return if is_boot_encrypted && !is_leap && !is_sle && !is_leap_micro && !is_sle_micro && !is_alp;
+    my $need_to_enter_passphrase = is_leap || is_sle('<15-sp6') || is_leap_micro || is_sle_micro || is_alp;
+    return if is_boot_encrypted && !$need_to_enter_passphrase;
 
     unlock_if_encrypted(check_typed_password => 1);
 }
