1a811c2e4e7391333d286b1b2ed7886d2f40d054
Paolo Stivanin <info@paolostivanin.com>
2023-12-14 11:14:08 +0100

    Refactor openjdk_fips and openjdk_ssh tests

---
+++ tests/fips/openjdk/prepare_env.pm
@@ -0,0 +1,42 @@
+# SUSE's openjdk fips tests
+#
+# Copyright 2024 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: prepare env for openjdk tests
+# Maintainer: QE Security <none@suse.de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use utils;
+use openjdktest;
+use version_utils qw(is_sle is_leap is_tumbleweed);
+
+sub run {
+    my $self = @_;
+    my $interactive_str = [
+        {
+            prompt => qr/Enter new password/m,
+            key => 'ret',
+        },
+        {
+            prompt => qr/Re-enter password/m,
+            key => 'ret',
+        },
+    ];
+
+    select_console "root-console";
+    zypper_call("in mozilla-nss-tools git-core");
+
+    assert_script_run("mkdir /etc/pki/nssdb");
+    script_run_interactive("certutil -d /etc/pki/nssdb -N", $interactive_str, 30);
+}
+
+sub test_flags {
+    return {no_rollback => 1};
+}
+
+1;
+
