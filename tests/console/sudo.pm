b4219513867662e8e199231df9bfb993a6e90061
phoenix <felix.niederwanger@suse.com>
2023-12-21 11:51:12 +0100

    Fix sudo on 15-SP4
    Fix for sudo on Azure for SLE 15-SP4, where the expected user is root.

--- tests/console/sudo.pm
+++ tests/console/sudo.pm
@@ -50,8 +50,8 @@ sub run {
     zypper_call 'in sudo expect';
     select_console 'user-console';
     # Defaults targetpw -> asks for root PW
-    my $exp_user = (is_azure && is_sle('>=15-SP4')) ? 'bernhard' : 'root';
-    assert_script_run("expect -c 'spawn sudo id -un;expect \"password for $exp_user\" {send \"$testapi::password\\r\";interact} default {exit 1}' | grep ^$exp_user");
+    my $exp_user = (is_azure && is_sle('>15-SP4')) ? 'bernhard' : 'root';
+    validate_script_output("expect -c 'spawn sudo id -un;expect \"password for $exp_user\" {send \"$testapi::password\\r\";interact} default {exit 1}'", sub { $_ !~ "^$exp_user\$" });
     select_console 'root-console';
     # Prepare a file with content '1' for later IO redirection test
     assert_script_run 'echo 1 >/run/openqa_sudo_test';
