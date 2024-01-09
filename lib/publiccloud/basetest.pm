os-autoinst/os-autoinst-distri-opensuse

commit 8c93916c4397494cb9076bee154f6a4ec11e72f7

Make region required

lib/publiccloud/basetest.pm
@@ -40,12 +40,12 @@ sub provider_factory {
 
         if ($args{service} eq 'ECR') {
             $provider = publiccloud::ecr->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'eu-central-1')
+                region => get_required_var('PUBLIC_CLOUD_REGION')
             );
         }
         elsif ($args{service} eq 'EKS') {
             $provider = publiccloud::eks->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'eu-central-1')
+                region => get_required_var('PUBLIC_CLOUD_REGION')
             );
         }
         elsif ($args{service} eq 'EC2') {
@@ -60,7 +60,7 @@ sub provider_factory {
         $args{service} //= 'AVM';
         if ($args{service} eq 'ACR') {
             $provider = publiccloud::acr->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'westeurope'),
+                region => get_required_var('PUBLIC_CLOUD_REGION'),
                 subscription => get_var('PUBLIC_CLOUD_AZURE_SUBSCRIPTION_ID'),
                 username => get_var('PUBLIC_CLOUD_USER', 'azureuser')
             );

lib/publiccloud/aws_client.pm
@@ -13,7 +13,7 @@ use testapi;
 use utils;
 use publiccloud::utils;
 
-has region => sub { get_var('PUBLIC_CLOUD_REGION', 'eu-central-1') };
+has region => sub { get_required_var('PUBLIC_CLOUD_REGION') };
 has aws_account_id => undef;
 has container_registry => sub { get_var("PUBLIC_CLOUD_CONTAINER_IMAGES_REGISTRY", 'suse-qec-testing') };
 has username => sub { get_var('PUBLIC_CLOUD_USER', 'ec2-user') };

lib/publiccloud/azure.pm
@@ -408,7 +408,7 @@ sub create_image_version {
     # execute test runs. For reasons of being concise we re-use the existing variable PUBLIC_CLOUD_REGION, but here
     # it can contain a comma-separated list of all regions, in which the uploaded image should be available
     # The $self->region is not used here as it contains only the first region from the list.
-    my $target_regions = get_var("PUBLIC_CLOUD_REGION", "westeurope");
+    my $target_regions = get_required_var("PUBLIC_CLOUD_REGION");
     $target_regions =~ s/,/ /g;    # CLI expects spaces as separation, not commas
 
     my $definition = $self->get_image_definition($resource_group, $gallery);

lib/publiccloud/azure_client.pm
@@ -14,7 +14,7 @@ use utils;
 use publiccloud::utils;
 
 has subscription => sub { get_var('PUBLIC_CLOUD_AZURE_SUBSCRIPTION_ID') };
-has region => sub { get_var('PUBLIC_CLOUD_REGION', 'westeurope') };
+has region => sub { get_required_var('PUBLIC_CLOUD_REGION') };
 has username => sub { get_var('PUBLIC_CLOUD_USER', 'azureuser') };
 has credentials_file_content => undef;
 has container_registry => sub { get_var('PUBLIC_CLOUD_CONTAINER_IMAGES_REGISTRY', 'suseqectesting') };

lib/publiccloud/gcp_client.pm
@@ -23,7 +23,7 @@ has storage_name => sub { get_var('PUBLIC_CLOUD_STORAGE_ACCOUNT', 'openqa-storag
 has project_id => sub { get_var('PUBLIC_CLOUD_GOOGLE_PROJECT_ID') };
 has account => sub { get_var('PUBLIC_CLOUD_GOOGLE_ACCOUNT') };
 has gcr_zone => sub { get_var('PUBLIC_CLOUD_GCR_ZONE', 'eu.gcr.io') };
-has region => sub { get_var('PUBLIC_CLOUD_REGION', 'europe-west1-b') };
+has region => sub { get_required_var('PUBLIC_CLOUD_REGION') };
 has username => sub { get_var('PUBLIC_CLOUD_USER', 'susetest') };
 
 sub init {

commit a908ed5ad55b16c4354cedcae9b366624f2fc6f9

Proper fix of regression caused by 8c93916
publiccloud::aws_client/azure_client needs to demand PUBLIC_CLOUD_REGION
due to other places where we do not want to have defaults and want tests
to fail when region is not defined so this is workaround to keep variable
required for publiccloud::aws_client/azure_client but not for cases where
we using publiccloud::eks/ecr/acr which also must to init
publiccloud::aws_client/azure_client and in this case we CAN NOT define it
on job level because publiccloud::aws_client will be initialized
together with publiccloud::azure_client so same variable will need to have two different values

lib/publiccloud/basetest.pm
@@ -39,14 +39,26 @@ sub provider_factory {
         $args{service} //= 'EC2';
 
         if ($args{service} eq 'ECR') {
-            $provider = publiccloud::ecr->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'eu-central-1')
-            );
+            # publiccloud::aws_client needs to demand PUBLIC_CLOUD_REGION due to other places where
+            # we don't want to have defaults and want tests to fail when region is not defined
+            # so this is workaround to keep variable required for publiccloud::aws_client
+            # but not for cases where we using publiccloud::ecr which also must to init publiccloud::aws_client
+            # and in this case we CAN NOT define it on job level because publiccloud::aws_client
+            # will be initialized together with publiccloud::azure_client so same variable will need
+            # to have two different values
+            set_var('PUBLIC_CLOUD_REGION', 'eu-central-1') unless get_var('PUBLIC_CLOUD_REGION');
+            $provider = publiccloud::ecr->new();
         }
         elsif ($args{service} eq 'EKS') {
-            $provider = publiccloud::eks->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'eu-central-1')
-            );
+            # publiccloud::aws_client needs to demand PUBLIC_CLOUD_REGION due to other places where
+            # we don't want to have defaults and want tests to fail when region is not defined
+            # so this is workaround to keep variable required for publiccloud::aws_client
+            # but not for cases where we using publiccloud::eks which also must to init publiccloud::aws_client
+            # and in this case we CAN NOT define it on job level because publiccloud::aws_client
+            # will be initialized together with publiccloud::azure_client so same variable will need
+            # to have two different values
+            set_var('PUBLIC_CLOUD_REGION', 'eu-central-1') unless get_var('PUBLIC_CLOUD_REGION');
+            $provider = publiccloud::eks->new();
         }
         elsif ($args{service} eq 'EC2') {
             $provider = publiccloud::ec2->new();
@@ -59,8 +71,15 @@ sub provider_factory {
     elsif ($args{provider} eq 'AZURE') {
         $args{service} //= 'AVM';
         if ($args{service} eq 'ACR') {
+            # publiccloud::azure_client needs to demand PUBLIC_CLOUD_REGION due to other places where
+            # we don't want to have defaults and want tests to fail when region is not defined
+            # so this is workaround to keep variable required for publiccloud::azure_client
+            # but not for cases where we using publiccloud::acr which also must to init publiccloud::azure_client
+            # and in this case we CAN NOT define it on job level because publiccloud::aws_client
+            # will be initialized together with publiccloud::azure_client so same variable will need
+            # to have two different values
+            set_var('PUBLIC_CLOUD_REGION', 'westeurope') unless get_var('PUBLIC_CLOUD_REGION');
             $provider = publiccloud::acr->new(
-                region => get_var('PUBLIC_CLOUD_REGION', 'westeurope'),
                 subscription => get_var('PUBLIC_CLOUD_AZURE_SUBSCRIPTION_ID'),
                 username => get_var('PUBLIC_CLOUD_USER', 'azureuser')
             );