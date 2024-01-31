d034a5f6a74ad24c1f7946f021ac0d1e0a6bd6fc
Michael Grifalconi <mgrifalconi@suse.com>
2024-01-31 08:16:40 +0100

    Remove double cleanup
    https://progress.opensuse.org/issues/154588

--- tests/console/python3_setuptools.pm
+++ tests/console/python3_setuptools.pm
@@ -46,7 +46,6 @@ sub run_tests ($python3_spec_release) {
     build_package($python_binary);
     http_install_test($version_number);
     local_install_test($version_number);
-    cleanup();
 }
 
 # Creating the source package with the name 'dist/user_package_setuptools-1.0.tar.gz' in the dist folder.
