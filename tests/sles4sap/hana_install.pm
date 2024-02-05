a9952c3f0d340451972904cd4a13a1bb1d1fce3f
Jan Kohoutek <jkohoutek@suse.com>
2024-02-05 14:49:34 +0100

    typos 2

--- tests/sles4sap/hana_install.pm
+++ tests/sles4sap/hana_install.pm
@@ -44,7 +44,7 @@ sub download_hana_assets_from_server {
     my $hana_location = data_url('ASSET_0');
     # Each HANA asset is about 16GB. A ten minute timeout assumes a generous
     # 27.3MB/s download speed. Adjust according to expected server conditions.
-    assert_script_run "wget -O -- - $hana_location | tar -xf -- -", timeout => $nettout;
+    assert_script_run "wget -O - $hana_location | tar -xf -", timeout => $nettout;
     # Skip checksum check if DISABLE_CHECKSUM is set, or if checksum file is not
     # part of the archive
     my $sap_chksum_file = 'MD5FILE.DAT';
@@ -56,7 +56,7 @@ sub download_hana_assets_from_server {
     assert_script_run "pushd $target";
     # If SAP provided MD5 sum file is present convert it to the md5sum format
     assert_script_run "[[ -f $sap_chksum_file ]] && awk '{print $2\" \"$1}' $target/$sap_chksum_file > $target/$chksum_file";
-    assert_script_run "md5sum -c --quiet -- $chksum_file", $nettout;
+    assert_script_run "md5sum -c --quiet $chksum_file", $nettout;
     assert_script_run "cd";
 }
 
