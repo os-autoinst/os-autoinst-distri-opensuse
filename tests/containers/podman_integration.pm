01ee4ffef02164b33d7fe0fee558ace4b16d6407
Ricardo Branco <rbranco@suse.de>
2024-02-06 19:13:08 +0100

    containers: Fix for skopeo not being available in SLEM 5.4

--- tests/containers/podman_integration.pm
+++ tests/containers/podman_integration.pm
@@ -42,8 +42,9 @@ sub run {
     }
 
     # Install tests dependencies
-    my @pkgs = qw(aardvark-dns bats catatonit jq make netavark netcat-openbsd openssl podman-remote python3-PyYAML socat skopeo sudo systemd-container);
+    my @pkgs = qw(aardvark-dns bats catatonit jq make netavark netcat-openbsd openssl podman-remote python3-PyYAML socat sudo systemd-container);
     push @pkgs, qw(apache2-utils buildah criu go gpg2) unless is_sle_micro;
+    push @pkgs, qw(skopeo) unless is_sle_micro('<5.5');
     if (is_transactional) {
         trup_call "-c pkg install -y @pkgs";
         check_reboot_changes;
