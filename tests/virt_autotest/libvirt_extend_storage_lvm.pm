e8c6facc310f63d022f42a07f8cb78b7e73fc915
Wayne Chen <wchen@suse.com>
2024-02-21 16:45:46 +0800

    Verify hard disk size before creating lvm
    Verify hard disk size is equal to or greater than 20G before creating
    lvm with it. This can help ensure test run proceed with suitable hard
    disk and increase test run stability. If there is no suitable disk to
    be used, fail test run immediately and give explicit error message.

--- tests/virt_autotest/libvirt_extend_storage_lvm.pm
+++ tests/virt_autotest/libvirt_extend_storage_lvm.pm
@@ -69,18 +69,33 @@ sub prepare_lvm_storage_pool_source {
     # Use a unused hard disk for LVM volumes
     $dev = "/dev/";
     foreach my $disk (@disks) {
-        if (script_run("set -o pipefail;findmnt -n -o SOURCE / | grep $disk") != 0) {
+        if (script_run("set -o pipefail;findmnt -n -o SOURCE / | grep $disk") != 0 and get_hard_disk_size($disk) >= 20) {
             $lvm_disk_name = $dev . $disk;
             last;
         }
     }
-    record_info "Assign a New Disk:", "$lvm_disk_name";
-    # Wipe Hard Disk Clean via dd for assigned a new full disk
-    wipe_hard_disk($lvm_disk_name);
-    ## About LVM volumes management
-    # Create a Volume Group (VG) with LVM named 'lvm_vg'
-    create_volume_group($lvm_disk_name);
-    return ($lvm_disk_name);
+
+    if ($lvm_disk_name) {
+        record_info "Assign a New Disk:", "$lvm_disk_name";
+        # Wipe Hard Disk Clean via dd for assigned a new full disk
+        wipe_hard_disk($lvm_disk_name);
+        ## About LVM volumes management
+        # Create a Volume Group (VG) with LVM named 'lvm_vg'
+        create_volume_group($lvm_disk_name);
+        return ($lvm_disk_name);
+    }
+    else {
+        record_info("Full information about block devices on host", script_output("lsblk --all --output-all"));
+        die("Failed to find spare hard disk with 20G or more size");
+    }
+}
+
+#Get disk size in gigabytes
+sub get_hard_disk_size {
+    my $hard_disk_name = shift;
+
+    my $hard_disk_size = script_output("blockdev --getsize64 /dev/$hard_disk_name");
+    return $hard_disk_size / (1024**3);
 }
 
 # Wipe Hard Disk Clean via dd
