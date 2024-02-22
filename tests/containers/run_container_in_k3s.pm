69b3f1033731895d8c3abe54d00af035526a8ed8
Marius Grossu <mgrossu@localhost.localdomain>
2024-02-22 15:10:19 +0100

    Move k3s part away from podman_pods

--- tests/containers/run_container_in_k3s.pm
+++ tests/containers/run_container_in_k3s.pm
@@ -16,7 +16,19 @@ use serial_terminal 'select_serial_terminal';
 use utils;
 use version_utils;
 use publiccloud::utils;
+use containers::utils qw(check_min_runtime_version);
+use Utils::Architectures qw(is_ppc64le);
 use containers::k8s qw(install_k3s uninstall_k3s apply_manifest wait_for_k8s_job_complete find_pods validate_pod_log);
+
+sub prepare_pod_yaml {
+    record_info('Prep', 'Generate the yaml from a pod');
+    my $image = "registry.suse.com/bci/bci-busybox:latest";
+    script_retry("podman pull $image", timeout => 300, delay => 60, retry => 3);
+    assert_script_run("podman container create -t --pod new:testing-pod $image");
+    assert_script_run("podman kube generate testing-pod | tee pod.yaml");
+    assert_script_run("grep 'image: $image' pod.yaml");
+    assert_script_run("podman pod rm testing-pod");
+}
 
 sub run {
     select_serial_terminal;
@@ -35,6 +47,17 @@ sub run {
     my $pod = find_pods("job-name=$job_name");
     validate_pod_log($pod, "SUSE Linux Enterprise Server");
     record_info('cmd', "Command `$cmd` successfully executed in the image.");
+
+    # Staging does not have access to repositories, only to DVD
+    # curl -sfL https://get.k3s.io is not supported on ppc poo#128456
+    if (check_min_runtime_version('4.4.0') && !is_staging && !is_ppc64le) {
+        prepare_pod_yaml();
+        record_info('Test', 'kube apply');
+        assert_script_run('podman kube apply --kubeconfig ~/.kube/config -f pod.yaml', timeout => 180);
+        assert_script_run('kubectl wait --timeout=600s --for=condition=Ready pod/testing-pod', timeout => 610);
+        validate_script_output('kubectl exec testing-pod -- cat /etc/os-release', sub { m/SUSE Linux Enterprise Server/ }, timeout => 300);
+    }
+
 }
 
 sub cleanup {
