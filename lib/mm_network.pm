5a944e4b34fcfb304e7afc4d890bcf2a42d0a79e
Dominik Heidler <dheidler@suse.de>
2024-02-23 13:25:16 +0100

    Actually validate network connectivity on NM MM setup
    Ticket: https://progress.opensuse.org/issues/155278

--- lib/mm_network.pm
+++ lib/mm_network.pm
@@ -237,7 +237,7 @@ sub restart_networking {
         assert_script_run 'nmcli networking off';
         assert_script_run 'nmcli networking on';
         # Wait until the connections are configured
-        assert_script_run 'nmcli networking connectivity check';
+        assert_script_run "until nmcli networking connectivity check | tee /dev/stderr | grep full; do sleep 10; done";
     } else {
         assert_script_run 'rcnetwork restart';
     }
