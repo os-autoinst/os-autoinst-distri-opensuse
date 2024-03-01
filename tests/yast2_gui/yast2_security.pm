a75ada937f81ae1683b7959f2133a9e23d86f824
Huajian.Luo <hluo@suse.com>
2024-03-01 21:41:27 +0800

    Apply the glitch workaround in yast2_security

--- tests/yast2_gui/yast2_security.pm
+++ tests/yast2_gui/yast2_security.pm
@@ -39,7 +39,7 @@ sub run {
     wait_screen_change { send_key "alt-o" };
 
     # Check previously set values + Login Settings
-    y2_module_guitest::launch_yast2_module_x11("security", match_timeout => 120);
+    y2_module_guitest::launch_yast2_module_x11("security", match_timeout => 120, apply_workaround => is_sle('>=15-SP4') ? 1 : 0);
     assert_and_click "yast2_security-pwd-settings";
     apply_workaround_poo124652('yast2_security-check-min-pwd-len-and-exp-days') if (is_sle('>=15-SP4'));
     assert_screen "yast2_security-check-min-pwd-len-and-exp-days";
@@ -62,7 +62,7 @@ sub run {
     wait_screen_change { send_key "alt-o" };
 
     # Check previously set values
-    y2_module_guitest::launch_yast2_module_x11("security", match_timeout => 120);
+    y2_module_guitest::launch_yast2_module_x11("security", match_timeout => 120, apply_workaround => is_sle('>=15-SP4') ? 1 : 0);
     assert_and_click "yast2_security-misc-settings";
     apply_workaround_poo124652('yast2_security-file-perms-secure') if (is_sle('>=15-SP4'));
     assert_screen "yast2_security-file-perms-secure";
