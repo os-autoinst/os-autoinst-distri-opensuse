6566386c67953179b50f7e1ac145bbdeeee9749c
Michael Moese <mmoese@suse.de>
2024-04-16 11:24:02 +0200

    Improvements to usb_nic.pm
    - Check for USB network interfaces before doing anything else
    - try pinging each neighbour of interface instead of the default gateway
    
    Co-authored-by: Martin Doucha <mdoucha@suse.cz>

--- tests/kernel/usb_nic.pm
+++ tests/kernel/usb_nic.pm
@@ -19,16 +19,31 @@ sub run {
 
     select_serial_terminal;
 
-    my $interface = script_output 'basename $(readlink /sys/class/net/* | grep usb )';
+    my $usb_net_devs = script_output('readlink /sys/class/net/* | grep usb', proceed_on_failure => 1);
+    die "no USB network interfaces found" unless $usb_net_devs ne "";
+
+    my $interface = script_output "basename $usb_net_devs | head -n1";
 
     assert_script_run "echo \"BOOTPROTO='dhcp'\" > /etc/sysconfig/network/ifcfg-$interface";
     assert_script_run "echo \"STARTMODE='auto'\" >> /etc/sysconfig/network/ifcfg-$interface";
 
     assert_script_run("ifup $interface -o debug", 60);
 
-    my $route = script_output "ip route show default";
-    my @ping_peer = $route =~ /default via (\S+)/;
-    assert_script_run "ping -I $interface -c 4 @ping_peer";
+    sleep 30;
+    my $inet = script_output("ip addr show dev $interface |  awk \'/inet / {split(\$0,a); print a[2]}\'", proceed_on_failure => 1);
+    die "no IP address configured" unless $inet ne "";
+
+    record_info("IP address(es)", "$inet");
+
+    my $neigh = script_output("ip neigh show dev $interface");
+    my $peer_count = 0;
+
+    while ($neigh =~ m/^(\S+)/mg) {
+        return unless script_run "ping -I $interface -c 4 $1";
+        $peer_count++;
+    }
+
+    record_info("Ping failed", "None of $peer_count peers responded to ping", result => 'fail');
 }
 
 sub test_flags {
