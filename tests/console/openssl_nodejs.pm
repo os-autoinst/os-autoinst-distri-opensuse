6f9b2d6e91008a094b7b49e9e3d0213604825be0
rfan1 <richard.fan@suse.com>
2024-04-29 22:58:46 -0400

    Enable source pool if no updated source package available
    https://progress.opensuse.org/issues/159609

--- tests/console/openssl_nodejs.pm
+++ tests/console/openssl_nodejs.pm
@@ -24,9 +24,17 @@ sub run {
     select_serial_terminal;
 
     my $os_version = generate_version();
+    my $source_repo;
+    if (zypper_call('se -t srcpackage nodejs', exitcode => [0, 104]) == 104) {
+        record_info('Source package not found', 'Enable the Source Pool');
+        set_var('ENABLE_SRC_REPO', 1);
+        $source_repo = script_output(q{zypper lr | grep Source-Pool | awk -F '|' '/Web_and_Scripting_Module/ {print $2}'});
+        zypper_call("mr -e $source_repo", exitcode => [0, 3]);
+    }
     assert_script_run 'wget --quiet ' . data_url('console/test_openssl_nodejs.sh');
     assert_script_run 'chmod +x test_openssl_nodejs.sh';
     assert_script_run "./test_openssl_nodejs.sh $os_version", 900;
+    zypper_call("mr -d $source_repo", exitcode => [0, 3]) if get_var('ENABLE_SRC_REPO', 0);
 }
 
 1;
