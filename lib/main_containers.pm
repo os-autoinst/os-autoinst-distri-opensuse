eea124e5eaa41d072f22834e2a5c2990fb001b80
Ricardo Branco <rbranco@suse.de>
2024-05-07 17:32:52 +0200

    containers: Introduce (SKOPEO|PODMAN|RUNC)_BATS_SKIP=all to skip module

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -292,9 +292,15 @@ sub load_container_tests {
     }
 
     if (get_var('PODMAN_BATS_SKIP')) {
-        loadtest 'containers/skopeo_integration' if (is_tumbleweed || is_microos || is_sle('>=15-SP4') || is_leap('>=15.4') || is_sle_micro('>=5.5'));
-        loadtest 'containers/podman_integration';
-        loadtest 'containers/runc_integration' if (is_tumbleweed || is_sle('>=15-SP4') || is_leap('>=15.4'));
+        if (!check_var('SKOPEO_BATS_SKIP', 'all')) {
+            loadtest 'containers/skopeo_integration' if (is_tumbleweed || is_microos || is_sle('>=15-SP4') || is_leap('>=15.4') || is_sle_micro('>=5.5'));
+        }
+        if (!check_var('PODMAN_BATS_SKIP', 'all')) {
+            loadtest 'containers/podman_integration';
+        }
+        if (!check_var('RUNC_BATS_SKIP', 'all')) {
+            loadtest 'containers/runc_integration' if (is_tumbleweed || is_sle('>=15-SP4') || is_leap('>=15.4'));
+        }
         return;
     }
 
