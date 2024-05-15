159d363025f344fbec2def0973726f625177278e
Ricardo Branco <rbranco@suse.de>
2024-05-15 10:34:10 +0200

    containers: Enable netavark & aardvark-dns upstream tests in SLE

--- tests/containers/aardvark_integration.pm
+++ tests/containers/aardvark_integration.pm
@@ -13,9 +13,10 @@ use serial_terminal qw(select_serial_terminal);
 use utils qw(script_retry);
 use containers::common;
 use containers::bats qw(install_bats enable_modules);
-use version_utils qw(is_sle);
+use version_utils qw(is_sle is_tumbleweed);
 
 my $test_dir = "/var/tmp";
+my $aardvark = "";
 my $aardvark_version = "";
 
 sub run_tests {
@@ -31,7 +32,7 @@ sub run_tests {
     script_run "rm test/$_.bats" foreach (@skip_tests);
 
     assert_script_run "echo $log_file .. > $log_file";
-    script_run "AARDVARK=/usr/libexec/podman/aardvark-dns bats --tap test | tee -a $log_file", 1200;
+    script_run "AARDVARK=$aardvark bats --tap test | tee -a $log_file", 1200;
     parse_extra_log(TAP => $log_file);
     assert_script_run "rm -rf test";
 }
@@ -44,16 +45,22 @@ sub run {
     enable_modules if is_sle;
 
     # Install tests dependencies
-    my @pkgs = qw(aardvark-dns dbus-1-daemon firewalld iproute2 iptables jq netavark slirp4netns);
+    my @pkgs = qw(aardvark-dns firewalld iproute2 iptables jq netavark slirp4netns);
+    if (is_tumbleweed) {
+        push @pkgs, qw(dbus-1-daemon ncat);
+    } elsif (is_sle) {
+        push @pkgs, qw(dbus-1);
+    }
     install_packages(@pkgs);
 
-    record_info("aardvark version", script_output("/usr/libexec/podman/aardvark-dns --version"));
+    my $aardvark = script_output "rpm -ql aardvark-dns | grep podman/aardvark-dns";
+    record_info("aardvark version", script_output("$aardvark --version"));
 
     my $test_dir = "/var/tmp";
     assert_script_run "cd $test_dir";
 
     # Download aardvark sources
-    $aardvark_version = script_output "/usr/libexec/podman/aardvark-dns --version | awk '{ print \$2 }'";
+    $aardvark_version = script_output "$aardvark --version | awk '{ print \$2 }'";
     script_retry("curl -sL https://github.com/containers/aardvark-dns/archive/refs/tags/v$aardvark_version.tar.gz | tar -zxf -", retry => 5, delay => 60, timeout => 300);
     assert_script_run "cd $test_dir/aardvark-dns-$aardvark_version/";
     assert_script_run "cp -r test test.orig";
