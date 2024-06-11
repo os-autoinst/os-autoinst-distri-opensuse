084a5706d981dc22ca14d3ef159a8d95a4585d19
Michele Pagot <michele.pagot@suse.com>
2024-06-11 13:07:36 +0200

    Add awk and tar to dependency as needed by github internal engine

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -5,4 +5,4 @@ zypper dup -y
 zypper in -y os-autoinst-distri-opensuse-deps perl-JSON-Validator gnu_parallel
 
 # other dependency that are needed
-zypper in -y make git vim gcc-c++ libxml2-devel libssh2-devel libexpat-devel dbus-1-devel python311 python311-devel python311-yamllint python311-PyYAML perl-App-cpanminus perl-Code-TidyAll podman
+zypper in -y awk tar make git vim gcc-c++ libxml2-devel libssh2-devel libexpat-devel dbus-1-devel python311 python311-devel python311-yamllint python311-PyYAML perl-App-cpanminus perl-Code-TidyAll podman
