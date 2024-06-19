089f70c814749ed5edfba2c7227d2ec7a5b22e74
m-dati <mdati@suse.com>
2024-06-19 19:58:00 +0200

    P.C. gce cleanup removed get terraform SAP input

--- lib/publiccloud/provider.pm
+++ lib/publiccloud/provider.pm
@@ -705,11 +705,12 @@ sub terraform_param_tags
     return encode_json($tags);
 }
 
-=head2 get_terraform_output
+=head2 get_terraform_output($jq_query, $tf_dir)
 
 Query the terraform data structure in json format.
 Automatic search and set valid terraform directory on helper image.
 Input: <jq-query-format> string; <empty> = no query then full output of data structure.
+       <tf_dir> string, optional terraform destination directory; when empty, dir.searched.
 E.g: to get the VM instance name from json data structure, the call is: 
     get_terraform_output(".vm_name.value[0]");
 To get the complete output structure, the call is:
@@ -718,21 +719,22 @@ To get the complete output structure, the call is:
 =cut
 
 sub get_terraform_output {
-    my ($self, $jq_query) = @_;
+    my ($self, $jq_query, $tf_dir) = @_;
     # cuurent dir saved
     my $dir = script_output("pwd");
     # terraform state file search first matching
-    my $cd = script_output("(find ~ -name *.tfstate -print | head -1 | xargs dirname) 2>/dev/null");
+    my $start = ($tf_dir) ? $tf_dir : '~';
+    $tf_dir = script_output("find $start -name \*.tfstate -exec dirname {} + -quit 2>/dev/null");
     # state file missing: no data found
-    return "" if ($cd eq "");
+    return "" if ($tf_dir eq "");
     # enter terraform dir. found
-    script_run("cd $cd") if ($cd ne $dir);
+    script_run("cd $tf_dir") if ($tf_dir ne $dir);
     # get data
     my $res = script_output("terraform output -no-color -json | jq -Mr '$jq_query'", proceed_on_failure => 1);
     # restore saved dir
-    script_run("cd $dir") if ($cd ne $dir);
+    script_run("cd $dir") if ($tf_dir ne $dir);
     # return data or empty when "null" found
-    return ($res =~ /^null$/) ? "" : $res;
+    return ($res eq "null") ? "" : $res;
 }
 
 sub escape_single_quote {
