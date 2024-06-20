86feab0c8af4c13c145df9af55ab922246ccf7e7
Pablo Herranz Ramírez <pherranz@suse.com>
2024-06-20 16:04:18 +0200

    Try to load tests in bci/main.pm

--- products/bci/main.pm
+++ products/bci/main.pm
@@ -3,14 +3,26 @@ use warnings;
 use needle;
 use File::Basename;
 use scheduler 'load_yaml_schedule';
+
 BEGIN {
     unshift @INC, dirname(__FILE__) . '/../../lib';
 }
 use utils;
-use main_common;
+use main_common qw(init_main);
+use main_micro_alp;
 
 init_main();
 
-load_common_opensuse_sle_tests;
+# Containerized YaST needs proper libyui initialization
+if (load_yaml_schedule) {
+    if (YuiRestClient::is_libyui_rest_api) {
+        YuiRestClient::set_libyui_backend_vars;
+        YuiRestClient::init_logger;
+    }
+    return 1;
+}
+
+main_micro_alp::load_tests();
+
 
 1;
