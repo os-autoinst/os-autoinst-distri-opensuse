e6a851d25694e19e2f4039d4432998e2714c031e
Michael Moese <mmoese@suse.com>
2024-07-18 11:48:21 +0200

    Make nfs tests more configurable and support for bonding interfaces
    This allows the NFS tests to be controlled by a bunch of (optional)
    variables. Also this checks for kernel support for NFSv3 and NFSv4
    before doing anything related, although normally we expect this to be
    present on the kernels.
    We also check for NFSv4.1 and NFSv4.2 support, even this is unused for
    now, we need this later for enabling multipath testing.
    
    Also support link aggregation (bonding):
    If using a switch with bonding, make sure to have the switch ports
    configured in a compatible way of the bonding interfaces. For a direct
    connection, nothing more is to be done here.
    
    We need to specify some more variables in order to enable and use this
    test:
    - NFS_BOND
      only when enabled by setting to '1' we configure the bond
    
    - NFS_IF_1 and NFS_IF_2
      specfify which ethernet devices should be configured for use with
      the bond interface
    
    - NFS_BOND_IP
      The IP address configuration used for bond0. Specify like
      192.168.100.10/24
    
    - BOND_MODE (optional)
      Allow to configure the bond mode to use, defaults to 802.3ad
    
    Also make sure to set SERVER_NODE and CLIENT_NODE to these IPs

--- tests/kernel/nfs_client.pm
+++ tests/kernel/nfs_client.pm
@@ -18,19 +18,49 @@ sub run {
     select_serial_terminal();
     record_info("hostname", script_output("hostname"));
     my $server_node = get_var('SERVER_NODE', 'server-node00');
-    my $local_nfs3 = "/home/localNFS3";
-    my $local_nfs4 = "/home/localNFS4";
-    my $local_nfs3_async = "/home/localNFS3async";
-    my $local_nfs4_async = "/home/localNFS4async";
+
+    my $local_nfs3 = get_var('NFS_LOCAL_NFS3', '/home/localNFS3');
+    my $local_nfs3_async = get_var('NFS_LOCAL_NFS3_ASYNC', '/home/localNFS3async');
+    my $local_nfs4 = get_var('NFS_LOCAL_NFS4', '/home/localNFS4');
+    my $local_nfs4_async = get_var('NFS_LOCAL_NFS4_ASYNC', '/home/localNFS4async');
+    my $multipath = get_var('NFS_MULTIPATH', '0');
+
+    # check kernel config options and set the variables
+    my $kernel_nfs3 = 0;
+    my $kernel_nfs4 = 0;
+    my $kernel_nfs4_1 = 0;
+    my $kernel_nfs4_2 = 0;
+    my $kernel_nfsd_v3 = 0;
+    my $kernel_nfsd_v4 = 0;
+
+    $kernel_nfs3 = 1 unless script_run('zgrep "CONFIG_NFS_V3=[my]" /proc/config.gz');
+    $kernel_nfs4 = 1 unless script_run('zgrep "CONFIG_NFS_V4=[my]" /proc/config.gz');
+    $kernel_nfs4_1 = 1 unless script_run('zgrep "CONFIG_NFS_V4_1=[my]" /proc/config.gz');
+    $kernel_nfs4_2 = 1 unless script_run('zgrep "CONFIG_NFS_V4_2=[my]" /proc/config.gz');
+    $kernel_nfsd_v3 = 1 unless script_run('zgrep "CONFIG_NFSD=[my]" /proc/config.gz');
+    $kernel_nfsd_v4 = 1 unless script_run('zgrep "CONFIG_NFSD_V4=[my]" /proc/config.gz');
+
 
     barrier_wait("NFS_SERVER_ENABLED");
     record_info("showmount", script_output("showmount -e $server_node"));
 
-    assert_script_run("mkdir $local_nfs3 $local_nfs4 $local_nfs3_async $local_nfs4_async");
-    assert_script_run("mount -t nfs -o nfsvers=3 $server_node:/nfs/shared_nfs3 $local_nfs3");
-    assert_script_run("mount -t nfs -o nfsvers=4 $server_node:/nfs/shared_nfs4 $local_nfs4");
-    assert_script_run("mount -t nfs -o nfsvers=3 $server_node:/nfs/shared_nfs3_async $local_nfs3_async");
-    assert_script_run("mount -t nfs -o nfsvers=4 $server_node:/nfs/shared_nfs4_async $local_nfs4_async");
+    if ($kernel_nfs3 == 1) {
+        record_info('INFO', 'Kernel has support for NFSv3');
+        assert_script_run("mkdir $local_nfs3 $local_nfs3_async");
+        assert_script_run("mount -t nfs -o nfsvers=3,sync $server_node:/nfs/shared_nfs3 $local_nfs3");
+        assert_script_run("mount -t nfs -o nfsvers=3 $server_node:/nfs/shared_nfs3_async $local_nfs3_async");
+    } else {
+        record_info('INFO', 'Kernel has no support for NFSv3, skipping NFSv3 tests');
+    }
+
+    if ($kernel_nfs4 == 1) {
+        record_info('INFO', 'Kernel has support for NFSv4');
+        assert_script_run("mkdir $local_nfs4 $local_nfs4_async");
+        assert_script_run("mount -t nfs -o nfsvers=4,sync $server_node:/nfs/shared_nfs4 $local_nfs4");
+        assert_script_run("mount -t nfs -o nfsvers=4 $server_node:/nfs/shared_nfs4_async $local_nfs4_async");
+    } else {
+        record_info('INFO', 'Kernel has no support for NFSv4, skipping NFSv4tests');
+    }
 
     barrier_wait("NFS_CLIENT_ENABLED");
 
@@ -38,10 +68,15 @@ sub run {
     #proper tests should come in the next modules
     assert_script_run("dd if=/dev/zero of=testfile bs=1024 count=10240");
     assert_script_run("md5sum testfile > md5sum.txt");
-    assert_script_run("cp testfile md5sum.txt $local_nfs3");
-    assert_script_run("cp testfile md5sum.txt $local_nfs4");
-    assert_script_run("cp testfile md5sum.txt $local_nfs3_async");
-    assert_script_run("cp testfile md5sum.txt $local_nfs4_async");
+
+    if ($kernel_nfs3 == 1) {
+        assert_script_run("cp testfile md5sum.txt $local_nfs3");
+        assert_script_run("cp testfile md5sum.txt $local_nfs3_async");
+    }
+    if ($kernel_nfs4 == 1) {
+        assert_script_run("cp testfile md5sum.txt $local_nfs4");
+        assert_script_run("cp testfile md5sum.txt $local_nfs4_async");
+    }
 
     barrier_wait("NFS_SERVER_CHECK");
 }
