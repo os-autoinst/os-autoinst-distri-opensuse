61fe23da78543a9666b40e1fe54e8a6e15b6775b
lpalovsky <lpalovsky@suse.com>
2024-07-25 14:06:40 +0200

    Cleanup logic improvements

--- lib/sles4sap/sap_deployment_automation_framework/basetest.pm
+++ lib/sles4sap/sap_deployment_automation_framework/basetest.pm
@@ -7,39 +7,60 @@
 # Basetest used for Microsoft SDAF deployment
 
 package sles4sap::sap_deployment_automation_framework::basetest;
+use parent 'opensusebasetest';
+
 use strict;
 use warnings;
 use testapi;
-use parent 'opensusebasetest';
+use Exporter qw(import);
 use sles4sap::sap_deployment_automation_framework::deployment qw(sdaf_cleanup az_login load_os_env_variables);
-use sles4sap::sap_deployment_automation_framework::deployment_connector qw(find_deployer_resources);
-use sles4sap::console_redirection qw(connect_target_to_serial disconnect_target_from_serial);
-use sles4sap::azure_cli qw(az_resource_delete);
+use sles4sap::sap_deployment_automation_framework::deployment_connector
+  qw(find_deployer_resources destroy_deployer_vm get_deployer_vm_name find_deployment_id get_deployer_ip);
+use sles4sap::console_redirection qw(connect_target_to_serial disconnect_target_from_serial check_serial_redirection);
 
+our @EXPORT = qw(full_cleanup);
 
-sub post_fail_hook {
+sub full_cleanup {
     if (get_var('SDAF_RETAIN_DEPLOYMENT')) {
         record_info('Cleanup OFF', 'OpenQA variable "SDAF_RETAIN_DEPLOYMENT" is active, skipping cleanup.');
         return;
     }
 
-    record_info('Post fail', 'Executing post fail hook');
-    # Trigger SDAF remover script to destroy 'workload zone' and 'sap systems' resources
-    # Clean up all config files, keys, etc.. on deployer VM
-    connect_target_to_serial();
-    load_os_env_variables();
+    # Disable any redirection being active
+    disconnect_target_from_serial if check_serial_redirection();
     az_login();
-    sdaf_cleanup();
-    disconnect_target_from_serial();
+    # collect required data first. Call functions only if mandatory args are found to avoid triggering croak/die
+    my $deployment_id = find_deployment_id();
+    my $deployer_vm_name = get_deployer_vm_name(deployment_id => find_deployment_id()) if $deployment_id;
+    my $deployer_ip = get_deployer_ip(deployer_vm_name => $deployer_vm_name) if $deployer_vm_name;
+    my $redirection_works;
 
-    # Cleanup deployer VM resources only
-    # Deployer VM is located in permanent deployer resource group. This RG **MUST STAY INTACT**
-    my @resource_cleanup_list = @{find_deployer_resources(return_value => 'id')};
-    record_info('Resources destroy',
-        "Following resources are being destroyed:\n" . join("\n", @{find_deployer_resources()}));
+    # Attempt console redirection only if mandatory arguments are defined
+    if ($deployer_ip) {
+        set_var('REDIRECT_DESTINATION_USER', get_var('PUBLIC_CLOUD_USER', 'azureadm'));
+        set_var('REDIRECT_DESTINATION_IP', $deployer_ip);
+        # Do not fail even if connection is not successful
+        $redirection_works = connect_target_to_serial(fail_ok => '1');
+    }
 
-    az_resource_delete(ids => join(' ', @resource_cleanup_list),
-        resource_group => get_required_var('SDAF_DEPLOYER_RESOURCE_GROUP'), timeout => '600');
+    # First check if redirection works. Skip dependent tasks if it does not
+    if ($redirection_works) {
+        # Trigger SDAF remover script to destroy 'workload zone' and 'sap systems' resources
+        # Clean up all config files, keys, etc.. on deployer VM
+        load_os_env_variables();
+        az_login();
+        sdaf_cleanup();
+        disconnect_target_from_serial();
+    }
+    record_info('SUT cleanup', 'Failed to set up redirection, skipping SDAF cleanup scripts.') unless $redirection_works;
+
+    # Destroys deployer VM and its resources
+    destroy_deployer_vm();
+}
+
+sub post_fail_hook {
+    record_info('Post fail', 'Executing post fail hook');
+    full_cleanup();
 }
 
 1;
