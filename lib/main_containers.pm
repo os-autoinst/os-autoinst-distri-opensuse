bf2628939e01ed74428f9d7e9461514e4da8a1eb
Ricardo Branco <rbranco@suse.de>
2024-08-19 09:32:54 +0200

    containers: Re-enable netavark upstream tests on aarch64

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -309,7 +309,7 @@ sub load_container_tests {
             loadtest 'containers/runc_integration' if (is_tumbleweed || is_sle || is_leap);
         }
         if (!check_var('NETAVARK_BATS_SKIP', 'all')) {
-            loadtest 'containers/netavark_integration' if (is_x86_64 && (is_tumbleweed || is_sle || is_leap));
+            loadtest 'containers/netavark_integration' if (is_tumbleweed || is_sle || is_leap);
         }
         if (!check_var('AARDVARK_BATS_SKIP', 'all')) {
             loadtest 'containers/aardvark_integration' if (is_tumbleweed);
