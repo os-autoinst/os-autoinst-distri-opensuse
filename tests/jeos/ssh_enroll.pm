40a9020c8b9ae5bd597c147a2f14dfbbe6e6cb20
Ricardo B. Marliere <ricardo.marliere@suse.com>
2024-08-22 19:41:48 +0200

    jeos/ssh_enroll: Add new test and its schedule
    This test is to be used as the parent of microos-wizard to test the first
    boot wizard's ssh enrollment feature.

---
+++ tests/jeos/ssh_enroll.pm
@@ -0,0 +1,27 @@
+# SUSE's openQA tests
+#
+# Copyright 2024 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Run a ssh client to test firstboot wizard ssh enrollment feature
+# Maintainer: QE-C team <qa-c@suse.de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use lockapi qw(mutex_wait);
+use mmapi;
+
+sub run {
+    select_console 'root-console';
+    script_run("ssh-keygen -t rsa -b 2048 -f id_rsa -N ''");
+
+    my $children = get_children();
+    my $child_id = (keys %$children)[0];
+    mutex_wait('SSH_ENROLL_PAIR', $child_id);
+
+    script_run("ssh -i id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root\@10.0.2.15");
+}
+
+1;
