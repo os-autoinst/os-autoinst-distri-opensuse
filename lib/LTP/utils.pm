os-autoinst/os-autoinst-distri-opensuse

commit 80bee236a949b98846a16dcff875b5952fab416f

LTP: Adapt record_info() output to Micro
By using enter_trup_shell and exit_trup_shell.

Signed-off-by: Petr Vorel <pvorel@suse.cz>

lib/LTP/utils.pm
@@ -20,6 +20,7 @@ use Utils::Architectures;
 use repo_tools 'add_qa_head_repo';
 use utils;
 use kernel 'get_kernel_flavor';
+use transactional;
 
 our @EXPORT = qw(
   check_kernel_taint
@@ -97,6 +98,9 @@ sub log_versions {
       (is_rt ? 'kernel-rt' : get_kernel_flavor);
     my $kernel_pkg_log = '/tmp/kernel-pkg.txt';
     my $ver_linux_log = '/tmp/ver_linux_before.txt';
+
+    enter_trup_shell(global_options => '-c') if is_transactional;
+
     my $kernel_config = script_output('for f in "/boot/config-$(uname -r)" "/usr/lib/modules/$(uname -r)/config" /proc/config.gz; do if [ -f "$f" ]; then echo "$f"; break; fi; done');
 
     script_run("rpm -qi $kernel_pkg > $kernel_pkg_log 2>&1");
@@ -139,6 +143,7 @@ sub log_versions {
 
     script_run('env');
     script_run('aa-enabled; aa-status');
+    exit_trup_shell if is_transactional;
 }
 
 sub export_ltp_env {

commit 2dcee5da95ffff6a27cc97b079e109e596bbf2a7

LTP/boot_ltp: Fix runtime failure on Micro
First, trup shell should be called after the system rebooted into the
latest snapshot, it will print wrong versions.

Also using 'rpm -qi /tmp/kernel-pkg.txt 2>&1' inside trup shell did
failed (on Micro):

    This fixes error in boot_ltp
    cat: /tmp/ver_linux_before.txt:

Therefore instead using trup shell use 'transactional-update -c run' (as
was in the original proposal #19983).

Also run ver_linux via 'transactional-update -c run' to get updated
packages version.

Fixes: 80bee236a ("LTP: Adapt record_info() output to Micro")
Fixes: https://progress.opensuse.org/issues/165731
Reported-by: Petr Cervinka <pcervinka@suse.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

lib/LTP/utils.pm
@@ -20,7 +20,6 @@ use Utils::Architectures;
 use repo_tools 'add_qa_head_repo';
 use utils;
 use kernel 'get_kernel_flavor';
-use transactional;
 
 our @EXPORT = qw(
   check_kernel_taint
@@ -98,16 +97,14 @@ sub log_versions {
       (is_rt ? 'kernel-rt' : get_kernel_flavor);
     my $kernel_pkg_log = '/tmp/kernel-pkg.txt';
     my $ver_linux_log = '/tmp/ver_linux_before.txt';
-
-    enter_trup_shell(global_options => '-c') if is_transactional;
-
     my $kernel_config = script_output('for f in "/boot/config-$(uname -r)" "/usr/lib/modules/$(uname -r)/config" /proc/config.gz; do if [ -f "$f" ]; then echo "$f"; break; fi; done');
+    my $run_cmd = is_transactional ? 'transactional-update -c run ' : '';
 
-    script_run("rpm -qi $kernel_pkg > $kernel_pkg_log 2>&1");
+    script_run("$run_cmd rpm -qi $kernel_pkg > $kernel_pkg_log 2>&1");
     upload_logs($kernel_pkg_log, failok => 1);
 
     if (get_var('LTP_COMMAND_FILE') || get_var('LIBC_LIVEPATCH')) {
-        script_run(get_ltproot . "/ver_linux > $ver_linux_log 2>&1");
+        script_run("$run_cmd " . get_ltproot . "/ver_linux > $ver_linux_log 2>&1");
         upload_logs($ver_linux_log, failok => 1);
     }
 
@@ -133,9 +130,8 @@ sub log_versions {
 
     record_info('KERNEL VERSION', script_output('uname -a'));
     record_info('KERNEL DEFAULT PKG', script_output("cat $kernel_pkg_log", proceed_on_failure => 1));
-    record_info('KERNEL EXTRA PKG', script_output('rpm -qi kernel-default-extra', proceed_on_failure => 1));
-
-    record_info('KERNEL pkg', script_output('rpm -qa | grep kernel', proceed_on_failure => 1));
+    record_info('KERNEL EXTRA PKG', script_output("$run_cmd rpm -qi kernel-default-extra", proceed_on_failure => 1));
+    record_info('KERNEL pkg', script_output("$run_cmd rpm -qa | grep kernel", proceed_on_failure => 1));
 
     if (get_var('LTP_COMMAND_FILE') || get_var('LIBC_LIVEPATCH')) {
         record_info('ver_linux', script_output("cat $ver_linux_log", proceed_on_failure => 1));
@@ -143,7 +139,6 @@ sub log_versions {
 
     script_run('env');
     script_run('aa-enabled; aa-status');
-    exit_trup_shell if is_transactional;
 }
 
 sub export_ltp_env {