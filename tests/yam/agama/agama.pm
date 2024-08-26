d5762697117fd642678a1d3cf122784b44c2fcf3
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2024-08-26 15:47:58 +0200

    Integrate the web automation script for agama in openQA
    Integrate the web automation script for agama in openQA
    
    Integrate the web automation script for agama in openQA
    
    Integrate the web automation script for agama in openQA
    
    Integrate the web automation script for agama in openQA
    
    Added puppeteer script launch
    
    Added test copy to testing folder
    
    Changed default live password
    
    Solve params error
    
    Moved password method
    
    Removed trim subroutine
    
    Remove pre_hook
    
    Fixed post_run_hook assert
    
    Increased integration tests timeout
    
    Disable btrf messages
    
    Added reboot handle and renamed agama test file
    
    Remove dmesg command run
    
    Added stderr upload and removed reboot schedule
    
    Removed upload traces function, not used by puppeteer
    
    Removed stderr upload
    
    Added Puppeteer log traces
    
    Tidying Perl code
    
    Applied review comments, removed code and simplified testing
    
    Using testapi select_console function
    
    Fix naming
    
    Changed testing folder
    
    Fix summary typo
    
    Changed screen order in test
    
    Renamed agama_integration script
    
    Removed upload_traces from agama script
    
    Reset console at switch step
    
    Change installation to displaymanager
    
    Added new screen in the screen list
    
    Changed way to add_console
    
    Changed screen due to new needle
    
    Remove development
    
    Improved patch code
    
    Added validation
    
    Remove validation

--- tests/yam/agama/agama.pm
+++ tests/yam/agama/agama.pm
@@ -1,33 +1,38 @@
-## Copyright 2023 SUSE LLC
+## Copyright 2024 SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 # Summary: Run interactive installation with Agama,
-# run playwright tests directly from the Live ISO.
+# using a web automation tool to test directly from the Live ISO.
 # Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
 
 use base Yam::Agama::agama_base;
 use strict;
 use warnings;
+use testapi;
 
 use testapi qw(
   assert_script_run
   assert_screen
   get_required_var
   enter_cmd
+  select_console
 );
 
 sub run {
     my $self = shift;
     my $test = get_required_var('AGAMA_TEST');
 
-    assert_script_run("playwright test --trace on --project chromium --config /usr/share/e2e-agama-playwright tests/" . $test . ".spec.ts", timeout => 1200);
-    $self->upload_traces();
-    enter_cmd 'reboot';
+    assert_script_run("agama-integration-tests /usr/share/agama/integration-tests/build/" . $test . ".js", timeout => 1200);
+
+    select_console 'displaymanager';
+    save_screenshot();
+
+    assert_screen('agama-install-finished', 10);
+    assert_and_click('reboot');
 }
 
 sub post_run_hook {
     my ($self) = shift;
-    assert_screen([qw(grub2-agama encrypted-disk-password-prompt)], 120);
     $self->SUPER::post_run_hook;
 }
 
