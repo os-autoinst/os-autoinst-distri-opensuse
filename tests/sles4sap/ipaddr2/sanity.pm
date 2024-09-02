24e2ebea23f6869ff1ca13c5f070236d395b5181
Michele Pagot <michele.pagot@suse.com>
2024-09-02 09:24:25 +0200

    Fix cloud-init log collection (#20096)
    Properly takes in account IPADDR_CLOUDINIT setting default when deciding
    if log collection is needed or not.

--- tests/sles4sap/ipaddr2/sanity.pm
+++ tests/sles4sap/ipaddr2/sanity.pm
@@ -29,7 +29,7 @@ sub test_flags {
 sub post_fail_hook {
     my ($self) = shift;
     ipaddr2_deployment_logs() if check_var('IPADDR2_DIAGNOSTIC', 1);
-    ipaddr2_os_cloud_init_logs() if (check_var('IPADDR2_CLOUDINIT', 1));
+    ipaddr2_os_cloud_init_logs() unless check_var('IPADDR2_CLOUDINIT', 0);
     ipaddr2_destroy();
     $self->SUPER::post_fail_hook;
 }
