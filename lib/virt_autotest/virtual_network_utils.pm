15d12b8d6710f58246396dc8fe5d4dd078dd83ae
Alice(Xiaoli Ai) <xlai@suse.com>
2024-09-09 17:14:11 +0800

    [Virtualization][MU]: Loose TEST check to pass PR validation jobs

--- lib/virt_autotest/virtual_network_utils.pm
+++ lib/virt_autotest/virtual_network_utils.pm
@@ -132,7 +132,7 @@ sub test_network_interface {
     script_retry("nmap $guest -PN -p ssh | grep open", delay => 30, retry => 6, timeout => 180);
     my $nic = script_output "ssh root\@$guest \"grep '$mac' /sys/class/net/*/address | cut -d'/' -f5 | head -n1\"";
     die "$mac not found in guest $guest" unless $nic;
-    if (check_var('TEST', 'qam-xen-install-and-features-test') || check_var('TEST', 'qam-kvm-install-and-features-test') || $is_sriov_test eq "true") {
+    if (get_var('TEST', '') =~ m/qam-(kvm|xen)-install-and-features-test/ || $is_sriov_test eq "true") {
         assert_script_run("ssh root\@$guest \"echo BOOTPROTO=\\'dhcp\\' > /etc/sysconfig/network/ifcfg-$nic\"");
 
         # Restart the network - the SSH connection may drop here, so no return code is checked.
