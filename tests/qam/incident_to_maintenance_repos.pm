c69b0df3ec02d4e83ec709eb03754c3168106b96
Alvaro Carvajal <acarvajal@suse.de>
2024-09-16 17:18:53 +0200

    Set MAINT_TEST_REPO to INCIDENT_REPO in HA Single Incident
    Different to Aggregate tests, Single Incident tests are not triggered
    with settings that will make the test code to set in the
    *MAINT_TEST_REPO* setting the list of test repositories to add. This is
    causing the Rolling Upgrade and Rolling Update tests scheduled in Single
    Incident job groups to skip adding the test repositories after the
    upgrade/update, which in turn could lead to the packages under test to
    not be installed. This commit corrects this by adding a test module to
    set *MAINT_TEST_REPO* with the content of *INCIDENT_REPO* when
    *MAINT_TEST_REPO* is empty or not set.

---
+++ tests/qam/incident_to_maintenance_repos.pm
@@ -0,0 +1,18 @@
+# Copyright 2024 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: Set MAINT_TEST_REPO to the content of INCIDENT_REPO when
+#          MAINT_TEST_REPO has not been set. Intended to be used only
+#          in Single Incident job groups.
+# Maintainer: QE SAP & HA <qe-sap@suse.de>
+
+use base "opensusebasetest";
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    set_var('MAINT_TEST_REPO', get_var('INCIDENT_REPO', '')) unless get_var('MAINT_TEST_REPO');
+}
+
+1;
