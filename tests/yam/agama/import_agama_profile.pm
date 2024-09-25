510303725f0ffad47fb42d5aaee2fff0d8de54c5
Leo Manfredi <leo.manfredi@suse.com>
2024-09-25 15:22:27 +0200

    Fix Agama: add dmesg before and after assert_script_run

--- tests/yam/agama/import_agama_profile.pm
+++ tests/yam/agama/import_agama_profile.pm
@@ -7,13 +7,15 @@
 use base Yam::Agama::patch_agama_base;
 use strict;
 use warnings;
-use testapi qw(assert_script_run data_url get_required_var select_console);
+use testapi qw(assert_script_run data_url get_required_var select_console script_run);
 
 sub run {
     select_console 'root-console';
     my $profile = get_required_var('AGAMA_PROFILE');
     my $profile_url = data_url($profile);
+    script_run("dmesg --console-off");
     assert_script_run("/usr/bin/agama profile import $profile_url", timeout => 300);
+    script_run("dmesg --console-on");
 }
 
 1;
