8ecc7ac76a0a35ce0f2fa85c78860b3871d05d1c
tbaev <teodor.baev@suse.com>
2024-10-18 01:04:41 +0300

    Fix typo in check var

--- lib/virt_autotest/common.pm
+++ lib/virt_autotest/common.pm
@@ -265,7 +265,7 @@ if (get_var("REGRESSION", '') =~ /xen/) {
             name => 'sles15sp6',
         },
     );
-    %guests = get_var('TERADATA') ? %guests{"sles${guest_version}TD"} : check_var->('EXTENDED_SECURITY', '1') ? %guests{"sles${guest_version}ES"} : %guests{"sles${guest_version}"};
+    %guests = get_var('TERADATA') ? %guests{"sles${guest_version}TD"} : check_var('EXTENDED_SECURITY', '1') ? %guests{"sles${guest_version}ES"} : %guests{"sles${guest_version}"};
 
 } elsif (get_var("REGRESSION", '') =~ /hyperv/) {
     %guests = (
@@ -297,7 +297,7 @@ if (get_var("REGRESSION", '') =~ /xen/) {
             vm_name => 'sles-15.6_openQA-virtualization-maintenance',
         },
     );
-    %guests = get_var('TERADATA') ? %guests{"sles${guest_version}TD"} : check_var->('EXTENDED_SECURITY', '1') ? %guests{"sles${guest_version}ES"} : %guests{"sles${guest_version}"};
+    %guests = get_var('TERADATA') ? %guests{"sles${guest_version}TD"} : check_var('EXTENDED_SECURITY', '1') ? %guests{"sles${guest_version}ES"} : %guests{"sles${guest_version}"};
 }
 
 our %imports = ();    # imports are virtual machines that we don't install but just import. We test those separately.
