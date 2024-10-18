7620f8ac1064b8152b94a5afced3265ccde6a7be
Huajian.Luo <hluo@suse.com>
2024-10-18 11:42:06 +0800

    Detect end of agama auto-installation in s390x using for example logs

--- lib/Yam/Agama/Pom/RebootTextmodePage.pm
+++ lib/Yam/Agama/Pom/RebootTextmodePage.pm
@@ -22,4 +22,28 @@ sub reboot {
     enter_cmd 'reboot';
 }
 
+sub expect_is_shown {
+    my ($self, %args) = @_;
+   #  my $timeout = $args{timeout};
+   my $timeout = 7200;
+   #  my $s3270 = console('x3270');
+   #  my $r;
+
+    while (1) {
+        # script_run('agama logs store');
+        script_run('tail /var/log/YaST2/y2log');
+
+        die "timeout ($timeout) hit on during installation" if $timeout <= 0;
+        # $r = $s3270->expect_3270(
+        #     output_delim => qr/Installation Finished/,
+        #     timeout => 300
+        #     );
+        #      last;
+        # } else {
+            $timeout -= 30;
+            diag("left total timeout: $timeout");
+            next;
+        }
+}
+
 1;
