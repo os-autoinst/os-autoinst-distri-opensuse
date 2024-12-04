b4160313c07ed97308d2a1a2a337e5ff95283045
lemon.li <leli@suse.com>
2024-12-04 02:46:50 -0500

    Remove hardcode password in agama profiles

--- data/yam/agama/auto/leap16.sh
+++ data/yam/agama/auto/leap16.sh
@@ -1,7 +1,7 @@
 set -ex
 
 /usr/bin/agama config set software.product=Leap16
-/usr/bin/agama config set user.userName=bernhard user.password=nots3cr3t
-/usr/bin/agama config set root.password=nots3cr3t
+/usr/bin/agama config set user.userName=bernhard user.password=$6$vYbbuJ9WMriFxGHY$gQ7shLw9ZBsRcPgo6/8KmfDvQ/lCqxW8/WnMoLCoWGdHO6Touush1nhegYfdBbXRpsQuy/FTZZeg7gQL50IbA
+/usr/bin/agama config set root.password=$6$vYbbuJ9WMriFxGHY$gQ7shLw9ZBsRcPgo6/8KmfDvQ/lCqxW8/WnMoLCoWGdHO6Touush1nhegYfdBbXRpsQuy/FTZZeg7gQL50IbA
 /usr/bin/sleep 30
 /usr/bin/agama install
