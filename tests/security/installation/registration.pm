66e056b9edcdd66c8f876b3032ac04b4a5666bb3
Paolo Stivanin <info@paolostivanin.com>
2024-12-12 14:00:37 +0100

    Refactor QR scheduling
    * have a single yaml file for QR scheduling (Full, Online)
    * adapt tests to deal with Full or Online

---
+++ tests/security/installation/registration.pm
@@ -0,0 +1,25 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: If Full-QR don't register the system, otherwise do the registration.
+# Maintainer: QE Security <none@suse.de>
+
+use base 'y2_installbase';
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    if (check_var('FLAVOR', 'Full-QR')) {
+        $testapi::distri->get_registration()->skip_registration();
+    } else {
+        $testapi::distri->get_registration()->register_via_scc({
+                email => get_var('SCC_EMAIL'),
+                reg_code => get_var('SCC_REGCODE')});
+        $testapi::distri->get_registration()->enable_update_repositories();
+    }
+}
+
+1;
