739d102508f7a5b524365c89e14b0bf000574f15
Andrea Manzini <ilmanzo@gmail.com>
2025-01-17 16:27:13 +0100

    Install openssl pkg needed for nss cryptopolicy test

--- data/security/crypto_policies/test_nss_client.sh
+++ data/security/crypto_policies/test_nss_client.sh
@@ -2,6 +2,8 @@
 # call this script with first argument the file where to write the client output
 set -eux
 OUTFILE=$1
+# sort out test client path name, which is different between SLES and Tumbleweed
+TSTCLNT=$(rpm -ql mozilla-nss-tools | grep /usr/lib.*tstclnt | head -1)
 # create NSS database
 rm -rf nssdb && mkdir -p nssdb
 certutil -N -d sql:./nssdb --empty-password
@@ -14,7 +16,7 @@ certutil -d ./nssdb -A -a -i localhost.pem -t TCP -n localhost
 openssl s_server -accept 4443 -cert localhost.pem -key localhost.key -www &
 SERVER_PID=$!
 # call the server with nss client
-(echo "GET / HTTP/1.0" | /usr/lib/nss/tstclnt -d ./nssdb -h localhost -p 4443 2>&1 > $OUTFILE) &
+(echo "GET / HTTP/1.0" | $TSTCLNT -d ./nssdb -h localhost -p 4443 2>&1 > $OUTFILE) &
 sleep 3
 # stop the server and the background client
 kill $SERVER_PID
