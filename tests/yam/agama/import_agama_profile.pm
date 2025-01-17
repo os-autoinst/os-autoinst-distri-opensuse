d26b8d15dba516b918fb679f21fde615e3be51bc
lemon.li <leli@suse.com>
2025-01-17 01:15:02 -0500

    Fix duplicated agama profile path issue

--- tests/yam/agama/import_agama_profile.pm
+++ tests/yam/agama/import_agama_profile.pm
@@ -8,10 +8,11 @@ use base Yam::Agama::patch_agama_base;
 use strict;
 use warnings;
 use testapi qw(assert_script_run data_url get_required_var select_console script_run);
+use autoyast qw(expand_agama_profile);
 
 sub run {
+    my $profile = expand_agama_profile(get_required_var('AGAMA_PROFILE'));
     select_console 'root-console';
-    my $profile = get_required_var('AGAMA_PROFILE');
     script_run("dmesg --console-off");
     assert_script_run("agama profile import $profile", timeout => 300);
     script_run("dmesg --console-on");
