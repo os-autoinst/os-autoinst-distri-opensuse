75af1d66d19f4c240b02b7a1f45d8b6b602c5cbb
Fabian Vogt <fvogt@suse.de>
2025-01-20 16:30:45 +0100

    security/selinux/selinux_smoke.pm: Drop obsolete softfailure

--- tests/security/selinux/selinux_smoke.pm
+++ tests/security/selinux/selinux_smoke.pm
@@ -16,18 +16,13 @@ use testapi;
 use serial_terminal 'select_serial_terminal';
 use utils;
 use registration qw(add_suseconnect_product register_product);
-use version_utils qw(is_sle is_tumbleweed);
+use version_utils qw(is_sle);
 
 sub run {
     select_serial_terminal;
 
     # make sure SELinux is "enabled"
     validate_script_output("sestatus", sub { m/SELinux\ status: .*enabled/sx });
-
-    if (is_tumbleweed && script_run('fixfiles check /etc/selinux/config |& grep "command not found"') == 0) {
-        record_soft_failure('boo#1190813');
-        assert_script_run("sed -i 's/ //' /etc/selinux/config");
-    }
 
     # https://progress.opensuse.org/issues/101481
     if (is_sle('<=15-sp2') && script_run('zypper lu|grep sle-we-release') == 0) {
