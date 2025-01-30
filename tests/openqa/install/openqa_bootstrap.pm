9fc4acd68b380481e81b980235c0ccc141164ec1
Gayane Osipyan <gosipyan@suse.com>
2025-01-30 15:12:12 +0100

    Enabling httpd_can_network_connect flag

--- tests/openqa/install/openqa_bootstrap.pm
+++ tests/openqa/install/openqa_bootstrap.pm
@@ -11,7 +11,7 @@ use warnings;
 use base "consoletest";
 use testapi;
 use utils;
-
+use version_utils qw(has_selinux);
 
 sub run {
     select_console 'root-console';
@@ -20,6 +20,10 @@ sub run {
         record_info('No nested virt', 'No /dev/kvm found');
     }
 
+    # allow connect to httpd_port
+    if (has_selinux) {
+        script_run('setsebool -P httpd_can_network_connect 1');
+    }
     zypper_call('in openQA-bootstrap');
     my $proxy_var = get_var('OPENQA_WEB_PROXY') ? 'setup_web_proxy=' . get_var('OPENQA_WEB_PROXY') . ' ' : '';
     assert_script_run($proxy_var . "/usr/share/openqa/script/openqa-bootstrap", 4000);
