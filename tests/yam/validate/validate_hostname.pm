66b93f6e4002d470ff367a9c4d7748bf075b392b
Leo Manfredi <leo.manfredi@suse.com>
2025-02-17 18:23:10 +0100

    Add test cases for setting hostname in Agama

---
+++ tests/yam/validate/validate_hostname.pm
@@ -0,0 +1,25 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that the hostname is set properly by Agama.
+# Check hostname against test data or default value from DHCP "localhost"
+
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use scheduler 'get_test_suite_data';
+use Test::Assert ':assert';
+
+sub run {
+    select_console 'root-console';
+    my $expected_install_hostname = get_test_suite_data()->{hostname_from_kernel_cmdline} // 'localhost';
+    my $hostname = script_output('hostnamectl hostname');
+    assert_str_equals($expected_install_hostname, $hostname, "Wrong hostname. Expected: '$expected_install_hostname', got '$hostname'");
+}
+
+1;
