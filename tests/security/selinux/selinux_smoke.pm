91498f3f9d508e5e37e889a8de6b5e46bdbcef21
Emil Miler <em@0x45.cz>
2025-03-05 09:55:00 +0100

    Update selinux_smoke test

--- tests/security/selinux/selinux_smoke.pm
+++ tests/security/selinux/selinux_smoke.pm
@@ -18,27 +18,16 @@ use utils;
 use registration qw(add_suseconnect_product register_product);
 use version_utils qw(is_sle);
 
-sub run {
-    select_serial_terminal;
-
-    # make sure SELinux is "enabled"
-    validate_script_output("sestatus", sub { m/SELinux\ status: .*enabled/sx });
-
-    # https://progress.opensuse.org/issues/101481
-    if (is_sle('<=15-sp2') && script_run('zypper lu|grep sle-we-release') == 0) {
-        validate_script_output('zypper -n up --auto-agree-with-product-licenses --no-recommends sle-we-release',
-            sub { m/\(1\/1\) Installing: sle-we-release/ });
-    }
-
-    # refresh & update
+sub test_zypper {
+    # Refresh & update
     zypper_call("ref", timeout => 1200);
     zypper_call("up", timeout => 1200);
 
-    # install & remove pkgs, e.g., apache2
+    # Install & remove packages
     zypper_call("in apache2");
     zypper_call("rm apache2");
 
-    # for sle, register available extensions and modules, e.g., free addons
+    # Register available extensions and modules for SLE, e.g., free addons
     if (is_sle && !main_common::is_updates_tests()) {
         register_product();
         my $version = get_required_var('VERSION') =~ s/([0-9]+).*/$1/r;
@@ -51,9 +40,42 @@ sub run {
         add_suseconnect_product("sle-module-web-scripting", "$version", "$arch", "$params", "$timeout");
     }
 
-    # install & remove patterns, e.g., mail_server
+    # Install & remove patterns
     zypper_call("in -t pattern mail_server");
     zypper_call("rm -t pattern mail_server");
+
+    # Add & remove a repository and test priorities
+    zypper_call("ar -f https://download.opensuse.org/repositories/utilities/openSUSE_Factory/ utilities");
+    zypper_call("mr -p 90 utilities");
+
+    my $priority = script_output("zypper lr -p | awk -F '|' '/utilities/ {print \$7}' | tr -d ' '");
+    die "Repository priority not set correctly" if $priority ne "90";
+
+    zypper_call("rr utilities");
+}
+
+sub test_sysctl {
+    my $sysctl = "vm.swappiness";
+    my $original_value = script_output("/sbin/sysctl -n $sysctl");
+
+    assert_script_run("/sbin/sysctl -w $sysctl=50");
+
+    my $new_value = script_output("/sbin/sysctl -n $sysctl");
+    die "Value not set correctly" if $new_value ne "50";
+
+    assert_script_run("/sbin/sysctl -w $sysctl=$original_value");
+}
+
+sub run {
+    select_serial_terminal;
+
+    # Make sure SELinux is "enabled"
+    validate_script_output("sestatus", sub { m/SELinux\ status: .*enabled/sx });
+
+    record_info("zypper");
+    test_zypper();
+    record_info("sysctl");
+    test_sysctl();
 }
 
 1;
