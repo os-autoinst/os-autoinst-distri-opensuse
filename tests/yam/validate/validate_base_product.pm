d4fd5a68a1ea052c78dfbca5f77e60b6a2eccd74
Chenzi Cao <chcao@suse.com>
2025-03-20 06:46:45 +0100

    Add test module to validate the base product
    Product cannot be validate with cat /etc/os-release due it doesn't
    differentiate well between sles and sles sap. So add a new test module
    to validate the product by reading /etc/products.d/baseproduct.

---
+++ tests/yam/validate/validate_base_product.pm
@@ -0,0 +1,22 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+#
+# Summary: Validate the base product from /etc/products.d/baseproduct.
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use Test::Assert ':assert';
+
+sub run {
+    select_console 'root-console';
+    my $expected_prod = get_required_var("AGAMA_PRODUCT_ID");
+    my $prod = script_output 'basename `readlink /etc/products.d/baseproduct ` .prod';
+    assert_equals($expected_prod, $prod, "Wrong product name in '/etc/products.d/baseproduct'");
+}
+
+1;
