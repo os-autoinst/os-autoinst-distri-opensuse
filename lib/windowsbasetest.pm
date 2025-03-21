c91b8433178d857a97c6dc9a5ad7fc95b0f8b712
Pablo Herranz Ramírez <pherranz@suse.com>
2025-03-21 09:41:37 +0100

    Cleanup of Win installation code
    After improving the unattended installation, there's no need for the attended parts. Also the code has been re-structured to be cleaner

--- lib/windowsbasetest.pm
+++ lib/windowsbasetest.pm
@@ -128,7 +128,7 @@ sub reboot_or_shutdown {
 
 sub wait_boot_windows {
 
-    my ($self, $is_firstboot) = @_;
+    my $self = @_;
 
     # Reset the consoles: there is no user logged in anywhere
     reset_consoles;
@@ -137,29 +137,10 @@ sub wait_boot_windows {
     send_key_until_needlematch 'windows-login', 'esc';
     type_password;
     send_key 'ret';
-    if ($is_firstboot) {
-        record_info('Windows firstboot', 'Starting Windows for the first time');
-        wait_still_screen stilltime => 60, timeout => 300;
-        # When starting Windows for the first time, several screens or pop-ups may appear
-        # in a different order. We'll try to handle them until the desktop is shown
-        assert_screen(['windows-edge-welcome', 'windows-desktop', 'windows-edge-decline', 'networks-popup-be-discoverable', 'windows-start-menu', 'windows-qemu-drivers', 'windows-setup-browser', 'windows-user-acount-ctl-yes'], timeout => 120);
-        while (not match_has_tag('windows-desktop')) {
-            assert_and_click "windows-user-acount-ctl-yes" if (match_has_tag 'windows-user-acount-ctl-yes');
-            assert_and_click 'windows-edge-welcome' if (match_has_tag 'windows-edge-welcome');
-            assert_and_click 'windows-setup-browser' if (match_has_tag 'windows-setup-browser');
-            assert_and_click 'network-discover-yes' if (match_has_tag 'networks-popup-be-discoverable');
-            assert_and_click 'windows-edge-decline' if (match_has_tag 'windows-edge-decline');
-            assert_and_click 'windows-start-menu' if (match_has_tag 'windows-start-menu');
-            assert_and_click 'windows-qemu-drivers' if (match_has_tag 'windows-qemu-drivers');
-            wait_still_screen stilltime => 15, timeout => 30;
-            assert_screen(['windows-edge-welcome', 'windows-desktop', 'windows-edge-decline', 'networks-popup-be-discoverable', 'windows-start-menu', 'windows-qemu-drivers', 'windows-setup-browser', 'windows-user-acount-ctl-yes'], timeout => 30);
-        }
-    } else {
-        record_info("Win boot", "Windows started properly");
-        assert_screen ['finish-setting', 'windows-desktop'], 240;
-        if (match_has_tag 'finish-setting') {
-            assert_and_click 'finish-setting';
-        }
+    record_info("Win boot", "Windows started properly");
+    assert_screen ['finish-setting', 'windows-desktop'], 240;
+    if (match_has_tag 'finish-setting') {
+        assert_and_click 'finish-setting';
     }
 }
 
