84870c633e8277fc59b224b1ea36d2e1d2623420
Santiago Zarate <santiago.zarate@suse.com>
2025-03-24 14:00:47 +0100

    Add test for gnome-console (kgx)

---
+++ tests/x11/gnome_console.pm
@@ -0,0 +1,30 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package: gnome-console
+# Summary: Basic functionality of gnome console
+# - Launch "gnome-console" and confirm it is running
+# - Open a second tab
+# - Type "If you can see this text gnome-console is working."
+# - Close gnome-console
+# Maintainer: Santiago Zarate <santiago.zarate@suse.com>
+
+use base "x11test";
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    my ($self) = @_;
+    mouse_hide(1);
+    x11_start_program('kgx');
+    send_key "ctrl-shift-t";
+    assert_screen "gnome-console-second-tab";
+    $self->enter_test_text('gnome-console', cmd => 1);
+    assert_screen 'test-gnome_console-1';
+    send_key 'alt-f4';
+}
+
+1;
