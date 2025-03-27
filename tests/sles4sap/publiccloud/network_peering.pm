b5ead72e90f1c4b2d4236bf6c7c6f111e539e5dd
Bill Anastasiadis <blee571575@live.com>
2025-03-27 19:33:44 +0100

    Enable terraform peering for Azure

--- tests/sles4sap/publiccloud/network_peering.pm
+++ tests/sles4sap/publiccloud/network_peering.pm
@@ -21,16 +21,21 @@ sub run {
     # Needed to have peering and ansible state propagated in post_fail_hook
     $self->import_context($run_args);
 
-    die 'Network peering already in place' if ($self->{network_peering_present});
-    my $ibs_mirror_resource_group = get_required_var('IBSM_RG');
-    if (is_azure) {
-        my $rg = qesap_az_get_resource_group();
-        qesap_az_vnet_peering(source_group => $rg, target_group => $ibs_mirror_resource_group);
-    } elsif (is_ec2) {
-        my $vpc_id = qesap_aws_get_vpc_id(resource_group => $self->deployment_name() . '*');
-        die "No vpc_id in this deployment" if $vpc_id eq 'None';
-        my $ibs_mirror_target_ip = get_required_var('IBSM_IPRANGE');    # '10.254.254.240/28'
-        die 'Error in network peering setup.' if !qesap_aws_vnet_peering(target_ip => $ibs_mirror_target_ip, vpc_id => $vpc_id, mirror_tag => get_var('IBSM_PRJ_TAG', 'IBS Mirror'));
+    if (get_var('IBSM_VNET')) {
+        record_info('PEERING MANAGED', 'Peering should already be created by terraform');
+    }
+    else {
+        die 'Network peering already in place' if ($self->{network_peering_present});
+        my $ibs_mirror_resource_group = get_required_var('IBSM_RG');
+        if (is_azure) {
+            my $rg = qesap_az_get_resource_group();
+            qesap_az_vnet_peering(source_group => $rg, target_group => $ibs_mirror_resource_group);
+        } elsif (is_ec2) {
+            my $vpc_id = qesap_aws_get_vpc_id(resource_group => $self->deployment_name() . '*');
+            die "No vpc_id in this deployment" if $vpc_id eq 'None';
+            my $ibs_mirror_target_ip = get_required_var('IBSM_IPRANGE');    # '10.254.254.240/28'
+            die 'Error in network peering setup.' if !qesap_aws_vnet_peering(target_ip => $ibs_mirror_target_ip, vpc_id => $vpc_id, mirror_tag => get_var('IBSM_PRJ_TAG', 'IBS Mirror'));
+        }
     }
     $run_args->{network_peering_present} = $self->{network_peering_present} = 1;
 }
