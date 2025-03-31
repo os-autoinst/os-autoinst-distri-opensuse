dc2440a151693ea9682ee2bf6a8d78ca803d1a58
Santiago Zarate <santiago.zarate@suse.com>
2025-03-31 10:47:09 +0200

    Add disk boot module for sdboot and grub-bls

---
+++ tests/installation/disk_boot.pm
@@ -0,0 +1,42 @@
+# SUSE's openQA tests
+#
+# Copyright 2016-2018 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Boot from disk
+# Maintainer: Santiago Zarate <santiago.zarate@suse.com>
+
+use base "bootbasetest";
+use strict;
+use warnings;
+use testapi;
+use Utils::Architectures qw(is_aarch64);
+use utils qw(is_uefi_boot);
+
+sub run {
+
+    # default timeout in grub2 is set to 10s
+    # Sometimes, machines tend to stall when trying to match grub2
+    # this leads to test failures because openQA does not assert grub2 properly
+    # KEEP_GRUB_TIMEOUT=0 will force the grub needle to match, useful when booting
+    # pre-configured images with disabled timeout. See opensusebasetest::handle_grub
+    if (is_uefi_boot || is_aarch64) {
+        shift->wait_boot(bootloader_time => 300);
+    } else {
+        die "This test module is meant for uefi capable systems";
+    }
+}
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+sub post_fail_hook {
+    assert_screen 'emergency-mode';
+    send_key 'ret';
+    enter_cmd "echo '##### initramfs logs #####'> /dev/$serialdev";
+    script_run "cat /run/initramfs/rdsosreport.txt > /dev/$serialdev ";
+    enter_cmd "echo '##### END #####'> /dev/$serialdev";
+}
+
+1;
