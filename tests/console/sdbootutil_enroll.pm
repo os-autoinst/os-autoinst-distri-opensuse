64ba7bf58c9a5d61b05590400b625eed552710a4
Santiago Zarate <santiago.zarate@suse.com>
2025-04-01 14:26:25 +0200

    Add test to enroll TPM in FDE

---
+++ tests/console/sdbootutil_enroll.pm
@@ -0,0 +1,21 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+# Package: sdbootutil
+# Summary: Enroll TPM to unlock encrypted disk
+# Maintainer: Santiago Zarate <santiago.zarate@suse.com>
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils;
+
+sub run {
+    select_console 'root-console';
+    zypper_call('in expect');
+    assert_script_run("expect -c 'spawn sdbootutil enroll --method tpm2; expect \"Password for /dev/.*:\";send $testapi::password\\n;interact'");
+}
+
+1;
