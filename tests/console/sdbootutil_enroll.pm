9b68317c7dd6ee6318c2fe42a3955f0af35112d3
Santiago Zarate <santiago.zarate@suse.com>
2025-04-02 16:03:48 +0200

    Reboot system after TPM enrollment

--- tests/console/sdbootutil_enroll.pm
+++ tests/console/sdbootutil_enroll.pm
@@ -11,12 +11,18 @@ use strict;
 use warnings;
 use testapi;
 use utils;
+use power_action_utils 'power_action';
 
 sub run {
     select_console 'root-console';
     zypper_call('in expect');
     assert_script_run("expect -c 'spawn sdbootutil enroll --method tpm2; expect \"Password for /dev/.*:\";send $testapi::password\\n;interact'");
     set_var("RUNTIME_TPM_ENROLLED", 1);
+    power_action('reboot', textmode => 1, keepconsole => 1);
+    shift->wait_boot(bootloader_time => 300);
+    # since this is a consoletest, allow post_run hook to run freely
+    # by switching back to root-console
+    select_console 'root-console';
 }
 
 1;
