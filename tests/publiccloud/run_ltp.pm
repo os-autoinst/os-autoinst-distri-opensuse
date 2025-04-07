6c7bd1c59b0da51ef2fd78b91f0b6f483b90be99
Ondřej Súkup <osukup@suse.com>
2025-04-07 20:33:36 +0200

    Record commit hash of kirk repo used in test
    https://progress.opensuse.org/issues/179311

--- tests/publiccloud/run_ltp.pm
+++ tests/publiccloud/run_ltp.pm
@@ -157,6 +157,9 @@ sub run {
     assert_script_run($log_start_cmd);
 
     assert_script_run("cd kirk");
+    my $ghash = script_output("git rev-parse HEAD", proceed_on_failure => 1);
+    set_var("LTR_RUN_NG_GIT_HASH", $ghash);
+    record_info("KIRK_GIT_HASH", "$ghash");
     assert_script_run("python3.11 -m venv env311");
     assert_script_run("source env311/bin/activate");
     assert_script_run("pip3.11 install asyncssh msgpack");
