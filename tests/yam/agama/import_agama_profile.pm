bc9bfc97e7089c3802bfcd127136b1fc0c778de1
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-04-10 16:35:09 +0200

    Adapt import_agama_profile test to use dynamic JSON
    Squashed commit of the following:
    
    commit 804efd2f336be6e8e8a660d7a8297102e7ad6d31
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Wed Mar 19 16:35:57 2025 +0100
    
        Adapt import_agama_profile step to dynamic JSON profiles
    
        Add support to generate templates to import
    
        Fix variable definition
    
        Added function import
    
        Fix minor typo

--- tests/yam/agama/import_agama_profile.pm
+++ tests/yam/agama/import_agama_profile.pm
@@ -8,12 +8,16 @@ use base Yam::Agama::patch_agama_base;
 use strict;
 use warnings;
 use testapi qw(assert_script_run data_url get_required_var select_console script_run);
-use autoyast qw(expand_agama_profile);
+use autoyast qw(expand_agama_profile generate_json_profile);
 
 sub run {
-    my $profile = expand_agama_profile(get_required_var('AGAMA_PROFILE'));
+    my $profile = get_required_var('AGAMA_PROFILE');
+    my $profile_url = ($profile =~ /\.libsonnet/) ?
+      generate_json_profile($profile) :
+      expand_agama_profile($profile);
+
     select_console 'root-console';
-    assert_script_run("agama profile import $profile", timeout => 300);
+    assert_script_run("agama profile import $profile_url", timeout => 300);
 }
 
 1;
