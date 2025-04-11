4498621dfe8cc5c516f3a93f21e505ee130d8c71
KuxaBeast <kuxa@darkne.dev>
2025-04-11 14:45:47 +0200

    Fix podmansh on multiple occasions
    Fixes for MicroOS, JeOS, Tumbleweed. Removes sideeffects, reinitializes
    consoles.

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -148,8 +148,8 @@ sub load_host_tests_podman {
     # exclude rootless podman on public cloud because of cgroups2 special settings
     unless (is_openstack || is_public_cloud) {
         loadtest 'containers/rootless_podman';
+        loadtest 'containers/podmansh' unless (is_staging || is_leap("<16") || is_sle("<16") || is_sle_micro("<6.1") || is_leap_micro("<6.1"));
         loadtest 'containers/podman_remote' if is_sle_micro('5.5+');
-        # loadtest 'containers/podmansh' unless (is_staging || is_leap("<16") || is_sle("<16") || is_sle_micro("<6.1") || is_leap_micro("<6.1"));
     }
     # Buildah is not available in SLE Micro, MicroOS and staging projects
     load_buildah_tests($run_args) unless (is_sle('<15') || is_sle_micro || is_microos || is_leap_micro || is_staging);
