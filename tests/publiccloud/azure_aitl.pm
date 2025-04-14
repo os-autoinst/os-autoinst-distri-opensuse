078803e4cdbf71a12b7bbe9650b80806f71fbb3d
Anton Smorodskyi <asmorodskyi@suse.com>
2025-04-14 12:50:13 +0200

    Make version of AITL configurable

--- tests/publiccloud/azure_aitl.pm
+++ tests/publiccloud/azure_aitl.pm
@@ -32,7 +32,7 @@ sub run {
     my $resource_group = "openqa-aitl-$job_id";
     my $subscription_id = $provider->provider_client->subscription;
 
-    my $aitl_client_version = "20241118.1";
+    my $aitl_client_version = get_required_var("PUBLIC_CLOUD_AZURE_AITL_VER");
     my $aitl_image_gallery = "test_image_gallery";
     my $aitl_image_version = "latest";
     my $aitl_job_name = "openqa-aitl-$job_id";
