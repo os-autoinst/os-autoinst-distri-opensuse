5f00350fbbaa4dc64265b2a027b63e5d1d6d5cac
Volodymyr Katkalov <volodymyr.katkalov@suse.com>
2025-04-16 18:04:59 +0300

    Fix Azure AITL success

--- tests/publiccloud/azure_aitl.pm
+++ tests/publiccloud/azure_aitl.pm
@@ -162,6 +162,10 @@ sub json_to_xml {
     $dom->toFile(hashed_string('aitl_results.xml'), 1);
 }
 
+sub _upload_logs {
+    return 1;
+}
+
 sub _cleanup {
     # because it is AITL where we don't create actual instance no point to call _cleanup
 }
