c77694720cf877c98c9f400b545414847e74eaae
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-04-23 16:04:56 +0200

    Added repository validation

---
+++ tests/yam/validate/validate_repositories.pm
@@ -0,0 +1,28 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that the repositories are set properly by Agama.
+# Check expected repositories against test data and they should be enabled.
+
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+use scheduler 'get_test_suite_data';
+use Test::Assert ':assert';
+use utils;
+
+sub run {
+    select_console 'root-console';
+    my @expected_repositories = get_test_suite_data()->{repositories};
+    my $repositories = zypper_call("repos --show-enabled-only");
+    for my $repo (@expected_repositories) {
+        $repo =~ $repositories ? diag("Repository $repo is enabled.") : die "Repostory $repo is not enabled on the host"
+    }
+}
+
+1;
