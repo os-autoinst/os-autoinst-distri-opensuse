eb5f9bc96dada188b6303a197d8f558c1f194b41
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-04-24 12:40:42 +0200

    Changed command to script_output

--- tests/yam/validate/validate_repositories.pm
+++ tests/yam/validate/validate_repositories.pm
@@ -19,8 +19,8 @@ use utils;
 sub run {
     select_console 'root-console';
     my @expected_repositories = @{get_test_suite_data()->{repositories}};
-    my $repositories = `zypper repos --show-enabled-only`;
-    diag("Host repository info: [$repositories]");
+    my $repositories = script_output("zypper repos --show-enabled-only");
+    diag("Host repository info: \n$repositories\n");
     for (@expected_repositories) {
         $repositories =~ $_ ? diag("Repository $_ is enabled.") : die "Repostory $_ is not enabled on the host";
     }
