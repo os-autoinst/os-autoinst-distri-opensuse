e8bf1bd8470f38146c32f6283a470ed6a96bb6b4
lemon.li <leli@suse.com>
2025-04-28 06:08:41 -0400

    Resize partitions and make home encryption

---
+++ lib/Yam/Agama/Pom/EnterPassphraseForHomePage.pm
@@ -0,0 +1,29 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Handles entering passphrase for home encryption after grub.
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+package Yam::Agama::Pom::EnterPassphraseForHomePage;
+use strict;
+use warnings;
+use testapi;
+
+sub new {
+    my ($class, $args) = @_;
+    return bless {
+        enter_passphrase_base => $args->{enter_passphrase_base},
+        tag_enter_passphrase_for_home_partition => 'enter-passphrase-for-home-partition',
+    }, $class;
+}
+
+sub expect_is_shown {
+    my ($self) = @_;
+    assert_screen($self->{tag_enter_passphrase_for_home_partition}, 60);
+}
+
+sub enter { shift->{enter_passphrase_base}->enter() }
+
+1;
