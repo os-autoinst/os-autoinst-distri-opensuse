84a602edc981bf241cff979df4c45e6cc2dc1d08
Sebastian Chlad <sebastian.chlad@suse.com>
2025-05-01 10:07:12 +0200

    Storage: Collect nvme test results

--- tests/kernel/blktests.pm
+++ tests/kernel/blktests.pm
@@ -51,7 +51,7 @@ sub run {
     #temp override of $tests and $devices
     if (is_sle(">=16")) {
         $tests = 'scsi,dm';
-        $devices = '/dev/sdb';
+        #$devices = '/dev/sdb';
     }
 
     my @tests = split(',', $tests);
