cdb9875b327a6b83c3e7db93ea29e41ece7585c4
linguini-dev <hristo.ilchev@suse.com>
2025-05-08 09:27:34 +0300

    dash-harbor

--- tests/containers/charts/privateregistry.pm
+++ tests/containers/charts/privateregistry.pm
@@ -59,16 +59,16 @@ sub run {
  
     # Smoketest - is everything Ready?
     foreach my $component (@private_registry_components) {
-      validate_script_output_retry("kubectl get pods -l component=$component", qr/$component/, retry => 10, delay => 30, timeout => 120, fail_message => "$release_name-$component didn't deploy");
+      validate_script_output_retry("kubectl get pods -l component=$component", qr/$component/, retry => 3, delay => 30, timeout => 120, fail_message => "$release_name-$component didn't deploy");
       my @pods = split(' ', script_output("kubectl get pods --no-headers -l component=$component"));
       my $full_pod_name = $pods[0];
-      assert_script_run("kubectl get pod $full_pod_name --no-headers -o 'jsonpath={.status.conditions[?(@.type==\"Ready\")].status}'", retry => 10, delay => 30, timeout => 120, fail_message => "$full_pod_name is not in the Ready state!");
+      assert_script_run("kubectl get pod $full_pod_name --no-headers -o 'jsonpath={.status.conditions[?(@.type==\"Ready\")].status}'", retry => 3, delay => 30, timeout => 120, fail_message => "$full_pod_name is not in the Ready state!");
       record_info("$component is ready", "$full_pod_name is in the Ready state.");
     }
     
     # Get the webui credentials & ingress url
-    my $registry_password = script_run("kubectl get secrets $release_name-core --template={{.data.HARBOR_ADMIN_PASSWORD}} | base64 -d -w 0");
-    my $registry_ingress_url = script_run("kubectl get ingress $release_name-ingress -o jsonpath='{.spec..host}'");
+    my $registry_password = script_run("kubectl get secrets $release_name-harbor-core --template={{.data.HARBOR_ADMIN_PASSWORD}} | base64 -d -w 0");
+    my $registry_ingress_url = script_run("kubectl get ingress $release_name-harbor-ingress -o jsonpath='{.spec..host}'");
 
     # Login 
     assert_script_run("podman login --username admin --password $registry_password --tls-verify=false");
