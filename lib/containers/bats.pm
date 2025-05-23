a78bfc48a91a8de25c68bd2a52cc8116a4cd9675
Ricardo Branco <rbranco@suse.de>
2025-05-23 09:21:57 +0200

    bats: Incorporate upstream patches into osado

--- lib/containers/bats.pm
+++ lib/containers/bats.pm
@@ -380,7 +380,12 @@ sub bats_patches {
     foreach my $patch (@patches) {
         my $url = ($patch =~ /^\d+$/) ? "https://github.com/$github_org/$package/pull/$patch.patch" : $patch;
         record_info("patch", $url);
-        run_command "curl $curl_opts -O $url", timeout => 900;
+        if ($patch =~ /^\d+$/) {
+            push @commands, "curl $curl_opts -O $url";
+            assert_script_run "curl -O " . data_url("containers/bats/patches/$package/$patch.patch");
+        } else {
+            run_command "curl $curl_opts -O $url", timeout => 900;
+        }
         run_command "git apply -3 --ours " . basename($url);
     }
 }
