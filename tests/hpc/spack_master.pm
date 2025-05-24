2f55f3c69ab83806579ed19cac43181e0b0029cd
Sebastian Chlad <sebastian.chlad@suse.com>
2025-05-24 13:13:10 +0200

    wip

--- tests/hpc/spack_master.pm
+++ tests/hpc/spack_master.pm
@@ -78,7 +78,7 @@ sub run ($self) {
     record_info('INFO', 'Run MPI over several nodes');
     sleep(6);
     my $nodes = join(',', @cluster_nodes);
-    sleep(6);
+    sleep(999999999);
     $rt = assert_script_run("$ld_library_path mpirun -n 2 --host slave-node00,slave-node01 -npernode 1 $exports_path{'bin'}/$mpi_bin -mpich-dbg=file -mpich-dbg-level=verbose --get-stack-traces --display-allocation", timeout => 240);    
     script_output('ls');
     sleep(6);
