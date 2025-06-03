ac176cc899a9e8ded4390161235012c5ef846e05
Ricardo Branco <rbranco@suse.de>
2025-06-03 18:29:36 +0200

    containers: Re-enable compose test on all architectures

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -108,9 +108,6 @@ sub load_compose_tests {
     my ($run_args) = @_;
     return if (is_staging);
     return unless (is_tumbleweed || is_microos);
-    # compose is only available on these arches:
-    # https://github.com/containers/podman/issues/21757
-    return unless (is_aarch64 || is_x86_64);
     loadtest('containers/compose', run_args => $run_args, name => $run_args->{runtime} . "_compose");
 }
 
