0bbce09c8b4f23242a5dc063c9a60d0e3a5993a1
Ricardo Branco <rbranco@suse.de>
2025-06-07 12:42:48 +0200

    bats: mkdir should not fail

--- lib/containers/bats.pm
+++ lib/containers/bats.pm
@@ -269,7 +269,7 @@ sub bats_post_hook {
     select_serial_terminal;
 
     my $log_dir = "/tmp/logs/";
-    assert_script_run "mkdir -p $log_dir";
+    assert_script_run "mkdir -p $log_dir || true";
     assert_script_run "cd $log_dir";
 
     script_run "rm -rf $test_dir";
