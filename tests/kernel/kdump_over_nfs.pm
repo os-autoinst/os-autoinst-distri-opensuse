38834926be8c22c3d321c02a47ae4c67df75dcf9
Sebastian Chlad <sebastian.chlad@suse.com>
2025-06-12 17:42:37 +0200

    Add kdump over NFS test
    The patch is introducing KDUMP check over NFS where NFS
    multimachine tests are re-used, KDUMP is configured for
    writing to the mounted NFS share and then all is checked
    on the nfs server side - if the dump is there and if it
    is readable etc.

---
+++ tests/kernel/kdump_over_nfs.pm
@@ -0,0 +1,53 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Configure and run kdump over NFS
+# Maintainer: QE Kernel <kernel-qa@suse.de>
+
+use base "opensusebasetest";
+use strict;
+use warnings;
+use testapi;
+use utils;
+use kdump_utils;
+use lockapi;
+
+sub run {
+    my ($self) = @_;
+    my $role = get_required_var('ROLE');
+    my $local_nfs3 = get_var('NFS_LOCAL_NFS3', '/home/localNFS3');
+
+    select_console('root-console');
+
+    if ($role eq 'nfs_client') {
+        set_kdump_config($local_nfs3);
+    }
+
+    barrier_wait("KDUMP_PROVISIONED");
+
+    if ($role eq 'nfs_client') {
+        record_info('CLINET');
+    }
+
+    barrier_wait("KDUMP_TRIGGERED");
+
+    if ($role eq 'nfs_server') {
+        record_info('SERVER');
+    }
+
+
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+
+    script_run 'ls -lah /boot/';
+    script_run 'tar -cvJf /tmp/crash_saved.tar.xz -C /var/crash .';
+    upload_logs '/tmp/crash_saved.tar.xz';
+
+    $self->SUPER::post_fail_hook;
+}
+
+1;
