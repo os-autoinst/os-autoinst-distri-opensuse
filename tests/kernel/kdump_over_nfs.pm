b599ac3eb3576ff484ed07d7e9276fb3040cd2ca
Sebastian Chlad <sebastian.chlad@suse.com>
2025-06-12 21:01:19 +0200

    wip

--- tests/kernel/kdump_over_nfs.pm
+++ tests/kernel/kdump_over_nfs.pm
@@ -17,12 +17,19 @@ use lockapi;
 sub run {
     my ($self) = @_;
     my $role = get_required_var('ROLE');
-    my $local_nfs3 = get_var('NFS_LOCAL_NFS3', '/home/localNFS3');
 
     select_console('root-console');
 
+    #kdump does not re-use exisiting mounted shares;
+    #if ($role eq 'nfs_server') {
+    #    my kdump_mount = "/nfs/kdump";
+    #    assert_script_run "mkdir -p $kdump_mount";
+    #    assert_script_run "chmod 777 $kdump_mount";
+    #    assert_script_run "echo $kdump_mount $cl\\($permissions\\) >> /etc/exports";
+    #}
+
     if ($role eq 'nfs_client') {
-        set_kdump_config('KDUMP_SAVEDIR', $local_nfs3);
+        set_kdump_config('KDUMP_SAVEDIR', 'nfs://server-node00:/nfs/shared_nfs3');
     }
 
     barrier_wait("KDUMP_PROVISIONED");
