91ddf7b03508bcf824d674ff13bf35573084007c
Santiago Zarate <santiago.zarate@suse.com>
2025-06-16 12:48:55 +0200

    Check for systemd module

--- tests/console/dracut.pm
+++ tests/console/dracut.pm
@@ -23,7 +23,7 @@ sub run {
 
     validate_script_output("lsinitrd", sub { m/Image:(.*\n)+( ?)Version: dracut(-|\d+|\.|\+|\w+)+(\n( ?))+( ?)Arguments(.*\n)+( ?)dracut modules:(\w+|-|\d+|\n|( ?))+\=+\n(l|d|r|w|x|-|( ?))+\s+\d+ root\s+root(.*\n)+( ?)\=+/ });
     assert_script_run("dracut -f", 180);
-    assert_script_run("dracut --list-modules");
+    validate_script_output("dracut --list-modules", sub { m/systemd/ });
 
     power_action('reboot', textmode => 1);
     $self->wait_boot(bootloader_time => 200);
