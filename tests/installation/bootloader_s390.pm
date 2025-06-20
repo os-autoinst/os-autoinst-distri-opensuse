a51a164cd8b718cc82bd02fd5bd382db33f6ea5e
Chenzi Cao <chcao@suse.com>
2025-06-20 06:59:55 +0200

    Shorten the url for agama dev s390x zvm cases
    To fix this issue: https://openqa.suse.de/tests/18146012#step/boot_agama/30
    Shorten the url for agama dev s390x zvm cases.

--- tests/installation/bootloader_s390.pm
+++ tests/installation/bootloader_s390.pm
@@ -91,7 +91,7 @@ sub prepare_parmfile {
     else {
         if (get_var('AGAMA')) {
             my $host = "ftp://" . get_var('REPO_HOST', 'openqa');
-            my $root_line = " root=live:" . ((get_var('FLAVOR') eq "Full") ?
+            my $root_line = " root=live:" . ((get_var('FLAVOR') =~ /^(Full|agama-installer)$/) ?
                   shorten_url($host . '/' . get_required_var('REPO_0') . "/LiveOS/squashfs.img") :
                   $host . '/' . get_var('REPO_999'));
             $params .= $root_line;
