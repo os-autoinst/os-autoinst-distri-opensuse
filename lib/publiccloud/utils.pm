6c254cec394bb9d5aafeea04b38dec9929bf68c1
Pavel Dostál <pdostal@suse.cz>
2025-07-03 14:08:07 +0200

    PC: Fix sshd grep

--- lib/publiccloud/utils.pm
+++ lib/publiccloud/utils.pm
@@ -385,14 +385,12 @@ sub prepare_ssh_tunnel {
     assert_script_run("install -o $testapi::username -g users -m 0600 ~/.ssh/* /home/$testapi::username/.ssh/");
 
     # Permit root passwordless login and TCP forwarding over SSH
-    if ($instance->ssh_script_run("sudo sshd -G | grep -i 'permitrootlogin prohibit-password'") != 0 && $instance->ssh_script_run("sudo sshd -G | grep -i 'allowtcpforwarding yes'") != 0) {
-        if (is_sle('>=16')) {
-            $instance->ssh_assert_script_run(q(echo "PermitRootLogin prohibit-password" | sudo tee /etc/ssh/sshd_config.d/10-root-login.conf));
-            $instance->ssh_assert_script_run(q(echo "AllowTcpForwarding yes" | sudo tee /etc/ssh/sshd_config.d/10-tcp-forwarding.conf));
-        } else {
-            $instance->ssh_assert_script_run('sudo sed -i "s/PermitRootLogin no/PermitRootLogin prohibit-password/g" /etc/ssh/sshd_config');
-            $instance->ssh_assert_script_run('sudo sed -i "/^AllowTcpForwarding/c\AllowTcpForwarding yes" /etc/ssh/sshd_config') if (is_hardened());
-        }
+    if (is_sle('>=16')) {
+        $instance->ssh_assert_script_run(q(echo "PermitRootLogin without-password" | sudo tee /etc/ssh/sshd_config.d/10-root-login.conf));
+        $instance->ssh_assert_script_run(q(echo "AllowTcpForwarding yes" | sudo tee /etc/ssh/sshd_config.d/10-tcp-forwarding.conf));
+    } else {
+        $instance->ssh_assert_script_run('sudo sed -i "s/PermitRootLogin no/PermitRootLogin prohibit-password/g" /etc/ssh/sshd_config');
+        $instance->ssh_assert_script_run('sudo sed -i "/^AllowTcpForwarding/c\AllowTcpForwarding yes" /etc/ssh/sshd_config') if (is_hardened());
     }
     $instance->ssh_assert_script_run('sudo systemctl reload sshd');
     record_info('sshd -G', $instance->ssh_script_output('sudo sshd -G', proceed_on_failure => 1));
