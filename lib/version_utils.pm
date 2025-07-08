e970cd40ae7a43f97eee16f5f990b08dabb4bc68
Santiago Zarate <santiago.zarate@suse.com>
2025-07-08 10:32:18 +0200

    Microos image uses grub2

--- lib/version_utils.pm
+++ lib/version_utils.pm
@@ -868,7 +868,7 @@ sub get_bootloader {
 
     return 'grub2' if !check_var('UEFI', 1);
     return 'grub2' if is_upgrade;
-    return 'systemd-boot' if is_microos && !(get_var('FLAVOR', '') =~ /(Image-ContainerHost|JeOS-for-kvm-and-xen|JeOS-for-OpenStack-Cloud)$/);
+    return 'systemd-boot' if is_microos && !(get_var('FLAVOR', '') =~ /(MicroOS-Image|Image-ContainerHost|JeOS-for-kvm-and-xen|JeOS-for-OpenStack-Cloud)$/);
     return 'grub2';
 }
 
