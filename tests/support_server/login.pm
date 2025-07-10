9fd453aeed2a758f0de757bd78a58b56d245da2c
Santiago Zarate <santiago.zarate@suse.com>
2025-07-10 14:46:56 +0200

    Allow overriding of bootloader for support_server
    This allows booting the support server image, that can be different
    from the expected bootloader, e.g because of remote installation where
    one machine is booting a different operating system, to the one that is
    about to be installed.

--- tests/support_server/login.pm
+++ tests/support_server/login.pm
@@ -6,23 +6,40 @@
 
 use strict;
 use warnings;
-use base 'basetest';
 use base 'opensusebasetest';
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use utils;
-use version_utils qw(is_desktop_installed is_tumbleweed);
+use version_utils qw(is_desktop_installed is_tumbleweed is_bootloader_grub2);
 
 sub run {
     my ($self) = @_;
     my $timeout = (is_tumbleweed) ? 180 : 80;
+
+    $self->override_default_bootloader unless is_bootloader_grub2;
+
     # we have some tests that waits for dvd boot menu timeout and boot from hdd
     # - the timeout here must cover it
     $self->wait_boot(bootloader_time => $timeout, textmode => !is_desktop_installed);
 
+    $self->restore_default_bootloader if is_bootloader_grub2 && $self->{bootloader_overriden};
+
     # the supportserver image can be different version than the currently tested system
     # so try to login without use of needles
     select_serial_terminal;
+}
+
+sub override_default_bootloader {
+    my ($self) = @_;
+    $self->{bootloader_overriden} = 1;
+    $self->{bootloader} = get_var('BOOTLOADER');
+    set_var('BOOTLOADER', 'grub2');
+}
+
+sub restore_default_bootloader {
+    my ($self) = @_;
+    $self->{bootloader_overriden} = 0;
+    set_var('BOOTLOADER', $self->{bootloader});
 }
 
 sub test_flags {
@@ -30,4 +47,3 @@ sub test_flags {
 }
 
 1;
-
