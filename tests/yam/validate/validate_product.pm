2aa9c42234c9f45709981afb49158002918cbef3
Chenzi Cao <chcao@suse.com>
2025-07-10 15:18:03 +0200

    Add more validations for the output of /etc/os-release
    Add more validations for the output of /etc/os-release for both sles and
    sap.

--- tests/yam/validate/validate_product.pm
+++ tests/yam/validate/validate_product.pm
@@ -16,13 +16,29 @@ use scheduler 'get_test_suite_data';
 
 sub run {
     select_console 'root-console';
-
-    my $os_release_name_expected = get_test_suite_data()->{os_release_name};
-
+    my $test_suite_data = get_test_suite_data();
     my $os_release_output = script_output('cat /etc/os-release');
-    my $os_release_name = Config::Tiny->read_string($os_release_output)->{_}->{NAME};
+    my $os_release = Config::Tiny->read_string($os_release_output)->{_};
 
-    assert_equals("\"" . $os_release_name_expected . "\"", $os_release_name, 'Wrong product NAME in /etc/os-release');
+    my %fields = (
+        NAME                 => $test_suite_data->{NAME},
+        PRETTY_NAME          => $test_suite_data->{PRETTY_NAME},
+        VARIANT              => $test_suite_data->{VARIANT},
+        VARIANT_ID           => $test_suite_data->{VARIANT_ID},
+        VERSION_ID           => $test_suite_data->{VERSION_ID},
+        ID                   => $test_suite_data->{ID},
+        ID_LIKE              => $test_suite_data->{ID_LIKE},
+        SUSE_SUPPORT_PRODUCT => $test_suite_data->{SUSE_SUPPORT_PRODUCT},
+    );
+
+    foreach my $key (keys %fields) {
+        next unless defined $fields{$key};
+        record_info $key, "Checking $key in /etc/os-release";
+        my $expected = $fields{$key};
+        my $actual = $os_release->{$key};
+        assert_defined($actual, "$key is defined in /etc/os-release");
+        assert_equals($expected, $actual, "Wrong product $key in /etc/os-release (expected='$expected', got='$actual')");
+    }
 }
 
 1;
