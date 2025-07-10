2f2f5a008770b766b2122cb932027ed272f0d689
Paolo Stivanin <info@paolostivanin.com>
2025-07-10 11:00:50 +0200

    oqs_provider: add more ciphers
    Ticket: https://progress.opensuse.org/issues/185242

--- tests/security/oqs_provider_openssl.pm
+++ tests/security/oqs_provider_openssl.pm
@@ -16,13 +16,14 @@ use version_utils qw(is_sle);
 sub run {
     select_serial_terminal;
 
-    if (zypper_call("--no-refresh if oqs-provider") != 0) {
-        record_info('SKIPPING TEST', "Skipping test due to missing oqs-provider package.");
-    } else {
-        zypper_call("in gcc wget cmake openssl oqs-provider");
+    if (script_run('zypper se oqs-provider') == 104) {
+        record_info('SKIPPING TEST', "oqs-provider is not available on this system.");
+        return;
+    }
+    zypper_call("in openssl oqs-provider");
 
-        my $conf_file = '/etc/ssl/oqs-openssl.cnf';
-        my $conf = <<EOF;
+    my $conf_file = '/etc/ssl/oqs-openssl.cnf';
+    my $conf = <<EOF;
 openssl_conf = openssl_init
 
 [openssl_init]
@@ -36,24 +37,27 @@ oqs = oqs_sect
 activate = 1
 
 [oqs_sect]
+activate = 1
 module = /usr/lib64/ossl-modules/oqsprovider.so
 EOF
 
-        script_output("echo '$conf' >> $conf_file");
-        assert_script_run("export OPENSSL_CONF=$conf_file");
-        assert_script_run("openssl list -provider oqs -public-key-algorithms | grep -q dilithium2");
+    script_output("echo '$conf' >> $conf_file");
+    assert_script_run("export OPENSSL_CONF=$conf_file");
 
-        my $key_path = "/root/dilithium2-key.pem";
-        assert_script_run("openssl genpkey -provider oqs -algorithm dilithium2 -out $key_path");
-        # Sign a message with the generated key
-        my $test_file = "/tmp/input.txt";
-        my $sig_file = "/tmp/input.sig";
-        assert_script_run("echo 'openQA test' > $test_file");
-        assert_script_run("openssl pkeyutl -sign -provider oqs -inkey $key_path -out $sig_file -in $test_file");
+    my $output = script_output("openssl list -provider oqs -public-key-algorithms | grep oqs | grep -E 'p256_mayo2|x25519_frodo640aes|p256_bikel1|rsa3072_falcon512'");
+    my ($algo) = $output =~ /(p256_mayo2|x25519_frodo640aes|p256_bikel1|rsa3072_falcon512)/;
+    defined $algo ? record_info("Selected algo: $algo") : die "No expected OQS algorithm found!";
+    my $key_path = "/root/$algo-key.pem";
 
-        # Verify the signature using the same key
-        assert_script_run("openssl pkeyutl -verify -provider oqs -inkey $key_path -sigfile $sig_file -in $test_file");
-    }
+    assert_script_run("openssl genpkey -provider oqs -algorithm $algo -out $key_path");
+    # Sign a message with the generated key
+    my $test_file = "/tmp/input.txt";
+    my $sig_file = "/tmp/input.sig";
+    assert_script_run("echo 'openQA test' > $test_file");
+    assert_script_run("openssl pkeyutl -sign -provider oqs -inkey $key_path -out $sig_file -in $test_file");
+
+    # Verify the signature using the same key
+    assert_script_run("openssl pkeyutl -verify -provider oqs -inkey $key_path -sigfile $sig_file -in $test_file");
 }
 
 sub test_flags {
