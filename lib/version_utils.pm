86b6555580b39e5d76213da684ce7442058481b3
Santiago Zarate <santiago.zarate@suse.com>
2025-07-14 16:06:35 +0200

    Switch Tumbleweed to use grub2-bls

--- lib/version_utils.pm
+++ lib/version_utils.pm
@@ -876,7 +876,10 @@ sub get_bootloader {
 
     return 'grub2' if !check_var('UEFI', 1);
     return 'grub2' if is_upgrade;
-    return 'systemd-boot' if is_microos && !(get_var('FLAVOR', '') =~ /(MicroOS-SelfInstall|MicroOS-Image|Image-ContainerHost|JeOS-for-kvm-and-xen|JeOS-for-OpenStack-Cloud)$/);
+    return 'grub2' if (get_var('FLAVOR', '') =~ /(MicroOS-SelfInstall|MicroOS-Image|Image-ContainerHost|JeOS-for-kvm-and-xen|JeOS-for-OpenStack-Cloud)$/);
+    return 'systemd-boot' if is_microos;
+    return 'grub2' if is_slowroll;
+    return 'grub2-bls' if !is_sle && !is_leap;
     return 'grub2';
 }
 
