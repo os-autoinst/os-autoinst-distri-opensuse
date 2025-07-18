41c15bd3c2d4565a6ff813906dff48d52d21a84a
lilyeyes <llzhao@suse.com>
2025-07-18 11:18:22 +0800

    Add POD to lib haclusterbasetest
    TEAM-10497 - [timeboxed]Add POD documentation to lib/haclusterbasetest

--- lib/haclusterbasetest.pm
+++ lib/haclusterbasetest.pm
@@ -18,7 +18,20 @@ use Utils::Logging qw(export_logs);
 use version_utils 'is_sle';
 use x11utils qw(ensure_unlocked_desktop);
 
+=head1 SYNOPSIS
+
+Base class for HA Cluster tests.
+=cut
+
 our $prev_console;
+
+=head2 pre_run_hook
+
+    pre_run_hook()
+
+This is one of the test module interfaces C<'pre_run_hook()'>.
+It sets C<'$prev_console'> before running test steps.
+=cut
 
 sub pre_run_hook {
     my ($self) = @_;
@@ -28,6 +41,14 @@ sub pre_run_hook {
     $prev_console = $testapi::selected_console;
     record_info(__PACKAGE__ . ':' . 'pre_run_hook' . ' ' . "prev_console=$prev_console");
 }
+
+=head2 post_run_hook
+
+    post_run_hook()
+
+This is one of the test module interfaces C<'post_run_hook()'>.
+It restores/unlocks C<'$prev_console'>, clear the console and ensure that it really got cleareafter.
+=cut
 
 sub post_run_hook {
     my ($self) = @_;
@@ -43,6 +64,14 @@ sub post_run_hook {
     }
 }
 
+=head2 post_fail_hook
+
+    post_fail_hook()
+
+This is one of the test module interfaces C<'post_fail_hook()'>.
+It saves a screenshot and logs.
+=cut
+
 sub post_fail_hook {
     my ($self) = @_;
     record_info(__PACKAGE__ . ':' . 'post_fail_hook');
@@ -54,6 +83,17 @@ sub post_fail_hook {
     ha_export_logs;
     export_logs;
 }
+
+=head2 test_flags
+
+    test_flags()
+
+This is one of the test module interfaces C<'test_flags()'>.
+
+  - Specifies what should happen when test execution of the current test module is finished depending on the result.
+  - Set C<'milestone=1'>: after this test succeeds, update the 'lastgood' snapshot of the SUT
+  - Set C<'fatal=1'>: when set to 1 the whole test suite is aborted if the test module fails. The overall state is set to failed.
+=cut
 
 sub test_flags {
     return {milestone => 1, fatal => 1};
