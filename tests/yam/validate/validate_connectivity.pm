caa6b0d986e24500c9d073f48d6efd603d7b6574
Leo Manfredi <leo.manfredi@suse.com>
2025-07-18 07:35:07 +0200

    Fix: add check to re-check the connectivity and unknow as output
    Related ticket: https://progress.opensuse.org/issues/184043
    
    Add "check" to refresh the status and "unknown" as output
    
    Add unknow

--- tests/yam/validate/validate_connectivity.pm
+++ tests/yam/validate/validate_connectivity.pm
@@ -17,8 +17,8 @@ sub run {
     select_console 'root-console';
     my $ip_address_show = script_output("ip address show");
     record_info("ip address show", $ip_address_show);
-    my $connectivity = check_var('INST_COPY_NETWORK', '0') ? 'none' : 'full';
-    validate_script_output("nmcli networking connectivity", sub { m/\b$connectivity\b/ });
+    my $connectivity = check_var('INST_COPY_NETWORK', '0') ? 'none|unknown' : 'full';
+    validate_script_output("nmcli networking connectivity check", sub { m/\b$connectivity\b/ });
 }
 
 1;
