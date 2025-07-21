db648bcb6f4db18f956299249fa4f30615752f91
m-dati <mdati@suse.com>
2025-07-21 13:04:39 +0200

    Fix code for SLE16 container host ppc64le create_hdd test
      Restore ppc64le condition, removed in PR#22611

--- tests/yam/agama/agama_auto.pm
+++ tests/yam/agama/agama_auto.pm
@@ -22,7 +22,7 @@ sub run {
 
     $self->upload_agama_logs() unless is_hyperv();
 
-    (is_s390x() || is_pvm() || is_vmware()) ?
+    (is_s390x() || is_ppc64le() || is_pvm() || is_vmware()) ?
       # reboot via console
       power_action('reboot', keepconsole => 1, first_reboot => 1) :
       # graphical reboot
