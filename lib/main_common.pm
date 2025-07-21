e6becdf3fb09c5243a7731970f205edddc98de60
Santiago Zarate <santiago.zarate@suse.com>
2025-07-21 14:27:44 +0200

    Only schedule openSUSE repos test in TW and leap16

--- lib/main_common.pm
+++ lib/main_common.pm
@@ -1211,7 +1211,7 @@ sub load_consoletests {
             loadtest "console/installation_snapshots" unless get_var('FLAVOR') =~ /OpenStack-Cloud/;
         }
     }
-    loadtest "console/opensuse_repos" if is_opensuse && !(is_staging || is_updates_tests);
+    loadtest "console/opensuse_repos" if (is_tumbleweed || is_leap('16.0+'));
     loadtest "console/zypper_lr";
     # Enable installation repo from the usb, unless we boot from USB, but don't use it
     # for the installation, like in case of LiveCDs and when using http/smb/ftp mirror
