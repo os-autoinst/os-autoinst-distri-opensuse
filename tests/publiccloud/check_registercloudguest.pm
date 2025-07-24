8aacc5be4e7c6c98e996aa867f5f51acac68ec1b
Pavel Dostál <pdostal@suse.cz>
2025-07-24 14:11:51 +0200

    PC: Drop PUBLIC_CLOUD_INFRA

--- tests/publiccloud/check_registercloudguest.pm
+++ tests/publiccloud/check_registercloudguest.pm
@@ -49,23 +49,7 @@ sub run {
         return;
     }
 
-    # this is special use case needed by dev ops team within SUSE. Should not be used for any product testing.
-    # Use this variable ONLY if you really know what you are doing!
-    if (check_var('PUBLIC_CLOUD_INFRA', '1')) {
-        my $prefix = "PREPARATION PHASE.";
-        my $rmt_ipv4 = get_required_var("PUBLIC_CLOUD_INFRA_RMT_V4");
-        my $rmt_ipv6 = get_required_var("PUBLIC_CLOUD_INFRA_RMT_V6");
-        if (is_ondemand()) {
-            $instance->ssh_assert_script_run(cmd => "sudo registercloudguest --clean", fail_message => "$prefix registercloudguest --clean failed");
-            $instance->ssh_script_run(cmd => 'sudo rm /var/log/cloudregister; sudo rm /etc/pki/trust/anchors/*.pem', fail_message => "$prefix Deletion of register log and/or certificates failed");
-        }
-        $instance->ssh_script_run(cmd => "sudo sed -i \"s/regionsrv.*\$/regionsrv = $rmt_ipv4,$rmt_ipv6/\" /etc/regionserverclnt.cfg");
-        $instance->ssh_assert_script_run(cmd => "sudo grep \"regionsrv = $rmt_ipv4,$rmt_ipv6\" /etc/regionserverclnt.cfg", fail_message => "$prefix regionserverclnt.cfg update failed");
-        $instance->ssh_assert_script_run(cmd => "sudo curl -k -O --output-dir /usr/lib/regionService/certs https://$rmt_ipv4/$rmt_ipv4.pem", fail_message => "$prefix Download of test cert failed");
-        $instance->ssh_assert_script_run(cmd => 'sudo update-ca-certificates', fail_message => "$prefix update-ca-certificates failed");
-        $instance->ssh_assert_script_run(cmd => "sudo registercloudguest $regcode_param", timeout => 300);
-        check_instance_registered($instance);
-    } elsif (is_container_host()) {
+    if (is_container_host()) {
         # CHOST images don't have registercloudguest pre-installed. To install it we need to register which make it impossible to do
         # all BYOS related checks. So we just regestering system and going further
         registercloudguest($instance);
