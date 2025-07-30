8044944e52e481ac480d4d10f1778f4f40f2fe24
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-07-30 09:59:23 +0200

    Ensure creation of the "after install" snapshot was removed
    https://progress.opensuse.org/issues/185356
    
    Ensure creation of the "after install" snapshot was removed
    
    https://progress.opensuse.org/issues/185356
    
    Ensure creation of the "after install" snapshot was removed
    
    https://progress.opensuse.org/issues/185356
    
    Added validate after installation snapshot
    
    Tidy code
    
    Applied requested comments
    
    Removed unused import

---
+++ tests/yam/validate/validate_snapshot.pm
@@ -0,0 +1,24 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that the snapshot "after install" is not present.
+
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use strict;
+use warnings;
+use testapi;
+
+sub run {
+    select_console 'root-console';
+
+    my $snapper_description = script_output("snapper list --columns description");
+    if ($snapper_description =~ /after\sinstallation/) {
+        die "After installation snapshot is present, check log";
+    }
+}
+
+1;
