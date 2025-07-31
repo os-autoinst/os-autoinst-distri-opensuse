79169602ef8f99cb65c4177c1d5ed4d21c66de85
Oliver Kurz <okurz@suse.de>
2025-07-31 16:16:19 +0200

    publiccloud: Fix run_ltp for build-less tests
    Trying to call openqa-investigate from
    https://github.com/os-autoinst/scripts/blob/master/openqa-investigate I
    encountered failed openQA jobs merely for the reason that the BUILD
    variable is unset so that openQA investigation jobs outside production
    job history can be created. This commit replaces the unnecessarily
    restrictive "get_required_var" with a sane default value of an empty
    string for the build value which is only used to print for debugging
    purposes anyway.
    
    Related progress issue: https://progress.opensuse.org/issues/186648

--- tests/publiccloud/run_ltp.pm
+++ tests/publiccloud/run_ltp.pm
@@ -232,7 +232,7 @@ sub gen_ltp_env {
     my ($instance, $ltp_pkg) = @_;
     my $environment = {
         product => get_required_var('DISTRI') . ':' . get_required_var('VERSION'),
-        revision => get_required_var('BUILD'),
+        revision => get_var('BUILD', ''),
         arch => get_var('PUBLIC_CLOUD_ARCH', get_required_var("ARCH")),
         kernel => $instance->run_ssh_command(cmd => 'uname -r'),
         backend => get_required_var('BACKEND'),
