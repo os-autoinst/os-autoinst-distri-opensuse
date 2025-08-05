3bd2d31b4a4569ac1e492a37a19186b2634a0da6
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-08-05 18:03:01 +0200

    Chaged function to write JSON

--- tests/yam/agama/agama_cli.pm
+++ tests/yam/agama/agama_cli.pm
@@ -21,10 +21,8 @@ sub run {
         'password' => 'nots3cr3t'
     };
 
-    open my $fh, '>', '/tmp/profile.json' or die "Cannot open /tmp/profile.json file to write JSON";
     my $json_pretty = to_json($json, {pretty => 1});
-    print $fh $json_pretty;
-    close $fh;
+    assert_script_run("echo $json_prety > /tmp/profile.json");
     assert_script_run("agama config load file:///tmp/profile.json");
     assert_script_run("agama install");
 
