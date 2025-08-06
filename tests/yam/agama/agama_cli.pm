6597e148a835fd89afe4c92f9ce5be8d80acded8
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-08-06 09:37:15 +0200

    Test another way to set to true

--- tests/yam/agama/agama_cli.pm
+++ tests/yam/agama/agama_cli.pm
@@ -20,12 +20,12 @@ sub run {
     $json->{'product'}->{'id'} = 'SLES';
     $json->{'product'}->{'registrationCode'} = get_var('SCC_REGCODE');
     $json->{'root'} = {
-        'hashedPassword' => JSON::PP::true,
+        'hashedPassword' => \1,
         'password' => '$6$vYbbuJ9WMriFxGHY$gQ7shLw9ZBsRcPgo6/8KmfDvQ/lCqxW8/WnMoLCoWGdHO6Touush1nhegYfdBbXRpsQuy/FTZZeg7gQL50IbA/',
     };
     $json->{'user'} = {
         'fullName' => 'Bernhard M. Wiedemann',
-        'hashedPassword' => JSON::PP::true,
+        'hashedPassword' => \1,
         'password' => '$6$vYbbuJ9WMriFxGHY$gQ7shLw9ZBsRcPgo6/8KmfDvQ/lCqxW8/WnMoLCoWGdHO6Touush1nhegYfdBbXRpsQuy/FTZZeg7gQL50IbA/',
         'userName' => 'bernhard'
     };
