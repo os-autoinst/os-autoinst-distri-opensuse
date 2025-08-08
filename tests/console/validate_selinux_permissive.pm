59f82fa281f810d0b2d1a2fc656fb7e2a6522f2f
Volodymyr Katkalov <volodymyr.katkalov@suse.com>
2025-08-08 11:42:57 +0300

    Validate SELinux permissive on SAP 16.0

---
+++ tests/console/validate_selinux_permissive.pm
@@ -0,0 +1,20 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that SELinux is running in permissive mode
+# Maintainer: QE C <qe-c@suse.de>
+
+use strict;
+use warnings;
+use testapi;
+use base "selinuxtest";
+
+sub run {
+    my ($self) = @_;
+
+    die "SELinux is not running in permissive mode" unless $self->is_selinux_permissive();
+}
+
+1;
