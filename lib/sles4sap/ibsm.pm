os-autoinst/os-autoinst-distri-opensuse

commit 8b988324e8bca873f203d3682e214f9acbca765b

Move Azure network peering to IBSm lib
Move code about peering the the IBSm in Azure to the IBSm library as
code does not have any dependency to qe-sap-deployment.
Implement some qesap::azure functions using the azure_cli lib.
Remove ipaddr2 test module dependency to qesap.
Create a cleanup function.

lib/sles4sap/ibsm.pm
@@ -32,9 +32,12 @@ use warnings;
 use Carp qw(croak);
 use Exporter 'import';
 use testapi;
+use sles4sap::azure_cli;
 
 our @EXPORT = qw(
   ibsm_calculate_address_range
+  ibsm_network_peering_azure_create
+  ibsm_network_peering_azure_delete
 );
 
 =head1 DESCRIPTION
@@ -53,7 +56,7 @@ ip2 and ip3 are calculated using the slot number as seed.
 
 =over
 
-=item B<SLOT> - integer to be used as seed in calculating addresses
+=item B<slot> - integer to be used as seed in calculating addresses
 
 =back
 
@@ -75,6 +78,123 @@ sub ibsm_calculate_address_range {
         main_address_range => sprintf("10.%d.%d.0/21", $ip2, $ip3),
         subnet_address_range => sprintf("10.%d.%d.0/24", $ip2, $ip3),
     );
+}
+
+=head2 ibsm_network_peering_azure_create
+
+    ibsm_network_peering_azure_create(ibsm_rg => 'IBSmMyRg');
+
+Create bidirectional network peering in Azure
+
+=over
+
+=item B<ibsm_rg> - Azure resource group of the IBSm
+
+=item B<sut_rg> - Azure resource group of the SUT
+
+=item B<name_prefix> - prefix to be applied at the beginning of each peering name
+
+=back
+=cut
+
+sub ibsm_network_peering_azure_create {
+    my (%args) = @_;
+    foreach (qw(ibsm_rg sut_rg)) { croak("Argument < $_ > missing") unless $args{$_}; }
+
+    my %vnet_names;
+    foreach ('ibsm', 'sut') {
+        my $res = az_network_vnet_get(resource_group => $args{$_ . '_rg'}, query => '[].name');
+        $vnet_names{$_} = $res->[0];
+    }
+
+    my @peering_name;
+    push @peering_name, $args{name_prefix} if ($args{name_prefix});
+    push @peering_name, $vnet_names{sut};
+    push @peering_name, $vnet_names{ibsm};
+    az_network_peering_create(name => join('-', @peering_name),
+        source_rg => $args{sut_rg}, source_vnet => $vnet_names{sut},
+        target_rg => $args{ibsm_rg}, target_vnet => $vnet_names{ibsm});
+    record_info('PEERING SUCCESS ' . join('-', @peering_name),
+        "Peering from $args{sut_rg}.$vnet_names{sut} server was successful");
+
+    @peering_name = ();
+    push @peering_name, $args{name_prefix} if ($args{name_prefix});
+    push @peering_name, $vnet_names{ibsm};
+    push @peering_name, $vnet_names{sut};
+    az_network_peering_create(name => join('-', @peering_name),
+        source_rg => $args{ibsm_rg}, source_vnet => $vnet_names{ibsm},
+        target_rg => $args{sut_rg}, target_vnet => $vnet_names{sut});
+    record_info('PEERING SUCCESS ' . join('-', @peering_name),
+        "Peering from $args{ibsm_rg}.$vnet_names{ibsm} server was successful");
+}
+
+=head3 ibsm_network_peering_azure_delete
+
+    Delete all the network peering between the two provided deployments.
+
+=over
+
+=item B<ibsm_rg> - Azure resource group of the IBSm
+
+=item B<sut_rg> - Azure resource group of the SUT
+
+=item B<sut_vnet> - substring in the SUT vnet. Optional and only needed if only one specific VNET has to be considered. Most of the time it is get_current_job_id()
+
+=item B<timeout> - default is 5 mins
+
+=back
+=cut
+
+sub ibsm_network_peering_azure_delete {
+    my (%args) = @_;
+    foreach (qw(sut_rg ibsm_rg)) { croak "Missing mandatory $_ argument" unless $args{$_}; }
+    $args{timeout} //= bmwqemu::scale_timeout(300);
+
+    my $res = az_network_vnet_get(resource_group => $args{sut_rg},
+        query => $args{sut_vnet} ? "[?contains(name,'" . $args{sut_vnet} . "')].name" : '[0].name');
+    my $sut_vnet = $res->[0];
+
+    $res = az_network_vnet_get(resource_group => $args{ibsm_rg},
+        query => '[0].name');
+    my $ibsm_vnet = $res->[0];
+
+    $res = az_network_peering_list(resource_group => $args{sut_rg}, vnet => $sut_vnet);
+    my $peering_name = $res->[0];
+    if (!$peering_name) {
+        record_info('NO PEERING',
+            "No peering between $args{sut_rg} and resources belonging to the current job to be destroyed!");
+        return;
+    }
+
+    my $source_ret = 0;
+    record_info("Destroying SUT->IBSM peering '$peering_name'");
+    if ($args{sut_rg}) {
+        $source_ret = az_network_peering_delete(
+            name => $peering_name,
+            resource_group => $args{sut_rg},
+            vnet => $sut_vnet,
+            timeout => $args{timeout});
+    }
+    else {
+        record_info('NO PEERING',
+            "No peering between SUT and IBSM - maybe it wasn't created, or the resources have been destroyed.");
+    }
+
+    $res = az_network_peering_list(resource_group => $args{ibsm_rg}, vnet => $ibsm_vnet);
+    $peering_name = $res->[0];
+    record_info("Destroying IBSM->SUT peering '$peering_name'");
+    my $target_ret = az_network_peering_delete(
+        name => $peering_name,
+        resource_group => $args{ibsm_rg},
+        vnet => $ibsm_vnet,
+        timeout => $args{timeout});
+
+    record_info("source_ret:'$source_ret' target_ret:'$target_ret'");
+    if ($source_ret == 0 && $target_ret == 0) {
+        record_info('Peering deletion SUCCESS', 'The peering was successfully destroyed');
+        return;
+    }
+    die("Peering destruction FAIL: There may be leftover peering connections, please check - jsc#7487");
 }
 
 1;

t/35_ibsm.t
@@ -3,8 +3,8 @@ use warnings;
 use Test::More;
 use Test::Exception;
 use Test::Warnings;
-
-
+use Test::MockModule;
+use List::Util qw(any);
 use sles4sap::ibsm;
 
 subtest '[ibsm_calculate_address_range]' => sub {
@@ -26,6 +26,135 @@ subtest '[ibsm_calculate_address_range]' => sub {
     is($result_8192{subnet_address_range}, "10.255.248.0/24", 'result_8192 subnet_address_range is correct');
     dies_ok { ibsm_calculate_address_range(slot => 0); } "Expected die for slot < 1";
     dies_ok { ibsm_calculate_address_range(slot => 8193); } "Expected die for slot > 8192";
+};
+
+subtest '[ibsm_network_peering_azure_create]' => sub {
+    my $ibsm = Test::MockModule->new('sles4sap::ibsm', no_auto => 1);
+
+    $ibsm->redefine(az_network_vnet_get => sub {
+            my (%args) = @_;
+            note(" --> az_network_vnet_get(resource_group => '$args{resource_group}', query => '$args{query}')");
+            return ['VNET' . $args{resource_group}]; });
+    my @peering_names;
+    $ibsm->redefine(az_network_peering_create => sub {
+            my (%args) = @_;
+            push @peering_names, $args{name};
+            note(" --> az_network_peering_create(name => '$args{name}, source_rg => '$args{source_rg}, source_vnet => '$args{source_vnet}', target_rg => '$args{target_rg},  target_vnet => '$args{target_vnet}')");
+            return; });
+    $ibsm->redefine(record_info => sub { note(join(' ', 'RECORD_INFO -->', @_)); });
+
+    ibsm_network_peering_azure_create(ibsm_rg => 'COLIBRI', sut_rg => 'PASSEROTTO');
+
+    note("\n  PN-->  " . join("\n  PN-->  ", @peering_names));
+    ok((any { /VNETPASSEROTTO-VNETCOLIBRI/ } @peering_names), 'Peering named VNETPASSEROTTO-VNETCOLIBRI');
+    ok((any { /VNETCOLIBRI-VNETPASSEROTTO/ } @peering_names), 'Peering named VNETCOLIBRI-VNETPASSEROTTO');
+};
+
+subtest '[ibsm_network_peering_azure_create] with name' => sub {
+    my $ibsm = Test::MockModule->new('sles4sap::ibsm', no_auto => 1);
+
+    $ibsm->redefine(az_network_vnet_get => sub {
+            my (%args) = @_;
+            note(" --> az_network_vnet_get(resource_group => '$args{resource_group}', query => '$args{query}')");
+            return ['VNET' . $args{resource_group}]; });
+    my @peering_names;
+    $ibsm->redefine(az_network_peering_create => sub {
+            my (%args) = @_;
+            push @peering_names, $args{name};
+            note(" --> az_network_peering_create(name => '$args{name}, source_rg => '$args{source_rg}, source_vnet => '$args{source_vnet}', target_rg => '$args{target_rg},  target_vnet => '$args{target_vnet}')");
+            return; });
+    $ibsm->redefine(record_info => sub { note(join(' ', 'RECORD_INFO -->', @_)); });
+
+    ibsm_network_peering_azure_create(
+        ibsm_rg => 'COLIBRI',
+        sut_rg => 'PASSEROTTO',
+        name_prefix => 'PETTIROSSO');
+
+    note("\n  PN-->  " . join("\n  PN-->  ", @peering_names));
+    ok((any { /PETTIROSSO-VNETPASSEROTTO-VNETCOLIBRI/ } @peering_names), 'Peering named VNETPASSEROTTO-VNETCOLIBRI');
+    ok((any { /PETTIROSSO-VNETCOLIBRI-VNETPASSEROTTO/ } @peering_names), 'Peering named VNETCOLIBRI-VNETPASSEROTTO');
+};
+
+subtest '[ibsm_network_peering_azure_create] az integration' => sub {
+    my $ibsm = Test::MockModule->new('sles4sap::ibsm', no_auto => 1);
+    my $az_cli = Test::MockModule->new('sles4sap::azure_cli', no_auto => 1);
+
+    my @calls;
+    $az_cli->redefine(script_run => sub {
+            push @calls, 'SR: ' . $_[0];
+            return; });
+
+    $az_cli->redefine(assert_script_run => sub {
+            push @calls, 'ASR: ' . $_[0];
+            return; });
+
+    $az_cli->redefine(script_output => sub {
+            push @calls, 'SO: ' . $_[0];
+            if ($_[0] =~ /az network vnet list -g (.*) --query.*/) { return '["VNET-' . $1 . '"]'; }
+            if ($_[0] =~ /az network vnet show --query id.*--name (.*)/) { return $1 . '-ID'; }
+            return 'NOT VALID'; });
+
+    $ibsm->redefine(record_info => sub { note(join(' ', 'RECORD_INFO -->', @_)); });
+
+    ibsm_network_peering_azure_create(ibsm_rg => 'COLIBRI', sut_rg => 'PASSEROTTO');
+
+    note("\n  C-->  " . join("\n  C-->  ", @calls));
+    ok((any { /.*ASR: az network vnet peering create.*/ } @calls), 'There is at least 1 "az network vnet peering create" (there should be exactly 2).');
+    ok((any { /.*ASR: az network vnet peering create.*name VNET-PASSEROTTO-VNET-COLIBRI.*/ } @calls), 'Peering named VNET-PASSEROTTO-VNET-COLIBRI');
+    ok((any { /.*ASR: az network vnet peering create.*resource-group PASSEROTTO.*/ } @calls), 'Peering resource group PASSEROTTO');
+    ok((any { /.*ASR: az network vnet peering create.*vnet-name VNET-PASSEROTTO.*/ } @calls), 'Peering source vnet VNET-PASSEROTTO');
+    ok((any { /.*ASR: az network vnet peering create.*remote-vnet VNET-COLIBRI-ID/ } @calls), 'Peering remote vnet id VNET-COLIBRI-ID');
+    ok((any { /.*ASR: az network vnet peering create.*name VNET-COLIBRI-VNET-PASSEROTTO.*/ } @calls), 'Peering named VNET-COLIBRI-VNET-PASSEROTTO');
+    ok((any { /.*ASR: az network vnet peering create.*resource-group COLIBRI.*/ } @calls), 'Peering resource group COLIBRI');
+    ok((any { /.*ASR: az network vnet peering create.*vnet-name VNET-COLIBRI.*/ } @calls), 'Peering source vnet VNET-COLIBRI');
+    ok((any { /.*ASR: az network vnet peering create.*remote-vnet VNET-PASSEROTTO-ID/ } @calls), 'Peering remote vnet id VNET-PASSEROTTO-ID');
+};
+
+subtest '[ibsm_network_peering_azure_delete]' => sub {
+    my $ibsm = Test::MockModule->new('sles4sap::ibsm', no_auto => 1);
+
+    $ibsm->redefine(az_network_vnet_get => sub {
+            my (%args) = @_;
+            note(" --> az_network_vnet_get(resource_group => '$args{resource_group}', query => '$args{query}')");
+            return ['VNET' . $args{resource_group}]; });
+    $ibsm->redefine(az_network_peering_list => sub {
+            my (%args) = @_;
+            note(" --> az_network_peering_list(resource_group => '$args{resource_group}', vnet => '$args{vnet}')");
+            return ['PEERING' . $args{resource_group}]; });
+    my $peering_delete = 0;
+    $ibsm->redefine(az_network_peering_delete => sub { $peering_delete = 1; return 0; });
+    $ibsm->redefine(record_info => sub { note(join(' ', 'RECORD_INFO -->', @_)); });
+
+    ibsm_network_peering_azure_delete(sut_rg => 'PICCIONE', ibsm_rg => 'COLOMBA');
+    ok($peering_delete eq 1), 'az_network_peering_delete called';
+};
+
+subtest '[ibsm_network_peering_azure_delete] az integrate' => sub {
+    my $ibsm = Test::MockModule->new('sles4sap::ibsm', no_auto => 1);
+    $ibsm->redefine(record_info => sub { note(join(' ', 'RECORD_INFO -->', @_)); });
+
+    my $az_cli = Test::MockModule->new('sles4sap::azure_cli', no_auto => 1);
+    my @calls;
+    $az_cli->redefine(script_run => sub {
+            push @calls, 'SR: ' . $_[0];
+            return 0; });
+
+    $az_cli->redefine(assert_script_run => sub {
+            push @calls, 'ASR: ' . $_[0];
+            return; });
+
+    $az_cli->redefine(script_output => sub {
+            push @calls, 'SO: ' . $_[0];
+            if ($_[0] =~ /az network vnet list -g (.*) --query.*/) { return '["VNET-' . $1 . '"]'; }
+            if ($_[0] =~ /az network vnet show --query id.*--name (.*)/) { return $1 . '-ID'; }
+            if ($_[0] =~ /az network vnet peering list/) { return '["GABBIANO"]'; }
+            if ($_[0] =~ /az network vnet peering delete/) { return 0; }
+            return 'NOT VALID'; });
+
+    ibsm_network_peering_azure_delete(sut_rg => 'PICCIONE', ibsm_rg => 'COLOMBA');
+    note("\n  C-->  " . join("\n  C-->  ", @calls));
+    ok((any { /.*SR: az network vnet peering delete.*/ } @calls), 'There is at least 1 "az network vnet peering delete" (there should be exactly 2).');
+    ok((any { /.*SR: az network vnet peering delete.*name GABBIANO / } @calls), 'There is at least 1 call with peering name GABBIANO.');
 };
 
 done_testing;

tests/sles4sap/ipaddr2/destroy.pm
@@ -57,13 +57,10 @@ QE-SAP <qe-sap@suse.de>
 use Mojo::Base 'publiccloud::basetest';
 use testapi;
 use serial_terminal qw( select_serial_terminal );
-use sles4sap::qesap::azure qw (qesap_az_vnet_peering_delete);
 use sles4sap::ipaddr2 qw(
-  ipaddr2_deployment_logs
   ipaddr2_infra_destroy
-  ipaddr2_cloudinit_logs
   ipaddr2_azure_resource_group
-);
+  ipaddr2_cleanup);
 
 sub run {
     my ($self) = @_;
@@ -74,8 +71,12 @@ sub run {
     select_serial_terminal;
 
     if (my $ibsm_rg = get_var('IBSM_RG')) {
-        qesap_az_vnet_peering_delete(source_group => ipaddr2_azure_resource_group(), target_group => $ibsm_rg);
+        ibsm_network_peering_azure_delete(
+            sut_rg => ipaddr2_azure_resource_group(),
+            sut_vnet => get_current_job_id(),
+            ibsm_rg => $ibsm_rg);
     }
+    ipaddr2_cloudinit_logs() unless (check_var('IPADDR2_CLOUDINIT', 0));
     ipaddr2_infra_destroy();
 }
 
@@ -85,12 +86,10 @@ sub test_flags {
 
 sub post_fail_hook {
     my ($self) = shift;
-    ipaddr2_deployment_logs() if check_var('IPADDR2_DIAGNOSTIC', 1);
-    ipaddr2_cloudinit_logs() unless check_var('IPADDR2_CLOUDINIT', 0);
-    if (my $ibsm_rg = get_var('IBSM_RG')) {
-        qesap_az_vnet_peering_delete(source_group => ipaddr2_azure_resource_group(), target_group => $ibsm_rg);
-    }
-    ipaddr2_infra_destroy();
+    ipaddr2_cleanup(
+        diagnostic => get_var('IPADDR2_DIAGNOSTIC', 0),
+        cloudinit => get_var('IPADDR2_CLOUDINIT', 1),
+        ibsm_rg => get_var('IBSM_RG'));
     $self->SUPER::post_fail_hook;
 }
 

lib/sles4sap/azure_cli.pm
@@ -265,7 +265,11 @@ sub az_network_vnet_subnet_update {
 
     my $res = az_network_vnet_get(resource_group => 'openqa-rg')
 
-Return the output of az network vnet list
+Return the output of az network vnet list as an object.
+Take care that this command is always calling the az cli with --json
+and returns the decoded object. But the format of the object can change
+accordingly to the provided value of --query. It is also possible that
+some query result in the function to die on decode_json.
 
 =over
 
@@ -692,7 +696,7 @@ sub az_vm_as_list {
 
 =head2 az_vm_as_show
 
-    az_vm_as_show(resource_group => 'openqa-rg');
+    az_vm_as_show(resource_group => 'openqa-rg', name => 'openqa-as');
 
 Show all the details of an availability set. For the moment it only show and does not return anything.
 
@@ -709,11 +713,13 @@ sub az_vm_as_show {
     my (%args) = @_;
     foreach (qw(resource_group name)) {
         croak("Argument < $_ > missing") unless $args{$_}; }
+
     my $az_cmd = join(' ', 'az vm availability-set show',
         '--resource-group', $args{resource_group},
         '--name', $args{name},
         '-o table');
     assert_script_run($az_cmd);
+
     $az_cmd = join(' ', 'az vm availability-set list-sizes',
         '--resource-group', $args{resource_group},
         '--name', $args{name});
@@ -1436,6 +1442,8 @@ Delete a specific network peering
 
 =item B<vnet> - existing vnet in resource_group, used as source of the peering
 
+=item B<timeout> - (Optional) Timeout for the assert_script_run command
+
 =back
 =cut
 
@@ -1924,4 +1932,5 @@ sub az_group_exists {
     croak "Missing mandatory argument: 'resource_group'" unless $args{resource_group};
     return script_output("az group exists --resource-group $args{resource_group}", quiet => $args{quiet});
 }
+
 1;

lib/sles4sap/ipaddr2.pm
@@ -6,6 +6,7 @@
 # Summary: Library sub and shared data for the ipaddr2 cloud test.
 
 package sles4sap::ipaddr2;
+
 use strict;
 use warnings FATAL => 'all';
 use testapi;
@@ -16,7 +17,6 @@ use mmapi qw( get_current_job_id );
 use publiccloud::utils qw( get_ssh_private_key_path register_addon);
 use utils qw( write_sut_file ssh_fully_patch_system);
 use hacluster qw($crm_mon_cmd cluster_status_matches_regex);
-use sles4sap::qesap::azure qw (qesap_az_vnet_peering qesap_az_clean_old_peerings);
 use sles4sap::ibsm;
 use sles4sap::azure_cli;
 
@@ -58,6 +58,7 @@ our @EXPORT = qw(
   ipaddr2_network_peering_create
   ipaddr2_patch_system
   ipaddr2_repos_add_server_to_hosts
+  ipaddr2_cleanup
 );
 
 use constant DEPLOY_PREFIX => 'ip2t';
@@ -450,7 +451,6 @@ sub ipaddr2_infra_deploy {
     my $bastion_ip = ipaddr2_bastion_pubip();
 
 Get the only public IP in the deployment associated to the VM used as bastion.
-
 =cut
 
 sub ipaddr2_bastion_pubip {
@@ -728,8 +728,7 @@ sub ipaddr2_internal_key_gen {
         src          => 1,
         dst          => 2,
         user         => 'cloudadmin',
-        key_checking => 'accept-new',
-    );
+        key_checking => 'accept-new');
 
 Helper for C<ipaddr2_internal_key_gen> and contribute to
 establish password-less SSH access from a source internal VM to a
@@ -780,6 +779,7 @@ sub ipaddr2_internal_key_authorize {
 
     # This is the destination VM where to register the public key
     $vm_name = ipaddr2_get_internal_vm_private_ip(id => $args{dst});
+
     # Create a place where to temporary upload the key
     ipaddr2_ssh_internal(id => $args{dst},
         cmd => "mkdir -p $remote_key_tmp_path",
@@ -827,8 +827,7 @@ sub ipaddr2_internal_key_authorize {
     ipaddr2_deployment_sanity()
 
 Run some checks on the existing deployment using the
-az command line.
-die in case of failure
+az command line. Die in case of failure
 =cut
 
 sub ipaddr2_deployment_sanity {
@@ -1717,7 +1716,7 @@ sub ipaddr2_infra_destroy {
 
 =head2 ipaddr2_get_internal_vm_name
 
-    my $vm_name = ipaddr2_get_internal_vm_name(id => 42);
+    my $vm_name = ipaddr2_get_internal_vm_name(id => 2);
 
 Compose and return a string for the vm name
 
@@ -1736,7 +1735,7 @@ sub ipaddr2_get_internal_vm_name {
 
 =head2 ipaddr2_get_internal_vm_private_ip
 
-    my $private_ip = ipaddr2_get_internal_vm_private_ip(id => 42);
+    my $private_ip = ipaddr2_get_internal_vm_private_ip(id => 2);
 
 compose and return a string representing the VM private IP
 
@@ -1755,7 +1754,7 @@ sub ipaddr2_get_internal_vm_private_ip {
 
 =head2 ipaddr2_get_worker_tmp_for_internal_vm
 
-    my $vm_tmp = ipaddr2_get_worker_tmp_for_internal_vm(42);
+    my $vm_tmp = ipaddr2_get_worker_tmp_for_internal_vm(2);
 
 Return a path in /tmp of the worker used to store files associated
 two one of the internal VM
@@ -2055,16 +2054,17 @@ sub ipaddr2_network_peering_create {
     my (%args) = @_;
     croak 'Missing mandatory argument < ibsm_rg >' unless $args{ibsm_rg};
 
-    # remove the older peering
-    my $vnet_name = az_network_vnet_get(resource_group => $args{ibsm_rg}, query => "[0].name");
-    qesap_az_clean_old_peerings(rg => $args{ibsm_rg}, vnet => $vnet_name);
-
-    qesap_az_vnet_peering(source_group => ipaddr2_azure_resource_group(), target_group => $args{ibsm_rg});
+    ibsm_network_peering_azure_create(
+        ibsm_rg => $args{ibsm_rg},
+        sut_rg => ipaddr2_azure_resource_group(),
+        name_prefix => DEPLOY_PREFIX);
 }
 
 =head2 ipaddr2_repos_add_server_to_hosts
 
-    ipaddr2_repos_add_server_to_hosts(ibsm_ip => , incident_repos => 'AAAA,BBBB');
+    ipaddr2_repos_add_server_to_hosts(
+        ibsm_ip => get_required_var('IBSM_IP'),
+        incident_repos => 'AAAA,BBBB');
 
 Add download.suse.de server to hosts by specifying IBSM address
 
@@ -2212,7 +2212,7 @@ sub ipaddr2_patch_system {
 
 =head2 ipaddr2_scc_addons
 
-    ipaddr2_scc_addons();
+    ipaddr2_scc_addons(scc_addons => get_var('SCC_ADDONS'));
 
 Register addons on SUT
 
@@ -2244,6 +2244,41 @@ sub ipaddr2_scc_addons {
             register_addon($remote_cmd, $addon);
         }
     }
+}
+
+=head2 ipaddr2_cleanup
+
+
+
+=over
+
+=item B<diagnostic> - Optionally collect diagnostic logs, from setting IPADDR2_DIAGNOSTIC
+
+=item B<cloudinit> - Optionally collect cloudinit logs, from setting IPADDR2_CLOUDINIT
+
+=item B<ibsm_rg> - Optionally delete the network peering to IBSs, from setting IBSM_RG
+
+=item B<bastion_ip> - Public IP address of the bastion. Calculated if not provided.
+                      Providing it as an argument is recommended
+                      to avoid having to query Azure to get it.
+
+=back
+=cut
+
+sub ipaddr2_cleanup {
+    my (%args) = @_;
+    $args{diagnostic} //= 0;
+    $args{cloudinit} //= 1;
+
+    ipaddr2_deployment_logs() if ($args{diagnostic} == 1);
+    ipaddr2_cloudinit_logs() unless ($args{cloudinit} == 0);
+    if ($args{ibsm_rg}) {
+        ibsm_network_peering_azure_delete(
+            sut_rg => ipaddr2_azure_resource_group(),
+            sut_vnet => get_current_job_id(),
+            ibsm_rg => $args{ibsm_rg});
+    }
+    ipaddr2_infra_destroy();
 }
 
 1;

commit 8f3b569448bdc4a0f9c192127dfb5b94fe0bb075

Add missing import in ipaddr2 destroy
Add missing imports in ipaddr2 destroy test
module.
Ensure that any call to az_network_vnet_get are using a query
ensuring the return to be a list.
This commit solves couple of regressions introduced in
8b988324e8bca873f203d3682e214f9acbca765b.

lib/sles4sap/ibsm.pm
@@ -150,12 +150,15 @@ sub ibsm_network_peering_azure_delete {
     foreach (qw(sut_rg ibsm_rg)) { croak "Missing mandatory $_ argument" unless $args{$_}; }
     $args{timeout} //= bmwqemu::scale_timeout(300);
 
+    # Take care to keep all the query to always return a json list, even if of a sigle element,
+    # and not a string.
+    my $vnet_get_query = '[].name';
     my $res = az_network_vnet_get(resource_group => $args{sut_rg},
-        query => $args{sut_vnet} ? "[?contains(name,'" . $args{sut_vnet} . "')].name" : '[0].name');
+        query => $args{sut_vnet} ? "[?contains(name,'" . $args{sut_vnet} . "')].name" : $vnet_get_query);
     my $sut_vnet = $res->[0];
 
     $res = az_network_vnet_get(resource_group => $args{ibsm_rg},
-        query => '[0].name');
+        query => $vnet_get_query);
     my $ibsm_vnet = $res->[0];
 
     $res = az_network_peering_list(resource_group => $args{sut_rg}, vnet => $sut_vnet);

t/35_ibsm.t
@@ -120,6 +120,8 @@ subtest '[ibsm_network_peering_azure_delete]' => sub {
     $ibsm->redefine(az_network_peering_list => sub {
             my (%args) = @_;
             note(" --> az_network_peering_list(resource_group => '$args{resource_group}', vnet => '$args{vnet}')");
+            # The function is calling the az cli with "[?contains(name,'" . $args{sut_vnet} . "')].name" or '[].name'
+            # that both return a json list, even if usually only of one element.
             return ['PEERING' . $args{resource_group}]; });
     my $peering_delete = 0;
     $ibsm->redefine(az_network_peering_delete => sub { $peering_delete = 1; return 0; });

tests/sles4sap/ipaddr2/destroy.pm
@@ -57,6 +57,8 @@ QE-SAP <qe-sap@suse.de>
 use Mojo::Base 'publiccloud::basetest';
 use testapi;
 use serial_terminal qw( select_serial_terminal );
+use mmapi qw( get_current_job_id );
+use sles4sap::ibsm qw( ibsm_network_peering_azure_delete );
 use sles4sap::ipaddr2 qw(
   ipaddr2_infra_destroy
   ipaddr2_azure_resource_group