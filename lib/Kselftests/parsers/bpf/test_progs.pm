618eb40f8c1efb7068ac1365802424eeae9c1b81
Ricardo B. Marlière <rbm@opensuse.org>
2025-08-25 10:07:14 -0300

    Kselftests: Add BPF parsers

---
+++ lib/Kselftests/parsers/bpf/test_progs.pm
@@ -0,0 +1,44 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Parser for the bpf:test_progs selftest
+# Maintainer: Kernel QE <kernel-qa@suse.de>
+
+package Kselftests::parsers::bpf::test_progs;
+
+use base 'Kselftests::parsers::main';
+use testapi;
+use strict;
+use warnings;
+
+our $test_idx = 1;
+
+sub parse_line {
+    my ($self, $test_ln) = @_;
+    my ($description, $st, $diag);
+    if ($test_ln =~ /^#\s#\S+\s+(\S+):(OK|FAIL|SKIP)$/) {
+        ($description, $st) = ($1, $2);
+    } elsif ($test_ln =~ /^#\s(\S*?):(PASS|SKIP|FAIL):(.*)$/) {
+        ($description, $st, $diag) = ($1, $2, $3);
+    }
+    if ($description) {
+        my $normalized;
+        if ($st eq 'PASS') {
+            $normalized = "# ok $test_idx $description";
+        } elsif ($st eq 'FAIL') {
+            $normalized = "# not ok $test_idx $description";
+        } else {
+            $normalized = "# ok $test_idx $description # SKIP";
+        }
+        if ($diag) {
+            $normalized .= " # $diag";
+        }
+        $test_idx++;
+        return $normalized;
+    }
+    return undef;
+}
+
+1;
---
+++ lib/Kselftests/parsers/bpf/test_tcpnotify_user.pm
@@ -0,0 +1,24 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Parser for the bpf:test_tcpnotify_user selftest
+# Maintainer: Kernel QE <kernel-qa@suse.de>
+
+package Kselftests::parsers::bpf::test_tcpnotify_user;
+
+use base 'Kselftests::parsers::main';
+use testapi;
+use strict;
+use warnings;
+
+sub parse_line {
+    my ($self, $test_ln) = @_;
+    if ($test_ln =~ /# PASSED!/) {
+        return "# ok 1 test_tcpnotify_user";
+    }
+    return undef;
+}
+
+1;
