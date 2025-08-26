91d1bd193f3d004ff55e77e9d2fde183dbb3fe8f
Julie-CAO <jcao@suse.com>
2025-08-26 10:41:30 +0800

    Correct parameters passing and add igbvf driver

--- tests/virt_autotest/sriov_network_card_pci_passthrough.pm
+++ tests/virt_autotest/sriov_network_card_pci_passthrough.pm
@@ -55,13 +55,13 @@ sub run_test {
     record_info("Before enable VF", script_output("ip r"));
     my $nm_config_file = "/usr/lib/NetworkManager/conf.d/00-server.conf";
     script_run("cat $nm_config_file");
-    script_run("sed -i 's/no-auto-default=\\*/no-auto-default=driver:iavf/' $nm_config_file");
+    script_run("sed -i 's/no-auto-default=\\*/no-auto-default=driver:iavf,driver:igbvf/' $nm_config_file");
     script_run("cat $nm_config_file");
     script_run("ip a");
     script_run("nmcli con");
 
     # enable 8 vfs for the SR-IOV device on host
-    my @host_vfs = enable_vf(pfs => @host_pfs, number => get_var("ENABLE_VF_COUNT", '7'));
+    my @host_vfs = enable_vf(number => get_var("ENABLE_VF_COUNT", '7'), pfs => \@host_pfs);
     record_info("VFs enabled", "@host_vfs");
 
     record_info("Before enable VF", script_output("ip r"));
@@ -216,18 +216,22 @@ sub find_sriov_ethernet_devices {
 #enable 8 virtual functions for the specified physical functions of the SR-IOV network device
 sub enable_vf {
     my %args = @_;
-    my @pfs = $args{pfs};
+    my $pfs_ref = $args{pfs};
     my $number = $args{number} // 7;
+    print "julie: \$number=$number\n";
+    print "julie: \@\$pfs_ref=@$pfs_ref\n";
 
     #enable VFs for SR-IOV PFs by modifying SYS PCI
     #modifying SYS PCI is much better than passing max_vfs=8 in reloading network device drivers
     #as no network break is required anymore(ie. no sol console is needed or no worries about ip/nic change),
     #also modifying SYS PCI allows to enable specified PFs
-    foreach my $pf (@pfs) {
+    foreach my $pf (@$pfs_ref) {
         #enable 7 VFs as all of SR-IOV ethernet cards allow the maxium fv number is beyond 7
         assert_script_run("echo $number > /sys/bus/pci/devices/0000:$pf/sriov_numvfs");
     }
 
+    # It takes a litte longer on SLE16 due to network activation
+    script_retry("lspci | grep Ethernet | grep \"Virtual Function\"", delay => 5, retry => 3);
     my $vf_devices = script_output "lspci | grep Ethernet | grep \"Virtual Function\" | cut -d ' ' -f1";
     record_info("Error", "The VF number is not correct!", result => 'fail') if script_output("echo '$vf_devices' | wc -l") != $number;
     my @vfs = split("\n", $vf_devices);
