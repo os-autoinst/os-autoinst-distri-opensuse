ff0e380fdc343d33d29aaea6d9227dc4cd9a389d
Rainer Koenig <rainer.konig@suse.com>
2025-09-01 16:55:28 +0200

    Fix problems with generate_dud
    - Related ticket: https://progress.opensuse.org/issues/188052

--- tests/yam/agama/generate_dud.pm
+++ tests/yam/agama/generate_dud.pm
@@ -12,18 +12,23 @@ use autoyast qw(expand_agama_profile generate_json_profile);
 
 sub run {
     my $self = shift;
-    my $dud = get_required_var('DUD');
+    my $archived_dud_with_profile = get_required_var('DUD');
     my $profile_url = get_required_var('AGAMA_PROFILE');
 
     select_console 'install-shell';
 
     # https://progress.opensuse.org/issues/185122
-    zypper_call("ar -f -G https://download.opensuse.org/repositories/home:/snwint:/ports/SLFO-Main/home:snwint:ports.repo");
+    zypper_call("ar -f -G https://openqa.suse.de/assets/repo/SLES-" . get_var('VERSION') . "-" . get_var('ARCH') . "-Build" . get_var('BUILD') . "?ssl_verify=no install");
     zypper_call("in -y mkdud");
     assert_script_run("mkdir -p tmp/dud/root");
     assert_script_run("curl -o tmp/dud/root/autoinst.json $profile_url");
-    assert_script_run("mkdud --create $dud tmp/dud/root --dist tw");
-    upload_asset($dud);
+    assert_script_run("mkdud --create $archived_dud_with_profile tmp/dud/root --dist tw");
+    upload_asset($archived_dud_with_profile);
+
+    my $arch = get_required_var('ARCH');
+    my $archived_dud_with_kernel_module = "kernel-" . $arch . ".dud";
+    assert_script_run("mkdud --create $archived_dud_with_kernel_module --arch $arch --dist sle16 /lib/modules/`uname -r`/kernel/fs/nfs/nfs.ko");
+    upload_asset($archived_dud_with_kernel_module);
 }
 
 1;
