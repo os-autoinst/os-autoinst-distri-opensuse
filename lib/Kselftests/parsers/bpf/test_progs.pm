65799f159557a48e8f9aa4863df4e1d9b94be707
Ricardo B. Marlière <rbm@opensuse.org>
2025-09-01 07:39:57 -0300

    Kselftests: Add $test_name to test_progs BPF parser

--- lib/Kselftests/parsers/bpf/test_progs.pm
+++ lib/Kselftests/parsers/bpf/test_progs.pm
@@ -17,13 +17,16 @@ our $test_idx = 1;
 
 sub parse_line {
     my ($self, $test_ln) = @_;
-    my ($description, $st, $diag);
+    my ($description, $st, $test_name, $diag);
     if ($test_ln =~ /^#\s#\S+\s+(\S+):(OK|FAIL|SKIP)$/) {
         ($description, $st) = ($1, $2);
-    } elsif ($test_ln =~ /^#\s(\S*?):(PASS|SKIP|FAIL):(.*)$/) {
-        ($description, $st, $diag) = ($1, $2, $3);
+    } elsif ($test_ln =~ /^#\s(\S*?):(PASS|SKIP|FAIL):(\S*?)\s(.*)$/) {
+        ($description, $st, $test_name, $diag) = ($1, $2, $3, $4);
     }
     if ($description) {
+        if ($test_name) {
+            $description .= "/$test_name";
+        }
         my $normalized;
         if ($st eq 'PASS') {
             $normalized = "# ok $test_idx $description";
