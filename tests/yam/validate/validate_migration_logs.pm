a5320eecf480e6f313dba459174dc01872ddd764
Wayne Chen <wchen@suse.com>
2025-09-02 19:35:14 +0800

    Test Pull Request

--- tests/yam/validate/validate_migration_logs.pm
+++ tests/yam/validate/validate_migration_logs.pm
@@ -9,11 +9,18 @@
 
 use base 'consoletest';
 use testapi;
+use utils 'upload_folders';
 
 sub run {
     select_console 'root-console';
 
     upload_logs("/var/log/distro_migration.log", failok => 1);
+    die("Migration failed") if (script_run("cat /var/log/distro_migration.log | grep -i \"migration failed\"") == 0);
+}
+
+sub post_fail_hook {
+    upload_logs("/boot/grub2/grub.cfg", failok => 1);
+    upload_folders(folders => '/etc/zypp/repos.d/');
 }
 
 1;
