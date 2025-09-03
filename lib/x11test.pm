fe19d181ab1761b8e1fa6dc05c4264b3669f5464
Santiago Zarate <santiago.zarate@suse.com>
2025-09-03 12:01:42 +0200

    Handle new LibreOffice welcome popup

--- lib/x11test.pm
+++ lib/x11test.pm
@@ -1013,6 +1013,9 @@ sub libreoffice_start_program {
     my %start_program_args;
     $start_program_args{timeout} = 100 if get_var('LIVECD') && check_var('MACHINE', 'uefi-usb');
     x11_start_program($program, %start_program_args);
+    if (check_screen('welcome-to-libreoffice')) {
+        send_key "alt-f4";
+    }
     if (check_screen([qw(ooffice-tip-of-the-day oomath-tip-of-the-day)], 5)) {
         # Unselect "_S_how tips on startup", select "_O_k"
         send_key "alt-s";
