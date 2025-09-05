5521d6f2259a9942b1c2c80a94bef09b69362f27
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-09-05 09:18:30 +0200

    Added CLI commands

--- tests/yam/agama/agama_cli.pm
+++ tests/yam/agama/agama_cli.pm
@@ -10,23 +10,39 @@ use power_action_utils qw(power_action);
 use Utils::Architectures qw(is_s390x);
 use Utils::Backends qw(is_svirt);
 
+sub agama_config_edit {
+    my $regex = @_;
+
+    script_run('agama config edit', timeout => 0);
+    wait_still_screen();
+    type_string($regex);
+    type_string(":wq\n");
+    wait_still_screen();
+}
+
 sub run {
     my $self = shift;
 
-    # Editing configuration
-    script_run('agama config edit', timeout => 0);
-    wait_still_screen();
-    type_string(":\%s/bernhard/jose/g\n");
-    type_string(":wq\n");
-    wait_still_screen();
-    assert_script_run('agama config show | grep jose');
+    my product_id = get_var('AGAMA_PRODUCT_ID');
+    my $agama_help = script_output('agama');
+    diag($agama_help);
+    die 'Agama Help not shown' unless $agama_help =~ "Agama's command-line interface";
 
-    # Restoring original value
-    script_run('agama config edit', timeout => 0);
-    wait_still_screen();
-    type_string(":\%s/jose/bernhard/g\n");
-    type_string(":wq\n");
-    wait_still_screen();
+    assert_script_run('agama config show | jq -C');
+
+    assert_script_run("jq -n '.root.password = \"$testapi::password\"' | agama config load");
+    assert_script_run("agama config show | grep $testapi::password");
+
+    assert_script_run("jq -n '.product.id = \"$product_id\"' | agama config load");
+    assert_script_run("agama config show | grep $product_id");
+
+    assert_script_run('agama probe');
+
+    assert_script_run('agama download https://github.com/os-autoinst/os-autoinst-distri-opensuse/raw/refs/heads/master/data/yam/agama/hello-world-0.1-1.1.noarch.rpm /tmp/hello-world.rpm');
+
+    agama_config_edit(":\%s/bernhard/jose/g\n");
+    assert_script_run('agama config show | grep jose');
+    agama_config_edit(":\%s/jose/bernhard/g\n");
 
     assert_script_run('agama install', timeout => 2400);
     $self->upload_agama_logs();
