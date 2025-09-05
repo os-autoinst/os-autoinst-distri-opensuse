3b75c3b1deeb95d148f7315dd6b515f84b034b7b
KuxaBeast <kuxa@darkne.dev>
2025-09-05 18:35:19 +0200

    Move html to separate template file

--- tests/containers/charts/kiosk_firefox.pm
+++ tests/containers/charts/kiosk_firefox.pm
@@ -16,23 +16,6 @@ use utils;
 use containers::helm;
 use containers::k8s qw(install_helm);
 
-
-my $base_url = autoinst_url;
-my $audio_html = <<_EOF_;
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sample audio file</title>
-</head>
-<body>
-  <audio loop controls> <!-- autoplay is blocked in modern browsers -->
-    <source src="$base_url/data/bar.wav" type="audio/wav">
-    Your browser does not support the audio tag.
-  </audio>
-</body>
-</html>
-_EOF_
-
 sub run {
     my ($self) = @_;
 
@@ -41,7 +24,9 @@ sub run {
     my $helm_values = autoinst_url("/data/containers/helm/kiosk/values.yaml");
 
     # Run an nginx container with a test page and wait for it
-    assert_script_run("kubectl create configmap audio-html-config --from-literal=audio.html='$audio_html'");
+    assert_script_run("curl -O " . autoinst_url("/data/containers/helm/kiosk/audio.html"));
+    file_content_replace("audio.html", '%AUTOINST_URL%' => autoinst_url);
+    assert_script_run("kubectl create configmap audio-html-config --from-file=audio.html");
     assert_script_run("kubectl apply -f " . autoinst_url("/data/containers/helm/kiosk/nginx.yaml"));
     assert_script_run("kubectl wait --for=condition=Ready pod/nginx-test --timeout=60s");
 
