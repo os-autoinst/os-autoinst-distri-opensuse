26e2ad9c99b9a7cf2f154724c3df2638807a91b3
Petr Vorel <pvorel@suse.cz>
2025-09-09 11:15:04 +0200

    install_kotd: Fix installation on z/VM (o3)
    Don't wait_boot() on z/VM (use the same check as on boot_ltp.pm).
    This fixes error on consoles::s3270::expect_3270() timed out due using
    root-console instead of serial console.
    
    While at it use serial console instead of root-console (fix from
    6681da95ae poo#54275 is needed only on SLES KOTD which uses KVM -
    s390x-kvm, it set's S390_ZKVM=1, s390x on o3 uses z/VM).
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- tests/kernel/install_kotd.pm
+++ tests/kernel/install_kotd.pm
@@ -18,9 +18,12 @@ use power_action_utils 'power_action';
 
 sub run {
     my $self = shift;
-    $self->wait_boot;
+
+    $self->wait_boot unless (is_backend_s390x);
+
     # Use root-console for KOTD installation on svirt instead of root-sut-serial poo#54275
-    is_svirt ? select_console('root-console') : select_serial_terminal;
+    is_svirt_except_s390x ? select_console('root-console') : select_serial_terminal;
+
     # Get url of kotd/kmp repositories
     my $kotd_repo = get_required_var('KOTD_REPO');
     my $kmp_repo = get_var('KMP_REPO');
