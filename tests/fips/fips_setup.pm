c455f8e6bc1918bcc592f0722cf9e2e5bb30dc66
Paolo Stivanin <info@paolostivanin.com>
2025-09-11 10:36:41 +0200

    Add VA to fips

--- tests/fips/fips_setup.pm
+++ tests/fips/fips_setup.pm
@@ -17,8 +17,9 @@ use serial_terminal 'select_serial_terminal';
 use transactional qw(trup_call process_reboot);
 use utils qw(zypper_call reconnect_mgmt_console);
 use Utils::Backends 'is_pvm';
-use Utils::Architectures 'is_aarch64';
+use Utils::Architectures qw(is_aarch64);
 use version_utils qw(is_jeos is_sle_micro is_sle is_tumbleweed is_transactional is_microos);
+use security::vendor_affirmation;
 
 my @vars = ('OPENSSL_FIPS', 'OPENSSL_FORCE_FIPS_MODE', 'LIBGCRYPT_FORCE_FIPS_MODE', 'NSS_FIPS', 'GNUTLS_FORCE_FIPS_MODE');
 
@@ -68,16 +69,18 @@ sub ensure_fips_enabled {
 
 sub install_fips {
     if (is_transactional) {
+        # crypto-policies-scripts cannot handle transactional systems.
         if (is_sle_micro('<6.0')) {
             trup_call("pkg install -t pattern microos-fips");
         } else {
             trup_call("setup-fips");
         }
-        # crypto-policies script reports Cannot handle transactional systems.
     } elsif (((is_sle('>=15-SP4') || is_jeos || is_tumbleweed)) && !get_var("FIPS_ENV_MODE")) {
         zypper_call("in crypto-policies-scripts");
         # Explicitly install openssl-3 on s390x SLE16 https://bugzilla.suse.com/show_bug.cgi?id=1247463
         zypper_call("in openssl-3") if (is_s390x && is_sle('>=16'));
+        # https://progress.opensuse.org/issues/181331: force install specific versions for Vendor Affirmation
+        install_vendor_affirmation_pkgs if (check_var('FIPS_USE_CERT_MODULE', '1') && is_sle('=15-SP7'));
     } elsif (is_sle('<=15-SP3') || get_var("FIPS_ENV_MODE")) {
         # No crypto-policies in older SLE
         zypper_call("in -t pattern fips");
@@ -85,6 +88,7 @@ sub install_fips {
         # update-crypto-policies, otherwise some tests will fail.
         zypper_call("in crypto-policies-scripts") if is_sle('>=15-SP6');
     }
+
     return;
 }
 
