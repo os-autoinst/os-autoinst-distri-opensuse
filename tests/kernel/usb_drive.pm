03e2273b5b4e59d476a46173b63d740f7ec0b3d5
Michael Moese <mmoese@suse.de>
2025-09-16 11:57:51 +0200

    Add support for testing unprivileged usb mounts (#22643)
    When we want to mount a usb device unprivileged, we can use "lklmount" for fuse.
    If a package lklfuse is available, we installl it, and perform basically
    the same functional test as user bernhard that we did as root before.
    
    Co-authored-by: Michael Moese <mmoese@suse.com>

--- tests/kernel/usb_drive.pm
+++ tests/kernel/usb_drive.pm
@@ -9,9 +9,18 @@
 
 use base 'opensusebasetest';
 use testapi;
-use serial_terminal 'select_serial_terminal';
+use serial_terminal;
 use utils;
+use package_utils 'install_package';
+use version_utils 'is_sle';
 use usb;
+
+sub md5sum {
+    my ($file) = @_;
+    my ($md5) = split /\s/, script_output "md5sum $file";
+    return $md5;
+}
+
 
 sub run {
     my ($self) = @_;
@@ -27,16 +36,18 @@ sub run {
     # create filesystem, mountpoint and temporary file
     my $tmp = script_output 'mktemp -d';
     my $file = "$tmp/file";
-    my $md5 = "$tmp/md5";
     my $mountpoint = "$tmp/mount";
     my $file_copy = "$mountpoint/file";
 
     assert_script_run "mkdir $mountpoint";
-    assert_script_run "mkfs.btrfs -f $device";
+    assert_script_run "chgrp disk $mountpoint";
+    assert_script_run "chmod 777 $mountpoint";
 
-    assert_script_run "mount -t btrfs $device $mountpoint";
+    assert_script_run "mkfs.ext4 -F $device";
+    assert_script_run "mount -t ext4 $device $mountpoint";
+
     assert_script_run "dd if=/dev/urandom of=$file bs=1M count=16";
-    assert_script_run "md5sum $file > $md5";
+    my $md5_orig = md5sum $file;
     assert_script_run "cp $file $file_copy";
 
     # unmount and flush slab and page cache
@@ -44,12 +55,42 @@ sub run {
     assert_script_run "echo 3 > /proc/sys/vm/drop_caches";
 
     # remount and check md5sum
-    assert_script_run "mount -t btrfs $device $mountpoint";
-    assert_script_run "cd $mountpoint; md5sum -c $md5; cd /";
-
-    # cleanup
+    assert_script_run "mount -t ext4 $device $mountpoint";
+    my $md5_copy = md5sum $file_copy;
+    die "MD5 sum mismatch: $md5_orig != $md5_copy" unless $md5_orig eq $md5_copy;
     assert_script_run("umount $mountpoint");
-    assert_script_run("rm -r $tmp");
+
+
+    if (@{zypper_search('lklfuse')}) {
+        install_package 'lklfuse';
+        assert_script_run "usermod -a -G disk bernhard";
+
+        select_user_serial_terminal;
+        $mountpoint = "/home/bernhard/mount";
+        $file = "/home/bernhard/file";
+        $file_copy = "$mountpoint/file2";
+
+        assert_script_run "dd if=/dev/urandom of=$file bs=1M count=15";
+        $md5_orig = md5sum $file;
+        assert_script_run "mkdir -p $mountpoint";
+        assert_script_run "lklfuse $device $mountpoint -o type=ext4";
+        my $mounts = script_output "mount";
+        die "$mountpoint not found in mtab!" unless $mounts =~ /\Q$mountpoint\E/;
+        assert_script_run "cp $file $file_copy";
+        assert_script_run "fusermount -u $mountpoint";
+        assert_script_run "sync";
+        # make sure the file doesn't exist after unmointing, this would mean the device wasn't mounted
+        assert_script_run "! [ -f $file_copy ]";
+        assert_script_run "lklfuse $device $mountpoint -o type=ext4";
+        record_info "Drive contents", script_output("ls $mountpoint");
+        my $md5_copy = md5sum $file_copy;
+        die "MD5 sum mismatch: $md5_orig != $md5_copy" unless $md5_orig eq $md5_copy;
+        assert_script_run "sync";
+        script_run "fusermount -u $mountpoint";
+
+    } elsif (is_sle('16+')) {
+        die "running on SLE16+ but lklfuse package is missing";
+    }
 }
 
 sub test_flags {
