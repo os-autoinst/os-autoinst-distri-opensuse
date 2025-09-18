os-autoinst/os-autoinst-distri-opensuse

commit df891de5672e7d4580c1402c1143dfadf103b91b

Mark 'empty repos after registration' with soft-failure
This commit introduces a check for the zypper exit code in a publiccloud
related zypper call so that we can check for the specific exit code and
record a reference to a known bug on detection. This commit also
introduces the dependency on Feature::Compat::Try which is unproblematic
as the same dependency is already mandatory for os-autoinst anyway.

lib/publiccloud/utils.pm
@@ -230,6 +230,8 @@ sub register_addons_in_pc {
     # Workaround for bsc#1245220
     my $env = is_sle("=15-SP3") ? "ZYPP_CURL2=1" : "";
     my $cmd = "sudo $env zypper -n --gpg-auto-import-keys ref";
+    my $ret = script_run $cmd, timeout => 300;
+    die 'No enabled repos defined: bsc#1245651' if $ret == 6;    # from zypper man page: ZYPPER_EXIT_NO_REPOS
     $instance->ssh_script_retry(cmd => $cmd, timeout => 300, retry => 3, delay => 120);
     for my $addon (@addons) {
         next if ($addon =~ /^\s+$/);

cpanfile
@@ -16,6 +16,7 @@ requires 'Date::Parse';
 requires 'DateTime';
 requires 'Digest::file';
 requires 'Exporter';
+requires 'Feature::Compat::Try';
 requires 'File::Basename';
 requires 'File::Copy';
 requires 'File::Find';

os-autoinst-distri-opensuse-deps.spec
@@ -37,6 +37,7 @@ Requires:       perl(Date::Parse)
 Requires:       perl(DateTime)
 Requires:       perl(Digest::file)
 Requires:       perl(Exporter)
+Requires:       perl(Feature::Compat::Try)
 Requires:       perl(File::Basename)
 Requires:       perl(File::Copy)
 Requires:       perl(File::Find)

tests/publiccloud/registration.pm
@@ -10,6 +10,7 @@
 # Maintainer: <qa-c@suse.de>
 
 use Mojo::Base 'publiccloud::basetest';
+use Feature::Compat::Try;
 use version_utils;
 use registration;
 use testapi;
@@ -24,7 +25,11 @@ sub run {
     select_host_console();    # select console on the host, not the PC instance
 
     registercloudguest($args->{my_instance}) if (is_byos() || get_var('PUBLIC_CLOUD_FORCE_REGISTRATION'));
-    register_addons_in_pc($args->{my_instance});
+    try {
+        register_addons_in_pc($args->{my_instance});
+    } catch ($e) {
+        force_soft_failure 'bsc#1245651' if $e =~ /bsc#1245651/;
+    }
     # Since SLE 15 SP6 CHOST images don't have curl and we need it for testing
     if (is_sle('>15-SP5') && is_container_host()) {
         $args->{my_instance}->ssh_assert_script_run('sudo zypper -n in --force-resolution -y curl');

commit 084b5af11a5cbccd57ae6d570beda7836f4ee5dc

Fix wrong context execution introduced by df891de5672

lib/publiccloud/utils.pm
@@ -231,7 +231,7 @@ sub register_addons_in_pc {
     # Workaround for bsc#1245220
     my $env = is_sle("=15-SP3") ? "ZYPP_CURL2=1" : "";
     my $cmd = "sudo $env zypper -n --gpg-auto-import-keys ref";
-    my $ret = script_run $cmd, timeout => 300;
+    my $ret = $instance->ssh_script_run(cmd => $cmd, timeout => 300);
     die 'No enabled repos defined: bsc#1245651' if $ret == 6;    # from zypper man page: ZYPPER_EXIT_NO_REPOS
     $instance->ssh_script_retry(cmd => $cmd, timeout => 300, retry => 3, delay => 120);
     for my $addon (@addons) {