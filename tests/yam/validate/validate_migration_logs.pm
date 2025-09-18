fef0a081cf39446529f9d44b622fdcf374ccc0d3
lemon.li <leli@suse.com>
2025-09-18 05:33:38 -0400

    Update logs for s390x
    Need upload logs of /var/cache/wicked_config, /var/cache/udev_rules and
    /etc/udev/rules.d after migration.

--- tests/yam/validate/validate_migration_logs.pm
+++ tests/yam/validate/validate_migration_logs.pm
@@ -3,7 +3,6 @@
 # Copyright 2025 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
-
 # Summary: Validate logs after migration to sle 16.
 # Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
 
@@ -15,6 +14,13 @@ sub run {
     select_console 'root-console';
 
     upload_logs("/var/log/distro_migration.log", failok => 1);
+    script_run 'tar zcvf /tmp/cache_wicked_config.tar.gz /var/cache/wicked_config/*';
+    upload_logs("/tmp/cache_wicked_config.tar.gz", failok => 1);
+    script_run 'tar zcvf /tmp/cache_udev_rules.tar.gz /var/cache/udev_rules/*';
+    upload_logs("/tmp/cache_udev_rules.tar.gz", failok => 1);
+    script_run("tar czvf /tmp/udev_rules.tar.gz /etc/udev/rules.d/*", {timeout => 60});
+    upload_logs("/tmp/udev_rules.tar.gz", failok => 1);
+
     if (script_run("cat /var/log/distro_migration.log | grep -i -E \"migration failed|aborting migration\"") == 0) {
         record_info("Migration failed", script_output("cat /var/log/distro_migration.log | grep -i -E \"migration failed|aborting migration\" -B50"), result => 'fail');
         die("Migration failed");
