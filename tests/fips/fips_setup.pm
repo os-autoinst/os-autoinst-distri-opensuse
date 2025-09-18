63c8b8ee8dea9c46efb05bc3c9563cdbcab1fb5e
Paolo Stivanin <info@paolostivanin.com>
2025-09-18 15:13:41 +0200

    Add VA stuff

--- tests/fips/fips_setup.pm
+++ tests/fips/fips_setup.pm
@@ -9,7 +9,7 @@
 # Maintainer: QE Security <none@suse.de>
 # Tags: poo#39071, poo#105591, poo#105999, poo#109133
 
-use base qw(consoletest);
+use base 'consoletest';
 use testapi;
 use bootloader_setup qw(add_grub_cmdline_settings change_grub_config);
 use power_action_utils 'power_action';
@@ -19,6 +19,7 @@ use utils qw(zypper_call reconnect_mgmt_console);
 use Utils::Backends 'is_pvm';
 use Utils::Architectures 'is_aarch64';
 use version_utils qw(is_jeos is_sle_micro is_sle is_tumbleweed is_transactional is_microos);
+use security::vendoraffirmation;
 
 my @vars = ('OPENSSL_FIPS', 'OPENSSL_FORCE_FIPS_MODE', 'LIBGCRYPT_FORCE_FIPS_MODE', 'NSS_FIPS', 'GNUTLS_FORCE_FIPS_MODE');
 
@@ -78,6 +79,7 @@ sub install_fips {
         zypper_call("in crypto-policies-scripts");
         # Explicitly install openssl-3 on s390x SLE16 https://bugzilla.suse.com/show_bug.cgi?id=1247463
         zypper_call("in openssl-3") if (is_s390x && is_sle('>=16'));
+        install_vendor_affirmation_pkgs if (check_var('FIPS_USE_CERT_MODULE', '1') && is_sle('=15-SP7'));
     } elsif (is_sle('<=15-SP3') || get_var("FIPS_ENV_MODE")) {
         # No crypto-policies in older SLE
         zypper_call("in -t pattern fips");
