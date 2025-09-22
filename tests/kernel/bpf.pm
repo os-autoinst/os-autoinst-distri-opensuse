a22ff7082bf80269c048089a383a6fde5c6b34e2
Ricardo B. Marlière <rbm@opensuse.org>
2025-09-22 11:47:12 -0300

    kernel: Add BPF smoke test

---
+++ tests/kernel/bpf.pm
@@ -0,0 +1,36 @@
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Package: bpf
+# Summary: Compile and load a BPF sample program from Linux mainline
+# Maintainer: kernel-qa@suse.de
+
+use Mojo::Base qw(opensusebasetest);
+use testapi;
+use utils;
+use version_utils qw(is_sle);
+use registration qw(add_suseconnect_product get_addon_fullname);
+use serial_terminal qw(select_serial_terminal);
+
+sub run {
+    my ($self) = @_;
+
+    select_serial_terminal;
+    record_info('KERNEL VERSION', script_output('uname -a'));
+
+    add_suseconnect_product(get_addon_fullname('phub')) if is_sle;    # For clang
+    zypper_call("in clang bpftool libbpf-devel");
+
+    # Build the BPF program
+    assert_script_run('curl -sO ' . data_url('kernel/trace_output.bpf.c'));
+    assert_script_run('bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h');
+    assert_script_run('clang -g -O2 -target bpf -c trace_output.bpf.c');
+
+    # Build the userspace test runner
+    assert_script_run('curl -sO ' . data_url('kernel/trace_output_user.c'));
+    assert_script_run('clang -lbpf -o trace_output trace_output_user.c');
+
+    record_info('BPF Sample', script_output('./trace_output'));
+}
+
+1;
