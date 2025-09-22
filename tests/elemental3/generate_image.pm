b3e471728bba57043b2f9ca68f5742f3b15c10d0
Loic Devulder <ldevulder@suse.com>
2025-09-22 10:29:58 +0200

    Extend Elemental3 tests
    Add support to use distros-test-framework for K8s testing.
    
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- tests/elemental3/generate_image.pm
+++ tests/elemental3/generate_image.pm
@@ -9,26 +9,23 @@
 # Maintainer: unified-core@suse.com, ldevulder@suse.com
 
 use base qw(opensusebasetest);
-use experimental qw(switch);
-
 use testapi;
+use experimental qw(switch);
 use transactional qw(trup_call);
 use serial_terminal qw(select_serial_terminal);
 use Mojo::File qw(path);
-use utils qw(file_content_replace zypper_call);
+use utils qw(file_content_replace);
 use Utils::Architectures qw(is_aarch64);
 
 sub run {
-    select_serial_terminal;
-
-    # Variables
     my $arch = get_required_var('ARCH');
     my $build = get_required_var('BUILD');
     my $image = get_required_var('CONTAINER_IMAGE_TO_TEST');
     my $repo_to_test = get_required_var('REPO_TO_TEST');
     my $rootpwd = get_required_var('TEST_PASSWORD');
     my $sysext_path = get_required_var('SYSEXT_PATH');
-    my $hdd_size = get_var('HDDSIZEGB', 30);
+    my $k8s = get_required_var('K8S');
+    my $hdd_size = get_var('HDDSIZEGB', '30');
     my $img_filename = "elemental-$build-$arch";
     my $shared_dir = '/root/shared';
     my $config_file = "$shared_dir/config.sh";
@@ -45,23 +42,30 @@ sub run {
     # Define timeouts based on the architecture
     my $timeout = (is_aarch64) ? 480 : 240;
 
+    # No GUI, easier and quicker to use the serial console
+    select_serial_terminal();
+
     # Set SELinux in permissive mode, as there is an issue with setfiles
     # It will be removed as soon as the issue will be fixed
     assert_script_run("setenforce Permissive");
-    validate_script_output("sestatus | grep 'Current mode:'", sub { m/permissive/ });
+    validate_script_output("sestatus | grep 'Current mode:'",
+        sub { m/permissive/ });
 
     # Create directories
     assert_script_run("mkdir -p $sysext_dir");
 
     # Add Unified Core repository and install Elemental package
     trup_call("run zypper addrepo --check --refresh $repo_to_test elemental");
-    trup_call("--continue run zypper --gpg-auto-import-keys refresh");
-    trup_call("--continue pkg install elemental3ctl");
-    trup_call("apply");
+    trup_call('--continue run zypper --gpg-auto-import-keys refresh');
+    trup_call('--continue pkg install elemental3ctl');
+    trup_call('apply');
 
     # OS configuration script
-    assert_script_run('curl -f ' . data_url('elemental3/' . path($config_file)->basename) . ' -o ' . $config_file);
-    file_content_replace($config_file, '%TEST_PASSWORD%' => $rootpwd);
+    assert_script_run(
+        "curl -v -o $config_file "
+          . data_url('elemental3/' . path($config_file)->basename)
+    );
+    file_content_replace($config_file, '%TEST_PASSWORD%' => $rootpwd, '%K8S%' => $k8s);
     assert_script_run("chmod 755 $config_file");
 
     # Define architecture for the system extensions
@@ -76,12 +80,18 @@ sub run {
 
     # Get the system extensions list
     # NOTE: '/' is mandatory at the end of $sysext_path!
-    my @list = split(/[\r\n]+/, script_output("curl -s ${sysext_path}/ | sed -n 's/.*href=\"\\(.*_${sysext_arch}.raw\\)\">.*/\\1/p'"));
+    my @list = split(
+        /[\r\n]+/,
+        script_output(
+            "curl -s ${sysext_path}/ | sed -n 's/.*href=\"\\(.*_${sysext_arch}.raw\\)\">.*/\\1/p'"
+        )
+    );
 
     # Clean the list
     foreach (sort @list) {
         # RKE2 is hard-coded but for now we don't support anything else
         if ($_ =~ /rke2/) {
+
             # Keep only the first RKE2 version found (the lower version)
             # Higher versions can be used in another upgrade test
             next if $rke2_sysext_found;
@@ -92,23 +102,33 @@ sub run {
 
     # Get the system extensions
     foreach my $sysext (@sysexts) {
-        assert_script_run("curl -f ${sysext_path}/${sysext} -o ${sysext_dir}/${sysext}", 300);
+        assert_script_run(
+            "curl -v -o ${sysext_dir}/${sysext} ${sysext_path}/${sysext}",
+            300);
     }
 
     # Package the system extensions
     assert_script_run("tar cvaf $overlay -C $sysext_root .");
 
     # Create a raw image and mount it as a loop device
-    assert_script_run("qemu-img create -f raw $shared_dir/$img_filename.raw ${hdd_size}G");
-    my $device = script_output("losetup --find --show $shared_dir/$img_filename.raw");
+    assert_script_run(
+        "qemu-img create -f raw $shared_dir/$img_filename.raw ${hdd_size}G");
+    my $device =
+      script_output("losetup --find --show $shared_dir/$img_filename.raw");
 
     # Generate RAW image
     record_info('QCOW2', 'Generate and upload QCOW2 image');
-    assert_script_run("elemental3ctl --debug install --os-image $image --overlay tar://$overlay --config $config_file --target $device", $timeout);
+    assert_script_run(
+        "elemental3ctl --debug install --os-image $image --overlay tar://$overlay --config $config_file --target $device",
+        $timeout
+    );
 
     # Generate and upload QCOW2 image
     assert_script_run("losetup -d $device");
-    assert_script_run("qemu-img convert -c -p -f raw -O qcow2 $shared_dir/$img_filename.raw $shared_dir/$img_filename.qcow2", $timeout);
+    assert_script_run(
+        "qemu-img convert -c -p -f raw -O qcow2 $shared_dir/$img_filename.raw $shared_dir/$img_filename.qcow2",
+        $timeout
+    );
     upload_asset("$shared_dir/$img_filename.qcow2", 1);
 }
 
