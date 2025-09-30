18f99795db00b14c302d5a4f92ad8cdb560b2a38
Santiago Zarate <santiago.zarate@suse.com>
2025-09-30 11:21:20 +0200

    Select HD to boot from unconditionally
    Partially revert 8a9d5c1a12618ff2c99d3c7751b76b984b6eff28

--- lib/opensusebasetest.pm
+++ lib/opensusebasetest.pm
@@ -336,9 +336,7 @@ sub handle_uefi_boot_disk_workaround {
     send_key_until_needlematch 'tianocore-boot_from_file', 'down';
     wait_screen_change { send_key 'ret' };
     # Device selection: HD or CDROM
-    (is_agama && is_aarch64 && get_var('BOOTFROM') eq 'd') ?
-      map { send_key 'down' } (1 .. 4) :
-      send_key_until_needlematch 'tianocore-select_HD', 'down';
+    send_key_until_needlematch 'tianocore-select_HD', 'down';
     wait_screen_change { send_key 'ret' };
     # cycle to last entry by going up in the next steps
     # <EFI>
