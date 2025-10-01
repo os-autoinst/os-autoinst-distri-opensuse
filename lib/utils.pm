os-autoinst/os-autoinst-distri-opensuse

commit 27da75aa8e9767c25024d9922a18949270a56c0e

zypper_search: Properly handle empty package list (#23353)
Add the --ignore-unknown (-i) parameter to zypper search commands
so that empty repositories or missing packages don't result in error
exit code.

lib/utils.pm
@@ -950,7 +950,7 @@ sub zypper_search {
         @fields = ('status', 'name', 'type', 'version', 'arch', 'repository');
     }
 
-    my $output = script_output("zypper -n se $params");
+    my $output = script_output("zypper -in se $params");
     return parse_zypper_table($output, \@fields);
 }
 

commit 7e0fb4cd772767755a7c2ada7d4513cecea12f1d

zypper_search: Fix code on SLE11-SP4
--ignore-unknown (-i) parameter to zypper search was added in zypper
1.8.7 in commit
https://github.com/openSUSE/zypper/commit/cdf3877d5fc2c8fd5ee0d51cbfca07ce6f27a625

SLE11-SP4 has older zypper 1.6.340, option is not supported on SLE11:

    Unknown option 'i'
    SCRIPT_FINISHEDPZEFL-2- at /usr/lib/os-autoinst/distribution.pm line 305.
	    distribution::script_output(Distribution::Opensuse::Tumbleweed=HASH(0x555acb862038), "zypper -in se -s -t package -r kernel-update-0", "timeout", undef, "quiet", undef, "type_command", undef, ...) called at /usr/lib/os-autoinst/testapi.pm line 1102
	    testapi::script_output("zypper -in se -s -t package -r kernel-update-0") called at sle/lib/utils.pm line 953

We still support SLE11-SP4 LTEC on kernel, where it is used. Rewrite
code to use zypper_call() to allow exit 0 and 104 and parse the output.

Fixes: 27da75aa8e ("zypper_search: Properly handle empty package list (#23353)")
Suggested-by: Martin Doucha <mdoucha@suse.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

lib/utils.pm
@@ -1004,7 +1004,8 @@ sub zypper_search {
         @fields = ('status', 'name', 'type', 'version', 'arch', 'repository');
     }
 
-    my $output = script_output("zypper -in se $params");
+    zypper_call("search $params", log => 'zypper.log', exitcode => [0, 104]);
+    my $output = script_output('cat /tmp/zypper.log');
     return parse_zypper_table($output, \@fields);
 }
 