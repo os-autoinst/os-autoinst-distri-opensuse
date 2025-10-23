0786673eb44d29a61a62dc6388055281e08ab3fd
Ricardo B. Marlière <rbm@opensuse.org>
2025-10-23 12:04:33 -0300

    publiccloud: nvidia: Limit tests on 15-SP4/SP5
    Port commit 6a8b56f91761 ("nvidia: Fix installation and limit tests on
    15-SP4/SP5") to Public Cloud test.

--- tests/publiccloud/nvidia.pm
+++ tests/publiccloud/nvidia.pm
@@ -11,20 +11,22 @@ use registration;
 use testapi;
 use utils;
 use nvidia_utils;
-use version_utils qw(is_sle);
+use version_utils qw(is_sle is_sle_micro);
 
 sub run
 {
     my ($self, $args) = @_;
 
-    nvidia_utils::install();
-    $args->{my_instance}->softreboot(timeout => get_var('PUBLIC_CLOUD_REBOOT_TIMEOUT', 600));
-    nvidia_utils::validate();
-
     nvidia_utils::install(variant => "cuda");
     $args->{my_instance}->softreboot(timeout => get_var('PUBLIC_CLOUD_REBOOT_TIMEOUT', 600));
     nvidia_utils::validate();
     nvidia_utils::validate_cuda() if is_sle;
+
+    if (is_sle('15-SP6+') || is_sle_micro('6.0+')) {
+        nvidia_utils::install();
+        $args->{my_instance}->softreboot(timeout => get_var('PUBLIC_CLOUD_REBOOT_TIMEOUT', 600));
+        nvidia_utils::validate();
+    }
 }
 
 sub test_flags {
