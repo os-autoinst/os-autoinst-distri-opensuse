efbb3b72cd92b107bf6dea0a6bb27dbe16bd1a27
Loic Devulder <ldevulder@suse.com>
2025-10-31 18:47:29 +0100

    elemental3: Add test for 'build' command
    Signed-off-by: Loic Devulder <ldevulder@suse.com

--- tests/elemental3/generate_image.pm
+++ tests/elemental3/generate_image.pm
@@ -16,55 +16,93 @@ use Mojo::File qw(path);
 use utils qw(file_content_replace);
 use Utils::Architectures qw(is_aarch64);
 
-sub run {
-    my $arch = get_required_var('ARCH');
-    my $build = get_required_var('BUILD');
+sub build_cmd {
+    my (%args) = @_;
+    my $build_dir = '/root/build';
+    my $tpl_tar = "$build_dir/build-tpl.tar.gz";
+    my $krnlcmdline = get_var('KERNEL_CMD_LINE');
+    my $manifest_uri = get_required_var('RELEASE_MANIFEST_URI');
+
+    # Create directories
+    assert_script_run("mkdir -p $build_dir");
+
+    # Download build configuration files
+    assert_script_run(
+        "curl -v -o $tpl_tar "
+          . data_url('elemental3/' . path($tpl_tar)->basename)
+    );
+    assert_script_run("tar xzvf $tpl_tar -C $build_dir");
+
+    # Configure the build
+    my $hashpwd = script_output("openssl passwd -6 $args{rootpwd}");
+    file_content_replace(
+        "$build_dir/butane.yaml",
+        '--sed-modifier' => 'g',
+        '%TEST_PASSWORD%' => $hashpwd,
+        '%K8S%' => $args{k8s}
+    );
+    file_content_replace(
+        "$build_dir/install.yaml",
+        '--sed-modifier' => 'g',
+        '%HDDSIZE%' => $args{hddsize},
+        '%KERNEL_CMD_LINE%' => $krnlcmdline
+    );
+    file_content_replace(
+        "$build_dir/release.yaml",
+        '--sed-modifier' => 'g',
+        '%RELEASE_MANIFEST_URI%' => $manifest_uri,
+        '%K8S%' => $args{k8s}
+    );
+
+    # Generate OS image
+    assert_script_run(
+        "elemental3 --debug build --image-type raw --config-dir $build_dir --output uc_image.raw",
+        $args{timeout}
+    );
+
+    # Convert RAW to QCOW2
+    assert_script_run(
+        "qemu-img convert -p -f raw -O qcow2 uc_image.raw ./$args{img_filename}.qcow2",
+        $args{timeout}
+    );
+
+    # Return HDD image
+    return ("$args{img_filename}.qcow2");
+}
+
+sub install_cmd {
+    my (%args) = @_;
     my $image = get_required_var('CONTAINER_IMAGE_TO_TEST');
-    my $repo_to_test = get_required_var('REPO_TO_TEST');
-    my $rootpwd = get_required_var('TEST_PASSWORD');
     my $sysext_path = get_required_var('SYSEXT_PATH');
-    my $k8s = get_required_var('K8S');
-    my $hdd_size = get_var('HDDSIZEGB', '30');
-    my $img_filename = "elemental-$build-$arch.qcow2";
-    my $shared_dir = '/root/shared';
     my $config_file = "$shared_dir/config.sh";
+    my $shared_dir = '/root/shared';
     my $sysext_root = "$shared_dir/sysexts";
     my $sysext_dir = "$sysext_root/etc/extensions";
     my $overlay = "$shared_dir/sysexts.tar.gz";
     my $device = '/dev/nbd0';
     my $sysext_arch;
-    my $rke2_sysext_found;
+    my $k8s_sysext_found;
     my @sysexts;
-
-    # Clean image filename (useful for cloned jobs)
-    $img_filename =~ tr/\/#/_/;
-
-    # Define timeouts based on the architecture
-    my $timeout = (is_aarch64) ? 480 : 240;
-
-    # No GUI, easier and quicker to use the serial console
-    select_serial_terminal();
 
     # Create directories
     assert_script_run("mkdir -p $sysext_dir");
-
-    # Add Unified Core repository and install Elemental package
-    trup_call("run zypper addrepo --check --refresh $repo_to_test elemental");
-    trup_call('--continue run zypper --gpg-auto-import-keys refresh');
-    trup_call('--continue pkg install elemental3ctl');
-    trup_call('apply');
 
     # OS configuration script
     assert_script_run(
         "curl -v -o $config_file "
           . data_url('elemental3/' . path($config_file)->basename)
     );
-    file_content_replace($config_file, '%TEST_PASSWORD%' => $rootpwd, '%K8S%' => $k8s);
+    file_content_replace(
+        $config_file,
+        '--sed-modifier' => 'g',
+        '%TEST_PASSWORD%' => $args{rootpwd},
+        '%K8S%' => $args{k8s}
+    );
     assert_script_run("chmod 755 $config_file");
 
     # Define architecture for the system extensions
-    $sysext_arch = 'arm64' if ($arch eq 'aarch64');
-    $sysext_arch = 'x86-64' if ($arch eq 'x86_64');
+    $sysext_arch = 'arm64' if ($args{arch} eq 'aarch64');
+    $sysext_arch = 'x86-64' if ($args{arch} eq 'x86_64');
 
     # Get the system extensions list
     # NOTE: '/' is mandatory at the end of $sysext_path!
@@ -77,13 +115,12 @@ sub run {
 
     # Clean the list
     foreach (sort @list) {
-        # RKE2 is hard-coded but for now we don't support anything else
-        if ($_ =~ /rke2/) {
+        if ($_ =~ /$args{k8s}/) {
 
-            # Keep only the first RKE2 version found (the lower version)
+            # Keep only the first K8s version found (the lower version)
             # Higher versions can be used in another upgrade test
-            next if $rke2_sysext_found;
-            $rke2_sysext_found = 1;
+            next if $k8s_sysext_found;
+            $k8s_sysext_found = 1;
         }
         push @sysexts, $_;
     }
@@ -101,21 +138,71 @@ sub run {
     record_info('QCOW2', 'Generate and upload QCOW2 image');
 
     # Create a raw image and mount it
-    assert_script_run("qemu-img create -f qcow2 $shared_dir/$img_filename ${hdd_size}G");
+    assert_script_run("qemu-img create -f qcow2 $shared_dir/$args{img_filename}.qcow2 ${hddsize}G");
     assert_script_run('modprobe nbd');
-    assert_script_run("qemu-nbd -c $device $shared_dir/$img_filename");
-
-    # Set SELinux in permissive mode, as there is an issue with enforcing mode and Elemental3 doesn't support it yet
-    assert_script_run("setenforce permissive");
+    assert_script_run("qemu-nbd -c $device $shared_dir/$args{img_filename}.qcow2");
 
     # Generate OS image
     assert_script_run(
         "elemental3ctl --debug install --os-image $image --overlay tar://$overlay --config $config_file --target $device",
-        $timeout
+        $args{timeout}
     );
 
+    # Return HDD image
+    return ("$shared_dir/$args{img_filename}.qcow2");
+}
+
+sub run {
+    my $arch = get_required_var('ARCH');
+    my $k8s = get_required_var('K8S');
+    my $hddsize = get_var('HDDSIZEGB', '30');
+    my $rootpwd = get_required_var('TEST_PASSWORD');
+    my $build = get_required_var('BUILD');
+    my $repo_to_test = get_required_var('REPO_TO_TEST');
+    my $img_filename = "elemental-$build-$arch";
+    my $out_file;
+
+    # Clean image filename (useful for cloned jobs)
+    $img_filename =~ tr/\/#/_/;
+
+    # Define timeouts based on the architecture
+    my $timeout = (is_aarch64) ? 960 : 480;
+
+    # No GUI, easier and quicker to use the serial console
+    select_serial_terminal();
+
+    # Add Unified Core repository and install Elemental package
+    trup_call("run zypper addrepo --check --refresh $repo_to_test elemental");
+    trup_call('--continue run zypper --gpg-auto-import-keys refresh');
+    trup_call('--continue pkg install elemental3 elemental3ctl');
+    trup_call('apply');
+
+    # Set SELinux in permissive mode, as there is an issue with enforcing mode and Elemental3 doesn't support it yet
+    assert_script_run("setenforce permissive");
+
+    # Create HDD image with different commands
+    $out_file = build_cmd(
+        timeout => $timeout,
+        k8s => $k8s,
+        hddsize => $hddsize,
+        rootpwd => $rootpwd,
+        build => $build,
+        repo_to_test => $repo_to_test,
+        img_filename => $img_filename
+    ) if check_var('TEST_TYPE', 'generate_with_build_cmd');
+    $out_file = install_cmd(
+        timeout => $timeout,
+        arch => $arch,
+        k8s => $k8s,
+        hddsize => $hddsize,
+        rootpwd => $rootpwd,
+        build => $build,
+        repo_to_test => $repo_to_test,
+        img_filename => $img_filename
+    ) if check_var('TEST_TYPE', 'generate_with_install_cmd');
+
     # Upload QCOW2 image
-    upload_asset("$shared_dir/$img_filename", 1);
+    upload_asset("$out_file", 1);
 }
 
 sub test_flags {
