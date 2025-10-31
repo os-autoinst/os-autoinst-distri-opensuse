2afb1a968ba60aba3c1c861e1390c8d1e22b9057
Huajian.Luo <hluo@suse.com>
2025-10-31 15:19:56 +0800

    Change the file system to Btrfs without snapshots
    - Change the file system to Btrfs without snapshots
    - Related ticket: https://progress.opensuse.org/issues/189879

---
+++ tests/yam/validate/validate_btrfs_without_snapshots.pm
@@ -0,0 +1,20 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that the snapshot is not configured for Btrfs without snapshots
+
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use testapi;
+
+sub run {
+    select_console 'root-console';
+
+    assert_script_run('! snapper list');
+    assert_script_run(qq{snapper list 2>&1 | grep "The config 'root' does not exist. Likely snapper is not configured."});
+}
+
+1;
