a4b1fb333c920f430aacdc2c57d128a599776c0f
Sebastian Chlad <sebastian.chlad@suse.com>
2025-11-11 17:48:42 +0100

    nfs: all sle codestreams should have the phub available

--- tests/kernel/nfs_stress_ng.pm
+++ tests/kernel/nfs_stress_ng.pm
@@ -30,12 +30,9 @@ sub client {
     my $run_stress_ng = "stress-ng --verbose --sequential -1 --timeout $stressor_timeout --class filesystem";
     my @paths = ($local_nfs4, $local_nfs4_async);
 
-    # in case this is SLE15 we need packagehub for stress-ng, let's enable it
-    # in case this is SLE16+ we need QA repo
-    if (is_sle('<16')) {
+    # in case this is SLE we need packagehub for stress-ng, let's enable it
+    if (is_sle) {
         add_suseconnect_product(get_addon_fullname('phub'));
-    } elsif (is_sle('>16')) {
-        add_qa_head_repo(priority => 100);
     }
 
     zypper_call("in stress-ng");
