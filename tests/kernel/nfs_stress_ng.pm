09a032265d519e1180ec3b9779cf73bb62d83e99
Sebastian Chlad <sebastian.chlad@suse.com>
2025-11-11 19:52:03 +0100

    NFS: switch stress-ng result handling to log-based parsing
    Previously, the test relied solely on stress-ng’s return value to
    determine success or failure. However, stress-ng may exit with a
    non-zero code even when all stressors pass successfully.
    
    This patch adds the --yaml and --log-file options to stress-ng runs
    to produce structured and complete runtime data. Based on that,
    the results are parsed and evaluated to determine test outcome.
    
    It also introduces an initial log parser that scans the generated
    stress-ng log file for detailed result metrics:
      - passed
      - failed
      - skipped
      - metrics-untrustworthy
    
    If any 'failed' or 'metrics-untrustworthy' entries are detected,
    the test module is marked as failed

--- tests/kernel/nfs_stress_ng.pm
+++ tests/kernel/nfs_stress_ng.pm
@@ -15,6 +15,25 @@ use utils;
 use registration;
 use version_utils 'is_sle';
 
+sub parse_stress_ng_log {
+    my ($file) = @_;
+    my $out = script_output("cat $file 2>/dev/null || echo ''");
+
+    my %results = (
+        failed => 0,
+        passed => 0,
+        skipped => 0,
+        untrustworthy => 0,
+    );
+
+    if ($out =~ /failed:\s+(\d+)/) { $results{failed} = $1; }
+    if ($out =~ /passed:\s+(\d+)/) { $results{passed} = $1; }
+    if ($out =~ /skipped:\s+(\d+)/) { $results{skipped} = $1; }
+    if ($out =~ /metrics[- ]untrustworthy:\s+(\d+)/) { $results{untrustworthy} = $1; }
+
+    return \%results;
+}
+
 sub server {
     barrier_wait('NFS_STRESS_NG_START');
     barrier_wait('NFS_STRESS_NG_END');
@@ -27,7 +46,6 @@ sub client {
     my $local_nfs4 = "/home/localNFS4";
     my $local_nfs4_async = "/home/localNFS4async";
     my $stressor_timeout = get_var('NFS_STRESS_NG_TIMEOUT') // 3;
-    my $run_stress_ng = "stress-ng --verbose --sequential -1 --timeout $stressor_timeout --class filesystem";
     my @paths = ($local_nfs4, $local_nfs4_async);
 
     # in case this is SLE we need packagehub for stress-ng, let's enable it
@@ -45,17 +63,29 @@ sub client {
     my $result = 0;
     foreach my $path (@paths) {
         assert_script_run('cd ' . $path);
+        my ($dirname) = $path =~ m|([^/]+)$|;
+        my $yaml = "/tmp/stress-ng_${dirname}.yaml";
+        my $log = "/tmp/stress-ng_${dirname}.log";
+
+        my $run_stress_ng = "stress-ng --verbose --sequential -1 --timeout $stressor_timeout " .
+          "--class filesystem " .
+          "--metrics-brief --yaml $yaml --log-file $log";
+
         my $ret = script_run($run_stress_ng, timeout => $stressor_timeout * 100);
 
-        if ($ret == 0) {
-            record_info('stress-ng', "return: 0 (success), path: $path");
-        } elsif ($ret == 2) {
-            record_info('stress-ng', "return: 2 (stressor failed), path: $path", result => 'fail');
-        } else {
-            record_info('stress-ng', "return: $ret (other failure), path: $path", result => 'fail');
-        }
+        my $metrics = parse_stress_ng_log($log);
+        record_info(
+            "Summary [$dirname]",
+            "passed=$metrics->{passed}, failed=$metrics->{failed}, skipped=$metrics->{skipped}, untrustworthy=$metrics->{untrustworthy}"
+        );
 
-        $result = $ret if ($ret > $result);
+        if ($metrics->{failed} > 0 || $metrics->{untrustworthy} > 0) {
+            record_info('stress-ng', "Detected failed or untrustworthy metrics on path: $path", result => 'fail');
+            $result = 1;
+        }
+        # TEMP
+        upload_logs($yaml, failok => 1);
+        upload_logs($log, failok => 1);
     }
 
     barrier_wait('NFS_STRESS_NG_END');
@@ -80,6 +110,13 @@ sub run {
     } else {
         $self->server;
     }
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+    upload_logs('/tmp/stress-ng*.yaml', failok => 1);
+    upload_logs('/tmp/stress-ng*.log', failok => 1);
+    $self->SUPER::post_fail_hook;
 }
 
 1;
