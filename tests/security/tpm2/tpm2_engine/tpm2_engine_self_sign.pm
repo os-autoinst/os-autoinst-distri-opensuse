d6b8a60a63576f2cd717441ffbebab2dfb7f45a5
Emil Miler <em@0x45.cz>
2025-11-13 10:54:51 +0100

    Refactor tpm2_engine_self_sign test module

--- tests/security/tpm2/tpm2_engine/tpm2_engine_self_sign.pm
+++ tests/security/tpm2/tpm2_engine/tpm2_engine_self_sign.pm
@@ -1,4 +1,4 @@
-# Copyright 2020 SUSE LLC
+# Copyright SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
 # Summary: Per TPM2 stack, we would like to add the tpm2-tss-engine,
@@ -14,32 +14,15 @@ use serial_terminal 'select_serial_terminal';
 
 sub run {
     select_serial_terminal;
-
-    # Self Signed certificate generate operation
-    my $test_dir = "tpm2_engine_self_sign";
+    my $test_dir = "tpm2_engine_self_sign_$$";
     my $tss_file = "rsa.tss";
-    my $crt_file = "rsa.tst";
-    assert_script_run "mkdir $test_dir";
-    assert_script_run "cd $test_dir";
+    my $crt_file = "rsa.crt";
+    my $subject = "/C=CZ/ST=Prague/L=Prague/O=SUSE/OU=QA/CN=geeko";
+    assert_script_run "mkdir $test_dir; cd $test_dir";
     assert_script_run "tpm2tss-genkey -a rsa $tss_file";
-    assert_script_run "expect -c 'spawn openssl req -new -x509 -engine tpm2tss -key $tss_file -keyform engine -out $crt_file; 
-expect \"Country Name (2 letter code) \\[AU\\]\"; send \"CN\\r\";
-expect \"State or Province Name (full name) \\[Some-State\\]:\"; send \"Beijing\\r\";
-expect \"Locality Name (eg, city) \\[\\]:\"; send \"Beijing\\r\";
-expect \"Organization Name (eg, company) \\[Internet Widgits Pty Ltd\\]:\"; send \"SUSE\\r\";
-expect \"Organizational Unit Name (eg, section) \\[\\]:\"; send \"QA\\r\";
-expect \"Common Name (e.g. server FQDN or YOUR name) \\[\\]:\"; send \"richard\\r\";
-expect \"Email Address \\[\\]:\"; send \"richard.fan\@suse.com\\r\";
-expect {
-    \"error\" {
-      exit 139
-   }
-   eof {
-       exit 0
-   }
-}'";
-    assert_script_run "ls |grep $crt_file";
-    assert_script_run "cd";
+    assert_script_run "openssl req -new -x509 -engine tpm2tss -key $tss_file -keyform engine -out $crt_file -subj \"$subject\"";
+    assert_script_run "openssl x509 -in $crt_file -noout -text";
+    assert_script_run "cd; rm -rf $test_dir";
 }
 
 1;
