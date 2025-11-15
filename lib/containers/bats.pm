8eb64cca06524b5378033ee078d37fd54148bb8c
Ricardo Branco <rbranco@suse.de>
2025-11-15 17:59:20 +0100

    bats: Remove useless conditional

--- lib/containers/bats.pm
+++ lib/containers/bats.pm
@@ -255,7 +255,7 @@ sub delegate_controllers {
 sub enable_modules {
     add_suseconnect_product(get_addon_fullname('desktop'));
     add_suseconnect_product(get_addon_fullname('sdk'));
-    add_suseconnect_product(get_addon_fullname('python3')) if is_sle('>=15-SP4');
+    add_suseconnect_product(get_addon_fullname('python3'));
     # Needed for criu, fakeroot & qemu-linux-user
     add_suseconnect_product(get_addon_fullname('phub')) if check_var("BATS_PACKAGE", "buildah");
 }
