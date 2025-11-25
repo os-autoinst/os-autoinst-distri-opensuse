9a61f437e1faebf94f734eb7f9a34676ff3adc6a
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2025-11-25 13:27:29 +0100

    Test new self-update mechanism 16 (PED-12506)
    https://progress.opensuse.org/issues/190407
    
    Test new self-update mechanism 16 (PED-12506)
    
    https://progress.opensuse.org/issues/190407
    
    Squashed commit of the following:
    
    commit 69bc1e2aaf10bd8144b391b40c5b0f7c3e93d49b
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Tue Nov 25 12:35:02 2025 +0100
    
        Avoid self_update to block installation
    
    commit 400dffd27290a27919b30a7ac9975e5b8747c8b7
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Mon Nov 24 12:07:22 2025 +0100
    
        Changed schedule
    
    commit a0790387682b569dcd22f54344f44841d260b433
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Mon Nov 24 12:02:16 2025 +0100
    
        Changed validation
    
    commit b760e41a55d177b8ce78c866c0b17408074606b4
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Thu Nov 13 10:17:09 2025 +0100
    
        Added SSL parameter
    
    commit 0668166af12187ffd90c798a5fe3ea9126df004a
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Thu Nov 13 10:01:48 2025 +0100
    
        Added defined check
    
    commit bcb6c8774ecf0522a1197b4996370f7f8b92d0e6
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Thu Nov 13 09:13:10 2025 +0100
    
        Moved validate self update step
    
    commit c1725ce8b372312ff276cb97f68a95bc02b95bd4
    Author: José Luis Fernández Aguilera <jose.fernandez1@suse.com>
    Date:   Thu Nov 13 07:38:01 2025 +0100
    
        Added validation of self update
    
    Applied requested comments
    
    Remove defined
    
    Applied changes
    
    Fixed conditional

---
+++ tests/yam/validate/validate_self_update.pm
@@ -0,0 +1,22 @@
+# SUSE's openQA tests
+#
+# Copyright 2025 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Validate that the self-update is performed by Agama via /etc/live-self-update/result
+# Maintainer: QE YaST and Migration (QE Yam) <qe-yam at suse de>
+
+use base "consoletest";
+use testapi;
+
+sub run {
+    select_console 'root-console';
+    my $retcode = script_output('cat /run/live-self-update/result');
+
+    # Pass validation if retcode is 0 (All OK) or 4 - ZYPPER_EXIT_ERR_ZYPP A problem is reported by ZYPP library
+    if ($retcode != 0 && $retcode != 4) {
+        die "Self update did not ended successfully";
+    }
+}
+
+1;
