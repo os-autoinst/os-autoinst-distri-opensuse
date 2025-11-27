1c6ad48bbb7e8a3fb6f8d5b07888cab49c0f70f0
Martin Doucha <mdoucha@suse.cz>
2025-11-27 17:59:37 +0100

    Add helper functions for selecting kernel source and devel RPMs
    Different kernel flavors need different kernel source and devel
    packages for certain tests. Which packages exactly is getting
    complicated with new product changes. Add helper functions
    to unify the package selection across different tests.

--- lib/kernel.pm
+++ lib/kernel.pm
@@ -12,17 +12,47 @@ use base Exporter;
 use testapi;
 use strict;
 use utils;
-use version_utils qw(is_sle is_transactional);
+use version_utils qw(is_sle is_sle_micro is_transactional);
 use transactional;
 use warnings;
 
 our @EXPORT = qw(
   remove_kernel_packages
   get_kernel_flavor
+  get_kernel_source_flavor
+  get_kernel_devel_flavor
 );
 
 sub get_kernel_flavor {
     return get_var('KERNEL_FLAVOR', 'kernel-default');
+}
+
+sub get_kernel_source_flavor {
+    my $src_pack = 'kernel-source';
+
+    if (check_var('SLE_PRODUCT', 'slert')) {
+        $src_pack = 'kernel-source-rt'
+          unless is_sle('16+') || is_sle_micro('6.2+');
+    }
+    elsif (get_var('COCO')) {
+        $src_pack = 'kernel-source-coco';
+    }
+
+    return $src_pack;
+}
+
+sub get_kernel_devel_flavor {
+    my $devel_pack = 'kernel-devel';
+
+    if (check_var('SLE_PRODUCT', 'slert')) {
+        $devel_pack = 'kernel-devel-rt'
+          unless is_sle('16+') || is_sle_micro('6.2+');
+    }
+    elsif (get_var('COCO')) {
+        $devel_pack = 'kernel-devel-coco';
+    }
+
+    return $devel_pack;
 }
 
 sub remove_kernel_packages {
