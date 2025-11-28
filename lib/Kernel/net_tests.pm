379bf49b4319441ecc44e2fa64fc64d382b3be47
Sebastian Chlad <sebastian.chlad@suse.com>
2025-11-28 19:08:35 +0100

    ipsec: Clean up and prepare refactor of ipsecbase
    This change introduces the new module Kernel::Net, intended to replace
    the old ipsecbase as the generic base class for future multi-host kernel
    network tests (IPsec, L2TP and other)

--- lib/Kernel/net_tests.pm
+++ lib/Kernel/net_tests.pm
@@ -1,16 +1,19 @@
 # SUSE's openQA tests
 #
-# Copyright 2016-2023 SUSE LLC
+# Copyright 2023-2025 SUSE LLC
 # SPDX-License-Identifier: FSFAP
-# Summary: Base module for IPsec tests
+# Summary: Generic network base module for multimachine tests (IPsec, L2TP, etc.)
 # Maintainer: Kernel QE <kernel-qa@suse.de>
 #
 # Test requirement and topology can refer following link:
 # https://github.com/linux-test-project/ltp/issues/920
 # https://www.ipv6ready.org/docs/Phase2_IPsec_Interoperability_Latest.pdf
 
-package ipsecbase;
+package Kernel::net_tests;
 use Mojo::Base 'opensusebasetest';
+use strict;
+use warnings;
+
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use utils;
