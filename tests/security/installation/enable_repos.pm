80edfe7285b95523a1013f1392fe63739898968b
Paolo Stivanin <info@paolostivanin.com>
2025-12-08 14:21:36 +0100

    Fix enable_repos
    Ticket https://progress.opensuse.org/issues/193051

--- tests/security/installation/enable_repos.pm
+++ tests/security/installation/enable_repos.pm
@@ -13,8 +13,8 @@ use Utils::Architectures qw(is_ppc64le);
 use serial_terminal 'select_serial_terminal';
 
 sub run {
-    return unless check_var('FLAVOR', 'Full-QR') || check_var('FLAVOR', 'Full');
     is_ppc64le() ? select_console('root-console') : select_serial_terminal();
+    return unless check_var('FLAVOR', 'Full-QR') || check_var('FLAVOR', 'Full');
     zypper_call('mr -e -a');
 }
 
