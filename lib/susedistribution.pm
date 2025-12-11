os-autoinst/os-autoinst-distri-opensuse

commit 0d8189165b588185ad264077e684fcb31896613c

Disable key repetition in tty's with "disable_key_repeat"
Needs os-autoinst testapi interface version >= 47.

We already prevent key repetition in X11 since 4547579485 but did not
have means to disable key repetition in true Linux tty's. This commit is
using the new interface of os-autoinst relying on virtio-keyboard
devices to disable the key repetition whenever a new tty console is
activated. As necessary "activate_console($console,
skip_disable_key_repeat => 1)" can be called to work around problems.

Related progress issue: https://progress.opensuse.org/issues/113441

lib/susedistribution.pm
@@ -815,14 +815,14 @@ sub get_console_info {
 
 =head2 activate_console
 
-  activate_console($console [, [ensure_tty_selected => 0|1] [, skip_set_standard_prompt => 0|1] [, skip_setterm => 0|1] [, timeout => $timeout]])
+  activate_console($console [, [ensure_tty_selected => 0|1] [, skip_set_standard_prompt => 0|1] [, skip_setterm => 0|1] [, skip_disable_key_repeat => 0|1] [, timeout => $timeout]])
 
 Callback whenever a console is selected for the first time. Accepts arguments
 provided to select_console().
 
-C<skip_set_standard_prompt> and C<skip_setterm> arguments skip respective routines,
-e.g. if you want select_console() without addition console setup. Then, at some
-point, you should set it on your own.
+C<skip_set_standard_prompt>, C<skip_setterm> and C<skip_disable_key_repeat>
+arguments skip respective routines, e.g. if you want select_console() without
+addition console setup. Then, at some point, you should set it on your own.
 
 Option C<ensure_tty_selected> ensures TTY is selected.
 
@@ -895,6 +895,15 @@ sub activate_console {
         unless ($args{skip_set_standard_prompt}) {
             $self->set_standard_prompt($user, skip_set_standard_prompt => $args{skip_set_standard_prompt});
             assert_screen $console;
+        }
+        unless ($args{skip_disable_key_repeat}) {
+            if (exists &distribution::disable_key_repeat) {
+                $self->disable_key_repeat();
+            }
+            # backward compatibility for os-autoinst below interface version 48
+            else {
+                enter_cmd('kbdrate -s -d99999');
+            }
         }
     }
     elsif ($type =~ /^(virtio-terminal|sut-serial)$/) {

commit 7e28c04028c22665f2701135f456671ad1f56166

fix(susedistribution): Fix kbdrate problem on s390x
Fixes a regression from 0d8189165b observed in s390x svirt tests
yielding the error message "kbdrate: Failed waiting for kbd controller"
by excluding the execution on s390x.

lib/susedistribution.pm
@@ -896,7 +896,9 @@ sub activate_console {
             $self->set_standard_prompt($user, skip_set_standard_prompt => $args{skip_set_standard_prompt});
             assert_screen $console;
         }
-        unless ($args{skip_disable_key_repeat}) {
+        # s390x excluded due to "kbdrate: Failed waiting for kbd controller!"
+        # error
+        unless ($args{skip_disable_key_repeat} || is_s390x) {
             if (exists &distribution::disable_key_repeat) {
                 $self->disable_key_repeat();
             }