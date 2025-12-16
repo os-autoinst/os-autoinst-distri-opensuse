b27adb37a6eb88078c6e9043c39fd4a8301c2a9a
Sebastian Chlad <sebastian.chlad@suse.com>
2025-12-16 11:37:16 +0100

    kernel: To be s

--- lib/Kernel/logging.pm
+++ lib/Kernel/logging.pm
@@ -6,6 +6,7 @@ use Exporter;
 use strict;
 use warnings;
 use testapi;
+use Utils::Logging;
 
 our @EXPORT_OK = qw(
   kernel_info
@@ -19,13 +20,10 @@ sub kernel_info {
 sub prepare_kernel_bug_report {
     my (%args) = @_;
 
-    my $filename = $args{filename} // 'kernel_bug_report.txt';
     my $content = $args{content} // kernel_info();
 
-    save_tmp_file($filename, $content);
-    upload_logs($filename);
-
-    return $filename;
+    # kernel_bug_report.txt hardcoded value as this is expected in the UI (report bug button)
+    save_and_upload_log('kernel_bug_report.txt', $content);
 }
 
 1;
