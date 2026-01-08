4c728c00458d79d69b4e20afdf026254a8163314
Paolo Stivanin <info@paolostivanin.com>
2026-01-08 10:59:18 +0100

    fips/xca: stabilize certificate creation
    * Improve DB creation workflow (explicit enter and slow typing)
    * Add country field step and adapt key generation flow
    * Minor cleanup and consistency fixes

--- tests/fips/xca.pm
+++ tests/fips/xca.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright 2022 SUSE LLC
+# Copyright 2026 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
 # Summary: xca basic tests in FIPS mode
@@ -9,34 +9,35 @@
 
 use base 'opensusebasetest';
 use testapi;
-use utils 'zypper_call';
+use utils qw(zypper_call type_string_slow);
 use x11utils qw(close_gui_terminal);
 
 sub run {
     select_console 'root-console';
-    zypper_call('in xca');
-    select_console('x11');
-    x11_start_program('xterm');
+    zypper_call 'in xca';
+    select_console 'x11';
+    x11_start_program 'xterm';
 
     # Start the xca in gui mode
     script_run('xca', 0);
-    assert_screen('xca_main_page');
+    assert_screen 'xca_main_page';
 
     # Create a new database
     wait_screen_change { send_key 'ctrl-n' };
     save_screenshot;
 
     # Enter database dir
-    wait_screen_change { type_string 'fips_xca' };
-    assert_and_click('xca_database_save');
-    wait_still_screen 2;
-    save_screenshot;
+    type_string_slow 'fips_xca';
+    wait_still_screen 5;
+    send_key 'ret';
+    wait_still_screen 3;
 
     # Enter the password, that will be used to encrypt the private keys
-    type_string("$testapi::password");
+    type_string_slow "$testapi::password";
+    wait_still_screen 3;
     send_key 'tab';
-    type_string("$testapi::password");
-    wait_still_screen 2;
+    type_string_slow "$testapi::password";
+    wait_still_screen 3;
     save_screenshot;
     send_key 'alt-o';
     wait_still_screen 2;
@@ -45,42 +46,42 @@ sub run {
     # Create new certificate
     send_key 'alt-n';
     wait_still_screen 2;
-    save_screenshot;
     send_key 'alt-o';
     wait_still_screen 2;
     save_screenshot;
     send_key 'tab';
-    send_key 'ret';
-    assert_and_click('xca_create_initernal_name');
-
-    # Enter the internal name for the certificate
-    type_string('susetest');
+    wait_screen_change { send_key 'ret' };
+    assert_and_click('xca_create_internal_name');
+    # internal name for the certificate
+    wait_screen_change { type_string 'susetest' };
+    # countryName
+    send_key 'tab';
+    wait_screen_change { type_string 'DE' };
 
     # Generate a new key
     send_key 'alt-g';
-    wait_still_screen 2;
+    wait_still_screen 3;
     save_screenshot;
-    # press enter to generate key.
-    # Key generation can take some time on a busy system
-    send_key 'ret', wait_screen_change => 1;
-    wait_still_screen 10;
-    save_screenshot;
-    send_key 'alt-o', wait_screen_change => 1;
-    assert_and_click('ok_to_create_certificate');
+    # create key
+    send_key 'ret';
+    wait_still_screen 5;
+    # key has been created
+    send_key 'ret';
+    wait_still_screen 5;
+    # ok to create certificate
+    send_key 'alt-o';
+    wait_still_screen 5;
 
-    # The certificate contains no extensions, you may apply the
-    # extensions of one of the templates to define the purpose
-    # of the certificate
     assert_and_click('xca_continue_rollout');
     wait_still_screen 2;
     if (check_screen('xca_fips_error_digital', 10)) {
         record_soft_failure('bsc#1198370: error:060800C8:digital envelope routines:EVP_DigestInit_ex:disabled for FIPS');
+        send_key 'alt-o';
     }
-    send_key 'alt-o';
     assert_screen('certificate_create_complete');
 
     # Clean up
-    send_key 'alt-o';
+    wait_screen_change { send_key 'alt-o' };
     close_gui_terminal;
 }
 
