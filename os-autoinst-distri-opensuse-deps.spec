os-autoinst/os-autoinst-distri-opensuse

commit 2ab27ec47e1be37e64ae1e014e62c96431e64a62

Add missing dependencies for reading info from agama isos
0af21eb68048729c3e11806a2791fd51dba52759 added Yam::Agama::LiveIso which
has some calls to new dependencies, but didn't update the spec file

os-autoinst-distri-opensuse-deps.spec
@@ -81,6 +81,10 @@ Requires:       perl(YAML::PP)
 Recommends:     os-autoinst-devel
 # This package is required for Agama profile generation with JSONNET tool, ticket link: https://progress.opensuse.org/issues/175728
 Requires:       golang-github-google-jsonnet
+# Required to get information from Agama isos
+Requires:       isoinfo
+Requires:       jq
+Requires:       gzip
 
 %description
 Metapackage that contains the dependencies of os-autoinst-distri-opensuse.

commit b99749cf951965f30bc4cb5f14e60976ddd8a253

fix: correct requirement for "isoinfo" application
Regression from 2ab27ec.

Related progress issues:
* https://progress.opensuse.org/issues/188931
* https://progress.opensuse.org/issues/195005

os-autoinst-distri-opensuse-deps.spec
@@ -82,7 +82,7 @@ Recommends:     os-autoinst-devel
 # This package is required for Agama profile generation with JSONNET tool, ticket link: https://progress.opensuse.org/issues/175728
 Requires:       golang-github-google-jsonnet
 # Required to get information from Agama isos
-Requires:       isoinfo
+Requires:       mkisofs
 Requires:       jq
 Requires:       gzip
 