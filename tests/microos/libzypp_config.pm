08884586f80ccffb8eb4424c41d1b6f09f6a3e20
Santiago Zarate <santiago.zarate@suse.com>
2026-01-22 10:57:43 +0100

    libzypp_config: Choose which zypp.conf file to use
    As libzypp moves to having its configuration in /usr/etc it means that
    the file might not exist in /etc anymore under certain circumstances

--- tests/microos/libzypp_config.pm
+++ tests/microos/libzypp_config.pm
@@ -12,17 +12,21 @@ use testapi;
 use version_utils qw(is_community_jeos is_jeos);
 use serial_terminal qw(select_serial_terminal);
 
+my $zypp_conf;
+
 sub run {
     select_serial_terminal();
+    my $ret = script_run('test -f /etc/zypp/zypp.conf');
+    $zypp_conf = !$ret ? '/etc/zypp/zypp.conf' : '/usr/etc/zypp/zypp.conf';
     unless (is_community_jeos()) {
-        assert_script_run 'grep -E -x "^solver.onlyRequires ?= ?true" /etc/zypp/zypp.conf';
-        assert_script_run 'grep -E -x "^rpm.install.excludedocs ?= ?yes" /etc/zypp/zypp.conf';
+        assert_script_run 'grep -E -x "^solver.onlyRequires ?= ?true" ' . $zypp_conf;
+        assert_script_run 'grep -E -x "^rpm.install.excludedocs ?= ?yes" ' . $zypp_conf;
     }
-    assert_script_run sprintf('grep -E -x "^multiversion ?=%s" /etc/zypp/zypp.conf', is_jeos ? ' ?provides:multiversion\(kernel\)' : '');
+    assert_script_run sprintf('grep -E -x "^multiversion ?=%s" ' . $zypp_conf, is_jeos ? ' ?provides:multiversion\(kernel\)' : '');
 }
 
 sub post_fail_hook {
-    upload_logs '/etc/zypp/zypp.conf';
+    upload_logs $zypp_conf;
 }
 
 1;
