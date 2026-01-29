33dd68abfdbf104b8874694cb54bc21faab145de
Emil Miler <em@0x45.cz>
2026-01-29 16:28:57 +0100

    Refactor tpm2_engine_rsa_operation test module

--- tests/security/tpm2/tpm2_engine/tpm2_engine_rsa_operation.pm
+++ tests/security/tpm2/tpm2_engine/tpm2_engine_rsa_operation.pm
@@ -1,4 +1,4 @@
-# Copyright 2020 SUSE LLC
+# Copyright SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
 # Summary: Per TPM2 stack, we would like to add the tpm2-tss-engine,
@@ -6,44 +6,45 @@
 #          We have several test modules, this test module will cover
 #          the RSA operations.
 # Maintainer: QE Security <none@suse.de>
-# Tags: poo#64902, tc#1742298
+# Tags: poo#64902, tc#1742298, poo#195065
 
 use base 'opensusebasetest';
 use testapi;
 use serial_terminal 'select_serial_terminal';
 use version_utils 'is_sle';
 
+sub rsa_decrypt {
+    my $test_dir = "tpm2_engine_rsa_decrypt";
+    my $data_file = "$test_dir/data";
+    my $key_file = "$test_dir/mykey";
+    my $enc_file = "$test_dir/mycipher";
+    my $openssl_opts = is_sle(">=15-SP6") ? "-pkeyopt rsa_padding_mode:oaep" : "";
+    assert_script_run "mkdir -p $test_dir";
+    assert_script_run "echo tpm2test > $data_file";
+    assert_script_run "tpm2tss-genkey -a rsa -s 2048 $key_file";
+    assert_script_run "openssl rsa -engine tpm2tss -inform engine -in $key_file -pubout -outform pem -out $key_file.pub";
+    assert_script_run "openssl pkeyutl $openssl_opts -pubin -inkey $key_file.pub -in $data_file -encrypt -out $enc_file";
+    assert_script_run "openssl pkeyutl $openssl_opts -engine tpm2tss -keyform engine -inkey $key_file -decrypt -in $enc_file -out $data_file.decrypt";
+    assert_script_run "diff $data_file $data_file.decrypt";
+}
+
+sub rsa_sign {
+    my $test_dir = "tpm2_engine_rsa_sign";
+    my $test_file = "$test_dir/data";
+    my $key_file = "$test_dir/mykey";
+    my $sig_file = "$test_dir/mysig";
+    assert_script_run "mkdir -p $test_dir";
+    assert_script_run "echo tpm2test > $test_file";
+    assert_script_run "tpm2tss-genkey -a rsa -s 2048 $key_file";
+    assert_script_run "openssl rsa -engine tpm2tss -inform engine -in $key_file -pubout -outform pem -out $key_file.pub";
+    assert_script_run "openssl pkeyutl -engine tpm2tss -keyform engine -inkey $key_file -sign -in $test_file -out $sig_file";
+    validate_script_output("openssl pkeyutl -pubin -inkey $key_file.pub -verify -in $test_file -sigfile $sig_file", sub { /Signature\s+Verified\s+Successfully/ });
+}
+
 sub run {
     select_serial_terminal;
-
-    # RSA operations
-    # RSA decrypt
-    my $test_enc_dir = "tpm2_engine_rsa_decrypt";
-    my $test_file = "mydata";
-    my $rsa_key = "mykey";
-    my $enc_file = "mycipher";
-    assert_script_run "mkdir $test_enc_dir";
-    assert_script_run "cd $test_enc_dir";
-    assert_script_run "echo tpm2test > $test_file";
-    assert_script_run "tpm2tss-genkey -a rsa -s 2048 $rsa_key";
-    assert_script_run "openssl rsa -engine tpm2tss -inform engine -in $rsa_key -pubout -outform pem -out $rsa_key.pub";
-    my $oaep_option = is_sle('>=15-SP6') ? "-pkeyopt rsa_padding_mode:oaep" : "";
-    assert_script_run "openssl pkeyutl $oaep_option -pubin -inkey $rsa_key.pub -in $test_file -encrypt -out $enc_file";
-    assert_script_run "openssl pkeyutl $oaep_option -engine tpm2tss -keyform engine -inkey $rsa_key -decrypt -in $enc_file -out $test_file.decrypt";
-    assert_script_run "diff $test_file $test_file.decrypt";
-    assert_script_run "cd";
-
-    # RSA sign
-    my $test_sign_dir = "tpm2_engine_rsa_sign";
-    my $sig_file = "mysig";
-    assert_script_run "mkdir $test_sign_dir";
-    assert_script_run "cd $test_sign_dir";
-    assert_script_run "echo tpm2test > $test_file";
-    assert_script_run "tpm2tss-genkey -a rsa -s 2048 $rsa_key";
-    assert_script_run "openssl rsa -engine tpm2tss -inform engine -in $rsa_key -pubout -outform pem -out $rsa_key.pub";
-    assert_script_run "openssl pkeyutl -engine tpm2tss -keyform engine -inkey $rsa_key -sign -in $test_file -out $sig_file";
-    validate_script_output "openssl pkeyutl -pubin -inkey $rsa_key.pub -verify -in $test_file -sigfile $sig_file", sub { m/Signature\sVerified\sSuccessfully/ };
-    assert_script_run "cd";
+    rsa_decrypt;
+    rsa_sign;
 }
 
 1;
