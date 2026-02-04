c6ebb38cc07d696da5157b95c53c6e03ad79397c
Huajian.Luo <hluo@suse.com>
2026-02-04 10:34:18 +0800

    Adapt self-update for QU1
    - Adapt self-update for QU1
    - Related ticket: https://progress.opensuse.org/issues/195818

--- tests/yam/validate/validate_self_update.pm
+++ tests/yam/validate/validate_self_update.pm
@@ -8,14 +8,23 @@
 
 use base "consoletest";
 use testapi;
+use utils qw(systemctl);
+use scheduler qw(get_test_suite_data);
 
 sub run {
     select_console 'root-console';
-    my $retcode = script_output('cat /run/live-self-update/result');
 
-    # Pass validation if retcode is 0 (All OK) or 4 - ZYPPER_EXIT_ERR_ZYPP A problem is reported by ZYPP library
-    if ($retcode != 0 && $retcode != 4) {
-        die "Self update did not ended successfully";
+    my $self_update_enabled = get_test_suite_data()->{self_update_enabled};
+    if ($self_update_enabled) {
+        my $retcode = script_output('cat /run/live-self-update/result');
+
+        # Pass validation if retcode is 0 (All OK) or 4 - ZYPPER_EXIT_ERR_ZYPP A problem is reported by ZYPP library
+        if ($retcode != 0 && $retcode != 4) {
+            die "Self update did not ended successfully";
+        }
+    } else {
+        systemctl('is-active live-self-update', expect_false => 1);
+        assert_script_run("journalctl -t live-self-update | grep \"Self update not configured\"");
     }
 }
 
