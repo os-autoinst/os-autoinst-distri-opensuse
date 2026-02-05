b4b0627d45f049b3d4fc93df40d7e5d3bb75dd98
Santiago Zarate <santiago.zarate@suse.com>
2026-02-05 13:13:35 +0100

    Use recursive search in grep to match multiple config files

--- tests/microos/libzypp_config.pm
+++ tests/microos/libzypp_config.pm
@@ -9,24 +9,29 @@
 
 use base "consoletest";
 use testapi;
-use version_utils qw(is_community_jeos is_jeos);
+use version_utils qw(is_community_jeos is_jeos is_sle_micro is_microos);
 use serial_terminal qw(select_serial_terminal);
+use Utils::Logging 'tar_and_upload_log';
 
-my $zypp_conf;
+my $zypp_conf_dir;
 
 sub run {
     select_serial_terminal();
-    my $ret = script_run('test -f /etc/zypp/zypp.conf');
-    $zypp_conf = !$ret ? '/etc/zypp/zypp.conf' : '/usr/etc/zypp/zypp.conf';
+    $zypp_conf_dir = (is_sle_micro || is_jeos) ? '/etc/zypp/' : '/usr/etc/zypp/';
     unless (is_community_jeos()) {
-        assert_script_run 'grep -E -x "^solver.onlyRequires ?= ?true" ' . $zypp_conf;
-        assert_script_run 'grep -E -x "^rpm.install.excludedocs ?= ?yes" ' . $zypp_conf;
+        assert_script_run 'grep -E -R -x "^solver.onlyRequires ?= ?true" ' . $zypp_conf_dir;
+        assert_script_run 'grep -E -R -x "^rpm.install.excludedocs ?= ?yes" ' . $zypp_conf_dir;
     }
-    assert_script_run sprintf('grep -E -x "^multiversion ?=%s" ' . $zypp_conf, is_jeos ? ' ?provides:multiversion\(kernel\)' : '');
+    assert_script_run sprintf('grep -E -R -x "^multiversion ?=%s" ' . $zypp_conf_dir, is_jeos ? ' ?provides:multiversion\(kernel\)' : '');
 }
 
 sub post_fail_hook {
-    upload_logs $zypp_conf;
+    my @dirs = ('/usr/etc/zypp', '/etc/zypp');
+    my @backup_dirs;
+    foreach my $dir (@dirs) {
+        push @backup_dirs, $dir if !script_run("test -d $dir");
+    }
+    tar_and_upload_log("@backup_dirs", "/tmp/zypp_conf_dir.tar.bz2", gzip => 1);
 }
 
 1;
