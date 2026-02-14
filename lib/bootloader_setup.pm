55322cd44c251458c1503aef356acdf933c154aa
Nan Zhang <nan.zhang@suse.com>
2026-02-14 22:50:47 +0800

    Fix grub gfxpayload change failure

--- lib/bootloader_setup.pm
+++ lib/bootloader_setup.pm
@@ -556,7 +556,7 @@ sub uefi_bootmenu_params {
         # sle15sp4+, leap15.4+ and TW (grub 2.06)
         $linux += 4 if is_sle('>12-SP5') && is_sle('<15-SP4');
         if (get_var('FLAVOR', '') =~ /encrypt/i) {
-            $linux += is_sle_micro('6.1+') ? 11 : 10;
+            $linux += is_sle_micro('6.1+') || is_sle('16.1+') ? 11 : 10;
         }
     }
     else {
