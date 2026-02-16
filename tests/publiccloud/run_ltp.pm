9e1bba89bd7f35a4fc0f1d01b84ea7d99e363d64
Volodymyr Katkalov <volodymyr.katkalov@suse.com>
2026-02-16 17:41:37 +0200

    PublicCloud Re-implement systemd_detect_virt check on GCE

--- tests/publiccloud/run_ltp.pm
+++ tests/publiccloud/run_ltp.pm
@@ -408,6 +408,93 @@ sub install_ec2_cloudwatch_agent
     );
 }
 
+sub systemd_detect_virt_expected_output_virtual {
+    my ($self) = @_;
+
+    return "google" if is_gce;
+    return "amazon" if is_ec2;
+    return "microsoft" if is_azure;
+
+    die "Unknown provider for systemd-detect-virt expected output";
+}
+
+sub assert_systemd_detect_virt_metal {
+    my ($self, $rc, $output) = @_;
+
+    my $not_expected_output = $self->systemd_detect_virt_expected_output_virtual();
+    my $bsc;
+
+    if (is_gce) {
+        $bsc = "bsc#1244449 - known issue on GCE SLE <=15-SP6"
+          if is_sle("<=15-SP6");
+
+        $bsc = "bsc#1244449 - known issue on GCE SLE >=15-SP7 with systemd without backported fix"
+          if is_sle(">=15-SP7");
+    }
+
+    my $is_softfail = defined $bsc;
+
+    if ($output eq $not_expected_output) {
+        die "systemd-detect-virt returned virtualized environment output: $output"
+          unless $is_softfail;
+
+        record_soft_failure("systemd-detect-virt output '$output' on metal ($bsc)");
+    }
+
+    if ($rc != 1) {
+        die "systemd-detect-virt unexpected rc: $rc (output: $output)"
+          unless $is_softfail;
+
+        record_soft_failure("systemd-detect-virt rc '$rc' on metal ($bsc)");
+    }
+}
+
+sub assert_systemd_detect_virt_virtual {
+    my ($self, $rc, $output) = @_;
+
+    if ($output eq "none"
+        && is_gce
+        && is_aarch64
+        && (is_sle_micro("=6.0") || is_sle_micro("=6.1")))
+    {
+        record_soft_failure("bsc#1256376 - systemd-detect-virt none on GCE SLE Micro 6.0 aarch64")
+          if is_sle_micro("=6.0");
+
+        record_soft_failure("bsc#1256377 - systemd-detect-virt none on GCE SLE Micro 6.1 aarch64")
+          if is_sle_micro("=6.1");
+
+        return;
+    }
+
+    die "systemd-detect-virt unexpected rc: $rc"
+      unless $rc == 0;
+
+    die "systemd-detect-virt unexpected output: $output"
+      unless $output eq $self->systemd_detect_virt_expected_output_virtual();
+}
+
+sub assert_systemd_detect_virt {
+    my ($self, $instance) = @_;
+
+    my $rc = int($instance->ssh_script_run(cmd => 'systemd-detect-virt'));
+    my $output = $instance->ssh_script_output(
+        cmd => 'systemd-detect-virt',
+        proceed_on_failure => 1
+    );
+
+    record_info('systemd-detect-virt', "rc: $rc; output: $output");
+
+    my $systemd_version = $instance->ssh_script_output('rpm -q systemd');
+
+    record_info('systemd version', $systemd_version);
+
+    if (get_var('PUBLIC_CLOUD_INSTANCE_TYPE') =~ /-metal$/) {
+        $self->assert_systemd_detect_virt_metal($rc, $output);
+    } else {
+        $self->assert_systemd_detect_virt_virtual($rc, $output);
+    }
+}
+
 sub run {
     my ($self, $args) = @_;
     my $qam = get_var('PUBLIC_CLOUD_QAM', 0);
@@ -540,19 +627,7 @@ sub prepare_kirk {
     script_retry("git clone -q --single-branch -b $kirk_branch --depth 1 $kirk_repo", retry => 5, delay => 60, timeout => 300);
     $instance->ssh_assert_script_run(cmd => 'sudo CREATE_ENTRIES=1 ' . get_ltproot() . '/IDcheck.sh', timeout => 300);
     record_info('Kernel info', $instance->ssh_script_output(cmd => q(rpm -qa 'kernel*' --qf '%{NAME}\n' | sort | uniq | xargs rpm -qi)));
-    if (get_var('PUBLIC_CLOUD_INSTANCE_TYPE') =~ /-metal$/) {
-        record_info('VM type', $instance->ssh_script_output(cmd => '! systemd-detect-virt')) unless is_gce;
-    } else {
-        my $output = $instance->ssh_script_output(cmd => 'systemd-detect-virt', proceed_on_failure => 1);
-        record_info('VM type', $output);
-
-        if (($output eq "none") && is_gce && is_aarch64 && (is_sle_micro("=6.0") || is_sle_micro("=6.1"))) {
-            record_soft_failure("bsc#1256376 - systemd-detect-virt none on GCE SLE Micro 6.0 aarch64") if is_sle_micro('=6.0');
-            record_soft_failure("bsc#1256377 - systemd-detect-virt none on GCE SLE Micro 6.1 aarch64") if is_sle_micro('=6.1');
-        } else {
-            $instance->ssh_assert_script_run('systemd-detect-virt');
-        }
-    }
+    $self->assert_systemd_detect_virt($instance);
     assert_script_run("cd kirk");
     my $ghash = script_output("git rev-parse HEAD", proceed_on_failure => 1);
     set_var("LTP_RUN_NG_GIT_HASH", $ghash);
