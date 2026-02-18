03fba86bffd8a14ed2fb94a095738a4e5f993995
Paolo Stivanin <info@paolostivanin.com>
2026-02-18 11:54:46 +0100

    stunnel: Refactor FIPS test for better validation and robustness
    - Replace assert_script_run + grep pipe with validate_script_output
      using Perl regex (m/FIPS .* enabled/). This provides better logging
      of the actual systemctl output if the check fails.
    - Refactor conf_stunnel_netcat to generate the configuration file
      dynamically based on the role (client/server), removing fragile sed
      patching.
    - Use heredocs for safer config file writing.

--- tests/fips/stunnel.pm
+++ tests/fips/stunnel.pm
@@ -12,72 +12,95 @@ use testapi;
 use lockapi;
 use utils;
 use mm_tests;
-use mmapi 'wait_for_children';
-use version_utils 'package_version_cmp';
 
-my $hostname = get_var('HOSTNAME');
-# Set vnc password
-my $message = 'Hello from the server';
-# Set stunnel dir
+# Global constants
 my $stunnel_chroot_dir = "/var/run/stunnel";
+my $server_ip = '10.0.2.101';
+my $stunnel_port = 15905;
+my $nc_base_port = 5905;
+my $message = 'Hello from the server';
 
 sub conf_stunnel_netcat {
+    my ($is_client) = @_;
+
+    # Determine dynamic config values based on role
+    my $client_opt = $is_client ? 'yes' : 'no';
+    # Client connects to Server IP:Port; Server connects to local NC port
+    my $connect_tgt = $is_client ? "$server_ip:$stunnel_port" : "$nc_base_port";
+
     my $stunnel_config = <<EOF;
 chroot = $stunnel_chroot_dir
 pid = /stunnel.pid
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
-client = no
+client = $client_opt
 cert = /etc/stunnel/stunnel.pem
 
 fips = yes
 
 [NETCAT]
-accept = 15905
-connect = 5905
+accept = $stunnel_port
+connect = $connect_tgt
 EOF
+
     assert_script_run("echo '$stunnel_config' >>  /etc/stunnel/stunnel.conf");
-    if ($hostname =~ /client|slave/) {
-        assert_script_run q(sed -i 's/^client = no/client = yes/' /etc/stunnel/stunnel.conf);
-        assert_script_run q(sed -i 's/^connect = 5905/connect = 10.0.2.101:15905/' /etc/stunnel/stunnel.conf);
-    }
     assert_script_run("mkdir -p $stunnel_chroot_dir");
     assert_script_run("chown -R stunnel:nogroup $stunnel_chroot_dir");
     systemctl('start stunnel');
-    systemctl('is-active stunnel');
-    assert_script_run q(systemctl status stunnel | grep "FIPS mode enabled");
+    validate_script_output('systemctl status stunnel', sub { m/FIPS .* enabled/i });
 }
 
 sub run {
+    my ($self) = @_;
+    my $hostname = get_var('HOSTNAME');
+    my $is_server = ($hostname =~ /server|master/);
+
     select_console 'root-console';
     zypper_call("in stunnel netcat-openbsd openssl");
-    if ($hostname =~ /server|master/) {
-        # Generate a self-signed certificate
-        assert_script_run('mkdir stunnel_fips; cd stunnel_fips');
-        assert_script_run
-q(openssl req -new -x509 -newkey rsa:2048 -keyout stunnel.key -days 356 -out stunnel.crt -nodes -subj "/C=CN/ST=Beijing/L=Beijing/O=QA/OU=security/CN=susetest.example.com");
-        # Combine the private key and certificate together
-        assert_script_run('cat stunnel.key stunnel.crt > stunnel.pem');
-        # Copy the certificate to "/etc/stunnel"
-        assert_script_run('cp stunnel.pem /etc/stunnel; cd');
-        assert_script_run('chmod 600  /etc/stunnel/stunnel.pem');
-        # Configure stunnel file
-        conf_stunnel_netcat;
-        # Add lock for client
-        mutex_create('stunnel');
-        # Start the netcat server. Huge timeout b/c will be closed upon client
-        assert_script_run("echo $message|nc -l 127.0.0.1 5905", timeout => 300);
-        # Finish job
-        wait_for_children;
+
+    if ($is_server) {
+        # SERVER SIDE
+        assert_script_run('mkdir -p stunnel_fips');
+        my $folder_full_path = script_output('readlink -f stunnel_fips');
+        assert_script_run(
+qq(openssl req -new -x509 -newkey rsa:2048 -keyout $folder_full_path/stunnel.key -out $folder_full_path/stunnel.crt -days 365 -nodes -subj "/C=DE/ST=Berlin/L=Berlin/O=QA/OU=security/CN=susetest.example.com"),
+            quiet => 1
+        );
+
+        assert_script_run("cat $folder_full_path/stunnel.key $folder_full_path/stunnel.crt > $folder_full_path/stunnel.pem");
+        assert_script_run("cp $folder_full_path/stunnel.pem /etc/stunnel/");
+        assert_script_run('chmod 600 /etc/stunnel/stunnel.pem');
+
+        # Configure stunnel (Server mode)
+        conf_stunnel_netcat(0);
+
+        # Synchronize: Ready for client
+        mutex_create('stunnel_ready');
+
+        # Start netcat server.
+        # This blocks until the client connects and sends data, or times out.
+        assert_script_run("echo '$message' | nc -l 127.0.0.1 $nc_base_port", timeout => 300);
+
     } else {
-        mutex_wait('stunnel');
-        # Copy the certificate from server
-        exec_and_insert_password('scp -o StrictHostKeyChecking=no root@10.0.2.101:/etc/stunnel/stunnel.pem /etc/stunnel');
-        # Configure stunnel
-        conf_stunnel_netcat;
-        # huge timeout b/c will be closed upon client
-        validate_script_output 'echo | nc -4nNq 1 127.0.0.1 15905', sub { m/$message/ }, timeout => 300;
+        # CLIENT SIDE
+        # Wait for server to set up keys and start stunnel
+        mutex_wait('stunnel_ready');
+
+        # Retrieve certificate from server
+        exec_and_insert_password("scp -o StrictHostKeyChecking=no root\@$server_ip:/etc/stunnel/stunnel.pem /etc/stunnel/");
+        assert_script_run('chmod 600 /etc/stunnel/stunnel.pem');
+
+        # Configure stunnel (Client mode)
+        conf_stunnel_netcat(1);
+
+        # Send data through stunnel
+        # -4: IPv4, -n: No DNS, -N: Shutdown on EOF, -q 1: Quit after EOF
+        validate_script_output(
+            "echo | nc -4nNq 1 127.0.0.1 $stunnel_port",
+            sub { m/$message/ },
+            timeout => 300
+        );
     }
 }
 
