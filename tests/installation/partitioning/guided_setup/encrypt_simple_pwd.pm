92719ddee1a4bee0f5cdfd7e07ff410b30893810
Santiago Zarate <santiago.zarate@suse.com>
2026-02-23 16:43:58 +0100

    Support selecting type of authentication to unlock disks

--- tests/installation/partitioning/guided_setup/encrypt_simple_pwd.pm
+++ tests/installation/partitioning/guided_setup/encrypt_simple_pwd.pm
@@ -6,10 +6,13 @@
 # Maintainer: QE Installation and Migration (QE Iam) <none@suse.de>
 
 use parent 'y2_installbase';
+use testapi;
 
 sub run {
     my $partitioning_scheme = $testapi::distri->get_partitioning_scheme();
     $partitioning_scheme->configure_encryption($testapi::password);
+    my $fde_enrollment = get_var('FDE_ENROLLMENT');
+    $partitioning_scheme->set_fde_enrollment_authentication($fde_enrollment) if $fde_enrollment;
     $partitioning_scheme->go_forward();
     $partitioning_scheme->get_weak_password_warning->press_yes();
 }
