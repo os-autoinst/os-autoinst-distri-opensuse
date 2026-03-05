2b2c3592db1de21abc05dc15213435f7e7ed74c8
phoenix <felix.niederwanger@suse.com>
2026-03-05 11:52:59 +0100

    Show repos at end
    Show a listing of all active repositories at the end of the test module
    because the assert_script_run output is cropped in the WebUI.

--- tests/publiccloud/transfer_repos.pm
+++ tests/publiccloud/transfer_repos.pm
@@ -86,7 +86,7 @@ sub run {
         $instance->ssh_assert_script_run("sudo find $repodir -name *.repo -exec echo '{}' \\;");
     }
 
-    $instance->ssh_assert_script_run("zypper lr -P");
+    record_info("zypper repos", $instance->ssh_script_output("zypper lr -P"));
 }
 
 sub test_flags {
