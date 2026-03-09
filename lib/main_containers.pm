18952ac369c8326673de3bb6a3adb9c8ce6189f4
phoenix <felix.niederwanger@suse.com>
2026-03-09 09:39:33 +0100

    Update format and comments on patch_and_reboot
    Adds a note that qa_automation/patch_and_reboot is only needed for SLFO
    and switch from a if block to a single if conditional, as it is only a
    single command.

--- lib/main_containers.pm
+++ lib/main_containers.pm
@@ -330,10 +330,10 @@ sub load_container_tests {
         loadtest 'boot/boot_to_desktop' unless is_public_cloud;
     }
 
-    # Note: Publiccloud uses publiccloud/patch_and_reboot
-    if (is_sle('16.0+') && is_updates_tests && !is_public_cloud) {
-        loadtest 'qa_automation/patch_and_reboot';
-    }
+
+    # qa_automation/patch_and_reboot is only needed for the SLFO workflow on SLES16.
+    # Publiccloud uses publiccloud/patch_and_reboot which is in conflict with qa_automation/patch_and_reboot.
+    loadtest 'qa_automation/patch_and_reboot' if (is_sle('16.0+') && is_updates_tests && !is_public_cloud);
 
     if (my $container_tests = get_var('CONTAINER_TESTS', '')) {
         loadtest "containers/$_" foreach (split(',\s*', $container_tests));
