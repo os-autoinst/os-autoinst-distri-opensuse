e1d0a0d6c8d5fe6e8b2112b1f8ebcd78a6747cfc
jknphy <jeriveramoya@suse.com>
2026-03-24 12:24:12 +0100

    Workaround to avoid network failure in patch_agama_tests

--- tests/yam/agama/patch_agama_tests.pm
+++ tests/yam/agama/patch_agama_tests.pm
@@ -6,11 +6,16 @@
 # Maintainer: QE Installation and Migration (QE Iam) <none@suse.de>
 
 use base Yam::Agama::patch_agama_base;
-use testapi qw(assert_script_run get_required_var select_console);
+use testapi qw(assert_script_run get_required_var select_console script_output);
 
 sub run {
     select_console 'install-shell';
     my ($repo, $branch) = split /#/, get_required_var('YUPDATE_GIT');
+
+    # Network workaround ensuring stability by reducing MTU
+    my $iface = script_output('ip -j r s | jq -r ".[] | select(.dst == \"default\") | .dev"');
+    assert_script_run("ip link set dev $iface mtu 1000");
+
     assert_script_run("AGAMA_TEST=" . get_required_var('AGAMA_TEST') . " yupdate patch $repo $branch", timeout => 60);
 }
 
