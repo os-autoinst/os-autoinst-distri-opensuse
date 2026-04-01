745db0854799840dc630c9daa1f60b6f196eaa8a
Ricardo Branco <rbranco@suse.de>
2026-04-02 00:22:56 +0200

    python_docker: Backport PR#3394 to fix test issue with Docker v29.3

--- tests/containers/python_docker.pm
+++ tests/containers/python_docker.pm
@@ -82,8 +82,6 @@ sub test ($target) {
         "tests.integration.api_container_test.AttachContainerTest::test_attach_no_stream",
         # This test with websockets is broken
         "tests.integration.api_container_test.AttachContainerTest::test_run_container_reading_socket_ws",
-        # https://github.com/docker/docker-py/issues/3389
-        "tests.integration.api_network_test.TestNetworks::test_connect_with_mac_address",
     );
     push @xfails, (
         "tests.unit.api_build_test.BuildTest::test_set_auth_headers_with_dict_and_no_auth_configs",
