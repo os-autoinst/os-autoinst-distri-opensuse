2ecc61f406a1d9862924d7d6a8b663b9550f4170
Pavel Dostál <pdostal@suse.cz>
2026-04-13 12:30:09 +0200

    PC: Run wait_for_ssh separately
    In `prepare_instance.pm`, don't run wait_for_ssh()
    inside create_instance() because if wait_for_ssh() fails in late
    stage we still need the $instance object for the
    upload_final_logs() which we call in destroy.pm

--- tests/publiccloud/prepare_instance.pm
+++ tests/publiccloud/prepare_instance.pm
@@ -35,10 +35,11 @@ sub run {
 
     # Create public cloud instance
     my %instance_args;
-    $instance_args{check_connectivity} = 1;
+    $instance_args{check_connectivity} = 0;    # Don't run wait_for_ssh() inside create_instance()
     $instance_args{use_extra_disk} = {size => $additional_disk_size, type => $additional_disk_type} if ($additional_disk_size > 0);
     $args->{my_provider} = $self->provider_factory();
     $args->{my_instance} = $args->{my_provider}->create_instance(%instance_args);
+    $args->{my_instance}->wait_for_ssh(scan_ssh_host_key => 1);
     $args->{my_instance}->wait_for_guestregister() if (is_ondemand);
     my $provider = $self->{my_provider} = $args->{my_provider};
     my $instance = $args->{my_instance};
