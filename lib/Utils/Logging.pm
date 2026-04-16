d325e3b49f609d383fe6c5748d65439829727430
Ricardo Branco <rbranco@suse.de>
2026-04-16 10:54:23 +0200

    cleanup_known_coredumps: Add entry for bsc#1261625

--- lib/Utils/Logging.pm
+++ lib/Utils/Logging.pm
@@ -175,6 +175,7 @@ sub cleanup_known_coredumps {
         'poo#198596' => q(openssl3-conf\/base_only.cnf -p \$'hello'),
         'bsc#1129403' => q(unzip-mem  v files.zip),
         'bsc#1261358' => q(gvfs-udisks2-volume-monitor),
+        'bsc#1261625' => q(ovs-vswitchd unix:),
     );
 
     for my $pid (split(/\n/, script_output(q(coredumpctl -q --no-pager --no-legend | awk '$9 == "present" { print $5 }'), proceed_on_failure => 1))) {
