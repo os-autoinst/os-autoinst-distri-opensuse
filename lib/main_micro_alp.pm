os-autoinst/os-autoinst-distri-opensuse

commit 4da77c9e9c44cccd7838d5f7de2aad906ce39190

De-schedule coredump_collect from SLEM as it lacks coredumpctl
Related ticket: https://progress.opensuse.org/issues/197969

lib/main_micro_alp.pm
@@ -316,7 +316,6 @@ sub load_rcshell_tests {
 sub load_journal_check_tests {
     # Enclosing test cases
     loadtest 'console/journal_check';
-    loadtest 'console/coredump_collect';
     loadtest 'shutdown/shutdown';
 }
 

commit 08b87eb1c2c5daf36195e9ab692a9af92a7a35e8

Re-enable coredump_collect on SLEM 6.x & MicroOS
On os-autoinst/os-autoinst-distri-opensuse#25108 we disabled this module
without taking into account that coredumpctl is not available only on
SLEM 5.x.  It's available on both SLEM 6.x and MicroOS

Fixes: 4da77c9e9c44cccd7838d5f7de2aad906ce39190

lib/main_micro_alp.pm
@@ -316,6 +316,8 @@ sub load_rcshell_tests {
 sub load_journal_check_tests {
     # Enclosing test cases
     loadtest 'console/journal_check';
+    # systemd-coredump is not available on SLEM 5.x
+    loadtest 'console/coredump_collect' unless (is_sle_micro("<6.0"));
     loadtest 'shutdown/shutdown';
 }
 