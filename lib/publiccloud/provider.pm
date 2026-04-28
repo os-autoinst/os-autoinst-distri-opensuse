96838cdcf2117ba01d6a5c83b969736874a7ee1d
Pavel Dostál <pdostal@suse.cz>
2026-04-28 17:22:38 +0200

    provider: increase terraform destroy retry count and delay

--- lib/publiccloud/provider.pm
+++ lib/publiccloud/provider.pm
@@ -686,7 +686,7 @@ sub terraform_destroy {
     my $cmd = terraform_cmd($runner . ' destroy -no-color -auto-approve -lock=false', %vars);
     # Retry 3 times with considerable delay. This has been introduced due to poo#95932 (RetryableError)
     # terraform keeps track of the allocated and destroyed resources, so its safe to run this multiple times.
-    my $ret = script_retry($cmd, retry => 3, delay => 60, timeout => get_var('TERRAFORM_TIMEOUT', TERRAFORM_TIMEOUT), die => 0);
+    my $ret = script_retry($cmd, retry => 9, delay => 180, timeout => get_var('TERRAFORM_TIMEOUT', TERRAFORM_TIMEOUT), die => 0);
     unless (defined $ret) {
         if (is_serial_terminal()) {
             type_string(qq(\c\\));    # Send QUIT signal
