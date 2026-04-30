9ac908dde5486de25a7ee689db177f5d984ee397
Ricardo B. Marlière <rbm@opensuse.org>
2026-04-30 06:15:26 -0300

    Kselftests: Install devel_kernel before using $build_dir
    Since build() defaults to O=/lib/modules/$(uname -r)/build, we need
    kernel-devel package installed. Some jobs depend on create_hdd_textmode
    instead of install_ltp, which does not come with it installed. This
    surfaced an issue in which we try to use $build_dir (`test -w
    $real_build_dir`) before having it.

--- lib/Kselftests/utils.pm
+++ lib/Kselftests/utils.pm
@@ -39,6 +39,14 @@ sub build
     $source_dir //= "/lib/modules/$version/source";
     my $build_dir = "/lib/modules/$version/build";
 
+    # Lock kernel-source/syms to their already-installed versions if present (e.g. pre-installed
+    # by install_kotd at the same version as the running kernel), so the devel_kernel pattern
+    # cannot upgrade them to a mismatched version pulled from a rolling repo like KOTD.
+    my $lock_kernel_pkgs = !script_run('rpm -q kernel-source kernel-syms');
+    zypper_call('al kernel-source kernel-syms') if $lock_kernel_pkgs;
+    install_package('-t pattern --recommends devel_kernel', trup_apply => 1);
+    zypper_call('rl kernel-source kernel-syms') if $lock_kernel_pkgs;
+
     # On immutable systems the build dir is read-only; mount a writable overlayfs on it.
     my $real_build_dir = script_output("readlink -f $build_dir");
     if (script_run("test -w $real_build_dir") != 0) {
@@ -47,14 +55,6 @@ sub build
         assert_script_run("mkdir -p $overlay/{upper,work}");
         assert_script_run("mount -t overlay overlay -o lowerdir=$real_build_dir,upperdir=$overlay/upper,workdir=$overlay/work $real_build_dir");
     }
-
-    # Lock kernel-source/syms to their already-installed versions if present (e.g. pre-installed
-    # by install_kotd at the same version as the running kernel), so the devel_kernel pattern
-    # cannot upgrade them to a mismatched version pulled from a rolling repo like KOTD.
-    my $lock_kernel_pkgs = !script_run('rpm -q kernel-source kernel-syms');
-    zypper_call('al kernel-source kernel-syms') if $lock_kernel_pkgs;
-    install_package('-t pattern --recommends devel_kernel', trup_apply => 1);
-    zypper_call('rl kernel-source kernel-syms') if $lock_kernel_pkgs;
 
     my $jobs = get_var('KSELFTEST_BUILD_JOBS', '$(getconf _NPROCESSORS_ONLN)');
     my $build_env = get_var('KSELFTEST_BUILD_ENV', '');
@@ -134,7 +134,7 @@ sub install_upstream_harness
 sub install_from_repo
 {
     zypper_ar(get_required_var('KSELFTEST_REPO'), name => 'kselftests', priority => 1, no_gpg_check => 1);
-    install_package('kselftests kernel-devel', trup_apply => 1);
+    install_package('kselftests', trup_apply => 1);
 
     # When using the `kselftests` package from a repository, make sure the KMP subpackage containing the test kernel modules
     # were built against the same kernel version the SUT is currently running.
