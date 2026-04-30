7c56ab35d9bafe308edb6ac1a804ce91c7927343
Ricardo Branco <rbranco@suse.de>
2026-04-30 09:35:16 +0200

    install_k3s: Remove dead code for 15-SP3 EOL

--- lib/containers/k8s.pm
+++ lib/containers/k8s.pm
@@ -118,14 +118,12 @@ sub install_k3s {
         assert_script_run("curl $curl_opts https://get.k3s.io -o install_k3s.sh");
         assert_script_run("sh install_k3s.sh $disables", timeout => 300);
         script_run("rm -f install_k3s.sh");
-        zypper_call('in apparmor-parser') if is_sle('<15-SP4', get_var('HOST_VERSION', get_required_var('VERSION')));
         setup_and_check_k3s;
         return;
     }
 
     # k3s-install script is already packaged for several products
     my @pkgs = qw(k3s-install);
-    push @pkgs, 'apparmor-parser' if is_sle('<15-SP4');
 
     if (script_run(sprintf('rpm -q %s', join(" ", @pkgs))) != 0) {
         if (is_transactional) {
