87e2b2a269da7e9d56963b0aa1e7d2f2daff3e2b
Ricardo Branco <rbranco@suse.de>
2026-05-18 21:33:39 +0200

    coredump_collect: Use while loop to wait for pgrep

--- tests/console/coredump_collect.pm
+++ tests/console/coredump_collect.pm
@@ -12,7 +12,6 @@ use testapi;
 use serial_terminal 'select_serial_terminal';
 use Utils::Logging qw(upload_coredumps cleanup_known_coredumps);
 use version_utils qw(is_tumbleweed);
-use utils qw(script_retry);
 
 sub run {
     my $self = shift;
@@ -23,7 +22,7 @@ sub run {
     #   Timestamp: Sun 2026-05-17 23:34:06 CEST (684ms ago)
     # Note: We can't use systemd-coredump in pgrep because we'd get:
     #   pgrep: pattern that searches for process name longer than 15 characters will result in zero matches
-    script_retry("pgrep systemd-cored >/dev/null", retry => 5, delay => 60);
+    script_run "while pgrep systemd-cored >/dev/null ; do sleep 1 ; done";
     cleanup_known_coredumps unless is_tumbleweed;
     upload_coredumps;
 }
