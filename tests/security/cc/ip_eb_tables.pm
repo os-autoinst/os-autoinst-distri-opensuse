1f35359059ad2466270eced62972f1fe14559c7a
Emil Miler <em@0x45.cz>
2026-05-29 14:36:03 +0200

    Refactor ip_eb_tables.pm test module

--- tests/security/cc/ip_eb_tables.pm
+++ tests/security/cc/ip_eb_tables.pm
@@ -1,6 +1,6 @@
 # SUSE's openQA tests
 #
-# Copyright 2021 SUSE LLC
+# Copyright SUSE LLC
 # SPDX-License-Identifier: FSFAP
 #
 # Summary: Run 'ip+eb-tables' test case of 'audit-test' test suite
@@ -10,9 +10,45 @@
 use Mojo::Base 'consoletest';
 use testapi;
 use utils;
-use Utils::Architectures qw(is_s390x);
-use version_utils qw(is_sle);
+use Utils::Architectures 'is_s390x';
+use version_utils 'is_sle';
 use audit_test qw(run_testcase compare_run_log);
+use network_utils 'iface';
+
+sub setup_bridge {
+    my $eth = iface();
+    my $f_ifcfg_br0 = '/etc/sysconfig/network/ifcfg-br0';
+    my $f_ifcfg_eth = "/etc/sysconfig/network/ifcfg-$eth";
+    my $bakf_ifcfg_eth = "/etc/sysconfig/network/ifcfg-$eth.bak";
+    my $br0_config = "BOOTPROTO='dhcp'\nSTARTMODE='auto'\nBRIDGE='yes'\nBRIDGE_PORTS='$eth'\nBRIDGE_STP='off'\nBRIDGE_FORWARDDELAY='15'\n";
+    my $eth_config = "IPADDR='0.0.0.0'\nBOOTPROTO='none'\nSTARTMODE='auto'\n";
+
+    # Configure bridge for ip_eb_tables workaround
+    assert_script_run("cat > $f_ifcfg_br0 <<'END'\n$br0_config\nEND\n( exit \$?)");
+
+    # Creating backup for network interface configuration
+    assert_script_run("cp $f_ifcfg_eth $bakf_ifcfg_eth");
+
+    # Configure network interface for ip_eb_tables workaround
+    assert_script_run("cat > $f_ifcfg_eth <<'END'\n$eth_config\nEND\n( exit \$?)");
+
+    assert_script_run("service network restart");
+    assert_script_run("bridge link show");
+}
+
+sub cleanup_bridge {
+    my $eth = iface();
+    my $f_ifcfg_br0 = '/etc/sysconfig/network/ifcfg-br0';
+    my $f_ifcfg_eth = "/etc/sysconfig/network/ifcfg-$eth";
+    my $bakf_ifcfg_eth = "/etc/sysconfig/network/ifcfg-$eth.bak";
+
+    # Clean-up and restore configuration
+    assert_script_run("rm -f $f_ifcfg_br0");
+    assert_script_run("rm -f $f_ifcfg_eth");
+    assert_script_run("cp $bakf_ifcfg_eth $f_ifcfg_eth");
+    assert_script_run("rm -f $bakf_ifcfg_eth");
+    assert_script_run("service network restart");
+}
 
 sub run {
     my ($self) = shift;
@@ -22,44 +58,27 @@ sub run {
         return;
     }
 
-    my $f_ifcfg_br0 = '/etc/sysconfig/network/ifcfg-br0';
-    my $f_ifcfg_eth0 = '/etc/sysconfig/network/ifcfg-eth0';
-    my $bakf_ifcfg_eth0 = '/etc/sysconfig/network/ifcfg-eth0.bak';
-    my $br0_config = "BOOTPROTO='dhcp'\nSTARTMODE='auto'\nBRIDGE='yes'\nBRIDGE_PORTS='eth0'\nBRIDGE_STP='off'\nBRIDGE_FORWARDDELAY='15'\n";
-    my $eth0_config = "IPADDR='0.0.0.0'\nBOOTPROTO='none'\nSTARTMODE='auto'\n";
-
     select_console 'root-console';
 
     # Audit ebtables tests need bridge network. When system role is CC, the bridge is set up
     # automatically in x86 and arm, so we only need to configure the bridge network for s390x
-    if (is_s390x) {
-        # Configure bridge for ip_eb_tables workaround
-        assert_script_run("cat > $f_ifcfg_br0 <<'END'\n$br0_config\nEND\n( exit \$?)");
-
-        # Creating backup for eth0 configuration
-        assert_script_run("cp $f_ifcfg_eth0 $bakf_ifcfg_eth0");
-
-        # Configure eth0 for ip_eb_tables workaround
-        assert_script_run("cat > $f_ifcfg_eth0 <<'END'\n$eth0_config\nEND\n( exit \$?)");
-
-        assert_script_run("service network restart");
-        assert_script_run("bridge link show");
-    }
+    $self->setup_bridge if is_s390x;
 
     record_info('Bridge network', 'bsc#1190475');
     # Run test case
     run_testcase('ip+eb-tables', timeout => 300);
 
-    if (is_s390x) {
-        # Clean-up and restore configuration
-        assert_script_run("rm $f_ifcfg_br0");
-        assert_script_run("rm $f_ifcfg_eth0");
-        assert_script_run("cp $bakf_ifcfg_eth0 $f_ifcfg_eth0");
-        assert_script_run("service network restart");
-    }
+    $self->cleanup_bridge if is_s390x;
+
     # Compare current test results with baseline
     my $result = compare_run_log('ip+eb-tables');
     $self->result($result);
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+    $self->cleanup_bridge if is_s390x;
+    $self->SUPER::post_fail_hook;
 }
 
 sub test_flags {
