b76a94a56b7d178ca7a4b8a9c45329defe901831
Ricardo B. Marlière <rbm@opensuse.org>
2026-06-02 08:45:47 -0300

    nvidia: validate_cuda: Use gcc13 by default
    In some systems such as SLE-15 the default compiler is gcc 7.5 currently,
    which is too old.

--- lib/nvidia_utils.pm
+++ lib/nvidia_utils.pm
@@ -145,7 +145,7 @@ sub validate
 
 sub validate_cuda
 {
-    zypper_call('install -l cmake git cuda-toolkit vulkan-devel freeglut-devel Mesa-libEGL-devel', timeout => 1200);
+    zypper_call('install -l cmake git gcc13 gcc13-c++ cuda-toolkit vulkan-devel freeglut-devel Mesa-libEGL-devel', timeout => 1200);
 
     # Query the GPU capabilities
     my $query = script_output('nvidia-smi --query-gpu=compute_cap --format=csv');
@@ -155,14 +155,14 @@ sub validate_cuda
 
     # Compiler smoke test with a simple hello_world program
     assert_script_run('curl -s -o hello_world.cu ' . data_url('cuda/hello_world.cu'));
-    assert_script_run("/usr/local/cuda/bin/nvcc -rdc=true -arch=sm_$compute_cap -o hello_world hello_world.cu");
+    assert_script_run("/usr/local/cuda/bin/nvcc -rdc=true -arch=sm_$compute_cap -ccbin g++-13 -std=c++20 -o hello_world hello_world.cu");
     record_info('CUDA Sample', script_output('./hello_world'));
 
     # Build NVIDIA/cuda-samples
     my $cuda_samples_branch = get_var('NVIDIA_CUDA_SAMPLES_BRANCH', 'v13.2update');
     assert_script_run("git clone --depth 1 --single-branch --branch $cuda_samples_branch https://github.com/NVIDIA/cuda-samples.git");
     assert_script_run('mkdir cuda-samples/build; cd $_');
-    assert_script_run("cmake .. -DCMAKE_CUDA_COMPILER_TOOLKIT_ROOT=/usr/local/cuda -DCMAKE_CUDA_ARCHITECTURES=$compute_cap -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc");
+    assert_script_run("cmake .. -DCMAKE_CUDA_COMPILER_TOOLKIT_ROOT=/usr/local/cuda -DCMAKE_CUDA_ARCHITECTURES=$compute_cap -DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc -DCMAKE_C_COMPILER=gcc-13 -DCMAKE_CXX_COMPILER=g++-13 -DCMAKE_CUDA_HOST_COMPILER=g++-13 -DCMAKE_CXX_STANDARD=20 -DCMAKE_CUDA_STANDARD=20");
     assert_script_run('make -j$(nproc)', 3000);
     record_info('CUDA Sample', script_output('./cpp/0_Introduction/clock/clock'));
 }
