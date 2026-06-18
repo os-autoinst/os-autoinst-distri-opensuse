7cc29e3ea8fc842e270d6668ef4093e3c3492f33
Loic Devulder <ldevulder@suse.com>
2026-06-18 11:53:14 +0200

    Elemental3: add tests for LCM and Longhorn
    LCM and Longhorn container images should have a minimal test to be able
    to be released through container-release-bot.
    
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- tests/elemental3/validate_container_image.pm
+++ tests/elemental3/validate_container_image.pm
@@ -9,9 +9,28 @@ use elemental3;
 use testapi;
 use serial_terminal qw(select_serial_terminal);
 
+sub extract_fileslist {
+    my %args = @_;
+
+    assert_script_run("$args{runtime} pull $args{uri}");
+    my $rootfs = script_output(
+        "$args{runtime} inspect -f '{{index .RootFS.Layers 0}}' $args{uri} | cut -d: -f2"
+    );
+
+    # Extract files from container
+    assert_script_run("$args{runtime} save $args{uri} -o $args{name}.tar");
+    assert_script_run("tar xvf $args{name}.tar");
+    my @files = script_output("tar tf $rootfs.tar");
+
+    # Record files list
+    record_info("$args{name} files", @files);
+
+    return (@files);
+}
+
 sub run {
     my $arch = get_required_var('ARCH');
-    my $container_type = get_required_var('TESTED_CONTAINER');
+    my $tested_container = get_required_var('TESTED_CONTAINER');
     my $k8s = get_var('K8S', 'null');
     my $runtime = get_required_var('CONTAINER_RUNTIMES');
     my $totest_path = get_required_var('TOTEST_PATH');
@@ -19,7 +38,8 @@ sub run {
     # No GUI, easier and quicker to use the serial console
     select_serial_terminal();
 
-    if ($container_type =~ /elemental/) {
+    if ($tested_container =~ /elemental/) {
+        # Basic tests of Elemental container image
         my $uri = get_container_uri(
             url => $totest_path,
             arch => $arch,
@@ -32,8 +52,8 @@ sub run {
 
         # Record the command version
         record_info('Elemental Version', script_output($cmd));
-    }
-    elsif ($container_type =~ /$k8s/) {
+    } elsif ($tested_container =~ /$k8s/) {
+        # Basic tests of K8s container image
         my $k8s_version_prefix = get_required_var('K8S_VERSION_PREFIX');
         my $uri = get_container_uri(
             url => $totest_path,
@@ -41,25 +61,37 @@ sub run {
             regex => ".*${k8s}-tar-\(${k8s_version_prefix}.*\)-\(.*\)"
         );
 
-        assert_script_run("$runtime pull $uri");
-        my $rootfs = script_output(
-            "$runtime inspect -f '{{index .RootFS.Layers 0}}' $uri | cut -d: -f2"
+        # Extract files list
+        my @files = extract_fileslist(name => $k8s, runtime => $runtime, uri => $uri);
+
+        die("Missing installer!") unless (join(' ', @files) =~ m/install\.sh/);
+    } elsif ($tested_container =~ /lcm/) {
+        # Basic tests of Lifecycle Manager container image
+        my $uri = get_container_uri(
+            url => $totest_path,
+            arch => $arch,
+            regex => ".*elemental-lifecycle-manager-\(.*\)-\(.*\)"
         );
 
-        # Extract files from container
-        assert_script_run("$runtime save $uri -o $k8s.tar");
-        assert_script_run("tar xvf $k8s.tar");
-        my $files_list = script_output("tar tf $rootfs.tar");
+        # Extract files list
+        my @files = extract_fileslist(name => 'lcm', runtime => $runtime, uri => $uri);
 
-        # Record files list
-        record_info("$k8s files", $files_list);
+        die("Missing raw image!") unless (join(' ', @files) =~ m/elemental-lifecycle-manager/);
+    } elsif ($tested_container =~ /longhorn/) {
+        # Basic tests of Longhorn container image
+        my $uri = get_container_uri(
+            url => $totest_path,
+            arch => $arch,
+            regex => ".*longhorn-\(.*\)-\(.*\)"
+        );
 
-        unless ($files_list =~ m/install.sh/) {
-            die("Missing installer!");
-        }
-    }
-    else {
-        die("Container type '$container_type' not supported!");
+        # Extract files list
+        my @files = extract_fileslist(name => 'longhorn', runtime => $runtime, uri => $uri);
+
+        die("Missing raw image!") unless (join(' ', @files) =~ m/longhorn_.*\.raw/);
+    } else {
+        # Not supported yet!
+        die("Container type '$tested_container' not supported!");
     }
 }
 
