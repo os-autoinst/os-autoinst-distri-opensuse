8a898d4b8b8b032e500671e2b75f1dacf0e9038d
Cédric Vidot <cedric.vidot@suse.com>
2026-07-07 09:50:34 +0200

    aplay: Explicitly define card used

--- tests/console/aplay.pm
+++ tests/console/aplay.pm
@@ -48,8 +48,7 @@ EOS
     select_console 'user-console';
 
     assert_script_run('set_default_volume -f');
-
-    script_run('alsamixer', 0);
+    script_run('alsamixer -c 0', 0);
     assert_screen 'test-aplay-2', 3;
     send_key "esc";
     $self->clear_and_verify_console;
