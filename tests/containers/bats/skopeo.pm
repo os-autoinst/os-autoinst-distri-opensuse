6be14cf05af207912eeb9af53ba0b518a819868b
Ricardo Branco <rbranco@suse.de>
2026-07-07 13:28:11 +0200

    bats/skopeo: Don't fail if we can't edit default.yaml
    The podman v6 suite doesn't come with this file anymore.

--- tests/containers/bats/skopeo.pm
+++ tests/containers/bats/skopeo.pm
@@ -57,7 +57,8 @@ sub run {
     $self->setup_pkgs(@pkgs);
 
     # Prevent https://github.com/containers/skopeo/issues/2718
-    run_command "sed -i '/sigstore-staging:/d' /etc/containers/registries.d/default.yaml";
+    # Note: This file is no longer present on podman v6.0 / skopeo v1.23
+    run_command "sed -i '/sigstore-staging:/d' /etc/containers/registries.d/default.yaml" if (script_run("test -f /etc/containers/registries.d/default.yaml") == 0);
 
     record_info("skopeo version", script_output("skopeo --version"));
     record_info("skopeo package version", script_output("rpm -q skopeo"));
