b35fc06e7576abbdf389e9cadf14c0cb542e4a61
Michele Pagot <michele.pagot@suse.com>
2026-07-21 11:13:41 +0200

    Query the GCP cloud about  availability_zone (#26122)
    terraform_apply can be configured to retry in different regions.
    Each region could have its specific availability_zones set.
    The code to query them was only invoked in case of deployment failure,
    as the first value is usually configurad via variable/setting.
    This does not work when transitioning to a new region.
    Move the reading of the zone list at the beginning of the region loop.

--- lib/publiccloud/provider.pm
+++ lib/publiccloud/provider.pm
@@ -535,6 +535,7 @@ sub terraform_apply {
     $vars{gpu} = 'true' if (get_var('PUBLIC_CLOUD_NVIDIA'));
     $vars{ssh_public_key} = $self->ssh_key . '.pub';
 
+    my @alternative_zones;
     my ($ret, $tf_apply_output);
     for my $region (@regions) {
         # Swap the active region inline so all the region-dependent variables
@@ -543,13 +544,17 @@ sub terraform_apply {
         record_info('REGION', "Attempting the deployment in region '$region'");
 
         if (is_ec2) {
-            $vars{availability_zone} = script_output("aws ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values=" . $instance_type . " --region '" . $self->provider_client->region . "' --query 'InstanceTypeOfferings[0].Location' --output 'text'");
+            $vars{availability_zone} = script_output("aws ec2 describe-instance-type-offerings --location-type availability-zone --filters Name=instance-type,Values=" . $instance_type . " --region '" . $region . "' --query 'InstanceTypeOfferings[0].Location' --output 'text'");
             die('Instance type not supported by the selected Availability Zone') if ($vars{availability_zone} =~ /None/);
-            $vars{vpc_security_group_ids} = script_output("aws ec2 describe-security-groups --region '" . $self->provider_client->region . "' --filters 'Name=group-name,Values=tf-sg' --query 'SecurityGroups[0].GroupId' --output text");
-            $vars{subnet_id} = script_output("aws ec2 describe-subnets --region '" . $self->provider_client->region . "' --filters 'Name=tag:Name,Values=tf-subnet' 'Name=availabilityZone,Values=" . $vars{availability_zone} . "' --query 'Subnets[0].SubnetId' --output text");
+            $vars{vpc_security_group_ids} = script_output("aws ec2 describe-security-groups --region '" . $region . "' --filters 'Name=group-name,Values=tf-sg' --query 'SecurityGroups[0].GroupId' --output text");
+            $vars{subnet_id} = script_output("aws ec2 describe-subnets --region '" . $region . "' --filters 'Name=tag:Name,Values=tf-subnet' 'Name=availabilityZone,Values=" . $vars{availability_zone} . "' --query 'Subnets[0].SubnetId' --output text");
         } elsif (is_azure) {
-            my $subnet_id = script_output("az network vnet subnet list -g 'tf-" . $self->provider_client->region . "-rg' --vnet-name 'tf-network' --query '[0].id' --output 'tsv'");
+            my $subnet_id = script_output("az network vnet subnet list -g 'tf-" . $region . "-rg' --vnet-name 'tf-network' --query '[0].id' --output 'tsv'");
             $vars{subnet_id} = $subnet_id if ($subnet_id);
+        } elsif (is_gce) {
+            @alternative_zones = split /\s*,\s*/,
+              script_output("gcloud compute zones list --filter='region=" . $region . "' --format=\"value(name.split('-').slice(-1))\" | tr '\n' ','");
+            $vars{availability_zone} = $alternative_zones[0];
         }
         $vars{region} = $self->provider_client->region;
 
@@ -566,8 +571,6 @@ sub terraform_apply {
         # when all instances of certain type are booked in one AZ there is a chance that other AZ in same region still have them
         # to improve test stability let's loop over all available AZ in case initial one throwing error that all instances are booked
         if ($ret != 0 && is_gce() && ($tf_apply_output =~ /A .* VM instance with 1 .* accelerator\(s\) is currently unavailable in the .* zone|Machine type with name .* does not exist in zone .*|The zone 'projects.*' does not have enough resources available to fulfill the request/)) {
-            my $zones_output = script_output("gcloud compute zones list --filter='region=" . $vars{region} . "' --format=\"value(name.split('-').slice(-1))\" | tr '\n' ','");
-            my @alternative_zones = split /\s*,\s*/, $zones_output;
             @alternative_zones = grep { $_ ne $vars{availability_zone} } @alternative_zones;
             record_info('ZONE UNAVAILABLE', "Alternative zones " . join(', ', @alternative_zones));
             for my $az (@alternative_zones) {
