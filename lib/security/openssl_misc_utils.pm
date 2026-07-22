df530d62f1cb915cc33b6ce174fdc9c43fe79e76
Frantisek Simorda <frantisek.simorda@suse.com>
2026-07-22 08:01:13 +0200

    Openssl install adjustment for immutable version

--- lib/security/openssl_misc_utils.pm
+++ lib/security/openssl_misc_utils.pm
@@ -13,6 +13,7 @@ use warnings;
 use testapi;
 use registration 'add_suseconnect_product';
 use utils 'zypper_call';
+use package_utils 'install_package';
 use version_utils qw(is_sle is_sle_micro is_microos is_transactional is_tumbleweed is_rt is_jeos is_sled);
 
 use base 'Exporter';
@@ -48,7 +49,7 @@ sub has_default_openssl3 {
 }
 
 sub install_openssl {
-    zypper_call 'in openssl' unless is_transactional;
+    install_package("openssl", trup_reboot => 1);
     if (is_sle('>=15-SP6') && is_sle('<16')) {
         if (has_no_legacy_module()) {
             install_11_workaround_when_no_legacy();
