2944d6663d87580a3cd0945221ff8ca0e767d135
Huajian.Luo <hluo@suse.com>
2026-07-23 15:18:00 +0800

    Set correct hostname in validate_hostname on s390x
    - Set correct hostname in validate_hostname on s390x
    - Related ticket: https://progress.opensuse.org/issues/188691

--- tests/yam/validate/validate_hostname.pm
+++ tests/yam/validate/validate_hostname.pm
@@ -12,10 +12,13 @@ use Mojo::Base 'consoletest';
 use testapi;
 use scheduler 'get_test_suite_data';
 use Test::Assert ':assert';
+use Utils::Architectures qw(is_s390x is_zvm);
 
 sub run {
     select_console 'root-console';
-    my $expected_install_hostname = get_test_suite_data()->{hostname} // 'localhost';
+    my $expected_install_hostname = is_zvm ? get_required_var('ZVM_GUEST')
+      : is_s390x ? (split(/\./, get_required_var('SUT_IP')))[0]
+      : get_test_suite_data()->{hostname} // 'localhost';
     my $hostname = script_output('hostnamectl hostname');
     assert_str_equals($expected_install_hostname, $hostname, "Wrong hostname. Expected: '$expected_install_hostname', got '$hostname'");
 }
