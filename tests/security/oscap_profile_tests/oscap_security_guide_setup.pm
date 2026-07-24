41f3491848efa6bea4fc7748e66da77e6126dcb3
Ferenc Szekely <feri.szekely@suse.com>
2026-07-24 11:18:28 +0200

    Use -p flag to silence mkdir if the dir already exists
    Add missing development-tools repo to be able to install ninja later on
    
    Fix poo#201894
    
    Signed-off-by: Ferenc Szekely <feri.szekely@suse.com>

--- tests/security/oscap_profile_tests/oscap_security_guide_setup.pm
+++ tests/security/oscap_profile_tests/oscap_security_guide_setup.pm
@@ -1,4 +1,4 @@
-# Copyright 2024 SUSE LLC
+# Copyright SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
 # Summary: Generic test for hardening profile in the 'scap-security-guide': setup environment
@@ -22,6 +22,14 @@ sub run {
     }
     if (get_var('OSCAP_UPLOAD_DEBUG_LOGS')) {
         $oscap_tests::oscap_upload_debug_logs = get_var('OSCAP_UPLOAD_DEBUG_LOGS');
+    }
+
+    # Ugly, but we need the development tools repo in order to satisfy
+    # package requirements of oscap_security_guide_setup.
+    # Particularly: ninja
+    if (is_sle('<16')) {
+        add_suseconnect_product('sle-module-desktop-applications');
+        add_suseconnect_product('sle-module-development-tools');
     }
 
     $self->oscap_security_guide_setup();
