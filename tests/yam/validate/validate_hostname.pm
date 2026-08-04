65b88bdfba73cf107c695a3d6eaad69c72e1e1bd
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2026-08-04 09:24:32 +0200

    Enable sles_extended in s390x

--- tests/yam/validate/validate_hostname.pm
+++ tests/yam/validate/validate_hostname.pm
@@ -16,9 +16,13 @@ use Utils::Architectures qw(is_s390x is_zvm);
 
 sub run {
     select_console 'root-console';
-    my $expected_install_hostname = is_zvm ? get_required_var('ZVM_GUEST')
-      : is_s390x ? (split(/\./, get_required_var('SUT_IP')))[0]
-      : get_test_suite_data()->{hostname} // 'localhost';
+    my $hostname_from_test_data = get_test_suite_data()->{hostname} // 'localhost';
+    my $expected_install_hostname = $hostname_from_test_data eq 'localhost' ? is_zvm
+          ? get_required_var('ZVM_GUEST')
+          : is_s390x ? (split(/\./, get_required_var('SUT_IP')))[0]
+          : 'localhost'
+      : $hostname_from_test_data;
+
     my $hostname = script_output('hostnamectl hostname');
     assert_str_equals($expected_install_hostname, $hostname, "Wrong hostname. Expected: '$expected_install_hostname', got '$hostname'");
 }
