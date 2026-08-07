15d48962cf7ed63be164c997255b89caa95a0385
Oliver Kurz <okurz@suse.de>
2026-08-07 12:04:29 +0200

    fix: remove obsolete device.map from log tarball
    Motivation:
    Under GRUB 2, /boot/grub2/device.map is deprecated and absent by default. The
    tar command failed when attempting to archive this non-existent file.
    
    Design Choices:
    Removed the reference to /boot/grub2/device.map in the tar command arguments.
    
    Benefits:
    Fixes the test suite crash caused by tar returning a non-zero exit code.

--- tests/autoyast/logs.pm
+++ tests/autoyast/logs.pm
@@ -17,7 +17,7 @@ sub run {
     # save all logs that might be useful
 
     script_run "systemctl status > /var/log/systemctl_status ||:";
-    script_run "tar cjf /tmp/logs.tar.bz2 --exclude=/etc/{brltty,udev/hwdb.bin} --exclude=/var/log/{YaST2,zypp,{pbl,zypper}.log} /var/{log,adm/autoinstall} /run/systemd/system/ /usr/lib/systemd/system/ /boot/grub2/{device.map,grub{.cfg,env}} /etc/ ||:", timeout => 300;
+    script_run "tar cjf /tmp/logs.tar.bz2 --exclude=/etc/{brltty,udev/hwdb.bin} --exclude=/var/log/{YaST2,zypp,{pbl,zypper}.log} /var/{log,adm/autoinstall} /run/systemd/system/ /usr/lib/systemd/system/ /boot/grub2/grub{.cfg,env} /etc/ ||:", timeout => 300;
     upload_logs "/tmp/logs.tar.bz2";
     enter_cmd "echo UPLOADFINISH >/dev/$serialdev";
     wait_serial("UPLOADFINISH", 200);
