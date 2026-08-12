25337b8541511135c0a673bd2f03d68c652e682b
José Luis Fernández Aguilera <jose.fernandez1@suse.com>
2026-08-12 10:47:36 +0200

    Enable sles_extended in s390x

--- tests/yam/validate/validate_hostname.pm
+++ tests/yam/validate/validate_hostname.pm
@@ -17,22 +17,27 @@ use Utils::Backends qw(is_svirt);
 
 sub run {
     select_console 'root-console';
-    my @s390x_resolvers = (
-        {
-            match => sub { is_s390x && is_zvm },
-            resolve => sub { get_required_var('ZVM_GUEST') },
-        },
-        {
-            match => sub { is_s390x && is_svirt },
-            resolve => sub { (split(/\./, get_required_var('SUT_IP')))[0] },
-        },
-    );
 
-    my ($active_rule) = grep { $_->{match}->() } @s390x_resolvers;
-
-    my $expected_install_hostname = $active_rule
-      ? $active_rule->{resolve}->()
-      : (get_test_suite_data()->{hostname} // 'localhost');
+    my $expected_install_hostname;
+    if (my $hostname = get_test_suite_data()->{hostname}) {
+        $expected_install_hostname = $hostname;
+    } elsif (is_s390x) {
+        my @s390x_resolvers = (
+            {
+                match => sub { is_zvm },
+                resolve => sub { get_required_var('ZVM_GUEST') },
+            },
+            {
+                match => sub { is_svirt },
+                resolve => sub { (split(/\./, get_required_var('SUT_IP')))[0] },
+            },
+        );
+        my ($active_rule) = grep { $_->{match}->() } @s390x_resolvers
+          or die "Unable to determine expected install hostname for s390x: neither zVM nor sVirt matched";
+        $expected_install_hostname = $active_rule->{resolve}->();
+    } else {
+        $expected_install_hostname = 'localhost';
+    }
 
     my $hostname = script_output('hostnamectl hostname');
     assert_str_equals($expected_install_hostname, $hostname, "Wrong hostname. Expected: '$expected_install_hostname', got '$hostname'");
