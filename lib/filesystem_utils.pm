f48ff633b0238010c2d0ed4cf92392ef2d356dd1
An Long <lan@suse.com>
2026-08-25 11:05:17 +0800

    filesystem_utils: align partition start to 1MB
    Guard `$part_start` in `create_partition` to ensure it is at least 1MB
    instead of extremely small offsets like 0.02MB found on clean GPT tables.
    
    This prevents the GNU parted error "Use a smaller unit instead of a value < 1"
    which occurs when `mkpart` receives fractional MB values less than 1.

--- lib/filesystem_utils.pm
+++ lib/filesystem_utils.pm
@@ -220,12 +220,13 @@ sub create_partition {
     if ($space_size == 0) {
         die 'No space left in device!';
     }
+    # Align partition start to 1MB if it is less than 1MB (e.g., 0.02MiB on a clean GPT table).
+    # This avoids GNU parted error "Use a smaller unit instead of a value < 1"
+    $part_start = $space{start} < 1 ? 1 : $space{start};
     if ($size =~ /max/ || $part_type =~ /extended/) {
-        $part_start = $space{start};
         $part_end = $space{end};
     }
     else {
-        $part_start = $space{start};
         $part_end = int($space{start}) + $size;
     }
     my $cmd = "parted -s -a min $dev mkpart $part_type $part_start" . "MB $part_end" . "MB";
