7dd39bdd134debc67a4043aace525a89d6459d17
Emil Miler <em@0x45.cz>
2026-09-23 17:12:23 +0200

    check_cap_bpf: set kernel bpf parameters after reboot

--- tests/security/ebpf/check_cap_bpf.pm
+++ tests/security/ebpf/check_cap_bpf.pm
@@ -17,14 +17,13 @@ sub run {
     select_console 'root-console';
 
     # Install runtime dependencies
-    install_package("wget", trup_reboot => 1);
+    install_package('wget gcc libcap-progs', trup_reboot => 1);
 
     # Set 'unprivileged_bpf_disabled' to 1
     validate_script_output('sysctl kernel.unprivileged_bpf_disabled=1', sub { m/kernel.unprivileged_bpf_disabled = 1/ });
     validate_script_output("cat /proc/sys/kernel/unprivileged_bpf_disabled", sub { m/1/ });
 
     # Download the C test code and compile
-    install_package('gcc libcap-progs', trup_reboot => 1);
     assert_script_run('cd /tmp');
     assert_script_run('wget ' . autoinst_url . '/data/ebpf/bpf_test.c');
     assert_script_run("gcc -o $f_bpf_test bpf_test.c");
