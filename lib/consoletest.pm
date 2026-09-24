a5e4a19171f29302baf05ee15caf371c9a22ad6a
bzoltan1 <zoltan@bakter.hu>
2026-09-24 20:40:18 +0300

    consoletest: drop the aarch64 serial prompt wait in post_run_hook
    post_run_hook has an aarch64 workaround that waits for the serial
    prompt before assert_script_run "cd". The wait consumes the buffered
    prompt, and "cd" then waits for a prompt the idle shell does not
    re-emit, timing out after 90 s. Each affected module pays it: 34 failed
    waits, about 29 min, per aarch64 job.
    
    assert_script_run already waits for the shell prompt before typing, so
    the extra wait is redundant. Added in 5ea264de46 for poo#183761.

--- lib/consoletest.pm
+++ lib/consoletest.pm
@@ -11,7 +11,6 @@ use known_bugs;
 use version_utils qw(is_public_cloud);
 use Utils::Logging qw(export_logs_basic export_logs_desktop record_avc_selinux_alerts);
 use utils;
-use Utils::Architectures qw(is_aarch64);
 
 =head1 consoletest
 
@@ -28,14 +27,11 @@ Method executed when run() finishes.
 sub post_run_hook {
     my ($self) = @_;
 
-    # https://progress.opensuse.org/issues/183761#note-45
-    wait_serial($testapi::distri->{serial_term_prompt}, timeout => 5, quiet => 1) if is_aarch64;
     # start next test in home directory
     assert_script_run "cd";
 
     $self->record_avc_selinux_alerts();
     # clear screen to make screen content ready for next test
-    wait_serial($testapi::distri->{serial_term_prompt}, timeout => 5, quiet => 1) if is_aarch64;
     $self->clear_and_verify_console;
 }
 
